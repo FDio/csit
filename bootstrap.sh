a0d7b50cbf3a6e089f885960aa5397751bf6f271
Stefan Kobza <skobza@cisco.com>
2016-01-11 18:03:25 +0100

    New version of RF tests.
    Change-Id: I241a2b7a7706e65f71cfd4a62e2a40f053fc5d07
    Signed-off-by: Stefan Kobza <skobza@cisco.com>

---
+++ bootstrap.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+set -euf -o pipefail
+
+#git clone ssh://rotterdam-jobbuilder@gerrit.fd.io:29418/vpp
+#
+#cd vpp/build-root
+#./bootstrap.sh
+#make PLATFORM=vpp TAG=vpp_debug install-deb
+#
+#ls -la
+
+IFS=',' read -ra ADDR <<< "${JCLOUDS_IPS}"
+
+function ssh_do() {
+    echo
+    echo "### "  ssh $@
+    ssh $@
+}
+
+
+for addr in "${ADDR[@]}"; do
+    echo
+    echo ${addr}
+    echo
+
+    ssh_do localadmin@${addr} hostname || true
+    ssh_do localadmin@${addr} ifconfig -a || true
+    ssh_do localadmin@${addr} lspci -Dnn || true
+    ssh_do localadmin@${addr} "lspci -Dnn | grep 0200" || true
+    ssh_do localadmin@${addr} free -m || true
+    ssh_do localadmin@${addr} cat /proc/meminfo || true
+done
+
+
+
+
