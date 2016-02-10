3e9c12838ae2c86e525de7e2476270a6e7a517ad
Stefan Kobza <skobza@cisco.com>
2016-02-10 19:41:09 +0100

    Test VIRL connection.
    Change-Id: I812ff9c8c9669b63907469c643c839e8bd3b419a
    Signed-off-by: Stefan Kobza <skobza@cisco.com>

--- resources/libraries/bash/dut_setup.sh
+++ resources/libraries/bash/dut_setup.sh
@@ -28,12 +28,17 @@ echo
 cat /proc/meminfo
 
 echo
-echo See vpe process
+echo See vpp process
 echo
-ps aux | grep vpe
+ps aux | grep vpp
 
 echo
 echo See free memory
 echo
 free -m
 
+echo UUID
+sudo dmidecode | grep UUID
+
+echo Add dpdk-input trace
+sudo vpp_api_test <<< "exec trace add dpdk-input 100"
