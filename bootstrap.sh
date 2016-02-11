188a54642b3b94a34c4cfe12e8be26d452548294
Miroslav Miklus <mmiklus@cisco.com>
2016-02-11 15:18:12 +0100

    LF testbed2 topology file
    Change-Id: Ia03c363e2b3c77a4e469509460b25028e5e1814f
    Signed-off-by: Miroslav Miklus <mmiklus@cisco.com>

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-set -euf -o pipefail
+#set -euf -o pipefail
 
 #git clone ssh://rotterdam-jobbuilder@gerrit.fd.io:29418/vpp
 #
@@ -11,15 +11,15 @@ set -euf -o pipefail
 
 set -x
 
-ping 10.30.51.17 -w 3 || true
-ping 10.30.51.18 -w 3 || true
-ping 10.30.51.16 -w 3 || true
-ping 10.30.51.21 -w 3 || true
-ping 10.30.51.22 -w 3 || true
-ping 10.30.51.20 -w 3 || true
-ping 10.30.51.25 -w 3 || true
-ping 10.30.51.26 -w 3 || true
-ping 10.30.51.24 -w 3 || true
+#ping 10.30.51.17 -w 3 || true
+#ping 10.30.51.18 -w 3 || true
+#ping 10.30.51.16 -w 3 || true
+#ping 10.30.51.21 -w 3 || true
+#ping 10.30.51.22 -w 3 || true
+#ping 10.30.51.20 -w 3 || true
+#ping 10.30.51.25 -w 3 || true
+#ping 10.30.51.26 -w 3 || true
+#ping 10.30.51.24 -w 3 || true
 
 
 #IFS=',' read -ra ADDR <<< "${JCLOUDS_IPS}"
@@ -47,5 +47,11 @@ ping 10.30.51.24 -w 3 || true
 #done
 
 
+virtualenv env
+. env/bin/activate
 
+echo pip install
+pip install -r requirements.txt
+
+PYTHONPATH=`pwd` pybot -L TRACE -v TOPOLOGY_PATH:topologies/available/lf_testbed2.yaml tests
 
