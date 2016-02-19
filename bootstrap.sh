464b9572f5fbf52c0f71d60f5b65065dac6f6fc3
Miroslav Miklus <mmiklus@cisco.com>
2016-02-19 09:36:58 +0100

    Bootstrap for performance suite
    Change-Id: I9ac58cd12b439c95b2c6aeb0efdcbd6f97d4c1ab
    Signed-off-by: Miroslav Miklus <mmiklus@cisco.com>

--- bootstrap.sh
+++ bootstrap.sh
@@ -32,10 +32,13 @@ virtualenv env
 echo pip install
 pip install -r requirements.txt
 
-PYTHONPATH=`pwd` pybot -L TRACE \
-    -v TOPOLOGY_PATH:topologies/available/virl.yaml \
-    --include vm_env \
-    --include 3_NODE_SINGLE_LINK_TOPO \
-    --exclude 3_node_double_link_topoNOT3_node_single_link_topo \
-    --exclude PERFTEST \
-    tests/
+#PYTHONPATH=`pwd` pybot -L TRACE \
+#    -v TOPOLOGY_PATH:topologies/available/virl.yaml \
+#    --include vm_env \
+#    --include 3_NODE_SINGLE_LINK_TOPO \
+#    --exclude 3_node_double_link_topoNOT3_node_single_link_topo \
+#    --exclude PERFTEST \
+#    tests/
+
+#PYTHONPATH=`pwd` pybot -L TRACE -v TOPOLOGY_PATH:topologies/available/lf_testbed2.yaml -s performance tests || true
+PYTHONPATH=`pwd` pybot -L TRACE -v TOPOLOGY_PATH:topologies/available/lf_testbed2-710-520.yaml -s performance tests || true
