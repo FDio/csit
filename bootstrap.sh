bfe330f8e289f50ce1404204585888c7c5db203f
Juraj Sloboda <jsloboda@cisco.com>
2016-02-23 15:03:13 +0100

    Modify sweep ping test cases
    - Write separate sweep ping test cases for jumbo frames
    - Compute sweep ping end size from reported MTU on DUT interface
    - Set MTU on TG according to MTU on DUT interface
    - Log VPP packet traces on IPv4 and IPv6 tests failure
    - Remove VM_ENV tag from sweep ping test cases for jumbo frames
    
    Change-Id: I47aa7977bcff9c4366c67578aef542924a1d055b
    Signed-off-by: Juraj Sloboda <jsloboda@cisco.com>

--- bootstrap.sh
+++ bootstrap.sh
@@ -173,9 +173,8 @@ pip install -r requirements.txt
 
 PYTHONPATH=`pwd` pybot -L TRACE \
     -v TOPOLOGY_PATH:topologies/enabled/topology.yaml \
-    --include vm_env \
-    --include 3_NODE_SINGLE_LINK_TOPO \
     --exclude 3_node_double_link_topoNOT3_node_single_link_topo \
+    --include VM_ENV \
     --exclude PERFTEST \
     --noncritical EXPECTED_FAILING \
     tests/
