cdf3213528f5f560d8d8b92c642f655cef706745
Miroslav Miklus <mmiklus@cisco.com>
2016-03-01 16:11:34 +0100

    Performance suite reorg, v4 tests
    - perf suite splitted to
    	1.xconnect
    	2.bridge-domain
    	3.IPv4
    - IPv4 perf tests implemented
    - renamed perf tests prefix 2core -> 1core
    
    Change-Id: I9ef2dbdfcfdcb36e8865989e6255d69ced33269c
    Signed-off-by: Miroslav Miklus <mmiklus@cisco.com>

--- resources/libraries/python/TrafficGenerator.py
+++ resources/libraries/python/TrafficGenerator.py
@@ -25,6 +25,8 @@ __all__ = ['TrafficGenerator']
 class TrafficGenerator(object):
     """Traffic Generator"""
 
+    ROBOT_LIBRARY_SCOPE = 'TEST SUITE'
+
     def __init__(self):
         self._result = None
         self._loss = None
@@ -33,15 +35,30 @@ class TrafficGenerator(object):
         #T-REX interface order mapping
         self._ifaces_reordered = 0
 
-    def initialize_traffic_generator(self, node, interface1, interface2, test_type):
+    def initialize_traffic_generator(self, tg_node, tg_if1, tg_if2,
+                                     dut1_node, dut1_if1, dut1_if2,
+                                     dut2_node, dut2_if1, dut2_if2,
+                                     test_type):
         """TG initialization
-        :param node: Traffic generator node
-        :param interface1: interface name of first interface
-        :param interface2: interface name of second interface
+        :param tg_node: Traffic generator node
+        :param tg_if1: TG - name of first interface
+        :param tg_if2: TG - name of second interface
+        :param dut1_node: DUT1 node
+        :param dut1_if1: DUT1 - name of first interface
+        :param dut1_if2: DUT1 - name of second interface
+        :param dut2_node: DUT2 node
+        :param dut2_if1: DUT2 - name of first interface
+        :param dut2_if2: DUT2 - name of second interface
         :test_type: 'L2' or 'L3' - src/dst MAC address
-        :type node: dict
-        :type interface1: str
-        :type interface2: str
+        :type tg_node: dict
+        :type tg_if1: str
+        :type tg_if2: str
+        :type dut1_node: dict
+        :type dut1_if1: str
+        :type dut1_if2: str
+        :type dut2_node: dict
+        :type dut2_if1: str
+        :type dut2_if2: str
         :type test_type: str
         :return: nothing
         """
@@ -50,48 +67,55 @@ class TrafficGenerator(object):
 
         topo = Topology()
 
-        if node['type'] != NodeType.TG:
+        if tg_node['type'] != NodeType.TG:
             raise Exception('Node type is not a TG')
-        if node['subtype'] == NodeSubTypeTG.TREX:
+        if tg_node['subtype'] == NodeSubTypeTG.TREX:
             ssh = SSH()
-            ssh.connect(node)
+            ssh.connect(tg_node)
 
-            iface1_pci = topo.get_interface_pci_addr(node, interface1)
-            iface2_pci = topo.get_interface_pci_addr(node, interface2)
-            iface1_mac = topo.get_interface_mac(node, interface1)
-            iface2_mac = topo.get_interface_mac(node, interface2)
-            if min(iface1_pci, iface2_pci) != iface1_pci:
-                iface1_mac, iface2_mac = iface2_mac, iface1_mac
-                iface1_pci, iface2_pci = iface2_pci, iface1_pci
-                self._ifaces_reordered = 1
-
-            iface1_mac_hex = "0x"+iface1_mac.replace(":", ",0x")
-            iface2_mac_hex = "0x"+iface2_mac.replace(":", ",0x")
+            if1_pci = topo.get_interface_pci_addr(tg_node, tg_if1)
+            if2_pci = topo.get_interface_pci_addr(tg_node, tg_if2)
+            if1_mac = topo.get_interface_mac(tg_node, tg_if1)
+            if2_mac = topo.get_interface_mac(tg_node, tg_if2)
 
             if test_type == 'L2':
-                (ret, stdout, stderr) = ssh.exec_command(
-                    "sudo sh -c 'cat << EOF > /etc/trex_cfg.yaml\n"
-                    "- port_limit      : 2\n"
-                    "  version         : 2\n"
-                    "  interfaces      : [\"{}\",\"{}\"]\n"
-                    "  port_info       :\n"
-                    "          - dest_mac        :   [{}]\n"
-                    "            src_mac         :   [{}]\n"
-                    "          - dest_mac        :   [{}]\n"
-                    "            src_mac         :   [{}]\n"
-                    "EOF'"\
-                    .format(iface1_pci, iface2_pci,
-                            iface2_mac_hex, iface1_mac_hex,
-                            iface1_mac_hex, iface2_mac_hex))
-                if int(ret) != 0:
-                    logger.error("failed to create t-rex config: {}"\
-                    .format(stdout + stderr))
-                    raise RuntimeError('trex config generation error')
+                if1_adj_mac = if2_mac
+                if2_adj_mac = if1_mac
             elif test_type == 'L3':
-                raise NotImplementedError("L3 test_type not supported")
+                if1_adj_mac = topo.get_interface_mac(dut1_node, dut1_if1)
+                if2_adj_mac = topo.get_interface_mac(dut2_node, dut2_if2)
             else:
                 raise Exception("test_type unknown")
 
+            if min(if1_pci, if2_pci) != if1_pci:
+                if1_mac, if2_mac = if2_mac, if1_mac
+                if1_pci, if2_pci = if2_pci, if1_pci
+                if1_adj_mac, if2_adj_mac = if2_adj_mac, if1_adj_mac
+                self._ifaces_reordered = 1
+
+            if1_mac_hex = "0x"+if1_mac.replace(":", ",0x")
+            if2_mac_hex = "0x"+if2_mac.replace(":", ",0x")
+            if1_adj_mac_hex = "0x"+if1_adj_mac.replace(":", ",0x")
+            if2_adj_mac_hex = "0x"+if2_adj_mac.replace(":", ",0x")
+
+            (ret, stdout, stderr) = ssh.exec_command(
+                "sudo sh -c 'cat << EOF > /etc/trex_cfg.yaml\n"
+                "- port_limit      : 2\n"
+                "  version         : 2\n"
+                "  interfaces      : [\"{}\",\"{}\"]\n"
+                "  port_info       :\n"
+                "          - dest_mac        :   [{}]\n"
+                "            src_mac         :   [{}]\n"
+                "          - dest_mac        :   [{}]\n"
+                "            src_mac         :   [{}]\n"
+                "EOF'"\
+                .format(if1_pci, if2_pci,
+                        if1_adj_mac_hex, if1_mac_hex,
+                        if2_adj_mac_hex, if2_mac_hex))
+            if int(ret) != 0:
+                logger.error("failed to create t-rex config: {}"\
+                .format(stdout + stderr))
+                raise RuntimeError('trex config generation error')
 
             (ret, stdout, stderr) = ssh.exec_command(
                 "sh -c 'cd {0}/scripts/ && "
@@ -124,6 +148,9 @@ class TrafficGenerator(object):
             ssh.connect(node)
             (ret, stdout, stderr) = ssh.exec_command(
                 "sh -c 'sudo pkill t-rex'")
+            if int(ret) != 0:
+                logger.error('pkill t-rex failed: {0}'.format(stdout + stderr))
+                raise RuntimeError('pkill t-rex failed')
 
     def send_traffic_on(self, nodes_info, duration, rate,
                         framesize, traffic_type):
@@ -155,28 +182,38 @@ class TrafficGenerator(object):
         ssh.connect(node)
 
         if node['subtype'] == NodeSubTypeTG.TREX:
+
+            _p0 = 1
+            _p1 = 2
+
+            if self._ifaces_reordered != 0:
+                _p0, _p1 = _p1, _p0
+
             if traffic_type in ["3-node-xconnect", "3-node-bridge"]:
                 (ret, stdout, stderr) = ssh.exec_command(
-                    "sh -c '/tmp/openvpp-testing/resources/tools/t-rex/t-rex-stateless.py "
+                    "sh -c '/tmp/openvpp-testing/resources/tools/t-rex/"
+                    "t-rex-stateless.py "
                     "-d {0} -r {1} -s {2} "
-                    "--p1_src_start_ip 10.10.10.1 "
-                    "--p1_src_end_ip 10.10.10.254 "
-                    "--p1_dst_start_ip 20.20.20.1 "
-                    "--p2_src_start_ip 20.20.20.1 "
-                    "--p2_src_end_ip 20.20.20.254 "
-                    "--p2_dst_start_ip 10.10.10.1'".\
-                    format(duration, rate, framesize), timeout=int(duration)+60)
+                    "--p{3}_src_start_ip 10.10.10.1 "
+                    "--p{3}_src_end_ip 10.10.10.254 "
+                    "--p{3}_dst_start_ip 20.20.20.1 "
+                    "--p{4}_src_start_ip 20.20.20.1 "
+                    "--p{4}_src_end_ip 20.20.20.254 "
+                    "--p{4}_dst_start_ip 10.10.10.1'".\
+                    format(duration, rate, framesize, _p0, _p1),\
+                    timeout=int(duration)+60)
             elif traffic_type in ["3-node-IPv4"]:
                 (ret, stdout, stderr) = ssh.exec_command(
-                    "sh -c '/tmp/openvpp-testing/resources/tools/t-rex/t-rex-stateless.py "
+                    "sh -c '/tmp/openvpp-testing/resources/tools/t-rex/"
+                    "t-rex-stateless.py "
                     "-d {0} -r {1} -s {2} "
-                    "--p1_src_start_ip 10.10.10.2 "
-                    "--p1_src_end_ip 10.10.10.254 "
-                    "--p1_dst_start_ip 20.20.20.2 "
-                    "--p2_src_start_ip 20.20.20.2 "
-                    "--p2_src_end_ip 20.20.20.254 "
-                    "--p2_dst_start_ip 10.10.10.2'".\
-                    format(duration, rate, framesize),\
+                    "--p{3}_src_start_ip 10.10.10.2 "
+                    "--p{3}_src_end_ip 10.10.10.254 "
+                    "--p{3}_dst_start_ip 20.20.20.2 "
+                    "--p{4}_src_start_ip 20.20.20.2 "
+                    "--p{4}_src_end_ip 20.20.20.254 "
+                    "--p{4}_dst_start_ip 10.10.10.2'".\
+                    format(duration, rate, framesize, _p0, _p1),\
                     timeout=int(duration)+60)
             else:
                 raise NotImplementedError('Unsupported traffic type')
