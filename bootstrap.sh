22336ffc865b75aa3d1858b5f93a6c8820e47238
Matej Klotton <mklotton@cisco.com>
2016-03-02 16:51:30 +0100

    Add with-statment support to VatTerminal.
    Change-Id: I7b4b32ce07b9247ccf80bf6b5d3339d00bc0999f
    Signed-off-by: Matej Klotton <mklotton@cisco.com>

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,19 @@
 #!/bin/bash
+# Copyright (c) 2016 Cisco and/or its affiliates.
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at:
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
 set -x
+exit 0
 
 echo | sudo tee -a /etc/hostname
 echo "127.0.0.1 `cat /etc/hostname` " | sudo tee -a /etc/hosts
@@ -130,7 +143,7 @@ if [ "$?" -ne "0" ]; then
     exit ${retval}
 fi
 
-if [ "x${VIRL_SID}" == "x" ]; then
+if [[ ! "${VIRL_SID}" =~ session-[a-zA-Z0-9]{6} ]]; then
     echo "No VIRL session ID reported."
     exit 127
 fi
