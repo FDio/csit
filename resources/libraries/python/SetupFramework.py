210ee733f5e0c9384ea6bba750729f8d5cd90215
Stefan Kobza <skobza@cisco.com>
2016-03-05 10:19:16 +0100

    Add Vagrantfile for local testing.
    Vagrantfile contains 3 VMs as of now, 2 DUTs 1 TG, with these notes:
     - login is csit/csit
     - by default provision script installs all deb packages from the dir
        where Vagrantfile is
     - developed for, and only tested on vbox (someone can pick up vmware)
     - All nodes have 1 shared mgmt network: 192.168.255.0/24
     - hosts have these IP addresses in host-only network
        TG : 192.168.255.100
        DUT1 : 192.168.255.101
        DUT2 : 192.168.255.102
     - script created to download MAC address information
     - PCI addresses are always the same for vbox (not sure about vmware)
    
    HOWTO (will create a wiki page once one is created for CSIT project):
     - copy Vagrantfile to separate dir on host
     - vagrant up --parallel
        sit-back-and-relax
     - from VM that has access to the same host-only network (192.168.255.0 above)
        - copy your ssh-key to csit@192.168.255.{101,102,250} using
            ssh-copy-id
        - cd ${csit_dir}
        - virtualenv & pip as in README
        - export PYTHONPATH=${csit_dir}
        - resources/tools/topology/update_topology.py -v -f
            -o topologies/available/vagrant_pci.yaml \
            topologies/available/vagrant.yaml
        - pybot -L TRACE \
            -v TOPOLOGY_PATH:topologies/available/vagrant_pci.yaml -s \
            "ipv4" tests
        - see tests results
    
    Change-Id: Ic27626605a9c820bca977b38f4e8ca37d1504ff5
    Signed-off-by: Stefan Kobza <skobza@cisco.com>

--- resources/libraries/python/SetupFramework.py
+++ resources/libraries/python/SetupFramework.py
@@ -11,19 +11,25 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+"""This module exists to provide setup utilities for the framework on topology
+nodes. All tasks required to be run before the actual tests are started is
+supposed to end up here.
+"""
+
 import shlex
 from subprocess import Popen, PIPE, call
 from multiprocessing import Pool
 from tempfile import NamedTemporaryFile
 from os.path import basename
+
 from robot.api import logger
 from robot.libraries.BuiltIn import BuiltIn
-from ssh import SSH
-from constants import Constants as con
-from topology import NodeType
+
+from resources.libraries.python.ssh import SSH
+from resources.libraries.python.constants import Constants as con
+from resources.libraries.python.topology import NodeType
 
 __all__ = ["SetupFramework"]
-
 
 def pack_framework_dir():
     """Pack the testing WS into temp file, return its name."""
@@ -48,6 +54,14 @@ def pack_framework_dir():
 
 
 def copy_tarball_to_node(tarball, node):
+    """Copy tarball file from local host to remote node.
+
+    :param tarball: path to tarball to upload
+    :param node: dictionary created from topology
+    :type tarball: string
+    :type node: dict
+    :return: nothing
+    """
     logger.console('Copying tarball to {0}'.format(node['host']))
     ssh = SSH()
     ssh.connect(node)
@@ -56,6 +70,16 @@ def copy_tarball_to_node(tarball, node):
 
 
 def extract_tarball_at_node(tarball, node):
+    """Extract tarball at given node.
+
+    Extracts tarball using tar on given node to specific CSIT loocation.
+
+    :param tarball: path to tarball to upload
+    :param node: dictionary created from topology
+    :type tarball: string
+    :type node: dict
+    :return: nothing
+    """
     logger.console('Extracting tarball to {0} on {1}'.format(
         con.REMOTE_FW_DIR, node['host']))
     ssh = SSH()
@@ -63,7 +87,7 @@ def extract_tarball_at_node(tarball, node):
 
     cmd = 'sudo rm -rf {1}; mkdir {1} ; tar -zxf {0} -C {1}; ' \
         'rm -f {0}'.format(tarball, con.REMOTE_FW_DIR)
-    (ret_code, stdout, stderr) = ssh.exec_command(cmd, timeout=30)
+    (ret_code, _, stderr) = ssh.exec_command(cmd, timeout=30)
     if 0 != ret_code:
         logger.error('Unpack error: {0}'.format(stderr))
         raise Exception('Failed to unpack {0} at node {1}'.format(
@@ -77,9 +101,9 @@ def create_env_directory_at_node(node):
     ssh = SSH()
     ssh.connect(node)
     (ret_code, stdout, stderr) = ssh.exec_command(
-            'cd {0} && rm -rf env && virtualenv env && '
-            '. env/bin/activate && '
-            'pip install -r requirements.txt'.format(con.REMOTE_FW_DIR), timeout=100)
+        'cd {0} && rm -rf env && virtualenv env && . env/bin/activate && '
+        'pip install -r requirements.txt'.format(con.REMOTE_FW_DIR),
+                                                 timeout=100)
     if 0 != ret_code:
         logger.error('Virtualenv creation error: {0}'.format(stdout + stderr))
         raise Exception('Virtualenv setup failed')
@@ -87,18 +111,32 @@ def create_env_directory_at_node(node):
         logger.console('Virtualenv created on {0}'.format(node['host']))
 
 def setup_node(args):
+    """Run all set-up methods for a node.
+
+    This method is used as map_async parameter. It receives tuple with all
+    parameters as passed to map_async function.
+
+    :param args: all parameters needed to setup one node
+    :type args: tuple
+    :return: nothing
+    """
     tarball, remote_tarball, node = args
     copy_tarball_to_node(tarball, node)
     extract_tarball_at_node(remote_tarball, node)
     if node['type'] == NodeType.TG:
         create_env_directory_at_node(node)
-
+    logger.console('Setup of node {0} done'.format(node['host']))
 
 def delete_local_tarball(tarball):
+    """Delete local tarball to prevent disk pollution.
+
+    :param tarball: path to tarball to upload
+    :type tarball: string
+    :return: nothing
+    """
     call(shlex.split('sh -c "rm {0} > /dev/null 2>&1"'.format(tarball)))
 
-
-class SetupFramework(object):
+class SetupFramework(object): # pylint: disable=too-few-public-methods
     """Setup suite run on topology nodes.
 
     Many VAT/CLI based tests need the scripts at remote hosts before executing
@@ -109,7 +147,8 @@ class SetupFramework(object):
     def __init__(self):
         pass
 
-    def setup_framework(self, nodes):
+    @staticmethod
+    def setup_framework(nodes):
         """Pack the whole directory and extract in temp on each node."""
 
         tarball = pack_framework_dir()
@@ -136,3 +175,5 @@ class SetupFramework(object):
         BuiltIn().set_log_level(log_level)
         logger.trace('Test framework copied to all topology nodes')
         delete_local_tarball(tarball)
+        logger.console('All nodes are ready')
+
