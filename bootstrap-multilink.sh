2287824d00ae8a3616103699a8496c3fbc938d2b
Stefan Kobza <skobza@cisco.com>
2016-03-05 11:01:01 +0100

    WORK IN PROGRESS, LF VM integration.
    Change-Id: I8113b83656974805e4fdb39829a559687d4be2ca
    Signed-off-by: Stefan Kobza <skobza@cisco.com>

--- bootstrap-multilink.sh
+++ bootstrap-multilink.sh
@@ -12,21 +12,47 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+set -x
+
+cat /etc/nodepool/*
+
+id
+
+function ssh_do() {
+    echo
+    echo "### "  ssh $@
+    ssh -i /etc/nodepool/id_rsa -o StrictHostKeyChecking=no $@
+}
+
+ssh -vv jenkins@`cat /etc/nodepool/primary_node_private` ifconfig -a
+
+for addr in `cat /etc/nodepool/sub_nodes_private`; do
+    echo
+    echo ${addr}
+    echo
+
+    ssh_do jenkins@${addr} hostname || true
+    ssh_do jenkins@${addr} "ifconfig -a" || true
+    ssh_do jenkins@${addr} "lspci -Dnn | grep 0200" || true
+    ssh_do jenkins@${addr} "free -m" || true
+    ssh_do jenkins@${addr} "cat /proc/meminfo" || true
+    ssh_do jenkins@${addr} "dpkg -l vpp\*" || true
+    ssh_do jenkins@${addr} "sudo lshw -c network" || true
+    ssh_do jenkins@${addr} "sudo cat /proc/cpuinfo" || true
+done
 
 
-sudo apt-get -y install python-virtualenv
-
-virtualenv env
-. env/bin/activate
-pip install -r requirements.txt
-
-cat > mock.robot <<EOF
-*** test cases ***
-| Temoporary placeholder test for multilink
-| | log | nothing here to see
-EOF
-
-pybot mock.robot
-
-
-exit 0
+#virtualenv env
+#. env/bin/activate
+#
+#echo pip install
+#pip install -r requirements.txt
+#
+#PYTHONPATH=`pwd` pybot -L TRACE \
+#    -v TOPOLOGY_PATH:topologies/enabled/topology.yaml \
+#    --include vm_env \
+#    --include 3_NODE_SINGLE_LINK_TOPO \
+#    --exclude 3_node_double_link_topoNOT3_node_single_link_topo \
+#    --exclude PERFTEST \
+#    --noncritical EXPECTED_FAILING \
+#    tests/
