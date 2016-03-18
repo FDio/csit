46915c54a522e0acd43812c45ee6297639f0a55b
Miroslav Miklus <mmiklus@cisco.com>
2016-03-18 18:28:26 +0100

    Lab reservation tool
    This tool provide client-based reservation system of tesbeds
    by using existing topology files.
    
    Change-Id: Ic0be944c77c9d6aa28e3fa7fa69e31d19861416f
    Signed-off-by: Miroslav Miklus <mmiklus@cisco.com>

--- bootstrap-hw-tb2.sh
+++ bootstrap-hw-tb2.sh
@@ -1,6 +1,24 @@
 #!/bin/bash
+# Copyright (c) 2016 Cisco and/or its affiliates.
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at:
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
 set -x
+
+# space separated list of available testbeds, described by topology files
+TOPOLOGIES="topologies/available/lf_testbed2-710-520.yaml"
+
+CUR_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+WORKING_TOPOLOGY=""
 
 sudo apt-get -y update
 sudo apt-get -y install libpython2.7-dev python-virtualenv
@@ -11,6 +29,37 @@ virtualenv env
 echo pip install
 pip install -r requirements.txt
 
+#we iterate over available topologies and wait until we reserve topology
+while :; do
+    for TOPOLOGY in ${TOPOLOGIES};
+    do
+        python ${CUR_DIR}/resources/tools/topo_reservation.py -t ${TOPOLOGY}
+        if [ $? -eq 0 ]; then
+            WORKING_TOPOLOGY=${TOPOLOGY}
+            echo "Reserved: ${WORKING_TOPOLOGY}"
+            break
+        fi
+    done
+
+    if [ ! -z "${WORKING_TOPOLOGY}" ]; then
+        #exit the infinite while loop if we made a reservation
+        break
+    fi
+
+    #wait 20 - 30 sec. before next try
+    SLEEP_TIME=$[ ( $RANDOM % 20 ) + 10 ]s
+    echo "Sleeping ${SLEEP_TIME}"
+    sleep ${SLEEP_TIME}
+done
+
+function cancel_reservation {
+    python ${CUR_DIR}/resources/tools/topo_reservation.py -c -t $1
+}
+
+#on script exit we cancel the reservation
+trap "cancel_reservation ${WORKING_TOPOLOGY}" EXIT
+
+#run performance test suite
 PYTHONPATH=`pwd` pybot -L TRACE \
-    -v TOPOLOGY_PATH:topologies/available/lf_testbed2-710-520.yaml \
+    -v TOPOLOGY_PATH:${WORKING_TOPOLOGY} \
     -s performance tests/
