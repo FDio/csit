e7491c2366351c3cb8c0ff65b28caf0e3cbaeb6d
Miroslav Miklus <mmiklus@cisco.com>
2016-04-29 13:55:40 +0200

    Update of vpp build for perf tests
    build: 1.0.0-437~g8f15e92_amd64
    
    Change-Id: Ie0d0f693bcae0d0e56472c25b6353890d07e85d9
    Signed-off-by: Miroslav Miklus <mmiklus@cisco.com>

--- bootstrap-verify-perf.sh
+++ bootstrap-verify-perf.sh
@@ -16,7 +16,7 @@ set -x
 
 # Space separated list of available testbeds, described by topology files
 TOPOLOGIES="topologies/available/lf_testbed2-710-520.yaml"
-VPP_STABLE_VER="1.0.0-369~g106f0ed_amd64"
+VPP_STABLE_VER="1.0.0-437~g8f15e92_amd64"
 VPP_REPO_URL="https://nexus.fd.io/service/local/repositories/fd.io.dev/content/io/fd/vpp"
 
 # Reservation dir
