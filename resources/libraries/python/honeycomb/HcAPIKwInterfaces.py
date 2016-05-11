21ec3d2b03e45637158d51896818903dd0ebd741
selias <samelias@cisco.com>
2016-05-11 11:42:54 +0200

    Update Honeycomb interface IPv4 test
     - add verification of ipv4 subnet prefix support
     - modify keywords to allow setting ipv4 address with network prefix
     - fix variable definitions to evaluate numbers as ints, not strings
    
    Change-Id: I9343ceb35856ddb33674d7067f1def6d40e99acc
    Signed-off-by: selias <samelias@cisco.com>

--- resources/libraries/python/honeycomb/HcAPIKwInterfaces.py
+++ resources/libraries/python/honeycomb/HcAPIKwInterfaces.py
@@ -23,6 +23,7 @@ from resources.libraries.python.honeycomb.HoneycombUtil \
     import DataRepresentation
 from resources.libraries.python.honeycomb.HoneycombUtil \
     import HoneycombUtil as HcUtil
+from resources.libraries.python.IPUtil import convert_ipv4_netmask_prefix
 
 
 class InterfaceKeywords(object):
@@ -331,7 +332,7 @@ class InterfaceKeywords(object):
             node, interface, path, value)
 
     @staticmethod
-    def add_first_ipv4_address(node, interface, ip_addr, netmask):
+    def add_first_ipv4_address(node, interface, ip_addr, network):
         """Add the first IPv4 address.
 
         If there are any other addresses configured, they will be removed.
@@ -339,39 +340,52 @@ class InterfaceKeywords(object):
         :param node: Honeycomb node.
         :param interface: The name of interface.
         :param ip_addr: IPv4 address to be set.
-        :param netmask: Netmask.
+        :param network: Netmask or length of network prefix.
         :type node: dict
         :type interface: str
         :type ip_addr: str
-        :type netmask: str
+        :type network: str or int
         :return: Content of response.
         :rtype: bytearray
         """
 
         path = ("interfaces", ("interface", "name", interface), "ietf-ip:ipv4")
-        address = {"address": [{"ip": ip_addr, "netmask": netmask}, ]}
+        if isinstance(network, basestring):
+            address = {"address": [{"ip": ip_addr, "netmask": network}, ]}
+        elif isinstance(network, int) and network in range(33):
+            address = {"address": [{"ip": ip_addr, "prefix-length": network}, ]}
+        else:
+            raise HoneycombError("Value {0} is not a valid netmask or network "
+                                 "prefix length.".format(network))
         return InterfaceKeywords._set_interface_properties(
             node, interface, path, address)
 
     @staticmethod
-    def add_ipv4_address(node, interface, ip_addr, netmask):
+    def add_ipv4_address(node, interface, ip_addr, network):
         """Add IPv4 address.
 
         :param node: Honeycomb node.
         :param interface: The name of interface.
         :param ip_addr: IPv4 address to be set.
-        :param netmask: Netmask.
+        :param network: Netmask or length of network prefix.
         :type node: dict
         :type interface: str
         :type ip_addr: str
-        :type netmask: str
+        :type network: str or int
         :return: Content of response.
         :rtype: bytearray
         """
 
         path = ("interfaces", ("interface", "name", interface), "ietf-ip:ipv4",
                 "address")
-        address = [{"ip": ip_addr, "prefix-length": netmask}, ]
+        if isinstance(network, basestring):
+            network = convert_ipv4_netmask_prefix(network)
+        elif isinstance(network, int) and network in range(33):
+            pass
+        else:
+            raise HoneycombError("Value {0} is not a valid netmask or network "
+                                 "prefix length.".format(network))
+        address = {"address": [{"ip": ip_addr, "prefix-length": network}, ]}
         return InterfaceKeywords._set_interface_properties(
             node, interface, path, address)
 
