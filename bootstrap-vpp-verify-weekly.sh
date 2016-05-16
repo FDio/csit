ee89fd7257288816b1c98b239f438d452764a0de
Jan Gelety <jgelety@cisco.com>
2016-05-16 15:35:29 +0200

    Update vpp stable build used in csit-vpp-verify-weekly jenkins job
    - vpp build version updated to release 1.0.0-451~g686a5a5_amd64
    
    Change-Id: Ic7d177d72a6d50c083d5b9733c8f63298eebd7c6
    Signed-off-by: Jan Gelety <jgelety@cisco.com>

--- bootstrap-vpp-verify-weekly.sh
+++ bootstrap-vpp-verify-weekly.sh
@@ -69,7 +69,7 @@ if [ "${#}" -ne "0" ]; then
     arr=(${@})
     echo ${arr[0]}
 else
-    VPP_STABLE_VER="1.0.0-437~g8f15e92_amd64"
+    VPP_STABLE_VER="1.0.0-451~g686a5a5_amd64"
     VPP_REPO_URL="https://nexus.fd.io/service/local/repositories/fd.io.dev/content/io/fd/vpp"
     wget -q "${VPP_REPO_URL}/vpp/${VPP_STABLE_VER}/vpp-${VPP_STABLE_VER}.deb" || exit
     wget -q "${VPP_REPO_URL}/vpp-dbg/${VPP_STABLE_VER}/vpp-dbg-${VPP_STABLE_VER}.deb" || exit
