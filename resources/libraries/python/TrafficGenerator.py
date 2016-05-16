35bee5e3075afa1f7084a6e1195a6a1403091560
Peter Mikus <pmikus@cisco.com>
2016-05-16 09:55:39 +0200

    Patch: Detection of running t-rex instance before initializing
    - JIRA: CSIT-89
    - Add detection of running t-rex traffic generator before new instance
      is initialized.
    
    Change-Id: I77009b0c5e83bb6e162b9800d02b4e5a2abd541f
    Signed-off-by: Peter Mikus <pmikus@cisco.com>

--- resources/libraries/python/TrafficGenerator.py
+++ resources/libraries/python/TrafficGenerator.py
@@ -168,6 +168,9 @@ class TrafficGenerator(object):
                 raise RuntimeError('trex-cfg failed')
 
             (ret, _, _) = ssh.exec_command(
+                "sh -c 'pgrep t-rex && sudo pkill t-rex'")
+
+            (ret, _, _) = ssh.exec_command(
                 "sh -c 'cd {0}/scripts/ && "
                 "sudo nohup ./t-rex-64 -i -c 7 --iom 0 > /dev/null 2>&1 &'"
                 "> /dev/null"\
