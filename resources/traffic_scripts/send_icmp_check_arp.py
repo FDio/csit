38b0e2f738a81decc9d32e4d6cac678506fd090b
Patrik Hrnciar <phrnciar@cisco.com>
2016-05-16 12:51:50 +0200

    CSIT-23 Add ARP request test.
    Change-Id: Ibb48e6d84efc55f7fb656283a954fbaefc7154e7
    Signed-off-by: Patrik Hrnciar <phrnciar@cisco.com>

---
+++ resources/traffic_scripts/send_icmp_check_arp.py
@@ -0,0 +1,118 @@
+#!/usr/bin/env python
+# Copyright (c) 2016 Cisco and/or its affiliates.
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at:
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Traffic script that sends an IP ICMP packet
+from one interface and expects ARP on the other one.
+"""
+
+import sys
+import ipaddress
+
+from scapy.all import Ether
+from scapy.layers.inet import ICMP, IP
+
+from resources.libraries.python.PacketVerifier import RxQueue, TxQueue
+from resources.libraries.python.TrafficScriptArg import TrafficScriptArg
+
+
+def valid_ipv4(ip):
+    """Check if IP address has the correct IPv4 address format.
+
+    :param ip: IP address.
+    :type ip: str
+    :return: True in case of correct IPv4 address format,
+             otherwise return False.
+    :rtype: bool
+    """
+    try:
+        ipaddress.IPv4Address(unicode(ip))
+        return True
+    except (AttributeError, ipaddress.AddressValueError):
+        return False
+
+
+def main():
+    """Send IP ICMP packet from one traffic generator interface and expects
+     ARP on the other."""
+    args = TrafficScriptArg(
+        ['tx_dst_mac', 'rx_dst_mac', 'tx_src_ip', 'tx_dst_ip', 'rx_arp_src_ip',
+         'rx_arp_dst_ip'])
+
+    tx_dst_mac = args.get_arg('tx_dst_mac')
+    rx_dst_mac = args.get_arg('rx_dst_mac')
+    src_ip = args.get_arg('tx_src_ip')
+    dst_ip = args.get_arg('tx_dst_ip')
+    tx_if = args.get_arg('tx_if')
+    rx_if = args.get_arg('rx_if')
+    rx_arp_src_ip = args.get_arg('rx_arp_src_ip')
+    rx_arp_dst_ip = args.get_arg('rx_arp_dst_ip')
+
+    rxq = RxQueue(rx_if)
+    txq = TxQueue(tx_if)
+
+    # Create empty IP ICMP packet
+    if valid_ipv4(src_ip) and valid_ipv4(dst_ip):
+        pkt_raw = Ether(dst=tx_dst_mac) / IP(src=src_ip, dst=dst_ip) / ICMP()
+
+    # Send created packet on one interface and receive on the other
+    txq.send(pkt_raw)
+
+    ether = rxq.recv(2)
+
+    if ether is None:
+        raise RuntimeError("Ethernet frame Rx timeout")
+
+    # ARP check
+    if ether['ARP'] is not None:
+        print("ARP packet received.")
+    else:
+        raise RuntimeError("Not an ARP packet received {0}"
+                           .format(ether.__repr__()))
+
+    # Compare data from packets
+    if ether['ARP'].op == 1:  # 1 - who-has request
+        print("ARP request matched.")
+    else:
+        raise RuntimeError("Matching ARP request unsuccessful: {0}"
+                           .format(ether.__repr__()))
+
+    if ether['ARP'].hwsrc == rx_dst_mac:
+        print("Source MAC matched.")
+    else:
+        raise RuntimeError("Matching Source MAC unsuccessful: {0}"
+                           .format(ether.__repr__()))
+
+    if ether['ARP'].hwdst == "00:00:00:00:00:00":
+        print("Destination MAC matched.")
+    else:
+        raise RuntimeError("Matching Destination MAC unsuccessful: {0}"
+                           .format(ether.__repr__()))
+
+    if ether['ARP'].psrc == rx_arp_src_ip:
+        print("Source ARP IP address matched.")
+    else:
+        raise RuntimeError("Matching Source ARP IP address unsuccessful: {0}"
+                           .format(ether.__repr__()))
+
+    if ether['ARP'].pdst == rx_arp_dst_ip:
+        print("Destination ARP IP address matched.")
+    else:
+        raise RuntimeError("Matching Destination ARP IP address unsuccessful: "
+                           "{0}".format(ether.__repr__()))
+
+    sys.exit(0)
+
+
+if __name__ == "__main__":
+    main()
---
+++ tests/suites/ipv4/ipv4_arp_untagged.robot
@@ -0,0 +1,58 @@
+# Copyright (c) 2016 Cisco and/or its affiliates.
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at:
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+*** Settings ***
+| Documentation | Check if VPP sends ARP request to unknown destinations.
+| Resource | resources/libraries/robot/default.robot
+| Resource | resources/libraries/robot/counters.robot
+| Resource | resources/libraries/robot/interfaces.robot
+| Resource | resources/libraries/robot/testing_path.robot
+| Resource | resources/libraries/robot/ipv4.robot
+| Resource | resources/libraries/robot/l2_xconnect.robot
+| Resource | resources/libraries/robot/traffic.robot
+| Library | resources.libraries.python.Trace
+
+| Force Tags | HW_ENV | VM_ENV | 3_NODE_SINGLE_LINK_TOPO
+| Suite Setup | Run Keywords | Setup all TGs before traffic script
+| ...         | AND          | Update All Interface Data On All Nodes | ${nodes}
+| Test Setup | Setup all DUTs before test
+| Test Teardown | Show packet trace on all DUTs | ${nodes}
+
+*** Variables ***
+| ${dut1_to_tg_ip}= | 192.168.1.1
+| ${dut1_to_dut2_ip}= | 192.168.2.1
+| ${dut1_to_dut2_ip_GW}= | 192.168.2.2
+| ${test_dst_ip}= | 32.0.0.1
+| ${test_src_ip}= | 16.0.0.1
+| ${prefix_length}= | 24
+
+*** Test Cases ***
+| VPP sends ARP requests for unknown destinations
+| | [Documentation] | Setup IP addresses and route.
+| | ...             | Send ICMP packet and check if VPP sends ARP request.
+| | Given Path for 3-node testing is set
+| | ... | ${nodes['TG']} | ${nodes['DUT1']} | ${nodes['DUT2']} | ${nodes['TG']}
+| | And Interfaces in 3-node path are up
+| | And L2 setup xconnect on DUT
+| | ... | ${dut2_node} | ${dut2_to_dut1} | ${dut2_to_tg}
+| | When Set Interface Address | ${dut1_node}
+| | ... | ${dut1_to_tg} | ${dut1_to_tg_ip} | ${prefix_length}
+| | And Set Interface Address | ${dut1_node}
+| | ... | ${dut1_to_dut2} | ${dut1_to_dut2_ip} | ${prefix_length}
+| | And Vpp Route Add
+| | ... | ${dut1_node} | ${test_dst_ip} | ${prefix_length}
+| | ... | ${dut1_to_dut2_ip_GW} | ${dut1_to_dut2}
+| | Then Send Packet And Check ARP Request | ${tg_node}
+| | ... | ${test_src_ip} | ${test_dst_ip} | ${tg_to_dut1}
+| | ... | ${tg_to_dut2_mac} | ${tg_to_dut2} | ${dut1_to_dut2_mac}
+| | ... | ${dut1_to_dut2_ip} | ${dut1_to_dut2_ip_GW}
