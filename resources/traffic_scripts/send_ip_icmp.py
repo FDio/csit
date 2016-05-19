c468b1a35f98645b3331abd4c4ed13434824358c
Jan Gelety <jgelety@cisco.com>
2016-05-19 16:36:48 +0200

    CSIT-98: MAC split-horizon group, part II
    - test cases to test MAC split-horizon group functionality together
    with VXLAN tunnel
    
    Change-Id: I1da6a98b11f5c39cd114abb35bb8f398cbbebf63
    Signed-off-by: Jan Gelety <jgelety@cisco.com>

--- resources/traffic_scripts/send_ip_icmp.py
+++ resources/traffic_scripts/send_ip_icmp.py
@@ -16,14 +16,35 @@
 from one interface to the other"""
 
 import sys
+import ipaddress
+
+from scapy.layers.inet import ICMP, IP
+from scapy.layers.inet6 import IPv6
+from scapy.layers.inet6 import ICMPv6EchoRequest
+from scapy.all import Ether
+
 from resources.libraries.python.PacketVerifier import RxQueue, TxQueue
 from resources.libraries.python.TrafficScriptArg import TrafficScriptArg
-from scapy.layers.inet import ICMP, IP
-from scapy.all import Ether
+
+
+def valid_ipv4(ip):
+    try:
+        ipaddress.IPv4Address(unicode(ip))
+        return True
+    except (AttributeError, ipaddress.AddressValueError):
+        return False
+
+
+def valid_ipv6(ip):
+    try:
+        ipaddress.IPv6Address(unicode(ip))
+        return True
+    except (AttributeError, ipaddress.AddressValueError):
+        return False
 
 
 def main():
-    """ Send IP icmp packet from one traffic generator interface to the other"""
+    """Send IP ICMP packet from one traffic generator interface to the other."""
     args = TrafficScriptArg(['src_mac', 'dst_mac', 'src_ip', 'dst_ip'])
 
     src_mac = args.get_arg('src_mac')
@@ -38,10 +59,25 @@ def main():
 
     sent_packets = []
 
-    # Create empty ip ICMP packet and add padding before sending
-    pkt_raw = Ether(src=src_mac, dst=dst_mac) / \
-              IP(src=src_ip, dst=dst_ip) / \
-              ICMP()
+    ip_format = ''
+    icmp_format = ''
+    pkt_raw = ''
+
+    # Create empty IP ICMP packet and add padding before sending
+    if valid_ipv4(src_ip) and valid_ipv4(dst_ip):
+        pkt_raw = (Ether(src=src_mac, dst=dst_mac) /
+                   IP(src=src_ip, dst=dst_ip) /
+                   ICMP())
+        ip_format = 'IP'
+        icmp_format = 'ICMP'
+    elif valid_ipv6(src_ip) and valid_ipv6(dst_ip):
+        pkt_raw = (Ether(src=src_mac, dst=dst_mac) /
+                   IPv6(src=src_ip, dst=dst_ip) /
+                   ICMPv6EchoRequest())
+        ip_format = 'IPv6'
+        icmp_format = 'ICMPv6EchoRequest'
+    else:
+        raise ValueError("IP not in correct format")
 
     # Send created packet on one interface and receive on the other
     sent_packets.append(pkt_raw)
@@ -53,15 +89,16 @@ def main():
     if ether is None:
         raise RuntimeError('ICMP echo Rx timeout')
 
-    if not ether.haslayer(IP):
+    if not ether.haslayer(ip_format):
         raise RuntimeError(
             'Not an IP packet received {0}'.format(ether.__repr__()))
 
-    if not ether.haslayer(ICMP):
+    if not ether.haslayer(icmp_format):
         raise RuntimeError(
             'Not an ICMP packet received {0}'.format(ether.__repr__()))
 
     sys.exit(0)
+
 
 if __name__ == "__main__":
     main()
