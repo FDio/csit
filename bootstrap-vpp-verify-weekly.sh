022cc77a6848eca746b20408856897a4e7cb5947
Jan Gelety <jgelety@cisco.com>
2016-06-02 15:29:08 +0200

    Update of vpp build in bootstrap-vpp-verify-weekly.sh
    - correction of REPO path
    
    Change-Id: Iff6012046c3d749692dcd63d30d9797f45fc64bc
    Signed-off-by: Jan Gelety <jgelety@cisco.com>

--- bootstrap-vpp-verify-weekly.sh
+++ bootstrap-vpp-verify-weekly.sh
@@ -70,7 +70,7 @@ if [ "${#}" -ne "0" ]; then
     echo ${arr[0]}
 else
     VPP_STABLE_VER="16.09-rc0~33-g4b46c84~b200_amd64"
-    VPP_REPO_URL="https://nexus.fd.io/service/local/repositories/fd.io.master.ubuntu.trusty.main/content/io/fd/vppp"
+    VPP_REPO_URL="https://nexus.fd.io/service/local/repositories/fd.io.master.ubuntu.trusty.main/content/io/fd/vpp"
     wget -q "${VPP_REPO_URL}/vpp/${VPP_STABLE_VER}/vpp-${VPP_STABLE_VER}.deb" || exit
     wget -q "${VPP_REPO_URL}/vpp-dbg/${VPP_STABLE_VER}/vpp-dbg-${VPP_STABLE_VER}.deb" || exit
     wget -q "${VPP_REPO_URL}/vpp-dev/${VPP_STABLE_VER}/vpp-dev-${VPP_STABLE_VER}.deb" || exit
