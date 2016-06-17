9feef3be4eab87bfff9b6409fcdc16fda856eb72
pmikus <pmikus@cisco.com>
2016-06-17 08:08:08 +0100

    CSIT-169 IPv4 Scale - performance
    - Add IPv4 scale test cases with 10 000, 100 000 and 1 000 000 routes
    - Add keywords to setup 3-node topology for testing
    - Modify libraries for adding specified number of routes
    
    Change-Id: If085e0d834e5ffe5538ba76756e8b9d0e8255cb2
    Signed-off-by: pmikus <pmikus@cisco.com>

--- resources/libraries/python/Routing.py
+++ resources/libraries/python/Routing.py
@@ -22,7 +22,7 @@ class Routing(object):
 
     @staticmethod
     def vpp_route_add(node, network, prefix_len, gateway=None, interface=None,
-                      use_sw_index=True, resolve_attempts=10):
+                      use_sw_index=True, resolve_attempts=10, count=1):
         """Add route to the VPP node.
 
         :param node: Node to add route on.
@@ -52,13 +52,17 @@ class Routing(object):
 
         via = 'via {}'.format(gateway) if gateway else ''
 
-        with VatTerminal(node) as vat:
+        cnt = 'count {}'.format(count) \
+            if count else ''
+
+        with VatTerminal(node, json_param=False) as vat:
             vat.vat_terminal_exec_cmd_from_template('add_route.vat',
                                                     network=network,
                                                     prefix_length=prefix_len,
                                                     via=via,
                                                     interface=int_cmd,
-                                                    resolve_attempts=rap)
+                                                    resolve_attempts=rap,
+                                                    count=cnt)
 
     @staticmethod
     def add_fib_table(node, network, prefix_len, fib_id, place):
