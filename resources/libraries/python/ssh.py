60e9a70a12a33acb398be2edef7e25a64214e480
pmikus <pmikus@cisco.com>
2016-06-24 06:17:57 +0100

    CSIT-176 Pybot loops if there is interactive terminal timeout
    Pybot runs itself into loop while blocking SIGTERM signal if there is
    timeout in interactive terminal session. This can happen e.g if
    vpp_api_test crashes and return to linux prompt.
    
    Change-Id: I2009ee2f80ea8bf368d6a53261e60592fb53fc3f
    Signed-off-by: pmikus <pmikus@cisco.com>

--- resources/libraries/python/ssh.py
+++ resources/libraries/python/ssh.py
@@ -53,7 +53,7 @@ class SSH(object):
             pkey = None
             if 'priv_key' in node:
                 pkey = RSAKey.from_private_key(
-                        StringIO.StringIO(node['priv_key']))
+                    StringIO.StringIO(node['priv_key']))
 
             self._ssh = paramiko.SSHClient()
             self._ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
@@ -170,15 +170,27 @@ class SSH(object):
         buf = ''
         try:
             with timeout(time_out, exception=RuntimeError):
-                while not buf.endswith(':~$ '):
-                    if chan.recv_ready():
-                        buf = chan.recv(4096)
+                while not buf.endswith(':~$ ') and not chan.closed:
+                    try:
+                        if chan.recv_ready():
+                            chunk = chan.recv(self.__MAX_RECV_BUF)
+                            buf += chunk
+                            if not chunk:
+                                break
+                        if chan.recv_stderr_ready():
+                            chunk = chan.recv_stderr(self.__MAX_RECV_BUF)
+                            buf += chunk
+                            if not chunk:
+                                break
+                    except socket.timeout:
+                        logger.error('Caught timeout exception, current '
+                                     'contents of buffer: {0}'.format(buf))
+                        raise
         except RuntimeError:
             raise Exception('Open interactive terminal timeout.')
         return chan
 
-    @staticmethod
-    def interactive_terminal_exec_command(chan, cmd, prompt,
+    def interactive_terminal_exec_command(self, chan, cmd, prompt,
                                           time_out=10):
         """Execute command on interactive terminal.
 
@@ -202,9 +214,28 @@ class SSH(object):
         buf = ''
         try:
             with timeout(time_out, exception=RuntimeError):
-                while not buf.endswith(prompt):
-                    if chan.recv_ready():
-                        buf += chan.recv(4096)
+                while not buf.endswith(prompt) and not chan.closed:
+                    try:
+                        if chan.recv_ready():
+                            chunk = chan.recv(self.__MAX_RECV_BUF)
+                            buf += chunk
+                            if not chunk:
+                                break
+                        if chan.recv_stderr_ready():
+                            chunk = chan.recv_stderr(self.__MAX_RECV_BUF)
+                            buf += chunk
+                            if not chunk:
+                                break
+                    except socket.timeout:
+                        logger.error('Caught timeout exception, current '
+                                     'contents of buffer: {0}'.format(buf))
+                        raise
+                    if chan.exit_status_ready()\
+                        and not chan.recv_stderr_ready()\
+                        and not chan.recv_ready():
+                            logger.trace("Channel exit status: {0}".format(
+                                chan.recv_exit_status()))
+                            chan.close()
         except RuntimeError:
             raise Exception("Exec '{c}' timeout.".format(c=cmd))
         tmp = buf.replace(cmd.replace('\n', ''), '')
