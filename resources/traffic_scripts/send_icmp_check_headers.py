515a787b2613e81ce5a59436fa4c861300696334
Jan Gelety <jgelety@cisco.com>
2016-06-28 14:05:09 +0200

    VLAN tag rewrite translate tests with x-connects
    - REVERT BOOTSTRAP BEFORE PUBLISHING
    
    - VLAN tag rewrite translate-1-1
    - VLAN tag rewrite translate-1-2
    - VLAN tag rewrite translate-2-1
    - VLAN tag rewrite translate-2-2
    
    Change-Id: I76045e1416fe1a72298ccc2090cd373fac09b468
    Signed-off-by: Jan Gelety <jgelety@cisco.com>

--- resources/traffic_scripts/send_icmp_check_headers.py
+++ resources/traffic_scripts/send_icmp_check_headers.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-"""Traffic script that sends an ip icmp packet
+"""Traffic script that sends an IP ICMPv4/ICMPv6 packet
 from one interface to the other.
 """
 
