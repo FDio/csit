b773df49c37351760ebe4e95356f0e62b11201f9
pmikus <pmikus@cisco.com>
2016-07-04 09:50:49 +0100

    DO_NOT_MERGE CSIT-176 Pybot loops if there is interactive terminal timeout
    SSH interactive session can be blocked in deadlock situation if there is
    some issue with executing command in VAT# console. There can be
    situation where vpp_api_test could send malformed output or exit/crash.
    In such situation receiving data from channel.recv and/or
    channel.recv_stderr must be done properly in order to prevent stall. One
    of possible solution is to combine stdout/stderr on channel to read from
    one source. Also proper detection of exit_status of remote console has
    to be implemented.
    
    Change-Id: I3f2702396289e632b9122aa9b971a335c550f8be
    Signed-off-by: pmikus <pmikus@cisco.com>

--- resources/libraries/python/ssh.py
+++ resources/libraries/python/ssh.py
@@ -56,7 +56,7 @@ class SSH(object):
             pkey = None
             if 'priv_key' in node:
                 pkey = RSAKey.from_private_key(
-                        StringIO.StringIO(node['priv_key']))
+                    StringIO.StringIO(node['priv_key']))
 
             self._ssh = paramiko.SSHClient()
             self._ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
@@ -198,19 +198,29 @@ class SSH(object):
         chan.get_pty()
         chan.invoke_shell()
         chan.settimeout(int(time_out))
+        chan.set_combine_stderr(True)
 
         buf = ''
         try:
             with timeout(time_out, exception=RuntimeError):
-                while not buf.endswith(':~$ '):
-                    if chan.recv_ready():
-                        buf = chan.recv(4096)
+                while not buf.endswith(':~$ ') and chan.recv_ready():
+                    try:
+                        chunk = chan.recv(self.__MAX_RECV_BUF)
+                        if not chunk:
+                            break
+                        buf += chunk
+                        if chan.exit_status_ready():
+                            logger.error('Channel exit status ready')
+                            break
+                    except socket.timeout:
+                        logger.error('Caught socket timeout exception, current '
+                                     'contents of buffer: {0}'.format(buf))
+                        raise
         except RuntimeError:
             raise Exception('Open interactive terminal timeout.')
         return chan
 
-    @staticmethod
-    def interactive_terminal_exec_command(chan, cmd, prompt,
+    def interactive_terminal_exec_command(self, chan, cmd, prompt,
                                           time_out=10):
         """Execute command on interactive terminal.
 
@@ -234,9 +244,19 @@ class SSH(object):
         buf = ''
         try:
             with timeout(time_out, exception=RuntimeError):
-                while not buf.endswith(prompt):
-                    if chan.recv_ready():
-                        buf += chan.recv(4096)
+                while not buf.endswith(prompt) and chan.recv_ready():
+                    try:
+                        chunk = chan.recv(self.__MAX_RECV_BUF)
+                        if not chunk:
+                            break
+                        buf += chunk
+                        if chan.exit_status_ready():
+                            logger.error('Channel exit status ready')
+                            break
+                    except socket.timeout:
+                        logger.error('Caught socket timeout exception, current '
+                                     'contents of buffer: {0}'.format(buf))
+                        raise
         except RuntimeError:
             raise Exception("Exec '{c}' timeout.".format(c=cmd))
         tmp = buf.replace(cmd.replace('\n', ''), '')
@@ -309,3 +329,4 @@ def exec_cmd_no_error(node, cmd, timeout=600, sudo=False):
     assert_equal(rc, 0, 'Command execution failed: "{}"\n{}'.
                  format(cmd, stderr))
     return stdout, stderr
+
