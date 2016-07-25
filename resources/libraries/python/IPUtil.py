0188f602a8e6ac09809a0126c7a6f71bf328b425
Matej Klotton <mklotton@cisco.com>
2016-07-25 17:52:22 +0200

    CSIT-197: MAP-E test cases
    Change-Id: Ib8d76abfebfb206fbbaa3c1422b2d321a3ed8712
    Signed-off-by: Matej Klotton <mklotton@cisco.com>

--- resources/libraries/python/IPUtil.py
+++ resources/libraries/python/IPUtil.py
@@ -12,12 +12,13 @@
 # limitations under the License.
 
 """Common IP utilities library."""
-from ipaddress import IPv4Network
+from ipaddress import IPv4Network, ip_address
 
 from resources.libraries.python.ssh import SSH
 from resources.libraries.python.constants import Constants
 
 from resources.libraries.python.topology import Topology
+
 
 class IPUtil(object):
     """Common IP utilities"""
@@ -53,6 +54,23 @@ class IPUtil(object):
         if int(ret_code) != 0:
             raise Exception('VPP ip probe {dev} {ip} failed on {h}'.format(
                 dev=iface_name, ip=addr, h=node['host']))
+
+    @staticmethod
+    def ip_addresses_should_be_equal(ip1, ip2):
+        """Fails if the given IP addresses are unequal.
+
+        :param ip1: IPv4 or IPv6 address.
+        :param ip2: IPv4 or IPv6 address.
+        :type ip1: str
+        :type ip2: str
+        """
+
+        addr1 = ip_address(unicode(ip1))
+        addr2 = ip_address(unicode(ip2))
+
+        if addr1 != addr2:
+            raise AssertionError('IP addresses are not equal: {} != {}'.
+                                 format(ip1, ip2))
 
 
 def convert_ipv4_netmask_prefix(network):
