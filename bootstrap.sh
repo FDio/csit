71eccee711e1d2f774d0e245c05349efb7159c10
Patrik Hrnciar <phrnciar@cisco.com>
2016-07-28 15:22:50 +0200

    CSIT-31 DHCPv6 Proxy test
    Change-Id: Ie06980f6e42af3676d8f1ee42c9b9733428365af
    Signed-off-by: Patrik Hrnciar <phrnciar@cisco.com>

--- bootstrap.sh
+++ bootstrap.sh
@@ -203,7 +203,7 @@ fi
 
 PYTHONPATH=`pwd` pybot -L TRACE -W 150 \
     -v TOPOLOGY_PATH:${SCRIPT_DIR}/topologies/enabled/topology.yaml \
-    --suite "tests.func" \
+    --suite "tests.func.dhcp.dhcp_v6_proxy" \
     --include vm_envAND3_node_single_link_topo \
     --include vm_envAND3_node_double_link_topo \
     --exclude PERFTEST \
