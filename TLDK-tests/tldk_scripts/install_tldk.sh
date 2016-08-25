7f57840ec90ce57287cc6a0ac16a3c938468c737
Fangyin Hu <fangyinx.hu@intel.com>
2016-08-25 07:43:56 -0700

    Change the code style, remove all trailing spaces and replace tabs by spaces.
    Change-Id: Id6f3ba9f5e9c380ba833490966eb9e6aead5ed29
    Signed-off-by: Fangyin Hu <fangyinx.hu@intel.com>

---
+++ TLDK-tests/tldk_scripts/install_tldk.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+ROOTDIR=/tmp/TLDK-testing
+PWDDIR=$(pwd)
+
+#install the libpcap-dev
+cd ${ROOTDIR}/TLDK-tests/tldk_deplibs/
+sudo dpkg -i libpcap*.deb
+cd ${PWDDIR}
+
+#compile and install the DPDK
+cd ${ROOTDIR}
+tar xJf dpdk-16.07.tar.xz
+cd ./dpdk-16.07
+sed -i 's/^CONFIG_RTE_LIBRTE_PMD_PCAP=n/CONFIG_RTE_LIBRTE_PMD_PCAP=y/g' ./config/common_base
+make install T=x86_64-native-linuxapp-gcc
+cd ${PWDDIR}
+
+#compile the TLDK
+export RTE_SDK=${ROOTDIR}/dpdk-16.07/
+export RTE_TARGET=x86_64-native-linuxapp-gcc
+cd ${ROOTDIR}/tldk
+make all
+cd ${PWDDIR}
+
+#setup the hugepages
+
+sudo killall -9 udpfwd 2>/dev/null
+
+MOUNT=$(mount | grep /mnt/huge)
+while [ "${MOUNT}" != "" ]
+do
+    sudo umount /mnt/huge
+    sleep 1
+    MOUNT=$(mount | grep /mnt/huge)
+done
+sudo sh -c "echo 1024 > /sys/kernel/mm/hugepages/hugepages-2048kB/nr_hugepages"
+sudo mkdir -p /mnt/huge
+sudo mount -t hugetlbfs nodev /mnt/huge/
