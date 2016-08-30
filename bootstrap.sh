901a85b0954a3f61b0d2f9b16248741c94e0d2c5
Jan Gelety <jgelety@cisco.com>
2016-08-30 11:13:28 +0200

    DO_NOT_MERGE: Test with stable 16.09 VPP build
    - try to run weekly test with stablbe 16.019 VPP build
    
    Change-Id: I1ffab3d6e41bc13da622bf674cc078fb6e4314c8
    Signed-off-by: Jan Gelety <jgelety@cisco.com>

--- bootstrap.sh
+++ bootstrap.sh
@@ -113,14 +113,13 @@ else
     rm -f *.deb
     VPP_STABLE_VER=$(cat ${SCRIPT_DIR}/VPP_STABLE_VER)
     VPP_REPO_URL=$(cat ${SCRIPT_DIR}/VPP_REPO_URL)
-    VPP_CLASSIFIER="-deb"
-    wget -q "${VPP_REPO_URL}/vpp/${VPP_STABLE_VER}/vpp-${VPP_STABLE_VER}${VPP_CLASSIFIER}.deb" || exit
-    wget -q "${VPP_REPO_URL}/vpp-dbg/${VPP_STABLE_VER}/vpp-dbg-${VPP_STABLE_VER}${VPP_CLASSIFIER}.deb" || exit
-    wget -q "${VPP_REPO_URL}/vpp-dev/${VPP_STABLE_VER}/vpp-dev-${VPP_STABLE_VER}${VPP_CLASSIFIER}.deb" || exit
-    wget -q "${VPP_REPO_URL}/vpp-dpdk-dev/${VPP_STABLE_VER}/vpp-dpdk-dev-${VPP_STABLE_VER}${VPP_CLASSIFIER}.deb" || exit
-    wget -q "${VPP_REPO_URL}/vpp-dpdk-dkms/${VPP_STABLE_VER}/vpp-dpdk-dkms-${VPP_STABLE_VER}${VPP_CLASSIFIER}.deb" || exit
-    wget -q "${VPP_REPO_URL}/vpp-lib/${VPP_STABLE_VER}/vpp-lib-${VPP_STABLE_VER}${VPP_CLASSIFIER}.deb" || exit
-    wget -q "${VPP_REPO_URL}/vpp-plugins/${VPP_STABLE_VER}/vpp-plugins-${VPP_STABLE_VER}${VPP_CLASSIFIER}.deb" || exit
+    wget -q "${VPP_REPO_URL}/vpp/${VPP_STABLE_VER}/vpp-${VPP_STABLE_VER}.deb" || exit
+    wget -q "${VPP_REPO_URL}/vpp-dbg/${VPP_STABLE_VER}/vpp-dbg-${VPP_STABLE_VER}.deb" || exit
+    wget -q "${VPP_REPO_URL}/vpp-dev/${VPP_STABLE_VER}/vpp-dev-${VPP_STABLE_VER}.deb" || exit
+    wget -q "${VPP_REPO_URL}/vpp-dpdk-dev/${VPP_STABLE_VER}/vpp-dpdk-dev-${VPP_STABLE_VER}.deb" || exit
+    wget -q "${VPP_REPO_URL}/vpp-dpdk-dkms/${VPP_STABLE_VER}/vpp-dpdk-dkms-${VPP_STABLE_VER}.deb" || exit
+    wget -q "${VPP_REPO_URL}/vpp-lib/${VPP_STABLE_VER}/vpp-lib-${VPP_STABLE_VER}.deb" || exit
+    wget -q "${VPP_REPO_URL}/vpp-plugins/${VPP_STABLE_VER}/vpp-plugins-${VPP_STABLE_VER}.deb" || exit
 fi
 
 VPP_DEBS=(*.deb)
@@ -135,7 +134,6 @@ done
 
 echo "Updated file names: " ${VPP_DEBS_FULL[@]}
 
-cat ${VIRL_PKEY}
 # Copy the files to VIRL host
 scp ${SSH_OPTIONS} *.deb \
     ${VIRL_USERNAME}@${VIRL_SERVER}:${VIRL_DIR_LOC}/
@@ -193,22 +191,83 @@ virtualenv --system-site-packages env
 echo pip install
 pip install -r ${SCRIPT_DIR}/requirements.txt
 
-pykwalify -s ${SCRIPT_DIR}/resources/topology_schemas/3_node_topology.sch.yaml \
-          -s ${SCRIPT_DIR}/resources/topology_schemas/topology.sch.yaml \
-          -d ${SCRIPT_DIR}/topologies/enabled/topology.yaml \
-          -vvv
+# There are used three iterations of tests there to check
+# the stability and reliability of the results
 
-if [ "$?" -ne "0" ]; then
-    echo "Topology schema validation failed."
-    echo "However, the tests will start."
+RC=0
+MORE_FAILS=0
+
+for test_set in 1 2 3
+do
+    echo
+    echo ${test_set}. test loop
+    PYTHONPATH=`pwd` pybot -L TRACE -W 136 \
+        -v TOPOLOGY_PATH:${SCRIPT_DIR}/topologies/enabled/topology.yaml \
+        --suite "tests.func" \
+        --include vm_envAND3_node_single_link_topo \
+        --include vm_envAND3_node_double_link_topo \
+        --exclude PERFTEST \
+        --noncritical EXPECTED_FAILING \
+        --output log_test_set${test_set} \
+        tests/
+    PARTIAL_RC=$(echo $?)
+    if [ ${PARTIAL_RC} -eq 250 ]; then
+        MORE_FAILS=1
+    fi
+    RC=$((RC+PARTIAL_RC))
+done
+
+# Log the final result
+if [ ${RC} -eq 0 ]; then
+    set +x
+    echo
+    echo "========================================================================================================================================"
+    echo "Final result of all test loops:                                                                                                 | PASS |"
+    echo "All critical tests have passed."
+    echo "========================================================================================================================================"
+    echo
+    set -x
+elif [ ${MORE_FAILS} -eq 0 ]; then
+    if [ ${RC} -eq 1 ]; then
+        HLP_STR="test has"
+    else
+        HLP_STR="tests have"
+    fi
+    set +x
+    echo
+    echo "========================================================================================================================================"
+    echo "Final result of all test loops:                                                                                                 | FAIL |"
+    echo "${RC} critical ${HLP_STR} failed."
+    echo "========================================================================================================================================"
+    echo
+    set -x
+else
+    set +x
+    echo
+    echo "========================================================================================================================================"
+    echo "Final result of all test loops:                                                                                                 | FAIL |"
+    echo "More then 250 critical tests have failed in one test loop."
+    echo "========================================================================================================================================"
+    echo
+    set -x
 fi
 
-PYTHONPATH=`pwd` pybot -L TRACE -W 145\
-    -v TOPOLOGY_PATH:${SCRIPT_DIR}/topologies/enabled/topology.yaml \
-    --suite "tests.func" \
-    --include vm_envAND3_node_single_link_topo \
-    --include vm_envAND3_node_double_link_topo \
-    --exclude PERFTEST \
-    --exclude SKIP_PATCH \
-    --noncritical EXPECTED_FAILING \
-    tests/
+echo Post-processing test data...
+
+# Rebot output post-processing
+rebot --noncritical EXPECTED_FAILING \
+      --output output.xml \
+      ./log_test_set1.xml ./log_test_set2.xml ./log_test_set3.xml
+
+# Remove unnecessary log files
+rm -f ./log_test_set1.xml ./log_test_set2.xml ./log_test_set3.xml
+
+echo Post-processing finished.
+
+if [ ${RC} -eq 0 ]; then
+    RETURN_STATUS=0
+else
+    RETURN_STATUS=1
+fi
+
+exit ${RETURN_STATUS}
