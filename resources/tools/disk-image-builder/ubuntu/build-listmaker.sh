cd6ee9d50bc88f9e52399397fe37996d47521d9f
pmikus <pmikus@cisco.com>
2016-10-07 13:00:53 +0100

    DO_NOT_MERGE: Update VIRL images to 16.04.1
    Change-Id: I93e8b5e977e974cda9ea4ebab327b59f9afd2fd0
    Signed-off-by: pmikus <pmikus@cisco.com>

--- resources/tools/disk-image-builder/ubuntu/build-listmaker.sh
+++ resources/tools/disk-image-builder/ubuntu/build-listmaker.sh
@@ -20,8 +20,19 @@ BUILD_DIR="$(pwd)/build"
 PACKER_DIR="${BUILD_DIR}/packer"
 OUT_DIR="${BUILD_DIR}/output/listmaker"
 
-RELEASE_NAME="csit-ubuntu-14.04.4-listmaker"
-PACKER_TEMPLATE="listmaker/ubuntu-14.04.4.json"
+if [ "$1" == "ubuntu-14.04.4" ]
+then
+    RELEASE_NAME="csit-ubuntu-14.04.4-listmaker"
+    PACKER_TEMPLATE="listmaker/ubuntu-14.04.4.json"
+elif [ "$1" == "ubuntu-16.04.1" ]
+then
+    RELEASE_NAME="csit-ubuntu-16.04.1-listmaker"
+    PACKER_TEMPLATE="listmaker/ubuntu-16.04.1.json"
+else
+    echo "Please provide OS as parameter:"
+    echo "Options: ${0} [ubuntu-14.04.4|ubuntu-16.04.1]"
+    exit 1
+fi
 
 VIRL_IMAGE_SUBTYPE=server
 VIRL_IMAGE_NAME="${RELEASE_NAME}"
