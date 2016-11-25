57e4a6ad8a31f78a6a8b2efcbcef5228178c855a
Fangyin Hu <fangyinx.hu@intel.com>
2016-11-25 03:52:22 -0800

    Add the DPDK l2fwd performance test cases.
    Change-Id: I996847a4871ed994cd9b5edb459fb079ff39c86d
    Signed-off-by: Fangyin Hu <fangyinx.hu@intel.com>

---
+++ dpdk-tests/dpdk_scripts/cleanup_dpdk.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+ROOTDIR=/tmp/openvpp-testing
+PWDDIR=$(pwd)
+
+TESTPMD_LOG=/tmp/testpmd.log
+TESTPMD_PID=/tmp/testpmd.pid
+
+port1_driver=$1
+port1_pci=$2
+port2_driver=$3
+port2_pci=$4
+
+#kill the dpdk application
+sudo pkill testpmd
+sudo pkill l2fwd
+sudo pkill l3fwd
+sudo rm -f ${TESTPMD_PID}
+sudo rm -f /dev/hugepages/*
+cat ${TESTPMD_LOG}
+
+sleep 2
+
+cd ${ROOTDIR}/dpdk-16.07/
+./tools/dpdk-devbind.py -b ${port1_driver} ${port1_pci}
+./tools/dpdk-devbind.py -b ${port2_driver} ${port2_pci}
+
+sleep 2
+
+if1_name=`./tools/dpdk-devbind.py --s | grep "${port1_pci}" | sed -n 's/.*if=\(\S\)/\1/p' | awk -F' ' '{print $1}'`
+if2_name=`./tools/dpdk-devbind.py --s | grep "${port2_pci}" | sed -n 's/.*if=\(\S\)/\1/p' | awk -F' ' '{print $1}'`
+
+ifconfig ${if1_name} up
+ifconfig ${if2_name} up
+
+rmmod igb_uio
+rmmod uio
+
+cd ${PWDDIR}
