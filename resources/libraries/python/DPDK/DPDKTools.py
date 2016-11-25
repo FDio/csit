39da38a23fa52de69f5e0ffcca65d2a53e6e8b2c
Fangyin Hu <fangyinx.hu@intel.com>
2016-11-25 03:52:22 -0800

    Add the DPDK l2fwd performance test cases.
    Change-Id: I996847a4871ed994cd9b5edb459fb079ff39c86d
    Signed-off-by: Fangyin Hu <fangyinx.hu@intel.com>

---
+++ resources/libraries/python/DPDK/DPDKTools.py
@@ -0,0 +1,83 @@
+# Copyright (c) 2016 Cisco and/or its affiliates.
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at:
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+"""
+This module exists to provide the init DPDK.
+"""
+
+from resources.libraries.python.ssh import SSH
+from resources.libraries.python.constants import Constants as con
+from resources.libraries.python.topology import Topology
+
+class DPDKTools(object):
+    """Test the DPDK l2fwd performance."""
+
+    @staticmethod
+    def initialize_dpdk_environment(dut_node, dut_if1, dut_if2):
+        """
+        Initialize the DPDK test environment on the dut_node.
+        load the module uio and igb_uio, then bind the test NIC to the igb_uio
+
+        :param dut_node: will init the DPDK on this node
+        :param dut_if1: DUT interface name
+        :param dut_if2: DUT interface name
+        :type dut_node: dict
+        :type dut_if1: str
+        :type dut_if2: str
+        :return: none
+        """
+        pci_address1 = Topology.get_interface_pci_addr(dut_node, dut_if1)
+        pci_address2 = Topology.get_interface_pci_addr(dut_node, dut_if2)
+
+        ssh = SSH()
+        ssh.connect(dut_node)
+
+        cmd = 'cd {0}/dpdk-tests/dpdk_scripts/ && ./init_dpdk.sh {1} {2}' \
+              .format(con.REMOTE_FW_DIR, pci_address1, pci_address2)
+
+        (ret_code, _, _) = ssh.exec_command(cmd, timeout=600)
+        if ret_code != 0:
+            raise Exception('Failed to bind the interfaces to igb_uio ' \
+                            'at node {0}'.format(dut_node['host']))
+
+    @staticmethod
+    def cleanup_dpdk_environment(dut_node, dut_if1, dut_if2):
+        """
+        Cleanup the DPDK test environment on the DUT node.
+        Unbind the NIC from the igb_uio and bind them to the kernel driver.
+
+        :param dut_node: will cleanup the DPDK on this node
+        :param dut_if1: DUT interface name
+        :param dut_if2: DUT interface name
+        :type dut_node: dict
+        :type dut_if1: str
+        :type dut_if2: str
+        :return: none
+        """
+        pci_address1 = Topology.get_interface_pci_addr(dut_node, dut_if1)
+        if1_driver = Topology.get_interface_driver(dut_node, dut_if1)
+        pci_address2 = Topology.get_interface_pci_addr(dut_node, dut_if2)
+        if2_driver = Topology.get_interface_driver(dut_node, dut_if2)
+
+        ssh = SSH()
+        ssh.connect(dut_node)
+
+        cmd = 'cd {0}/dpdk-tests/dpdk_scripts/ && ./cleanup_dpdk.sh ' \
+              '{1} {2} {3} {4}'.format(con.REMOTE_FW_DIR, if1_driver, \
+              pci_address1, if2_driver, pci_address2)
+
+        (ret_code, _, _) = ssh.exec_command(cmd, timeout=600)
+        if ret_code != 0:
+            raise Exception('Failed to cleanup the dpdk at node {0}'
+                            .format(dut_node['host']))
