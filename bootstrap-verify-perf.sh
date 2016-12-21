67b1d80416fcd0c4163af81c5023d2fcff38796e
Kirill Rybalchenko <kirill.rybalchenko@intel.com>
2016-12-21 12:16:29 +0000

    IPsec Multi-Tunnel performance test suite
    Change-Id: I4b0ba83960e50089f29cab9a30ab760241c6f566
    Signed-off-by: Kirill Rybalchenko <kirill.rybalchenko@intel.com>

--- bootstrap-verify-perf.sh
+++ bootstrap-verify-perf.sh
@@ -172,7 +172,7 @@ case "$TEST_TAG" in
         pybot ${PYBOT_ARGS} \
               -L TRACE \
               -v TOPOLOGY_PATH:${WORKING_TOPOLOGY} \
-              -s "tests.perf" \
+              -s "tests.perf" -i IPSEC \
               tests/
         RETURN_STATUS=$(echo $?)
 esac
