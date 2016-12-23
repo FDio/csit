2ab75725de131a4adf5c8f952a1b67fc866a8ee3
Fangyin Hu <fangyinx.hu@intel.com>
2016-12-23 02:30:04 -0800

    Add the DPDK l3fwd performance test case. 	Fix the merge conflict, rebase the code.
    Change-Id: I5c5625ebcd5ff16f47fbee361789be3fece1ddbc
    Signed-off-by: Fangyin Hu <fangyinx.hu@intel.com>

--- dpdk-tests/dpdk_scripts/cleanup_dpdk.sh
+++ dpdk-tests/dpdk_scripts/cleanup_dpdk.sh
@@ -5,6 +5,8 @@ PWDDIR=$(pwd)
 
 TESTPMD_LOG=/tmp/testpmd.log
 TESTPMD_PID=/tmp/testpmd.pid
+DPDK_VERSION=16.07
+DPDK_DIR=dpdk-${DPDK_VERSION}
 
 port1_driver=$1
 port1_pci=$2
@@ -21,7 +23,7 @@ cat ${TESTPMD_LOG}
 
 sleep 2
 
-cd ${ROOTDIR}/dpdk-16.07/
+cd ${ROOTDIR}/${DPDK_DIR}/
 ./tools/dpdk-devbind.py -b ${port1_driver} ${port1_pci}
 ./tools/dpdk-devbind.py -b ${port2_driver} ${port2_pci}
 
