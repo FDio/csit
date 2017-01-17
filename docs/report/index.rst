6402df4f66539c28ec8cfa47f5c164b15e5b343e
pmikus <pmikus@cisco.com>
2017-01-17 15:32:46 +0100

    CSIT 1701 report files and script
    Add RST source files and script to generate CSIT 1701 report
    
    Change-Id: I4345564547270ba10c64d6beebf2c2b5a83de459
    Signed-off-by: pmikus <pmikus@cisco.com>

---
+++ docs/report/index.rst
@@ -0,0 +1,16 @@
+CSIT 17.01
+==========
+
+.. toctree::
+    :numbered:
+    :maxdepth: 3
+    :caption: Contents:
+
+    introduction
+    functional_tests
+    performance_tests
+    performance_plots
+    functional_results
+    performance_results
+    legal
+
---
+++ resources/tools/report_gen/run_plot.py
@@ -0,0 +1,264 @@
+#!/usr/bin/python
+
+# Copyright (c) 2016 Cisco and/or its affiliates.
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at:
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Plot the performance data"""
+
+import argparse
+import operator
+import os
+import sys
+
+from lxml import etree
+import plotly.offline as ploff
+import plotly.graph_objs as plgo
+
+def select_files_in_subfolders(directory, ext='xml'):
+    """Get all files in folder and its subfolders.
+
+    :param dir: Input folder.
+    :param ext: File extension.
+    :type dir: str
+    :type ext: str
+    :return: List of filex matching the parameters.
+    :rtype list
+    """
+    for _, _, files in os.walk(directory):
+        for file in files:
+            if file.endswith('.%s' % ext):
+                yield os.path.join(directory, file)
+
+
+def select_files_in_folder(directory, ext='xml'):
+    """Get all files in folder.
+
+    :param dir: Input folder.
+    :param ext: File extension.
+    :type dir: str
+    :type ext: str
+    :return: List of filex matching the parameters.
+    :rtype list
+    """
+    for file in os.listdir(directory):
+        if file.endswith('.%s' % ext):
+            yield os.path.join(directory, file)
+
+
+def combine_dicts(first, second, oper=operator.add):
+    """Combine two dictionaries.
+
+    :param first: First dict.
+    :param second: Second dict.
+    :param oper: Operator.
+    :type first: dict
+    :type second: dict
+    :type oper: operator
+    :return: Combined dictionary.
+    :rtype dict
+    """
+
+    return dict(first.items() + second.items() +\
+        [(k, oper(first[k], second[k])) for k in set(second) & set(first)])
+
+
+def parse_data_pps(args):
+    """Get PPS data out of XML file into array.
+
+    :param args: Command line parameters.
+    :type suite: ArgumentParser
+    :return: X-data and Y-data dictionaries.
+    :rtype tuple of dict
+    """
+    xdata = []
+    ydata_pps = {}
+
+    for i, file in enumerate(sorted(select_files_in_folder(args.input))):
+        xml_tree = etree.parse(file)
+        sel = xml_tree.xpath(args.xpath)
+        if sel:
+            ydata_pps = combine_dicts(ydata_pps, dict((elem.attrib['name'],\
+                (i, float(elem.text))) for elem in sel))
+            try:
+                vdevice = xml_tree.getroot().attrib['vdevice'].split('~')
+                xdata.append(vdevice[0])
+            except IndexError:
+                xdata.append(str(i)+":"+xml_tree.getroot().attrib['vdevice'])
+    return xdata, ydata_pps
+
+
+def parse_data_lat(args):
+    """Get latency data out of XML file into array.
+
+    :param args: Command line parameters.
+    :type suite: ArgumentParser
+    :return: X-data and Y-data dictionaries.
+    :rtype tuple of dict
+    """
+    xdata = []
+    ydata_lat = {}
+
+    for i, file in enumerate(sorted(select_files_in_folder(args.input))):
+        xml_tree = etree.parse(file)
+        sel = xml_tree.xpath(args.xpath)
+        if sel:
+            try:
+                ydata_lat = combine_dicts(ydata_lat, dict((elem.attrib['name'],\
+                    ('/'.join(str(i)*6), elem.attrib[args.latency])) for elem in sel))
+            except KeyError:
+                raise RuntimeError('Retrieving latency data error (PDR?)')
+            try:
+                vdevice = xml_tree.getroot().attrib['vdevice'].split('~')
+                xdata.append(vdevice[0])
+            except IndexError:
+                xdata.append(str(i)+":"+xml_tree.getroot().attrib['vdevice'])
+    return xdata, ydata_lat
+
+
+def parse_args():
+    """Parse arguments from cmd line.
+
+    :return: Parsed arguments.
+    :rtype ArgumentParser
+    """
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-x", "--xpath", required=True,
+                        help="Xpath filter")
+    parser.add_argument("-t", "--title", required=True,
+                        help="Plot title")
+    parser.add_argument("-l", "--lower",
+                        default=False,
+                        help="Lower boudary of Y-axis")
+    parser.add_argument("-u", "--upper",
+                        default=False,
+                        help="Upper boudary of Y-axis")
+    parser.add_argument("-e", "--errorbar",
+                        default=False,
+                        help="Errorbar for Y-axis")
+    parser.add_argument("-d", "--latency",
+                        choices=['lat_10', 'lat_50', 'lat_100'],
+                        help="Latency to draw")
+    parser.add_argument("-i", "--input",
+                        help="Input folder")
+    parser.add_argument("-o", "--output", required=True,
+                        help="Output image file name")
+    return parser.parse_args()
+
+
+def main():
+    """Main function."""
+
+    args = parse_args()
+    if not args.latency:
+        xdata, ydata = parse_data_pps(args)
+    else:
+        xdata, ydata = parse_data_lat(args)
+
+    # Print data into console for debug
+    row_format = "{:>60}" * (len(xdata) + 1)
+    print row_format.format(args.title, *xdata)
+    for data in ydata:
+        try:
+            print row_format.format(data, *ydata[data][1::2])
+        except IndexError:
+            print "{:>60}".format(data), ydata[data]
+
+    if xdata and ydata:
+        traces = []
+        # Add plot traces
+        for suite in ydata:
+            traces.append(plgo.Scatter(
+                x=ydata[suite][0::2],
+                y=ydata[suite][1::2],
+                mode='lines+markers',
+                name=suite.lower(),
+            ))
+            print suite + ' :: ' + str(ydata[suite][1::2]) + ' :: ' + str(ydata[suite][0::2])
+        traces = [
+        {
+            'y': [0.2, 0.6, 1.0, 0.2, 0.6, 1.0, 0.1, 0.5, 0.3, 0.1, 0.5, 0.3],
+            'x': ['0', '0', '0', '0', '0', '0', '1', '1', '1', '1', '1', '1'],
+            'name':'Suite Lat A',
+            'boxmean': False,
+            'type': 'box',
+            'legendgroup': 'abc',
+        },
+        {
+            'y': [0.2, 0.6, 1.0, 0.2, 0.6, 1.0, 0.1, 0.5, 0.3, 0.1, 0.5, 0.3],
+            'x': ['0', '0', '0', '0', '0', '0', '1', '1', '1', '1', '1', '1'],
+            'name':'Suite Lat B',
+            'boxmean': False,
+            "type": "box",
+            'showlegend ': False,
+        }
+        ]
+
+        # Add plot layout
+        layout = plgo.Layout(
+            title='{0}'.format(args.title),
+            xaxis=dict(
+                autorange=True,
+                autotick=False,
+                color='rgb(238, 238, 238)',
+                fixedrange=False,
+                gridcolor='rgb(238, 238, 238)',
+                linecolor='rgb(238, 238, 238)',
+                linewidth=1,
+                showgrid=True,
+                showline=True,
+                showticklabels=True,
+                tickcolor='rgb(238, 238, 238)',
+                tickmode='linear',
+                zeroline=False,
+            ),
+            yaxis=dict(
+                gridcolor='rgb(238, 238, 238)',
+                linecolor='rgb(238, 238, 238)',
+                linewidth=1,
+                range=[args.lower, args.upper],
+                showgrid=True,
+                showline=True,
+                showticklabels=True,
+                tickcolor='rgb(238, 238, 238)',
+                title='Packets Per Second',
+                zeroline=False,
+            ),
+            boxmode='group',
+            autosize=False,
+            margin=dict(
+                autoexpand=False,
+                b=200,
+                l=50,
+                r=50,
+            ),
+            showlegend=True,
+            legend=dict(
+                orientation='h',
+                y=-0.5,
+            ),
+            width=700,
+            height=700,
+        )
+        # Create plot
+        plpl = plgo.Figure(data=traces, layout=layout)
+        # Export Plot
+        ploff.plot(plpl,
+                   show_link=False, auto_open=False,
+                   filename='{0}.html'.format(args.output))
+    else:
+        sys.stderr.write('No data found!\n')
+
+
+if __name__ == "__main__":
+    sys.exit(main())
