a66d2ec8e7314b991e3584bd9e6e6acd8b048ed3
pmikus <pmikus@cisco.com>
2017-01-17 09:43:24 +0100

    Add unified latency output for PDRDISC and NDRDISC
    Add same latency formatting for PDR as for NDR search results.
    This will help us to parse the data in same way.
    
    Change-Id: I3be79cc7623f8c9d39fd44babd252ceec58c114b
    Signed-off-by: pmikus <pmikus@cisco.com>

--- bootstrap-verify-perf.sh
+++ bootstrap-verify-perf.sh
@@ -164,6 +164,7 @@ case "$TEST_TAG" in
               -L TRACE \
               -v TOPOLOGY_PATH:${WORKING_TOPOLOGY} \
               -s "tests.perf" \
+              -i THIS \
               tests/
         RETURN_STATUS=$(echo $?)
 esac
