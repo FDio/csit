e507c9603efd1a9377ff835d108e51ddb9a0b5c3
Jan Gelety <jgelety@cisco.com>
2017-01-24 13:41:23 +0100

    Correction of dpdk packages download when vpp deb packages provided
    Change-Id: I7cefd0797103e5062eb48df95ad2e48cdddc19b6
    Signed-off-by: Jan Gelety <jgelety@cisco.com>

--- bootstrap.sh
+++ bootstrap.sh
@@ -125,20 +125,19 @@ done
 
 echo "Selected VIRL servers: ${VIRL_SERVER[@]}"
 
-# Temporarily download VPP packages from nexus.fd.io
+# Temporarily download VPP and DPDK packages from nexus.fd.io
+DPDK_STABLE_VER=$(cat ${SCRIPT_DIR}/DPDK_STABLE_VER)
+VPP_REPO_URL=$(cat ${SCRIPT_DIR}/VPP_REPO_URL)
+VPP_CLASSIFIER="-deb"
 if [ "${#}" -ne "0" ]; then
     arr=(${@})
     echo ${arr[0]}
     # DPDK is not part of the vpp build
-    DPDK_STABLE_VER=$(cat ${SCRIPT_DIR}/DPDK_STABLE_VER)
     wget -q "${VPP_REPO_URL}/vpp-dpdk-dev/${DPDK_STABLE_VER}/vpp-dpdk-dev-${DPDK_STABLE_VER}${VPP_CLASSIFIER}.deb" || exit
     wget -q "${VPP_REPO_URL}/vpp-dpdk-dkms/${DPDK_STABLE_VER}/vpp-dpdk-dkms-${DPDK_STABLE_VER}${VPP_CLASSIFIER}.deb" || exit
 else
     rm -f *.deb
-    DPDK_STABLE_VER=$(cat ${SCRIPT_DIR}/DPDK_STABLE_VER)
     VPP_STABLE_VER=$(cat ${SCRIPT_DIR}/VPP_STABLE_VER)
-    VPP_REPO_URL=$(cat ${SCRIPT_DIR}/VPP_REPO_URL)
-    VPP_CLASSIFIER="-deb"
     wget -q "${VPP_REPO_URL}/vpp/${VPP_STABLE_VER}/vpp-${VPP_STABLE_VER}${VPP_CLASSIFIER}.deb" || exit
     wget -q "${VPP_REPO_URL}/vpp-dbg/${VPP_STABLE_VER}/vpp-dbg-${VPP_STABLE_VER}${VPP_CLASSIFIER}.deb" || exit
     wget -q "${VPP_REPO_URL}/vpp-dev/${VPP_STABLE_VER}/vpp-dev-${VPP_STABLE_VER}${VPP_CLASSIFIER}.deb" || exit
