3167772288db719dc985f353f0615271fd752e1b
Tibor Frank <tifrank@cisco.com>
2017-02-07 12:58:47 +0100

    CSIT-518: Add testpmd numa awareness
    Change-Id: I982834de1fbe71cf5303808ea58d4b58e530ffcb
    Signed-off-by: Tibor Frank <tifrank@cisco.com>

--- dpdk-tests/dpdk_scripts/run_l2fwd.sh
+++ dpdk-tests/dpdk_scripts/run_l2fwd.sh
@@ -6,7 +6,7 @@ PWDDIR=$(pwd)
 TESTPMD_LOG=/tmp/testpmd.log
 TESTPMD_PID=/tmp/testpmd.pid
 
-cpu_coremask=$1
+cpu_corelist=$1
 nb_cores=$2
 queue_nums=$3
 jumbo_frames=$4
@@ -35,23 +35,35 @@ sudo rm -f ${TESTPMD_PID}
 #run the testpmd
 cd ${ROOTDIR}
 if [ "$jumbo_frames" = "yes" ]; then
-#sudo sh -c "screen -dmS DPDK-test ./dpdk-16.07/x86_64-native-linuxapp-gcc/app/testpmd -c ${cpu_coremask} \
-#    -n 4 -- --nb-ports=2 --portmask=0x3 --nb-cores=${nb_cores} \
-#    --max-pkt-len=9000 --txqflags=0 --forward-mode=io --rxq=${queue_nums} \
-#    --txq=${queue_nums} --auto-start"
-tail -f /dev/null | nohup ./dpdk-16.07/x86_64-native-linuxapp-gcc/app/testpmd -c ${cpu_coremask} \
-    -n 4 -- --nb-ports=2 --portmask=0x3 --nb-cores=${nb_cores} \
-    --max-pkt-len=9000 --txqflags=0 --forward-mode=io --rxq=${queue_nums} \
-    --txq=${queue_nums} --auto-start > ${TESTPMD_LOG} 2>&1 &
-echo $! > ${TESTPMD_PID}
+    tail -f /dev/null | nohup \
+    ./dpdk-16.07/x86_64-native-linuxapp-gcc/app/testpmd \
+        -l ${cpu_corelist} \
+        -n 4 -- \
+        --nb-ports=2 \
+        --portmask=0x3 \
+        --nb-cores=${nb_cores} \
+        --max-pkt-len=9000 \
+        --txqflags=0 \
+        --forward-mode=io \
+        --rxq=${queue_nums} \
+        --txq=${queue_nums} \
+        --auto-start \
+    > ${TESTPMD_LOG} 2>&1 &
+    echo $! > ${TESTPMD_PID}
 else
-#sudo sh -c "screen -dmS DPDK-test ./dpdk-16.07/x86_64-native-linuxapp-gcc/app/testpmd -c ${cpu_coremask} \
-#    -n 4 -- --nb-ports=2 --portmask=0x3 --nb-cores=${nb_cores} \
-#    --forward-mode=io --rxq=${queue_nums} --txq=${queue_nums} --auto-start"
-tail -f /dev/null | nohup ./dpdk-16.07/x86_64-native-linuxapp-gcc/app/testpmd -c ${cpu_coremask} \
-    -n 4 -- --nb-ports=2 --portmask=0x3 --nb-cores=${nb_cores} \
-    --forward-mode=io --rxq=${queue_nums} --txq=${queue_nums} --auto-start > ${TESTPMD_LOG} 2>&1 &
-echo $! > ${TESTPMD_PID}
+    tail -f /dev/null | nohup \
+    ./dpdk-16.07/x86_64-native-linuxapp-gcc/app/testpmd \
+        -c ${cpu_corelist} \
+        -n 4 -- \
+        --nb-ports=2 \
+        --portmask=0x3 \
+        --nb-cores=${nb_cores} \
+        --forward-mode=io \
+        --rxq=${queue_nums} \
+        --txq=${queue_nums} \
+        --auto-start \
+    > ${TESTPMD_LOG} 2>&1 &
+    echo $! > ${TESTPMD_PID}
 fi
 
 cd ${PWDDIR}
