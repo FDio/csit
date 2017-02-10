05add04e56cfbcf5bde76c3aba8148399e17a2c8
Tibor Frank <tifrank@cisco.com>
2017-02-10 09:24:00 +0100

    Add x710 and xl710 tests for testpmd
    10ge2p1x710-eth-l2xcbase-ndrdisc
    40ge2p1xl710-eth-l2xcbase-ndrdisc
    
    Change-Id: Iea411182fd41e1ae9ed9b5a17f540befc247adb9
    Signed-off-by: Tibor Frank <tifrank@cisco.com>

--- dpdk-tests/dpdk_scripts/init_dpdk.sh
+++ dpdk-tests/dpdk_scripts/init_dpdk.sh
@@ -1,17 +1,28 @@
 #!/bin/bash
 
+set -x
+
+# Setting variables
 ROOTDIR=/tmp/openvpp-testing
 PWDDIR=$(pwd)
+cd ${ROOTDIR}/dpdk-17.02/
 
-cd ${ROOTDIR}/dpdk-16.07/
 modprobe uio
+echo "  RC = $?"
+
 lsmod | grep igb_uio
 if [ $? -eq 1 ];
 then
     insmod ./x86_64-native-linuxapp-gcc/kmod/igb_uio.ko || exit 1
+    echo "  RC = $?"
 else
     rmmod igb_uio
+    echo "  RC = $?"
     insmod ./x86_64-native-linuxapp-gcc/kmod/igb_uio.ko || exit 1
+    echo "  RC = $?"
 fi
-./tools/dpdk-devbind.py -b igb_uio $1 $2
+
+# Binding
+./usertools/dpdk-devbind.py -b igb_uio $1 $2
+
 cd ${PWDDIR}
