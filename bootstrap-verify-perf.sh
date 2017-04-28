ab1d9168cfa56765fbaf1aa0251fc70465bd0153
Peter Mikus <pmikus@cisco.com>
2017-04-28 08:43:25 +0200

    Fix: VAT timeout in IPv6 scale 2M
    - Fix VAT timeout issue in IPv6 scale testing with 2M FIB entries
    by increasing heapsize to 3G
    
    Change-Id: Ie81619085fbd5a2a86848513d139932a12babf67
    Signed-off-by: Peter Mikus <pmikus@cisco.com>

--- bootstrap-verify-perf.sh
+++ bootstrap-verify-perf.sh
@@ -312,6 +312,7 @@ case "$TEST_TAG" in
         pybot ${PYBOT_ARGS} \
               -v TOPOLOGY_PATH:${WORKING_TOPOLOGY} \
               -s "tests.perf" \
+              -i fib_2mANDip6fwd\
               tests/
         RETURN_STATUS=$(echo $?)
 esac
