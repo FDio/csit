5c38144ac57d3f919480e37f95b6fccdb472f6be
selias <samelias@cisco.com>
2017-05-26 14:51:50 +0200

    CSIT-619 HC Test: Honeycomb performance testing - initial commit
     - keywords and scripts for HC performance testing setup
     - basic performance suite: operational data read
     - traffic script and keywords used in tests
    
    Change-Id: Ic0290be73a7c925ea2561f8cd2524c5cb83fcda2
    Signed-off-by: selias <samelias@cisco.com>

---
+++ bootstrap-hc2vpp-perf.sh
@@ -0,0 +1,119 @@
+#!/bin/bash
+# Copyright (c) 2017 Cisco and/or its affiliates.
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at:
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+set -x
+
+STREAM=$1
+OS=$2
+ODL=$3
+
+# Space separated list of available testbeds, described by topology files
+TOPOLOGIES="topologies/available/lf_testbed1.yaml \
+            topologies/available/lf_testbed2.yaml \
+            topologies/available/lf_testbed3.yaml"
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+# Reservation dir
+RESERVATION_DIR="/tmp/reservation_dir"
+INSTALLATION_DIR="/tmp/install_dir"
+
+PYBOT_ARGS="-W 150 -L TRACE"
+
+ARCHIVE_ARTIFACTS=(log.html output.xml report.html output_perf_data.xml)
+
+WORKING_TOPOLOGY=""
+export PYTHONPATH=${SCRIPT_DIR}
+
+sudo apt-get -y update
+sudo apt-get -y install libpython2.7-dev python-virtualenv
+
+virtualenv --system-site-packages env
+. env/bin/activate
+
+echo pip install
+pip install -r requirements.txt
+
+# We iterate over available topologies and wait until we reserve topology
+while :; do
+    for TOPOLOGY in ${TOPOLOGIES};
+    do
+        python ${SCRIPT_DIR}/resources/tools/topo_reservation.py -t ${TOPOLOGY}
+        if [ $? -eq 0 ]; then
+            WORKING_TOPOLOGY=${TOPOLOGY}
+            echo "Reserved: ${WORKING_TOPOLOGY}"
+            break
+        fi
+    done
+
+    if [ ! -z "${WORKING_TOPOLOGY}" ]; then
+        # Exit the infinite while loop if we made a reservation
+        break
+    fi
+
+    # Wait ~3minutes before next try
+    SLEEP_TIME=$[ ( $RANDOM % 20 ) + 180 ]s
+    echo "Sleeping ${SLEEP_TIME}"
+    sleep ${SLEEP_TIME}
+done
+
+function cancel_all {
+    python ${SCRIPT_DIR}/resources/tools/topo_installation_hc.py -c -d ${INSTALLATION_DIR} -t $1
+    python ${SCRIPT_DIR}/resources/tools/topo_reservation.py -c -t $1
+}
+
+# On script exit we cancel the reservation and installation and delete all vpp
+# packages
+trap "cancel_all ${WORKING_TOPOLOGY}" EXIT
+
+# Download VPP and HC packages from the current branch
+echo Downloading packages...
+bash ${SCRIPT_DIR}/resources/tools/download_hc_pkgs.sh ${STREAM} ${OS}
+
+if [ "${OS}" == "centos7" ]; then
+    VPP_PKGS=(*.rpm)
+else
+    VPP_PKGS=(*.deb)
+fi
+echo ${VPP_PKGS[@]}
+
+# Install packages
+python ${SCRIPT_DIR}/resources/tools/topo_installation.py -t ${WORKING_TOPOLOGY} \
+                                                       -d ${INSTALLATION_DIR} \
+                                                       -p ${VPP_PKGS} \
+                                                       -hc True
+if [ $? -eq 0 ]; then
+    echo "VPP Installed on hosts from: ${WORKING_TOPOLOGY}"
+else
+    echo "Failed to copy vpp deb files to DUTs"
+    exit 1
+fi
+
+# run full performance test suite and exit on fail
+        pybot ${PYBOT_ARGS} \
+              -v TOPOLOGY_PATH:${WORKING_TOPOLOGY} \
+              -s "tests.perf.honeycomb" \
+              --variable install_dir:${INSTALLATION_DIR} \
+              tests/
+        RETURN_STATUS=$(echo $?)
+
+# Archive artifacts
+mkdir archive
+for i in ${ARCHIVE_ARTIFACTS[@]}; do
+    cp $( readlink -f ${i} | tr '\n' ' ' ) archive/
+done
+
+echo Post-processing finished.
+
+exit ${RETURN_STATUS}
