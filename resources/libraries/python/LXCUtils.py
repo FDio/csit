9312477a3d17a0fa9c092652e09a35115b0472f5
Peter Mikus <pmikus@cisco.com>
2017-05-30 14:10:14 +0200

    CSIT-649 Add library for creating lxc container
    Add RF library for manipulating lxc container
    Add python library for manipulating lxc container
    
    Change-Id: I02140aa879c7ebd40360d588ab1438c58cf560a1
    Signed-off-by: Peter Mikus <pmikus@cisco.com>

---
+++ resources/libraries/python/LXCUtils.py
@@ -0,0 +1,399 @@
+# Copyright (c) 2017 Cisco and/or its affiliates.
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at:
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Library for manipulating with LXC."""
+
+from resources.libraries.python.ssh import SSH
+from resources.libraries.python.topology import NodeType
+
+__all__ = ["LXCUtils"]
+
+class LXCUtils(object):
+    """LXC utilities."""
+
+    def __init__(self, container_name='slave'):
+        # LXC container name
+        self._container_name = container_name
+        self._node = None
+        # Host dir that will be mounted inside LXC
+        self._host_dir = '/tmp/'
+        # Guest dir to mount host dir to
+        self._guest_dir = '/mnt/host'
+
+    def lxc_set_node(self, node):
+        """Set node for LXC control.
+
+        :param node: Node to control LXC on.
+        :type node: dict
+        """
+        if node['type'] != NodeType.DUT:
+            raise RuntimeError('Node type is not DUT.')
+        self._node = node
+
+    def lxc_set_host_dir(self, node, host_dir):
+        """Set shared dir on parent node for LXC.
+
+        :param node: Node to control LXC on.
+        :type node: dict
+        """
+        if node['type'] != NodeType.DUT:
+            raise RuntimeError('Node type is not DUT.')
+        self._host_dir = host_dir
+
+    def lxc_set_guest_dir(self, node, guest_dir):
+        """Set mount dir on LXC.
+
+        :param node: Node to control LXC on.
+        :type node: dict
+        """
+        if node['type'] != NodeType.DUT:
+            raise RuntimeError('Node type is not DUT.')
+        self._guest_dir = guest_dir
+
+    def _lxc_checkconfig(self):
+        """Check the current kernel for lxc support.
+
+        :raises RuntimeError: If failed to check LXC support.
+        """
+
+        ssh = SSH()
+        ssh.connect(self._node)
+
+        ret, _, _ = ssh.exec_command_sudo('lxc-checkconfig')
+        if int(ret) != 0:
+            raise RuntimeError('Failed to check LXC support.')
+
+    def _lxc_create(self, distro='ubuntu', release='xenial', arch='amd64'):
+        """Creates a privileged system object where is stored the configuration
+        information and where can be stored user information.
+
+        :param distro: Linux distribution name.
+        :param release: Linux distribution release.
+        :param arch: Linux distribution architecture.
+        :type distro: str
+        :type release: str
+        :type arch: str
+        :raises RuntimeError: If failed to create a container.
+        """
+
+        ssh = SSH()
+        ssh.connect(self._node)
+
+        ret, _, _ = ssh.exec_command_sudo(
+            'lxc-create -t download --name {0} -- -d {1} -r {2} -a {3}'
+            .format(self._container_name, distro, release, arch), timeout=1800)
+        if int(ret) != 0:
+            raise RuntimeError('Failed to create LXC container.')
+
+    def _lxc_info(self):
+        """Queries and shows information about a container.
+
+        :raises RuntimeError: If failed to get info about a container.
+        """
+
+        ssh = SSH()
+        ssh.connect(self._node)
+
+        ret, _, _ = ssh.exec_command_sudo(
+            'lxc-info --name {0}'.format(self._container_name))
+        if int(ret) != 0:
+            raise RuntimeError('Failed to get info about LXC container {0}.'
+                               .format(self._container_name))
+
+    def _lxc_start(self):
+        """Start an application inside a container.
+
+        :raises RuntimeError: If failed to start container.
+        """
+
+        ssh = SSH()
+        ssh.connect(self._node)
+
+        ret, _, _ = ssh.exec_command_sudo(
+            'lxc-start --name {0} --daemon'.format(self._container_name))
+        if int(ret) != 0:
+            raise RuntimeError('Failed to start LXC container {0}.'
+                               .format(self._container_name))
+
+    def _lxc_stop(self):
+        """Stop an application inside a container.
+
+        :raises RuntimeError: If failed to stop container.
+        """
+
+        ssh = SSH()
+        ssh.connect(self._node)
+
+        ret, _, _ = ssh.exec_command_sudo(
+            'lxc-stop --name {0}'.format(self._container_name))
+        if int(ret) != 0:
+            raise RuntimeError('Failed to stop LXC container {}.'
+                               .format(self._container_name))
+
+    def _lxc_destroy(self):
+        """Destroy a container.
+
+        :raises RuntimeError: If failed to destroy container.
+        """
+
+        ssh = SSH()
+        ssh.connect(self._node)
+
+        ret, _, _ = ssh.exec_command_sudo(
+            'lxc-destroy --force --name {0}'.format(self._container_name))
+        if int(ret) != 0:
+            raise RuntimeError('Failed to destroy LXC container {}.'
+                               .format(self._container_name))
+
+    def _lxc_wait(self, state):
+        """Wait for a specific container state.
+
+        :param state: Specify the container state(s) to wait for.
+        :type state: str
+        :raises RuntimeError: If failed to wait for state of a container.
+        """
+
+        ssh = SSH()
+        ssh.connect(self._node)
+
+        ret, _, _ = ssh.exec_command_sudo(
+            'lxc-wait --name {0} --state "{1}"'
+            .format(self._container_name, state))
+        if int(ret) != 0:
+            raise RuntimeError('Failed to wait for "{0}" of LXC container {1}.'
+                               .format(state, self._container_name))
+
+    def _lxc_cgroup(self, state_object, value=''):
+        """Manage the control group associated with a container.
+
+        :param state_object: Specify the state object name.
+        :param value: Specify the value to assign to the state object. If empty,
+        then action is GET, otherwise is action SET.
+        :type state_object: str
+        :type value: str
+        :raises RuntimeError: If failed to get/set for state of a container.
+        """
+
+        ssh = SSH()
+        ssh.connect(self._node)
+
+        ret, _, _ = ssh.exec_command_sudo(
+            'lxc-cgroup --name {0} {1} {2}'
+            .format(self._container_name, state_object, value))
+        if int(ret) != 0:
+            if value:
+                raise RuntimeError('Failed to set {0} of LXC container {1}.'
+                                   .format(state_object, self._container_name))
+            else:
+                raise RuntimeError('Failed to get {0} of LXC container {1}.'
+                                   .format(state_object, self._container_name))
+
+    def lxc_attach(self, command):
+        """Start a process inside a running container. Runs the specified
+        command inside the container specified by name. The container has to
+        be running already.
+
+        :param command: Command to run inside container.
+        :type command: str
+        :raises RuntimeError: If failed to run the command.
+        """
+        attach_cmd = 'lxc-attach --keep-env --name {0} -- /bin/sh -c "{1}"'\
+            .format(self._container_name, command)
+
+        ssh = SSH()
+        ssh.connect(self._node)
+
+        if not self.lxc_is_running():
+            raise RuntimeError('LXC {} is not running.'
+                               .format(self._container_name))
+
+        ret, _, _ = ssh.exec_command_sudo(attach_cmd, timeout=180)
+        if int(ret) != 0:
+            raise RuntimeError('Failed to run "{0}" on LXC container {1}.'
+                               .format(command, self._container_name))
+
+    def lxc_exist(self):
+        """Check if LXC container exists on node."""
+
+        ssh = SSH()
+        ssh.connect(self._node)
+
+        ret, _, _ = ssh.exec_command_sudo(
+            'lxc-info --name {0}'.format(self._container_name))
+        if int(ret) != 0:
+            return False
+
+        return True
+
+    def lxc_is_created(self, force_create=True):
+        """Create and start a container."""
+
+        if self.lxc_exist():
+            if force_create:
+                self.lxc_is_destroyed()
+            else:
+                return
+
+        self._lxc_checkconfig()
+        self._lxc_create(distro='ubuntu', release='xenial', arch='amd64')
+        self.lxc_is_started()
+
+    def lxc_is_started(self):
+        """Start a container and waits for running state."""
+
+        self._lxc_start()
+        self._lxc_wait('RUNNING')
+        self._lxc_info()
+
+    def lxc_is_running(self):
+        """Verify if container is running.
+
+        :raises RuntimeError: If failed to get info about a container.
+        """
+
+        ssh = SSH()
+        ssh.connect(self._node)
+
+        ret, stdout, _ = ssh.exec_command_sudo(
+            'lxc-info --state --name {0}'.format(self._container_name))
+        if int(ret) != 0:
+            raise RuntimeError('Failed to get info about LXC container {}.'
+                               .format(self._container_name))
+
+        if 'RUNNING' not in stdout:
+            return False
+
+        return True
+
+    def lxc_is_stopped(self):
+        """Stop a container and waits for stopped state."""
+
+        self._lxc_stop()
+        self._lxc_wait('STOPPED|FROZEN')
+        self._lxc_info()
+
+    def lxc_restart(self):
+        """Restart container."""
+
+        self.lxc_is_stopped()
+        self.lxc_is_started()
+
+    def lxc_is_destroyed(self):
+        """Stop and destroy a container."""
+
+        self._lxc_destroy()
+
+    def lxc_cpuset_cpus(self, container_cpu):
+        """Set cpuset.cpus control group associated with a container.
+
+        :param container_cpu: Cpuset.cpus string.
+        :type container_cpu: str
+        :raises RuntimeError: If failed to set cgroup for a container.
+        """
+
+        ssh = SSH()
+        ssh.connect(self._node)
+
+        ret, _, _ = ssh.exec_command_sudo('cgset --copy-from / lxc')
+        if int(ret) != 0:
+            raise RuntimeError('Failed to copy cgroup settings from root.')
+
+        self._lxc_cgroup(state_object='cpuset.cpus')
+        self._lxc_cgroup(state_object='cpuset.cpus', value=container_cpu)
+        self._lxc_cgroup(state_object='cpuset.cpus')
+
+    def lxc_host_dir_is_mounted(self):
+        """Mount shared folder inside container.
+
+        :raises RuntimeError: If failed to mount host dir in a container.
+        :raises RuntimeError: If container is not running.
+        """
+
+        ssh = SSH()
+        ssh.connect(self._node)
+
+        if not self.lxc_is_running():
+            raise RuntimeError('LXC Container {1} is not running'
+                               .format(self._container_name))
+
+        self.lxc_attach('mkdir -p {0}'.format(self._guest_dir))
+
+        mnt_cfg = 'lxc.mount.entry = {0} /var/lib/lxc/{1}/rootfs{2} ' \
+            'none bind 0 0'.format(self._host_dir, self._container_name,
+                                   self._guest_dir)
+        ret, _, _ = ssh.exec_command_sudo(
+            "sh -c 'echo \"{0}\" >> /var/lib/lxc/{1}/config'"
+            .format(mnt_cfg, self._container_name))
+        if int(ret) != 0:
+            raise RuntimeError('Failed to mount {0} in lxc: {1}'
+                               .format(self._host_dir, self._container_name))
+
+        self.lxc_restart()
+
+    # Installation of VPP
+    # Note: To install dkms inside container there is need to intall dkms
+    # package with:
+    # self.lxc_attach('apt-get update')
+    # self.lxc_attach('export DEBIAN_FRONTEND=noninteractive && '
+    #                 'export LC_ALL="en_US.UTF-8" && '
+    #                 'apt-get install -y dkms')
+    def lxc_vpp_is_installed(self):
+        """Install vpp inside a container.
+
+        :raises RuntimeError: If container is not running.
+        """
+
+        ssh = SSH()
+        ssh.connect(self._node)
+
+        if not self.lxc_is_running():
+            raise RuntimeError('LXC Container {1} is not running'
+                               .format(self._container_name))
+
+        #for i in $(ls --ignore="*dkms*" /mnt/host/install_dir/); do dpkg -i --force-all /mnt/host/install_dir/$i; done
+        self.lxc_attach('apt-get update')
+        self.lxc_attach('export DEBIAN_FRONTEND=noninteractive && '
+                        'export LC_ALL="en_US.UTF-8" && '
+                        'apt-get install -y dkms')
+        self.lxc_attach("sh -c 'dpkg -i --force-all {}/install_dir/*.deb'"
+                        .format(self._guest_dir))
+
+    def lxc_vpp_is_uninstalled(self):
+        """Uninstall vpp inside a container.
+
+        :raises RuntimeError: If container is not running.
+        """
+
+        ssh = SSH()
+        ssh.connect(self._node)
+
+        if not self.lxc_is_running():
+            raise RuntimeError('LXC Container {1} is not running'
+                               .format(self._container_name))
+
+        self.lxc_attach('apt-get purge -y "vpp.*"')
+
+    def lxc_vpp_is_restarted(self):
+        """Restart vpp service inside a container.
+
+        :raises RuntimeError: If container is not running.
+        """
+
+        ssh = SSH()
+        ssh.connect(self._node)
+
+        if not self.lxc_is_running():
+            raise RuntimeError('LXC Container {1} is not running'
+                               .format(self._container_name))
+
+        self.lxc_attach('service vpp restart')
