b5ef0fdcec1a3f91a3997533bbedc2aea0ed112c
Peter Mikus <pmikus@cisco.com>
2017-05-31 13:25:24 +0000

    VIRL lspci fix
    Change-Id: I884b56c5cc2e0d5f40c0e9d0b5fb7356dc3b4028
    Signed-off-by: pmikus <pmikus@cisco.com>

--- resources/libraries/python/InterfaceUtil.py
+++ resources/libraries/python/InterfaceUtil.py
@@ -348,27 +348,38 @@ class InterfaceUtil(object):
         ssh = SSH()
         ssh.connect(node)
 
-        # First rescan PCI devices in the system
-        cmd = 'sh -c "echo 1 > /sys/bus/pci/rescan"'
-        (ret_code, _, _) = ssh.exec_command_sudo(cmd)
-        if int(ret_code) != 0:
-            raise RuntimeError("'{0}' failed on '{1}'"
-                               .format(cmd, node['host']))
+        value = None
+        found = False
 
-        cmd = 'lspci -vmmks {0}'.format(pci_addr)
-        (ret_code, stdout, _) = ssh.exec_command(cmd)
-        if int(ret_code) != 0:
-            raise RuntimeError("'{0}' failed on '{1}'"
-                               .format(cmd, node['host']))
+        for i in range(3):
+            logger.trace('Try {}: Get interface driver'.format(i))
+            cmd = 'sh -c "echo 1 > /sys/bus/pci/rescan"'
+            (ret_code, _, _) = ssh.exec_command_sudo(cmd)
+            if int(ret_code) != 0:
+                raise RuntimeError("'{0}' failed on '{1}'"
+                                   .format(cmd, node['host']))
 
-        for line in stdout.splitlines():
-            if len(line) == 0:
-                continue
-            (name, value) = line.split("\t", 1)
-            if name == 'Driver:':
-                return value
+            cmd = 'lspci -vmmks {0}'.format(pci_addr)
+            (ret_code, stdout, _) = ssh.exec_command(cmd)
+            if int(ret_code) != 0:
+                raise RuntimeError("'{0}' failed on '{1}'"
+                                   .format(cmd, node['host']))
 
-        return None
+            for line in stdout.splitlines():
+                if len(line) == 0:
+                    continue
+                (name, lvalue) = line.split("\t", 1)
+                if name == 'Driver:':
+                    found = True
+                    value = lvalue
+                    break
+            if found:
+                break
+        else:
+            raise RuntimeError('Get interface driver for: {0}'\
+                .format(if_pci))
+
+        return value
 
     @staticmethod
     def tg_set_interfaces_udev_rules(node):
