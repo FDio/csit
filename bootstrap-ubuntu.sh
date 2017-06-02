3b101eb30eb5e0802a939492cec622b6c14b74f8
Jan Gelety <jgelety@cisco.com>
2017-06-02 09:49:39 +0200

    Remove tb4-virl1 IP from VIRL_SERVERS list
    Change-Id: I09166ccf78757f455da13e3f519ba3b735184d23
    Signed-off-by: Jan Gelety <jgelety@cisco.com>

--- bootstrap-ubuntu.sh
+++ bootstrap-ubuntu.sh
@@ -21,7 +21,7 @@ export DEBIAN_FRONTEND=noninteractive
 sudo apt-get -y update
 sudo apt-get -y install libpython2.7-dev python-virtualenv
 
-VIRL_SERVERS=("10.30.51.28" "10.30.51.29" "10.30.51.30")
+VIRL_SERVERS=("10.30.51.29" "10.30.51.30")
 
 VIRL_USERNAME=jenkins-in
 VIRL_PKEY=priv_key
