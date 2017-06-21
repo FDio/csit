ef8525b911b687616410daa6461a627a4ff12316
Fangyin Hu <fangyinx.hu@intel.com>
2017-06-20 22:08:17 -0400

    Patch to tldk bootstrap on the tldk package download.     Rebase and fix the path issue.
    Change-Id: I572e4c81feb54e7f391af0007af709cafa42c255
    Signed-off-by: qun wan <qun.wan@intel.com>
    Signed-off-by: Fangyin Hu <fangyinx.hu@intel.com>

--- bootstrap-TLDK.sh
+++ bootstrap-TLDK.sh
@@ -116,9 +116,10 @@ VIRL_DIR_LOC="/tmp"
 TLDK_TAR_FILE="tldk_depends.tar.gz"
 
 wget "fast.dpdk.org/rel/${DPDK_PACKAGE}"
+git clone https://gerrit.fd.io/r/tldk
 
 tar zcf ${TLDK_TAR_FILE} ${DPDK_PACKAGE} ./tldk/ \
-    ./tests/tldk/tldk_testconfig/ ./tests/tldk/tldk_deplibs/
+    ./tests/tldk/tldk_testconfig/
 
 cat ${VIRL_PKEY}
 # Copy the files to VIRL host
