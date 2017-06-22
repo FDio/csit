e5b2369ac5e1a0b41cbdbe46f08f61bb0a35eb30
Fangyin Hu <fangyinx.hu@intel.com>
2017-06-22 02:32:56 -0700

    Fix the tcpdump start issue.
    Change-Id: I529899bdc031e8d5e367bad5a829857c2c83a3ba
    Signed-off-by: Fangyin Hu <fangyinx.hu@intel.com>

--- nsh_sfc_tests/sfc_scripts/start_tcpdump.sh
+++ nsh_sfc_tests/sfc_scripts/start_tcpdump.sh
@@ -6,6 +6,11 @@ PWDDIR=$(pwd)
 cd ${ROOTDIR}/nsh_sfc_tests/sfc_scripts/
 sudo rm -f temp_packet.pcap
 
-sudo tcpdump -i $1 -c 1 -w temp_packet.pcap dst host $2 >/dev/null 2>&1 &
+sudo /usr/sbin/tcpdump -i $1 -c 1 -w temp_packet.pcap dst host $2 &
+
+if [ ! $? -eq 0 ]; then
+    echo "Start the tcpdump failed!!!"
+    exit 1
+fi
 
 cd ${PWDDIR}
