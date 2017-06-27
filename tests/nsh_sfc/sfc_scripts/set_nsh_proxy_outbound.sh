00a95f8547caec71ba402aa73626f083182be7cc
Fangyin Hu <fangyinx.hu@intel.com>
2017-06-26 20:34:56 -0700

    Change the bootstrap script file and test the CI-management. 	Debug the NSH SFC functional test. 	Change the test cases and not use the tcpdump. 	Delete the unused code. 	Change the directory structure.
    Change-Id: I52486b9ba4b02e305f702ffc8cdb84e941cb18ff
    Signed-off-by: Fangyin Hu <fangyinx.hu@intel.com>

--- tests/nsh_sfc/sfc_scripts/set_nsh_proxy_outbound.sh
+++ tests/nsh_sfc/sfc_scripts/set_nsh_proxy_outbound.sh
@@ -2,15 +2,20 @@
 
 set -x
 
+vpp_interface1=$1
+vpp_interface2=$2
+vpp_intf1_adj_mac=$3
+vpp_intf2_adj_mac=$4
+
 sudo service vpp restart
 sleep 5
 
-sudo vppctl set int state $1 up
+sudo vppctl set int state ${vpp_interface1} up
 sleep 10
-sudo vppctl set int ip table $1 0
-sudo vppctl set int ip address $1 192.168.50.76/24
+sudo vppctl set int ip table ${vpp_interface1} 0
+sudo vppctl set int ip address ${vpp_interface1} 192.168.50.76/24
 
-sudo vppctl create vxlan-gpe tunnel local 192.168.50.76 remote 192.168.50.71 vni 9 next-nsh encap-vrf-id 0 decap-vrf-id 0
+sudo vppctl create vxlan-gpe tunnel local 192.168.60.76 remote 192.168.60.71 vni 9 next-nsh encap-vrf-id 0 decap-vrf-id 0
 sudo vppctl set int l2 bridge vxlan_gpe_tunnel0 1 1
 
 sudo vppctl create vxlan tunnel src 192.168.50.76 dst 192.168.50.72 vni 1 encap-vrf-id 0 decap-next node nsh-proxy
@@ -26,7 +31,13 @@ sudo vppctl create nsh map nsp 185 nsi 255 mapped-nsp 185 mapped-nsi 255 nsh_act
 sudo vppctl create nsh map nsp 185 nsi 254 mapped-nsp 185 mapped-nsi 254 nsh_action push encap-vxlan-gpe-intf ${vxlan_gpe_index}
 
 sudo vppctl ip route add 192.168.50.72/24 via 192.168.50.76
-sudo vppctl set ip arp $1 192.168.50.71 $2
-sudo vppctl set ip arp $1 192.168.50.72 $2
+
+sudo vppctl set int state ${vpp_interface2} up
+sleep 10
+sudo vppctl set int ip table ${vpp_interface2} 0
+sudo vppctl set int ip address ${vpp_interface2} 192.168.60.76/24
+
+sudo vppctl ip route add 192.168.60.71/24 via 192.168.60.76
+sudo vppctl set ip arp ${vpp_interface2} 192.168.60.71 ${vpp_intf2_adj_mac}
 
 sudo vppctl trace add dpdk-input 100
