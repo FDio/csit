97f62b1e23d1f34d172fff6f44b73a9c9758bc19
selias <samelias@cisco.com>
2017-07-13 11:45:10 +0200

    HC Test: Update archive items path in csit-hc2vpp jobs
    merge after https://gerrit.fd.io/r/7314
    
    Change-Id: I36d10bc7fd3c720eaf75a427b691d516a7399ea0
    Signed-off-by: selias <samelias@cisco.com>
    (cherry picked from commit e6d3baeead965e36546efd4bb6d80f974b650d29)

--- bootstrap-hc2vpp-integration.sh
+++ bootstrap-hc2vpp-integration.sh
@@ -29,6 +29,8 @@ VIRL_PKEY=priv_key
 VIRL_SERVER_STATUS_FILE="status"
 VIRL_SERVER_EXPECTED_STATUS="PRODUCTION"
 
+ARCHIVE_ARTIFACTS=(log.html output.xml report.html honeycomb.log)
+
 STREAM=$1
 OS=$2
 
@@ -228,6 +230,16 @@ PYTHONPATH=`pwd` pybot -L TRACE -W 136\
     --noncritical EXPECTED_FAILING \
     tests/
 
+RETURN_STATUS=$?
+
 # Get Honeycomb log file from virl host
 scp ${SSH_OPTIONS} \
     ${VIRL_USERNAME}@${VIRL_SERVER}:/scratch/${VIRL_SID}/honeycomb.log . || true
+
+# Archive artifacts
+mkdir archive
+for i in ${ARCHIVE_ARTIFACTS[@]}; do
+    cp $( readlink -f ${i} | tr '\n' ' ' ) archive/
+done
+
+exit ${RETURN_STATUS}
