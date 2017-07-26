98176e1b2c5eaee3030aea8f01aec5c79a843472
Fangyin Hu <fangyinx.hu@intel.com>
2017-07-25 21:05:11 -0700

    Add the NSH Over Ethernet test cases.
    Change-Id: Ia82cafb6910fcd495a7dc82ead5b370c2b375ebd
    Signed-off-by: Fangyin Hu <fangyinx.hu@intel.com>

--- tests/nsh_sfc/sfc_scripts/install_sfc.sh
+++ tests/nsh_sfc/sfc_scripts/install_sfc.sh
@@ -5,15 +5,6 @@ set -x
 ROOTDIR=/tmp/openvpp-testing
 PWDDIR=$(pwd)
 
-if_name1=$1
-if_name2=$2
-
-VPP_VERSION=`cat ${ROOTDIR}/NSH_SFC_VER | grep VPP | awk -F'= ' '{print $2}'`
-NSH_SFC_VERSION=`cat ${ROOTDIR}/NSH_SFC_VER | grep NSH_SFC | awk -F'= ' '{print $2}'`
-
-VPP_CODE_DIR=${ROOTDIR}/vpp_codes
-NSH_SFC_CODE_DIR=${ROOTDIR}/nsh_sfc_codes
-
 #at first, we need to stop the vpp service if have
 sudo service vpp stop
 
@@ -23,12 +14,7 @@ sudo service vpp stop
 nsh_need_install=0
 sudo dpkg -l vpp-nsh-plugin >/dev/null 2>&1
 if [ $? -eq 0 ]; then
-    nsh_plugin_version=`dpkg -s vpp-nsh-plugin | grep Version | awk -F' ' '{print $2}'`
-    if [ "${nsh_plugin_version}" \< "${NSH_SFC_VERSION}" ]; then
-        sudo dpkg -P vpp-nsh-plugin vpp-nsh-plugin-dbg vpp-nsh-plugin-dev >/dev/null 2>&1
-        test $? -eq 0 || exit 1
-        nsh_need_install=1
-    fi
+    nsh_need_install=0
 else
     nsh_need_install=1
 fi
@@ -36,54 +22,24 @@ fi
 vpp_need_install=0
 sudo dpkg -l vpp >/dev/null 2>&1
 if [ $? -eq 0 ]; then
-    vpp_version=`dpkg -s vpp | grep Version | awk -F' ' '{print $2}'`
-    if [ "${vpp_version}" \< "${VPP_VERSION}" ]; then
-        sudo dpkg -P vpp vpp-dbg vpp-dev vpp-dpdk-dev vpp-dpdk-dkms vpp-lib \
-                     vpp-plugins vpp-python-api >/dev/null 2>&1
-        test $? -eq 0 || exit 1
-        vpp_need_install=1
-    fi
+    vpp_need_install=0
 else
     vpp_need_install=1
 fi
 
-sleep 5
+#if vpp or nsh_sfc plugin not install, we will reinstall them
+cd ${ROOTDIR}
+VPP_NSH_DEBS=(*.deb)
 
-##begin to clone the vpp source code
-if [ ${vpp_need_install} -eq 1 ]; then
-    sudo rm -rf ${VPP_CODE_DIR}
-    sudo mkdir -p ${VPP_CODE_DIR}
-    cd ${VPP_CODE_DIR}
-    git clone -b v${VPP_VERSION} https://gerrit.fd.io/r/vpp
+if [ ${vpp_need_install} -eq 1 ] || [ ${nsh_need_install} -eq 1 ]; then
+    sudo dpkg -P vpp-nsh-plugin vpp-nsh-plugin-dbg vpp-nsh-plugin-dev >/dev/null 2>&1
+    sudo dpkg -P vpp vpp-dbg vpp-dev vpp-dpdk-dev vpp-dpdk-dkms vpp-lib \
+                vpp-plugins vpp-python-api >/dev/null 2>&1
 
-    #compile the vpp code
-    cd ./vpp/build-root/
-    make distclean
-    ./bootstrap.sh
-    make V=0 PLATFORM=vpp TAG=vpp install-deb
-
-    #after that, install vpp
-    sudo dpkg -i *.deb
-    cd ${PWDDIR}
+    sudo dpkg -i ${VPP_NSH_DEBS[@]}
+    test $? -eq 0 || exit 1
 fi
-
-##begin to clone the nsh sfc source code
-if [ ${nsh_need_install} -eq 1 ]; then
-    sudo rm -rf ${NSH_SFC_CODE_DIR}
-    sudo mkdir -p ${NSH_SFC_CODE_DIR}
-    cd ${NSH_SFC_CODE_DIR}
-    git clone -b v${NSH_SFC_VERSION} https://gerrit.fd.io/r/nsh_sfc
-
-    #compile the nsh sfc code
-    cd ./nsh_sfc/nsh-plugin/
-    sudo rm -rf build
-    sudo ./build.sh
-
-    #after that, install the nsh sfc plugin
-    cd ./packaging/
-    sudo dpkg -i *.deb
-    cd ${PWDDIR}
-fi
+cd ${PWDDIR}
 
 #check and setup the hugepages
 SYS_HUGEPAGE=$(cat /sys/kernel/mm/hugepages/hugepages-2048kB/nr_hugepages)
