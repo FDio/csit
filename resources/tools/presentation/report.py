557014322f6b6933210440be01091e34fea50248
Tibor Frank <tifrank@cisco.com>
2017-08-11 10:44:36 +0200

    CSIT-755: Presentation and analytics layer
    - CSIT-760: Configuration - real example
    - CSIT-774: Implementation - parse configuration
    - CSIT-779: Implementation - set environment
    - CSIT-780: Implementation - download data
    - CSIT-783: Implementation - debug mode
    - CSIT-761: Implementation - Data pre-processing - parse input files
    - CSIT-784: Implementation - Data pre-processing - store the data, access to data
    - CSIT-789: Implementation - Data pre-processing - extract Documentation of the suite
    - CSIT-757: Low Level Design
    - CSIT-788: Implementation - Data pre-processing - extract VAT history and show runtime
    - CSIT-785: Implementation - Data filtering
    - CSIT-763: Presentation - tables
    - CSIT-804: Presentation - files
    - CSIT-762: Presentation - plots
    - LLD: API + functional diagram
    - CSIT-807: Element's models
    - CSIT-813: Process static content
    - CSIT-812: Report generation
    - CSIT-764: Integration to CSIT
    - CSIT-822: Archiving
    - CSIT-790: Documentation
    - configuration od the Input data is the same as for 17.07 report
    
    Change-Id: I6fd1eb1df4af99eaf91925282cdee1c892698c59
    Signed-off-by: Tibor Frank <tifrank@cisco.com>

---
+++ resources/tools/presentation/report.py
@@ -0,0 +1,238 @@
+# Copyright (c) 2017 Cisco and/or its affiliates.
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at:
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Report generation.
+"""
+
+import subprocess
+import logging
+import datetime
+
+from os import makedirs, environ
+from os.path import isdir
+from shutil import copy, Error, make_archive
+
+from utils import get_files
+from errors import PresentationError
+
+
+# .css file for the html format of the report
+THEME_OVERRIDES = """/* override table width restrictions */
+@media screen and (min-width: 767px) {
+    .wy-table-responsive table td, .wy-table-responsive table th {
+        white-space: normal !important;
+    }
+
+    .wy-table-responsive {
+        font-size: small;
+        margin-bottom: 24px;
+        max-width: 100%;
+        overflow: visible !important;
+    }
+}
+"""
+
+# Command to build the html format of the report
+HTML_BUILDER = 'sphinx-build -v -c . -a ' \
+               '-b html -E ' \
+               '-D release={release} ' \
+               '-D version="{release} report - {date}" ' \
+               '{working_dir} ' \
+               '{build_dir}/'
+
+# Command to build the pdf format of the report
+PDF_BUILDER = 'sphinx-build -v -c . -a ' \
+              '-b latex -E ' \
+              '-D release={release} ' \
+              '-D version="{release} report - {date}" ' \
+              '{working_dir} ' \
+              '{build_dir}'
+
+
+def generate_report(release, config):
+    """Generate all formats and versions of the report.
+
+    :param release: Release string of the product.
+    :param config: Configuration read from the specification file.
+    :type release: str
+    :type config: Configuration
+    """
+
+    logging.info("Generating the report ...")
+
+    report = {
+        "html": generate_html_report,
+        "pdf": generate_pdf_report
+    }
+
+    for report_format, versions in config.output.items():
+        report[report_format](release, config, versions)
+
+    archive_input_data(config)
+    archive_report(config)
+
+    logging.info("Done.")
+
+
+def generate_html_report(release, config, versions):
+    """Generate html format of the report.
+
+    :param release: Release string of the product.
+    :param config: Configuration read from the specification file.
+    :param versions: List of versions to generate.
+    :type release: str
+    :type config: Configuration
+    :type versions: list
+    """
+
+    logging.info("  Generating the html report, give me a few minutes, please "
+                 "...")
+
+    cmd = HTML_BUILDER.format(
+        release=release,
+        date=datetime.date.today().strftime('%d-%b-%Y'),
+        working_dir=config.environment["paths"]["DIR[WORKING,SRC]"],
+        build_dir=config.environment["paths"]["DIR[BUILD,HTML]"])
+    _execute_command(cmd)
+
+    with open(config.environment["paths"]["DIR[CSS_PATCH_FILE]"], "w") as \
+            css_file:
+        css_file.write(THEME_OVERRIDES)
+
+    logging.info("  Done.")
+
+
+def generate_pdf_report(release, config, versions):
+    """Generate html format of the report.
+
+    :param release: Release string of the product.
+    :param config: Configuration read from the specification file.
+    :param versions: List of versions to generate.
+    :type release: str
+    :type config: Configuration
+    :type versions: list
+    """
+
+    logging.info("  Generating the pdf report, give me a few minutes, please "
+                 "...")
+
+    convert_plots = "xvfb-run -a wkhtmltopdf {html} {pdf}.pdf"
+
+    # Convert PyPLOT graphs in HTML format to PDF.
+    plots  = get_files(config.environment["paths"]["DIR[STATIC,VPP]"], "html")
+    for plot in plots:
+        file_name = "{0}.pdf".format(plot.rsplit(".", 1)[0])
+        cmd = convert_plots.format(html=plot, pdf=file_name)
+        _execute_command(cmd)
+
+    # Generate the LaTeX documentation
+    build_dir = config.environment["paths"]["DIR[BUILD,LATEX]"]
+    cmd = PDF_BUILDER.format(
+        release=release,
+        date=datetime.date.today().strftime('%d-%b-%Y'),
+        working_dir=config.environment["paths"]["DIR[WORKING,SRC]"],
+        build_dir=build_dir)
+    _execute_command(cmd)
+
+    # Build pdf documentation
+    archive_dir = config.environment["paths"]["DIR[STATIC,ARCH]"]
+    cmds = [
+        'cd {build_dir} && '
+        'pdflatex -shell-escape -interaction nonstopmode csit.tex || true'.
+        format(build_dir=build_dir),
+        'cd {build_dir} && '
+        'pdflatex -interaction nonstopmode csit.tex || true'.
+        format(build_dir=build_dir),
+        'cd {build_dir} && '
+        'cp csit.pdf ../{archive_dir}/csit_{release}.pdf'.
+        format(build_dir=build_dir,
+               archive_dir=archive_dir,
+               release=release)
+    ]
+
+    for cmd in cmds:
+        _execute_command(cmd)
+
+    logging.info("  Done.")
+
+
+def archive_report(config):
+    """Archive the report.
+
+    :param config: Configuration read from the specification file.
+    :type config: Configuration
+    """
+
+    logging.info("  Archiving the report ...")
+
+    make_archive("csit.report",
+                 "gztar",
+                 base_dir=config.environment["paths"]["DIR[BUILD,HTML]"])
+
+    logging.info("  Done.")
+
+
+def archive_input_data(config):
+    """Archive the report.
+
+    :param config: Configuration read from the specification file.
+    :type config: Configuration
+    :raises PresentationError: If it is not possible to archive the input data.
+    """
+
+    logging.info("    Archiving the input data files ...")
+
+    data_files = get_files(config.environment["paths"]["DIR[WORKING,DATA]"])
+    dst = config.environment["paths"]["DIR[STATIC,ARCH]"]
+    logging.info("      Destination: {0}".format(dst))
+
+    try:
+        if not isdir(dst):
+            makedirs(dst)
+
+        for data_file in data_files:
+            logging.info("      Copying the file: {0} ...".format(data_file))
+            copy(data_file, dst)
+
+    except (Error, OSError) as err:
+        raise PresentationError("Not possible to archive the input data.",
+                                str(err))
+
+    logging.info("    Done.")
+
+
+def _execute_command(cmd):
+    """Execute the command in a subprocess and log the stdout and stderr.
+
+    :param cmd: Command to execute.
+    :type cmd: str
+    :returns: Return code of the executed command.
+    :rtype: int
+    """
+
+    env = environ.copy()
+    proc = subprocess.Popen(
+        [cmd],
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+        shell=True,
+        env=env)
+
+    stdout, stderr = proc.communicate()
+
+    logging.info(stdout)
+    logging.info(stderr)
+
+    if proc.returncode != 0:
+        logging.error("    Command execution failed.")
+    return proc.returncode
---
+++ resources/tools/presentation/utils.py
@@ -0,0 +1,104 @@
+# Copyright (c) 2017 Cisco and/or its affiliates.
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at:
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""General purpose utilities.
+"""
+
+from os import walk
+from os.path import join
+from math import sqrt
+
+
+def mean(items):
+    """Calculate mean value from the items.
+
+    :param items: Mean value is calculated from these items.
+    :type items: list
+    :returns: MEan value.
+    :rtype: float
+    """
+
+    return float(sum(items)) / len(items)
+
+
+def stdev(items):
+    """Calculate stdev from the items.
+
+    :param items: Stdev is calculated from these items.
+    :type items: list
+    :returns: Stdev.
+    :rtype: float
+    """
+
+    avg = mean(items)
+    variance = [(x - avg) ** 2 for x in items]
+    stddev = sqrt(mean(variance))
+    return stddev
+
+
+def relative_change(nr1, nr2):
+    """Compute relative change of two values.
+
+    :param nr1: The first number.
+    :param nr2: The second number.
+    :type nr1: float
+    :type nr2: float
+    :returns: Relative change of nr1.
+    :rtype: float
+    """
+
+    return (nr1 - nr2) / nr1 * 100
+
+
+def get_files(path, extension=None, full_path=True):
+    """Generates the list of files to process.
+
+    :param path: Path to files.
+    :param extension: Extension of files to process. If it is the empty string,
+    all files will be processed.
+    :param full_path: If True, the files with full path are generated.
+    :type path: str
+    :type extension: str
+    :type full_path: bool
+    :returns: List of files to process.
+    :rtype: list
+    """
+
+    file_list = list()
+    for root, _, files in walk(path):
+        for filename in files:
+            if extension:
+                if filename.endswith(extension):
+                    if full_path:
+                        file_list.append(join(root, filename))
+                    else:
+                        file_list.append(filename)
+            else:
+                file_list.append(join(root, filename))
+
+    return file_list
+
+
+def get_rst_title_char(level):
+    """Return character used for the given title level in rst files.
+
+    :param level: Level of the title.
+    :type: int
+    :returns: Character used for the given title level in rst files.
+    :rtype: str
+    """
+    chars = ('=', '-', '`', "'", '.', '~', '*', '+', '^')
+    if level < len(chars):
+        return chars[level]
+    else:
+        return chars[-1]
