5dbd45fffe746e1bb6e2d9e39016315686477199
Peter Mikus <pmikus@cisco.com>
2017-09-22 14:59:40 +0200

    CSIT-773 Implement RF keywords for k8s
    - Implementation of Test Suite Setup for Ligato vnf-agent testing
    - Implementation of KubernetesUtil for controlling kubectl
    - Yaml templates for L2XC topology with 1cswitch and 1cnf
    - Yaml templates for L2BD topology with 1cswitch and 1cnf
    - ligato bootstrap script for creating vnf-agent image
    
    Change-Id: Iebefde0eb984a27a0afcdf29fe549ca4edf8a61e
    Signed-off-by: Peter Mikus <pmikus@cisco.com>

---
+++ resources/libraries/bash/k8s_setup.sh
@@ -0,0 +1,82 @@
+#!/bin/bash
+# Copyright (c) 2017 Cisco and/or its affiliates.
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at:
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+#if test "$(id -u)" -ne 0
+#then
+#    echo "Please use root or sudo to be able to init Kubernetes"
+#    exit 1
+#fi
+
+set -x
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+K8S_URL="https://storage.googleapis.com/kubernetes-release"
+K8S_VERSION="v1.7.7"
+K8S_KUBECTL_BIN="/usr/local/bin/kubectl"
+K8S_KUBECTL_URL="${K8S_URL}/release/${K8S_VERSION}/bin/linux/amd64/kubectl"
+K8S_CALICO="${SCRIPT_DIR}/../../templates/kubernetes/calico_v2.4.1.yaml"
+K8S_CSIT="${SCRIPT_DIR}/../../templates/kubernetes/csit.yaml"
+
+for i in "$@"; do
+    case $i in
+        --version=*)
+            K8S_VERSION="${i#*=}"
+            shift ;;
+        --bin=*)
+            K8S_KUBECTL_BIN="${i#*=}"
+            shift ;;
+        *)
+            ;;
+    esac
+done
+
+trap "sudo kubeadm reset && rm -rf $HOME/.kube" ERR
+
+# If kubectl version does not match download it
+if ! sudo /usr/local/bin/kubectl version | grep ${K8S_VERSION}
+then
+    wget -O kubectl -q ${K8S_KUBECTL_URL} || \
+        { echo "Failed to download Kubernetes ${K8S_VERSION}"; exit 1; }
+    chmod +x ./kubectl
+    sudo mv ./kubectl ${K8S_KUBECTL_BIN}
+fi
+
+# Revert any changes made to this host by 'kubeadm init' or 'kubeadm join'
+sudo kubeadm reset || \
+    { echo "Failed to reset kubeadm"; exit 1; }
+
+# Ret up the Kubernetes master
+sudo kubeadm init --token-ttl 0 --pod-network-cidr=192.168.0.0/16 || \
+    { echo "Failed to init kubeadm"; exit 1; }
+
+rm -rf $HOME/.kube
+mkdir -p $HOME/.kube
+sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+sudo chown $(id -u):$(id -g) $HOME/.kube/config
+
+# Apply resources
+${K8S_KUBECTL_BIN} apply -f ${K8S_CALICO}  || \
+    { echo "Failed to apply Calico resources"; exit 1; }
+${K8S_KUBECTL_BIN} apply -f ${K8S_CSIT}  || \
+    { echo "Failed to apply CSIT resource"; exit 1; }
+
+# Update the taints
+${K8S_KUBECTL_BIN} taint nodes --all node-role.kubernetes.io/master- || \
+    { echo "Failed to taint nodes"; exit 1; }
+
+# Dump Kubernetes objects ...
+${K8S_KUBECTL_BIN} get services
+
+echo Kubernetes is ready
---
+++ resources/libraries/python/KubernetesUtils.py
@@ -0,0 +1,340 @@
+# Copyright (c) 2017 Cisco and/or its affiliates.
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at:
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Library to control Kubernetes kubectl."""
+
+import time
+import yaml
+
+from resources.libraries.python.constants import Constants
+from resources.libraries.python.topology import NodeType
+from resources.libraries.python.ssh import SSH
+from resources.libraries.python.CpuUtils import CpuUtils
+from resources.libraries.python.VppConfigGenerator import VppConfigGenerator
+
+__all__ = ["KubernetesUtils"]
+
+
+class KubernetesUtils(object):
+    """Kubernetes utilities class."""
+
+    def __init__(self):
+        """Initialize KubernetesUtils class."""
+        pass
+
+    @staticmethod
+    def setup_kubernetes_on_node(node):
+        """Set up Kubernetes on node.
+
+        :param node: DUT node.
+        :type node: dict
+        """
+        ssh = SSH()
+        ssh.connect(node)
+
+        cmd = '{dir}/{lib}/k8s_setup.sh --version={version} '\
+              '--bin={bin}'.format(dir=Constants.REMOTE_FW_DIR,
+                                   lib=Constants.RESOURCES_LIB_SH,
+                                   version=Constants.K8S_KUBECTL_VERSION,
+                                   bin=Constants.K8S_KUBECTL_BIN)
+        (ret_code, _, _) = ssh.exec_command(cmd, timeout=120)
+        if int(ret_code) != 0:
+            raise Exception('Failed to setup Kubernetes on {node}.'
+                            .format(node=node['host']))
+
+    @staticmethod
+    def setup_kubernetes_on_all_duts(nodes):
+        """Set up Kubernetes on all DUTs.
+
+        :param nodes: Topology nodes.
+        :type nodes: dict
+        """
+        for node in nodes.values():
+            if node['type'] == NodeType.DUT:
+                KubernetesUtils.setup_kubernetes_on_node(node)
+
+    @staticmethod
+    def apply_kubernetes_resource_on_node(node, yaml_file, **kwargs):
+        """Apply Kubernetes resource on node.
+
+        :param node: DUT node.
+        :param yaml_file: YAML configuration file.
+        :param kwargs: Key-value pairs to replace in YAML template.
+        :type node: dict
+        :type yaml_file: str
+        :type kwargs: dict
+        """
+        ssh = SSH()
+        ssh.connect(node)
+
+        stream = file('{tpl}/{yaml}'.format(tpl=Constants.RESOURCES_TPL_K8S,
+                                            yaml=yaml_file), 'r')
+
+        for data in yaml.load_all(stream):
+            data = reduce(lambda a, kv: a.replace(*kv), kwargs.iteritems(),
+                          yaml.dump(data, default_flow_style=False))
+            # Workaround to avoid using RAW string anotated with | in YAML as
+            # library + bash is misinterpreting spaces.
+            data = data.replace('.conf:\n', '.conf: |\n')
+            cmd = 'cat <<EOF | kubectl apply -f - \n{data}\nEOF'.format(
+                data=data)
+            (ret_code, _, _) = ssh.exec_command_sudo(cmd, timeout=120)
+            if int(ret_code) != 0:
+                raise Exception('Failed to apply Kubernetes template {yaml} '
+                                'on {node}.'.format(yaml=yaml_file,
+                                                    node=node['host']))
+
+    @staticmethod
+    def apply_kubernetes_resource_on_all_duts(nodes, yaml_file, **kwargs):
+        """Apply Kubernetes resource on all DUTs.
+
+        :param nodes: Topology nodes.
+        :param yaml_file: YAML configuration file.
+        :param kwargs: Key-value pairs to replace in YAML template.
+        :type nodes: dict
+        :type yaml_file: str
+        :type kwargs: dict
+        """
+        for node in nodes.values():
+            if node['type'] == NodeType.DUT:
+                KubernetesUtils.apply_kubernetes_resource_on_node(node,
+                                                                  yaml_file,
+                                                                  **kwargs)
+
+    @staticmethod
+    def create_kubernetes_cm_from_file_on_node(node, name, key, src_file):
+        """Create Kubernetes ConfigMap from file on node.
+
+        :param node: DUT node.
+        :param name: ConfigMap name.
+        :param key: Key (destination file).
+        :param src_file: Source file.
+        :type node: dict
+        :type name: str
+        :type key: str
+        :type src_file: str
+        """
+        ssh = SSH()
+        ssh.connect(node)
+
+        cmd = 'kubectl create -n csit configmap {name} --from-file={key}='\
+            '{src_file}'.format(name=name, key=key, src_file=src_file)
+        (ret_code, _, _) = ssh.exec_command_sudo(cmd, timeout=120)
+        if int(ret_code) != 0:
+            raise Exception('Failed to create Kubernetes ConfigMap {name} '
+                            'on {node}.'.format(name=name, node=node['host']))
+
+    @staticmethod
+    def create_kubernetes_cm_from_file_on_all_duts(nodes, name, key, src_file):
+        """Create Kubernetes ConfigMap from file on all DUTs.
+
+        :param nodes: Topology nodes.
+        :param name: ConfigMap name.
+        :param key: Key (destination file).
+        :param src_file: Source file.
+        :type nodes: dict
+        :type name: str
+        :type key: str
+        :type src_file: str
+        """
+        for node in nodes.values():
+            if node['type'] == NodeType.DUT:
+                KubernetesUtils.create_kubernetes_cm_from_file_on_node(node,
+                                                                       name,
+                                                                       key,
+                                                                       src_file)
+
+    @staticmethod
+    def delete_kubernetes_resource_on_node(node, name=None):
+        """Delete Kubernetes resource on node.
+
+        :param node: DUT node.
+        :name name: Name of resource.
+        :type node: dict
+        :type name: str
+        """
+        ssh = SSH()
+        ssh.connect(node)
+
+        name = '{name}'.format(name=name) if name else '--all'
+
+        cmd = 'kubectl delete -n csit pods,services,configmaps {name}'\
+            .format(name=name)
+        (ret_code, _, _) = ssh.exec_command_sudo(cmd, timeout=120)
+        if int(ret_code) != 0:
+            raise Exception('Failed to delete Kubernetes resources in CSIT '
+                            'namespace on {node}.'.format(node=node['host']))
+
+    @staticmethod
+    def delete_kubernetes_resource_on_all_duts(nodes, name=None):
+        """Delete all Kubernetes resource on all DUTs.
+
+        :param nodes: Topology nodes.
+        :name name: Name of resource.
+        :type nodes: dict
+        :type name: str
+        """
+        for node in nodes.values():
+            if node['type'] == NodeType.DUT:
+                KubernetesUtils.delete_kubernetes_resource_on_node(node, name)
+
+    @staticmethod
+    def describe_kubernetes_pods_on_node(node):
+        """Describe Kubernetes PODs on node.
+
+        :param node: DUT node.
+        :type node: dict
+        """
+        ssh = SSH()
+        ssh.connect(node)
+
+        cmd = 'kubectl describe -n csit pods'
+        (ret_code, _, _) = ssh.exec_command_sudo(cmd, timeout=120)
+        if int(ret_code) != 0:
+            raise Exception('Failed to describe Kubernetes PODs on {node}.'
+                            .format(node=node['host']))
+
+    @staticmethod
+    def describe_kubernetes_pods_on_all_duts(nodes):
+        """Describe Kubernetes PODs on all DUTs.
+
+        :param nodes: Topology nodes.
+        :type nodes: dict
+        """
+        for node in nodes.values():
+            if node['type'] == NodeType.DUT:
+                KubernetesUtils.describe_kubernetes_pods_on_node(node)
+
+    @staticmethod
+    def reset_kubernetes_on_node(node):
+        """Reset Kubernetes on node.
+
+        :param node: DUT node.
+        :type node: dict
+        """
+        ssh = SSH()
+        ssh.connect(node)
+
+        cmd = 'kubeadm reset && rm -rf $HOME/.kube'
+        (ret_code, _, _) = ssh.exec_command_sudo(cmd, timeout=120)
+        if int(ret_code) != 0:
+            raise Exception('Failed to reset Kubernetes on {node}.'
+                            .format(node=node['host']))
+
+    @staticmethod
+    def reset_kubernetes_on_all_duts(nodes):
+        """Reset Kubernetes on all DUTs.
+
+        :param nodes: Topology nodes.
+        :type nodes: dict
+        """
+        for node in nodes.values():
+            if node['type'] == NodeType.DUT:
+                KubernetesUtils.reset_kubernetes_on_node(node)
+
+    @staticmethod
+    def wait_for_kubernetes_pods_on_node(node):
+        """Wait for Kubernetes PODs to become in Running state on node.
+
+        :param node: DUT node.
+        :type node: dict
+        """
+        ssh = SSH()
+        ssh.connect(node)
+
+        cmd = 'kubectl get pods -n csit --no-headers'
+        for i in range(12):
+            (ret_code, stdout, _) = ssh.exec_command_sudo(cmd, timeout=120)
+            if int(ret_code) == 0:
+                ready = True
+                for line in stdout.splitlines():
+                    if 'Running' not in line:
+                        ready = False
+                if ready:
+                    return
+            time.sleep(5)
+        else:
+            raise RuntimeError('Kubernetes PODs are not ready on {node}.'
+                               .format(node=node['host']))
+
+    @staticmethod
+    def wait_for_kubernetes_pods_on_all_duts(nodes):
+        """Wait for Kubernetes PODs to become in Running state on all DUTs.
+
+        :param nodes: Topology nodes.
+        :type nodes: dict
+        """
+        for node in nodes.values():
+            if node['type'] == NodeType.DUT:
+                KubernetesUtils.wait_for_kubernetes_pods_on_node(node)
+
+    def create_kubernetes_vswitch_startup_config(self, **kwargs):
+        """Create Kubernetes VSWITCH startup configuration.
+
+        :param kwargs: Key-value pairs used to create configuration.
+        :param kwargs: dict
+        """
+        cpuset_cpus = \
+            CpuUtils.cpu_slice_of_list_per_node(node=kwargs['node'],
+                                                cpu_node=kwargs['cpu_node'],
+                                                skip_cnt=kwargs['cpu_skip'],
+                                                cpu_cnt=kwargs['cpu_cnt'],
+                                                smt_used=kwargs['smt_used'])
+
+        # Create config instance
+        vpp_config = VppConfigGenerator()
+        vpp_config.set_node(kwargs['node'])
+        vpp_config.add_unix_cli_listen(value='0.0.0.0:5002')
+        vpp_config.add_unix_nodaemon()
+        vpp_config.add_dpdk_socketmem('1024,1024')
+        vpp_config.add_heapsize('3G')
+        vpp_config.add_ip6_hash_buckets('2000000')
+        vpp_config.add_ip6_heap_size('3G')
+        if kwargs['framesize'] < 1522:
+            vpp_config.add_dpdk_no_multi_seg()
+        vpp_config.add_dpdk_dev_default_rxq(kwargs['rxq'])
+        vpp_config.add_dpdk_dev(kwargs['if1'], kwargs['if2'])
+        # We will pop first core from list to be main core
+        vpp_config.add_cpu_main_core(str(cpuset_cpus.pop(0)))
+        # if this is not only core in list, the rest will be used as workers.
+        if cpuset_cpus:
+            corelist_workers = ','.join(str(cpu) for cpu in cpuset_cpus)
+            vpp_config.add_cpu_corelist_workers(corelist_workers)
+        vpp_config.apply_config(filename=kwargs['filename'], restart_vpp=False)
+
+    def create_kubernetes_vnf_startup_config(self, **kwargs):
+        """Create Kubernetes VNF startup configuration.
+
+        :param kwargs: Key-value pairs used to create configuration.
+        :param kwargs: dict
+        """
+        cpuset_cpus = \
+            CpuUtils.cpu_slice_of_list_per_node(node=kwargs['node'],
+                                                cpu_node=kwargs['cpu_node'],
+                                                skip_cnt=kwargs['cpu_skip'],
+                                                cpu_cnt=kwargs['cpu_cnt'],
+                                                smt_used=kwargs['smt_used'])
+
+        # Create config instance
+        vpp_config = VppConfigGenerator()
+        vpp_config.set_node(kwargs['node'])
+        vpp_config.add_unix_cli_listen(value='0.0.0.0:5002')
+        vpp_config.add_unix_nodaemon()
+        # We will pop first core from list to be main core
+        vpp_config.add_cpu_main_core(str(cpuset_cpus.pop(0)))
+        # if this is not only core in list, the rest will be used as workers.
+        if cpuset_cpus:
+            corelist_workers = ','.join(str(cpu) for cpu in cpuset_cpus)
+            vpp_config.add_cpu_corelist_workers(corelist_workers)
+        vpp_config.add_plugin_disable('dpdk_plugin.so')
+        vpp_config.apply_config(filename=kwargs['filename'], restart_vpp=False)
