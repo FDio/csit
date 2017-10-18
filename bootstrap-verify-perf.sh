44a31ce5bda277050b33b171cd80070aedff8888
Tibor Frank <tifrank@cisco.com>
2017-10-18 17:14:11 +0200

    Add IPSEC tests to DAILY Jenkins job
    Change-Id: Ie4558b53a0c0a19b9fa24e951bcf0fcb320076b6
    Signed-off-by: Tibor Frank <tifrank@cisco.com>

--- bootstrap-verify-perf.sh
+++ bootstrap-verify-perf.sh
@@ -137,6 +137,7 @@ case "$TEST_TAG" in
               -v TOPOLOGY_PATH:${WORKING_TOPOLOGY} \
               -s "tests.vpp.perf" \
               --include ndrdiscANDnic_intel-x520-da2AND1t1cORndrdiscANDnic_intel-x520-da2AND2t2c \
+              --include ndrdiscAND1t1cANDipsecORndrdiscAND2t2cANDipsec \
               tests/
         RETURN_STATUS=$(echo $?)
         ;;
