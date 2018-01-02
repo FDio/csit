2c91462828ded2e58d886a40656a3d7e2ef3a1b7
Peter Mikus <pmikus@cisco.com>
2018-01-02 14:37:24 +0100

    TRex ASTF onboarding Part I
    - Preparing initialization of TRex in L7 mode
    - Updating Topology files
    - Adding sample ASTF profiles
    
    Change-Id: If71f7f8f3db66425a1b543f1d29069a7543f4090
    Signed-off-by: Peter Mikus <pmikus@cisco.com>

--- resources/libraries/python/constants.py
+++ resources/libraries/python/constants.py
@@ -35,6 +35,9 @@ class Constants(object):
     # QEMU install directory
     QEMU_INSTALL_DIR = '/opt/qemu-2.5.0'
 
+    # TRex install directory
+    TREX_INSTALL_DIR = '/opt/trex-core-2.34'
+
     # Kubernetes templates location
     RESOURCES_TPL_K8S = 'resources/templates/kubernetes'
 
