bd0dea3afa293aa24769668fa4f760cd50eebb74
Peter Mikus <pmikus@cisco.com>
2018-01-04 09:08:43 +0000

    Test trigger
    Change-Id: I09cf992bce91673ceeea529dfa087d73207ea4b1
    Signed-off-by: Peter Mikus <pmikus@cisco.com>

--- resources/libraries/python/VatExecutor.py
+++ resources/libraries/python/VatExecutor.py
@@ -152,6 +152,9 @@ class VatExecutor(object):
             vat=Constants.VAT_BIN_NAME,
             input=vat_name)
 
+        with open(vat_name, 'r') as tmp_f:
+            VatHistory.add_to_vat_history(node, tmp_f.read())
+
         try:
             (ret_code, stdout, stderr) = ssh.exec_command(cmd, timeout)
         except SSHTimeout:
@@ -196,6 +199,10 @@ class VatExecutor(object):
         cmd = "{vat} {json}".format(json="json" if json_out is True else "",
                                     vat=Constants.VAT_BIN_NAME)
         cmd_input = "exec exec {0}".format(fname)
+
+        VatHistory.add_to_vat_history(node, cmd_input)
+        with open(fname, 'r') as tmp_f:
+            VatHistory.add_to_vat_history(node, tmp_f.read())
 
         try:
             (ret_code, stdout, stderr) = ssh.exec_command_sudo(cmd, cmd_input,
