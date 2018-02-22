973261f413f9f00ce236135feb0fd586b3a46e09
Marek Gradzki <mgradzki@cisco.com>
2018-02-22 07:46:09 +0100

    Adapt hc2vpp-csit bootstrap scripts to HC2VPP-285/CIMANAGE-22
    Since
    
    https://gerrit.fd.io/r/#/c/10668/
    and
    https://gerrit.fd.io/r/#/c/10669/
    
    honeycomb packages from stable branches
    no longer depend on single VPP version.
    
    This patch updates download_hc_pkgs.hc script,
    used by hc2vpp-csit jobs, to use the latest honeycomb package
    in such case.
    
    Change-Id: If5b006e72caa8cc905137b1bf02af7d9e927e37a
    Signed-off-by: Marek Gradzki <mgradzki@cisco.com>

--- resources/tools/scripts/download_hc_pkgs.sh
+++ resources/tools/scripts/download_hc_pkgs.sh
@@ -68,13 +68,20 @@ for ART in ${NSH_ARTIFACTS}; do
 done
 
 # determine VPP dependency
+# use latest if honeycomb package does not depend on single VPP version, e.g. stable branches since HC2VPP-285
+VER="RELEASE"
 if [ "${OS}" == "centos7" ]; then
-    VER=`rpm -qpR honeycomb*.rpm | grep 'vpp ' | cut -d ' ' -f 3`
-    VER=${VER}.x86_64
+    HC_VER=`rpm -qpR honeycomb*.rpm | grep 'vpp ' | cut -d ' ' -f 3`
+    if [ "${HC_VER}" != "" ]; then
+        VER=${HC_VER}.x86_64
+    fi
 else
-    VER=`dpkg -I honeycomb*.deb | grep -oP 'vpp \(= \K[^\)]+'`
-    VER=${VER}_amd64
+    HC_VER=`dpkg -I honeycomb*.deb | grep -oP 'vpp \(= \K[^\)]+'`
+    if [ "${HC_VER}" != "" ]; then
+        VER=${HC_VER}_amd64
+    fi
 fi
+echo "Downloading vpp artifacts: ${VPP_ARTIFACTS}(${PACKAGE}), VER=${VER}"
 
 # download VPP packages
 for ART in ${VPP_ARTIFACTS}; do
