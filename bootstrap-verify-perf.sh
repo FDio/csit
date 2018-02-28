d23012c09135cefc4a1dea38f4f4c9528814adca
Jan Gelety <jgelety@cisco.com>
2018-02-28 16:12:10 +0100

    bisect 8 - parent patch ab7a805 NAT44: identity NAT (VPP-1073)
    Change-Id: Ie8e78844b72b418abeed77117b95c814af1b610c
    Signed-off-by: Jan Gelety <jgelety@cisco.com>

--- bootstrap-verify-perf.sh
+++ bootstrap-verify-perf.sh
@@ -15,9 +15,7 @@
 set -x
 
 # Space separated list of available testbeds, described by topology files
-TOPOLOGIES="topologies/available/lf_testbed1.yaml \
-            topologies/available/lf_testbed2.yaml \
-            topologies/available/lf_testbed3.yaml"
+TOPOLOGIES="topologies/available/lf_testbed2.yaml"
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
@@ -58,14 +56,14 @@ then
 #        wget -q "${VPP_REPO_URL}/vpp-dpdk-dkms/${DPDK_STABLE_VER}/vpp-dpdk-dkms-${DPDK_STABLE_VER}${VPP_CLASSIFIER}.deb" || exit
 #        wget -q "${VPP_REPO_URL}/vpp-lib/${VPP_STABLE_VER}/vpp-lib-${VPP_STABLE_VER}${VPP_CLASSIFIER}.deb" || exit
 #        wget -q "${VPP_REPO_URL}/vpp-plugins/${VPP_STABLE_VER}/vpp-plugins-${VPP_STABLE_VER}${VPP_CLASSIFIER}.deb" || exit
-        VPP_URL="https://jenkins.fd.io/job/vpp-verify-1801-ubuntu1604/82/artifact/build-root"
-        VPP_VER="18.01-rc0~407-g9413fae~b82_amd64"
+        VPP_URL="https://jenkins.fd.io/job/vpp-verify-1801-ubuntu1604/83/artifact/build-root"
+        VPP_VER="18.01-rc0~406-g5ec9444~b83_amd64"
         wget -q "${VPP_URL}/vpp_${VPP_VER}.deb" || exit
         wget -q "${VPP_URL}/vpp-dbg_${VPP_VER}.deb" || exit
         wget -q "${VPP_URL}/vpp-dev_${VPP_VER}.deb" || exit
         wget -q "${VPP_URL}/vpp-lib_${VPP_VER}.deb" || exit
         wget -q "${VPP_URL}/vpp-plugins_${VPP_VER}.deb" || exit
-        wget -q "https://jenkins.fd.io/job/vpp-verify-1801-ubuntu1604/82/artifact/dpdk/vpp-dpdk-dkms_17.11-vpp1_amd64.deb" || exit
+        wget -q "https://jenkins.fd.io/job/vpp-verify-1801-ubuntu1604/83/artifact/dpdk/vpp-dpdk-dkms_17.08-vpp2_amd64.deb" || exit
         VPP_DEBS="$( readlink -f *.deb | tr '\n' ' ' )"
     fi
 
