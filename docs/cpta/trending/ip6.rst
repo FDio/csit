6d2a9a1ff17a6b7a03f70ece7e9eaadddc8ae2af
Tibor Frank <tifrank@cisco.com>
2018-03-01 14:52:47 +0100

    CSIT-913: Continuous Trending, Analysis and Change Detection
     - CSIT-915: LLD
     - CSIT-917: Functions to evaluate the results according to the PASS / FAIL criteria
     - CSIT-918: Sphinx configuration
     - CSIT-948: Statistical functions
     - CSIT-949: Data models for trending plots
     - CSIT-950: Code trending plots
     - CSIT-951: Static content
     - CSIT-984: PAL Specification file
     - CSIT-996: Download data from nexus
    
    Change-Id: Icb9305945bb0f142135bb177cb8781ba0096280e
    Signed-off-by: Tibor Frank <tifrank@cisco.com>

---
+++ docs/cpta/trending/ip6.rst
@@ -0,0 +1,20 @@
+IPv6 Routed-Forwarding
+======================
+
+.. raw:: html
+
+    <iframe width="1100" height="800" frameborder="0" scrolling="no" src="../_static/vpp/cpmt-ip6-1.html"></iframe>
+
+*Figure 1. Daily trend.*
+
+.. raw:: html
+
+    <iframe width="1100" height="800" frameborder="0" scrolling="no" src="../_static/vpp/cpmt-ip6-5.html"></iframe>
+
+*Figure 2. Weekly trend.*
+
+.. raw:: html
+
+    <iframe width="1100" height="800" frameborder="0" scrolling="no" src="../_static/vpp/cpmt-ip6-30.html"></iframe>
+
+*Figure 3. Monthly trend.*
--- resources/tools/presentation/run_cpta.sh
+++ resources/tools/presentation/run_cpta.sh
@@ -1,27 +1,16 @@
 #!/bin/bash
 
-set -x
-
-RELEASE=$1
+set +x
 
 # set default values in config array
-typeset -A CFG
 typeset -A DIR
 
 DIR[WORKING]=_tmp
-CFG[BLD_LATEX]=1
 
 # Install system dependencies
 sudo apt-get -y update
 sudo apt-get -y install libxml2 libxml2-dev libxslt-dev build-essential \
     zlib1g-dev unzip
-
-if [[ ${CFG[BLD_LATEX]} -eq 1 ]] ;
-then
-    sudo apt-get -y install xvfb texlive-latex-recommended \
-        texlive-fonts-recommended texlive-fonts-extra texlive-latex-extra latexmk wkhtmltopdf inkscape
-    sudo sed -i.bak 's/^\(main_memory\s=\s\).*/\110000000/' /usr/share/texlive/texmf-dist/web2c/texmf.cnf
-fi
 
 # Clean-up when finished
 trap 'rm -rf ${DIR[WORKING]}; exit' EXIT
@@ -40,8 +29,7 @@ pip install -r requirements.txt
 export PYTHONPATH=`pwd`
 
 python pal.py \
-    --specification specification.yaml \
-    --release ${RELEASE} \
+    --specification specification_CPTA.yaml \
     --logging INFO \
     --force
 
