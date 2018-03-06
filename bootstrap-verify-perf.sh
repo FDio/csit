2ee5b26a49f6cfbf028c385cf5fc8a4d5a1fb596
Jan Gelety <jgelety@cisco.com>
2018-03-06 09:24:42 +0100

    bisect12 - bisect6 with reverted dpdk
    - added x710 pdr tests
    
    Change-Id: I71c7dff78307d3bd7c9df676cbf774141ad47995
    Signed-off-by: Jan Gelety <jgelety@cisco.com>

--- bootstrap-verify-perf.sh
+++ bootstrap-verify-perf.sh
@@ -15,9 +15,7 @@
 set -x
 
 # Space separated list of available testbeds, described by topology files
-TOPOLOGIES="topologies/available/lf_testbed1.yaml \
-            topologies/available/lf_testbed2.yaml \
-            topologies/available/lf_testbed3.yaml"
+TOPOLOGIES="topologies/available/lf_testbed3.yaml"
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
@@ -58,14 +56,14 @@ then
 #        wget -q "${VPP_REPO_URL}/vpp-dpdk-dkms/${DPDK_STABLE_VER}/vpp-dpdk-dkms-${DPDK_STABLE_VER}${VPP_CLASSIFIER}.deb" || exit
 #        wget -q "${VPP_REPO_URL}/vpp-lib/${VPP_STABLE_VER}/vpp-lib-${VPP_STABLE_VER}${VPP_CLASSIFIER}.deb" || exit
 #        wget -q "${VPP_REPO_URL}/vpp-plugins/${VPP_STABLE_VER}/vpp-plugins-${VPP_STABLE_VER}${VPP_CLASSIFIER}.deb" || exit
-        VPP_URL="https://jenkins.fd.io/job/vpp-verify-1801-ubuntu1604/91/artifact/build-root"
-        VPP_VER="18.01-rc0~419-g5a71869~b91_amd64"
+        VPP_URL="https://jenkins.fd.io/job/vpp-verify-1801-ubuntu1604/93/artifact/build-root"
+        VPP_VER="18.01-rc0~413-g44f22bf~b93_amd64"
         wget -q "${VPP_URL}/vpp_${VPP_VER}.deb" || exit
         wget -q "${VPP_URL}/vpp-dbg_${VPP_VER}.deb" || exit
         wget -q "${VPP_URL}/vpp-dev_${VPP_VER}.deb" || exit
         wget -q "${VPP_URL}/vpp-lib_${VPP_VER}.deb" || exit
         wget -q "${VPP_URL}/vpp-plugins_${VPP_VER}.deb" || exit
-        wget -q "https://jenkins.fd.io/job/vpp-verify-1801-ubuntu1604/91/artifact/dpdk/vpp-dpdk-dkms_17.08-vpp2_amd64.deb" || exit
+        wget -q "https://jenkins.fd.io/job/vpp-verify-1801-ubuntu1604/93/artifact/dpdk/vpp-dpdk-dkms_17.08-vpp2_amd64.deb" || exit
         VPP_DEBS="$( readlink -f *.deb | tr '\n' ' ' )"
     fi
 
