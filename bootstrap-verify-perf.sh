a585a423fd7dd671fd93feba887d5e599bed8783
Jan Gelety <jgelety@cisco.com>
2018-03-19 08:48:41 +0100

    CSIT-998: MRR tests
     - CSIT-1001: MRR tests: IP4
       - ip4
         - 10ge2p1x520-ethip4-ip4base-copwhtlistbase-mrr.robot
         - 10ge2p1x520-ethip4-ip4base-iacldstbase-mrr.robot
         - 10ge2p1x520-ethip4-ip4base-ipolicemarkbase-mrr.robot
         - 10ge2p1x520-ethip4-ip4scale200k-mrr.robot
         - 10ge2p1x520-ethip4-ip4scale20k-mrr.robot
         - 10ge2p1x520-ethip4-ip4scale2m-mrr.robot
         - 10ge2p1x520-ethip4udp-ip4base-iacl1sf-10kflows-mrr.robot
         - 10ge2p1x520-ethip4udp-ip4base-iacl1sl-10kflows-mrr.robot
         - 10ge2p1x520-ethip4udp-ip4base-iacl50sf-10kflows-mrr.robot
         - 10ge2p1x520-ethip4udp-ip4base-iacl50sl-10kflows-mrr.robot
         - 10ge2p1x520-ethip4udp-ip4base-nat44-mrr.robot
         - 10ge2p1x520-ethip4udp-ip4scale1000-udpsrcscale15-nat44-mrr.robot
         - 40ge2p1xl710-ethip4-ip4base-mrr.robot
       - ip4_tunnels
         - 10ge2p1x520-ethip4vxlan-l2bdbasemaclrn-mrr.robot
         - 10ge2p1x520-ethip4vxlan-l2xcbase-mrr.robot
         - 10ge2p1x520-ethip4lispip4-ip4base-mrr.robot
    
    Change-Id: Ibfd03aea9b97d29a8fa05e2956f6df54603a2208
    Signed-off-by: Jan Gelety <jgelety@cisco.com>

--- bootstrap-verify-perf.sh
+++ bootstrap-verify-perf.sh
@@ -177,7 +177,8 @@ case "$TEST_TAG" in
         pybot ${PYBOT_ARGS} \
               -v TOPOLOGY_PATH:${WORKING_TOPOLOGY} \
               -s "tests.vpp.perf" \
-              --include mrrAND1t1cORmrrAND2t2c \
+              --include mrrAND1t1cANDip4fwdORmrrAND2t2cANDip4fwd \
+              --include mrrAND1t1cANDip4unrlayORmrrAND2t2cANDip4unrlay \
               tests/
         RETURN_STATUS=$(echo $?)
         ;;
