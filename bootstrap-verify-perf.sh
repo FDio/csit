c590b01622c796b816d953c605694a3935127d8e
Jan Gelety <jgelety@cisco.com>
2018-03-22 13:57:51 +0100

    Update sr_steer_add_del.vat template
    Change-Id: I7b7fd3a8815cf33fa50ca5f3f7a9128edef8b827
    Signed-off-by: Jan Gelety <jgelety@cisco.com>

--- bootstrap-verify-perf.sh
+++ bootstrap-verify-perf.sh
@@ -356,7 +356,7 @@ case "$TEST_TAG" in
         # run full performance test suite and exit on fail
         pybot ${PYBOT_ARGS} \
               -v TOPOLOGY_PATH:${WORKING_TOPOLOGY} \
-              -s "tests.vpp.perf" \
+              -s "tests.vpp.perf.srv6" \
               tests/
         RETURN_STATUS=$(echo $?)
 esac
