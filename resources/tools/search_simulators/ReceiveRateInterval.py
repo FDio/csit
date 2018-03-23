2714048cf9708793a93090314fbc69389db3eb8c
Vratko Polak <vrpolak@cisco.com>
2018-03-23 17:00:49 +0100

    DO_NOT_MERGE: Rate simulators
    Change-Id: I58ceb54eebe8a45978c12303274ee3a5999c76b7
    Signed-off-by: Vratko Polak <vrpolak@cisco.com>

---
+++ resources/tools/search_simulators/ReceiveRateInterval.py
@@ -0,0 +1,32 @@
+# FIXME: License.
+
+from ReceiveRateMeasurement import ReceiveRateMeasurement
+
+
+class ReceiveRateInterval(object):
+    """Structure defining two Rr measurements, presumably close to each other."""
+
+    def __init__(self, measured_low, measured_high):
+        """Constructor, store the measurement after checking argument types."""
+        # TODO: Type checking is not very pythonic, perhaps users can fix wrong usage without it?
+        if not isinstance(measured_low, ReceiveRateMeasurement):
+            raise TypeError("measured_low is not a ReceiveRateMeasurement: " + repr(measured_low))
+        if not isinstance(measured_high, ReceiveRateMeasurement):
+            raise TypeError("measured_high is not a ReceiveRateMeasurement: " + repr(measured_high))
+        self.measured_low = measured_low
+        self.measured_high = measured_high
+        self.sort()
+
+    def sort(self):
+        """Interval always have to be sorted by Tr."""
+        if self.measured_low.transmit_rate > self.measured_high.transmit_rate:
+            self.measured_low, self.measured_high = self.measured_high, self.measured_low
+
+    def __str__(self):
+        """Return string as half-open interval."""
+        return "[" + str(self.measured_low) + ";" + str(self.measured_high) + ")"
+
+    def __repr__(self):
+        """Return string evaluable as a constructor call."""
+        return "ReceiveRateInterval(measured_low=" + repr(self.measured_low) + \
+               ",measured_high=" + repr(self.measured_high) + ")"
