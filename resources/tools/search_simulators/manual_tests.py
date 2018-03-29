9409588ff9873b74575235255edf331c46affad9
Vratko Polak <vrpolak@cisco.com>
2018-03-29 16:13:06 +0200

    DO_NOT_MERGE: Rate simulators
    Change-Id: I58ceb54eebe8a45978c12303274ee3a5999c76b7
    Signed-off-by: Vratko Polak <vrpolak@cisco.com>

---
+++ resources/tools/search_simulators/manual_tests.py
@@ -0,0 +1,56 @@
+# FIXME: License and similar.
+
+import logging
+
+#from KeyboardRateProvider import KeyboardRateProvider as provider
+#from RandomRateProvider import RandomRateProvider as provider
+from TwoPhaseRateProvider import TwoPhaseRateProvider as provider
+from LoggingRateProvider import LoggingRateProvider as infologging
+from TimeTrackingRateProvider import TimeTrackingRateProvider as tracking
+from BasicBinarySearchAlgorithm import BasicBinarySearchAlgorithm as search
+
+logging.basicConfig(level=getattr(logging, "INFO"))
+
+p = tracking(infologging(provider()))
+
+#print "basic binary search, 7 iterations, random provider, printing"
+##print "basic binary search, 7 iterations, two phase provider, printing"
+#
+#s = search(rate_provider=p, iterations=7, duration=60)
+#result = s.narrow_down_ndr_and_pdr(fail_rate=1000000, line_rate=30000000, allowed_drop_fraction=0.005)
+#
+#print "result repr:", repr(result)
+#print "result string:", str(result)
+#print "search took", p.total_time, "seconds"
+#p.total_time = 0.0
+#
+#with open("binary.txt", "a") as f:
+#    f.write(str(result.ndr_interval.measured_low.target_tr) + " ")
+#    f.write(str(result.ndr_interval.measured_high.target_tr) + " ")
+#    f.write(str(result.pdr_interval.measured_low.target_tr) + " ")
+#    f.write(str(result.pdr_interval.measured_high.target_tr))
+#    f.write('\n')
+#
+
+print "total time search, 60s final duration, random provider, printing"
+#print "relative width based search, 60s final duration, two phase provider"
+
+from RelativeWidthBasedSearch import RelativeWidthBasedSearch as my_search
+s = my_search(rate_provider=p, final_width=0.015)
+#from TotalTimeBasedSearch import TotalTimeBasedSearch as my_search
+#s = my_search(rate_provider=p, length=7.7)
+
+result = s.narrow_down_ndr_and_pdr(fail_rate=1000000, line_rate=30000000, allowed_drop_fraction=0.005)
+
+#print "result repr:", repr(result)
+print "result string:", str(result)
+print "RW:", "NDR", result.ndr_interval.rel_tr_width, "PDR", result.pdr_interval.rel_tr_width
+print "search took", p.total_time, "seconds"
+p.total_time = 0.0
+
+#with open("smart.txt", "a") as f:
+#    f.write(str(result.ndr_interval.measured_low.target_tr) + " ")
+#    f.write(str(result.ndr_interval.measured_high.target_tr) + " ")
+#    f.write(str(result.pdr_interval.measured_low.target_tr) + " ")
+#    f.write(str(result.pdr_interval.measured_high.target_tr))
+#    f.write('\n')
