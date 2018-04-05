0846f129fedcb3f4bcedf95b28eddc14aed34839
Vratko Polak <vrpolak@cisco.com>
2018-04-05 13:53:32 +0200

    CSIT-1030: Add x710 MRR tests - vm_vhost
    - DO_NOT_MERGE: non-standard bootstrap.
    
    + 10ge2p1x710-dot1q-l2bdbasemaclrn-eth-2vhostvr1024-1vm-mrr.robot
    + 10ge2p1x710-dot1q-l2xcbase-eth-2vhostvr1024-1vm-mrr.robot
    + 10ge2p1x710-eth-l2bdbasemaclrn-eth-2vhostvr256-1vm-mrr.robot
    + 10ge2p1x710-eth-l2bdbasemaclrn-eth-4vhostvr1024-2vm-mrr.robot
    + 10ge2p1x710-eth-l2xcbase-eth-2vhostvr1024-1vm-mrr.robot
    + 10ge2p1x710-eth-l2xcbase-eth-2vhostvr256-1vm-mrr.robot
    + 10ge2p1x710-eth-l2xcbase-eth-4vhostvr1024-2vm-mrr.robot
    + 10ge2p1x710-ethip4-ip4base-eth-2vhostvr1024-1vm-mrr.robot
    + 10ge2p1x710-ethip4-ip4base-eth-2vhostvr256-1vm-mrr.robot
    + 10ge2p1x710-ethip4-ip4base-eth-4vhostvr1024-2vm-mrr.robot
    + 10ge2p1x710-ethip4vxlan-l2bdbasemaclrn-eth-2vhostvr1024-1vm-mrr.robot
    
    Copies of the respective x520 files,
    with each "X520-DA2" or "X520" replaced by "X710".
    
    Change-Id: I15bce75b704072b26a908f868e90a42dc3ea7661
    Signed-off-by: Vratko Polak <vrpolak@cisco.com>

--- bootstrap-verify-perf.sh
+++ bootstrap-verify-perf.sh
@@ -192,7 +192,8 @@ case "$TEST_TAG" in
         pybot ${PYBOT_ARGS} \
               -v TOPOLOGY_PATH:${WORKING_TOPOLOGY} \
               -s "tests.vpp.perf" \
-              --include mrrAND1t1cORmrrAND2t2c \
+              --include mrrANDvhostANDnic_intel-x710 \
+              --exclude t4c4 \
               tests/
         RETURN_STATUS=$(echo $?)
         ;;
