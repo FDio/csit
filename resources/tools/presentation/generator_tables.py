6dd81b487807d0c57602287bbf7ba96013978255
Tibor Frank <tifrank@cisco.com>
2018-04-23 13:30:48 +0200

    CSIT-1041: Trending dashboard
    Change-Id: I3b8e545bd198281af502acc6e23268e840112423
    Signed-off-by: Tibor Frank <tifrank@cisco.com>

--- resources/tools/presentation/generator_tables.py
+++ resources/tools/presentation/generator_tables.py
@@ -786,13 +786,15 @@ def table_performance_trending_dashboard(table, input_data):
                     else classification
                 for idx in range(first_idx, len(classification_lst)):
                     if classification_lst[idx] == tmp_classification:
-                        index = idx
-                        break
+                        if rel_change_lst[idx]:
+                            index = idx
+                            break
                 for idx in range(index+1, len(classification_lst)):
                     if classification_lst[idx] == tmp_classification:
-                        if (abs(rel_change_lst[idx]) >
-                                abs(rel_change_lst[index])):
-                            index = idx
+                        if rel_change_lst[idx]:
+                            if (abs(rel_change_lst[idx]) >
+                                    abs(rel_change_lst[index])):
+                                index = idx
 
             trend = round(float(median_lst[-1]) / 1000000, 2) \
                 if not isnan(median_lst[-1]) else '-'
