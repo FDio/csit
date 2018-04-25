e519089c7d0e75a9a6a3ea0d6b8869351725902c
Vratko Polak <vrpolak@cisco.com>
2018-04-25 18:19:51 +0200

    CSIT-992: Add libraries for optimized search
    + Place the libraries into resources/libraries/python/search/.
     + Except OptimizedTrexSearch in TrafficGenerator.py
    + Change traffic generator to support floats for duration and warmup.
    + Remove explicit type conversions where not needed.
    + Add robot keywords to performance_utils.robot
     + for calling the optimized search.
     + for reporting the resulting values.
     + for checking the minimal performance has been reached.
     + for running five second "Traffic should pass with no loss" after the search.
    - Add methodology documentation in subsequent Change.
    - Add simulator for testing algorithm correctness in a subsequent Change.
    - Add tests using the libraries in subsequent Change.
    
    Change-Id: Ia041008382ee4c9a562172099aea794c854d5f2f
    Signed-off-by: Vratko Polak <vrpolak@cisco.com>

---
+++ resources/libraries/python/search/AbstractSearchAlgorithm.py
@@ -0,0 +1,46 @@
+# Copyright (c) 2018 Cisco and/or its affiliates.
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at:
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from abc import ABCMeta, abstractmethod
+
+
+class AbstractSearchAlgorithm(object):
+    """Abstract class defining common API for search algorithms."""
+
+    __metaclass__ = ABCMeta
+
+    def __init__(self, rate_provider):
+        """Store the rate provider.
+
+        :param rate_provider: Object able to perform trial measurements.
+        :type rate_provider: AbstractRateProvider
+        """
+        # TODO: Type check for AbstractRateProvider?
+        self.rate_provider = rate_provider
+
+    @abstractmethod
+    def narrow_down_ndr_and_pdr(self, fail_rate, line_rate, allowed_drop_fraction):
+        """Perform measurements to narrow down intervals, return them.
+
+        :param fail_rate: Minimal target transmit rate [pps].
+        :param line_rate: Maximal target transmit rate [pps].
+        :param allowed_drop_fraction: Fraction of dropped packets for PDR [1].
+        :type fail_rate: float
+        :type line_rate: float
+        :type allowed_drop_fraction: float
+        :returns: Structure containing narrowed down intervals
+            and their measurements.
+        :rtype: NdrPdrResult
+        """
+        # TODO: Do we agree on arguments related to precision or trial duration?
+        pass
---
+++ resources/libraries/python/search/NdrPdrResult.py
@@ -0,0 +1,58 @@
+# Copyright (c) 2018 Cisco and/or its affiliates.
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at:
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from resources.libraries.python.search.ReceiveRateInterval import ReceiveRateInterval
+
+
+class NdrPdrResult(object):
+    """Two measurement intervals, return value of search algorithms.
+
+    Partial fraction is NOT part of the result. Pdr interval should be valid
+    for all partial fractions implied by the interval."""
+
+    def __init__(self, ndr_interval, pdr_interval):
+        """Store the measured intervals after checking argument types.
+
+        :param ndr_interval: Object containing data for NDR part of the result.
+        :param pdr_interval: Object containing data for PDR part of the result.
+        :type ndr_interval: ReceiveRateInterval
+        :type pdr_interval: ReceiveRateInterval
+        """
+        # TODO: Type checking is not very pythonic, perhaps users can fix wrong usage without it?
+        if not isinstance(ndr_interval, ReceiveRateInterval):
+            raise TypeError("ndr_interval, is not a ReceiveRateInterval: " + repr(ndr_interval))
+        if not isinstance(pdr_interval, ReceiveRateInterval):
+            raise TypeError("pdr_interval, is not a ReceiveRateInterval: " + repr(pdr_interval))
+        self.ndr_interval = ndr_interval
+        self.pdr_interval = pdr_interval
+
+    def width_in_goals(self, relative_width_goal):
+        """Return a debug string related to current widths in logarithmic scale.
+
+        :param relative_width_goal: Upper bound times this is the goal
+            difference between upper bound and lower bound.
+        :type relative_width_goal: float
+        :returns: Message containing NDR and PDR widths in goals.
+        :rtype: str
+        """
+        return (  "ndr " + str(self.ndr_interval.width_in_goals(relative_width_goal))
+                + "; pdr " + str(self.pdr_interval.width_in_goals(relative_width_goal)))
+
+    def __str__(self):
+        """Return string as tuple of named values."""
+        return "NDR=" + str(self.ndr_interval) + ";PDR=" + str(self.pdr_interval)
+
+    def __repr__(self):
+        """Return string evaluable as a constructor call."""
+        return "NdrPdrResult(ndr_interval=" + repr(self.ndr_interval) + \
+               ",pdr_interval=" + repr(self.pdr_interval) + ")"
