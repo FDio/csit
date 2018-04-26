b789f4582ab94946229444e0b65c58e47531e842
Marek Gradzki <mgradzki@cisco.com>
2018-04-26 10:11:32 +0200

    HC tests: reenable NSH plugin (CSIT-994)
    Reverts https://gerrit.fd.io/r/#/c/11088/
    
    Change-Id: Ib7b70d33f4a8110656dd39f25b7f5a67a2f52104
    Signed-off-by: Marek Gradzki <mgradzki@cisco.com>

--- resources/tools/scripts/download_hc_build_pkgs.sh
+++ resources/tools/scripts/download_hc_build_pkgs.sh
@@ -22,9 +22,8 @@ OS=$2
 URL="https://nexus.fd.io/service/local/artifact/maven/content"
 VER="RELEASE"
 VPP_GROUP="io.fd.vpp"
-# TODO(CSIT-994): reenable NSH
-# NSH_GROUP="io.fd.nsh_sfc"
-# NSH_ARTIFACTS="vpp-nsh-plugin"
+NSH_GROUP="io.fd.nsh_sfc"
+NSH_ARTIFACTS="vpp-nsh-plugin"
 
 if [ "${OS}" == "ubuntu1404" ]; then
     OS="ubuntu.trusty.main"
@@ -51,12 +50,11 @@ for ART in ${VPP_ARTIFACTS}; do
     done
 done
 
-# TODO(CSIT-994): reenable NSH
-# for ART in ${NSH_ARTIFACTS}; do
-#     for PAC in ${PACKAGE}; do
-#         curl "${URL}?r=${REPO}&g=${NSH_GROUP}&a=${ART}&p=${PAC}&v=${VER}&c=${CLASS}" -O -J || exit
-#     done
-# done
+for ART in ${NSH_ARTIFACTS}; do
+    for PAC in ${PACKAGE}; do
+        curl "${URL}?r=${REPO}&g=${NSH_GROUP}&a=${ART}&p=${PAC}&v=${VER}&c=${CLASS}" -O -J || exit
+    done
+done
 
 # verify downloaded packages
 if [ "${OS}" == "centos7" ]; then
