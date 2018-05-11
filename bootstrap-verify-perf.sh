29769ba7d616412b4c0fc60f375bfc8d6ab5ee33
Jan Gelety <jgelety@cisco.com>
2018-05-11 10:36:41 +0200

    DPDK link bonding: L1 and L2 KWs
    CSIT-1073
    CSIT-1074
    
    Change-Id: I248e079021c30988210fd792b32d5afdca71aadb
    Signed-off-by: Jan Gelety <jgelety@cisco.com>

--- bootstrap-verify-perf.sh
+++ bootstrap-verify-perf.sh
@@ -264,7 +264,7 @@ case "$TEST_TAG" in
               'mrrANDnic_intel-xl710AND2t2cANDipsechw')
         ;;
     * )
-        TAGS=('perftest')
+        TAGS=('DPDK_LBOND')
 esac
 
 # Catenate TAG selections by 'OR'
