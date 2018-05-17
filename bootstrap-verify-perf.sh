d0a47e2e5af1af5783221fe35a50dee6c7dba9cb
Jan Gelety <jgelety@cisco.com>
2018-05-17 14:31:43 +0200

    VPP link bonding
    CSIT-1072
    CSIT-1083
    CSIT-1084
    
    Change-Id: Icaf0e09af633d301306a84251f47822e95b411a2
    Signed-off-by: Jan Gelety <jgelety@cisco.com>

--- bootstrap-verify-perf.sh
+++ bootstrap-verify-perf.sh
@@ -264,7 +264,7 @@ case "$TEST_TAG" in
               'mrrANDnic_intel-xl710AND2t2cANDipsechw')
         ;;
     * )
-        TAGS=('dpdk_lbond')
+        TAGS=('vpp_lbond')
 esac
 
 # Catenate TAG selections by 'OR'
