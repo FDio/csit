52c6688e432700f01b0c0d9136b56d6f35c24b0f
Vratko Polak <vrpolak@cisco.com>
2018-06-06 15:13:09 +0200

    CSIT-1110: Integrate anomaly detection into PAL
    + Do not remove (nor detect) outliers.
    + Trend line shows constant average withing a group.
    + Anomaly circles are placed at the changed average.
    + Small bias against too similar averages.
    + Should be ready for moving the detection to pip.
    - Skipping apt-get phase.
    
    Change-Id: I7ab1a92b79eeeed53ba65a071b1305e927816a89
    Signed-off-by: Vratko Polak <vrpolak@cisco.com>

--- resources/tools/presentation/run_cpta.sh
+++ resources/tools/presentation/run_cpta.sh
@@ -7,10 +7,10 @@ typeset -A DIR
 
 DIR[WORKING]=_tmp
 
-# Install system dependencies
-sudo apt-get -y update
-sudo apt-get -y install libxml2 libxml2-dev libxslt-dev build-essential \
-    zlib1g-dev unzip
+## Install system dependencies
+#sudo apt-get -y update
+#sudo apt-get -y install libxml2 libxml2-dev libxslt-dev build-essential \
+#    zlib1g-dev unzip
 
 # Create working directories
 mkdir ${DIR[WORKING]}
@@ -22,7 +22,7 @@ virtualenv ${DIR[WORKING]}/env
 # Install python dependencies:
 pip install -r requirements.txt
 
-export PYTHONPATH=`pwd`
+export PYTHONPATH=`pwd`:`pwd`/jumpavg
 
 STATUS=$(python pal.py \
     --specification specification_CPTA.yaml \
