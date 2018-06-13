2baca0b26c830abe61e6862697212e3ad3483f93
Peter Mikus <pmikus@cisco.com>
2018-06-13 12:09:47 +0000

    CSIT-PERF: New trigger design
    - CSIT-1128
    - This patch is implementing processing of TAGS specified
    as part of commit message
    
    Change-Id: Iad0923d22fae369dfe923a917e1c0d47d5cbf656
    Signed-off-by: Peter Mikus <pmikus@cisco.com>
    (cherry picked from commit 5120b1082aa70f6e75511e9d95a2a7c303e25f9a)

--- bootstrap-verify-perf.sh
+++ bootstrap-verify-perf.sh
@@ -276,6 +276,38 @@ case "$TEST_TAG" in
         TAGS=('mrrANDsrv6AND1t1c'
               'mrrANDsrv6AND2t2c')
         ;;
+    VERIFY-PERF-PATCH )
+        if [[ -z "$TEST_TAG_STRING" ]]; then
+            # If nothing is specified, we will run pre-selected tests by
+            # following tags. Items of array will be concatenated by OR in Robot
+            # Framework.
+            TEST_TAG_ARRAY=('mrrANDnic_intel-x710AND1t1cAND64bANDip4base'
+                            'mrrANDnic_intel-x710AND1t1cAND78bANDip6base'
+                            'mrrANDnic_intel-x710AND1t1cAND64bANDl2bdbase')
+        else
+            # If trigger contains tags, split them into array.
+            TEST_TAG_ARRAY=(${TEST_TAG_STRING//:/ })
+        fi
+
+        TAGS=()
+
+        for TAG in "${TEST_TAG_ARRAY[@]}"; do
+            if [[ ${TAG} == "!"* ]]; then
+                # Exclude tags are not prefixed.
+                TAGS+=("${TAG}")
+            else
+                # We will prefix with perftest to prevent running other tests
+                # (e.g. Functional).
+                prefix="perftestAND"
+                if [[ ${JOB_NAME} == vpp-* ]] ; then
+                    # Automatic prefixing for VPP jobs to limit the NIC used and
+                    # traffic evaluation to MRR.
+                    prefix="${prefix}mrrANDnic_intel-x710AND"
+                fi
+                TAGS+=("$prefix${TAG}")
+            fi
+        done
+        ;;
     * )
         TAGS=('perftest')
 esac
