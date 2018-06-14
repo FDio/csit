6149ec451efff00068f38e3343e66cdec7b943f4
Vratko Polak <vrpolak@cisco.com>
2018-06-14 14:04:03 +0200

    CSIT-1110: Use jumpavg library from pip
    + Move the jumpavg library code to separate directory.
    - Bump to 0.1.2 has to be done later.
    
    Change-Id: I9722ede48f00e99eeb68ca3f91e0bdeee2937973
    Signed-off-by: Vratko Polak <vrpolak@cisco.com>

--- PyPI/jumpavg/setup.py
+++ PyPI/jumpavg/setup.py
@@ -14,11 +14,12 @@ with open(path.join(here, "README.rst"), encoding="utf-8") as f:
 
 setup(
     name="jumpavg",
-    version="0.1.0",  # This is currently the only place listing the version.
-    description="Library for finding changes in time series by grouping results",
+    version="0.1.2",  # This is currently the only place listing the version.
+    description="Library for finding changes in time series by grouping results.",
     long_description=long_description,
     long_description_content_type="text/x-rst",
-    url="https://github.com/vrpolakatcisco/jumpavg",
+    # TODO: Create a separate webpage for jumpavg library.
+    url="https://gerrit.fd.io/r/gitweb?p=csit.git;a=tree;f=PyPI/jumpavg;hb=refs/heads/master",
     author="Cisco Systems Inc. and/or its affiliates",
     author_email="csit-dev@lists.fd.io",
     classifiers=[
@@ -26,13 +27,14 @@ setup(
         "Intended Audience :: Science/Research",
         # Pick your license as you wish
         "License :: OSI Approved :: Apache Software License",
-        "Natural Language :: English"
+        "Natural Language :: English",
         # TODO: Test which Python versions is the code compatible with.
         "Programming Language :: Python :: 2.7",
         "Topic :: Scientific/Engineering :: Information Analysis"
     ],
     keywords="progression regression anomaly detection",
     packages=find_packages(exclude=[]),
+    # TODO: python_requires="~=2.7"
     install_requires=[],
     # TODO: Include simulator and tests.
     extras_require={
@@ -44,7 +46,7 @@ setup(
         ],
     },
     project_urls={
-        "Bug Reports": "https://github.com/vrpolakatcisco/jumpavg/issues",
-        "Source": "https://github.com/vrpolakatcisco/jumpavg/",
+        "Bug Reports": "https://jira.fd.io/projects/CSIT/issues",
+        "Source": "https://gerrit.fd.io/r/gitweb?p=csit.git;a=tree;f=PyPI/jumpavg;hb=refs/heads/master",
     },
 )
