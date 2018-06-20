08280f4c53dced6b113f0489107933a57e68bee5
Vratko Polak <vrpolak@cisco.com>
2018-06-20 16:17:24 +0200

    CSIT-1134: Store comparison scipt in csit repo
    + Include requirements.txt for the comparison script.
    + Include parse.sh for the bash parsing part.
    
    Change-Id: I828478dad4d6103ca9c8572bac6b1ccd4c601ebb
    Signed-off-by: Vratko Polak <vrpolak@cisco.com>

---
+++ resources/tools/jenkins/patch_vote/compare.py
@@ -0,0 +1,56 @@
+# Copyright (c) 2018 Cisco and/or its affiliates.
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at:
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Script for determining whether per-patch perf test votes -1.
+
+FIXME: Docstrings, line lengths, and so on.
+"""
+
+import json
+import sys
+
+import jumpavg
+
+parent_lines = list()
+new_lines = list()
+with open("csit/results.txt") as parent_file:
+    parent_lines = parent_file.readlines()
+with open("csit_/results.txt") as new_file:
+    new_lines = new_file.readlines()
+# TODO: Figure out what to do if only parent fails.
+if len(parent_lines) != len(new_lines):
+    print "Number of passed tests does not match!"
+    sys.exit(1)
+classifier = jumpavg.BitCountingClassifier()
+num_tests = len(parent_lines)
+exit_code = 0
+for index in range(num_tests):
+    parent_values = json.loads(parent_lines[index])
+    new_values = json.loads(new_lines[index])
+    parent_stats = jumpavg.AvgStdevMetadataFactory(parent_values)
+    new_stats = jumpavg.AvgStdevMetadataFactory(new_values)
+    classified_list = classifier.classify([parent_stats, new_stats])
+    if len(classified_list) < 2:
+        print "Test index {index} no group boundary detected".format(index=index)
+        continue
+    anomaly = classified_list[1].metadata.classification
+    if anomaly == "regression":
+        print "Regression in test index {index}".format(index=index)
+        print "Parent stats {stats}".format(stats=parent_stats)
+        print "New stats {stats}".format(stats=new_stats)
+        print "Parent values: {values}".format(values=parent_values)
+        print "New values: {values}".format(values=new_values)
+        exit_code = 1
+        continue
+    print "Test index {index} anomaly {anomaly}".format(index=index, anomaly=anomaly
+sys.exit(exit_code)
