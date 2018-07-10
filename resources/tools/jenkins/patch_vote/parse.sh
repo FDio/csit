957b57b812416059fde4f360389cecda38877e89
Vratko Polak <vrpolak@cisco.com>
2018-07-10 13:34:24 +0200

    CSIT-1134: Store comparison scipt in csit repo
    + Include requirements.txt for the comparison script.
    + Include parse.sh for the bash parsing part.
    
    Change-Id: I828478dad4d6103ca9c8572bac6b1ccd4c601ebb
    Signed-off-by: Vratko Polak <vrpolak@cisco.com>

---
+++ resources/tools/jenkins/patch_vote/parse.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -exu
+
+# TODO: Do we need to make filepaths configurable?
+# TODO: Absolute paths via "readlink -e"?
+IN_FILE="csit/output.xml"
+OUT_FILE="csit/results.txt"
+
+echo "Parsing $IN_FILE putting results into $OUT_FILE"
+echo "TODO: Re-use parts of PAL when they support subsample test parsing."
+
+grep -o "'\\\\nMaximum Receive Rate trial results in packets per second: .*\]'" "$IN_FILE" | grep -o '\[.*\]' > "$OUT_FILE"
