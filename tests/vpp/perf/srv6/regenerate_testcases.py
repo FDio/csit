6e3597df23aa4e425d80c338bc5fa9a1fb31560b
Vratko Polak <vrpolak@cisco.com>
2018-07-10 17:37:28 +0200

    CSIT-1186: Add autogeneration and few suites
    WIP
    
    Change-Id: I394ad61f262b3078e5ca719034b15ada48987ee0
    Signed-off-by: Vratko Polak <vrpolak@cisco.com>

---
+++ tests/vpp/perf/srv6/regenerate_testcases.py
@@ -0,0 +1,35 @@
+# Copyright (c) 2018 Cisco and/or its affiliates.
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at:
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import glob
+
+from resources.libraries.python.autogen.DefaultTestcase import DefaultTestcase
+
+for filename_prolog in glob.glob("*-ndrpdr.robot"):
+    with open(filename_prolog, "r") as file_in:
+        text = file_in.read()
+        text_prolog = "".join(text.partition("*** Test Cases ***")[:-1])
+    filename_no_ext = filename_prolog.replace(".robot", "")
+    suite_id = filename_no_ext.split("-", 1)[1]
+    print "Processing suite_id", suite_id
+    testcase = DefaultTestcase(suite_id)
+    filename_robot = filename_no_ext + ".robot"
+    with open(filename_robot, "w") as file_out:
+        file_out.write(text_prolog)
+        # TODO: testcase.write()?
+        file_out.write(testcase.generate(num=1, framesize=78, cores=1))
+        file_out.write(testcase.generate(num=2, framesize=78, cores=2))
+        file_out.write(testcase.generate(num=3, framesize=78, cores=4))
+        file_out.write(testcase.generate(num=4, framesize=1518, cores=1))
+        file_out.write(testcase.generate(num=5, framesize=1518, cores=2))
+        file_out.write(testcase.generate(num=6, framesize=1518, cores=4))
