52a28d7b83605ca42b64ab1c9465098ebda32e65
Peter Mikus <pmikus@cisco.com>
2018-07-13 11:44:29 +0000

    Refactor DPDK testcases to new structure
    + RXQ refactor
    
    Change-Id: Ic03a2e208b9fe5d324a5ed75a603af4cff1854a9
    Signed-off-by: Peter Mikus <pmikus@cisco.com>

--- bootstrap-verify-perf-DPDK.sh
+++ bootstrap-verify-perf-DPDK.sh
@@ -151,9 +151,7 @@ case "$TEST_TAG" in
             # If nothing is specified, we will run pre-selected tests by
             # following tags. Items of array will be concatenated by OR in Robot
             # Framework.
-            TEST_TAG_ARRAY=('mrrANDnic_intel-x710AND1cAND64bANDip4base'
-                            'mrrANDnic_intel-x710AND1cAND78bANDip6base'
-                            'mrrANDnic_intel-x710AND1cAND64bANDl2bdbase')
+            TEST_TAG_ARRAY=('mrrANDnic_intel-x710AND1cAND64b')
         else
             # If trigger contains tags, split them into array.
             TEST_TAG_ARRAY=(${TEST_TAG_STRING//:/ })
