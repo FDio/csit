a6fbe9a807c84caef47a0689ae1b48737a85e329
Peter Mikus <pmikus@cisco.com>
2018-07-13 11:44:29 +0000

    Refactor DPDK testcases to new structure
    + RXQ refactor
    
    Change-Id: Ic03a2e208b9fe5d324a5ed75a603af4cff1854a9
    Signed-off-by: Peter Mikus <pmikus@cisco.com>

--- resources/libraries/python/DPDK/L2fwdTest.py
+++ resources/libraries/python/DPDK/L2fwdTest.py
@@ -33,12 +33,13 @@ class L2fwdTest(object):
         :param cpu_cores: The DPDK run cores.
         :param nb_cores: The cores number for the forwarding.
         :param queue_nums: The queues number for the NIC.
-        :param jumbo_frames: Are jumbo frames used or not.
+        :param jumbo_frames: Indication if the jumbo frames are used (True) or
+                             not (False).
         :type dut_node: dict
         :type cpu_cores: str
         :type nb_cores: str
         :type queue_nums: str
-        :type jumbo_frames: str
+        :type jumbo_frames: bool
         :raises RuntimeError: If the script "run_l2fwd.sh" fails.
         """
         if dut_node['type'] == NodeType.DUT:
@@ -46,11 +47,12 @@ class L2fwdTest(object):
             ssh.connect(dut_node)
 
             arch = Topology.get_node_arch(dut_node)
+            jumbo = 'yes' if jumbo_frames else 'no'
             cmd = '{fwdir}/tests/dpdk/dpdk_scripts/run_l2fwd.sh {cpu_cores} ' \
                   '{nb_cores} {queues} {jumbo} {arch}'.\
                   format(fwdir=Constants.REMOTE_FW_DIR, cpu_cores=cpu_cores,
                          nb_cores=nb_cores, queues=queue_nums,
-                         jumbo=jumbo_frames, arch=arch)
+                         jumbo=jumbo, arch=arch)
 
             ret_code, _, _ = ssh.exec_command_sudo(cmd, timeout=600)
             if ret_code != 0:
