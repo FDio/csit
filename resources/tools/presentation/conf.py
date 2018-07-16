d11b76ac1b77e3eba89d2d2643b6a995f5c64db9
Tibor Frank <tifrank@cisco.com>
2018-07-16 15:32:57 +0200

    CSIT - 1196: Add hsw/skx and 2n/3n to the report structure
    Change-Id: I3a0cc2792b931798e466250e30bc30ad8a32b84e
    Signed-off-by: Tibor Frank <tifrank@cisco.com>

--- resources/tools/presentation/conf.py
+++ resources/tools/presentation/conf.py
@@ -133,7 +133,20 @@ html_theme = 'sphinx_rtd_theme'
 # further.  For a list of options available for each theme, see the
 # documentation.
 #
-# html_theme_options = {}
+html_theme_options = {
+    'canonical_url': '',
+    'analytics_id': '',
+    'logo_only': False,
+    'display_version': True,
+    'prev_next_buttons_location': 'bottom',
+    'style_external_links': False,
+    # Toc options
+    'collapse_navigation': True,
+    'sticky_navigation': True,
+    'navigation_depth': 3,
+    'includehidden': True,
+    'titles_only': False
+}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
@@ -148,6 +161,15 @@ html_context = {
         '_static/theme_overrides.css',  # overrides for wide tables in RTD theme
         ],
     }
+
+# If false, no module index is generated.
+html_domain_indices = True
+
+# If false, no index is generated.
+html_use_index = True
+
+# If true, the index is split into individual pages for each letter.
+html_split_index = False
 
 # -- Options for LaTeX output ---------------------------------------------
 
