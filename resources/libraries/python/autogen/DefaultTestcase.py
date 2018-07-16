bb64b2ed3a2163e5f470301f2064fcaca8c9d059
Vratko Polak <vrpolak@cisco.com>
2018-07-16 12:29:44 +0200

    CSIT-1186: Add srv6 NDRPDR suites
    Delete the corresponding ndrpdrdisc suites.
    
    Also, there is a script to re-generate testcases of selected suites.
    For that, resources/libraries/python/autogen is created,
    with Regenerator holdin most of script logic.
    
    The script looks at files matching a glob expression,
    reuses a "prolog", but overwites test cases with the default template
    and hardcoded set of testcase arguments.
    
    Also, handling of overhead size has been simplified,
    using new keyword Get Max Rate And Jumbo And Handle Multi Seg.
    
    Change-Id: I394ad61f262b3078e5ca719034b15ada48987ee0
    Signed-off-by: Vratko Polak <vrpolak@cisco.com>

---
+++ resources/libraries/python/autogen/DefaultTestcase.py
@@ -0,0 +1,34 @@
+# Copyright (c) 2018 Cisco and/or its affiliates.
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at:
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Module with utilities for autogeneration of non-customizable testcases."""
+
+from Testcase import Testcase
+
+
+class DefaultTestcase(Testcase):
+    """Testcase subclass with a rigid template string."""
+
+    def __init__(self, suite_id):
+        """Construct instance for identified suite.
+
+        :param suite_id: Suite identifier, without NIC prefix and .robot suffix.
+            Example: ethip6srhip6-ip6base-srv6enc2sids-nodecaps-ndrpdr
+        :type suite_id: str
+        """
+        template_string = r'''
+| ${tc_num}-${frame_str}-${cores_str}c-''' + suite_id + r'''
+| | [Tags] | ${frame_str} | ${cores_str}C
+| | framesize=${frame_num} | phy_cores=${cores_num}
+'''
+        super(DefaultTestcase, self).__init__(template_string)
