949b744140667655f462409bb3ff19a65388a04d
Vratko Polak <vrpolak@cisco.com>
2018-07-25 11:51:28 +0200

    DO_NOT_MERGE: Attempt to use TB22 only
    Rebase on this if you wish
    to not interfere with release activities.
    
    Change-Id: I6dc3d919acc9ba2afc3a5ffedf362ceb06c61ea1
    Signed-off-by: Vratko Polak <vrpolak@cisco.com>

--- bootstrap-verify-perf.sh
+++ bootstrap-verify-perf.sh
@@ -16,13 +16,7 @@ set -xo pipefail
 
 # TOPOLOGY
 # Space separated list of available testbeds, described by topology files
-TOPOLOGIES_3N_HSW="topologies/available/lf_3n_hsw_testbed1.yaml \
-                   topologies/available/lf_3n_hsw_testbed2.yaml \
-                   topologies/available/lf_3n_hsw_testbed3.yaml"
-TOPOLOGIES_2N_SKX="topologies/available/lf_2n_skx_testbed21.yaml \
-                   topologies/available/lf_2n_skx_testbed24.yaml"
-TOPOLOGIES_3N_SKX="topologies/available/lf_3n_skx_testbed31.yaml \
-                   topologies/available/lf_3n_skx_testbed32.yaml"
+TOPOLOGIES_2N_SKX="topologies/available/lf_2n_skx_testbed22.yaml"
 
 # SYSTEM
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
