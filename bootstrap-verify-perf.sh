cec57b730431e770eeace89d5b063389c07800ec
Jan Gelety <jgelety@cisco.com>
2018-07-26 09:02:17 +0200

    TRIGGER PERF MRR 1807 - 3n-hsw
    TEST_TAG_ARRAY=('mrr'
                    '!nic_intel-xxv710')
    
    Change-Id: I2ff1c124f65b0937752c26c6ad596aed3cefc119
    Signed-off-by: Jan Gelety <jgelety@cisco.com>

--- bootstrap-verify-perf.sh
+++ bootstrap-verify-perf.sh
@@ -196,9 +196,8 @@ case "$TEST_TAG" in
             # If nothing is specified, we will run pre-selected tests by
             # following tags. Items of array will be concatenated by OR in Robot
             # Framework.
-            TEST_TAG_ARRAY=('mrrANDnic_intel-x710AND1cAND64bANDip4base'
-                            'mrrANDnic_intel-x710AND1cAND78bANDip6base'
-                            'mrrANDnic_intel-x710AND1cAND64bANDl2bdbase')
+            TEST_TAG_ARRAY=('mrr'
+                            '!nic_intel-xxv710')
         else
             # If trigger contains tags, split them into array.
             TEST_TAG_ARRAY=(${TEST_TAG_STRING//:/ })
