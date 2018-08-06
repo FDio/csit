5e1382bf4511e82e9f3618839ebf2663b430d4be
Tibor Frank <tifrank@cisco.com>
2018-08-06 10:51:57 +0200

    CSIT-1208: Add new data to 1807 report
    Change-Id: I15b8ad91c9f64c8fe5170905a8c7566f55dfe5d9
    Signed-off-by: Tibor Frank <tifrank@cisco.com>
    (cherry picked from commit dadb06ada83c7f25f9cf884d54d3a30fad979b29)

--- resources/tools/presentation/generator_tables.py
+++ resources/tools/presentation/generator_tables.py
@@ -233,7 +233,10 @@ def table_performance_comparison(table, input_data):
                 tst_name_mod = tst_name.replace("-ndrpdrdisc", "").\
                     replace("-ndrpdr", "").replace("-pdrdisc", "").\
                     replace("-ndrdisc", "").replace("-pdr", "").\
-                    replace("-ndr", "")
+                    replace("-ndr", "").\
+                    replace("1t1c", "1c").replace("2t1c", "1c").\
+                    replace("2t2c", "2c").replace("4t2c", "2c").\
+                    replace("4t4c", "4c").replace("8t4c", "4c")
                 if tbl_dict.get(tst_name_mod, None) is None:
                     name = "{0}-{1}".format(tst_data["parent"].split("-")[0],
                                             "-".join(tst_data["name"].
@@ -271,7 +274,10 @@ def table_performance_comparison(table, input_data):
                 tst_name_mod = tst_name.replace("-ndrpdrdisc", ""). \
                     replace("-ndrpdr", "").replace("-pdrdisc", ""). \
                     replace("-ndrdisc", "").replace("-pdr", ""). \
-                    replace("-ndr", "")
+                    replace("-ndr", "").\
+                    replace("1t1c", "1c").replace("2t1c", "1c").\
+                    replace("2t2c", "2c").replace("4t2c", "2c").\
+                    replace("4t4c", "4c").replace("8t4c", "4c")
                 try:
                     # TODO: Re-work when NDRPDRDISC tests are not used
                     if table["include-tests"] == "MRR":
@@ -305,7 +311,10 @@ def table_performance_comparison(table, input_data):
                         tst_name_mod = tst_name.replace("-ndrpdrdisc", ""). \
                             replace("-ndrpdr", "").replace("-pdrdisc", ""). \
                             replace("-ndrdisc", "").replace("-pdr", ""). \
-                            replace("-ndr", "")
+                            replace("-ndr", "").\
+                            replace("1t1c", "1c").replace("2t1c", "1c").\
+                            replace("2t2c", "2c").replace("4t2c", "2c").\
+                            replace("4t4c", "4c").replace("8t4c", "4c")
                         if tbl_dict.get(tst_name_mod, None) is None:
                             continue
                         if tbl_dict[tst_name_mod].get("history", None) is None:
