8a1ceff7c9e2bc4fc60308d98df8f84627139e28
Vratko Polak <vrpolak@cisco.com>
2018-08-07 09:52:53 +0200

    DO_NOT_MERGE: CSIT-1135: Use scripts from CSIT
    WIP
    
    Change-Id: If88fa528ce155ea86b614e3d77c0550b91bbdf11
    Signed-off-by: Vratko Polak <vrpolak@cisco.com>

--- resources/tools/jenkins/patch_vote/prepare_test_parent.sh
+++ resources/tools/jenkins/patch_vote/prepare_test_parent.sh
@@ -11,5 +11,19 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# TODO: Convert to use the code from cloned CSIT git, not from pip.
-jumpavg==0.1.3
+# Bash script fragment, to be sourced from main.sh
+
+set -exu -o pipefail
+
+cd "$vpp_dir"
+rm -rf csit_new
+mkdir -p csit_new
+for filename in output.xml log.html report.html; do
+    mv csit/$filename csit_new/$filename
+done
+bash "$script_dir/parse.sh" csit_new
+# TODO: Also handle archive/ and make job archive everything useful.
+( cd "$csit_dir" && git reset --hard HEAD && git clean -dffx )
+cp build_parent/*.deb csit/
+# Create symlinks so that if job fails on robot test, results can be archived.
+ln -s csit csit_parent
