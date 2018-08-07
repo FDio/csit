ce9f0b31d84ff689d737f9b13a2e724934ac8f78
Vratko Polak <vrpolak@cisco.com>
2018-08-07 14:31:17 +0200

    DO_NOT_MERGE: CSIT-1135: Use scripts from CSIT
    WIP
    
    Change-Id: If88fa528ce155ea86b614e3d77c0550b91bbdf11
    Signed-off-by: Vratko Polak <vrpolak@cisco.com>

---
+++ resources/libraries/bash/jenkins/activate_csit_virtualenv.sh
@@ -0,0 +1,32 @@
+# Copyright (c) 2018 Cisco and/or its affiliates.
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at:
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+set -exuo pipefail
+
+ENV_DIR="${CSIT_DIR}/env"
+rm -rf "${ENV_DIR}"
+
+pip install --upgrade virtualenv || {
+    die 1 "Failed to install virtual env!"
+}
+virtualenv --system-site-packages "${ENV_DIR}" || {
+    die 1 "Failed to create virtual env!"
+}
+set +u
+source "${ENV_DIR}/bin/activate" || {
+    die 1 "Failed to activate virtual env!"
+}
+set -u
+pip install -r "${CSIT_DIR}/requirements.txt" || {
+    die 1 "Failed to install requirements to virtual env!"
+}
---
+++ resources/libraries/bash/jenkins/common_dirs.sh
@@ -0,0 +1,26 @@
+# Copyright (c) 2018 Cisco and/or its affiliates.
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at:
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+set -exuo pipefail
+
+# Typically this cripts is sourced from another script:
+# source "${BASH_LIBRARY_DIR}/common_dirs.sh"
+
+BASH_LIBRARY_DIR=$(dirname $(readlink -e "${BASH_SOURCE[0]}"))
+CSIT_DIR=$(readlink -e "${BASH_LIBRARY_DIR}/../../../..")
+RESOURCES_DIR=$(readlink -e "${CSIT_DIR}/resources")
+TOOLS_DIR=$(readlink -e "${RESOURCES_DIR}/tools")
+PYTHON_SCRIPTS_DIR=$(readlink -e "${TOOLS_DIR}/scripts")
+
+ARCHIVE_DIR=$(readlink -f "${CSIT_DIR}/archive")
+mkdir -p "${ARCHIVE_DIR}"
