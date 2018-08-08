5be5f08fddeb16dc74772fdbadfec6658b5ecf1f
Vratko Polak <vrpolak@cisco.com>
2018-08-08 18:53:24 +0200

    DO_NOT_MERGE: CSIT-1135: Use scripts from CSIT
    WIP
    
    Change-Id: If88fa528ce155ea86b614e3d77c0550b91bbdf11
    Signed-off-by: Vratko Polak <vrpolak@cisco.com>

---
+++ resources/libraries/bash/jenkins/compare_test_results.sh
@@ -0,0 +1,34 @@
+# Copyright (c) 2018 Cisco and/or its affiliates.
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at:
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+set -exuo pipefail
+
+# Variables read:
+# - VPP_DIR - Path to directory with VPP git repo (or at least built parts).
+# - BASH_LIBRARY_DIR - Path to directory holding parser script.
+# - PYTHON_SCRIPTS_DIR - Path to directory holding comparison utility.
+# Directories recreated:
+# - csit_parent - Sibling to csit directory, for holding results of parent build.
+
+cd "${VPP_DIR}"
+rm -rf csit_parent
+mkdir -p csit_parent
+for filename in "output.xml" "log.html" "report.html"; do
+    mv "csit/${filename}" "csit_parent/${filename}"
+done
+source "${BASH_LIBRARY_DIR}/parse_bmrr_results.sh" csit_parent
+
+# Reusing CSIT main virtualenv.
+pip install -r "${PYTHON_SCRIPTS_DIR}/perpatch_requirements.txt"
+python "${PYTHON_SCRIPTS_DIR}/compare_perpatch.py"
+# The exit code determines the vote result.
