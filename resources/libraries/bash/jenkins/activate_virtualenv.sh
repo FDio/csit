d3aa7db198ba944f55cb335a31d633fffbb494d7
Vratko Polak <vrpolak@cisco.com>
2018-08-09 13:57:52 +0200

    DO_NOT_MERGE: CSIT-1135: Use scripts from CSIT
    WIP
    
    Change-Id: If88fa528ce155ea86b614e3d77c0550b91bbdf11
    Signed-off-by: Vratko Polak <vrpolak@cisco.com>

---
+++ resources/libraries/bash/jenkins/activate_virtualenv.sh
@@ -0,0 +1,44 @@
+# Copyright (c) 2018 Cisco and/or its affiliates.
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at:
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+set -exuo pipefail
+
+# Variables read:
+# - WORKSPACE - Path to directory for creating virtualenv subdirectory in.
+# - CSIT_DIR - Path to directory with root of local CSIT git repository.
+# Variables set:
+# - ENV_DIR - Path to the created virtualenv subdirectory.
+# Variables exported:
+# - PYTHONPATH - CSIT_DIR, as CSIT python scripts need this.
+
+# Using WORKSPACE, as per-patch resets contents of csit/.
+ENV_DIR="${WORKSPACE}/env"
+rm -rf "${ENV_DIR}"
+
+pip install --upgrade virtualenv || {
+    die 1 "Failed to install virtual env!"
+}
+virtualenv --system-site-packages "${ENV_DIR}" || {
+    die 1 "Failed to create virtual env!"
+}
+set +u
+source "${ENV_DIR}/bin/activate" || {
+    die 1 "Failed to activate virtual env!"
+}
+set -u
+pip install -r "${CSIT_DIR}/requirements.txt" || {
+    die 1 "Failed to install requirements to virtual env!"
+}
+
+# Most CSIT Python scripts assume PYTONPATH is set and exported.
+export PYTHONPATH="${CSIT_DIR}"
