f72c44308fc63f11f87ef63c222c4f925c7c9186
Vratko Polak <vrpolak@cisco.com>
2018-08-10 10:20:30 +0200

    CSIT-1135: Scripts for VPP per-patch testing
    + Scripts do not rely on bootstraps.
    + Scripts are divided to fragments for better re-use.
    - Still using jumpavg from pipy.
    - Specific simplified parsing (instead of the PAL one).
    - Missing bash style document.
    
    Change-Id: If88fa528ce155ea86b614e3d77c0550b91bbdf11
    Signed-off-by: Vratko Polak <vrpolak@cisco.com>

---
+++ resources/libraries/bash/fragment/perpatch/parse_bmrr_results.sh
@@ -0,0 +1,39 @@
+# Copyright (c) 2018 Cisco and/or its affiliates.
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at:
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+set -exuo pipefail
+
+# Currently "parsing" is just two greps.
+# TODO: Re-use PAL parsing code to make output more general and deduplicated.
+#
+# Arguments:
+# - $1 - Path to (existing) directory holding robot output.xml result.
+# Variables (re)set:
+# - REL_DIR - Path normalized from argument value.
+# - IN_FILE - Normalized path to existing output.xml file.
+# - OUT_FILE - Normalized path to created results.txt file.
+# Functions called:
+# - die - Print to stderr and exit, defined in common_functions.sh
+
+# TODO: Do we want to explicitly report readlink failure?
+REL_DIR=$(readlink -e "$1")
+IN_FILE="$REL_DIR/output.xml"
+OUT_FILE="$REL_DIR/results.txt"
+
+echo "Parsing $IN_FILE putting results into $OUT_FILE"
+echo "TODO: Re-use parts of PAL when they support subsample test parsing."
+
+# TODO: How should we wrap this line?
+grep -o "Maximum Receive Rate trial results in packets per second: .*\]</status>" "$IN_FILE" | grep -o '\[.*\]' > "$OUT_FILE" || {
+    die 1 "Some grep for parsing has failed."
+}
