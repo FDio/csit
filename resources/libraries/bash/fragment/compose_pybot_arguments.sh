797de74ebb72326908891988bc53d5434fa99cf6
Vratko Polak <vrpolak@cisco.com>
2018-08-10 10:20:30 +0200

    CSIT-1135: Scripts for VPP per-patch testing
    + Scripts do not rely on bootstraps.
    + Scripts are divided to fragments for better re-use.
    + Bootstrap also divided to fragments.
    - Still using jumpavg from pipy.
    - Specific simplified parsing (instead of the PAL one).
    - Bash style document in separate Change.
    
    Change-Id: If88fa528ce155ea86b614e3d77c0550b91bbdf11
    Signed-off-by: Vratko Polak <vrpolak@cisco.com>

---
+++ resources/libraries/bash/fragment/compose_pybot_arguments.sh
@@ -0,0 +1,34 @@
+# Copyright (c) 2018 Cisco and/or its affiliates.
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at:
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+set -exuo pipefail
+
+# Variables read:
+# - WORKING_TOPOLOGY - Path to topology yaml file of the reserved testbed.
+# - DUT - CSIT test/ subdirectory, set while processing tags.
+# - TAGS - Array variable holding selected tag boolean expressions.
+# - TOPOLOGIES_TAGS - Tag boolean expression filtering tests for topology.
+# Variables set:
+# - PYBOT_ARGS - String holding part of all arguments for pybot.
+# - EXPANDED_TAGS - Array of strings pybot arguments compiled from tags.
+
+PYBOT_ARGS="--loglevel TRACE --variable TOPOLOGY_PATH:${WORKING_TOPOLOGY} --suite tests.${DUT}.perf"
+
+EXPANDED_TAGS=()
+for TAG in "${TAGS[@]}"; do
+    if [[ ${TAG} == "!"* ]]; then
+        EXPANDED_TAGS+=(" --exclude ${TAG#$"!"} ")
+    else
+        EXPANDED_TAGS+=(" --include ${TOPOLOGIES_TAGS}AND${TAG} ")
+    fi
+done
