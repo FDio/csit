165a50ce608d7dd2d34476dafeb853a074d923d3
Vratko Polak <vrpolak@cisco.com>
2018-08-10 10:20:30 +0200

    CSIT-1135: Scripts for VPP per-patch testing
    + Scripts do not rely on bootstraps.
    + Scripts are divided to fragments for better re-use.
    + Bootstrap also divided to fragments.
    - Still using jumpavg from pipy.
    - Specific simplified parsing (instead of the PAL one).
    - Bash style document in separate Change.
    
    Change-Id: If88fa528ce155ea86b614e3d77c0550b91bbdf11
    Signed-off-by: Vratko Polak <vrpolak@cisco.com>

---
+++ resources/libraries/bash/entry/bootstrap.sh
@@ -0,0 +1,68 @@
+#!/usr/bin/env bash
+
+# Copyright (c) 2018 Cisco and/or its affiliates.
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at:
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+set -exuo pipefail
+
+# Assumptions:
+# + There is a directory holding CSIT code to use (this script is there).
+# + At least one of the following is true:
+# ++ JOB_NAME environment variable is set,
+# ++ or this entry script has access to arguments.
+# Consequences (and specific assumptions) are multiple,
+# examine tree of fragments for current description.
+
+# FIXME: Define API contract (as opposed to just help) for bootstrap.
+
+# "set -eu" handles failures from the following five lines.
+BASH_ENTRY_DIR=$(dirname $(readlink -e "${BASH_SOURCE[0]}"))
+BASH_FRAGMENT_DIR=$(readlink -e "${BASH_ENTRY_DIR}/../fragment")
+source "${BASH_FRAGMENT_DIR}/common_dirs.sh"
+common_dirs
+source "${BASH_FRAGMENT_DIR}/common_functions.sh"
+# Function "die" is now available.
+source "${BASH_FRAGMENT_DIR}/activate_virtualenv.sh" || die 1 "Source failed."
+source "${BASH_FRAGMENT_DIR}/check_download_dir.sh" || die 1 "Source failed."
+source "${BASH_FRAGMENT_DIR}/cleanup_testbed.sh" || die 1 "Source failed."
+source "${BASH_FRAGMENT_DIR}/compose_pybot_arguments.sh" || {
+    die 1 "Source failed."
+}
+source "${BASH_FRAGMENT_DIR}/copy_archives.sh" || die 1 "Source failed."
+source "${BASH_FRAGMENT_DIR}/fail_fast.sh" || die 1 "Source failed."
+source "${BASH_FRAGMENT_DIR}/get_test_tag_string.sh" || die 1 "Source failed."
+source "${BASH_FRAGMENT_DIR}/reserve_testbed.sh" || die 1 "Source failed."
+source "${BASH_FRAGMENT_DIR}/run_pybot.sh" || die 1 "Source failed."
+source "${BASH_FRAGMENT_DIR}/select_topology.sh" || die 1 "Source failed."
+source "${BASH_FRAGMENT_DIR}/unreserve_testbed.sh" || die 1 "Source failed."
+source "${BASH_FRAGMENT_DIR}/verify/bootstrap_help.sh" || die 1 "Source failed."
+source "${BASH_FRAGMENT_DIR}/verify/gather_build.sh" || die 1 "Source failed."
+source "${BASH_FRAGMENT_DIR}/verify/get_test_code.sh" || die 1 "Source failed."
+source "${BASH_FRAGMENT_DIR}/verify/select_verify_tags.sh" || {
+    die 1 "Source failed."
+}
+
+get_test_tag_string || die 1 "Function call failed."
+get_test_code "${1-}" || die 1 "Function call failed."
+select_topology "${NODENESS}" "${FLAVOR}" || die 1 "Function call failed."
+gather_build || die 1 "Function call failed."
+check_download_dir || die 1 "Function call failed."
+activate_virtualenv "${CSIT_DIR}" || die 1 "Function call failed."
+reserve_testbed || die 1 "Function call failed."
+cleanup_testbed || die 1 "Function call failed."
+select_verify_tags || die 1 "Function call failed."
+compose_pybot_arguments || die 1 "Function call failed."
+run_pybot || die 1 "Function call failed."
+unreserve_testbed || die 1 "Function call failed."
+copy_archives || die 1 "Function call failed."
+fail_fast || die 1 "Function call failed."
---
+++ resources/libraries/bash/fragment/perpatch/prepare_build_parent.sh
@@ -0,0 +1,43 @@
+# Copyright (c) 2018 Cisco and/or its affiliates.
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at:
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+set -exuo pipefail
+
+function prepare_build_parent () {
+
+    set -exuo pipefail
+
+    # Variables read:
+    # - VPP_DIR - Path to existing directory, parent to accessed directories.
+    # Directories read:
+    # - build-root - Existing directory with built VPP artifacts (also DPDK).
+    # Directories updated:
+    # - ${VPP_DIR} - A local git repository, parent commit gets checked out.
+    # - build_new - Old contents removed, content of build-root copied here.
+    # Functions called:
+    # - die - Print to stderr and exit, defined in common_functions.sh
+
+    cd "${VPP_DIR}" || die 1 "Change directory operation failed."
+    rm -rf "build_new" || die 1 "Remove operation failed."
+    mkdir -p "build_new" || die 1 "Directory creation failed."
+    mv "build-root"/*".deb" "build_new"/ || die 1 "Move operation failed."
+    # The previous build could have left some incompatible leftovers,
+    # e.g. DPDK artifacts of different version.
+    # "make -C dpdk clean" does not actually remove such .deb file.
+    # Also, there usually is a copy of dpdk artifact in build-root.
+    git clean -dffx "dpdk"/ "build-root"/ || die 1 "Git clean operation failed."
+    # Finally, check out the parent commit.
+    git checkout HEAD~ || die 1 "Git checkout operation failed."
+    # Display any other leftovers.
+    git status || die 1 "Git status operation failed."
+}
