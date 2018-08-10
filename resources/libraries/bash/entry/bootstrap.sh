a836d1d84a4c513f523be82fc73c9ac20e149893
Vratko Polak <vrpolak@cisco.com>
2018-08-10 10:20:30 +0200

    CSIT-1135: Scripts for VPP per-patch testing
    + Scripts do not rely on bootstraps.
    + Scripts are divided to fragments for better re-use.
    + Bootstrap also divided to fragments.
    - Still using jumpavg from pipy.
    - Specific simplified parsing (instead of the PAL one).
    - Bash style document in separate Change.
    
    Change-Id: If88fa528ce155ea86b614e3d77c0550b91bbdf11
    Signed-off-by: Vratko Polak <vrpolak@cisco.com>

---
+++ resources/libraries/bash/entry/bootstrap.sh
@@ -0,0 +1,48 @@
+#!/usr/bin/env bash
+
+# Copyright (c) 2018 Cisco and/or its affiliates.
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at:
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+set -exuo pipefail
+
+# Assumptions:
+# + There is a directory holding CSIT code to use (this script is there).
+# + At least one of the following is true:
+# ++ JOB_NAME environment variable is set,
+# ++ or this entry script has access to arguments.
+# Consequences (and specific assumptions) are multiple,
+# examine tree of fragments for current description.
+
+# FIXME: Define API contract (as opposed to just help) for bootstrap.
+
+# "set -eu" handles failures from the following two lines.
+BASH_ENTRY_DIR=$(dirname $(readlink -e "${BASH_SOURCE[0]}"))
+BASH_FRAGMENT_DIR=$(readlink -e "${BASH_ENTRY_DIR}/../fragment")
+# Each fragment should pass or abort, so no need to "|| die" when sourcing.
+source "${BASH_FRAGMENT_DIR}/common_dirs.sh"
+source "${BASH_FRAGMENT_DIR}/common_functions.sh"
+source "${BASH_FRAGMENT_DIR}/verify/bootstrap_help.sh"
+source "${BASH_FRAGMENT_DIR}/get_test_tag_string.sh"
+source "${BASH_FRAGMENT_DIR}/verify/get_test_code.sh"
+source "${BASH_FRAGMENT_DIR}/select_topology.sh" "${NODENESS}" "${FLAVOR}"
+source "${BASH_FRAGMENT_DIR}/verify/gather_build.sh"
+source "${BASH_FRAGMENT_DIR}/check_download_dir.sh"
+source "${BASH_FRAGMENT_DIR}/activate_virtualenv.sh" "${CSIT_DIR}"
+source "${BASH_FRAGMENT_DIR}/reserve_testbed.sh"
+source "${BASH_FRAGMENT_DIR}/cleanup_testbed.sh"
+source "${BASH_FRAGMENT_DIR}/verify/select_verify_tags.sh"
+source "${BASH_FRAGMENT_DIR}/compose_pybot_arguments.sh"
+source "${BASH_FRAGMENT_DIR}/run_pybot.sh" 1
+source "${BASH_FRAGMENT_DIR}/unreserve_testbed.sh"
+source "${BASH_FRAGMENT_DIR}/cp_archives.sh"
+source "${BASH_FRAGMENT_DIR}/fail_fast.sh"
---
+++ resources/libraries/bash/fragment/verify/gather_build.sh
@@ -0,0 +1,49 @@
+# Copyright (c) 2018 Cisco and/or its affiliates.
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at:
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+set -exuo pipefail
+
+# Variables read:
+# - TEST_CODE - String affecting test selection, usually jenkins job name.
+# - BASH_FRAGMENT_DIR - Path to directory holding specialized gather scripts.
+# Variables set:
+# - DUT - CSIT test/ subdirectory containing suites to execute.
+# Directories updated:
+# - ${DOWNLOAD_DIR} - Files needed by tests are gathered here.
+# Functions called:
+# - die - Print to stderr and exit, defined in common_functions.sh
+# Multiple other side effects are possible,
+# see fragments sourced from here for their current description.
+
+pushd "${DOWNLOAD_DIR}" || die 1 "Pushd failed."
+case "${TEST_CODE}" in
+    *hc2vpp*)
+        DUT="hc2vpp"
+        ;;
+    *vpp*)
+        DUT="vpp"
+        source "${BASH_FRAGMENT_DIR}/verify/gather_vpp.sh"
+        ;;
+    *ligato*)
+        DUT="kubernetes"
+        source "${BASH_FRAGMENT_DIR}/verify/gather_ligato.sh"
+        ;;
+    *dpdk*)
+        DUT="dpdk"
+        source "${BASH_FRAGMENT_DIR}/verify/gather_dpdk.sh"
+        ;;
+    *)
+        die 1 "Unable to identify DUT type from: ${TEST_CODE}!"
+        ;;
+esac
+popd || die 1 "Popd failed."
