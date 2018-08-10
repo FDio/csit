1f9a84f2e2b31d473c57f10ce86e87872a4949b5
Vratko Polak <vrpolak@cisco.com>
2018-08-10 10:20:30 +0200

    CSIT-1135: Scripts for VPP per-patch testing
    + Scripts do not rely on bootstraps.
    + Scripts are divided to fragments for better re-use.
    + Bootstrap also divided to fragments.
    - Still using jumpavg from pipy.
    - Specific simplified parsing (instead of the PAL one).
    - Bash style document in separate Change.
    
    Change-Id: If88fa528ce155ea86b614e3d77c0550b91bbdf11
    Signed-off-by: Vratko Polak <vrpolak@cisco.com>

---
+++ resources/libraries/bash/fragment/cp_archives.sh
@@ -0,0 +1,37 @@
+# Copyright (c) 2018 Cisco and/or its affiliates.
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at:
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+set -exuo pipefail
+
+function cp_archives () {
+
+    set -exuo pipefail
+
+    # Variables read:
+    # - WORKSPACE - Jenkins workspace, copy only if the value is not empty.
+    #   Can be unset, then it speeds up manual testing.
+    # - ARCHIVE_DIR - Path to directory with content to be copied.
+    # Directories updated:
+    # - ${WORKSPACE}/archives/ - Created if does not exist.
+    #   Content of ${ARCHIVE_DIR}/ is copied here.
+    # Functions called:
+    # - die - Print to stderr and exit, defined in common_functions.sh
+
+    # We will create additional archive if workspace variable is set.
+    # This way if script is running in jenkins all will be
+    # automatically archived to logs.fd.io.
+    if [[ -n "${WORKSPACE-}" ]]; then
+        mkdir -p "${WORKSPACE}/archives/" || die 1 "Archives dir create failed."
+        cp -r "${ARCHIVE_DIR}"/* "${WORKSPACE}/archives/" || die 1 "Cp failed."
+    fi
+}
