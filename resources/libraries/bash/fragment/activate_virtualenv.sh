66abe1a2813fd41a18cb421392a5ad1cd295c4ee
Vratko Polak <vrpolak@cisco.com>
2018-08-10 10:20:30 +0200

    CSIT-1135: Scripts for VPP per-patch testing
    + Scripts do not rely on bootstraps.
    + Scripts are divided to fragments for better re-use.
    - Still using jumpavg from pipy.
    - Specific simplified parsing (instead of the PAL one).
    - Missing bash style document.
    
    Change-Id: If88fa528ce155ea86b614e3d77c0550b91bbdf11
    Signed-off-by: Vratko Polak <vrpolak@cisco.com>

---
+++ resources/libraries/bash/fragment/activate_virtualenv.sh
@@ -0,0 +1,36 @@
+# Copyright (c) 2018 Cisco and/or its affiliates.
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at:
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+set -exuo pipefail
+
+# Variables read:
+# - WORKSPACE - Path to directory for creating virtualenv subdirectory in.
+# - CSIT_DIR - Path to directory with root of local CSIT git repository.
+# Variables set:
+# - ENV_DIR - Path to the created virtualenv subdirectory.
+# Variables exported:
+# - PYTHONPATH - CSIT_DIR, as CSIT Python scripts usually need this.
+
+# Using WORKSPACE, as per-patch resets contents of csit/.
+ENV_DIR="${WORKSPACE}/env"
+rm -rf "${ENV_DIR}"
+
+pip install --upgrade virtualenv
+virtualenv --system-site-packages "${ENV_DIR}"
+set +u
+source "${ENV_DIR}/bin/activate"
+set -u
+pip install -r "${CSIT_DIR}/requirements.txt"
+
+# Most CSIT Python scripts assume PYTHONPATH is set and exported.
+export PYTHONPATH="${CSIT_DIR}"
