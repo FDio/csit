231080f4296aa91eb539571c72f89156fd2d4a1c
sharath <sharathkumarboyanapally@gmail.com>
2018-08-13 20:16:56 +0530

    copying all .so[newly added] into app directory
    Change-Id: I5b89732c5dd1a4b74ffbafbd78803721dbf9c61c
    Signed-off-by: sharath <sharathkumarboyanapally@gmail.com>

--- tests/dmm/dmm_scripts/run_dmm.sh
+++ tests/dmm/dmm_scripts/run_dmm.sh
@@ -49,15 +49,17 @@ cd ${LIB_PATH}
 chmod 777 *
 ls -l
 
+export LD_LIBRARY_PATH=${LIB_PATH}
+
 cd ${APP_DIR}
-cp -r ${LIB_PATH}/libnStackAPI.so .
+cp -r ${LIB_PATH}/* .
 cp -r ../configure/* .
 chmod 777 *
 
 if [ "$OS_ID" == "ubuntu" ]; then
-	ifaddress1=$(ifconfig eth1 | grep 'inet addr' | cut -d: -f2 | awk '{print $1}')
+	ifaddress1=$(ifconfig enp0s8 | grep 'inet addr' | cut -d: -f2 | awk '{print $1}')
 	echo $ifaddress1
-	ifaddress2=$(ifconfig eth2 | grep 'inet addr' | cut -d: -f2 | awk '{print $1}')
+	ifaddress2=$(ifconfig enp0s9 | grep 'inet addr' | cut -d: -f2 | awk '{print $1}')
 	echo $ifaddress2
 elif [ "$OS_ID" == "centos" ]; then
 	ifaddress1=$(ifconfig enp0s8 | grep 'inet' | cut -d: -f2 | awk '{print $2}')
@@ -110,9 +112,9 @@ ls -l
 
 #only for kernal stack
 if [ ${proc_name} -eq 0 ]; then
-sudo LD_PRELOAD=${LIB_PATH}/libnStackAPI.so ./vs_epoll -p 20000 -d ${dut2_ip} -a 10000 -s ${dut1_ip} -l 200 -t 50000 -i 0 -f 1 -r 20000 -n 1 -w 10 -u 10000 -e 10 -x 1
+sudo LD_LIBRARY_PATH=${LIB_PATH} ./vs_epoll -p 20000 -d ${dut2_ip} -a 10000 -s ${dut1_ip} -l 200 -t 50000 -i 0 -f 1 -r 20000 -n 1 -w 10 -u 10000 -e 10 -x 1
 else
-sudo LD_PRELOAD=${LIB_PATH}/libnStackAPI.so ./vc_common -p 20000 -d ${dut1_ip} -a 10000 -s ${dut2_ip} -l 200 -t 50000 -i 0 -f 1 -r 20000 -n 1 -w 10 -u 10000 -e 10 -x 1
+sudo LD_LIBRARY_PATH=${LIB_PATH} ./vc_common -p 20000 -d ${dut1_ip} -a 10000 -s ${dut2_ip} -l 200 -t 50000 -i 0 -f 1 -r 20000 -n 1 -w 10 -u 10000 -e 10 -x 1
 fi
 
 cd ${PWDDIR}
