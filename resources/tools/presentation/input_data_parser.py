972212ffe801e015bfb498feaf1c4fc5a7c8284a
Vratko Polak <vrpolak@cisco.com>
2018-08-15 13:28:43 +0200

    WIP: List tags on err
    Change-Id: Iafccb05f51b84ac0c4bbea5f744ea457b1676967
    Signed-off-by: Vratko Polak <vrpolak@cisco.com>

--- resources/tools/presentation/input_data_parser.py
+++ resources/tools/presentation/input_data_parser.py
@@ -671,6 +671,7 @@ class ExecutionChecker(ResultVisitor):
                     self._data["tests"][self._test_ID] = test_result
                     logging.error("The test '{0}' has no or more than one "
                                   "multi-threading tags.".format(self._test_ID))
+                    logging.info("Tags present: {tags}".format(tags=tags))
                     return
 
         if test.status == "PASS" and ("NDRPDRDISC" in tags or
