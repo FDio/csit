56778a99d5b370f3f1d61ae595fe2f39c5c00957
Peter Mikus <pmikus@cisco.com>
2018-08-16 15:32:40 +0000

    Bump report version to 0.6
    Change-Id: I820288a7209b6c0017ee42f5087f0687679ab078
    Signed-off-by: Peter Mikus <pmikus@cisco.com>

--- resources/tools/presentation/run_report.sh
+++ resources/tools/presentation/run_report.sh
@@ -38,7 +38,7 @@ export PYTHONPATH=`pwd`
 python pal.py \
     --specification specification.yaml \
     --release ${RELEASE} \
-    --version "0.5" \
+    --version "0.6" \
     --logging INFO \
     --force
 
