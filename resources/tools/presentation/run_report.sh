8fdf612a77a9c6d6888364d41b238dcad34ff554
Peter Mikus <pmikus@cisco.com>
2018-08-17 11:27:37 +0000

    Remove DPDK MRR changes section from report Bump report version to 0.7
    Change-Id: I77478e3b58daab25bdece1e67795cc911f470136
    Signed-off-by: Peter Mikus <pmikus@cisco.com>

--- resources/tools/presentation/run_report.sh
+++ resources/tools/presentation/run_report.sh
@@ -38,7 +38,7 @@ export PYTHONPATH=`pwd`
 python pal.py \
     --specification specification.yaml \
     --release ${RELEASE} \
-    --version "0.6" \
+    --version "0.7" \
     --logging INFO \
     --force
 
