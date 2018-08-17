94240edbf9219aa61ce52cf34480ea7d0ef59d86
Maciek Konstantynowicz <mkonstan@cisco.com>
2018-08-17 23:04:51 +0100

    report 18.07: Bump report version to v0.9.
    Change-Id: Ic4f5520f69d0a97afe54aa8fed269812993d66f7
    Signed-off-by: Maciek Konstantynowicz <mkonstan@cisco.com>
    (cherry picked from commit ec497a5a8a47544147f130187f178de40b80e5c0)

--- resources/tools/presentation/run_report.sh
+++ resources/tools/presentation/run_report.sh
@@ -38,7 +38,7 @@ export PYTHONPATH=`pwd`
 python pal.py \
     --specification specification.yaml \
     --release ${RELEASE} \
-    --version "0.8" \
+    --version "0.9" \
     --logging INFO \
     --force
 
