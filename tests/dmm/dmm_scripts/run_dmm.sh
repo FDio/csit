91afa0e5f4d11d191df920b806611b51f3c7e69b
sharath <sharathkumarboyanapally@gmail.com>
2018-08-21 11:01:26 +0530

    integration of DMM+lwip testscripts
    Change-Id: Ifb1a0702353e71c61a694dea5164df41f4e23389
    Signed-off-by: sharath <sharathkumarboyanapally@gmail.com>

--- tests/dmm/dmm_scripts/run_dmm.sh
+++ tests/dmm/dmm_scripts/run_dmm.sh
@@ -11,6 +11,40 @@ dut1_ip=$1
 dut2_ip=$2
 proc_name=$3
 #proc_name => 0 = server, 1= client
+if [ $proc_name -eq "0"  ]; then
+  man_ip="172.28.128.3"
+else
+  man_ip="172.28.128.4"
+fi
+
+set ifname ""
+set ifaddress ""
+set ifmac ""
+
+for i in $(ls -1 /sys/class/net); do
+  ifaddress_i=$(ifconfig $i | grep 'inet' | head -n 1 | cut -d: -f2 | awk '{print $1}')
+  encap_i=$(ifconfig $i | grep 'encap' | head -n 1 | cut -d: -f2 | awk '{print $1}')
+  ifmac_i=$(ifconfig $i | grep 'HWaddr' | awk -F " " '{print $5}')
+  if [ -z $ifaddress_i ] && [ $encap_i == "Ethernet" ] ; then
+	ifname=$i
+	ifaddress=$man_ip
+	ret=$(ifconfig $i $ifaddress netmask 255.255.255.0 up) ## Manually assign ip address here
+	ifmac=$ifmac_i
+	break
+  elif [ ${ifaddress_i%%.*} != "10" ] && [ ${ifaddress_i%%.*} != "127" ] && [ $encap_i == "Ethernet" ] ; then
+	ifname=$i
+        ifaddress=$ifaddress_i
+	ifmac=$ifmac_i
+ 	break
+  fi
+done
+
+if [ -z $ifname ] || [ -z $ifaddress ] || [ -z $ifmac ]; then
+echo "No suitable interface found"
+fi
+
+## use $ifname $ifaddress $ifmac
+echo "ifname : " $ifname " address: "  $ifaddress " Mac :" $ifmac
 
 # Try to kill the vs_epoll
 sudo killall vs_epoll
@@ -54,15 +88,18 @@ cp -r ${LIB_PATH}/* .
 cp -r ../configure/* .
 chmod 777 *
 
+ifconfig
+sudo lshw -c network -businfo
+
 if [ "$OS_ID" == "ubuntu" ]; then
-	ifaddress1=$(ifconfig eth1 | grep 'inet addr' | cut -d: -f2 | awk '{print $1}')
+	ifaddress1=$(ifconfig $INTERFACE | grep 'inet addr' | cut -d: -f2 | awk '{print $1}')
 	echo $ifaddress1
-	ifaddress2=$(ifconfig eth2 | grep 'inet addr' | cut -d: -f2 | awk '{print $1}')
+	ifaddress2=$(ifconfig $INTERFACE | grep 'inet addr' | cut -d: -f2 | awk '{print $1}')
 	echo $ifaddress2
 elif [ "$OS_ID" == "centos" ]; then
-	ifaddress1=$(ifconfig enp0s8 | grep 'inet' | cut -d: -f2 | awk '{print $2}')
+	ifaddress1=$(ifconfig $INTERFACE | grep 'inet' | cut -d: -f2 | awk '{print $2}')
 	echo $ifaddress1
-	ifaddress2=$(ifconfig enp0s9 | grep 'inet' | cut -d: -f2 | awk '{print $2}')
+	ifaddress2=$(ifconfig $INTERFACE | grep 'inet' | cut -d: -f2 | awk '{print $2}')
 	echo $ifaddress2
 fi
 
