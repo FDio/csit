d0004f0be065608ad2a175a12f1ec16274d10ca3
sharath <sharathkumarboyanapally@gmail.com>
2018-08-21 11:01:26 +0530

    integration of DMM+lwip testscripts
    Change-Id: Ifb1a0702353e71c61a694dea5164df41f4e23389
    Signed-off-by: sharath <sharathkumarboyanapally@gmail.com>

--- tests/dmm/dmm_scripts/install_dmm.sh
+++ tests/dmm/dmm_scripts/install_dmm.sh
@@ -114,7 +114,7 @@ if [ ${SYS_HUGEPAGE} -lt 1024 ] || [ $hugepageFree -eq 0 ]; then
     while [ "${sock_count}" -ne 0 ]
     do
         sock_count=$[$sock_count - 1]
-        echo 1024 | sudo tee /sys/devices/system/node/node"$sock_count"/hugepages/hugepages-2048kB/nr_hugepages
+        echo 1536 | sudo tee /sys/devices/system/node/node"$sock_count"/hugepages/hugepages-2048kB/nr_hugepages
     done
 
     sudo mkdir -p /mnt/nstackhuge
