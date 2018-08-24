985e582fc7c7f9f15c03a77a0f94b8e7ad92d7f3
Vratko Polak <vrpolak@cisco.com>
2018-08-24 14:59:16 +0200

    DO_NOT_MERGE: Use TB23 only to not interfere with release
    For Vratko only, currently he wants to control TB idle time.
    
    Change-Id: I6dc3d919acc9ba2afc3a5ffedf362ceb06c61ea1
    Signed-off-by: Vratko Polak <vrpolak@cisco.com>

--- resources/libraries/bash/function/common.sh
+++ resources/libraries/bash/function/common.sh
@@ -479,25 +479,25 @@ function select_topology () {
     case "${NODENESS}_${FLAVOR}" in
         3n_hsw)
             TOPOLOGIES=(
-                        "${TOPOLOGIES_DIR}/lf_3n_hsw_testbed1.yaml"
-                        "${TOPOLOGIES_DIR}/lf_3n_hsw_testbed2.yaml"
-                        "${TOPOLOGIES_DIR}/lf_3n_hsw_testbed3.yaml"
+                        #"${TOPOLOGIES_DIR}/lf_3n_hsw_testbed1.yaml"
+                        #"${TOPOLOGIES_DIR}/lf_3n_hsw_testbed2.yaml"
+                        #"${TOPOLOGIES_DIR}/lf_3n_hsw_testbed3.yaml"
                        )
             TOPOLOGIES_TAGS="3_node_*_link_topo"
             ;;
         2n_skx)
             TOPOLOGIES=(
-                        "${TOPOLOGIES_DIR}/lf_2n_skx_testbed21.yaml"
-                        #"${TOPOLOGIES_DIR}/lf_2n_skx_testbed22.yaml"
-                        #"${TOPOLOGIES_DIR}/lf_2n_skx_testbed23.yaml"
-                        "${TOPOLOGIES_DIR}/lf_2n_skx_testbed24.yaml"
+                        #"${TOPOLOGIES_DIR}/lf_2n_skx_testbed21.yaml"
+                        ##"${TOPOLOGIES_DIR}/lf_2n_skx_testbed22.yaml"
+                        "${TOPOLOGIES_DIR}/lf_2n_skx_testbed23.yaml"
+                        #"${TOPOLOGIES_DIR}/lf_2n_skx_testbed24.yaml"
                        )
             TOPOLOGIES_TAGS="2_node_*_link_topo"
             ;;
         3n_skx)
             TOPOLOGIES=(
-                        "${TOPOLOGIES_DIR}/lf_3n_skx_testbed31.yaml"
-                        "${TOPOLOGIES_DIR}/lf_3n_skx_testbed32.yaml"
+                        #"${TOPOLOGIES_DIR}/lf_3n_skx_testbed31.yaml"
+                        #"${TOPOLOGIES_DIR}/lf_3n_skx_testbed32.yaml"
                        )
             TOPOLOGIES_TAGS="3_node_*_link_topo"
             ;;
