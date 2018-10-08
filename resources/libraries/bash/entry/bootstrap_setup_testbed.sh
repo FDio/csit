ac7bb33ae37729e63ed125c1eaae2f79e23c005e
Peter Mikus <pmikus@cisco.com>
2018-10-08 09:33:20 +0000

    Script for PXE host staging
    Introducing script for staging server via PXE and controlling
    CIMC/IPMI reboot.
    
    Change-Id: I1667e1906e7de73b7bca994d82de75e12c0d382a
    Signed-off-by: Peter Mikus <pmikus@cisco.com>
    (cherry picked from commit 2f5317d594411424be9bffeb4ccfe441e7b50530)

---
+++ resources/libraries/bash/entry/bootstrap_setup_testbed.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+
+# Copyright (c) 2018 Cisco and/or its affiliates.
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at:
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+set -exuo pipefail
+
+# Assumptions:
+# + There is a directory holding CSIT code to use (this script is there).
+# ++ This entry script has access to arguments:
+# ++ ${@} = <linux_ip> <mgmt_ip> <mgmt_username> <mgmt_pass>
+# Consequences (and specific assumptions) are multiple,
+# examine tree of functions for current description.
+
+# "set -eu" handles failures from the following two lines.
+BASH_ENTRY_DIR="$(dirname $(readlink -e "${BASH_SOURCE[0]}"))"
+BASH_FUNCTION_DIR="$(readlink -e "${BASH_ENTRY_DIR}/../function")"
+source "${BASH_FUNCTION_DIR}/common.sh" || {
+    echo "Source failed." >&2
+    exit 1
+}
+source "${BASH_FUNCTION_DIR}/setup.sh" || die "Source failed."
+common_dirs || die
+pxe_host "${@}" || die
+ansible_host || die
