de17a85409206211426d32554c268bcb6c0acc93
Jan Gelety <jgelety@cisco.com>
2018-10-29 14:35:39 +0100

    TRIGGER 1810 PERF NDRPDR FULL - 3n-hsw, part 4 (rest)
    Change-Id: Ifa4aaa865f844594024ee0237687996f387358e2
    Signed-off-by: Jan Gelety <jgelety@cisco.com>

--- resources/libraries/bash/function/common.sh
+++ resources/libraries/bash/function/common.sh
@@ -222,7 +222,14 @@ function compose_pybot_arguments () {
             PYBOT_ARGS+=("--suite" "tests.${DUT}.func")
             ;;
         *"perf"*)
-            PYBOT_ARGS+=("--suite" "tests.${DUT}.perf")
+            PYBOT_ARGS+=("--suite" "tests.${DUT}.perf.container_memif")
+            PYBOT_ARGS+=("--suite" "tests.${DUT}.perf.crypto")
+            PYBOT_ARGS+=("--suite" "tests.${DUT}.perf.ip4_tunnels")
+            PYBOT_ARGS+=("--suite" "tests.${DUT}.perf.ip6")
+            PYBOT_ARGS+=("--suite" "tests.${DUT}.perf.ip6_tunnels")
+            PYBOT_ARGS+=("--suite" "tests.${DUT}.perf.srv6")
+            PYBOT_ARGS+=("--suite" "tests.${DUT}.perf.tcp")
+            PYBOT_ARGS+=("--suite" "tests.${DUT}.perf.vts")
             ;;
         *)
             die "Unknown specification: ${TEST_CODE}"
@@ -634,11 +641,7 @@ function select_tags () {
             if [[ -z "${TEST_TAG_STRING-}" ]]; then
                 # If nothing is specified, we will run pre-selected tests by
                 # following tags.
-                test_tag_array=("mrrANDnic_intel-x710AND1cAND64bANDip4base"
-                                "mrrANDnic_intel-x710AND1cAND78bANDip6base"
-                                "mrrANDnic_intel-x710AND1cAND64bANDl2bdbase"
-                                "mrrANDnic_intel-x710AND1cAND64bANDl2xcbase"
-                                "!dot1q")
+                test_tag_array=("ndrpdr")
             else
                 # If trigger contains tags, split them into array.
                 test_tag_array=(${TEST_TAG_STRING//:/ })
