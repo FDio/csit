d6f546eeb3e7cfe546a1f14e8f878dea59b29f3b
Tibor Frank <tifrank@cisco.com>
2018-10-31 21:31:44 +0100

    Change the version
    Change-Id: I4fac272bf3f0bf9bdbf446ea3c2eff48966c600e
    Signed-off-by: Tibor Frank <tifrank@cisco.com>
    (cherry picked from commit e12b14a8a0713a08122ac0096bcd88002670bcba)
    (cherry picked from commit b7e8f88289895dfbd9458146a4bf9565ed9e89dc)
    (cherry picked from commit 9690198c59a4f6544337a7ac5cb86cf71cfaa957)

--- resources/tools/presentation/run_report.sh
+++ resources/tools/presentation/run_report.sh
@@ -38,7 +38,7 @@ export PYTHONPATH=`pwd`
 python pal.py \
     --specification specification.yaml \
     --release ${RELEASE} \
-    --version "1.1" \
+    --version "0.1" \
     --logging INFO \
     --force
 
