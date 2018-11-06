86096d284901aa7b8faaf1a51ffcc895076a4736
Tibor Frank <tifrank@cisco.com>
2018-11-06 14:40:11 +0100

    CSIT-1350: Add new data to 1810 report
    Change-Id: I1c82729c2abc7e5d3699f254629da6a9095d3464
    Signed-off-by: Tibor Frank <tifrank@cisco.com>

--- resources/tools/presentation/generator_plots.py
+++ resources/tools/presentation/generator_plots.py
@@ -221,6 +221,8 @@ def plot_latency_error_bars(plot, input_data):
                         elif "-ndr" in plot_title.lower():
                             ttype = "NDR"
                         else:
+                            logging.warning("Invalid test type: {0}".
+                                            format(test["type"]))
                             continue
                         y_tmp_vals[test["parent"]][0].append(
                             test["latency"][ttype]["direction1"]["min"])
@@ -235,10 +237,12 @@ def plot_latency_error_bars(plot, input_data):
                         y_tmp_vals[test["parent"]][5].append(
                             test["latency"][ttype]["direction2"]["max"])
                     else:
+                        logging.warning("Invalid test type: {0}".
+                                        format(test["type"]))
                         continue
-                except (KeyError, TypeError):
-                    pass
-
+                except (KeyError, TypeError) as err:
+                    logging.warning(err)
+    logging.info("y_tmp_vals: {0}\n".format(y_tmp_vals))
     # Sort the tests
     order = plot.get("sort", None)
     if order and y_tags:
@@ -265,6 +269,7 @@ def plot_latency_error_bars(plot, input_data):
     else:
         y_sorted = y_tmp_vals
 
+    logging.info("y_sorted: {0}\n".format(y_sorted))
     x_vals = list()
     y_vals = list()
     y_mins = list()
@@ -280,6 +285,10 @@ def plot_latency_error_bars(plot, input_data):
         y_mins.append(mean(val[3]) if val[3] else None)
         y_maxs.append(mean(val[5]) if val[5] else None)
 
+    logging.info("x_vals :{0}\n".format(x_vals))
+    logging.info("y_vals :{0}\n".format(y_vals))
+    logging.info("y_mins :{0}\n".format(y_mins))
+    logging.info("y_maxs :{0}\n".format(y_maxs))
     traces = list()
     annotations = list()
 
@@ -306,6 +315,9 @@ def plot_latency_error_bars(plot, input_data):
             arrayminus = [y_vals[idx] - y_mins[idx], ]
         else:
             arrayminus = [None, ]
+        logging.info("y_vals[{1}] :{0}\n".format(y_vals[idx], idx))
+        logging.info("array :{0}\n".format(array))
+        logging.info("arrayminus :{0}\n".format(arrayminus))
         traces.append(plgo.Scatter(
             x=[idx, ],
             y=[y_vals[idx], ],
