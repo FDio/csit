9ede0e8ddccfbf9a9797a17b2fa21c42b6a667ba
Vratko Polak <vrpolak@cisco.com>
2018-11-23 19:53:53 +0100

    Test if disconnection helps
    Change-Id: I1c3b1f799f1e940153238a09b64e3f4f615d3dbd
    Signed-off-by: Vratko Polak <vrpolak@cisco.com>

--- resources/libraries/python/TrafficGenerator.py
+++ resources/libraries/python/TrafficGenerator.py
@@ -671,8 +671,8 @@ class OptimizedSearch(object):
     @staticmethod
     def perform_soak_search(
             frame_size, traffic_type, minimum_transmit_rate,
-            maximum_transmit_rate, plr_target=1e-7, tdpt=0.05,
-            initial_count=50, timeout=36000.0):
+            maximum_transmit_rate, plr_target=1e-7, tdpt=0.1,
+            initial_count=50, timeout=1800.0):
         """FIXME."""
         # we need instance of TrafficGenerator instantiated by Robot Framework
         # to be able to use trex_stl-*()
