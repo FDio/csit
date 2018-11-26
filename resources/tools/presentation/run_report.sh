b85dd5fcbf78b514b50be51dbd688236f2536e6e
Tibor Frank <tifrank@cisco.com>
2018-11-26 16:01:13 +0100

    Report: versioning
    Change-Id: I890d52b4b5b87cd1b3952b3d4bb3cbbff069a9c7
    Signed-off-by: Tibor Frank <tifrank@cisco.com>

--- resources/tools/presentation/run_report.sh
+++ resources/tools/presentation/run_report.sh
@@ -38,7 +38,7 @@ export PYTHONPATH=`pwd`
 python pal.py \
     --specification specification.yaml \
     --release ${RELEASE} \
-    --version "w48" \
+    --version "48" \
     --logging INFO \
     --force
 
