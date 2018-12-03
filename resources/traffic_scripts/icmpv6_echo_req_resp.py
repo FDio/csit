f0353312447ed2d99f0d0f85962cf9e81e3c0436
Jan Gelety <jgelety@cisco.com>
2018-12-03 09:08:09 +0100

    VPP_Device - add baseline tests - part I
    CSIT-1370
    
    Add following baseline tests to VPP_Device:
    - ip4
      eth2p-ethip4-ip4base-dev
    - ip6
      eth2p-ethip6-ip6base-dev
    - l2bd
      eth2p-eth-l2bdbasemaclrn-dev
    
    Change-Id: Ic20000eb38c5511c27d020f3aa76b2620d7f7ef9
    Signed-off-by: Jan Gelety <jgelety@cisco.com>

--- resources/traffic_scripts/icmpv6_echo_req_resp.py
+++ resources/traffic_scripts/icmpv6_echo_req_resp.py
@@ -35,12 +35,10 @@ from resources.libraries.python.TrafficScriptArg import TrafficScriptArg
 
 def main():
     args = TrafficScriptArg(['src_mac', 'dst_mac', 'src_nh_mac', 'dst_nh_mac',
-                             'src_ip', 'dst_ip', 'h_num'])
+                             'src_ip', 'dst_ip', 'h_num'], ['is_dst_tg'])
 
-    src_rxq = RxQueue(args.get_arg('rx_if'))
-    src_txq = TxQueue(args.get_arg('rx_if'))
-    dst_rxq = RxQueue(args.get_arg('tx_if'))
-    dst_txq = TxQueue(args.get_arg('tx_if'))
+    src_rxq = RxQueue(args.get_arg('tx_if'))
+    src_txq = TxQueue(args.get_arg('tx_if'))
 
     src_mac = args.get_arg('src_mac')
     dst_mac = args.get_arg('dst_mac')
@@ -49,6 +47,11 @@ def main():
     src_ip = args.get_arg('src_ip')
     dst_ip = args.get_arg('dst_ip')
     hop_num = int(args.get_arg('h_num'))
+
+    is_dst_tg = True if args.get_arg('is_dst_tg') in ['True', ''] else False
+    dst_rxq = RxQueue(args.get_arg('rx_if')) if is_dst_tg else None
+    dst_txq = TxQueue(args.get_arg('rx_if')) if is_dst_tg else None
+
     hop_limit = 64
     echo_id = 0xa
     echo_seq = 0x1
@@ -63,12 +66,15 @@ def main():
                 ICMPv6NDOptDstLLAddr(lladdr=src_mac))
     src_sent_packets.append(pkt_send)
     src_txq.send(pkt_send)
-    pkt_send = (Ether(src=dst_mac, dst='ff:ff:ff:ff:ff:ff') /
-                IPv6(src=dst_ip, dst='ff02::1:ff00:2') /
-                ICMPv6ND_NA(tgt=dst_ip, R=0) /
-                ICMPv6NDOptDstLLAddr(lladdr=dst_mac))
-    dst_sent_packets.append(pkt_send)
-    dst_txq.send(pkt_send)
+
+    if is_dst_tg:
+        # send ICMPv6 neighbor advertisement message
+        pkt_send = (Ether(src=dst_mac, dst='ff:ff:ff:ff:ff:ff') /
+                    IPv6(src=dst_ip, dst='ff02::1:ff00:2') /
+                    ICMPv6ND_NA(tgt=dst_ip, R=0) /
+                    ICMPv6NDOptDstLLAddr(lladdr=dst_mac))
+        dst_sent_packets.append(pkt_send)
+        dst_txq.send(pkt_send)
 
     # send ICMPv6 echo request from first TG interface
     pkt_send = (Ether(src=src_mac, dst=src_nh_mac) /
@@ -77,56 +83,58 @@ def main():
     src_sent_packets.append(pkt_send)
     src_txq.send(pkt_send)
 
-    # receive ICMPv6 echo request on second TG interface
-    while True:
-        ether = dst_rxq.recv(2, dst_sent_packets)
-        if ether is None:
-            raise RuntimeError('ICMPv6 echo reply Rx timeout')
+    if is_dst_tg:
+        # receive ICMPv6 echo request on second TG interface
+        while True:
+            ether = dst_rxq.recv(2, dst_sent_packets)
+            if ether is None:
+                raise RuntimeError('ICMPv6 echo reply Rx timeout')
 
-        if ether.haslayer(ICMPv6ND_NS):
-            # read another packet in the queue if the current one is ICMPv6ND_NS
-            continue
-        else:
-            # otherwise process the current packet
-            break
+            if ether.haslayer(ICMPv6ND_NS):
+                # read another packet in the queue if the current one is
+                # ICMPv6ND_NS
+                continue
+            else:
+                # otherwise process the current packet
+                break
 
-    if not ether.haslayer(IPv6):
-        raise RuntimeError('Unexpected packet with no IPv6 received: {0}'.
-                           format(ether.__repr__()))
+        if not ether.haslayer(IPv6):
+            raise RuntimeError('Unexpected packet with no IPv6 received: {0}'.
+                               format(ether.__repr__()))
 
-    ipv6 = ether[IPv6]
+        ipv6 = ether[IPv6]
 
-    # verify hop limit processing
-    if ipv6.hlim != (hop_limit - hop_num):
-        raise RuntimeError('Invalid hop limit {0} should be {1}'.
-                           format(ipv6.hlim,hop_limit - hop_num))
+        # verify hop limit processing
+        if ipv6.hlim != (hop_limit - hop_num):
+            raise RuntimeError('Invalid hop limit {0} should be {1}'.
+                               format(ipv6.hlim,hop_limit - hop_num))
 
-    if not ipv6.haslayer(ICMPv6EchoRequest):
-        raise RuntimeError('Unexpected packet with no IPv6 ICMP received {0}'.
-                           format(ipv6.__repr__()))
+        if not ipv6.haslayer(ICMPv6EchoRequest):
+            raise RuntimeError('Unexpected packet with no IPv6 ICMP received '
+                               '{0}'.format(ipv6.__repr__()))
 
-    icmpv6 = ipv6[ICMPv6EchoRequest]
+        icmpv6 = ipv6[ICMPv6EchoRequest]
 
-    # check identifier and sequence number
-    if icmpv6.id != echo_id or icmpv6.seq != echo_seq:
-        raise RuntimeError('Invalid ICMPv6 echo reply received ID {0} '
-                           'seq {1} should be ID {2} seq {3}'.
-                           format(icmpv6.id, icmpv6.seq, echo_id, echo_seq))
+        # check identifier and sequence number
+        if icmpv6.id != echo_id or icmpv6.seq != echo_seq:
+            raise RuntimeError('Invalid ICMPv6 echo reply received ID {0} '
+                               'seq {1} should be ID {2} seq {3}'.
+                               format(icmpv6.id, icmpv6.seq, echo_id, echo_seq))
 
-    # verify checksum
-    cksum = icmpv6.cksum
-    del icmpv6.cksum
-    tmp = ICMPv6EchoRequest(str(icmpv6))
-    if not checksum_equal(tmp.cksum, cksum):
-        raise RuntimeError('Invalid checksum {0} should be {1}'.
-                           format(cksum, tmp.cksum))
+        # verify checksum
+        cksum = icmpv6.cksum
+        del icmpv6.cksum
+        tmp = ICMPv6EchoRequest(str(icmpv6))
+        if not checksum_equal(tmp.cksum, cksum):
+            raise RuntimeError('Invalid checksum {0} should be {1}'.
+                               format(cksum, tmp.cksum))
 
-    # send ICMPv6 echo reply from second TG interface
-    pkt_send = (Ether(src=dst_mac, dst=dst_nh_mac) /
-                IPv6(src=dst_ip, dst=src_ip) /
-                ICMPv6EchoReply(id=echo_id, seq=echo_seq))
-    dst_sent_packets.append(pkt_send)
-    dst_txq.send(pkt_send)
+        # send ICMPv6 echo reply from second TG interface
+        pkt_send = (Ether(src=dst_mac, dst=dst_nh_mac) /
+                    IPv6(src=dst_ip, dst=src_ip) /
+                    ICMPv6EchoReply(id=echo_id, seq=echo_seq))
+        dst_sent_packets.append(pkt_send)
+        dst_txq.send(pkt_send)
 
     # receive ICMPv6 echo reply on first TG interface
     while True:
