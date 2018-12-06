60df07cfbd93b37600d846a5ae697061344d8823
Jan Gelety <jgelety@cisco.com>
2018-12-06 13:23:05 +0100

    VPP_Device - add baseline tests - part II
    CSIT-1372
    
    Add following baseline tests to VPP_Device:
    - ip4
      eth2p-ethip4-ip4base-eth-2vhost-1vm-dev
      eth2p-ethip4-ip4base-eth-2memif-1dcr-dev
    
    - ip6
      eth2p-ethip6-ip6base-eth-2vhost-1vm-dev
      eth2p-ethip6-ip6base-eth-2memif-1dcr-dev
    
    - l2bd
      eth2p-eth-l2bdbasemaclrn-eth-2vhost-1vm-dev
      eth2p-eth-l2bdbasemaclrn-eth-2memif-1dcr-dev
    
    - l2xc
      eth2p-eth-l2xcbase-eth-2vhost-1vm-dev
      eth2p-eth-l2xcbase-eth-2memif-1dcr-dev
    
    Change-Id: Icc5ed801f3e8f6dedaa611779bd86fc20102fa74
    Signed-off-by: Jan Gelety <jgelety@cisco.com>

--- resources/libraries/python/ssh.py
+++ resources/libraries/python/ssh.py
@@ -373,8 +373,25 @@ def exec_cmd(node, cmd, timeout=600, sudo=False):
         raise ValueError('Empty command parameter')
 
     ssh = SSH()
+
+    if node.get('host_port') is not None:
+        ssh_node = dict()
+        ssh_node['host'] = '127.0.0.1'
+        ssh_node['port'] = node['port']
+        ssh_node['username'] = node['username']
+        ssh_node['password'] = node['password']
+        import pexpect
+        cmd = 'ssh -L {port}:127.0.0.1:{port} {user}@{host} -p {host_port}'.\
+            format(port=node['port'], user=node['host_username'],
+                   host=node['host'], host_port=node['host_port'])
+        child = pexpect.spawn(cmd)
+        child.expect('.* password: ')
+        child.sendline(node['host_password'])
+    else:
+        ssh_node = node
+
     try:
-        ssh.connect(node)
+        ssh.connect(ssh_node)
     except SSHException as err:
         logger.error("Failed to connect to node" + str(err))
         return None, None, None
