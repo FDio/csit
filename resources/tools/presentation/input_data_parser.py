6e2b5996dfaa65db6305a8d021fd2bc5f2560ee4
Tibor Frank <tifrank@cisco.com>
2018-12-12 12:55:43 +0100

    CSIT-1377: Implement systematic solution using defined time period
    Change-Id: I03d8a5fcf9e6392e238e04a06c0463c17b5a4a47
    Signed-off-by: Tibor Frank <tifrank@cisco.com>

--- resources/tools/presentation/input_data_parser.py
+++ resources/tools/presentation/input_data_parser.py
@@ -30,10 +30,17 @@ from robot import errors
 from collections import OrderedDict
 from string import replace
 from os import remove
+from os.path import join
+from datetime import datetime as dt
+from datetime import timedelta
 from jumpavg.AvgStdevMetadataFactory import AvgStdevMetadataFactory
 
 from input_data_files import download_and_unzip_data_file
 from utils import Worker
+
+
+# Separator used in file names
+SEPARATOR = "__"
 
 
 class ExecutionChecker(ResultVisitor):
@@ -1143,7 +1150,40 @@ class InputData(object):
                 remove(build["file-name"])
             except OSError as err:
                 logs.append(("ERROR", "Cannot remove the file '{0}': {1}".
-                             format(build["file-name"], err)))
+                             format(build["file-name"], repr(err))))
+
+        timeperiod = self._cfg.input.get("time-period", None)
+        if timeperiod:
+            now = dt.utcnow()
+            timeperiod = timedelta(int(timeperiod))
+            generated = data["metadata"].get("generated", None)
+            if generated:
+                generated = dt.strptime(generated, "%Y%m%d %H:%M")
+                if (now - generated) > timeperiod:
+                    # Remove the data and the file:
+                    state = "remove"
+                    data = None
+                    logs.append(
+                        ("INFO",
+                         "The build {job}/{build} is outdated, will be removed".
+                         format(job=job, build=build["build"])))
+                    file_name = self._cfg.input["file-name"]
+                    full_name = join(
+                        self._cfg.environment["paths"]["DIR[WORKING,DATA]"],
+                        "{job}{sep}{build}{sep}{name}".
+                            format(job=job,
+                                   sep=SEPARATOR,
+                                   build=build["build"],
+                                   name=file_name))
+                    try:
+                        remove(full_name)
+                        logs.append(("INFO", "The file {name} has been removed".
+                                     format(name=full_name)))
+                    except OSError as err:
+                        logs.append(("ERROR",
+                                    "Cannot remove the file '{0}': {1}".
+                                    format(full_name, repr(err))))
+
         logs.append(("INFO", "  Done."))
 
         result = {
