a27f8cab79ef6f600eb7e9f2d1416cb380035bc5
Vratko Polak <vrpolak@cisco.com>
2018-12-19 10:20:03 +0100

    DO_NOT_MERGE: Soak: Log also traces
    Output will be huge, only use for finding bugs
    
    Change-Id: I3c1157cd138035dde2d9fa60732065826600b467
    Signed-off-by: Vratko Polak <vrpolak@cisco.com>

--- resources/libraries/python/PLRsearch/PLRsearch.py
+++ resources/libraries/python/PLRsearch/PLRsearch.py
@@ -575,7 +575,7 @@ class PLRsearch(object):
                     "Worker {name} did not finish!".format(name=name))
             result_or_traceback = pipe.recv()
             try:
-                avg, stdev, bias_avg, bias_cov, debug_list, _ = (
+                avg, stdev, bias_avg, bias_cov, debug_list, trace_list = (
                     result_or_traceback)
             except ValueError:
                 raise RuntimeError(
@@ -584,6 +584,8 @@ class PLRsearch(object):
             logging.info("Logs from worker {name}:".format(name=name))
             for message in debug_list:
                 logging.debug(message)
+            for message in trace_list:
+                logging.trace(message)
             return avg, stdev, bias_avg, bias_cov
         stretch_avg, stretch_stdev, stretch_bias_avg, stretch_bias_cov = (
             1e6, 1e2, [0.0, 0.0], [[1.0, 0.0], [0.0, 1.0]])
