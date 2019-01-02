d5f69aab9da24f85d87c923c81f55e0a74ef2574
Vratko Polak <vrpolak@cisco.com>
2019-01-02 14:18:50 +0100

    DO_NOT_MERGE: PLRsearch: debugging
    + Add target duration to trex logging
    
    Change-Id: Icf56429b34ab6de2d18a12e6c676e2d242b3ba4c
    Signed-off-by: Vratko Polak <vrpolak@cisco.com>

--- resources/libraries/python/PLRsearch/Integrator.py
+++ resources/libraries/python/PLRsearch/Integrator.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2018 Cisco and/or its affiliates.
+# Copyright (c) 2019 Cisco and/or its affiliates.
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at:
@@ -20,7 +20,6 @@ TODO: Separate optimizations specific to PLRsearch
 """
 
 import math
-import random
 import time
 import traceback
 
@@ -194,6 +193,7 @@ def estimate_nd(communication_pipe, scale_coeff=10.0, trace_enabled=False):
         param_hint_cov = [
             [1.0 if first == second else 0.0 for first in range(dimension)]
             for second in range(dimension)]
+    numpy.random.seed(0)
     while not communication_pipe.poll():
         # Compute focus data.
         if len(top_weight_param) < len_top:
@@ -226,7 +226,7 @@ def estimate_nd(communication_pipe, scale_coeff=10.0, trace_enabled=False):
         while 1:
             # Multivariate Gauss can fall outside (-1, 1) interval
             sample_point = numpy.random.multivariate_normal(
-                param_focus_avg, param_focus_cov, 1)[0]
+                param_focus_avg, param_focus_cov, 1)[0].tolist()
             for first in range(dimension):
                 sample_coordinate = sample_point[first]
                 if sample_coordinate <= -1.0 or sample_coordinate >= 1.0:
@@ -235,7 +235,7 @@ def estimate_nd(communication_pipe, scale_coeff=10.0, trace_enabled=False):
                 break
         add_trace("sample_point", sample_point)
         samples += 1
-        value, log_weight = value_logweight_function(*sample_point)
+        value, log_weight = value_logweight_function(add_trace, *sample_point)
         add_trace("value", value)
         add_trace("log_weight", log_weight)
         # Update bias related statistics.
