a8430195e50183a269ee914dca4a4e1d39726886
Tibor Frank <tifrank@cisco.com>
2019-01-02 09:49:01 +0100

    Trending: Align the content of old and new dirs
    Change-Id: I39d1f71adac0bad3dbe18a987909904ea5c1e6cb
    Signed-off-by: Tibor Frank <tifrank@cisco.com>
    (cherry picked from commit 42fe666a56bb77efec0dd08268ee57e6482962d3)

--- resources/tools/presentation_new/generator_CPTA.py
+++ resources/tools/presentation_new/generator_CPTA.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2018 Cisco and/or its affiliates.
+# Copyright (c) 2019 Cisco and/or its affiliates.
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at:
@@ -427,7 +427,8 @@ def _generate_all_charts(spec, input_data):
             builds_dict[job] = list()
         for build in spec.input["builds"][job]:
             status = build["status"]
-            if status != "failed" and status != "not found":
+            if status != "failed" and status != "not found" and \
+                status != "removed":
                 builds_dict[job].append(str(build["build"]))
 
     # Create "build ID": "date" dict:
