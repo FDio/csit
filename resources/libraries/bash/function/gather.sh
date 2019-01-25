aea7bffadbeaf001b6dc3e1b900658229e7d325b
Jan Gelety <jgelety@cisco.com>
2019-01-25 10:23:14 +0100

    TRIGGER FUNC 1901
    Change-Id: I31bc3f19fc1b03bc3cff9f53855a616ae4efdb62
    Signed-off-by: Jan Gelety <jgelety@cisco.com>

--- resources/libraries/bash/function/gather.sh
+++ resources/libraries/bash/function/gather.sh
@@ -273,7 +273,8 @@ function gather_vpp () {
                 warn "Downloading latest VPP packages from Packagecloud."
             else
                 warn "Downloading stable VPP packages from Packagecloud."
-                if [[ "${TEST_CODE}" == *"device"* ]];
+                if [[ "${TEST_CODE}" == *"device"* ]] ||
+                   [[ "${TEST_CODE}" == *"ligato"* ]];
                 then
                     VPP_VERSION="$(<"${CSIT_DIR}/VPP_STABLE_VER_UBUNTU_BIONIC")" || {
                         die "Read VPP stable version failed."
