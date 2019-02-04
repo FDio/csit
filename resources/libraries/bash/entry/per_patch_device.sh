20fcdf068d80f34ef3edcd42211040215a1ef947
Peter Mikus <pmikus@cisco.com>
2019-02-04 10:16:48 +0000

    DO_NOT_MERGE: CSIT-1419 Convert vpp-virl job to vpp-device
    Change-Id: I5af6de4b96775a72855e1644ca4d8d9b4b2f9e25
    Signed-off-by: Peter Mikus <pmikus@cisco.com>

--- resources/libraries/bash/entry/per_patch_device.sh
+++ resources/libraries/bash/entry/per_patch_device.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-# Copyright (c) 2018 Cisco and/or its affiliates.
+# Copyright (c) 2019 Cisco and/or its affiliates.
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at:
@@ -23,11 +23,8 @@ set -exuo pipefail
 # + It contains csit subdirectory with CSIT code to use (this script is there).
 # + Everything needed to build VPP is already installed locally.
 # Consequences:
-# + At the end, VPP repo has parent commit checked out and built.
-# + Directories build_root, build and csit are reset during the run.
 # + The following directories (relative to VPP repo) are (re)created:
-# ++ csit_new, csit_parent, build_new, build_parent,
-# ++ archive, csit/archive, csit_download_dir.
+# ++ csit_new, build_new, archive, csit/archive, csit_download_dir.
 
 # TODO: Implement some kind of VPP build caching.
 
@@ -43,37 +40,18 @@ common_dirs || die
 set_perpatch_vpp_dir || die
 build_vpp_ubuntu_amd64 "CURRENT" || die
 set_aside_commit_build_artifacts || die
-build_vpp_ubuntu_amd64 "PARENT" || die
-set_aside_parent_build_artifacts || die
-## Replace previous 4 lines with this to speed up testing.
-#download_builds "REPLACE_WITH_URL" || die
 initialize_csit_dirs || die
 get_test_code "${1-}" || die
 get_test_tag_string || die
 set_perpatch_dut || die
 select_topology || die
+select_build "build_current" || die
+check_download_dir || die
 activate_virtualenv "${VPP_DIR}" || die
-reserve_testbed || die
-select_tags || die
+activate_docker_topology || die
+select_vpp_device_tags || die
 compose_pybot_arguments || die
-iterations=8
-for ((iter=0; iter<iterations; iter++)); do
-    # TODO: Use less heavy way to avoid apt remove falilures.
-    cleanup_topo
-    select_build "build_parent" || die
-    check_download_dir || die
-    run_pybot || die
-    copy_archives || die
-    archive_parse_test_results "csit_parent/${iter}" || die
-    die_on_pybot_error || die
-    cleanup_topo
-    select_build "build_current" || die
-    check_download_dir || die
-    run_pybot || die
-    copy_archives || die
-    archive_parse_test_results "csit_current/${iter}" || die
-    die_on_pybot_error || die
-done
-untrap_and_unreserve_testbed || die
-compare_test_results  # The error code becomes this script's error code.
-# TODO: After merging, make sure archiving works as expected.
+run_pybot || die
+copy_archives || die
+archive_parse_test_results "csit_current" || die
+die_on_pybot_error || die
