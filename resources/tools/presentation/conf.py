4343381c7e244d2e39067c3ece4c8d6929c20916
Tibor Frank <tifrank@cisco.com>
2019-02-11 11:11:02 +0100

    Report: Add NFV tests
    Change-Id: Ib996b4929fb4e31eceb475691dc7517a9144f0b3
    Signed-off-by: Tibor Frank <tifrank@cisco.com>
    (cherry picked from commit adab3cb4d5abc7f555f40b5f268fb611a77cace5)

--- resources/tools/presentation/conf.py
+++ resources/tools/presentation/conf.py
@@ -45,7 +45,7 @@ source_suffix = ['.rst', '.md']
 master_doc = 'index'
 
 # General information about the project.
-report_week = '06'
+report_week = '07'
 project = u'FD.io CSIT-1901.{week}'.format(week=report_week)
 copyright = u'2019, FD.io'
 author = u'FD.io CSIT'
