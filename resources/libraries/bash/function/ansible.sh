19b493697a0fc6d5d343a50c46344ba576c4616c
Peter Mikus <pmikus@cisco.com>
2019-02-26 10:24:41 +0000

    CSIT-1449 Ansible: Adding calibration support
    - Remove obsolete bootstrap_setup.sh.
    - Add calibration support into ansible.
    - Inject Ansible run with calibration to every job.
    
    Change-Id: Ib2d6cd0c04d3bce700b205bef34c3c88edb26e28
    Signed-off-by: Peter Mikus <pmikus@cisco.com>

---
+++ resources/libraries/bash/function/ansible.sh
@@ -0,0 +1,57 @@
+#!/usr/bin/env bash
+
+# Copyright (c) 2019 Cisco and/or its affiliates.
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at:
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+set -exuo pipefail
+
+
+function ansible_hosts () {
+    # Run ansible playbook on hosts in working topology file. Ansible scope is
+    # determined by tags passed as parameters to this function.
+    #
+    # Variable read:
+    # - ${WORKING_TOPOLOGY} - Reserved working topology.
+    # - ${TOOLS_DIR} - Server production IP address (Linux).
+
+    set -exuo pipefail
+
+    if ! installed ansible-playbook; then
+        sudo pip install ansible=2.5.1
+    fi
+
+    hosts=($(fgrep "host" "${WORKING_TOPOLOGY}" | cut -d ":" -f 2))
+    pushd "${TOOLS_DIR}"/testbed-setup/ansible || die "Pushd failed!"
+    ansible-playbook \
+        --vault-password-file=vault_pass \
+        --extra-vars '@vault.yml' \
+        --inventory inventories/lf_inventory/hosts site.yaml \
+        --limit "${hosts[@]//\"}" \
+        --tags "$@"|| die "Failed to run ansible on host!"
+    popd || die "Popd failed!"
+}
+
+function installed () {
+
+    set -exuo pipefail
+
+    # Check if the given utility is installed. Fail if not installed.
+    #
+    # Arguments:
+    # - ${1} - Utility to check.
+    # Returns:
+    # - 0 - If command is installed.
+    # - 1 - If command is not installed.
+
+    command -v "${1}"
+}
