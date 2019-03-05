71675350204b5e6a451537643f9e3e1d95cc3845
Tibor Frank <tifrank@cisco.com>
2019-03-05 15:05:58 +0100

    Report: Add LB Data
    - Change the version to 1901.10
    
    Change-Id: I4a976a55b375a00fe3498ef26e676ce9410ff7f2
    Signed-off-by: Tibor Frank <tifrank@cisco.com>

--- resources/tools/presentation/run_report.sh
+++ resources/tools/presentation/run_report.sh
@@ -38,7 +38,7 @@ export PYTHONPATH=`pwd`
 python pal.py \
     --specification specification.yaml \
     --release ${RELEASE} \
-    --week "09" \
+    --week "10" \
     --logging INFO \
     --force
 
