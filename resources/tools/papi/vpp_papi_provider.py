fc9366553315e2eadec5b8ec0cd89bb09671c961
Jan Gelety <jgelety@cisco.com>
2019-03-06 21:47:28 +0100

    CSIT-1337: Migrate L2Util library from VAT to PAPI
    Change-Id: I22879c7bdd100d00216b9528663bf17406169826
    Signed-off-by: Jan Gelety <jgelety@cisco.com>

--- resources/tools/papi/vpp_papi_provider.py
+++ resources/tools/papi/vpp_papi_provider.py
@@ -120,17 +120,23 @@ def convert_reply(api_r):
     :returns: Processed API reply / a part of API reply.
     :rtype: dict
     """
-    unwanted_fields = ['count', 'index']
+    unwanted_fields = ['count', 'context']
 
     reply_dict = dict()
     reply_key = repr(api_r).split('(')[0]
     reply_value = dict()
     for item in dir(api_r):
         if not item.startswith('_') and item not in unwanted_fields:
-            # attr_value = getattr(api_r, item)
+            attr_value = getattr(api_r, item)
+            if isinstance(attr_value, unicode):
+                value = binascii.hexlify(attr_value)
+            elif hasattr(attr_value, '__str__'):
+                value = str(attr_value)
+            else:
+                value = attr_value
             # value = binascii.hexlify(attr_value) \
             #     if isinstance(attr_value, str) else attr_value
-            value = getattr(api_r, item)
+            # value = getattr(api_r, item)
             reply_value[item] = value
     reply_dict[reply_key] = reply_value
     return reply_dict
