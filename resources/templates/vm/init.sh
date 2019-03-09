e0ff277c9d3e60c8c17e4f405d17e2c64ee6be1b
Peter Mikus <pmikus@cisco.com>
2019-03-09 08:00:54 +0000

    CSIT-1386 KernelVM - Part II
    - DRAFT
    
    Change-Id: Ife5c2e1ab419c55cbcd442792f940db3a41da471
    Signed-off-by: Peter Mikus <pmikus@cisco.com>

---
+++ resources/templates/vm/init.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+mount -t sysfs -o "nodev,noexec,nosuid" sysfs /sys
+mount -t proc -o "nodev,noexec,nosuid" proc /proc
+mkdir /dev/pts
+mount -t devpts -o "rw,noexec,nosuid,gid=5,mode=0620" devpts /dev/pts || true
+mount -t tmpfs -o "rw,noexec,nosuid,size=10%,mode=0755" tmpfs /run
+mount -t tmpfs -o "rw,noexec,nosuid,size=10%,mode=0755" tmpfs /tmp
+echo 0000:00:06.0 > /sys/bus/pci/devices/0000:00:06.0/driver/unbind
+echo 0000:00:07.0 > /sys/bus/pci/devices/0000:00:07.0/driver/unbind
+echo uio_pci_generic > /sys/bus/pci/devices/0000:00:06.0/driver_override
+echo uio_pci_generic > /sys/bus/pci/devices/0000:00:07.0/driver_override
+echo 0000:00:06.0 > /sys/bus/pci/drivers/uio_pci_generic/bind
+echo 0000:00:07.0 > /sys/bus/pci/drivers/uio_pci_generic/bind
+$vnf_bin
+poweroff -f
