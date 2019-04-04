41990d9606906644a4350064177104d553df5d63
Vratko Polak <vrpolak@cisco.com>
2019-04-04 13:40:42 +0200

    DO_NOT_MERGE: Add tag manipulation utility scripts
    Change-Id: I928beaa59feb1758b82b135e11e455c8d120ce7b
    Signed-off-by: Vratko Polak <vrpolak@cisco.com>

---
+++ resources/libraries/bash/function/o2st.py
@@ -0,0 +1,25 @@
+
+"""Process output.xml, output suites and tests executed."""
+
+with open("output.xml", "r") as file_in, open("st.txt", "w") as file_out:
+    while 1:
+        line = file_in.readline()
+        if not line:
+            break
+        if line[0] != '<':
+            continue
+        splitted = line[1:].split(" ", 1)
+        if len(splitted) < 2:
+            continue
+        out0, rest = splitted
+        if out0 not in ("suite", "test"):
+            continue
+        sep = 'name="' if out0 != "suite" else 'source="'
+        _, rest = rest.split(sep, 1)
+        out1, _ = rest.split('"', 1)
+        if out0 == "suite":
+            _, out1 = out1.split("/tests")
+        file_out.write(out0)
+        file_out.write(" ")
+        file_out.write(out1)
+        file_out.write("\n")
