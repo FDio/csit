47a84b0ee2c31e273e56f4683f9f66af10b1f131
Vratko Polak <vrpolak@cisco.com>
2019-04-04 13:45:11 +0200

    Upgrade autogen to NICs and search types
    + All perf suites updated, as autogen change is backward incompatible.
    + Only x710 -ndrpdr suites remain in git repository.
    + Removed retry feature of run_tests.
    + Renamed topology_type and traffic_type to traffic_profile.
    + Renamed framesize to frame_size; nic_model to nic_name.
    + Reordered Variables table to start with nic_name and overhead.
    + Fixed wrong overhead value in some cbc-sha1 suites.
    + Fixed some suite tags.
    ++ Introduced tags to distinguish suites, such as IPSECINT.
    + Bound crypto hardware to NIC name.
    + Implemented NIC limit hiding.
    + Also search and teardown related arguments are now hidden.
    + Main measurement keywords updated to new arg handling.
    + Max rate related keywords moved to performance_limits.robot
    + Teardowns unified.
    + Generated tests are archived.
    ++ Generated directory is .gitignore-d.
    + Regenerator raises an exception on seeing non-compatible suite.
    ++ Relatively helpful message should be seen in exception.
    + Suite and template Documentation has generated parts.
    +- With short NIC names only.
    + Autogen checker also upgraded.
    + Tag expressions replaced with long files to ensure analogous tests.
    
    Change-Id: I60e9a999187e7da1f60d0eb4fb02afa14682aa46
    Signed-off-by: Vratko Polak <vrpolak@cisco.com>

--- resources/libraries/bash/entry/per_patch_perf.sh
+++ resources/libraries/bash/entry/per_patch_perf.sh
@@ -56,9 +56,12 @@ activate_virtualenv "${VPP_DIR}" || die
 reserve_testbed || die
 select_tags || die
 compose_pybot_arguments || die
+generate_tests || die
+archive_tests || die
 iterations=8
 for ((iter=0; iter<iterations; iter++)); do
     # TODO: Use less heavy way to avoid apt remove falilures.
+    # Also, reserve_testbed has already called cleanup once.
     cleanup_topo
     select_build "build_parent" || die
     check_download_dir || die
