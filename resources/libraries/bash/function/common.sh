9fd22f689dc2549169b7f8d9f78cb8f92f8128d0
Jan Gelety <jgelety@cisco.com>
2019-04-13 10:35:38 +0200

    TRIGGER 1904 NDRPDR SEL1
    Change-Id: I3c2ef737b443f8c42e5f11e798af4069be169f96
    Signed-off-by: Jan Gelety <jgelety@cisco.com>

--- resources/libraries/bash/function/common.sh
+++ resources/libraries/bash/function/common.sh
@@ -644,11 +644,7 @@ function select_tags () {
             if [[ -z "${TEST_TAG_STRING-}" ]]; then
                 # If nothing is specified, we will run pre-selected tests by
                 # following tags.
-                test_tag_array=("mrrAND${DEFAULT_NIC}AND1cAND64bANDip4base"
-                                "mrrAND${DEFAULT_NIC}AND1cAND78bANDip6base"
-                                "mrrAND${DEFAULT_NIC}AND1cAND64bANDl2bdbase"
-                                "mrrAND${DEFAULT_NIC}AND1cAND64bANDl2xcbase"
-                                "!dot1q" "!drv_avf")
+                readarray -t test_tag_array < "${BASH_FUNCTION_DIR}/ndrpdr-daily.txt"
             else
                 # If trigger contains tags, split them into array.
                 test_tag_array=(${TEST_TAG_STRING//:/ })
