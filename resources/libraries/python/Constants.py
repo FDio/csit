d2c3a61c424faa44b1898b6130ef7851e5ae83fa
Jan Gelety <jgelety@cisco.com>
2019-05-11 06:52:56 +0200

    Update of VPP_STABLE_VER files + quick fix for gre create tunnel
    - use new vpp ref build - ubuntu 16.04:
      19.08-rc0~212-gf6c7aec~b7060
    
    - use new vpp ref build - ubuntu 18.04:
      19.08-rc0~212-gf6c7aec95
    
    - use new vpp ref build - centos7:
      19.08-rc0~212_gf6c7aec~b6916
    
    Change-Id: Ifd042fae1a46b07a5f463309f2b9cceb8054412f
    Signed-off-by: Jan Gelety <jgelety@cisco.com>

--- resources/libraries/python/Constants.py
+++ resources/libraries/python/Constants.py
@@ -91,6 +91,9 @@ class Constants(object):
     # Core dump directory
     CORE_DUMP_DIR = '/tmp'
 
+    # Equivalent to ~0 used in vpp code
+    BITWISE_NON_ZERO = 0xffffffff
+
     # Mapping from NIC name to its bps limit.
     # TODO: Implement logic to lower limits to TG NIC or software. Or PCI.
     NIC_NAME_TO_LIMIT = {
