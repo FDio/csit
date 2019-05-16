975ef99884990076c220b39d398c4915f8906060
Tibor Frank <tifrank@cisco.com>
2019-05-16 08:42:59 +0200

    Report: Configure 1904.21
    Change-Id: Ib65580067d9c41137293036bbd786e31c2bdf503
    Signed-off-by: Tibor Frank <tifrank@cisco.com>
    (cherry picked from commit c653499009183a8431350733df81c4162406f3ab)

--- resources/tools/presentation/conf.py
+++ resources/tools/presentation/conf.py
@@ -45,7 +45,7 @@ source_suffix = ['.rst', '.md']
 master_doc = 'index'
 
 # General information about the project.
-report_week = '20'
+report_week = '21'
 project = u'FD.io CSIT-1904.{week}'.format(week=report_week)
 copyright = u'2019, FD.io'
 author = u'FD.io CSIT'
