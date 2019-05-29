62730c5778bd56e98987f2ccf903af1593b1208d
Vratko Polak <vrpolak@cisco.com>
2019-05-29 12:36:39 +0200

    DO_NOT_MERGE: PoC of PapiSocketProvider
    - Still heavily WIP.
    + Support list command arguments in ssh.py
    + Prepare for using chained exceptions.
    + Bumped stable VPP versions, to get VPP side fixes.
    
    Change-Id: I05882cb6b620ad14638f7404b5ad38c7a5de9e6c
    Signed-off-by: Vratko Polak <vrpolak@cisco.com>

--- resources/libraries/python/IPsecUtil.py
+++ resources/libraries/python/IPsecUtil.py
@@ -18,7 +18,7 @@ from ipaddress import ip_network, ip_address
 
 from enum import Enum, IntEnum
 
-from resources.libraries.python.PapiExecutor import PapiExecutor
+from resources.libraries.python.PapiExecutor import PapiSocketExecutor
 from resources.libraries.python.topology import Topology
 from resources.libraries.python.VatExecutor import VatExecutor
 from resources.libraries.python.VatJsonUtil import VatJsonUtil
@@ -275,16 +275,12 @@ class IPsecUtil(object):
         """
 
         cmd = 'ipsec_select_backend'
-        cmd_reply = 'ipsec_select_backend_reply'
         err_msg = 'Failed to select IPsec backend on host {host}'.format(
             host=node['host'])
         args = dict(protocol=protocol, index=index)
-        with PapiExecutor(node) as papi_exec:
-            papi_resp = papi_exec.add(cmd, **args).execute_should_pass(err_msg)
-        data = papi_resp.reply[0]['api_reply'][cmd_reply]
-        if data['retval'] != 0:
-            raise RuntimeError('Failed to select IPsec backend on host {host}'.
-                               format(host=node['host']))
+        with PapiSocketExecutor(node) as papi_exec:
+            data = papi_exec.add(cmd, **args).get_replies().verify_reply(
+                err_msg)
 
     @staticmethod
     def vpp_ipsec_backend_dump(node):
@@ -296,9 +292,9 @@ class IPsecUtil(object):
 
         err_msg = 'Failed to dump IPsec backends on host {host}'.format(
             host=node['host'])
-        with PapiExecutor(node) as papi_exec:
-            papi_exec.add('ipsec_backend_dump').execute_should_pass(
-                err_msg, process_reply=False)
+        with PapiSocketExecutor(node) as papi_exec:
+            papi_exec.add('ipsec_backend_dump').get_replies().verify_reply(
+                err_msg)
 
     @staticmethod
     def vpp_ipsec_add_sad_entry(node, sad_id, spi, crypto_alg, crypto_key,
