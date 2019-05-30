b9e7ea89db47c0e5a636252b3ec7a2d91ebe733e
Tibor Frank <tifrank@cisco.com>
2019-05-30 15:18:31 +0200

    VAT-to-PAPI: VhostUser
    Change-Id: I235d4654b04275877b3c5f4cf49ef5755c3f4985
    Signed-off-by: Tibor Frank <tifrank@cisco.com>

--- resources/libraries/python/VhostUser.py
+++ resources/libraries/python/VhostUser.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2016 Cisco and/or its affiliates.
+# Copyright (c) 2019 Cisco and/or its affiliates.
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at:
@@ -13,13 +13,42 @@
 
 """Vhost-user interfaces library."""
 
-from resources.libraries.python.VatExecutor import VatExecutor, VatTerminal
+import binascii
+
+import logging
+
+from resources.libraries.python.PapiExecutor import PapiExecutor
 from resources.libraries.python.topology import NodeType, Topology
 from resources.libraries.python.InterfaceUtil import InterfaceUtil
 
 
 class VhostUser(object):
     """Vhost-user interfaces"""
+
+    @staticmethod
+    def _sw_interface_vhost_user_dump(node):
+        """Get the Vhost dump on the given node.
+
+        :param node: Given node to get Vhost dump from.
+        :type node: dict
+        :returns: List of vhosts extracted from Papi response.
+        :rtype: list
+        """
+        with PapiExecutor(node) as papi_exec:
+            dump = papi_exec.add("sw_interface_vhost_user_dump").get_dump()
+
+        key = "sw_interface_vhost_user_details"
+        data = list()
+        for item in dump.reply[0]["api_reply"]:
+            item[key]["interface_name"] = \
+                item[key]["interface_name"].rstrip('\x00')
+            item[key]["sock_filename"] = \
+                item[key]["sock_filename"].rstrip('\x00')
+            data.append(item)
+
+        logging.debug("VhostUser data:\n{data}".format(data))
+
+        return data
 
     @staticmethod
     def vpp_create_vhost_user_interface(node, socket):
@@ -31,23 +60,33 @@ class VhostUser(object):
         :type socket: str
         :returns: SW interface index.
         :rtype: int
-        :raises RuntimeError: If Vhost-user interface creation failed.
         """
-        out = VatExecutor.cmd_from_template(node, 'create_vhost_user_if.vat',
-                                            sock=socket)
-        if out[0].get('retval') == 0:
-            sw_if_idx = int(out[0].get('sw_if_index'))
-            if_key = Topology.add_new_port(node, 'vhost')
-            Topology.update_interface_sw_if_index(node, if_key, sw_if_idx)
-            ifc_name = InterfaceUtil.vpp_get_interface_name(node, sw_if_idx)
-            Topology.update_interface_name(node, if_key, ifc_name)
-            ifc_mac = InterfaceUtil.vpp_get_interface_mac(node, sw_if_idx)
-            Topology.update_interface_mac_address(node, if_key, ifc_mac)
-            Topology.update_interface_vhost_socket(node, if_key, socket)
-            return sw_if_idx
-        else:
-            raise RuntimeError('Create Vhost-user interface failed on node '
-                               '"{}"'.format(node['host']))
+        cmd = 'create_vhost_user_if'
+        err_msg = 'Failed to create Vhost-user interface on host {host}'.format(
+            host=node['host'])
+        args = dict(
+            sock_filename=str(socket)
+        )
+        with PapiExecutor(node) as papi_exec:
+            data = papi_exec.add(cmd, **args).get_replies(err_msg). \
+                verify_reply(err_msg=err_msg)
+
+        # Extract sw_if_idx:
+        sw_if_idx = data["sw_if_index"]
+
+        # Update the Topology:
+        if_key = Topology.add_new_port(node, 'vhost')
+        Topology.update_interface_sw_if_index(node, if_key, sw_if_idx)
+
+        ifc_name = InterfaceUtil.vpp_get_interface_name(node, sw_if_idx)
+        Topology.update_interface_name(node, if_key, ifc_name)
+
+        ifc_mac = InterfaceUtil.vpp_get_interface_mac(node, sw_if_idx)
+        Topology.update_interface_mac_address(node, if_key, ifc_mac)
+
+        Topology.update_interface_vhost_socket(node, if_key, socket)
+
+        return sw_if_idx
 
     @staticmethod
     def get_vhost_user_if_name_by_sock(node, socket):
@@ -78,14 +117,7 @@ class VhostUser(object):
         :rtype: str
         """
 
-        with VatTerminal(node) as vat:
-            if_data = vat.vat_terminal_exec_cmd_from_template(
-                "interface_dump.vat")
-        for iface in if_data[0]:
-            if iface["sw_if_index"] == sw_if_index:
-                return ':'.join("%02x" % (b) for b in iface["l2_address"][:6])
-
-        return None
+        return InterfaceUtil.vpp_get_interface_mac(node, sw_if_index)
 
     @staticmethod
     def vpp_show_vhost(node):
@@ -95,8 +127,7 @@ class VhostUser(object):
         :type node: dict
         :returns: nothing
         """
-        vat = VatExecutor()
-        vat.execute_script("show_vhost.vat", node, json_out=False)
+        VhostUser._sw_interface_vhost_user_dump(node)
 
     @staticmethod
     def show_vpp_vhost_on_all_duts(nodes):
