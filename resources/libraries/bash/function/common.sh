059cc8789676670030d250a8ea8b8dfd1caad86c
Peter Mikus <pmikus@cisco.com>
2019-06-09 05:59:57 +0000

    FIX: Disable AVF on 3n-hsw
    Change-Id: Id7c81a92dd50db365c219529a687fa37fd05ceb3
    Signed-off-by: Peter Mikus <pmikus@cisco.com>

--- resources/libraries/bash/function/common.sh
+++ resources/libraries/bash/function/common.sh
@@ -675,9 +675,7 @@ function select_tags () {
             test_tag_array+=("!vts")
             ;;
         *"3n-hsw"*)
-            # TODO: If AVF tests are expected to fail on haswell,
-            # add (before excluding) an explanation to:
-            # https://github.com/FDio/vpp/blob/master/src/plugins/avf/README.md
+            test_tag_array+=("!drv_avf")
             test_tag_array+=("!ipsechwNOTnic_intel-xl710")
             ;;
         *)
