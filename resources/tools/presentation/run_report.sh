40465e292dba276e9f3ede1907a40ec268fc9ea7
Tibor Frank <tifrank@cisco.com>
2019-06-10 12:08:21 +0200

    Report: Configure report 1904.24
    Change-Id: I4d6d913f0139fe0fab00668ec16081c04cc6bc0e
    Signed-off-by: Tibor Frank <tifrank@cisco.com>

--- resources/tools/presentation/run_report.sh
+++ resources/tools/presentation/run_report.sh
@@ -38,7 +38,7 @@ export PYTHONPATH=`pwd`
 python pal.py \
     --specification specification.yaml \
     --release ${RELEASE} \
-    --week "23" \
+    --week "24" \
     --logging INFO \
     --force
 
