11d778db0b49f48ff0296f693c45b00ad54ff3d6
Vratko Polak <vrpolak@cisco.com>
2019-06-11 18:32:32 +0200

    Add VPP API CRC checking to PapiSocketExecutor
    Separate for debugging, will probably get squashed
    into the main socket papi change later.
    
    Change-Id: I1dfc3846d1bcdad0b09017d4ce8edd5028e17e0c
    Signed-off-by: Vratko Polak <vrpolak@cisco.com>

---
+++ resources/libraries/python/VppApiCrc.py
@@ -0,0 +1,82 @@
+# Copyright (c) 2019 Cisco and/or its affiliates.
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at:
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Module for keeping track of VPP API CRCs relied on by CSIT."""
+
+import json
+import os
+
+
+class VppApiCrcChecker(object):
+    """Nothing dynamic here, just a wrapper to import."""
+
+    VPP_API_CRC_VERIFIED = {
+        "cli_inband": (),
+    }
+    """Dictionary from message name to tuple of supported CRC values."""
+
+    VPP_API_CRC_FOUND = dict()
+    """Dictionary from message to CRC, filled by what is found and not known."""
+
+    _get_name(msg_obj):
+        """FIXME"""
+        for item in msg_obj:
+            if isinstance(item, (dict, list)):
+                continue
+            return item
+
+    @classmethod
+    def check_dir(cls, directory):
+        """Parse every .api.json found under directory, fail on conflict.
+
+        :param directory: Root directory of the search for .api.json files.
+        :type directory: str
+        :raises RuntimeError: If CRC mismatch is detected.
+        """
+        for root, dirs, files in os.walk(directory):
+            for filename in files:
+                if not filename.endswith(".api.json"):
+                    continue
+                with open(filename, "r") as file_in:
+                    json_obj = json.read(file_in)
+                msgs = json_obj["messages"]
+                for msg_obj in msgs:
+                    msg_name = _get_name(msg_obj)
+                    msg_crc = msg_obj["crc"]
+                    if msg_name not in cls.VPP_API_CRC_VERIFIED:
+                        if msg_name not in cls.VPP_API_CRC_FOUND:
+                            cls.VPP_API_CRC_FOUND[msg_name] = [msg_crc]
+                        elif msg_crc not in cls.VPP_API_CRC_FOUND[msg_name]:
+                            cls.VPP_API_CRC_FOUND[msg_name].append(msg_crc)
+                        continue
+                    if msg_crc in cls.VPP_API_CRC_VERIFIED[msg]:
+                        continue
+                    raise RuntimeError(
+                        "Found message with unsupported crc: "
+                        "{root} {filename} {msg} {crc}".format(
+                            root, filename, msg, msg_crc))
+
+    @classmethod
+    def check_message(cls, message):
+        """Fail if the message has no known CRC associated.
+
+        :param message: VPP API message name to check.
+        :type message: str
+        :raises RuntimeError: If no verified CRC for the message is found.
+        """
+        if cls.VPP_API_CRC_VERIFIED.get(message, tuple()):
+            return
+        candidates = cls.VPP_API_CRC_FOUND.get(message, tuple())
+        raise RuntimeError(
+            "Attempt to use message with no verified CRC: {message}, "
+            "candidate CRCs found: {crcs}".format(message, candidates))
