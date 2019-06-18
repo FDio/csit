c2405c9416ee0ff9cfe31b0c36d22b0798181a1d
Vratko Polak <vrpolak@cisco.com>
2019-06-18 14:41:58 +0200

    Store BUILD_TAG as filename in reservation dir
    Access to a failed testbed is graphical (no text copying available),
    so no reason for storing anything fancy, such as URL.
    
    String "Unknown" is used if job tag is not supplied,
    so this change should be backward compatible.
    
    Change-Id: If704d02e51374087fe39ad0f69432477d1b310d3
    Signed-off-by: Vratko Polak <vrpolak@cisco.com>

--- resources/tools/scripts/topo_reservation.py
+++ resources/tools/scripts/topo_reservation.py
@@ -33,9 +33,12 @@ def main():
                         help="Topology file")
     parser.add_argument("-c", "--cancel", help="Cancel reservation",
                         action="store_true")
+    parser.add_argument("-j", "--jobtag", required=False, default="Unknown"
+                        help="Identifier for Jenkins run suitable as filename")
     args = parser.parse_args()
     topology_file = args.topo
     cancel_reservation = args.cancel
+    jobtag = args.jobtag
 
     work_file = open(topology_file)
     topology = load(work_file.read())['nodes']
@@ -57,7 +60,9 @@ def main():
     if cancel_reservation:
         ret, _, err = ssh.exec_command("rm -r {}".format(RESERVATION_DIR))
     else:
-        ret, _, err = ssh.exec_command("mkdir {}".format(RESERVATION_DIR))
+        ret, _, err = ssh.exec_command(
+            "mkdir '{dir}' && touch '{dir}/{jobtag}'".format(
+                dir=RESERVATION_DIR, jobtag=jobtag))
 
     if ret != 0:
         print("{} unsuccessful:\n{}".
