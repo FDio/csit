c7ebcfd56d300b2c8a8e94960704627807cbe196
Vratko Polak <vrpolak@cisco.com>
2019-06-18 16:10:07 +0200

    Store "tag" and "url" in files in reservation dir
    String "Unknown" is used if job tag or url is not supplied,
    so this change should be backward compatible.
    
    + Fixed some pylint violations.
    + Docstring explaining the steps taken and reasoning behind two files.
    
    Change-Id: If704d02e51374087fe39ad0f69432477d1b310d3
    Signed-off-by: Vratko Polak <vrpolak@cisco.com>

--- resources/tools/scripts/topo_reservation.py
+++ resources/tools/scripts/topo_reservation.py
@@ -22,17 +22,48 @@ As source of truth, TG node from the topology file is used.
 
 import sys
 import argparse
-from resources.libraries.python.ssh import SSH
 from yaml import load
+
+from resources.libraries.python.ssh import SSH
+
 
 RESERVATION_DIR = "/tmp/reservation_dir"
 
+
 def main():
+    """Parse arguments, perform the intended action, propagate RC.
+
+    If the intended action is cancellation, reservation dir is deleted.
+
+    If the intended action is reservation, the list is longer:
+    1. List contents of reservation dir.
+    2. List contents of job.url file in the dir.
+    3. Create reservation dir.
+    4. Touch file according to -j option.
+    5. Put -u option string to file job.url
+    From these 5 steps, 1 and 2 are performed always, their RC ignored.
+    Steps 3-5 are chained with logical and, any non-zero RC breaks and returns.
+
+    The two files in reservation dir are there for reporting
+    which job holds the reservation, so people can manually fix the testbed
+    if the job has been aborted, or otherwise failed to unregister.
+
+    The two files have different audiences.
+    The URL content is useful if the console is being watched
+    from another job web UI. Copy and paste and you see the job run.
+    The "tag" as a filename is useful for admins accessing the testbed
+    via a graphical terminal, which does not allow copying of text,
+    as they need less keypresses to identify the job run.
+    """
     parser = argparse.ArgumentParser()
     parser.add_argument("-t", "--topo", required=True,
                         help="Topology file")
     parser.add_argument("-c", "--cancel", help="Cancel reservation",
                         action="store_true")
+    parser.add_argument("-j", "--jobtag", required=False, default="Unknown",
+                        help="Identifier for Jenkins run suitable as filename")
+    parser.add_argument("-u", "--url", required=False, default="Unknown",
+                        help="Identifier for Jenkins run suitable as URL")
     args = parser.parse_args()
     topology_file = args.topo
     cancel_reservation = args.cancel
@@ -57,13 +88,17 @@ def main():
     if cancel_reservation:
         ret, _, err = ssh.exec_command("rm -r {}".format(RESERVATION_DIR))
     else:
-        ret, _, err = ssh.exec_command("mkdir {}".format(RESERVATION_DIR))
+        ret, _, err = ssh.exec_command(
+            "ls '{dir}'; head -1 '{dir}/job.url'; mkdir '{dir}' && touch "
+            "'{dir}/{jobtag}' && ( echo '{url}' > '{dir}/job.url' )".format(
+                dir=RESERVATION_DIR, jobtag=args.jobtag, url=args.url))
 
     if ret != 0:
         print("{} unsuccessful:\n{}".
               format(("Cancellation " if cancel_reservation else "Reservation"),
                      err))
     return ret
+
 
 if __name__ == "__main__":
     sys.exit(main())
