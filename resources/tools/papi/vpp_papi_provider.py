d8ebe2f3dc40f58b894984c1d179f4633ee28c0c
Jan Gelety <jgelety@cisco.com>
2019-06-20 19:49:35 +0200

    FIX: IPUtil after vpp api changes
     + do not log all ip_route_add_del api calls to PAPI history
       in case of scale tests
    
    Change-Id: I9972e126b68ce1cb57f781e3d90ea9b3914de69e
    Signed-off-by: Jan Gelety <jgelety@cisco.com>

--- resources/tools/papi/vpp_papi_provider.py
+++ resources/tools/papi/vpp_papi_provider.py
@@ -93,10 +93,9 @@ def _convert_reply(api_r):
 
     def process_value(val):
         if isinstance(val, dict):
-            val_dict = dict()
             for val_k, val_v in val.iteritems():
-                val_dict[str(val_k)] = process_value(val_v)
-            return val_dict
+                val[str(val_k)] = process_value(val_v)
+            return val
         elif isinstance(val, list):
             for idx, val_l in enumerate(val):
                 val[idx] = process_value(val_l)
@@ -141,13 +140,12 @@ def process_json_request(args):
 
     def process_value(val):
         if isinstance(val, dict):
-            val_dict = dict()
             for val_k, val_v in val.iteritems():
-                val_dict[str(val_k)] = process_value(val_v)
-            return val_dict
+                val[str(val_k)] = process_value(val_v)
+            return val
         elif isinstance(val, list):
-            for idx, item in enumerate(val):
-                val[idx] = process_value(item)
+            for idx, val_l in enumerate(val):
+                val[idx] = process_value(val_l)
             return val
         elif isinstance(val, unicode):
             return binascii.unhexlify(val)
