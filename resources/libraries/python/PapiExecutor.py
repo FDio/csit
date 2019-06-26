72627d6d34391606ce34f7534e43b4854fc15abe
Vratko Polak <vrpolak@cisco.com>
2019-06-26 11:59:04 +0200

    DEBUG: Patch more logging into PAPI
    Change-Id: I9989d3aceb7bf43ca46ad34d41fe20dd4c8ed89f
    Signed-off-by: Vratko Polak <vrpolak@cisco.com>

--- resources/libraries/python/PapiExecutor.py
+++ resources/libraries/python/PapiExecutor.py
@@ -204,6 +204,9 @@ class PapiSocketExecutor(object):
             # When present locally, we finally can find the installation path.
             package_path = glob.glob(tmp_dir + installed_papi_glob)[-1]
             logger.debug("papi module path: {path}".format(path=package_path))
+            run(["bash", "-c", "patch " + package_path
+                 + "/vpp_transport_socket.py < " + os.environ["CSIT_DIR"]
+                 + "/vpp_transport_socket.diff"], check=True)
             # Package path has to be one level above the vpp_papi directory.
             package_path = package_path.rsplit('/', 1)[0]
             logger.debug("package path: {path}".format(path=package_path))
