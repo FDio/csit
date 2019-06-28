38d15c2f3d7c405d500b863c4141197b7431cfd0
Vratko Polak <vrpolak@cisco.com>
2019-06-28 14:55:32 +0200

    Avoid ci-management archiving files twice
    https://gerrit.fd.io/r/20119
    has changed the way archival works,
    everything should now go to logs.fd.io
    (instead of appearing on run page in jenkins.fd.io).
    
    The glob pattern for archiving is quite eager,
    doing recursive search. That is good, as it can find
    also misplaced useful outputs.
    But it also means our usage of copy_archives function
    creates two copies of archived directories,
    usually archives/ and archives/archive/.
    
    This change simply removes copy_archives,
    relying on the new recursive search.
    
    A solution with changing ARCHIVE_DIR and moving if needed
    would be better, but looking at amout of jobs
    and various func bootstraps, it is not worth the effort.
    
    Change-Id: Iad2bc09455944307e59eecafa64d1db6dc8befe8
    Signed-off-by: Vratko Polak <vrpolak@cisco.com>

--- resources/libraries/bash/entry/per_patch_perf.sh
+++ resources/libraries/bash/entry/per_patch_perf.sh
@@ -70,7 +70,6 @@ for ((iter=0; iter<iterations; iter++)); do
     select_build "build_parent" || die
     check_download_dir || die
     run_pybot || die
-    copy_archives || die
     archive_parse_test_results "csit_parent/${iter}" || die
     die_on_pybot_error || die
     # TODO: Use less heavy way to avoid apt remove failures.
@@ -78,7 +77,6 @@ for ((iter=0; iter<iterations; iter++)); do
     select_build "build_current" || die
     check_download_dir || die
     run_pybot || die
-    copy_archives || die
     archive_parse_test_results "csit_current/${iter}" || die
     die_on_pybot_error || die
 done
