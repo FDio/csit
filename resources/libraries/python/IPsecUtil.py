4f1cc0dfd57ce1c5d259bf87fdbe505b5f6e6d5a
Peter Mikus <pmikus@cisco.com>
2019-07-11 06:35:21 +0000

    VPPD: IPSec GCM
    Change-Id: Ibb19d8527c5f5f72c90118ca0e888e426d21ef6e
    Signed-off-by: Peter Mikus <pmikus@cisco.com>

--- resources/libraries/python/IPsecUtil.py
+++ resources/libraries/python/IPsecUtil.py
@@ -237,6 +237,28 @@ class IPsecUtil(object):
         return int(IPsecProto.SEC_AH)
 
     @staticmethod
+    def generate_keys_for_ipsec(crypto_alg, integ_alg):
+        """Generate Crypto and Integration strings as a keys.
+
+        :param crypto_alg: Encryption key.
+        :param integ_alg: Integration key.
+        :type crypto_alg: CryptoAlg
+        :type integ_alg: IntegAlg
+        :returns: The generated keys and salted version.
+        :rtype: tuple of str
+        """
+        clen = IPsecUtil.get_crypto_alg_key_len(crypto_alg)
+        ckey = ''.join(choice(letters) for _ in range(clen))
+
+        if integ_alg:
+            ilen = IPsecUtil.get_integ_alg_key_len(integ_alg)
+            ikey = ''.join(choice(letters) for _ in range(ilen))
+        else:
+            ikey = ''
+
+        return ckey, ikey
+
+    @staticmethod
     def vpp_ipsec_select_backend(node, protocol, index=1):
         """Select IPsec backend.
 
