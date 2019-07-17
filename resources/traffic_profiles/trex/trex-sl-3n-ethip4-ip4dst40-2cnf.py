ce149b36ef1883a071b2fd7b109ccac88850cc30
Ludovit Mikula <ludovit.mikula@pantheon.tech>
2019-07-17 14:36:21 +0000

    Introduce VPP-IPsec container tests. Removed 9000B test cases from ipsec test suites.
    Change-Id: Ie64d662e81879bd52785e0188450d998bf056bda
    Signed-off-by: Ludovit Mikula <ludovit.mikula@pantheon.tech>

---
+++ resources/traffic_profiles/trex/trex-sl-3n-ethip4-ip4dst40-2cnf.py
@@ -0,0 +1,103 @@
+# Copyright (c) 2019 Cisco and/or its affiliates.
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at:
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Stream profile for T-rex traffic generator.
+
+Stream profile:
+ - Two streams sent in directions 0 --> 1 and 1 --> 0 at the same time.
+ - Packet: ETH / IP /
+ - Direction 0 --> 1:
+   - Source IP address range:      10.0.0.1
+   - Destination IP address range: 20.0.0.0 - 20.0.0.39
+ - Direction 1 --> 0:
+   - Source IP address range:      20.0.0.1
+   - Destination IP address range: 10.0.0.0 - 10.0.0.39
+"""
+
+from trex.stl.api import *
+from profile_trex_stateless_base_class import TrafficStreamsBaseClass
+
+
+class TrafficStreams(TrafficStreamsBaseClass):
+    """Stream profile."""
+
+    def __init__(self):
+        """Initialization and setting of streams' parameters."""
+
+        super(TrafficStreamsBaseClass, self).__init__()
+
+        self.p2_dst_start_mac = '02:02:00:00:00:00'
+        self.p2_dst_end_mac = '02:02:00:00:00:01'
+
+        # IPs used in packet headers.
+        self.p1_src_start_ip = '10.0.0.1'
+        self.p1_dst_start_ip = '20.0.0.0'
+        self.p1_dst_end_ip = '20.0.0.39'
+
+        self.p2_src_start_ip = '20.0.0.1'
+        self.p2_dst_start_ip = '10.0.0.0'
+        self.p2_dst_end_ip = '10.0.0.39'
+
+    def define_packets(self):
+        """Defines the packets to be sent from the traffic generator.
+
+        Packet definition: | ETH | IP |
+
+        :returns: Packets to be sent from the traffic generator.
+        :rtype: tuple
+        """
+
+        # Direction 0 --> 1
+        base_pkt_a = (Ether() /
+                      IP(src=self.p1_src_start_ip,
+                         dst=self.p1_dst_start_ip,
+                         proto=61))
+        # Direction 1 --> 0
+        base_pkt_b = (Ether(dst=self.p2_dst_start_mac) /
+                      IP(src=self.p2_src_start_ip,
+                         dst=self.p2_dst_start_ip,
+                         proto=61))
+
+        # Direction 0 --> 1
+        vm1 = STLScVmRaw([STLVmFlowVar(name="dst",
+                                       min_value=self.p1_dst_start_ip,
+                                       max_value=self.p1_dst_end_ip,
+                                       size=4, op="inc"),
+                          STLVmWrFlowVar(fv_name="dst", pkt_offset="IP.dst"),
+                          STLVmFixIpv4(offset="IP")])
+        # Direction 1 --> 0
+        vm2 = STLScVmRaw([STLVmFlowVar(name="mac_dst",
+                                       min_value=0,
+                                       max_value=1,
+                                       size=1, op="inc"),
+                          STLVmWrFlowVar(fv_name="mac_dst", pkt_offset=5),
+                          STLVmFlowVar(name="dst",
+                                       min_value=self.p2_dst_start_ip,
+                                       max_value=self.p2_dst_end_ip,
+                                       size=4, op="inc"),
+                          STLVmWrFlowVar(fv_name="dst", pkt_offset="IP.dst"),
+                          STLVmFixIpv4(offset="IP")])
+
+        return base_pkt_a, base_pkt_b, vm1, vm2
+
+
+def register():
+    """Register this traffic profile to T-rex.
+
+    Do not change this function.
+
+    :return: Traffic streams.
+    :rtype: Object
+    """
+    return TrafficStreams()
+
