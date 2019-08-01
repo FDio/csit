39a4965b1ee3ce227974b76c70821126005944c7
Vratko Polak <vrpolak@cisco.com>
2019-08-01 13:36:24 +0200

    Revert "Disable CRC checking at runtime"
    This reverts commit d541b2b7d99651b53bd21ff75cd5fdacf8472a98.
    This reverts commit 2dd27f5a638b5231c0f074ca61e6b67fed9d1faf.
    
    Needed to verify CSIT expects the correct CRCs.
    
    Change-Id: Id459800744cd93c578eab9c2e84cb9528235b064
    Signed-off-by: Vratko Polak <vrpolak@cisco.com>

--- resources/libraries/python/VppApiCrc.py
+++ resources/libraries/python/VppApiCrc.py
@@ -274,6 +274,5 @@ class VppApiCrcChecker(object):
             return
         crc = self._found.get(api_name, None)
         self._reported[api_name] = crc
-        # Disabled temporarily during CRC mismatch.
-        #raise RuntimeError("No active collection has API {api} CRC found {crc}"\
-        #    .format(api=api_name, crc=crc))
+        raise RuntimeError("No active collection has API {api} CRC found {crc}"\
+            .format(api=api_name, crc=crc))
