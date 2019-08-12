42cec57b46ff26307f03ad45bbfafb3009000fd5
Vratko Polak <vrpolak@cisco.com>
2019-08-12 10:56:27 +0200

    WiP: pktloss
    + One suite stub.
    + Config copied from other test, vxlan used.
    + Layer keywords prepared for additional chains.
    + Renamed "create" argument to "vlan_per_chain".
    + TrafficGenerator result extraction improvements.
    + Throughput estimated as PDR for zero PLR.
    + Actual Qemu start is skipped.
    - In progress:
     + Patch Trex so stats at async stop are good.
    - Missing:
     - Expose loss count as appropriate test message.
     - Support dot1q without vxlan?
     - Support also deletion?
     - Support also vlan_per-chain=True?
    
    Change-Id: If0270697f9ffa837c34ef802917220a53f156703
    Signed-off-by: Vratko Polak <vrpolak@cisco.com>

--- resources/tools/trex/trex_installer.sh
+++ resources/tools/trex/trex_installer.sh
@@ -1,4 +1,6 @@
-#!/bin/sh
+#!/bin/bash
+
+set -exuo pipefail
 
 TREX_VERSION=$1
 
@@ -9,28 +11,37 @@ TARGET_DIR="/opt/"
 TREX_DIR="trex-core-${TREX_VERSION}/"
 TREX_INSTALL_DIR="${TARGET_DIR}${TREX_DIR}"
 
-if test "$(id -u)" -ne 0
-then
+if [[ "$(id -u)" != "0" ]]; then
     echo "Please use root or sudo to be able to access target installation directory: ${TARGET_DIR}"
     exit 1
 fi
 
-WORKING_DIR=$(mktemp -d)
-test $? -eq 0 || exit 1
+WORKING_DIR=$(mktemp -d) || exit 1
 
 cleanup () {
-    rm -r ${WORKING_DIR}
+    rm -rf ${WORKING_DIR}
 }
-
 trap cleanup EXIT
 
-test -d ${TREX_INSTALL_DIR} && echo "T-REX aleready installed: ${TREX_INSTALL_DIR}" && exit 0
+if [[ -d "${TREX_INSTALL_DIR}" ]]; then
+    echo "T-REX aleready installed: ${TREX_INSTALL_DIR}"
+else
+    wget -P "${WORKING_DIR}" "${TREX_PACKAGE_URL}" || exit 1
+    unzip "${WORKING_DIR}/${TREX_DOWNLOAD_PACKAGE}" -d "${TARGET_DIR}" || exit 1
+    cd "${TREX_INSTALL_DIR}/linux_dpdk/" && ./b configure && ./b build || exit 1
+    cd "${TREX_INSTALL_DIR}/scripts/ko/src" && make && make install || exit 1
+fi
 
-wget -P ${WORKING_DIR} ${TREX_PACKAGE_URL}
-test $? -eq 0 || exit 1
-
-unzip ${WORKING_DIR}/${TREX_DOWNLOAD_PACKAGE} -d ${TARGET_DIR}
-test $? -eq 0 || exit 1
-
-cd ${TREX_INSTALL_DIR}/linux_dpdk/ && ./b configure && ./b build || exit 1
-cd ${TREX_INSTALL_DIR}/scripts/ko/src && make && make install || exit 1
+dir_with_patch="/tmp/openvpp-testing/resources/patch/trex/2.54"
+dir_to_patch="/opt/trex-core-2.54/"
+dir_to_patch+="/scripts/automation/trex_control_plane/interactive/trex/common/"
+pushd "${dir_to_patch}"
+set +e
+patch -N < "${dir_with_patch}/no_stats_clear_on_connect.patch"
+status="${?}"
+set -e
+if [[ "${status}" == "0" ]]; then
+    echo "Trex patched and ready."
+else
+    echo "Patch has been probably applied already."
+popd
