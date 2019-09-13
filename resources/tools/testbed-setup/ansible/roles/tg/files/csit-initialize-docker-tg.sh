eb96176b98530d7a6f90e6891d2ce24433fec322
Peter Mikus <pmikus@cisco.com>
2019-09-13 12:00:49 +0000

    Add: Use containers for shared TG
    + Ansible
      + Service for auto load during restart
      + By default created 2 instances of Docker container
    + Small modification of Trex to use prefixes
    + DNV topology to use ssh port to connect to docker
    
    Signed-off-by: Peter Mikus <pmikus@cisco.com>
    Change-Id: Ibc2b2f0e509d920fc3e05d012b88abc388f5c3e7

---
+++ resources/tools/testbed-setup/ansible/roles/tg/files/csit-initialize-docker-tg.sh
@@ -0,0 +1,56 @@
+#!/usr/bin/env bash
+
+# Copyright (c) 2019 Cisco and/or its affiliates.
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at:
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# CSIT SRIOV VF initialization and isolation.
+
+set -euo pipefail
+
+case "${1:-start}" in
+    "start" )
+        # Run TG
+        for cnt in $(seq 1 ${2:-1}); do
+            # If the IMAGE is not already loaded then docker run will pull the
+            # IMAGE, and all image dependencies, before it starts the container.
+            dcr_image="snergster/csit-sut:latest"
+            # Run the container in the background and print the new container
+            # ID.
+            dcr_stc_params="--detach=true "
+            # Give extended privileges to this container. A "privileged"
+            # container is given access to all devices and able to run nested
+            # containers.
+            dcr_stc_params+="--privileged "
+            # Publish all exposed ports to random ports on the host interfaces.
+            dcr_stc_params+="--publish 600${cnt}:22 "
+            # Automatically remove the container when it exits.
+            dcr_stc_params+="--rm "
+            # Size of /dev/shm.
+            dcr_stc_params+="--shm-size 4G "
+            # Mount vfio to be able to bind to see binded interfaces. We cannot
+            # use --device=/dev/vfio as this does not see newly binded
+            # interfaces.
+            dcr_stc_params+="--volume /dev:/dev "
+            # Mount /opt/boot/ where VM kernel and initrd are located.
+            dcr_stc_params+="--volume /opt:/opt "
+            # Mount host hugepages for VMs.
+            dcr_stc_params+="--volume /dev/hugepages:/dev/hugepages "
+
+            params=(${dcr_stc_params} --name csit-tg-"${cnt}" "${dcr_image}")
+            docker run "${params[@]}"
+        done
+        ;;
+    "stop" )
+        docker rm --force $(docker ps --all --quiet --filter name=csit-tg)
+        ;;
+esac
