1e032d97620f8685841a76febe3078f19e73ce55
Tibor Frank <tifrank@cisco.com>
2019-09-26 09:16:32 +0200

    Report: Configure 1908.1.40
    Signed-off-by: Tibor Frank <tifrank@cisco.com>
    Change-Id: I5f92b1cf84c61ebab5606092e20361ea4c416f1a

--- resources/tools/presentation/run_report.sh
+++ resources/tools/presentation/run_report.sh
@@ -38,7 +38,7 @@ export PYTHONPATH=`pwd`
 python pal.py \
     --specification specification.yaml \
     --release ${RELEASE} \
-    --week "39" \
+    --week "40" \
     --logging INFO \
     --force
 
