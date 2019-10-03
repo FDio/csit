cb0d344595a2949b831c3fc189a5bbded7a4fc24
Tibor Frank <tifrank@cisco.com>
2019-10-03 09:35:56 +0200

    Report: Add data and configure 1908.1.41
    Signed-off-by: Tibor Frank <tifrank@cisco.com>
    Change-Id: I3cad74cc1f183ba86d5e7377d68bbdd58c8ad42c

--- resources/tools/presentation/run_report.sh
+++ resources/tools/presentation/run_report.sh
@@ -38,7 +38,7 @@ export PYTHONPATH=`pwd`
 python pal.py \
     --specification specification.yaml \
     --release ${RELEASE} \
-    --week "40" \
+    --week "41" \
     --logging INFO \
     --force
 
