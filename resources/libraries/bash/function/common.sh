8907089e7f723dcdae6f4affa42aef5d440a5eb9
Vratko Polak <vrpolak@cisco.com>
2019-10-16 10:43:58 +0200

    Hack perf per patch job to test once
    Change-Id: If320453d8d0884fd919375ea9e86e004aedd6ab3
    Signed-off-by: Vratko Polak <vrpolak@cisco.com>

--- resources/libraries/bash/function/common.sh
+++ resources/libraries/bash/function/common.sh
@@ -865,9 +865,9 @@ function select_tags () {
         # Automatic prefixing for VPP jobs to limit the NIC used and
         # traffic evaluation to MRR.
         if [[ "${TEST_TAG_STRING-}" == *"nic_"* ]]; then
-            prefix="${prefix}mrrAND"
+            prefix="${prefix}"
         else
-            prefix="${prefix}mrrAND${default_nic}AND"
+            prefix="${prefix}${default_nic}AND"
         fi
     fi
     for tag in "${test_tag_array[@]}"; do
