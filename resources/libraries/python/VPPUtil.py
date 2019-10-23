d5eeffc125979d69301561222018287770f1c997
Vratko Polak <vrpolak@cisco.com>
2019-10-23 12:50:30 +0200

    DO_NOT_MERGE: Examine Taishan failure
    Change-Id: I87a5f5230914bf8636d72fff362be9564d1ee4df
    Signed-off-by: Vratko Polak <vrpolak@cisco.com>

--- resources/libraries/python/VPPUtil.py
+++ resources/libraries/python/VPPUtil.py
@@ -131,14 +131,18 @@ class VPPUtil(object):
         :type node: dict
         :raises RuntimeError: If VPP service fails to start.
         """
+#        return  # DEBUG
         VPPUtil.verify_vpp_installed(node)
         try:
             # Verify responsivness of vppctl.
             VPPUtil.verify_vpp_started(node)
             # Verify responsivness of PAPI.
-            VPPUtil.show_log(node)
+            logger.trace("Next: show version")
             VPPUtil.vpp_show_version(node)
+            logger.trace("Next: show log")
+            VPPUtil.show_log(node)
         finally:
+            logger.trace("Next: get service logs")
             DUTSetup.get_service_logs(node, Constants.VPP_UNIT)
 
     @staticmethod
