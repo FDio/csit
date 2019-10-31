ead0d82ac329ab6da3f865216e49487a4e7bd1c6
Vratko Polak <vrpolak@cisco.com>
2019-10-31 17:53:47 +0100

    Refactor jumpavg to be more readable and usable
    The older version is clunky for bisect script usage.
    
    + New version is 0.2.0 due to amount o API changes.
    + Jumpavg is now part of CSIT resource package tree.
    + Perpatch migrated to new jumpavg.
    + PAL included to CSIT resource package tree and migrated.
    - Not completely tested.
    
    Change-Id: Id87dd214389d0371e200814bbf1d5f9a5a310dd4
    Signed-off-by: Vratko Polak <vrpolak@cisco.com>

--- resources/tools/presentation/input_data_files.py
+++ resources/tools/presentation/input_data_files.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2018 Cisco and/or its affiliates.
+# Copyright (c) 2019 Cisco and/or its affiliates.
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at:
@@ -28,7 +28,7 @@ from requests.packages.urllib3.util.retry import Retry
 from requests import codes, RequestException, Timeout, TooManyRedirects, \
     HTTPError, ConnectionError
 
-from errors import PresentationError
+from .errors import PresentationError
 
 
 # Chunk size used for file download
