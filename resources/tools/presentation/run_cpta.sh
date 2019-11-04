437d88644b5e15b86b63cf1e87f1a18c38714977
Peter Mikus <pmikus@cisco.com>
2019-11-04 09:27:28 +0000

    Python3: PIP requirement
    - Install via PIP3
    - Use virtualenv Python3 interpret
    
    Signed-off-by: Peter Mikus <pmikus@cisco.com>
    Change-Id: Iad22b344b418a1d91774d23d5ea17bfd48d394b3

--- resources/tools/presentation/run_cpta.sh
+++ resources/tools/presentation/run_cpta.sh
@@ -16,11 +16,11 @@ sudo apt-get -y install libxml2 libxml2-dev libxslt-dev build-essential \
 mkdir ${DIR[WORKING]}
 
 # Create virtual environment
-virtualenv ${DIR[WORKING]}/env
-. ${DIR[WORKING]}/env/bin/activate
+virtualenv -p $(which python3) ${DIR[WORKING]}/env
+source ${DIR[WORKING]}/env/bin/activate
 
 # Install python dependencies:
-pip install -r requirements.txt
+pip3 install -r requirements.txt
 
 export PYTHONPATH=`pwd`
 
