a34426e7dd601289b1cb35211f1a149ab207531f
Tibor Frank <tifrank@cisco.com>
2019-11-20 11:43:44 +0100

    Python3: PAL
    Change-Id: Icddc01d3ccb451abb92b9e5d912b642d01866033
    Signed-off-by: Tibor Frank <tifrank@cisco.com>

--- resources/tools/presentation/utils.py
+++ resources/tools/presentation/utils.py
@@ -29,7 +29,7 @@ from datetime import datetime
 from pandas import Series
 
 from resources.libraries.python import jumpavg
-from .errors import PresentationError
+from errors import PresentationError
 
 
 def mean(items):
@@ -273,7 +273,7 @@ def classify_anomalies(data):
     # Nan means something went wrong.
     # Use 0.0 to cause that being reported as a severe regression.
     bare_data = [0.0 if np.isnan(sample) else sample
-                 for sample in data.itervalues()]
+                 for sample in data.values()]
     # TODO: Make BitCountingGroupList a subclass of list again?
     group_list = jumpavg.classify(bare_data).group_list
     group_list.reverse()  # Just to use .pop() for FIFO.
@@ -282,7 +282,7 @@ def classify_anomalies(data):
     active_group = None
     values_left = 0
     avg = 0.0
-    for sample in data.itervalues():
+    for sample in data.values():
         if np.isnan(sample):
             classification.append("outlier")
             avgs.append(sample)
@@ -313,7 +313,7 @@ def convert_csv_to_pretty_txt(csv_file, txt_file):
     """
 
     txt_table = None
-    with open(csv_file, 'rb') as csv_file:
+    with open(csv_file, 'rt') as csv_file:
         csv_content = csv.reader(csv_file, delimiter=',', quotechar='"')
         for row in csv_content:
             if txt_table is None:
