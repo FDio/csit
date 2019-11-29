32806be17cacfc80b6d14f1a4f6c5a6b040f1c56
Vratko Polak <vrpolak@cisco.com>
2019-11-29 12:31:19 +0100

    DO_NOT_MERGE: Build debug image for vpp-csit
    Currently just for examining currently failing tap tests.
    
    Change-Id: Ic682429d9d1402edd3540ab9fae667747c6285b5
    Signed-off-by: Vratko Polak <vrpolak@cisco.com>

--- resources/libraries/bash/function/per_patch.sh
+++ resources/libraries/bash/function/per_patch.sh
@@ -82,7 +82,9 @@ function build_vpp_ubuntu_amd64 () {
     set -exuo pipefail
 
     cd "${VPP_DIR}" || die "Change directory command failed."
-    make UNATTENDED=y pkg-verify || die "VPP build using make pkg-verify failed."
+    make UNATTENDED=y install-dep install-ext-deps pkg-deb-debug || {
+        die "VPP build using make pkg-verify failed."
+    }
     echo "* VPP ${1-} BUILD SUCCESSFULLY COMPLETED" || {
         die "Argument not found."
     }
