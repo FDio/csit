032e632d1c40bcbfa110e19b93737e927b610976
Jan Gelety <jgelety@cisco.com>
2019-11-29 14:29:53 +0100

    DEBUG: ipsec via VAT on HSW
    Change-Id: Ibf1979b87aeea0f4c195b97c8e6b59a4a23b1b77
    Signed-off-by: Jan Gelety <jgelety@cisco.com>

--- resources/libraries/python/IPsecUtil.py
+++ resources/libraries/python/IPsecUtil.py
@@ -21,6 +21,7 @@ from random import choice
 from string import ascii_letters
 
 from ipaddress import ip_network, ip_address
+from robot.api import logger
 
 from resources.libraries.python.IPUtil import IPUtil
 from resources.libraries.python.InterfaceUtil import InterfaceUtil, \
@@ -270,7 +271,9 @@ class IPsecUtil:
         """
         err_msg = f"Failed to dump IPsec backends on host {node[u'host']}"
         with PapiSocketExecutor(node) as papi_exec:
-            papi_exec.add(u"ipsec_backend_dump").get_details(err_msg)
+            data = papi_exec.add(u"ipsec_backend_dump").get_details(err_msg)
+
+        logger.debug(f"IPSEC backend data:\n{data}")
 
     @staticmethod
     def vpp_ipsec_add_sad_entry(
@@ -823,6 +826,10 @@ class IPsecUtil:
                 tmp_f2.write(
                     f"exec set interface ip address {if2_n} {if2_ip}/{mask}\n"
                 )
+                tmp_f2.write(
+                    f"exec ip route add {ip_network(if1_ip_addr + '/8', False)}"
+                    f" via {if2_n} {if2_ip - 1}\n"
+                )
                 for i in range(n_tunnels):
                     ckey = gen_key(
                         IPsecUtil.get_crypto_alg_key_len(crypto_alg)
@@ -871,10 +878,6 @@ class IPsecUtil:
             os.remove(tmp_fn2)
 
             with open(tmp_fn1, 'w') as tmp_f1, open(tmp_fn2, 'w') as tmp_f2:
-                raddr = ip_network(if1_ip_addr + u"/8", False)
-                tmp_f2.write(
-                    f"exec ip route add {raddr} via {if2_n} {if2_ip - 1}\n"
-                )
                 for i in range(n_tunnels):
                     tmp_f1.write(
                         f"exec set interface unnumbered ipip{i} use {if1_n}\n"
