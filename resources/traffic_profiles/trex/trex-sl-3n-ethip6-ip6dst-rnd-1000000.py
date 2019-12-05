025699cad80753a1c481dfb388a2f70ce7eec349
Balaji Venkatraman <balajiv@cisco.com>
2019-12-05 07:40:09 -0800

    Python3: IPv6 FIB scale tests with random flows.
    Signed-off-by: Balaji Venkatraman <balajiv@cisco.com>
    Change-Id: I92918dd1448d43db71a2ddd18e13a4cb871fb8d0

---
+++ resources/traffic_profiles/trex/trex-sl-3n-ethip6-ip6dst-rnd-1000000.py
@@ -0,0 +1,103 @@
+# Copyright (c) 2019 Cisco and/or its affiliates.
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at:
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Stream profile for T-rex traffic generator.
+
+Stream profile:
+ - Two streams sent in directions 0 --> 1 and 1 --> 0 at the same time.
+ - Packet: ETH / IPv6 /
+ - Direction 0 --> 1:
+   - Source IP address range:      2001:1::1
+   - Destination IP address range: 2001:2::0 - 2001:2::F:423F
+ - Direction 1 --> 0:
+   - Source IP address range:      2001:2::1
+   - Destination IP address range: 2001:1::0 - 2001:1::F:423F
+"""
+
+from trex.stl.api import *
+from profile_trex_stateless_base_class import TrafficStreamsBaseClass
+
+
+class TrafficStreams(TrafficStreamsBaseClass):
+    """Stream profile."""
+
+    def __init__(self):
+        """Initialization and setting of streams" parameters."""
+
+        super(TrafficStreamsBaseClass, self).__init__()
+
+        # IPs used in packet headers.
+        self.p1_src_start_ip = u"2001:1::1"
+        self.p1_dst_start_ip = u"2001:2::0"
+        self.p1_dst_end_ip = u"2001:2::F:423F"
+
+        self.p2_src_start_ip = u"2001:2::1"
+        self.p2_dst_start_ip = u"2001:1::0"
+        self.p2_dst_end_ip = u"2001:1::F:423F"
+
+    def define_packets(self):
+        """Defines the packets to be sent from the traffic generator.
+
+        Packet definition: | ETH | IPv6 |
+
+        :returns: Packets to be sent from the traffic generator.
+        :rtype: tuple
+        """
+
+        base_p1, count_p1 = self._get_start_end_ipv6(self.p1_dst_start_ip,
+                                                     self.p1_dst_end_ip)
+        base_p2, count_p2 = self._get_start_end_ipv6(self.p2_dst_start_ip,
+                                                     self.p2_dst_end_ip)
+
+        # Direction 0 --> 1
+        base_pkt_a = Ether() / IPv6(src=self.p1_src_start_ip,
+                                    dst=self.p1_dst_start_ip)
+        # Direction 1 --> 0
+        base_pkt_b = Ether() / IPv6(src=self.p2_src_start_ip,
+                                    dst=self.p2_dst_start_ip)
+
+        # Direction 0 --> 1
+        vm1 = STLScVmRaw([STLVmFlowVarRepeatableRandom (\
+                              name=u"dst",
+                              min_value=self.p1_dst_start_ip,
+                              max_value=self.p1_dst_end_ip,
+                              size=8,
+                              limit=1000000, seed=0x0000),
+                          STLVmWrFlowVar(\
+                              fv_name=u"ipv6_dst",
+                              pkt_offset=u"IPv6.dst",
+                              offset_fixup=8)])
+        # Direction 1 --> 0
+        vm2 = STLScVmRaw([STLVmFlowVarRepeatableRandom (\
+                              name=u"dst",
+                              min_value=self.p2_dst_start_ip,
+                              max_value=self.p2_dst_end_ip,
+                              size=8,
+                              limit=1000000, seed=0x0000),
+                          STLVmWrFlowVar(\
+                              fv_name=u"ipv6_dst",
+                              pkt_offset=u"IPv6.dst",
+                              offset_fixup=8)])
+
+        return base_pkt_a, base_pkt_b, vm1, vm2
+
+
+def register():
+    """Register this traffic profile to T-rex.
+
+    Do not change this function.
+
+    :return: Traffic streams.
+    :rtype: Object
+    """
+    return TrafficStreams()
