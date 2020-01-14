55a37ed6e44fa04bea45d0725c755965cf5ec109
Balaji Venkatraman <balajiv@cisco.com>
2020-01-14 06:57:10 -0800

    Python3: Migration of files under traffic-profiles/trex
    Change-Id: Iae16225d3d3daa4e1e9b177228fb89efe4aa5405
    Signed-off-by: Balaji Venkatraman <balajiv@cisco.com>

--- resources/traffic_profiles/trex/trex-sl-3n-ethip4-ip4dst400-8cnf.py
+++ resources/traffic_profiles/trex/trex-sl-3n-ethip4-ip4dst400-8cnf.py
@@ -61,40 +61,84 @@ class TrafficStreams(TrafficStreamsBaseClass):
         """
 
         # Direction 0 --> 1
-        base_pkt_a = (Ether(dst=self.p1_dst_start_mac) /
-                      IP(src=self.p1_src_start_ip,
-                         dst=self.p1_dst_start_ip,
-                         proto=61))
+        base_pkt_a = (
+            Ether(
+                dst=self.p1_dst_start_mac
+            ) /
+            IP(
+                src=self.p1_src_start_ip,
+                dst=self.p1_dst_start_ip,
+                proto=61
+            )
+        )
         # Direction 1 --> 0
-        base_pkt_b = (Ether(dst=self.p2_dst_start_mac) /
-                      IP(src=self.p2_src_start_ip,
-                         dst=self.p2_dst_start_ip,
-                         proto=61))
+        base_pkt_b = (
+            Ether(
+                dst=self.p2_dst_start_mac
+            ) /
+            IP(
+                src=self.p2_src_start_ip,
+                dst=self.p2_dst_start_ip,
+                proto=61
+            )
+        )
 
         # Direction 0 --> 1
-        vm1 = STLScVmRaw([STLVmFlowVar(name=u"mac_dst",
-                                       min_value=0,
-                                       max_value=7,
-                                       size=1, op=u"inc"),
-                          STLVmWrFlowVar(fv_name=u"mac_dst", pkt_offset=5),
-                          STLVmFlowVar(name=u"dst",
-                                       min_value=self.p1_dst_start_ip,
-                                       max_value=self.p1_dst_end_ip,
-                                       size=4, op=u"inc"),
-                          STLVmWrFlowVar(fv_name=u"dst", pkt_offset=u"IP.dst"),
-                          STLVmFixIpv4(offset=u"IP")])
+        vm1 = STLScVmRaw(
+            [
+                STLVmFlowVar(
+                    name=u"mac_dst",
+                    min_value=0,
+                    max_value=7,
+                    size=1,
+                    op=u"inc"
+                ),
+                STLVmFlowVar(
+                    name=u"dst",
+                    min_value=self.p1_dst_start_ip,
+                    max_value=self.p1_dst_end_ip,
+                    size=4,
+                    op=u"inc"
+                ),
+                STLVmWrFlowVar(
+                    fv_name=u"dst",
+                    pkt_offset=u"IP.dst"
+                ),
+                STLVmFixIpv4(
+                    offset=u"IP"
+                )
+            ]
+        )
         # Direction 1 --> 0
-        vm2 = STLScVmRaw([STLVmFlowVar(name=u"mac_dst",
-                                       min_value=0,
-                                       max_value=7,
-                                       size=1, op=u"inc"),
-                          STLVmWrFlowVar(fv_name=u"mac_dst", pkt_offset=5),
-                          STLVmFlowVar(name=u"dst",
-                                       min_value=self.p2_dst_start_ip,
-                                       max_value=self.p2_dst_end_ip,
-                                       size=4, op=u"inc"),
-                          STLVmWrFlowVar(fv_name=u"dst", pkt_offset=u"IP.dst"),
-                          STLVmFixIpv4(offset=u"IP")])
+        vm2 = STLScVmRaw(
+            [
+                STLVmFlowVar(
+                    name=u"mac_dst",
+                    min_value=0,
+                    max_value=7,
+                    size=1,
+                    op=u"inc"
+                ),
+                STLVmWrFlowVar(
+                    fv_name=u"mac_dst",
+                    pkt_offset=5
+                ),
+                STLVmFlowVar(
+                    name=u"dst",
+                    min_value=self.p2_dst_start_ip,
+                    max_value=self.p2_dst_end_ip,
+                    size=4,
+                    op=u"inc"
+                ),
+                STLVmWrFlowVar(
+                    fv_name=u"dst",
+                    pkt_offset=u"IP.dst"
+                ),
+                STLVmFixIpv4(
+                    offset=u"IP"
+                )
+            ]
+        )
 
         return base_pkt_a, base_pkt_b, vm1, vm2
 
