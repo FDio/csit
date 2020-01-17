ba12d401a330cb8d8ba0f095eef7d4d9a7582cd2
Jan Gelety <jgelety@cisco.com>
2020-01-17 16:10:41 +0100

    FIX: configure avf for double link too
    Change-Id: I9f5f2fff1452ebd54a8fe7f84777d7c0b076ccce
    Signed-off-by: Jan Gelety <jgelety@cisco.com>

--- resources/libraries/python/autogen/Regenerator.py
+++ resources/libraries/python/autogen/Regenerator.py
@@ -259,7 +259,7 @@ def write_default_files(in_filename, in_prolog, kwargs_list):
                     1, u"Error adding driver prefix.", in_filename
                 )
                 out_prolog = replace_defensively(
-                    tmp2_prolog, u"vfio-pci", driver, 1,
+                    tmp2_prolog, u"vfio-pci", driver, 2,
                     u"Driver name should appear once.", in_filename
                 )
                 out_prolog = replace_defensively(
