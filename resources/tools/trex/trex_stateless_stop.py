5e2e25442e5ae45035a2a9225038b333b4929022
Vratko Polak <vrpolak@cisco.com>
2020-01-20 17:34:41 +0100

    Use more inclusive xstats for trex stop result
    The new formulas are constructed in such a way
    that results derived from xstats agree with results derived from stats.
    
    Change-Id: I0e2631586190f24510f7984cab766b9d05e8e870
    Signed-off-by: Vratko Polak <vrpolak@cisco.com>

--- resources/tools/trex/trex_stateless_stop.py
+++ resources/tools/trex/trex_stateless_stop.py
@@ -82,8 +82,15 @@ def main():
     print(u"##### statistics port 1 #####")
     print(json.dumps(xstats1, indent=4, separators=(u",", u": ")))
 
-    tx_0, rx_0 = xstats0[u"tx_good_packets"], xstats0[u"rx_good_packets"]
-    tx_1, rx_1 = xstats1[u"tx_good_packets"], xstats1[u"rx_good_packets"]
+    # We have used only *_good_packets before, but that is not enough.
+    # For data, see:
+    # https://gerrit.fd.io/r/c/csit/+/24287#message-eb71be79_7731c52f
+    tx_0 = xstats0[u"tx_good_packets"] + xstats0[u"tx_q0packets"]
+    rx_0 = xstats0[u"rx_good_packets"] + xstats0[u"rx_q0packets"]
+    rx_0 += xstats0[u"rx_q0errors"]
+    tx_1 = xstats1[u"tx_good_packets"] + xstats1[u"tx_q0packets"]
+    rx_1 = xstats1[u"rx_good_packets"] + xstats1[u"rx_q0packets"]
+    rx_1 += xstats1[u"rx_q0errors"]
     lost_a, lost_b = tx_0 - rx_1, tx_1 - rx_0
 
     print(f"\npackets lost from 0 --> 1:   {lost_a} pkts")
