3666a896812e7fbda37beab9a3a198d831aa7beb
Vratko Polak <vrpolak@cisco.com>
2020-01-20 14:55:21 +0100

    Separate files needing GPL license
    + Move the affected files to GPL/.
     + Update paths so files are executed from the new location.
    + Change their copyright notice to GPLv2+.
     - Change "program" to "file" to avoid naming programs.
     + Bump copyright year.
    + Start traffic scripts as python modules to allow relative imports.
    
    Change-Id: Ieb135629e890adbaf5b79497570f3be25b746f9f
    Signed-off-by: Vratko Polak <vrpolak@cisco.com>

---
+++ GPL/traffic_scripts/vxlan.py
@@ -0,0 +1,34 @@
+# Copyright (c) 2020 Cisco and/or its affiliates.
+#
+# This file is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This file is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this file.  If not, see <https://www.gnu.org/licenses/>.
+
+from scapy.fields import BitField, XByteField, X3BytesField
+from scapy.layers.inet import UDP
+from scapy.layers.l2 import Ether
+from scapy.packet import Packet, bind_layers
+
+
+class VXLAN(Packet):
+    name = u"VXLAN"
+    fields_desc = [
+        BitField(u"flags", 0x08000000, 32),
+        X3BytesField(u"vni", 0),
+        XByteField(u"reserved", 0x00)
+    ]
+
+    def mysummary(self):
+        return self.sprintf(f"VXLAN (vni={VXLAN.vni})")
+
+bind_layers(UDP, VXLAN, dport=4789)
+bind_layers(VXLAN, Ether)
