f3386337742014594089420afc1cb9774e383265
Dave Wallace <dwallacelf@gmail.com>
2020-01-21 19:02:14 +0000

    perf: hoststack iperf3 test tuning
    Change-Id: I53425f57fe9ecef9cff2c94642cc7cb24537a961
    Signed-off-by: Dave Wallace <dwallacelf@gmail.com>

--- resources/libraries/python/autogen/Regenerator.py
+++ resources/libraries/python/autogen/Regenerator.py
@@ -471,12 +471,8 @@ class Regenerator:
         ]
         hoststack_iperf3_kwargs_list = [
             {u"phy_cores": 1, u"frame_size": 0, u"clients": 1,
-             u"streams": 1, u"bytes_str": u"1G"},
+             u"streams": 1, u"bytes_str": u"10G"},
             {u"phy_cores": 1, u"frame_size": 0, u"clients": 1,
-             u"streams": 10, u"bytes_str": u"10G"},
-            {u"phy_cores": 2, u"frame_size": 0, u"clients": 1,
-             u"streams": 10, u"bytes_str": u"10G"},
-            {u"phy_cores": 4, u"frame_size": 0, u"clients": 1,
              u"streams": 10, u"bytes_str": u"10G"},
         ]
         hoststack_quic_kwargs_list = [
