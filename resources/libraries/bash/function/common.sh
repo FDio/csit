525a080331fee601c93d193228c2d6f04df5f66a
Jan Gelety <jgelety@cisco.com>
2020-01-21 03:01:46 +0100

    TRIGGER 2001 PERF NDRPDR FULL1
    - crypto part I
    
    Signed-off-by: Jan Gelety <jgelety@cisco.com>
    Change-Id: I18500c7fb2c2c9da3b0b3ceb31b271022c615ac8

--- resources/libraries/bash/function/common.sh
+++ resources/libraries/bash/function/common.sh
@@ -787,8 +787,7 @@ function select_tags () {
 #                                "mrrAND${default_nic}AND1cAND64bANDl2xcbase"
 #                                "!dot1q" "!drv_avf")
                 readarray -t test_tag_array <<< $(sed 's/ //g' \
-                    ${tfd}/mrr-daily-${NODENESS}-${FLAVOR}.txt |
-                    eval ${sed_nics_sub_cmd} | sed 's/ANDmrrAND/ANDndrpdrAND/g') || die
+                    ${tfd}/ndrpdr-report-full.txt) || die
             else
                 # If trigger contains tags, split them into array.
                 test_tag_array=(${TEST_TAG_STRING//:/ })
