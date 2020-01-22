3f9c77e427f19306f2abc05426ebbdf84d9e27bb
Jan Gelety <jgelety@cisco.com>
2020-01-22 16:38:39 +0100

    FIX: Detection if l2fwd/l3fwd is up/down
    Change-Id: Ide5de222e8314a0ea0be59f9a478f8d59147f722
    Signed-off-by: Jan Gelety <jgelety@cisco.com>

--- tests/dpdk/dpdk_scripts/cleanup_dpdk.sh
+++ tests/dpdk/dpdk_scripts/cleanup_dpdk.sh
@@ -20,7 +20,7 @@ if [ $? -eq "0" ]; then
     success=false
     sudo pkill testpmd
     echo "RC = $?"
-    for attempt in {1..5}; do
+    for attempt in {1..30}; do
         echo "Checking if testpmd is still alive, attempt nr ${attempt}"
         sudo pgrep testpmd
         if [ $? -eq "1" ]; then
@@ -47,7 +47,7 @@ if [ $? -eq "0" ]; then
     success=false
     sudo pkill l3fwd
     echo "RC = $?"
-    for attempt in {1..5}; do
+    for attempt in {1..30}; do
         echo "Checking if l3fwd is still alive, attempt nr ${attempt}"
         sudo pgrep l3fwd
         if [ $? -eq "1" ]; then
--- tests/dpdk/dpdk_scripts/run_l2fwd.sh
+++ tests/dpdk/dpdk_scripts/run_l2fwd.sh
@@ -42,7 +42,7 @@ if [ ${?} -eq "0" ]; then
     success=false
     sudo pkill testpmd
     echo "RC = ${?}"
-    for attempt in {1..30}; do
+    for attempt in {1..58}; do
         echo "Checking if testpmd is still alive, attempt nr ${attempt}"
         sudo pgrep testpmd
         if [ ${?} -eq "1" ]; then
@@ -69,7 +69,7 @@ if [ ${?} -eq "0" ]; then
     success=false
     sudo pkill l3fwd
     echo "RC = ${?}"
-    for attempt in {1..30}; do
+    for attempt in {1..58}; do
         echo "Checking if l3fwd is still alive, attempt nr ${attempt}"
         sudo pgrep l3fwd
         if [ ${?} -eq "1" ]; then
