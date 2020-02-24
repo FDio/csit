f0ffe4e24aaeab80aa143d3a69b560e0f360ae8d
Tibor Frank <tifrank@cisco.com>
2020-02-24 14:49:00 +0100

    PAL: Process local xml file
    Change-Id: I2d60b2c6616de6d6b2e1d742407ac392471883aa
    Signed-off-by: Tibor Frank <tifrank@cisco.com>

---
+++ resources/tools/presentation/run_local.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+set -x
+
+RELEASE=$1
+INPUT=$2
+
+# set default values in config array
+typeset -A CFG
+typeset -A DIR
+
+DIR[WORKING]=_tmp
+CFG[BLD_LATEX]=0
+
+# Install system dependencies
+sudo apt-get -y update
+sudo apt-get -y install libxml2 libxml2-dev libxslt-dev build-essential \
+    zlib1g-dev unzip
+
+if [[ ${CFG[BLD_LATEX]} -eq 1 ]] ;
+then
+    sudo apt-get -y install xvfb texlive-latex-recommended \
+        texlive-fonts-recommended texlive-fonts-extra texlive-latex-extra latexmk wkhtmltopdf inkscape
+    sudo sed -i.bak 's/^\(main_memory\s=\s\).*/\110000000/' /usr/share/texlive/texmf-dist/web2c/texmf.cnf
+fi
+
+# Create working directories
+mkdir ${DIR[WORKING]}
+
+# Create virtual environment
+virtualenv -p $(which python3) ${DIR[WORKING]}/env
+source ${DIR[WORKING]}/env/bin/activate
+
+# Install python dependencies:
+pip3 install -r requirements.txt
+
+export PYTHONPATH=`pwd`:`pwd`/../../../:`pwd`/../../libraries/python
+
+python pal.py \
+    --specification specification_local.yaml \
+    --release ${RELEASE} \
+    --week "09" \
+    --logging INFO \
+    --force \
+#    --input-file ${INPUT}
+    --input-directory  ${INPUT}
+
+RETURN_STATUS=$(echo $?)
+exit ${RETURN_STATUS}
