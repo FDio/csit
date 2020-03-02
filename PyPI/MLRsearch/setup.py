ff9d920a3c663743a9a3a5915359e25faec93d17
Vratko Polak <vrpolak@cisco.com>
2020-03-02 15:31:43 +0100

    Update PyPI metadata for MLRsearch 0.3.0
    Change-Id: I6a9c4739167f96bd2123f85ff2f6693562dce3bc
    Signed-off-by: Vratko Polak <vrpolak@cisco.com>

--- PyPI/MLRsearch/setup.py
+++ PyPI/MLRsearch/setup.py
@@ -9,32 +9,32 @@ from os import path
 from io import open
 
 here = path.abspath(path.dirname(__file__))
-with open(path.join(here, "README.rst"), encoding="utf-8") as f:
+with open(path.join(here, u"README.rst"), encoding=u"utf-8") as f:
     long_description = f.read()
 
 setup(
-    name="MLRsearch",
-    version="0.2.0",  # This is currently the only place listing the version.
-    description="Library for speeding up binary search using shorter measurements.",
+    name=u"MLRsearch",
+    version=u"0.3.0",  # This is currently the only place listing the version.
+    description=u"Library for speeding up binary search using shorter measurements.",
     long_description=long_description,
-    long_description_content_type="text/x-rst",
+    long_description_content_type=u"text/x-rst",
     # TODO: Create a separate webpage for MLRsearch library.
-    url="https://gerrit.fd.io/r/gitweb?p=csit.git;a=tree;f=PyPI/MLRsearch;hb=refs/heads/master",
-    author="Cisco Systems Inc. and/or its affiliates",
-    author_email="csit-dev@lists.fd.io",
+    url=u"https://gerrit.fd.io/r/gitweb?p=csit.git;a=tree;f=PyPI/MLRsearch;hb=refs/heads/master",
+    author=u"Cisco Systems Inc. and/or its affiliates",
+    author_email=u"csit-dev@lists.fd.io",
     classifiers=[
-        "Development Status :: 3 - Alpha",
-        "Intended Audience :: Science/Research",
-        "Intended Audience :: Telecommunications Industry",
+        u"Development Status :: 3 - Alpha",
+        u"Intended Audience :: Science/Research",
+        u"Intended Audience :: Telecommunications Industry",
         # Pick your license as you wish
-        "License :: OSI Approved :: Apache Software License",
+        u"License :: OSI Approved :: Apache Software License",
         # TODO: Test which Python versions is the code compatible with.
-        "Programming Language :: Python :: 2.7",
-        "Topic :: System :: Networking"
+        u"Programming Language :: Python :: 3.6",
+        u"Topic :: System :: Networking"
     ],
-    keywords="binary search throughput networking",
+    keywords=u"binary search throughput networking",
     packages=find_packages(exclude=[]),
-    # TODO: python_requires="~=2.7"
+    python_requires="~=3.6"
     install_requires=[],
     # TODO: Include simulator and tests.
     extras_require={
@@ -42,11 +42,11 @@ setup(
     package_data={
     },
     entry_points={
-        "console_scripts": [
+        u"console_scripts": [
         ],
     },
     project_urls={
-        "Bug Reports": "https://jira.fd.io/projects/CSIT/issues",
-        "Source": "https://gerrit.fd.io/r/gitweb?p=csit.git;a=tree;f=PyPI/MLRsearch;hb=refs/heads/master",
+        u"Bug Reports": "https://jira.fd.io/projects/CSIT/issues",
+        u"Source": "https://gerrit.fd.io/r/gitweb?p=csit.git;a=tree;f=PyPI/MLRsearch;hb=refs/heads/master",
     },
 )
