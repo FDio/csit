867452ab010e289cc1187de81a78989e46b91a9e
Tibor Frank <tifrank@cisco.com>
2020-03-13 17:05:59 +0100

    Report: hide 2n-skx NDR data
    Change-Id: Iee860b33338cfe60ba04081b9bd6cb6b685adb9b
    Signed-off-by: Tibor Frank <tifrank@cisco.com>
    (cherry picked from commit d74845dfa5875bc54882d8d74debe103dac07761)

--- resources/tools/presentation/generator_tables.py
+++ resources/tools/presentation/generator_tables.py
@@ -345,7 +345,7 @@ def table_merged_details(table, input_data):
                         # Temporary solution: remove NDR results from message:
                         if bool(table.get(u'remove-ndr', False)):
                             try:
-                                col_data = col_data.split(u"\n", 1)[1]
+                                col_data = col_data.split(u" |br| ", 1)[1]
                             except IndexError:
                                 pass
                         col_data = f" |prein| {col_data} |preout| "
