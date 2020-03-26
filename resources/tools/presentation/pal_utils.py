7c88b4a87d09b24292c39680ea281f5165fe5772
Tibor Frank <tifrank@cisco.com>
2020-03-26 07:37:27 +0100

    Compare old 1908 vs new 1908 and new 1908 vs 2001
    DO NOT MERGE
    
    Change-Id: I4cc6260e8376fcf9d4992e8f736c7ea10f7a2c91
    Signed-off-by: Tibor Frank <tifrank@cisco.com>

--- resources/tools/presentation/pal_utils.py
+++ resources/tools/presentation/pal_utils.py
@@ -316,8 +316,27 @@ def convert_csv_to_pretty_txt(csv_file_name, txt_file_name, delimiter=u","):
                 txt_table = prettytable.PrettyTable(row)
             else:
                 txt_table.add_row(row)
-        txt_table.align[u"Test case"] = u"l"
-        txt_table.align[u"RCA"] = u"l"
-    if txt_table:
+    txt_table.align = u"r"
+    txt_table.align[u"Test Case"] = u"l"
+    txt_table.align[u"RCA"] = u"l"
+    txt_table.align[u"RCA1"] = u"l"
+    txt_table.align[u"RCA2"] = u"l"
+    txt_table.align[u"RCA3"] = u"l"
+
+    if not txt_table:
+        return
+
+    if txt_file_name.endswith(u".txt"):
         with open(txt_file_name, u"wt") as txt_file:
             txt_file.write(str(txt_table))
+    elif txt_file_name.endswith(u".rst"):
+
+        with open(txt_file_name, u"wt") as txt_file:
+            txt_file.write(
+                u"\n.. only:: html\n\n"
+                u"    .. csv-table::\n"
+                u"        :header-rows: 1\n"
+                u"        :widths: auto\n"
+                u"        :align: center\n"
+                f"        :file: {csv_file_name.split(u'/')[-1]}\n"
+            )
