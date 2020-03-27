e25550ab0262b401be8bdd52b21cf4745626be14
Jan Gelety <jgelety@cisco.com>
2020-03-27 20:58:22 +0100

    Use mrr-daily test sets for mrr-weekly jobs
    Signed-off-by: Jan Gelety <jgelety@cisco.com>
    Change-Id: I7afb3b733a228a1c5fc44fe79f4183c41c71ed83
    (cherry picked from commit c3b3520d4a2f6e40a450a7b3d502e027746af0dc)

--- resources/libraries/bash/function/common.sh
+++ resources/libraries/bash/function/common.sh
@@ -1,5 +1,5 @@
-# Copyright (c) 2019 Cisco and/or its affiliates.
-# Copyright (c) 2019 PANTHEON.tech and/or its affiliates.
+# Copyright (c) 2020 Cisco and/or its affiliates.
+# Copyright (c) 2020 PANTHEON.tech and/or its affiliates.
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at:
@@ -786,7 +786,8 @@ function select_tags () {
                 ${tfd}/mrr-daily-${FLAVOR}.txt) || die
             ;;
         *"mrr-weekly"* )
-            readarray -t test_tag_array < "${tfd}/mrr-weekly.txt" || die
+            readarray -t test_tag_array <<< $(${sed_nic_sub_cmd} \
+                ${tfd}/mrr-weekly.txt) || die
             ;;
         * )
             if [[ -z "${TEST_TAG_STRING-}" ]]; then
