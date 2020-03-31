9ea766397049a4b4a5a1812e13ecae0e85c00d65
Jan Gelety <jgelety@cisco.com>
2020-03-31 02:59:44 +0200

    CSIT-1597 API cleanup: lisp
    - cover API changes in VPP: https://gerrit.fd.io/r/c/vpp/+/24663
    - update vpp stable to version 20.05-rc0~
    - remove unused L1 and L2 lisp KWs
    
    Change-Id: I2672b6a375ad70c82f331dcc991c145e868108b9
    Signed-off-by: Jan Gelety <jgelety@cisco.com>

--- resources/test_data/lisp/lisp.py
+++ resources/test_data/lisp/lisp.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2019 Cisco and/or its affiliates.
+# Copyright (c) 2020 Cisco and/or its affiliates.
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at:
@@ -14,7 +14,7 @@
 """Test variables of lisp and lispgpe (ip4/ip6) encapsulation test suite."""
 
 # Lisp default global value
-locator_name = b"tst_locator"
+locator_name = u"tst_locator"
 
 # Test configuration data
 tg_if1_ip4= u"6.0.0.2"
