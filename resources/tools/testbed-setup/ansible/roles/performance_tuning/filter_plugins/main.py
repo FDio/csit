5f65fc6854e6191c0ea71e91f3f4a2327923ecac
Peter Mikus <pmikus@cisco.com>
2020-04-03 08:14:50 +0000

    Ansible: Kernel installation
    Signed-off-by: Peter Mikus <pmikus@cisco.com>
    Change-Id: Iee65272169339ed0e5561cf3dee11567335b12be

---
+++ resources/tools/testbed-setup/ansible/roles/performance_tuning/filter_plugins/main.py
@@ -0,0 +1,29 @@
+"""Extra Ansible filters"""
+
+def irqbalance_banned_cpu_mask(
+        processor_cores, processor_count, processor_threads_per_core):
+    """
+    Return best matching kernel version.
+    Args:
+        processor_cores (int): Physical processor unit.
+        processor_counts (int): Processors per physical unit.
+        processor_threads_per_core (int): Threads per physical unit.
+    Returns:
+       str: irqbalance_banned_cpus.
+    """
+    mask = int("1" * 128, 2)
+
+    for i in range(processor_count * processor_threads_per_core):
+        mask &= ~(1 << i * processor_cores)
+
+    import re
+    return ",".join(re.findall('.{1,8}', str(hex(mask))[2:]))
+
+
+class FilterModule(object):
+    """Return filter plugin"""
+
+    @staticmethod
+    def filters():
+        """Return filter"""
+        return {'irqbalance_banned_cpu_mask': irqbalance_banned_cpu_mask}
