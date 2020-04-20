96732c9ce2ed750a232f4a83da39952f9ea535c6
Tibor Frank <tifrank@cisco.com>
2020-04-20 09:07:32 +0200

    Report: Configure report 2001.17
    Change-Id: I809ee89a5798c36d883fd49de75b6f67f1d336f8
    Signed-off-by: Tibor Frank <tifrank@cisco.com>

--- resources/tools/presentation/run_report.sh
+++ resources/tools/presentation/run_report.sh
@@ -38,7 +38,7 @@ export PYTHONPATH=`pwd`:`pwd`/../../../:`pwd`/../../libraries/python
 python pal.py \
     --specification specification.yaml \
     --release ${RELEASE} \
-    --week "16" \
+    --week "17" \
     --logging INFO \
     --force
 
