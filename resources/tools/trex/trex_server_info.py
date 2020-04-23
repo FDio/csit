06260dc3be2df1337b8e39f8d99ec8ebdd3ea9fc
Peter Mikus <pmikus@cisco.com>
2020-04-23 13:35:17 +0000

    Performance: Bump T-Rex to 2.80
    DO NOT RUN YET
    
    DPDK 20.02 - the performance is lower for i40e and higher for Mellanox
    (mlx5)
    
    Signed-off-by: Peter Mikus <pmikus@cisco.com>
    Change-Id: I39923d7152dbf0804303f11b4512ee2780477fac

--- resources/tools/trex/trex_server_info.py
+++ resources/tools/trex/trex_server_info.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-# Copyright (c) 2019 Cisco and/or its affiliates.
+# Copyright (c) 2020 Cisco and/or its affiliates.
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at:
@@ -29,7 +29,7 @@ Functionality:
 import sys
 
 sys.path.insert(
-    0, u"/opt/trex-core-2.73/scripts/automation/trex_control_plane/interactive/"
+    0, u"/opt/trex-core-2.80/scripts/automation/trex_control_plane/interactive/"
 )
 from trex.stl.api import *
 
--- resources/tools/trex/trex_stateless_stop.py
+++ resources/tools/trex/trex_stateless_stop.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-# Copyright (c) 2019 Cisco and/or its affiliates.
+# Copyright (c) 2020 Cisco and/or its affiliates.
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at:
@@ -34,7 +34,7 @@ import sys
 from collections import OrderedDict  # Needed to parse xstats representation.
 
 sys.path.insert(
-    0, u"/opt/trex-core-2.73/scripts/automation/trex_control_plane/interactive/"
+    0, u"/opt/trex-core-2.80/scripts/automation/trex_control_plane/interactive/"
 )
 from trex.stl.api import *
 
