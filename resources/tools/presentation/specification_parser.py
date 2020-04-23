aadb67faae1d4f27dfaaeb2dcc191500af1b6dea
Tibor Frank <tifrank@cisco.com>
2020-04-23 08:30:33 +0200

    PAL: Reverse download order for trending
    Change-Id: Idd6bf8b89ce0fb5ebf9734b39a8f6058861b8ec5
    Signed-off-by: Tibor Frank <tifrank@cisco.com>

--- resources/tools/presentation/specification_parser.py
+++ resources/tools/presentation/specification_parser.py
@@ -533,9 +533,7 @@ class Specification:
                     except ValueError:
                         # defined as a range <start, build_type>
                         build_end = self._get_build_number(job, build_end)
-                    builds = [x for x in range(builds[u"start"],
-                                               build_end + 1)
-                              if x not in builds.get(u"skip", list())]
+                    builds = [x for x in range(builds[u"start"], build_end + 1)]
                     self.configuration[u"data-sets"][set_name][job] = builds
                 elif isinstance(builds, list):
                     for idx, item in enumerate(builds):
@@ -588,16 +586,25 @@ class Specification:
 
             for job, builds in self._cfg_yaml[idx][u"builds"].items():
                 if builds:
+                    max_builds = builds.get(u"max-builds", None)
+                    reverse = bool(builds.get(u"reverse", False))
                     if isinstance(builds, dict):
                         build_end = builds.get(u"end", None)
                         try:
                             build_end = int(build_end)
                         except ValueError:
                             # defined as a range <start, build_type>
+                            if build_end in (u"lastCompletedBuild",
+                                             u"lastSuccessfulBuild"):
+                                reverse = True
                             build_end = self._get_build_number(job, build_end)
                         builds = [x for x in range(builds[u"start"],
                                                    build_end + 1)
                                   if x not in builds.get(u"skip", list())]
+                    if reverse:
+                        builds.reverse()
+                    if max_builds and max_builds < len(builds):
+                        builds = builds[:max_builds]
                     self._specification[u"input"][u"builds"][job] = list()
                     for build in builds:
                         self._specification[u"input"][u"builds"][job]. \
@@ -608,6 +615,9 @@ class Specification:
                         f"No build is defined for the job {job}. Trying to "
                         f"continue without it."
                     )
+
+            logging.info(self._specification[u"input"])
+
         except KeyError:
             raise PresentationError(u"No data to process.")
 
