062a26b812bbcacce4a0e58059dc6f12a19b8317
Jan Gelety <jgelety@cisco.com>
2020-05-05 10:59:37 +0200

    Perf: NAT44 endpoint-dependent mode
    Change-Id: I7dd5fe8637020b4b120563fca716e8160d7291ef
    Signed-off-by: Jan Gelety <jgelety@cisco.com>

--- GPL/tools/trex/trex_astf_profile.py
+++ GPL/tools/trex/trex_astf_profile.py
@@ -112,6 +112,7 @@ def simple_burst(
     l7_data = u""
     stats = dict()
     stats_sampling = 1.0
+    approximated_duration = 0
 
     # Read the profile.
     try:
@@ -154,7 +155,7 @@ def simple_burst(
                 for warning in client.get_warnings():
                     print(warning)
 
-            client.reset(ports=ports)
+            client.reset()
 
             print(u"##### Warmup Statistics #####")
             print(json.dumps(stats, indent=4, separators=(u",", u": ")))
@@ -176,7 +177,7 @@ def simple_burst(
 
         # Choose CPS and start traffic.
         client.start(
-            mult=mult, duration=duration, nc=True,
+            mult=mult, duration=duration, nc=False,
             latency_pps=mult if latency else 0, client_mask=2**len(ports)-1
         )
         time_start = time.monotonic()
@@ -206,12 +207,13 @@ def simple_burst(
                 for warning in client.get_warnings():
                     print(warning)
 
-            client.reset(ports=ports)
+            client.reset()
 
             print(u"##### Statistics #####")
             print(json.dumps(stats, indent=4, separators=(u",", u": ")))
 
             # TODO: check stats format
+            approximated_duration = list(sorted(stats.keys()))[-1]
             stats = stats[sorted(stats.keys())[-1]]
             lost_a = stats[port_0][u"opackets"] - stats[port_1][u"ipackets"]
             if traffic_directions > 1:
@@ -292,6 +294,7 @@ def simple_burst(
                 print(
                     f"rate={mult!r}, totalReceived={total_rcvd}, "
                     f"totalSent={total_sent}, frameLoss={lost_a + lost_b}, "
+                    f"approximatedDuration={approximated_duration}, "
                     f"latencyStream0(usec)={lat_a}, "
                     f"latencyStream1(usec)={lat_b}, "
                     f"latencyHistStream0={lat_a_hist}, "
