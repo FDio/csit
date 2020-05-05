8fb827999dbba7ef1fb6e595e716a275127c1f63
Jan Gelety <jgelety@cisco.com>
2020-05-05 10:59:37 +0200

    Perf: NAT44 endpoint-dependent mode
    Change-Id: I7dd5fe8637020b4b120563fca716e8160d7291ef
    Signed-off-by: Jan Gelety <jgelety@cisco.com>

--- GPL/traffic_profiles/trex/trex-astf-ethip4udp-1024h.py
+++ GPL/traffic_profiles/trex/trex-astf-ethip4udp-1024h.py
@@ -38,26 +38,16 @@ class TrafficProfile(TrafficProfileBaseClass):
 
         super(TrafficProfileBaseClass, self).__init__()
 
-        # Number of requests per HTTP transaction.
-        self.requests = kwargs.get('req', 1)
-        # Number of transactions per HTTP connection.
-        self.transaction_per_conn = kwargs.get('tpc', 1)
-        # Use TCP RST instead of FIN+ACK.
-        self.tcp_reset = kwargs.get('rst', False)
-        # HTTP connection.
-        self.http_conn = kwargs.get('con', 'close')
-
         # IPs used in packet headers.
-        self.p1_src_start_ip = u"20.0.0.0"
-        self.p1_src_end_ip = u"20.0.0.10"
-        self.p1_dst_start_ip = u"30.0.0.0"
-
-        self.p2_src_start_ip = u"20.20.20.1"
-        self.p2_src_end_ip = u"20.20.20.254"
-        self.p2_dst_start_ip = u"10.10.10.1"
+        self.p1_src_start_ip = u"192.168.0.0"
+        self.p1_src_end_ip = u"192.168.3.255"
+        self.p1_dst_start_ip = u"20.0.0.0"
+        self.p1_dst_end_ip = u"20.0.3.255"
 
         self.udp_req = u"GET"
         self.udp_res = u"ACK"
+
+        self.headers_size = 42  # 14B l2 + 20B ipv4 + 8B udp
 
     def define_profile(self):
         """Define profile to be used by advanced stateful traffic generator.
@@ -70,14 +60,16 @@ class TrafficProfile(TrafficProfileBaseClass):
         :rtype: tuple
         """
         # client commands
+        self.udp_req += self._gen_padding(self.headers_size + len(self.udp_req))
         prog_c = ASTFProgram(stream=False)
-        prog_c.send_msg(self.udp_req, size=self.framesize)
+        prog_c.send_msg(self.udp_req)  # size and fill not supported in v2.73
         prog_c.recv_msg(1)
 
         # server commands
+        self.udp_res += self._gen_padding(self.headers_size + len(self.udp_res))
         prog_s = ASTFProgram(stream=False)
         prog_s.recv_msg(1)
-        prog_s.send_msg(self.udp_res, size=self.framesize)
+        prog_s.send_msg(self.udp_res)
 
         # ip generators
         ip_gen_c = ASTFIPGenDist(
@@ -85,7 +77,7 @@ class TrafficProfile(TrafficProfileBaseClass):
             distribution=u"seq"
         )
         ip_gen_s = ASTFIPGenDist(
-            ip_range=[self.p2_src_start_ip, self.p2_src_end_ip],
+            ip_range=[self.p1_dst_start_ip, self.p1_dst_end_ip],
             distribution=u"seq"
         )
         ip_gen = ASTFIPGen(
@@ -94,15 +86,18 @@ class TrafficProfile(TrafficProfileBaseClass):
             dist_server=ip_gen_s
         )
 
+        # server association
+        s_assoc = ASTFAssociation(rules=ASTFAssociationRule(port=8080))
+
         # template
-        temp_c = ASTFTCPClientTemplate(program=prog_c, ip_gen=ip_gen)
-        temp_s = ASTFTCPServerTemplate(program=prog_s)  # TODO: default association ?
+        temp_c = ASTFTCPClientTemplate(program=prog_c, ip_gen=ip_gen, port=8080)
+        temp_s = ASTFTCPServerTemplate(program=prog_s, assoc=s_assoc)  # TODO: default association ?
         template = ASTFTemplate(client_template=temp_c, server_template=temp_s)
 
         return ip_gen, template
 
 
-        # TODO: imix packet size ?
+        # TODO: packet size ?
 
 
 def register():
