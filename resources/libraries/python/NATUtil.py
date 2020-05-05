5b9acc293fd438ce46b1185051263837718e15eb
Jan Gelety <jgelety@cisco.com>
2020-05-05 10:59:37 +0200

    Perf: NAT44 endpoint-dependent mode
    Change-Id: I7dd5fe8637020b4b120563fca716e8160d7291ef
    Signed-off-by: Jan Gelety <jgelety@cisco.com>

--- resources/libraries/python/NATUtil.py
+++ resources/libraries/python/NATUtil.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2019 Cisco and/or its affiliates.
+# Copyright (c) 2020 Cisco and/or its affiliates.
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at:
@@ -14,16 +14,17 @@
 """NAT utilities library."""
 
 from pprint import pformat
-from socket import AF_INET, inet_pton
 from enum import IntEnum
 
+from ipaddress import IPv4Address
 from robot.api import logger
 
+from resources.libraries.python.Constants import Constants
 from resources.libraries.python.InterfaceUtil import InterfaceUtil
 from resources.libraries.python.PapiExecutor import PapiSocketExecutor
 
 
-class NATConfigFlags(IntEnum):
+class NatConfigFlags(IntEnum):
     """Common NAT plugin APIs"""
     NAT_IS_NONE = 0x00
     NAT_IS_TWICE_NAT = 0x01
@@ -36,11 +37,42 @@ class NATConfigFlags(IntEnum):
     NAT_IS_EXT_HOST_VALID = 0x80
 
 
+class NatAddrPortAllocAlg(IntEnum):
+    """NAT Address and port assignment algorithms."""
+    NAT_ALLOC_ALG_DEFAULT = 0
+    NAT_ALLOC_ALG_MAP_E = 1
+    NAT_ALLOC_ALG_PORT_RANGE = 2
+
+
 class NATUtil:
     """This class defines the methods to set NAT."""
 
     def __init__(self):
         pass
+
+    @staticmethod
+    def set_nat44_interface(node, interface, flag):
+        """Set inside and outside interfaces for NAT44.
+
+        :param node: DUT node.
+        :param interface: Inside interface.
+        :param flag: Interface NAT configuration flag name.
+        :type node: dict
+        :type interface: str
+        :type flag: str
+        """
+        cmd = u"nat44_interface_add_del_feature"
+
+        err_msg = f"Failed to set {flag} interface {interface} for NAT44 " \
+            f"on host {node[u'host']}"
+        args_in = dict(
+            sw_if_index=InterfaceUtil.get_sw_if_index(node, interface),
+            is_add=1,
+            flags=getattr(NatConfigFlags, flag).value
+        )
+
+        with PapiSocketExecutor(node) as papi_exec:
+            papi_exec.add(cmd, **args_in).get_reply(err_msg)
 
     @staticmethod
     def set_nat44_interfaces(node, int_in, int_out):
@@ -53,31 +85,8 @@ class NATUtil:
         :type int_in: str
         :type int_out: str
         """
-        cmd = u"nat44_interface_add_del_feature"
-
-        int_in_idx = InterfaceUtil.get_sw_if_index(node, int_in)
-        err_msg = f"Failed to set inside interface {int_in} for NAT44 " \
-            f"on host {node[u'host']}"
-        args_in = dict(
-            sw_if_index=int_in_idx,
-            is_add=1,
-            flags=getattr(NATConfigFlags, u"NAT_IS_INSIDE").value
-        )
-
-        with PapiSocketExecutor(node) as papi_exec:
-            papi_exec.add(cmd, **args_in).get_reply(err_msg)
-
-        int_out_idx = InterfaceUtil.get_sw_if_index(node, int_out)
-        err_msg = f"Failed to set outside interface {int_out} for NAT44 " \
-            f"on host {node[u'host']}"
-        args_in = dict(
-            sw_if_index=int_out_idx,
-            is_add=1,
-            flags=getattr(NATConfigFlags, u"NAT_IS_OUTSIDE").value
-        )
-
-        with PapiSocketExecutor(node) as papi_exec:
-            papi_exec.add(cmd, **args_in).get_reply(err_msg)
+        NATUtil.set_nat44_interface(node, int_in, u"NAT_IS_INSIDE")
+        NATUtil.set_nat44_interface(node, int_out, u"NAT_IS_OUTSIDE")
 
     @staticmethod
     def set_nat44_deterministic(node, ip_in, subnet_in, ip_out, subnet_out):
@@ -99,10 +108,82 @@ class NATUtil:
             f"on host {node[u'host']}"
         args_in = dict(
             is_add=True,
-            in_addr=inet_pton(AF_INET, str(ip_in)),
+            in_addr=IPv4Address(str(ip_in)).packed,
             in_plen=int(subnet_in),
-            out_addr=inet_pton(AF_INET, str(ip_out)),
+            out_addr=IPv4Address(str(ip_out)).packed,
             out_plen=int(subnet_out)
+        )
+
+        with PapiSocketExecutor(node) as papi_exec:
+            papi_exec.add(cmd, **args_in).get_reply(err_msg)
+
+    @staticmethod
+    def set_nat44_address_range(
+            node, start_ip, end_ip, vrf_id=Constants.BITWISE_NON_ZERO,
+            flag=u"NAT_IS_NONE"):
+        """Set NAT44 address range.
+
+        :param node: DUT node.
+        :param start_ip: IP range start.
+        :param end_ip: IP range end.
+        :param vrf_id: VRF index (Optional).
+        :param flag: NAT flag name.
+        :type node: dict
+        :type start_ip: str
+        :type end_ip: str
+        :type vrf_id: int
+        :type flag: str
+        """
+        cmd = u"nat44_add_del_address_range"
+        err_msg = f"Failed to set NAT44 address range on host {node[u'host']}"
+        args_in = dict(
+            is_add=True,
+            first_ip_address=IPv4Address(str(start_ip)).packed,
+            last_ip_address=IPv4Address(str(end_ip)).packed,
+            vrf_id=vrf_id,
+            flags=getattr(NatConfigFlags, flag).value
+        )
+
+        with PapiSocketExecutor(node) as papi_exec:
+            papi_exec.add(cmd, **args_in).get_reply(err_msg)
+
+    @staticmethod
+    def set_nat_addr_port_alloc_alg(
+            node, alg, start_port, end_port, psid_offset=0, psid_length=0,
+            psid=0):
+        """Set NAT address and port assignment algorithm.
+
+        :param node: DUT node.
+        :param alg: Address and port assignment algorithm:
+            DEFAULT, MAP_E, PORT_RANGE.
+        :param start_port: Port range start.
+        :param end_port: Port range end.
+        :param psid_offset: Number of offset bits (Optional, valid oly for MAP-E
+            algorithm).
+        :param psid_length: Length of PSID (Optional, valid oly for MAP-E
+            algorithm).
+        :param psid: Port Set Identifier value (Optional, valid oly for MAP-E
+            algorithm).
+        :type node: dict
+        :type alg: str
+        :type start_port: int
+        :type end_port: int
+        :type psid_offset: int
+        :type psid_length: int
+        :type psid: int
+        """
+        cmd = u"nat_set_addr_and_port_alloc_alg"
+        err_msg = f"Failed to set NAT address and port assignment algorithm " \
+            f"on host {node[u'host']}"
+        args_in = dict(
+            alg=getattr(
+                NatAddrPortAllocAlg, f"NAT_ALLOC_ALG_{alg.upper()}"
+            ).value,
+            psid_offset=int(psid_offset),
+            psid_length=int(psid_length),
+            psid=int(psid),
+            start_port=int(start_port),
+            end_port=int(end_port)
         )
 
         with PapiSocketExecutor(node) as papi_exec:
