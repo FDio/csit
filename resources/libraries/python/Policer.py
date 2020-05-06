57901bcc04fa825f46854b0834d8ad36ab57006c
Vratko Polak <vrpolak@cisco.com>
2020-05-06 14:00:43 +0200

    PAPI: Add fast modes for big scale tests
    + New way of connecting in async mode.
    + Optionally generate data using template.
     - Fast execution does not support a mix of different commands (yet).
    + Optionally skip any reply processing.
    - Accesses private parts of VPPApiClient.transport class.
     - Introducing and using official methods will be done in later Changes.
     + Private access is needed for testing old VPP builds (e.g. bisect).
    + Update big scale call sites to the new pattern.
     + Including medium scale libraries, such as TestConfig.
     + The pattern does not need history bool.
    + Make IPsec keys type bytes.
     + Make sure get_key still generates letter-only keys.
     + Make sure subsequent keys differ in each byte.
      - Without that data generator misidentifis which bytes belong to key.
    + Re-introduce raise_from, it is still needed (after Python3 migration).
    + All VAT related files are removed.
    + Fix parameter raddr_range not being previously used in papi code.
    + Refactor various methods to keep pylint happy.
     - The new pattern adds local variables.
     - An alternative was to create a function to handle the pattern,
       but that would need injectable generator of args,
       which would make the code less readable.
    
    Ticket: CSIT-1547 CSIT-1671
    
    Change-Id: I8264d9cfcb5e145a9b1462c70342e5a26f7ab022
    Signed-off-by: Vratko Polak <vrpolak@cisco.com>

--- resources/libraries/python/Policer.py
+++ resources/libraries/python/Policer.py
@@ -16,7 +16,7 @@
 from enum import IntEnum
 
 from resources.libraries.python.Constants import Constants
-from resources.libraries.python.PapiExecutor import PapiSocketExecutor
+from resources.libraries.python.PapiSocketExecutor import PapiSocketExecutor
 from resources.libraries.python.topology import Topology
 
 
