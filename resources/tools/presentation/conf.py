a969b966af9cd033910fcfafb5885396602e9bd0
Tibor Frank <tifrank@cisco.com>
2020-05-07 10:19:56 +0200

    Report: Configure report 2001.20
    Change-Id: Ib0a3190dfd5b216aa9ff93d751fa48cd42cef88f
    Signed-off-by: Tibor Frank <tifrank@cisco.com>

--- resources/tools/presentation/conf.py
+++ resources/tools/presentation/conf.py
@@ -46,7 +46,7 @@ source_suffix = [u'.rst', u'.md']
 master_doc = u'index'
 
 # General information about the project.
-report_week = u'19'
+report_week = u'20'
 project = u'FD.io CSIT-2001.{week}'.format(week=report_week)
 copyright = u'2020, FD.io'
 author = u'FD.io CSIT'
