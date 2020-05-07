a9a620af4d321d9ae2884c01970d7da1585730f1
Jan Gelety <jgelety@cisco.com>
2020-05-07 04:37:19 +0200

    T-Rex: Add advanced stateful mode
    Change-Id: Ib0dc5f2919c370753335f6446860683dc4b12d93
    Signed-off-by: Jan Gelety <jgelety@cisco.com>

--- GPL/tools/trex/trex_astf_assert.py
+++ GPL/tools/trex/trex_astf_assert.py
@@ -13,17 +13,16 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-"""This script uses T-REX stateless API to drive t-rex instance.
+"""This script uses T-Rex advanced stateful API to drive T-Rex instance.
 
 Requirements:
-- T-REX: https://github.com/cisco-system-traffic-generator/trex-core
- - compiled and running T-REX process (eg. ./t-rex-64 -i)
- - trex.stl.api library
-- Script must be executed on a node with T-REX instance
+- T-Rex: https://github.com/cisco-system-traffic-generator/trex-core
+ - compiled and running T-Rex process (eg. ./t-rex-64 -i)
+ - trex.astf.api library
+- Script must be executed on a node with T-Rex instance.
 
 Functionality:
-1. Verify the API functionality and get server information
-
+1. Verify the API functionality and get server information.
 """
 
 import sys
@@ -31,19 +30,19 @@ import sys
 sys.path.insert(
     0, u"/opt/trex-core-2.73/scripts/automation/trex_control_plane/interactive/"
 )
-from trex.stl.api import *
+from trex.astf.api import *
 
 
 def main():
     """Check server info and quit."""
-    client = STLClient()
+    client = ASTFClient()
     try:
         # connect to server
         client.connect()
 
         # get server info
         print(client.get_server_system_info())
-    except STLError as ex_error:
+    except TRexError as ex_error:
         print(ex_error, file=sys.stderr)
         sys.exit(1)
     finally:
