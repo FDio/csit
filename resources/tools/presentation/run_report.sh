5859eade8c3e626f055d4a47da6fbd852952859f
Tibor Frank <tifrank@cisco.com>
2020-05-11 11:22:25 +0000

    Revert "Report: Configure report 2001.20"
    This reverts commit 996e23b4deb530e82d6beaa958452f3315f0eb69.
    
    Reason for revert: buggy
    
    Change-Id: Ic948637c1e43f3bc98eac8f3d94d4e5ce95d93b2
    Signed-off-by: Tibor Frank <tifrank@cisco.com>
    (cherry picked from commit 10b63157fbd6c9bb4e9da146e3a0e250841fd468)
    (cherry picked from commit 7d224d924dc6dff646c171fe60d6fb7d3cce6c23)

--- resources/tools/presentation/run_report.sh
+++ resources/tools/presentation/run_report.sh
@@ -38,11 +38,7 @@ export PYTHONPATH=`pwd`:`pwd`/../../../:`pwd`/../../libraries/python
 python pal.py \
     --specification specification.yaml \
     --release ${RELEASE} \
-<<<<<<< HEAD   (d5fe67 Report: Add data)
-    --week "18" \
-=======
     --week "20" \
->>>>>>> CHANGE (f4c460 Report: Configure report 2001.20)
     --logging INFO \
     --force
 
