eb15d933ea0b68809095775f7279e77c165ebe1c
Vratko Polak <vrpolak@cisco.com>
2020-05-12 14:04:58 +0200

    DO_NOT_MERGE Override stable VPP version
    During manual CSIT bisect it is good to keep VPP version pinned.
    For come commits, the version matches,
    doing whitespace edit to not lose this Change on rebase.
    
    + Do path-on-patch (only test current, do not compare per-patch).
    + Do not fail on CRC mismatch (needs to be explicit for older CSIT).
    + Do not force MRR (nor NIC).
    + Bump TRex version (some versions are not installed on TGs right now).
     - Do not touch ansible files, even if related to TRex.
    
    Change-Id: I7c107516f47542a27be8cfcfebf0cae13d61fcb2
    Signed-off-by: Vratko Polak <vrpolak@cisco.com>

--- resources/libraries/python/Constants.py
+++ resources/libraries/python/Constants.py
@@ -188,7 +188,10 @@ class Constants(object):
     TREX_INSTALL_VERSION = '2.54'
 
     # TRex install directory
-    TREX_INSTALL_DIR = '/opt/trex-core-2.54'
+    TREX_INSTALL_DIR = u"/opt/trex-core-2.73"
+
+    # TRex limit memory.
+    TREX_LIMIT_MEMORY = get_int_from_env(u"TREX_LIMIT_MEMORY ", 4096)
 
     # Honeycomb directory location at topology nodes:
     REMOTE_HC_DIR = '/opt/honeycomb'
@@ -229,7 +232,9 @@ class Constants(object):
     DUT1_UUID = get_str_from_env("DUT1_UUID", "")
 
     # Global "kill switch" for CRC checking during runtime.
-    FAIL_ON_CRC_MISMATCH = get_optimistic_bool_from_env("FAIL_ON_CRC_MISMATCH")
+    FAIL_ON_CRC_MISMATCH = False  # get_pessimistic_bool_from_env(
+#        u"FAIL_ON_CRC_MISMATCH"
+#    )
 
     # Mapping from NIC name to its bps limit.
     # TODO: Implement logic to lower limits to TG NIC or software. Or PCI.
