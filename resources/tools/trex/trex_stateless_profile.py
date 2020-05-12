b2aefea7ea147172656db11495ec48e49dc544a7
Vratko Polak <vrpolak@cisco.com>
2020-05-12 14:58:38 +0200

    DO_NOT_MERGE Override stable VPP version
    During manual CSIT bisect it is good to keep VPP version pinned.
    For come commits, the version matches,
    doing whitespace edit to not lose this Change on rebase.
    
    + Do path-on-patch (only test current, do not compare per-patch).
    + Do not fail on CRC mismatch (needs to be explicit for older CSIT).
    + Do not force MRR (nor NIC).
    + Bump TRex version (some versions are not installed on TGs right now).
     - Do not touch ansible files, even if related to TRex.
    
    Change-Id: I7c107516f47542a27be8cfcfebf0cae13d61fcb2
    Signed-off-by: Vratko Polak <vrpolak@cisco.com>

--- resources/tools/trex/trex_stateless_profile.py
+++ resources/tools/trex/trex_stateless_profile.py
@@ -22,8 +22,10 @@ import sys
 import argparse
 import json
 
-sys.path.insert(0, "/opt/trex-core-2.54/scripts/automation/"
-                   "trex_control_plane/interactive/")
+sys.path.insert(
+    0, u"/opt/trex-core-2.73/scripts/automation/trex_control_plane/interactive/"
+)
+
 from trex.stl.api import *
 
 
