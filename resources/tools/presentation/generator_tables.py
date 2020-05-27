0c1cc15e2e04950d9fa99f11dca3d5323ff1a7c6
Tibor Frank <tifrank@cisco.com>
2020-05-27 10:57:46 +0200

    PAL: Fix table_comparison again
    Change-Id: Ie408afd7bb8390ebe2fc90eca6daf8da44d22e3b
    Signed-off-by: Tibor Frank <tifrank@cisco.com>
    (cherry picked from commit 3ea4fcebabfaac866d469a7b4355ac8df7732a0f)

--- resources/tools/presentation/generator_tables.py
+++ resources/tools/presentation/generator_tables.py
@@ -1702,7 +1702,9 @@ def table_comparison(table, input_data):
     for line in tbl_cmp_lst:
         row = [line[0], ]
         for idx, itm in enumerate(line[1:]):
-            if itm is None:
+            if itm is None or not isinstance(itm, dict) or \
+                    itm.get(u'mean', None) is None or \
+                    itm.get(u'stdev', None) is None:
                 new_itm = u"NT"
             else:
                 if idx < len(cols):
