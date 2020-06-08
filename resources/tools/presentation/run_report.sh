afca858752d4910edaad119d7921367b2b7081f3
Tibor Frank <tifrank@cisco.com>
2020-06-08 12:14:09 +0200

    Report 2005: Add data, configure 2005.24
    Change-Id: Iabd8dadda4ee7e0b9fc7044f4fc6addf8a106ab5
    Signed-off-by: Tibor Frank <tifrank@cisco.com>
    (cherry picked from commit ba7130cebc9bc6ef66b9eb4f0e7d888462e10e48)

--- resources/tools/presentation/run_report.sh
+++ resources/tools/presentation/run_report.sh
@@ -38,7 +38,7 @@ export PYTHONPATH=`pwd`:`pwd`/../../../:`pwd`/../../libraries/python
 python pal.py \
     --specification specification.yaml \
     --release ${RELEASE} \
-    --week "23" \
+    --week "24" \
     --logging INFO \
     --force
 
