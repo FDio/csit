ac20f1386cc9e0a03c79a1a37e9199e7e7e509ed
Tibor Frank <tifrank@cisco.com>
2020-06-25 21:02:50 +0200

    Report 2001: Configure rls2001.26
    Signed-off-by: Tibor Frank <tifrank@cisco.com>
    Change-Id: I6fe7168c74cd12d25567ffb9f18c0441b45391f3

--- resources/tools/presentation/run_report.sh
+++ resources/tools/presentation/run_report.sh
@@ -38,7 +38,7 @@ export PYTHONPATH=`pwd`:`pwd`/../../../:`pwd`/../../libraries/python
 python pal.py \
     --specification specification.yaml \
     --release ${RELEASE} \
-    --week "24" \
+    --week "26" \
     --logging INFO \
     --force
 
