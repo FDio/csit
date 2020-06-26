966bfc7ab560fa00dfec913e8e04fd01301ec7ae
Jan Gelety <jgelety@cisco.com>
2020-06-26 19:25:28 +0200

    Perf: NAT44-ED tcp tests
    Change-Id: I973f97da131566042e55f35df8f9869cd401de1e
    Signed-off-by: Jan Gelety <jgelety@cisco.com>
    Signed-off-by: Maros Mullner <maros.mullner@pantheon.tech>

--- resources/libraries/python/Constants.py
+++ resources/libraries/python/Constants.py
@@ -188,7 +188,7 @@ class Constants:
     DOCKER_SUT_IMAGE_UBUNTU_ARM = u"snergster/csit-arm-sut:latest"
 
     # TRex install directory
-    TREX_INSTALL_DIR = u"/opt/trex-core-2.73"
+    TREX_INSTALL_DIR = u"/opt/trex-core-2.82"
 
     # TRex pcap files directory
     TREX_PCAP_DIR = f"{TREX_INSTALL_DIR}/scripts/avl"
@@ -197,7 +197,7 @@ class Constants:
     TREX_LIMIT_MEMORY = get_int_from_env(u"TREX_LIMIT_MEMORY", 8192)
 
     # TRex number of cores
-    TREX_CORE_COUNT = get_int_from_env(u"TREX_CORE_COUNT", 8)
+    TREX_CORE_COUNT = get_int_from_env(u"TREX_CORE_COUNT", 16)
 
     # Trex force start regardless ports state
     TREX_SEND_FORCE = get_pessimistic_bool_from_env(u"TREX_SEND_FORCE")
