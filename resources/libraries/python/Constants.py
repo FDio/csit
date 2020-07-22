61d57b41f0ba16f2533b2c98f31435b8c51afd6d
Xiaolong Jiang <xiaolongx.jiang@intel.com>
2020-07-22 17:30:21 +0800

    vsap: configure and run test case
    Configure and run VPP and Nginx at DUT.
    Run ab at TG.
    
    Signed-off-by: Xiaolong Jiang <xiaolongx.jiang@intel.com>
    Change-Id: I416e82f239b3adea4943e2100f5d3cc9ac59f097

--- resources/libraries/python/Constants.py
+++ resources/libraries/python/Constants.py
@@ -130,6 +130,9 @@ class Constants:
     # shell scripts location
     RESOURCES_LIB_SH = u"resources/libraries/bash"
 
+    # shell scripts entry location
+    RESOURCES_LIB_ENTRY = u"resources/libraries/bash/entry"
+
     # Python API provider location
     RESOURCES_PAPI_PROVIDER = u"resources/tools/papi/vpp_papi_provider.py"
 
