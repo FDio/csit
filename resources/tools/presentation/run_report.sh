40537fb9c1045de244424ea989bb0e84d8640b8b
Tibor Frank <tifrank@cisco.com>
2020-07-23 07:54:18 +0200

    Report 2005: Configure rls2005.31, add data
    Change-Id: I87e07e334783e10dc863194f240e5dbcbf141915
    Signed-off-by: Tibor Frank <tifrank@cisco.com>

--- resources/tools/presentation/run_report.sh
+++ resources/tools/presentation/run_report.sh
@@ -38,7 +38,7 @@ export PYTHONPATH=`pwd`:`pwd`/../../../:`pwd`/../../libraries/python
 python pal.py \
     --specification specification.yaml \
     --release ${RELEASE} \
-    --week "30" \
+    --week "31" \
     --logging INFO \
     --force
 
