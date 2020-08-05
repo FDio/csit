959817353670be9d1b1502515bdeb9c8d201e4a0
Tibor Frank <tifrank@cisco.com>
2020-08-05 06:52:06 +0200

    Report 2005: Add data, configure rls2005.32
    Change-Id: I79129cac48c60a959e4dc1ebfc9d63b9b230e933
    Signed-off-by: Tibor Frank <tifrank@cisco.com>

--- resources/tools/presentation/conf.py
+++ resources/tools/presentation/conf.py
@@ -46,7 +46,7 @@ source_suffix = [u'.rst', u'.md']
 master_doc = u'index'
 
 # General information about the project.
-report_week = u'31'
+report_week = u'32'
 project = u'FD.io CSIT-2005.{week}'.format(week=report_week)
 copyright = u'2020, FD.io'
 author = u'FD.io CSIT'
--- resources/tools/presentation/run_report.sh
+++ resources/tools/presentation/run_report.sh
@@ -38,7 +38,7 @@ export PYTHONPATH=`pwd`:`pwd`/../../../:`pwd`/../../libraries/python
 python pal.py \
     --specification specification.yaml \
     --release ${RELEASE} \
-    --week "31" \
+    --week "32" \
     --logging INFO \
     --force
 
