9856e1e876cbb66b5f200630e295e856ce242a9f
Vratko Polak <vrpolak@cisco.com>
2020-09-14 14:25:56 +0200

    Support existing test types with ASTF
    FIXME: Update commit message.
    
    + Introduce DUT resetter support.
     + Add resetters for the two NAT modes.
     + Add an option for suites to not reset NAT state between trials.
     - Those suites need a warmup trial instead, not added.
     - Only added a FIXME instead a full suite documentation update.
    - Disallow warmup for ASTF.
     - As that does not call resetter before real measurement.
    + Add suite variable n_transactions.
     + If nonzero, trial attempts to run that many transactions.
    + TPS parameter no longer hardcoded.
     + MRR tests compute it from usual pps max rate and packet per transactions.
     + MLRsearch and PLRsearch are searching for that value.
     + In profile driver, the value is calle multiplier.
    + Together it means recomputed duration can be far from exprectation.
     + Added duration limit.
      + For cases where we do not need to wait for all transactions.
     + Skipping latency trials for ASTF.
     + Few parameters to govern time computation.
     + Soak still waits for time the numeric computation needs.
     + Use different time parameters in MLRsearch to save time.
      + But still avoid using results of initial phase directly.
    + Progress report during trial:
     + ipackets and opackets difference each 0.1s.
     + Activity check is even faster at 0.01s period.
    + Use semicolon to delimit profile driver output fields.
    - Some trials do not end right when traffic stops.
    + Hard stop when traffic takes longer than computed duration.
     + Still produces results for search algorithms.
     + Transaction attempted set to n_transactions.
    + Only CPS profiles supported.
     + Both UDP and TCP.
     - No delays to separate session teardown.
     + Use big enough keepalive period.
    + Introduce two secondary receive rates:
     + Approximated receive rate:
      + Uses measured duration instead of target duration.
     + Relative receive rate.
      + Uses target rate and loss ratio.
     - Not sure their usage is best.
    + Use geometric distribution in soak.
     + Helps agains math errors.
    + Move more values from arguments to test / instance variables.
     + Separate internal traffic function to call when values are stored.
     + Move some other blocks in TrafficGenerator around.
     + Move explicit type conversions to earlier occurences.
    + Support different counters for pass count depending on test type.
    + Change test message to use the correct unit (pps or cps).
    + Put scale values to suite variables instead of hardcoding them.
     + Compute derived values.
    + Add -cps to suites with limited n_transactions (if missing).
    + Few fixmes to update api crc file comments.
    + Tolerate somewhat situations where traffic does not start.
    
    Change-Id: I84729355edbec051298a9de1162107f88ff5737d
    Signed-off-by: Vratko Polak <vrpolak@cisco.com>

--- resources/libraries/python/MLRsearch/ReceiveRateMeasurement.py
+++ resources/libraries/python/MLRsearch/ReceiveRateMeasurement.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2019 Cisco and/or its affiliates.
+# Copyright (c) 2020 Cisco and/or its affiliates.
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at:
@@ -14,21 +14,37 @@
 """Module defining ReceiveRateMeasurement class."""
 
 
+import logging
+
+
 class ReceiveRateMeasurement:
     """Structure defining the result of single Rr measurement."""
 
-    def __init__(self, duration, target_tr, transmit_count, loss_count):
+    def __init__(
+            self, duration, target_tr, transmit_count, loss_count,
+            approximated_duration=0.0):
         """Constructor, normalize primary and compute secondary quantities.
+
+        If approximated_duration is nonzero, it is stored.
+        If approximated_duration is zero, duration value is stored.
+        Either way, additional secondary quantities are computed
+        from the store value.
+
+        If there is zero transmit_count, fractions are set to zero.
+
+        TODO: Use None instead of zero?
 
         :param duration: Measurement duration [s].
         :param target_tr: Target transmit rate [pps].
             If bidirectional traffic is measured, this is bidirectional rate.
         :param transmit_count: Number of packets transmitted [1].
         :param loss_count: Number of packets transmitted but not received [1].
+        :param approximated_duration: Estimate of the actual time of the trial.
         :type duration: float
         :type target_tr: float
         :type transmit_count: int
         :type loss_count: int
+        :type approximated_duration: float
         """
         self.duration = float(duration)
         self.target_tr = float(target_tr)
@@ -38,17 +54,37 @@ class ReceiveRateMeasurement:
         self.transmit_rate = transmit_count / self.duration
         self.loss_rate = loss_count / self.duration
         self.receive_rate = self.receive_count / self.duration
-        self.loss_fraction = float(self.loss_count) / self.transmit_count
-        # TODO: Do we want to store also the real time (duration + overhead)?
+        self.loss_fraction = (
+            float(self.loss_count) / self.transmit_count
+            if self.transmit_count > 0 else 0.0
+        )
+        self.receive_fraction = (
+            float(self.receive_count) / self.transmit_count
+            if self.transmit_count > 0 else 0.0
+        )
+        # If the traffic generator is unreliable and sends less packets,
+        # the absolute receive rate might be too low for next target.
+        self.relative_receive_rate = self.target_tr * self.receive_fraction
+        logging.debug(f"transmit_count {self.transmit_count} receive_count {self.receive_count} loss_count {self.loss_count} receive_fraction {self.receive_fraction} target_tr {self.target_tr} relative_receive_rate {self.relative_receive_rate}")
+        if approximated_duration:
+            self.approximated_duration = approximated_duration
+        else:
+            self.approximated_duration = duration
+        self.approximated_receive_rate = self.receive_count
+        if self.approximated_duration > 0.0:
+            self.approximated_receive_rate /= self.approximated_duration
+        else:
+            self.approximated_receive_rate = 0.0
 
     def __str__(self):
         """Return string reporting input and loss fraction."""
         return f"d={self.duration!s},Tr={self.target_tr!s}," \
-            f"Df={self.loss_fraction!s}"
+            f"Df={self.loss_fraction!s},ad={self.approximated_duration}"
 
     def __repr__(self):
         """Return string evaluable as a constructor call."""
         return f"ReceiveRateMeasurement(duration={self.duration!r}," \
             f"target_tr={self.target_tr!r}," \
             f"transmit_count={self.transmit_count!r}," \
-            f"loss_count={self.loss_count!r})"
+            f"loss_count={self.loss_count!r}," \
+            f"approximated_duration={self.approximated_duration!r})"
