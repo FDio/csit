6f8a69729c108b5efc7f36bc2daf5d71afd25196
Tibor Frank <tifrank@cisco.com>
2020-09-28 09:38:25 +0200

    Report: Add RC2 data
    Change-Id: I6c4ae6b428571a392586f5b817c5392a0de8c727
    Signed-off-by: Tibor Frank <tifrank@cisco.com>

--- resources/tools/presentation/run_report.sh
+++ resources/tools/presentation/run_report.sh
@@ -38,7 +38,7 @@ export PYTHONPATH=`pwd`:`pwd`/../../../:`pwd`/../../libraries/python
 python pal.py \
     --specification specification.yaml \
     --release ${RELEASE} \
-    --week "39" \
+    --week "40" \
     --logging INFO \
     --force
 
