783b1df99b9ca99d99d1624a0efd31743606a9c7
pmikus <pmikus@cisco.com>
2020-10-02 05:48:34 +0000

    GSO: TAP use case
    Signed-off-by: pmikus <pmikus@cisco.com>
    Change-Id: Ibafaaca4438587284d1e255c764f0701002941e8

---
+++ resources/templates/vm/init_iperf3.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+mkdir /dev/pts
+mkdir /dev/hugepages
+mount -t devpts -o "rw,noexec,nosuid,gid=5,mode=0620" devpts /dev/pts || true
+mount -t tmpfs -o "rw,noexec,nosuid,size=10%,mode=0755" tmpfs /run
+mount -t tmpfs -o "rw,noexec,nosuid,size=10%,mode=0755" tmpfs /tmp
+mount -t hugetlbfs -o "rw,relatime,pagesize=2M" hugetlbfs /dev/hugepages
+echo Y > /sys/module/vfio/parameters/enable_unsafe_noiommu_mode
+mkdir /run/sshd
+# Qemu virtio-net-pci mgmt
+ip address add dev ens3 10.0.2.15/24
+ip link set dev ens3 up
+ip route add default via 10.0.2.2
+# Qemu virtio-net-pci vhost1
+ip address add dev ens6 ${ip_address_l}
+ip link set dev ens6 up
+ip route add ${ip_route_r} via ${ip_address_r}
+# Payload
+${vnf_bin}
+# Safenet
+poweroff -f
