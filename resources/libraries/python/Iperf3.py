60fbf8d3f6cd2d1271840321ab5eea0cc94b196d
pmikus <pmikus@cisco.com>
2020-10-02 05:48:34 +0000

    GSO: TAP use case
    Signed-off-by: pmikus <pmikus@cisco.com>
    Change-Id: Ibafaaca4438587284d1e255c764f0701002941e8

---
+++ resources/libraries/python/Iperf3.py
@@ -0,0 +1,349 @@
+# Copyright (c) 2020 Cisco and/or its affiliates.
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at:
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""iPerf3 utilities library."""
+
+import json
+
+from resources.libraries.python.Constants import Constants
+from resources.libraries.python.CpuUtils import CpuUtils
+from resources.libraries.python.IPUtil import IPUtil
+from resources.libraries.python.Namespaces import Namespaces
+from resources.libraries.python.OptionString import OptionString
+from resources.libraries.python.ssh import exec_cmd, exec_cmd_no_error
+
+
+class Iperf3:
+    """iPerf3 traffic generator utilities."""
+
+    @staticmethod
+    def initialize_iperf_server(
+            node, if1, if2, interface, bind, bind_gw, bind_mask,
+            namespace=None, flows=1, cpu_skip_cnt=0, cpu_cnt=1):
+        """iPerf3 initialization.
+
+        :param node: Topology node running iPerf3 server.
+        :param if1: First TG's interface (To compute numa location).
+        :param if2: Second TG's interface (To compute numa location).
+        :param interface: Name of TG bind interface.
+        :param bind: Bind to host, one of node's addresses.
+        :param bind_gw: Bind gateway (required for default route).
+        :param bind_mask: Bind address mask.
+        :param namespace: Name of TG namespace to execute.
+        :param flows: Number of flows.
+        :param cpu_skip_cnt: Amount of CPU cores to skip.
+        :param cpu_cnt: iPerf3 main thread count.
+        :type node: dict
+        :type if1: str
+        :type if2: str
+        :type interface: str
+        :type bind: str
+        :type bind_gw: str
+        :type bind_mask: str
+        :type namespace: str
+        :type flows: int
+        :type cpu_skip_cnt: int
+        :type cpu_cnt: int
+        """
+        # Compute affinity for iPerf server.
+        s_affinity = CpuUtils.get_affinity_iperf(
+            node, if1, if2, cpu_skip_cnt=cpu_skip_cnt,
+            cpu_cnt=cpu_cnt
+        )
+        # Compute affinity for iPerf client.
+        c_affinity = CpuUtils.get_affinity_iperf(
+            node, if1, if2, cpu_skip_cnt=cpu_skip_cnt + cpu_cnt,
+            cpu_cnt=cpu_cnt
+        )
+
+        config = {
+            u"server": {
+                u"bind": bind,
+                u"bind_gw": bind_gw,
+                u"bind_mask": bind_mask,
+                u"interface": interface,
+                u"namespace": namespace,
+                u"affinity": s_affinity,
+                u"port": 5201
+            },
+            u"client": {
+                u"bind": bind,
+                u"bind_mask": bind_mask,
+                u"affinity": c_affinity,
+                u"flows": flows,
+                u"port": 5201
+            }
+        }
+        exec_cmd_no_error(
+            node,
+            f"sh -c 'cat << EOF > /etc/iperf_cfg.json\n"
+            f"{json.dumps(config, sort_keys=True, indent=4)}\n"
+            f"EOF'", sudo=True, message=u"iPerf3 config generation!"
+        )
+
+        Iperf3.start_iperf_server(
+            node, bind=bind, bind_gw=bind_gw, bind_mask=bind_mask,
+            interface=interface, namespace=namespace, port=5201,
+            affinity=s_affinity)
+
+        exec_cmd_no_error(
+            node,
+            f"grep -h 'vhost' /proc/*/comm | xargs pidof | "
+            f"sudo xargs -n 1 taskset -pc {s_affinity},{c_affinity}"
+        )
+
+    @staticmethod
+    def start_iperf_server(
+            node, bind, bind_gw, bind_mask, interface, namespace=None,
+            port=5201, affinity=0):
+        """Start iPerf3 server as a deamon.
+
+        :param node: Topology node running iPerf3 server.
+        :param bind: Bind to host, one of node's addresses.
+        :param bind_gw: Bind gateway (required for default route).
+        :param bind_mask: Bind address mask.
+        :param interface: Name of TG bing interface.
+        :param namespace: Name of TG namespace to execute.
+        :param port: The server port for the server to listen on.
+        :param affinity: iPerf3 server affinity.
+        :type node: dict
+        :type bind: str
+        :type bind_gw: str
+        :type bind_mask: str
+        :type interface: str
+        :type namespace: str
+        :type port: int
+        :type affinity: str
+        """
+        if Iperf3.is_iperf_running(node):
+            Iperf3.teardown_iperf(node)
+
+        IPUtil.set_linux_interface_ip(
+            node, interface=interface, ip_addr=bind, prefix=bind_mask,
+            namespace=namespace)
+        IPUtil.set_linux_interface_up(
+            node, interface=interface, namespace=namespace)
+        Namespaces.add_default_route_to_namespace(
+            node, namespace=namespace, default_route=bind_gw)
+
+        cmd = IPerf3Server.iperf3_cmdline(
+            bind=bind, bind_gw=bind_gw, bind_mask=bind_mask,
+            interface=interface, namespace=namespace, port=port,
+            affinity=affinity)
+        exec_cmd_no_error(
+            node, cmd, sudo=True, message=u"Failed to start iPerf3 server!")
+
+    @staticmethod
+    def is_iperf_running(node):
+        """Check if iPerf3 is running using pgrep.
+
+        :param node: Topology node running iPerf3.
+        :type node: dict
+        :returns: True if iPerf3 is running otherwise False.
+        :rtype: bool
+        """
+        ret, _, _ = exec_cmd(node, u"pgrep iperf3", sudo=True)
+        return bool(int(ret) == 0)
+
+    @staticmethod
+    def teardown_iperf(node):
+        """iPerf3 teardown.
+
+        :param node: Topology node running iPerf3.
+        :type node: dict
+        """
+        pidfile = u"/tmp/iperf3_server.pid"
+        logfile = u"/tmp/iperf3.log"
+
+        exec_cmd_no_error(
+            node,
+            f"sh -c 'if [ -f {pidfile} ]; then "
+            f"pkill -F {pidfile}; "
+            f"cat {logfile}; "
+            f"rm {logfile}; "
+            f"fi'",
+            sudo=True, message=u"iPerf3 kill failed!"
+        )
+
+    @staticmethod
+    def iperf_client_start_remote_exec(
+            node, duration, rate, frame_size, async_call=False, warmup_time=0,
+            traffic_directions=1, namespace=None, udp=False):
+        """Execute iPerf3 client script on remote node over ssh to start running
+        traffic.
+
+        :param node: Topology node running iPerf3.
+        :param duration: Time expressed in seconds for how long to send traffic.
+        :param rate: Traffic rate expressed with units (pps, %)
+        :param frame_size: L2 frame size to send (without padding and IPG).
+        :param async_call: If enabled then don't wait for all incoming traffic.
+        :param warmup_time: Warmup time period.
+        :param traffic_directions: Traffic is bi- (2) or uni- (1) directional.
+            Default: 1
+        :param namespace: Namespace to execute iPerf3 client on.
+        :param udp: UDP traffic.
+        :type node: dict
+        :type duration: float
+        :type rate: str
+        :type frame_size: str
+        :type async_call: bool
+        :type warmup_time: float
+        :type traffic_directions: int
+        :type namespace: str
+        :type udp: bool
+        :returns: List of iPerf3 PIDs.
+        :rtype: list
+        """
+        if not isinstance(duration, (float, int)):
+            duration = float(duration)
+        if not isinstance(warmup_time, (float, int)):
+            warmup_time = float(warmup_time)
+
+        cmd = IPerf3Client.iperf3_cmdline(
+            duration=duration, rate=rate, frame_size=frame_size,
+            async_call=async_call, warmup_time=warmup_time,
+            traffic_directions=1, namespace=namespace, udp=udp)
+
+        stdout, _ = exec_cmd_no_error(
+            node, cmd, timeout=int(duration) + 30,
+            message=u"iPerf3 runtime error!"
+        )
+
+        if async_call:
+            return stdout.split()
+        return json.loads(stdout)
+
+    @staticmethod
+    def iperf_client_stop_remote_exec(node, pids):
+        """Stop iPerf3 client execution.
+
+        :param pids: PID or List of PIDs of iPerf3 client.
+        :type pids: str or list
+        """
+        if not isinstance(pids, list):
+            pids = [pids]
+
+        for pid in pids:
+            exec_cmd_no_error(
+                node, f"kill {pid}", sudo=True, message=u"Kill iPerf3 failed!"
+            )
+
+
+class IPerf3Server:
+    """iPerf3 server utilities."""
+
+    @staticmethod
+    def iperf3_cmdline(
+            bind, bind_gw, bind_mask, interface, namespace=None, port=5201,
+            affinity=0):
+        """Get iPerf3 server command line.
+
+        :param bind: Bind to host, one of node's addresses.
+        :param bind_gw: Bind gateway (required for default route).
+        :param bind_mask: Bind address mask.
+        :param interface: Name of TG bing interface.
+        :param namespace: Name of TG namespace to execute.
+        :param port: The server port for the server to listen on.
+        :param affinity: iPerf3 server affinity.
+        :type bind: str
+        :type bind_gw: str
+        :type bind_mask: str
+        :type interface: str
+        :type namespace: str
+        :type port: int
+        :type affinity: str
+        :returns: iPerf3 server command line.
+        :rtype: str
+        """
+        cmd = OptionString()
+        if namespace:
+            cmd.add(f"ip netns exec {namespace}")
+        cmd.add(f"iperf3")
+
+        cmd_options = OptionString(prefix=u"--")
+        # Run iPerf in server mode. (This will only allow one iperf connection
+        # at a time)
+        cmd_options.add(u"server")
+        # Run the server in background as a daemon.
+        cmd_options.add(u"daemon")
+        # Write a file with the process ID, most useful when running as a
+        # daemon.
+        cmd_options.add_with_value(u"pidfile", f"/tmp/iperf3_server.pid")
+        # Send output to a log file.
+        cmd_options.add_with_value(u"logfile", f"/tmp/iperf3.log")
+        # The server port for the server to listen on and the client to
+        # connect to. This should be the same in both client and server.
+        # Default is 5201."
+        cmd_options.add_with_value(u"port", port)
+        # Set the CPU affinity, if possible (Linux and FreeBSD only).
+        cmd_options.add_with_value(u"affinity", affinity)
+        # Output in JSON format.
+        cmd_options.add(u"json")
+        # Give more detailed output.
+        cmd_options.add(u"verbose")
+
+        return cmd.extend(cmd_options)
+
+
+class IPerf3Client:
+    """iPerf3 client utilities."""
+
+    @staticmethod
+    def iperf3_cmdline(
+            duration, rate=None, frame_size=None, async_call=False,
+            warmup_time=5.0, traffic_directions=1, namespace=None, udp=False):
+        """Get iPerf3 client driver command line.
+
+        :param duration: Time expressed in seconds for how long to send traffic.
+        :param rate: Traffic rate expressed with units (pps, %)
+        :param frame_size: L2 frame size to send (without padding and IPG).
+        :param async_call: If enabled then don't wait for all incoming traffic.
+        :param warmup_time: Warmup time period.
+        :param traffic_directions: Traffic is bi- (2) or uni- (1) directional.
+            Default: 1
+        :param namespace: Namespace to execute iPerf3 client on.
+        :param udp: UDP traffic.
+        :type duration: float
+        :type rate: str
+        :type frame_size: str
+        :type async_call: bool
+        :type warmup_time: float
+        :type traffic_directions: int
+        :type namespace: str
+        :type udp: bool
+        :returns: iPerf3 client driver command line.
+        :rtype: str
+        """
+        if not isinstance(duration, (float, int)):
+            duration = float(duration)
+        if not isinstance(warmup_time, (float, int)):
+            warmup_time = float(warmup_time)
+
+        cmd = OptionString()
+        cmd.add(u"python3")
+        dirname = f"{Constants.REMOTE_FW_DIR}/resources/tools/iperf"
+        cmd.add(f"'{dirname}/iperf_client.py'")
+
+        cmd_options = OptionString(prefix=u"--")
+        cmd_options.add_with_value(u"port_netns", namespace)
+        cmd_options.add_with_value(u"duration", duration)
+        if frame_size:
+            cmd_options.add_with_value(u"frame_size", frame_size)
+        if rate:
+            cmd_options.add_with_value(u"rate", rate)
+        cmd_options.add_with_value(u"warmup_time", warmup_time)
+        cmd_options.add_with_value(u"traffic_directions", traffic_directions)
+        cmd_options.add_if(u"async_start", async_call)
+        cmd_options.add_if(u"udp", udp)
+
+        return cmd.extend(cmd_options)
---
+++ tests/vpp/perf/tap/10ge2p1x710-ethip4-ip4base-2tap-gso-ndrpdr.robot
@@ -0,0 +1,106 @@
+# Copyright (c) 2020 Cisco and/or its affiliates.
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at:
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+*** Settings ***
+| Resource | resources/libraries/robot/shared/default.robot
+|
+| Force Tags | 3_NODE_SINGLE_LINK_TOPO | PERFTEST | HW_ENV | NDRPDR
+| ... | NIC_Intel-X710 | IP4FWD | BASE | IP4BASE | DRV_TAPV2
+| ... | RXQ_SIZE_0 | TXQ_SIZE_0 | GSO_TRUE
+| ... | ethip4-ip4base-2tap-gso
+
+| Suite Setup | Setup suite topology interfaces
+| Suite Teardown | Tear down suite
+| Test Setup | Setup test
+| Test Teardown | Tear down test | iPerf3 | namespace
+|
+| Test Template | Local Template
+|
+| Documentation | *RFC2544: Pkt throughput L2BD test cases with vhost and vpp
+| ... | link bonding*
+|
+| ... | *[Top] Network Topologies:* DUT1 1-node topology without physical links.
+| ... | *[Enc] Packet Encapsulations:* Eth-IPv4-UDP for L2 switching of IPv4.
+| ... | *[Cfg] DUT configuration:* DUT1 is configured with VPP L2 bridge-domain\
+| ... | with MAC learning enabled.
+| ... | *[Ver] TG verification:* iPerf3 client/server is used for Packet\
+| ... | generation and verification.
+| ... | *[Ref] Applicable standard specifications:* RFC2544.
+
+*** Variables ***
+| @{plugins_to_enable}= | ping_plugin.so
+| ${crypto_type}= | ${None}
+| ${nic_name}= | Intel-X710
+| ${nic_driver}= | tap
+| ${nic_rxq_size}= | 4096
+| ${nic_txq_size}= | 4096
+| ${nic_pfs}= | 2
+| ${nic_vfs}= | 0
+| ${osi_layer}= | L3
+| ${overhead}= | ${0}
+| ${enable_gso}= | ${True}
+# iPerf3 client settings:
+| ${iperf_client_bind}= | 1.1.1.1
+| ${iperf_client_bind_gw}= | 1.1.1.2
+| ${iperf_client_bind_mask}= | 30
+| ${iperf_client_interface}= | tap0
+| ${iperf_client_udp}= | ${False}
+# iPerf3 server settings:
+| ${iperf_server_bind}= | 2.2.2.2
+| ${iperf_server_bind_gw}= | 2.2.2.1
+| ${iperf_server_bind_mask}= | 30
+| ${iperf_server_interface}= | tap1
+# Traffic profile:
+| ${traffic_profile}= | iperf3-ethip4udp
+
+*** Keywords ***
+| Local Template
+| | [Documentation]
+| | ... | [Cfg] DUT1 is configured with VPP L2 bridge-domain with MAC learning\
+| | ... | enabled.
+| | ... | [Ver] Measure NDR and PDR values using MLRsearch algorithm.\
+| |
+| | ... | *Arguments:*
+| | ... | - frame_size - Framesize in Bytes in integer or string (IMIX_v4_1).
+| | ... | Type: integer, string
+| | ... | - phy_cores - Number of physical cores. Type: integer
+| | ... | - rxq - Number of RX queues, default value: ${None}. Type: integer
+| |
+| | [Arguments] | ${frame_size} | ${phy_cores} | ${rxq}=${None}
+| |
+| | Set Test Variable | \${frame_size}
+| |
+| | Given Set Max Rate And Jumbo
+| | And Add worker threads to all DUTs | ${phy_cores} | ${rxq}
+| | And Pre-initialize layer driver | ${nic_driver}
+| | And Apply startup configuration on all VPP DUTs
+| | When Initialize layer driver | ${nic_driver}
+| | And Initialize layer interface
+| | And VPP Interface Set IP Address
+| | ... | ${dut1} | ${DUT1_${int}1}[0] | 1.1.1.2 | 30
+| | And VPP Interface Set IP Address
+| | ... | ${dut1} | ${DUT1_${int}2}[0] | 2.2.2.1 | 30
+| | Then Traffic should pass with maximum rate on iPerf3
+
+*** Test Cases ***
+| 128KB-1c-ethip4-ip4base-2tap-gso-ndrpdr
+| | [Tags] | 128KB | 1C
+| | frame_size=${128000} | phy_cores=${1}
+
+| 128KB-2c-ethip4-ip4base-2tap-gso-ndrpdr
+| | [Tags] | 128KB | 2C
+| | frame_size=${128000} | phy_cores=${2}
+
+| 128KB-4c-ethip4-ip4base-2tap-gso-ndrpdr
+| | [Tags] | 128KB | 4C
+| | frame_size=${128000} | phy_cores=${4}
