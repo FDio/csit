06c992d7aa962499bc54cd80d751cbd50aa92265
Vratko Polak <vrpolak@cisco.com>
2020-10-15 14:25:31 +0200

    Add ability to parse more kinds of test results
    Previously, only BMRR results were recongnized.
    Now also lower bounds for PDR (optionally NDR) and soak are recongnized.
    If all parsing fails, generate fake data based on overall pass or fail,
    so at least passrate of unknown tests can be compared in theory.
    
    Currently affects only per-patch job,
    but is useful mainly for the upcoming bisect job.
    
    + Do not force MRR test type in vpp-csit jobs.
     + Starting per-patch parsing for PDR is useful for RCA.
     - Some test results are still not recognized (e.g. hoststack).
    + Changes which keep or fix a failure get +1 from per-patch perf job.
     + Only changes that introduce a failure get -1.
     + Both is true also for unrecognized test results.
     - The fake values from passrate can be misleading.
    + Use only default NIC if NIC tag is missing.
     + In all jobs, not only in vpp-csit ones.
    - No job supports NDR parsing yet.
     + Will enable on ci-manager side for per-patch and bisect jobs.
    
    Change-Id: Ibbb9bfe4560f640c67b52f892f5b584fe3704e86
    Signed-off-by: Vratko Polak <vrpolak@cisco.com>

--- resources/libraries/bash/function/common.sh
+++ resources/libraries/bash/function/common.sh
@@ -1,5 +1,5 @@
-# Copyright (c) 2020 Cisco and/or its affiliates.
-# Copyright (c) 2020 PANTHEON.tech and/or its affiliates.
+# Copyright (c) 2021 Cisco and/or its affiliates.
+# Copyright (c) 2021 PANTHEON.tech and/or its affiliates.
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at:
@@ -491,6 +491,8 @@ function get_test_tag_string () {
     # Variables set:
     # - TEST_TAG_STRING - The string following trigger word in gerrit comment.
     #   May be empty, or even not set on event types not adding comment.
+    # Variables exported optionally:
+    # - GRAPH_NODE_VARIANT - Node variant to test with, set if found in trigger.
 
     # TODO: ci-management scripts no longer need to perform this.
 
@@ -943,16 +945,13 @@ function select_tags () {
     TAGS=()
     prefix=""
 
-    set +x
-    if [[ "${TEST_CODE}" == "vpp-"* ]]; then
-        # Automatic prefixing for VPP jobs to limit the NIC used and
-        # traffic evaluation to MRR.
-        if [[ "${TEST_TAG_STRING-}" == *"nic_"* ]]; then
-            prefix="${prefix}mrrAND"
-        else
-            prefix="${prefix}mrrAND${default_nic}AND"
-        fi
+    # Automatic limiting of NIC used if not specified.
+    if [[ "${TEST_TAG_STRING-}" == *"nic_"* ]]; then
+        prefix=""
+    else
+        prefix="${default_nic}AND"
     fi
+    set +x
     for tag in "${test_tag_array[@]}"; do
         if [[ "${tag}" == "!"* ]]; then
             # Exclude tags are not prefixed.
