a4f1acff75d84b5e91d4788dd3be3326d65edd30
Vratko Polak <vrpolak@cisco.com>
2020-10-16 14:53:30 +0200

    Add bisect script for locating regressions
    A copy suitable for 2009 RCA.
    
    TB28 is still allowed. RDMA tests fail on it,
    but because of that it is usually not reserved by serious jobs.
    
    Change-Id: I4747994a934ac3bc7a23587d846f393e5287a320
    Signed-off-by: Vratko Polak <vrpolak@cisco.com>

--- resources/libraries/bash/function/common.sh
+++ resources/libraries/bash/function/common.sh
@@ -257,6 +257,10 @@ function common_dirs () {
 
 function compose_pybot_arguments () {
 
+    # This function is called by run_tests function.
+    # The reason is that some jobs (bisect) perform reservation multiple times,
+    # so WORKING_TOPOLOGY can be different each time.
+    #
     # Variables read:
     # - WORKING_TOPOLOGY - Path to topology yaml file of the reserved testbed.
     # - DUT - CSIT test/ subdirectory, set while processing tags.
@@ -273,11 +277,13 @@ function compose_pybot_arguments () {
     PYBOT_ARGS=("--loglevel" "TRACE")
     PYBOT_ARGS+=("--variable" "TOPOLOGY_PATH:${WORKING_TOPOLOGY}")
 
+    # TODO: The rest does not need to be recomputed on each reservation.
+    #       Refactor TEST_CODE so this part can be called only once.
     case "${TEST_CODE}" in
         *"device"*)
             PYBOT_ARGS+=("--suite" "tests.${DUT}.device")
             ;;
-        *"perf"*)
+        *"perf"* | *"bisect"*)
             PYBOT_ARGS+=("--suite" "tests.${DUT}.perf")
             ;;
         *)
@@ -502,6 +508,10 @@ function get_test_tag_string () {
     # Variables set:
     # - TEST_TAG_STRING - The string following trigger word in gerrit comment.
     #   May be empty, or even not set on event types not adding comment.
+    # - GIT_BISECT_FROM - If bisecttest, the commit hash to bisect from.
+    #   Else not set.
+    # Variables exported optionally:
+    # - GRAPH_NODE_VARIANT - Node variant to test with, set if found in trigger.
 
     # TODO: ci-management scripts no longer need to perform this.
 
@@ -509,6 +519,10 @@ function get_test_tag_string () {
 
     if [[ "${GERRIT_EVENT_TYPE-}" == "comment-added" ]]; then
         case "${TEST_CODE}" in
+            # Order matters, bisect job contains "perf" in its name.
+            *"bisect"*)
+                trigger="bisecttest"
+                ;;
             *"device"*)
                 trigger="devicetest"
                 ;;
@@ -527,6 +541,18 @@ function get_test_tag_string () {
         cmd=("grep" "-oP" '\S*'"${trigger}"'\S*\s\K.+$') || die "Unset trigger?"
         # On parsing error, TEST_TAG_STRING probably stays empty.
         TEST_TAG_STRING=$("${cmd[@]}" <<< "${comment}") || true
+        if [[ "${trigger}" == "bisecttest" ]]; then
+            # Intentionally without quotes, so spaces delimit elements.
+            test_tag_array=(${TEST_TAG_STRING}) || die "How could this fail?"
+            # First "argument" of bisecttest is a commit hash.
+            GIT_BISECT_FROM="${test_tag_array[0]}" || {
+                die "Bisect job requires commit hash."
+            }
+            # Update the tag string (tag expressions only, no commit hash).
+            TEST_TAG_STRING="${test_tag_array[@]:1}" || {
+                die "Bisect job needs a single test, no default."
+            }
+        fi
         if [[ -n "${TEST_TAG_STRING-}" ]]; then
             test_tag_array=(${TEST_TAG_STRING})
             if [[ "${test_tag_array[0]}" == "icl" ]]; then
@@ -641,19 +667,29 @@ function reserve_and_cleanup_testbed () {
 
 function run_pybot () {
 
-    # Run pybot with options based on input variables. Create output_info.xml
+    # Run pybot with options computed from inputs. Create output_info.xml
     #
     # Variables read:
     # - CSIT_DIR - Path to existing root of local CSIT git repository.
     # - ARCHIVE_DIR - Path to store robot result files in.
     # - PYBOT_ARGS, EXPANDED_TAGS - See compose_pybot_arguments.sh
     # - GENERATED_DIR - Tests are assumed to be generated under there.
+    # - WORKING_TOPOLOGY - Path to topology yaml file of the reserved testbed.
+    # - DUT - CSIT test/ subdirectory, set while processing tags.
+    # - TAGS - Array variable holding selected tag boolean expressions.
+    # - TOPOLOGIES_TAGS - Tag boolean expression filtering tests for topology.
+    # - TEST_CODE - The test selection string from environment or argument.
     # Variables set:
+    # - PYBOT_ARGS - String holding part of all arguments for pybot.
+    # - EXPANDED_TAGS - Array of strings pybot arguments compiled from tags.
     # - PYBOT_EXIT_STATUS - Exit status of most recent pybot invocation.
     # Functions called:
     # - die - Print to stderr and exit.
 
     set -exuo pipefail
+
+    # Recompute args, as we can have different reservation since the last call.
+    compose_pybot_arguments || die "Failed to compose robot arguments."
 
     all_options=("--outputdir" "${ARCHIVE_DIR}" "${PYBOT_ARGS[@]}")
     all_options+=("--noncritical" "EXPECTED_FAILING")
@@ -890,19 +926,13 @@ function select_tags () {
 
     TAGS=()
 
-    # We will prefix with perftest to prevent running other tests
-    # (e.g. Functional).
-    prefix="perftestAND"
-    set +x
-    if [[ "${TEST_CODE}" == "vpp-"* ]]; then
-        # Automatic prefixing for VPP jobs to limit the NIC used and
-        # traffic evaluation to MRR.
-        if [[ "${TEST_TAG_STRING-}" == *"nic_"* ]]; then
-            prefix="${prefix}mrrAND"
-        else
-            prefix="${prefix}mrrAND${default_nic}AND"
-        fi
+    # Automatic limiting of NIC used if not specified.
+    if [[ "${TEST_TAG_STRING-}" == *"nic_"* ]]; then
+        prefix=""
+    else
+        prefix="${default_nic}AND"
     fi
+    set +x
     for tag in "${test_tag_array[@]}"; do
         if [[ "${tag}" == "!"* ]]; then
             # Exclude tags are not prefixed.
