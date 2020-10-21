ba6bb32e5ba41c86b293f5a0309ca26e6c5039a5
Tibor Frank <tifrank@cisco.com>
2020-10-21 09:38:17 +0200

    Report: Configure 2009.44
    Change-Id: I7f7e50610e71af7f2b0dc1eed267a421ffd8aef0
    Signed-off-by: Tibor Frank <tifrank@cisco.com>
    (cherry picked from commit 1b25441a3d82a12d01334bd82795bd35a4f7c2e3)

--- resources/tools/presentation/run_report.sh
+++ resources/tools/presentation/run_report.sh
@@ -38,7 +38,7 @@ export PYTHONPATH=`pwd`:`pwd`/../../../:`pwd`/../../libraries/python
 python pal.py \
     --specification specification.yaml \
     --release ${RELEASE} \
-    --week "43" \
+    --week "44" \
     --logging INFO \
     --force
 
