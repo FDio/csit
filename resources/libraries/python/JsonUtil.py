eb7011b70f0e4d889e2be4feafbf4f5b2497df11
Jan Gelety <jgelety@cisco.com>
2020-10-24 07:12:05 +0200

    Perf: GENEVE tunnel perf tests, l2 mode
    Jira: CSIT-1767
    
    Change-Id: I0ab906966f760b809b8431530c010a8f0835d62c
    Signed-off-by: Jan Gelety <jgelety@cisco.com>

--- resources/libraries/python/JsonUtil.py
+++ resources/libraries/python/JsonUtil.py
@@ -11,23 +11,21 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from scapy.fields import BitField, XByteField, X3BytesField
-from scapy.layers.inet import UDP
-from scapy.layers.l2 import Ether
-from scapy.packet import Packet, bind_layers
+"""Common JSON utilities library."""
+
+import json
 
 
-class VXLAN(Packet):
-    name = u"VXLAN"
-    fields_desc = [
-        BitField(u"flags", 0x08000000, 32),
-        X3BytesField(u"vni", 0),
-        XByteField(u"reserved", 0x00)
-    ]
+class JsonUtil:
+    """Common JSON utilities"""
 
-    def mysummary(self):
-        return self.sprintf(f"VXLAN (vni={VXLAN.vni})")
+    @staticmethod
+    def convert_to_json(data):
+        """Serialize Python data structure to a JSON formatted string.
 
-
-bind_layers(UDP, VXLAN, dport=4789)
-bind_layers(VXLAN, Ether)
+        :param data: Python data structure to serialize to JSON string.
+        :type data: object
+        :returns: JSON formatted string.
+        :rtype: str
+        """
+        return json.dumps(data).replace(u'"', u"'")
