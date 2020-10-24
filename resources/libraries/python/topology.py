6a4fb9e70541c4f341bffa9ef94b861948aaf362
Jan Gelety <jgelety@cisco.com>
2020-10-24 07:12:05 +0200

    Perf: GENEVE tunnel perf tests, l2 mode
    Jira: CSIT-1767
    
    Change-Id: I0ab906966f760b809b8431530c010a8f0835d62c
    Signed-off-by: Jan Gelety <jgelety@cisco.com>

--- resources/libraries/python/topology.py
+++ resources/libraries/python/topology.py
@@ -174,7 +174,7 @@ class Topology:
         port_types = (
             u"subinterface", u"vlan_subif", u"memif", u"tap", u"vhost",
             u"loopback", u"gre_tunnel", u"vxlan_tunnel", u"eth_bond",
-            u"eth_avf", u"eth_rdma"
+            u"eth_avf", u"eth_rdma", u"geneve_tunnel"
         )
 
         for node_data in nodes.values():
