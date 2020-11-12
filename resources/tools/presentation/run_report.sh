5e81988831eb6141cd9e89a958f349c4f186101e
Tibor Frank <tifrank@cisco.com>
2020-11-12 06:53:50 +0100

    Report: Add NAT44 data
    - reorder NAT44 graphs
    
    Change-Id: Ied762a85fb1ee69c7d12971e3e15822571a18451
    Signed-off-by: Tibor Frank <tifrank@cisco.com>

--- resources/tools/presentation/run_report.sh
+++ resources/tools/presentation/run_report.sh
@@ -38,7 +38,7 @@ export PYTHONPATH=`pwd`:`pwd`/../../../:`pwd`/../../libraries/python
 python pal.py \
     --specification specification.yaml \
     --release ${RELEASE} \
-    --week "46" \
+    --week "47" \
     --logging INFO \
     --force
 
