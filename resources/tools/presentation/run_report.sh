235c35199436fe4d6ff996543aea47e8ce6a148d
Tibor Frank <tifrank@cisco.com>
2020-11-23 10:44:30 +0100

    Report: Configure rls2009.48
    Change-Id: Ib7c4bc51bea43dafa2bd788c709c800caeb75988
    Signed-off-by: Tibor Frank <tifrank@cisco.com>
    (cherry picked from commit ef51e552aa2d0e134646d28aa3ada2da0d467f99)

--- resources/tools/presentation/run_report.sh
+++ resources/tools/presentation/run_report.sh
@@ -38,7 +38,7 @@ export PYTHONPATH=`pwd`:`pwd`/../../../:`pwd`/../../libraries/python
 python pal.py \
     --specification specification.yaml \
     --release ${RELEASE} \
-    --week "47" \
+    --week "48" \
     --logging INFO \
     --force
 
