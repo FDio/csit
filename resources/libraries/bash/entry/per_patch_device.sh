921be93fe7d83870c9f1576737f6171a29771b6e
Vratko Polak <vrpolak@cisco.com>
2020-12-07 15:11:15 +0100

    Fix vpp-csit device script
    In Gerrit 30041 that was not tested properly.
    
    Change-Id: I42f57f1933b0e7d029030f4ec502f7268780827b
    Signed-off-by: Vratko Polak <vrpolak@cisco.com>
    (cherry picked from commit 70b633f637f6923d1e266473389ddbba02ca37f1)

--- resources/libraries/bash/entry/per_patch_device.sh
+++ resources/libraries/bash/entry/per_patch_device.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-# Copyright (c) 2020 Cisco and/or its affiliates.
+# Copyright (c) 2021 Cisco and/or its affiliates.
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at:
@@ -57,5 +57,4 @@ select_vpp_device_tags || die
 compose_pybot_arguments || die
 run_pybot || die
 move_archives || die
-archive_test_results "csit_current" || die
 die_on_pybot_error || die
