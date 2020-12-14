95de51db79d23e57968e0d27a2a4458326a59639
Tibor Frank <tifrank@cisco.com>
2020-12-14 07:39:45 +0100

    Report: Add data
    Change-Id: I298c3bdec3f9254a2644401a9ffb7ff09c4ecbd2
    Signed-off-by: Tibor Frank <tifrank@cisco.com>

--- resources/tools/presentation/run_report.sh
+++ resources/tools/presentation/run_report.sh
@@ -38,7 +38,7 @@ export PYTHONPATH=`pwd`:`pwd`/../../../:`pwd`/../../libraries/python
 python pal.py \
     --specification specification.yaml \
     --release ${RELEASE} \
-    --week "50" \
+    --week "51" \
     --logging INFO \
     --force
 
