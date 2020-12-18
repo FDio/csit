1f4c24b677ef86e0f021516f34e52ba868c2e0ac
Vratko Polak <vrpolak@cisco.com>
2020-12-18 13:48:05 +0100

    Experiment: MRR with low load
    There is some examination of efficiency of VPP code ith low traffic,
    especially the dependence on packet size.
    
    This lowers per-nic pps limit, so we can see what it does to show run.
    
    Change-Id: I049267dd4f6c6fa703726e96dcd30bf9918ee477
    Signed-off-by: Vratko Polak <vrpolak@cisco.com>

--- resources/libraries/python/Constants.py
+++ resources/libraries/python/Constants.py
@@ -280,23 +280,23 @@ class Constants:
 
     # Mapping from NIC name to its pps limit.
     NIC_NAME_TO_PPS_LIMIT = {
-        u"Cisco-VIC-1227": 14880952,
-        u"Cisco-VIC-1385": 18750000,
-        u"Intel-X520-DA2": 14880952,
-        u"Intel-X553": 14880952,
-        u"Intel-X710": 14880952,
-        u"Intel-XL710": 18750000,
-        u"Intel-XXV710": 18750000,
-        u"Intel-E810CQ": 58500000,
+        u"Cisco-VIC-1227": 1000000,
+        u"Cisco-VIC-1385": 1000000,
+        u"Intel-X520-DA2": 1000000,
+        u"Intel-X553": 1000000,
+        u"Intel-X710": 1000000,
+        u"Intel-XL710": 1000000,
+        u"Intel-XXV710": 1000000,
+        u"Intel-E810CQ": 1000000,
         # 2n-clx testbeds show duration stretching on high rates,
         # depending on encapsulation TRex has to generate.
         # 40 Mpps is still too much for dot1q (~8% stretching).
         # 36 Mpps is around the maximal VPP throughput (l2patch 4c8t).
         # Vxlan traffic will still show stretching at 36 Mpps (>12%),
         # but we do not care about those tests that much.
-        u"Mellanox-CX556A": 36000000, # 148809523,
-        u"Amazon-Nitro-50G": 1500000,
-        u"virtual": 14880952,
+        u"Mellanox-CX556A": 1000000, # 148809523,
+        u"Amazon-Nitro-50G": 1000000,
+        u"virtual": 1000000,
     }
 
     # Suite file names use codes for NICs.
