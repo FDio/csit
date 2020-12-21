84359e86b6d95d25ee79b2df9e256d00533b0896
Jan Gelety <jgelety@cisco.com>
2020-12-22 00:22:48 +0100

    fix: use csit lts branch in vpp-csit lts jobs
    Change-Id: Iebd62fd6b0c798f7b4dd1f3b093c156e533b3900
    Signed-off-by: Jan Gelety <jgelety@cisco.com>
    (cherry picked from commit a5b3a8b91d8e6c9baa4361d70b96769a98bfc454)
    (cherry picked from commit 061e7d8d0f49c26a2cd09cc30886ae5b23f0b376)

--- resources/libraries/bash/function/branch.sh
+++ resources/libraries/bash/function/branch.sh
@@ -1,4 +1,4 @@
-# Copyright (c) 2020 Cisco and/or its affiliates.
+# Copyright (c) 2021 Cisco and/or its affiliates.
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at:
@@ -57,6 +57,10 @@ function checkout_csit_for_vpp () {
     set -exuo pipefail
 
     case "${1}" in
+        "stable/2009")
+            # LTS branch
+            branch_id="origin/${1/stable\//oper-rls}_lts"
+            ;;
         "stable/"*)
             branch_id="origin/${1/stable\//oper-rls}"
             ;;
