9446b9bf9c15999feec3c8a48fec428bca094500
Tomas Alexy <tomas.alexy@pantheon.tech>
2021-01-04 10:15:51 +0100

    API: deprecated COP APIs
    Signed-off-by: Tomas Alexy <tomas.alexy@pantheon.tech>
    Change-Id: Ifb97088a26f3c2ab2ecee28ed43e6e957a844c64

--- resources/tools/presentation/generator_tables.py
+++ resources/tools/presentation/generator_tables.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2020 Cisco and/or its affiliates.
+# Copyright (c) 2021 Cisco and/or its affiliates.
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at:
@@ -1063,6 +1063,8 @@ def _generate_url(testbed, test_name):
     elif u"iacl" in test_name:
         bsf = u"features"
     elif u"policer" in test_name:
+        bsf = u"features"
+    elif u"adl" in test_name:
         bsf = u"features"
     elif u"cop" in test_name:
         bsf = u"features"
