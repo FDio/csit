f4bfce52937294abfa90ea91cd6cfc6349472ab7
xizhanx <xix.zhang@intel.com>
2021-01-05 13:17:01 +0800

    add vsap test case
    Change-Id: Ie3b573952c1bc9f06e3ecc75daf7d9c575ca5473
    Signed-off-by: xizhanx <xix.zhang@intel.com>

--- resources/libraries/bash/function/artifacts.sh
+++ resources/libraries/bash/function/artifacts.sh
@@ -16,6 +16,129 @@
 
 set -exuo pipefail
 
+function download_vsap () {
+
+    # Download or install VSAP artifacts from packagecloud.io.
+    #
+    # Variables set:
+    # - REPO_URL - FD.io Packagecloud repository.
+    # - PKG_KEY - Keywords to download package.
+    # Functions conditionally called (see their documentation for side effects):
+    # - download_ubuntu_repo_file
+    # - download_ubuntu
+
+    set -exuo pipefail
+
+    mode=$1
+    os_id=$(grep '^ID=' /etc/os-release | cut -f2- -d= | sed -e 's/\"//g') || {
+        die "Get OS release failed."
+    }
+
+    repo_url_path="${CSIT_DIR}/VSAP_REPO_URL"
+    if [ -e "${repo_url_path}" ]; then
+        REPO_URL="$(<${repo_url_path})" || {
+            die "Read repo URL from ${repo_url_path} failed."
+        }
+    else
+        die "Read repo URL failed."
+    fi
+
+    if [ "${os_id}" == "ubuntu" ]; then
+        download_ubuntu_repo_file || die
+        PKG_KEY=vsap-${mode}
+        download_ubuntu_packages || die
+        PKG_KEY=openssl3
+        download_ubuntu_packages || die
+    else
+        die "${os_id} is not yet supported."
+    fi
+
+}
+
+function download_ubuntu_repo_file () {
+
+    # Download fdio repo file from packagecloud.io.
+    #
+    # Variables read:
+    # - APT_FDIO_REPO_FILE - FD.io repo file in packagecloud.io.
+
+    set -exuo pipefail
+
+    curl -s "${REPO_URL}"/script.deb.sh | sudo -E bash || {
+        die "Packagecloud FD.io repo fetch failed."
+    }
+    # If version is set we will add suffix.
+    artifacts=()
+    both_quotes='"'"'"
+    match="[^${both_quotes}]*"
+    qmatch="[${both_quotes}]\?"
+    sed_command="s#.*apt_source_path=${qmatch}\(${match}\)${qmatch}#\1#p"
+    APT_FDIO_REPO_FILE=$(curl -s "${REPO_URL}"/script.deb.sh | \
+                         sed -n ${sed_command}) || {
+                             die "Local fdio repo file path fetch failed."
+                         }
+    if [ ! -f ${APT_FDIO_REPO_FILE} ]; then
+        die "${APT_FDIO_REPO_FILE} not found,
+	repository installation was not successful."
+    fi
+}
+
+function download_ubuntu_packages () {
+
+    # Download or install Ubuntu packages from packagecloud.io.
+    #
+    # Variables read:
+    # - INSTALL - Whether install packages (if set to "true") or download only.
+    #             Default: "false".
+
+    packages=$(apt-cache -o Dir::Etc::SourceList=${APT_FDIO_REPO_FILE} \
+               -o Dir::Etc::SourceParts=${APT_FDIO_REPO_FILE} dumpavail \
+               | grep Package: | cut -d " " -f 2 | grep ${PKG_KEY}) || {
+                   die "Retrieval of available ${PKG_KEY} packages failed."
+               }
+
+    if [ -z "${pkg_version-}" ]; then
+        # If version is not specified, find out the most recent version
+        pkg_version=$(apt-cache -o Dir::Etc::SourceList=${APT_FDIO_REPO_FILE} \
+                      -o Dir::Etc::SourceParts=${APT_FDIO_REPO_FILE} \
+                      --no-all-versions show ${PKG_KEY} | grep Version: | \
+                      cut -d " " -f 2) || {
+                          die "Retrieval of most recent version failed."
+                      }
+    fi
+    set +x
+
+    for package in ${packages}; do
+        # Filter packages with given version
+        pkg_info=$(apt-cache show -- ${package}) || {
+            die "apt-cache show on ${package} failed."
+        }
+        ver=$(echo ${pkg_info} | grep -o "Version: ${pkg_version-}[^ ]*" | \
+              head -1) || true
+        if [ -n "${ver-}" ]; then
+            echo "Found '${pkg_version-}' among '${package}' versions."
+            ver=$(echo "$ver" | cut -d " " -f 2)
+            artifacts+=(${package[@]/%/=${ver-}})
+        else
+            echo "Didn't find '${pkg_version-}' among '${package}' versions."
+        fi
+    done
+    set -x
+    pkg_version=""
+
+    if [[ "${INSTALL:-false}" == "true" ]]; then
+        sudo apt-get -y install "${artifacts[@]}" || {
+            die "Install ${PKG_KEY} PKG artifacts failed."
+        }
+    else
+        apt-get -y download "${artifacts[@]}" || {
+            die "Download ${PKG_KEY} PKG artifacts failed."
+        }
+    fi
+
+
+}
+
 function download_artifacts () {
 
     # Download or install VPP artifacts from packagecloud.io.
@@ -59,75 +182,14 @@ function download_ubuntu_artifacts () {
 
     # Download or install Ubuntu VPP artifacts from packagecloud.io.
     #
-    # Variables read:
-    # - REPO_URL - FD.io Packagecloud repository.
-    # - VPP_VERSION - VPP version.
-    # - INSTALL - Whether install packages (if set to "true") or download only.
-    #             Default: "false".
-
-    set -exuo pipefail
-
-    curl -s "${REPO_URL}"/script.deb.sh | sudo -E bash || {
-        die "Packagecloud FD.io repo fetch failed."
-    }
-    # If version is set we will add suffix.
-    artifacts=()
-    both_quotes='"'"'"
-    match="[^${both_quotes}]*"
-    qmatch="[${both_quotes}]\?"
-    sed_command="s#.*apt_source_path=${qmatch}\(${match}\)${qmatch}#\1#p"
-    apt_fdio_repo_file=$(curl -s "${REPO_URL}"/script.deb.sh | \
-                         sed -n ${sed_command}) || {
-                             die "Local fdio repo file path fetch failed."
-                         }
-
-    if [ ! -f ${apt_fdio_repo_file} ]; then
-        die "${apt_fdio_repo_file} not found, \
-            repository installation was not successful."
-    fi
-
-    packages=$(apt-cache -o Dir::Etc::SourceList=${apt_fdio_repo_file} \
-               -o Dir::Etc::SourceParts=${apt_fdio_repo_file} dumpavail \
-               | grep Package: | cut -d " " -f 2 | grep vpp) || {
-                   die "Retrieval of available VPP packages failed."
-               }
-    if [ -z "${VPP_VERSION-}" ]; then
-        # If version is not specified, find out the most recent version
-        VPP_VERSION=$(apt-cache -o Dir::Etc::SourceList=${apt_fdio_repo_file} \
-                      -o Dir::Etc::SourceParts=${apt_fdio_repo_file} \
-                      --no-all-versions show vpp | grep Version: | \
-                      cut -d " " -f 2) || {
-                          die "Retrieval of most recent VPP version failed."
-                      }
-    fi
-
-    set +x
-    for package in ${packages}; do
-        # Filter packages with given version
-        pkg_info=$(apt-cache show -- ${package}) || {
-            die "apt-cache show on ${package} failed."
-        }
-        ver=$(echo ${pkg_info} | grep -o "Version: ${VPP_VERSION-}[^ ]*" | \
-              head -1) || true
-        if [ -n "${ver-}" ]; then
-            echo "Found '${VPP_VERSION-}' among '${package}' versions."
-            ver=$(echo "$ver" | cut -d " " -f 2)
-            artifacts+=(${package[@]/%/=${ver-}})
-        else
-            echo "Didn't find '${VPP_VERSION-}' among '${package}' versions."
-        fi
-    done
-    set -x
-
-    if [[ "${INSTALL:-false}" == "true" ]]; then
-        sudo apt-get -y install "${artifacts[@]}" || {
-            die "Install VPP artifacts failed."
-        }
-    else
-        apt-get -y download "${artifacts[@]}" || {
-            die "Download VPP artifacts failed."
-        }
-    fi
+    # Variables set:
+    # - PKG_KEY - Keywords to download package.
+    # Functions called (see their documentation for side effects):
+    # - download_ubuntu_repo_file
+    # - download_ubuntu
+    download_ubuntu_repo_file || die
+    PKG_KEY=vpp
+    download_ubuntu_packages || die
 }
 
 function download_centos_artifacts () {
