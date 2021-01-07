4b964ef23e3b83efb99aa53bc042f3ea16632a84
pmikus <pmikus@cisco.com>
2021-01-07 14:27:38 +0000

    Infra: Monitoring capability
    + Monitoring SOA
      + Nomad alertmanager job
      + Nomad prometheus job
      + Nomad grafana job
    + Vpp_device SOA
      + Nomad vpp_device job cleanup
    
    Signed-off-by: pmikus <pmikus@cisco.com>
    Change-Id: I0b32e9c87276ba1a2d4a5322816f3473c737eae2

--- terraform-ci-infra/1n_nmd/minio/outputs.tf
+++ terraform-ci-infra/1n_nmd/minio/outputs.tf
@@ -2,20 +2,3 @@ output "minio_service_name" {
   description = "Minio service name"
   value       = data.template_file.nomad_job_minio.vars.service_name
 }
-
-output "minio_access_key" {
-  description = "Minio access key"
-  value       = data.template_file.nomad_job_minio.vars.access_key
-  sensitive   = true
-}
-
-output "minio_secret_key" {
-  description = "Minio secret key"
-  value       = data.template_file.nomad_job_minio.vars.secret_key
-  sensitive   = true
-}
-
-output "minio_port" {
-  description = "Minio port number"
-  value = data.template_file.nomad_job_minio.vars.port
-}
