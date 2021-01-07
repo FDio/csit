b03610781b8116cd7e59937258d8e2bf01bdb4aa
pmikus <pmikus@cisco.com>
2021-01-07 14:27:38 +0000

    Infra: Monitoring capability
    + Monitoring SOA
      + Nomad alertmanager job
      + Nomad prometheus job
      + Nomad grafana job
    
    Signed-off-by: pmikus <pmikus@cisco.com>
    Change-Id: I0b32e9c87276ba1a2d4a5322816f3473c737eae2

---
+++ terraform-ci-infra/1n_nmd/alertmanager/variables.tf
@@ -0,0 +1,84 @@
+# Nomad
+variable "nomad_datacenters" {
+  description = "Nomad data centers"
+  type        = list(string)
+  default     = [ "dc1" ]
+}
+
+# Alermanager
+variable "alertmanager_job_name" {
+  description = "Job name"
+  type        = string
+  default     = "alertmanager"
+}
+
+variable "alertmanager_group_count" {
+  description = "Number of group instances"
+  type        = number
+  default     = 1
+}
+
+variable "alertmanager_service_name" {
+  description = "Service name"
+  type        = string
+  default     = "alertmanager"
+}
+
+variable "alertmanager_version" {
+  description = "Version"
+  type        = string
+  default     = "0.21.0"
+}
+
+variable "alertmanager_use_canary" {
+  description = "Uses canary deployment"
+  type        = bool
+  default     = false
+}
+
+variable "alertmanager_vault_secret" {
+  description = "Set of properties to be able to fetch secret from vault"
+  type        = object({
+    use_vault_provider        = bool,
+    vault_kv_policy_name      = string,
+    vault_kv_path             = string,
+    vault_kv_field_access_key = string,
+    vault_kv_field_secret_key = string
+  })
+}
+
+variable "alertmanager_cpu" {
+  description = "CPU allocation"
+  type        = number
+  default     = 1000
+}
+
+variable "alertmanager_mem" {
+  description = "RAM allocation"
+  type        = number
+  default     = 1024
+}
+
+variable "alertmanager_port" {
+  description = "TCP allocation"
+  type        = number
+  default     = 9093
+}
+
+variable "alertmanager_default_receiver" {
+  description = "Alertmanager default receiver"
+  type        = string
+  default     = "default-receiver"
+}
+
+variable "alertmanager_slack_api_url" {
+  description = "Alertmanager slack API URL"
+  type        = string
+  default     = "https://hooks.slack.com/services/XXXXXXXXX/XXXXXXXXXXX/XXXXXXXXXXXXXXXXXXXXXXXX"
+}
+
+variable "alertmanager_slack_channel" {
+  description = "Alertmanager slack channel"
+  type        = string
+  default     = "slack-channel"
+}
