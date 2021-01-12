1631e05221bea943385eed5043bcf2c773146ad1
Vratko Polak <vrpolak@cisco.com>
2021-01-12 16:28:42 +0100

    Add 3n ip4-rnd tests
    + Rename traffic profiles to avoid mentioning number of nodes.
    + Improve 2n rnd suite documentation slightly.
    
    Change-Id: I82d6fb6a99133163a58d56f2acf8a7b9568ee77c
    Signed-off-by: Vratko Polak <vrpolak@cisco.com>

--- GPL/traffic_profiles/trex/trex-stl-ethip4-ip4dst-rnd100000.py
+++ GPL/traffic_profiles/trex/trex-stl-ethip4-ip4dst-rnd100000.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2020 Cisco and/or its affiliates.
+# Copyright (c) 2021 Cisco and/or its affiliates.
 #
 # SPDX-License-Identifier: Apache-2.0 OR GPL-2.0-or-later
 #
@@ -28,10 +28,10 @@ Stream profile:
  - Packet: ETH / IP /
  - Direction 0 --> 1:
    - Source IP address range:      10.0.0.1
-   - Destination IP address range: 20.0.0.0 - 20.15.66.63
+   - Destination IP address range: 20.0.0.0 - 20.1.134.159
  - Direction 1 --> 0:
    - Source IP address range:      20.0.0.1
-   - Destination IP address range: 10.0.0.0 - 10.15.66.63
+   - Destination IP address range: 10.0.0.0 - 10.1.134.159
 """
 
 from trex.stl.api import *
@@ -49,11 +49,11 @@ class TrafficStreams(TrafficStreamsBaseClass):
         # IPs used in packet headers.
         self.p1_src_start_ip = u"10.0.0.1"
         self.p1_dst_start_ip = u"20.0.0.0"
-        self.p1_dst_end_ip = u"20.15.66.63"
+        self.p1_dst_end_ip = u"20.1.134.159"
 
         self.p2_src_start_ip = u"20.0.0.1"
         self.p2_dst_start_ip = u"10.0.0.0"
-        self.p2_dst_end_ip = u"10.15.66.63"
+        self.p2_dst_end_ip = u"10.1.134.159"
 
     def define_packets(self):
         """Defines the packets to be sent from the traffic generator.
@@ -91,7 +91,7 @@ class TrafficStreams(TrafficStreamsBaseClass):
                     min_value=self.p1_dst_start_ip,
                     max_value=self.p1_dst_end_ip,
                     size=4,
-                    limit=1000000
+                    limit=100000
                 ),
                 STLVmWrFlowVar(
                     fv_name=u"dst",
@@ -110,7 +110,7 @@ class TrafficStreams(TrafficStreamsBaseClass):
                     min_value=self.p2_dst_start_ip,
                     max_value=self.p2_dst_end_ip,
                     size=4,
-                    limit=1000000
+                    limit=100000
                 ),
                 STLVmWrFlowVar(
                     fv_name=u"dst",
