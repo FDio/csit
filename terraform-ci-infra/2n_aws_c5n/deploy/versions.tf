9c6bcc46dfc6c98e32aa79de608db93963666177
Tomas Alexy <tomas.alexy@pantheon.tech>
2021-01-28 08:59:22 +0100

    Infra: Fix AWS deployment
    Signed-off-by: Tomas Alexy <tomas.alexy@pantheon.tech>
    Change-Id: Ie24f5fac5827e28b1ac7c22192a94994700b2910

---
+++ terraform-ci-infra/2n_aws_c5n/deploy/versions.tf
@@ -0,0 +1,17 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.26.0"
+    }
+    null = {
+      source  = "hashicorp/null"
+      version = "~> 3.0.0"
+    }
+    tls = {
+      source  = "hashicorp/tls"
+      version = "~> 3.0.0"
+    }
+  }
+  required_version = ">= 0.13"
+}
