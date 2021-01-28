635524ea18b492c9ad9a4ca41eb66e8137f6aa69
Vratko Polak <vrpolak@cisco.com>
2021-01-28 17:05:06 +0100

    WiP: Really multiple MLRsearch
    + Library edits drafted.
    + Robot integration drafted.
    - Debugging in progress.
    - Update commit message.
    - TODO: Use "fraction" instead of "ratio" everywhere.
    
    Change-Id: I93f693b4f186f59030ee5ac21b78acc890109813
    Signed-off-by: Vratko Polak <vrpolak@cisco.com>

---
+++ resources/libraries/python/MLRsearch/MeasurementDatabase.py
@@ -0,0 +1,247 @@
+# Copyright (c) 2021 Cisco and/or its affiliates.
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at:
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Module defining MeasurementDatabase class."""
+
+import logging
+
+from .ReceiveRateMeasurement import ReceiveRateMeasurement
+from .ReceiveRateInterval import ReceiveRateInterval
+
+
+class MeasurementDatabase:
+    """Dict-like structure holding measurement results.
+
+    Several utility methods are added, accomplishing tasks useful for MLRsearch.
+    All methods assume there is a non-zero number of measurements.
+
+    This class contains most of replacement and selection logic required
+    for MLRsearch. One exception is lack of any special handling for maximal
+    or minimal rates.
+    """
+
+    def __init__(self, measurements):
+        """Store (copy of) measurement results and normalize them.
+
+        :param measurements: The measurement results to store.
+        :type measurements: Iterable[ReceiveRateMeasurement]
+        """
+        self.measurements = [measurement.copy() for measurement in measurements]
+        self._normalize()
+
+    def __repr__(self):
+        """Return string executable to get equivalent instance.
+
+        :returns: Code to construct equivalent instance.
+        :rtype: str
+        """
+        return f"MeasurementDatabase(measurements={self.measurements!r})"
+
+    def _normalize(self):
+        """Sort by target_tr, keep only one result per target_tr.
+
+        Also set effective loss fractions.
+
+        If durations differ, keep the longer duration result.
+        If loss fractions differ, keep the more lossy result.
+        Otherwise keep the most recent result.
+        """
+        measurements_old = self.measurements
+        measurements_new = list()
+        while len(measurements_old) > 0:
+            # Find next bext measurement.
+            best_measurement = None
+            best_tr = None
+            best_index = None
+            for index, measurement in enumerate(measurements_old):
+                if best_index is None:
+                    best_index, best_measurement = index, measurement
+                    best_tr = best_measurement.target_tr
+                    continue
+                measured_tr = measurement.target_tr
+                if measured_tr > best_tr:
+                    continue
+                if measured_tr == best_tr:
+                    if measurement.duration >= best_measurement.duration:
+                        best_index, best_measurement = index, measurement
+                        best_tr = best_measurement.target_tr
+                        continue
+                # measured_tr < best_tr
+                best_index, best_measurement = index, measurement
+                best_tr = best_measurement.target_tr
+            measurements_new.append(best_measurement)
+            # Remove duplicates.
+            measurements_old = [
+                measurement for measurement in measurements_old
+                if measurement.target_tr > best_tr
+            ]
+        # Update effective fractions.
+        fraction_previous = None
+        for measurement in measurements_new:
+            if fraction_previous is None:
+                fraction_previous = measurement.loss_fraction
+                measurement.effective_loss_fraction = fraction_previous
+                continue
+            fraction_previous = max(fraction_previous, measurement.loss_fraction)
+            measurement.effective_loss_fraction = fraction_previous
+        self.measurements = measurements_new
+
+    def add(self, measurement):
+        """Add measurement and normalize.
+
+        :param measurement: Measurement result to add to the database.
+        :type measurement: ReceiveRateMeasurement
+        """
+        self.measurements.append(measurement)
+        self._normalize()
+
+    def get_valid_lower_bound(self, ratio, duration=None):
+        """Return None or a valid lower bound measurement.
+
+        Two modes are supported. If duration is None, any measurement is used.
+        If duration is not None, only long enough measurements are used.
+
+        :param ratio: Target ratio, valid has to be lower or equal.
+        :param duration: Target duration [s], valid has to be higher or equal.
+        :type ratio: float
+        :type duration: float
+        :returns: Whether database has measurement good for valid lower bound.
+        :rtype: ReceiveRateMeasurement
+        """
+        bound = None
+        for measurement in self.measurements:
+            if measurement.effective_loss_fraction > ratio:
+                # Measurements are sorted, so no point searching further.
+                break
+            if duration is None or measurement.duration >= duration:
+                bound = measurement
+                # Continuing to find a tighter bound.
+        return bound
+
+    def get_valid_upper_bound(self, ratio, duration=None):
+        """Return None or a valid upper bound measurement.
+
+        Two modes are supported. If duration is None, any measurement is used.
+        If duration is not None, only long enough measurements are used.
+
+        :param ratio: Target ratio, valid has to be lower or equal.
+        :param duration: Target duration [s], valid has to be higher or equal.
+        :type ratio: float
+        :type duration: float
+        :returns: Whether database has measurement good for valid lower bound.
+        :rtype: ReceiveRateMeasurement
+        """
+        bound = None
+        for measurement in reversed(self.measurements):
+            if measurement.effective_loss_fraction < ratio:
+                # Measurements are sorted, so no point searching further.
+                break
+            if duration is None or measurement.duration >= duration:
+                bound = measurement
+                # Continuing to find a tighter bound.
+        return bound
+
+    def select_tr_for_lower_bound(self, ratio, min_rate, max_rate):
+        """Choose next target load based on conditions and measurements.
+
+        This is a very specific method to be used by MLRsearch.
+        As measurements from smaller durations are likely to have similar
+        loss fraction upon re-measuring at higher durations,
+        this method attemps to find such a measurement and return its target_tr.
+        The measurement has to have low enough effective loss fraction.
+        MLRsearch gives min_rate and max_rate as additional conditions,
+        due to window width reasons. If no measurement fits in the interval
+        (target_tr equal to min_rate or max_rate fits), return min_rate.
+        If multiple measurements fit, return the largest target_tr.
+
+        :param ratio: Loss ratio, measurements need to have equal or smaller.
+        :param min_rate: Minimal transmit rate eligible for selection.
+        :param max_rate: Maximal transmit rate eligible for selection.
+        :type ratio: float
+        :type min_rate: float
+        :type max_rate: float
+        :returns: Next candidate transmit rate fitting the conditions.
+        :rtype: float
+        """
+        for measurement in reversed(self.measurements):
+            measured_tr = measurement.target_tr
+            if measured_tr > max_rate:
+                continue
+            if measured_tr < min_rate:
+                break
+            measured_fraction = measurement.effective_loss_fraction
+            if measured_fraction > ratio:
+                continue
+            return measured_tr
+        return min_rate
+
+    def select_tr_for_upper_bound(self, ratio, min_rate, max_rate):
+        """Choose next target load based on conditions and measurements.
+
+        This is a very specific method to be used by MLRsearch.
+        As measurements from smaller durations are likely to have similar
+        loss fraction upon re-measuring at higher durations,
+        this method attemps to find such a measurement and return its target_tr.
+        The measurement has to have high enough effective loss fraction.
+        MLRsearch gives min_rate and max_rate as additional conditions,
+        due to window width reasons. If no measurement fits in the interval
+        (target_tr equal to min_rate or max_rate fits), return max_rate.
+        If multiple measurements fit, return the smallest target_tr.
+
+        :param ratio: Loss ratio, measurements need to have equal or larger.
+        :param min_rate: Minimal transmit rate eligible for selection.
+        :param max_rate: Maximal transmit rate eligible for selection.
+        :type ratio: float
+        :type min_rate: float
+        :type max_rate: float
+        :returns: Next candidate transmit rate fitting the conditions.
+        :rtype: float
+        """
+        for measurement in self.measurements:
+            measured_tr = measurement.target_tr
+            if measured_tr < min_rate:
+                continue
+            if measured_tr > max_rate:
+                break
+            measured_fraction = measurement.effective_loss_fraction
+            if measured_fraction < ratio:
+                continue
+            return measured_tr
+        return max_rate
+
+    def get_results(self, duration, ratio_list):
+        """Return list of interval for given ratios.
+
+        Attempt to construct valid intervals. If a valid bound is missing,
+        use smallest/biggest target_tr for lower/upper bound.
+        This can result in degenerate intervals.
+        The current implementation honors duration for valid bounds,
+        but not for invalid bounds.
+
+        :param duration: Ignore measurements shorter than this [s].
+        :param ratio_list: Ratios to create intervals for.
+        :type duration: float
+        :type ratio_list: Iterable[float]
+        :returns: List of intervals.
+        :rtype: List[ReceiveRateInterval]
+        """
+        ret_list = list()
+        for ratio in ratio_list:
+            lower_bound = self.get_valid_lower_bound(ratio, duration)
+            if lower_bound is None:
+                lower_bound = self.measurements[0]
+            upper_bound = self.get_valid_upper_bound(ratio, duration)
+            if upper_bound is None:
+                upper_bound = self.measurements[-1]
+            ret_list.append(ReceiveRateInterval(lower_bound, upper_bound))
+        return ret_list
