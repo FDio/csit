38f1274ac136690650d42ab1ffa58a592841b304
pmikus <pmikus@cisco.com>
2021-02-05 14:51:43 +0000

    Infra: JenkinsJobHealthExporter
    - Integration of Jenkins Job checker
    
    Signed-off-by: pmikus <pmikus@cisco.com>
    Change-Id: I822039cb64a3a352b49314ddab7c6099af3fe644

--- terraform-ci-infra/1n_nmd/main.tf
+++ terraform-ci-infra/1n_nmd/main.tf
@@ -5,31 +5,33 @@
 # and have them automatically associated with the root provider
 # configurations.
 module "alertmanager" {
-  source                         = "./alertmanager"
-  providers                      = {
+  source                             = "./alertmanager"
+  providers                          = {
     nomad = nomad.yul1
   }
 
   # nomad
-  nomad_datacenters              = [ "yul1" ]
+  nomad_datacenters                  = [ "yul1" ]
 
   # alertmanager
-  alertmanager_job_name          = "prod-alertmanager"
-  alertmanager_use_canary        = true
-  alertmanager_group_count       = 1
-  alertmanager_vault_secret      = {
-    use_vault_provider           = false,
-    vault_kv_policy_name         = "kv-secret",
-    vault_kv_path                = "secret/data/prometheus",
-    vault_kv_field_access_key    = "access_key",
-    vault_kv_field_secret_key    = "secret_key"
+  alertmanager_job_name              = "prod-alertmanager"
+  alertmanager_use_canary            = true
+  alertmanager_group_count           = 1
+  alertmanager_vault_secret          = {
+    use_vault_provider               = false,
+    vault_kv_policy_name             = "kv-secret",
+    vault_kv_path                    = "secret/data/prometheus",
+    vault_kv_field_access_key        = "access_key",
+    vault_kv_field_secret_key        = "secret_key"
   }
-  alertmanager_version           = "0.21.0"
-  alertmanager_cpu               = 1000
-  alertmanager_mem               = 1024
-  alertmanager_port              = 9093
-  alertmanager_slack_api_key     = "TE07RD1V1/B01L7PQK9S8/pbADGhhhj60JSxHRi3K0NoW6"
-  alertmanager_slack_channel     = "fdio-infra-monitoring"
+  alertmanager_version               = "0.21.0"
+  alertmanager_cpu                   = 1000
+  alertmanager_mem                   = 1024
+  alertmanager_port                  = 9093
+  alertmanager_slack_jenkins_api_key = "TE07RD1V1/B01LPL8KM0F/JZQh0gF5U5SoNYnedBknzdHz"
+  alertmanager_slack_jenkins_channel = "fdio-jobs-monitoring"
+  alertmanager_slack_default_api_key = "TE07RD1V1/B01L7PQK9S8/pbADGhhhj60JSxHRi3K0NoW6"
+  alertmanager_slack_default_channel = "fdio-infra-monitoring"
 }
 
 module "grafana" {
