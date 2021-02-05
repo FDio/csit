277b661ff9da38336ea0aca44d58bc8fd3e47833
pmikus <pmikus@cisco.com>
2021-02-05 14:51:43 +0000

    Infra: JenkinsJobHealthExporter
    - Integration of Jenkins Job checker
    
    Signed-off-by: pmikus <pmikus@cisco.com>
    Change-Id: I822039cb64a3a352b49314ddab7c6099af3fe644

--- terraform-ci-infra/1n_nmd/alertmanager/main.tf
+++ terraform-ci-infra/1n_nmd/alertmanager/main.tf
@@ -14,20 +14,23 @@ locals {
 data "template_file" "nomad_job_alertmanager" {
   template         = file("${path.module}/conf/nomad/alertmanager.hcl")
   vars             = {
-    datacenters        = local.datacenters
-    url                = local.alertmanager_url
-    job_name           = var.alertmanager_job_name
-    use_canary         = var.alertmanager_use_canary
-    group_count        = var.alertmanager_group_count
-    service_name       = var.alertmanager_service_name
-    use_vault_provider = var.alertmanager_vault_secret.use_vault_provider
-    version            = var.alertmanager_version
-    cpu                = var.alertmanager_cpu
-    mem                = var.alertmanager_mem
-    port               = var.alertmanager_port
-    slack_api_key      = var.alertmanager_slack_api_key
-    slack_channel      = var.alertmanager_slack_channel
-    default_receiver   = var.alertmanager_default_receiver
+    datacenters            = local.datacenters
+    url                    = local.alertmanager_url
+    job_name               = var.alertmanager_job_name
+    use_canary             = var.alertmanager_use_canary
+    group_count            = var.alertmanager_group_count
+    service_name           = var.alertmanager_service_name
+    use_vault_provider     = var.alertmanager_vault_secret.use_vault_provider
+    version                = var.alertmanager_version
+    cpu                    = var.alertmanager_cpu
+    mem                    = var.alertmanager_mem
+    port                   = var.alertmanager_port
+    slack_jenkins_api_key  = var.alertmanager_slack_jenkins_api_key
+    slack_jenkins_channel  = var.alertmanager_slack_jenkins_channel
+    slack_jenkins_receiver = var.alertmanager_slack_jenkins_receiver
+    slack_default_api_key  = var.alertmanager_slack_default_api_key
+    slack_default_channel  = var.alertmanager_slack_default_channel
+    slack_default_receiver = var.alertmanager_slack_default_receiver
   }
 }
 
