8777939b9172c4d9aaabc17bb56df192475a5d44
Juraj Linkeš <juraj.linkes@pantheon.tech>
2021-02-16 13:59:59 +0100

    IPsec: add 2n crypto udir perf tests
    Add unidirectional 2n crypto tests. Only one direction can be tested on
    a 2 node topology, since we can't use the same interface for both
    encrypted and unencrypted traffic. Add the following tests:
    * {n}tnlsw-ip4base-int ndrpdr tests
    * {n}tnlsw-1atnl-ip4base-int reconf tests
    * {n}tnlswasync-scheduler-ip4base-int ndrpdr tests
    
    Where n is the number of tunnels:
    1, 4, 40, 400, 1000, 5000, 10000, 20000, 40000, 60000 for the first two
    1, 2, 4, 8 for the async scheduler tests
    
    All of these with the following ecryption-auth algorithms:
    aes128gcm
    aes256gcm
    aes128cbc-hmac256sha
    aes128cbc-hmac512sha
    
    Also add the corresponding trex profiles:
    trex-stl-2n-ethip4-ip4dst{n}-udir.py
    
    Where n is the number of tunnels listed above. The profiles are shared
    among the tests.
    
    Change-Id: I22bb46e6ad59801581a78aa19310bee8a5293e56
    Signed-off-by: Juraj Linkeš <juraj.linkes@pantheon.tech>
    (cherry picked from commit b3eaa178dbcf40736471303ef8efa334264d05c2)

---
+++ GPL/traffic_profiles/trex/trex-stl-2n-ethip4-ip4dst1-udir.py
@@ -0,0 +1,84 @@
+# Copyright (c) 2021 Cisco and/or its affiliates.
+#
+# SPDX-License-Identifier: Apache-2.0 OR GPL-2.0-or-later
+#
+# Licensed under the Apache License 2.0 or
+# GNU General Public License v2.0 or later;  you may not use this file
+# except in compliance with one of these Licenses. You
+# may obtain a copy of the Licenses at:
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#     https://www.gnu.org/licenses/old-licenses/gpl-2.0-standalone.html
+#
+# Note: If this file is linked with Scapy, which is GPLv2+, your use of it
+# must be under GPLv2+.  If at any point in the future it is no longer linked
+# with Scapy (or other GPLv2+ licensed software), you are free to choose
+# Apache 2.
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Stream profile for T-rex traffic generator.
+
+Stream profile:
+ - Two streams sent in directions 0 --> 1 and 1 --> 0 at the same time.
+ - Packet: ETH / IP /
+ - Direction 0 --> 1:
+   - Source IP address:      10.0.0.1
+   - Destination IP address: 20.0.0.0
+"""
+
+from trex.stl.api import *
+from profile_trex_stateless_base_class import TrafficStreamsBaseClass
+
+
+class TrafficStreams(TrafficStreamsBaseClass):
+    """Stream profile."""
+
+    def __init__(self):
+        """Initialization and setting of streams' parameters."""
+
+        super(TrafficStreamsBaseClass, self).__init__()
+
+        # IPs used in packet headers.
+        self.p1_src_ip = u"10.0.0.1"
+        self.p1_dst_ip = u"20.0.0.0"
+
+    def define_packets(self):
+        """Defines the packets to be sent from the traffic generator.
+
+        Packet definition: | ETH | IP |
+
+        :returns: Packets to be sent from the traffic generator.
+        :rtype: tuple
+        """
+
+        # Direction 0 --> 1
+        base_pkt_a = (
+            Ether() /
+            IP(
+                src=self.p1_src_ip,
+                dst=self.p1_dst_ip,
+                proto=61
+            )
+        )
+        # Direction 1 --> 0
+        base_pkt_b = (
+            Ether()
+        )
+
+        return base_pkt_a, base_pkt_b, None, None
+
+
+def register():
+    """Register this traffic profile to T-rex.
+
+    Do not change this function.
+
+    :return: Traffic streams.
+    :rtype: Object
+    """
+    return TrafficStreams()
