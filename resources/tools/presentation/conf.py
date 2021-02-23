1d5c4cbf4f4ab1cba438b1b6d132eef5cdb59ae4
Tibor Frank <tifrank@cisco.com>
2021-02-23 11:57:27 +0100

    Report: Set .08 revision, fix GSO graphs
    Change-Id: I5948dbaf69d183caddbefd16f2198b54a24ff904
    Signed-off-by: Tibor Frank <tifrank@cisco.com>
    (cherry picked from commit 5f0782b8bd0db96a6496d55f44d157146b08de12)

--- resources/tools/presentation/conf.py
+++ resources/tools/presentation/conf.py
@@ -46,7 +46,7 @@ source_suffix = [u'.rst', u'.md']
 master_doc = u'index'
 
 # General information about the project.
-report_week = u'07'
+report_week = u'08'
 project = u'FD.io CSIT-2101.{week}'.format(week=report_week)
 copyright = u'2021, FD.io'
 author = u'FD.io CSIT'
