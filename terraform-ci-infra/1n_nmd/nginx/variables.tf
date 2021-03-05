7c538a63347674ac091813da6fd82dee80524339
pmikus <pmikus@cisco.com>
2021-03-05 11:48:19 +0000

    Infra: Move nginx service closer to storage
    Signed-off-by: pmikus <pmikus@cisco.com>
    Change-Id: I3bbaa6c458e7994e964f7a07f69f9dab008a48fd

--- terraform-ci-infra/1n_nmd/nginx/variables.tf
+++ terraform-ci-infra/1n_nmd/nginx/variables.tf
@@ -5,9 +5,21 @@ variable "nomad_datacenters" {
   default     = [ "dc1" ]
 }
 
+variable "nomad_host_volume" {
+  description = "Nomad Host Volume"
+  type        = string
+  default     = "persistence"
+}
+
 # Nginx
 variable "nginx_job_name" {
   description = "Nginx job name"
   type        = string
   default     = "nginx"
+}
+
+variable "nginx_use_host_volume" {
+  description = "Use Nomad host volume feature"
+  type        = bool
+  default     = false
 }
