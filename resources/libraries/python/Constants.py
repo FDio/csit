699c4cad1f28b931a637be0bf365d0ab1e08f7d5
pmikus <pmikus@cisco.com>
2021-03-16 06:51:48 +0000

    Infra: Upgrade csit-dut image
    Signed-off-by: pmikus <pmikus@cisco.com>
    Change-Id: I0341a1564ba510acf46bda3e24225209abef2f82
    (cherry picked from commit 480e1675f3d8e3045265b351681169a1478dbc7d)

--- resources/libraries/python/Constants.py
+++ resources/libraries/python/Constants.py
@@ -179,10 +179,10 @@ class Constants:
     QEMU_VM_DPDK = u"/opt/dpdk-20.02"
 
     # Docker container SUT image
-    DOCKER_SUT_IMAGE_UBUNTU = u"csit_sut-ubuntu1804:local"
+    DOCKER_SUT_IMAGE_UBUNTU = u"csit_sut-ubuntu2004:local"
 
     # Docker container arm SUT image
-    DOCKER_SUT_IMAGE_UBUNTU_ARM = u"csit_sut-ubuntu1804:local"
+    DOCKER_SUT_IMAGE_UBUNTU_ARM = u"csit_sut-ubuntu2004:local"
 
     # TRex install directory
     TREX_INSTALL_DIR = u"/opt/trex-core-2.88"
