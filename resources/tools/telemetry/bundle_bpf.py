1c3bc94f2d75ee6775a132611a11f91967176a3c
pmikus <pmikus@cisco.com>
2021-04-19 12:22:20 +0000

    Framework: Telemetry retake
    Signed-off-by: pmikus <pmikus@cisco.com>
    Change-Id: I2f019a083916aec9f7816266f6ad5b92dcc31fa0
    (cherry picked from commit d255d2545ee6cdc871bc35314fad72c3c48b225b)

---
+++ resources/tools/telemetry/bundle_bpf.py
@@ -0,0 +1,112 @@
+# Copyright (c) 2021 Cisco and/or its affiliates.
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at:
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""BPF performance bundle."""
+
+from logging import getLogger
+import sys
+
+from bcc import BPF
+
+
+class BundleBpf:
+    """
+    Creates a BPF object. This is the main object for defining a BPF program,
+    and interacting with its output.
+
+    Syntax: BPF({text=BPF_program | src_file=filename}
+                [, usdt_contexts=[USDT_object, ...]]
+                [, cflags=[arg1, ...]] [, debug=int]
+            )
+
+    Exactly one of text or src_file must be supplied (not both).
+    """
+    def __init__(self, program, serializer, hook):
+        """Initialize Bundle BPF Perf event class.
+
+        :param program: BPF C code.
+        :param serializer: Metric serializer.
+        :param hook: Process ID.
+        :type program: dict
+        :type serializer: Serializer
+        :type hook: int
+        """
+        self.obj = None
+        self.code = program[u"code"]
+        self.metrics = program[u"metrics"]
+        self.events = program[u"events"]
+        self.api_replies_list = list()
+        self.serializer = serializer
+        self.hook = hook
+
+        self.obj = BPF(text=self.code)
+
+    def attach(self, duration):
+        """
+        Attach events to BPF.
+
+        :param duration: Trial duration.
+        :type duration: int
+        """
+        try:
+            for event in self.events:
+                self.obj.attach_perf_event(
+                    ev_type=event[u"type"],
+                    ev_config=event[u"name"],
+                    fn_name=event[u"target"],
+                    sample_period=duration
+                )
+        except AttributeError:
+            getLogger(__name__).error(u"Cannot attach BPF events!")
+            sys.exit(1)
+
+    def detach(self):
+        """
+        Dettach events from BPF.
+        """
+        try:
+            for event in self.events:
+                self.obj.detach_perf_event(
+                    ev_type=event[u"type"],
+                    ev_config=event[u"name"]
+                )
+        except AttributeError:
+            getLogger(__name__).error(u"Cannot dettach BPF events!")
+            sys.exit(1)
+
+    def fetch_data(self):
+        """
+        Fetch data by invoking API calls to BPF.
+        """
+        self.serializer.create(metrics=self.metrics)
+        for _, metric_list in self.metrics.items():
+            for metric in metric_list:
+                for (key, val) in self.obj.get_table(metric[u"name"]).items():
+                    item = dict()
+                    labels = dict()
+                    item[u"name"] = metric[u"name"]
+                    item[u"value"] = val.value
+                    for label in metric[u"labels"]:
+                        labels[label] = getattr(key, label)
+                    item[u"labels"] = labels
+                    self.api_replies_list.append(item)
+                    getLogger(__name__).info(item)
+
+    def process_data(self):
+        """
+        Post process API replies.
+        """
+        for item in self.api_replies_list:
+            self.serializer.serialize(
+                metric=item[u"name"], labels=item[u"labels"], item=item
+            )
---
+++ resources/tools/telemetry/serializer.py
@@ -0,0 +1,110 @@
+# Copyright (c) 2021 Cisco and/or its affiliates.
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at:
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Config executor library."""
+
+from importlib import import_module
+from logging import getLogger
+
+
+class Serializer:
+    """
+    Executor class reponsible for executing configuration.
+    """
+    def __init__(self):
+        """
+        Config Executor init.=
+        """
+        self.metric_registry = dict()
+
+    def create(self, metrics):
+        """
+        Create metrics based on input configuration.
+
+        :param metrics: Metric list to create.
+        :type metrics: list
+        """
+        for metric_type, metric_list in metrics.items():
+            for metric in metric_list:
+                module = import_module(
+                    name=u"telemetry.metrics", package=metric_type.capitalize()
+                )
+                self.metric_registry[metric[u"name"]] = getattr(
+                    module, metric_type.capitalize()
+                )(**metric)
+
+    def serialize(self, metric, labels, item):
+        """
+        Serialize metric into destination format.
+
+        :param metrics: Metric name.
+        :param labels: Metric labels.
+        :param item: Metric dict.
+        :type metrics: str
+        :type labels: dict
+        :type item: dict
+        """
+        if type(self.metric_registry[metric]).__name__ == u"Counter":
+            self.metric_registry[metric].labels(**labels).inc(
+                float(item[u"value"])
+            )
+        if type(self.metric_registry[metric]).__name__ == u"Gauge":
+            self.metric_registry[metric].labels(**labels).set(
+                float(item[u"value"])
+            )
+        if type(self.metric_registry[metric]).__name__ == u"Info":
+            self.metric_registry[metric].labels(**labels).info(
+                item[u"value"]
+            )
+
+    def publish(self):
+        """
+        Publish metric into logger.
+        """
+        output = []
+        for _, metric_list in self.metric_registry.items():
+            for metric in metric_list.collect():
+                mname = metric.name
+                mtype = metric.type
+
+                # Adjust from OpenMetrics into Prometheus format.
+                mname = f"{mname}_total" if mtype == u"counter" else mname
+                mname = f"{mname}_info" if mtype == u"info" else mname
+                if mtype in (u"info", u"stateset"):
+                    mtype = u"gauge"
+                if mtype in (u"gaugehistogram", u"histogram"):
+                    mtype = u"histogram"
+
+                mdocumentation = metric.documentation.replace(u"\\", r"\\")
+                mdocumentation = mdocumentation.replace(u"\n", r"\n")
+                output.append(f"# HELP {mname} {mdocumentation}\n")
+                output.append(f"# TYPE {mname} {mtype}\n")
+
+                for line in metric.samples:
+                    if line.labels:
+                        llabel = []
+                        for k, value in sorted(line.labels.items()):
+                            value = value.replace(u"\\", r"\\")
+                            value = value.replace(u"\n", r"\n")
+                            value = value.replace(u'"', r'\"')
+                            llabel.append(f'{k}="{value}"')
+                        labelstr = f"{{{','.join(llabel)}}}"
+                    else:
+                        labelstr = u""
+
+                    timestamp = f" {int(float(line.timestamp) * 1000):d}" \
+                        if line.timestamp else u""
+                    output.append(
+                        f"{line.name}{labelstr} {line.value}{timestamp}\n"
+                    )
+        getLogger(u"prom").info(u"".join(output))
