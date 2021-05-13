8183df216a8eae9fed092069d1fc9487f22518bc
Vratko Polak <vrpolak@cisco.com>
2021-05-13 18:54:40 +0200

    [reopen] VPP build: Archive compile commands
    Change-Id: Id4184c7c7c16657a046a8c56a90c7231c7538b9c
    Signed-off-by: Vratko Polak <vrpolak@cisco.com>

--- resources/libraries/bash/function/per_patch.sh
+++ resources/libraries/bash/function/per_patch.sh
@@ -96,7 +96,11 @@ function build_vpp_ubuntu_amd64 () {
              "using build default ($(grep -c ^processor /proc/cpuinfo))."
     fi
 
-    make UNATTENDED=y pkg-verify || die "VPP build using make pkg-verify failed."
+    export CMAKE_EXPORT_COMPILE_COMMANDS=ON
+    make UNATTENDED=y pkg-verify || {
+        die "VPP build command failed."
+    }
+    mv "compile_commands.json" "${ARCHIVE_DIR}/compile_${1-}.json.log" || true
     echo "* VPP ${1-} BUILD SUCCESSFULLY COMPLETED" || {
         die "Argument not found."
     }
