05f429e08c76c4a2e8161d775cdb8fecd44d1b89
Vratko Polak <vrpolak@cisco.com>
2021-05-13 19:12:55 +0200

    DO_NOT_MERGE: Build VPP in debug mode
    + Enable extended_debug to get packet trace.
    + Lower pps limit.
    + Lower number of transaction in ASTF profile.
    + Increase number of packets in packet trace.
    + Lower ramp-up rates.
    
    Change-Id: I443c72734ed87afe4c6306042bff69c9cc489e9d
    Signed-off-by: Vratko Polak <vrpolak@cisco.com>

--- GPL/traffic_profiles/trex/trex-astf-ethip4tcp-1024h.py
+++ GPL/traffic_profiles/trex/trex-astf-ethip4tcp-1024h.py
@@ -108,7 +108,7 @@ class TrafficProfile(TrafficProfileBaseClass):
         temp_c = ASTFTCPClientTemplate(
             program=prog_c,
             ip_gen=ip_gen,
-            limit=64512,  # TODO: set via input parameter
+            limit=5000,  # TODO: set via input parameter
             port=8080
         )
         temp_s = ASTFTCPServerTemplate(program=prog_s, assoc=s_assoc)
