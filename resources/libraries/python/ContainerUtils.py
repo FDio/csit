3ca17fddae05b5ee427b9a2f372b6d1ac8078ef5
Vratko Polak <vrpolak@cisco.com>
2021-07-01 12:28:46 +0200

    Topology: Log any edit of "nodes" global state
    + Introduce ProtectedDict type.
     + Also ProtectedList class and protect_object function.
    + ${nodes} is protected, so most untracked edits will fail.
     + Tracked edits are always logged (TRACE level for now).
     + Tracked edits require "reason" message, it is logged.
     + Moved few external writes into Topology for tracking.
    + More specific data types are introduced.
     + SubNodeDict, NodeDict, NodesDict.
     - Not actually required by the current implementation.
     + Used in docstrings to clarify intent.
    + Do not read topology file on import.
     + Explicitly initialize from top-level suite setup.
    + Correctly behaving callers should not see any impact of this refactor.
     - Maybe scale test with many interfaces will run somewhat slower.
    - Some TODOs.
     + Some for later changes, e.g. JSON export.
     - Some should be addressed during review.
    
    Change-Id: Ice7fbc562a70c7d0a5a43e026e0a11d80d7808ef
    Signed-off-by: Vratko Polak <vrpolak@cisco.com>

--- resources/libraries/python/ContainerUtils.py
+++ resources/libraries/python/ContainerUtils.py
@@ -19,6 +19,8 @@ from re import search
 from string import Template
 from time import sleep
 
+from robot.api import logger
+
 from resources.libraries.python.Constants import Constants
 from resources.libraries.python.CpuUtils import CpuUtils
 from resources.libraries.python.PapiExecutor import PapiSocketExecutor
@@ -66,7 +68,7 @@ class ContainerManager:
         except KeyError:
             raise RuntimeError(f"Failed to get container with name: {name}")
 
-    def construct_container(self, **kwargs):
+    def construct_container(self, kwargs):
         """Construct container object on node with specified parameters.
 
         :param kwargs: Key-value pairs used to construct container.
@@ -87,7 +89,7 @@ class ContainerManager:
         # Store container instance
         self.containers[kwargs[u"name"]] = self.engine.container
 
-    def construct_containers(self, **kwargs):
+    def construct_containers(self, kwargs):
         """Construct 1..N container(s) on node with specified name.
 
         Ordinal number is automatically added to the name of container as
@@ -101,7 +103,7 @@ class ContainerManager:
             # Name will contain ordinal suffix
             kwargs[u"name"] = u"".join([name, str(i+1)])
             # Create container
-            self.construct_container(i=i, **kwargs)
+            self.construct_container(dict(i=i, **kwargs))
 
     def acquire_all_containers(self):
         """Acquire all containers."""
@@ -583,16 +585,14 @@ class ContainerEngine:
             u"setsid /usr/bin/vpp -c /etc/vpp/startup.conf "
             u">/tmp/vppd.log 2>&1 < /dev/null &")
 
-        topo_instance = get_library_instance(
-            u"resources.libraries.python.topology.Topology"
-        )
-        topo_instance.add_new_socket(
+        logger.trace(f"container node {self.container.node!r}")
+        Topology.add_new_socket(
             self.container.node,
             SocketType.PAPI,
             self.container.name,
             self.container.api_socket,
         )
-        topo_instance.add_new_socket(
+        Topology.add_new_socket(
             self.container.node,
             SocketType.STATS,
             self.container.name,
