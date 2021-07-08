50f5518b6f35977fd3da6364acc88f9e83bd3bdf
Vratko Polak <vrpolak@cisco.com>
2021-07-08 13:59:07 +0200

    Topology: Log any edit of "nodes" global state
    + Introduce ProtectedDict type.
     + Also ProtectedList class and protect_object function.
    + ${nodes} is protected, so most untracked edits will fail.
     + Moved few external writes into Topology for tracking.
     + Tracked edits are always logged (TRACE level for now).
     + Tracked edits require "reason" message, it is logged.
    + More specific data types are introduced.
     + SubNodeDict, NodeDict, NodesDict.
     - Not actually required by the current implementation.
     + Used in docstrings to clarify intent.
    + Do not read topology file on import.
     + Explicitly initialize from top-level suite setup.
    + Correctly behaving callers should not see any impact of this refactor.
     - Maybe scale test with many interfaces will run somewhat slower.
    - Some TODOs.
     + Some for later changes, e.g. JSON export.
     - Some should be addressed during review.
    
    Change-Id: Ice7fbc562a70c7d0a5a43e026e0a11d80d7808ef
    Signed-off-by: Vratko Polak <vrpolak@cisco.com>

--- resources/libraries/python/robot_interaction.py
+++ resources/libraries/python/robot_interaction.py
@@ -88,3 +88,23 @@ def get_variable(name, default=None):
         return _builtin().get_variable_value(name, default)
     except RobotNotRunningError:
         return default
+
+def set_global_variable(name, value):
+    """Store value into Robot variable of global scope.
+
+    It is recommended to include the "\\${" and "}" in the variable name.
+    Inside, you can use more curly bracket substitutions
+    supported by Robot, e.g. u"\\${${foo}}" gets you the value of the variable
+    whose name is stored in variable named "foo".
+    Beware of f-formatting, which also treats curly braces in a special way,
+    use double braces to escape, e.g. f"\\${{text{python_var}text}}".
+
+    TODO: Maybe silent noop instead of RobotNotRunningError?
+
+    :param name: Variable name, insensitive to case and spaces/underscores.
+    :param value: What value to set as a global variable.
+    :type name: str
+    :type value: object
+    :raise RobotNotRunningError: If Robot is not running.
+    """
+    _builtin().set_global_variable(name, value)
