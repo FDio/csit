b9648942c6e5ec46bc9def44d8d726996800009f
pmikus <pmikus@cisco.com>
2021-07-13 11:35:32 +0000

    Migrate report/trending/docs to bash framework
    Signed-off-by: pmikus <pmikus@cisco.com>
    Change-Id: Idf8462f15a2dd76b42dcaea9df6a0fc15a45503f

---
+++ resources/libraries/bash/function/docs.sh
@@ -0,0 +1,183 @@
+#!/usr/bin/env bash
+
+# Copyright (c) 2021 Cisco and/or its affiliates.
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at:
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+set -exuo pipefail
+
+
+function die_on_report_error () {
+
+    # Source this fragment if you want to abort on any failure.
+    #
+    # Variables read:
+    # - REPORT_EXIT_STATUS - Set by a report generation function.
+    # Functions called:
+    # - die - Print to stderr and exit.
+
+    set -exuo pipefail
+
+    if [[ "${REPORT_EXIT_STATUS}" != "0" ]]; then
+        die "Failed to generate report!" "${REPORT_EXIT_STATUS}"
+    fi
+}
+
+function generate_report () {
+
+    # Generate report content.
+    #
+    # Variable read:
+    # - ${TOOLS_DIR} - Path to existing resources subdirectory "tools".
+    # - ${GERRIT_BRANCH} - Path to existing resources subdirectory "tools".
+    # Variables set:
+    # - REPORT_EXIT_STATUS - Exit status of report generation.
+    # Functions called:
+    # - die - Print to stderr and exit.
+
+    set -exuo pipefail
+
+    pushd "${TOOLS_DIR}"/presentation || die "Pushd failed!"
+
+    # Set default values in config array.
+    typeset -A CFG
+    typeset -A DIR
+
+    DIR[WORKING]="_tmp"
+
+    # Create working directories.
+    mkdir "${DIR[WORKING]}" || die "Mkdir failed!"
+
+    export PYTHONPATH=`pwd`:`pwd`/../../../ || die "Export failed!"
+
+    all_options=("pal.py")
+    all_options+=("--specification" "specifications/report")
+    all_options+=("--release" "${GERRIT_BRANCH:-master}")
+    all_options+=("--week" "28")
+    all_options+=("--logging" "INFO")
+    all_options+=("--force")
+
+    set +e
+    python "${all_options[@]}"
+    REPORT_EXIT_STATUS="$?"
+    set -e
+
+}
+
+function generate_report_local () {
+
+    # Generate report from local content.
+    #
+    # Variable read:
+    # - ${TOOLS_DIR} - Path to existing resources subdirectory "tools".
+    # Variables set:
+    # - REPORT_EXIT_STATUS - Exit status of report generation.
+    # Functions called:
+    # - die - Print to stderr and exit.
+
+    set -exuo pipefail
+
+    pushd "${TOOLS_DIR}"/presentation || die "Pushd failed!"
+
+    filename="${CSIT_REPORT_FILENAME-}"
+    directoryname="${CSIT_REPORT_DIRECTORYNAME-}"
+    install_dependencies="${CSIT_REPORT_INSTALL_DEPENDENCIES:-1}"
+    install_latex="${CSIT_REPORT_INSTALL_LATEX:-0}"
+
+    # Set default values in config array.
+    typeset -A CFG
+    typeset -A DIR
+
+    DIR[WORKING]="_tmp"
+
+    # Install system dependencies.
+    if [[ ${install_dependencies} -eq 1 ]] ;
+    then
+        sudo apt -y update || die "APT update failed!"
+        sudo apt -y install libxml2 libxml2-dev libxslt-dev \
+            build-essential zlib1g-dev unzip || die "APT install failed!"
+    fi
+
+    if [[ ${install_latex} -eq 1 ]] ;
+    then
+        sudo apt -y update || die "APT update failed!"
+        sudo apt -y install xvfb texlive-latex-recommended \
+            texlive-fonts-recommended texlive-fonts-extra texlive-latex-extra \
+            latexmk wkhtmltopdf inkscape || die "APT install failed!"
+        target="/usr/share/texlive/texmf-dist/web2c/texmf.cnf"
+        sudo sed -i.bak 's/^\(main_memory\s=\s\).*/\110000000/' "${target}" || {
+            die "Patching latex failed!"
+        }
+    fi
+
+    # Create working directories.
+    mkdir "${DIR[WORKING]}" || die "Mkdir failed!"
+
+    export PYTHONPATH=`pwd`:`pwd`/../../../ || die "Export failed!"
+
+    all_options=("pal.py")
+    all_options+=("--specification" "specifications/report_local")
+    all_options+=("--release" "${RELEASE:-master}")
+    all_options+=("--week" "${WEEK:-1}")
+    all_options+=("--logging" "INFO")
+    all_options+=("--force")
+    if [[ ${filename} != "" ]]; then
+        all_options+=("--input-file" "${filename}")
+    fi
+    if [[ ${directoryname} != "" ]]; then
+        all_options+=("--input-directory" "${directoryname}")
+    fi
+
+    set +e
+    python "${all_options[@]}"
+    REPORT_EXIT_STATUS="$?"
+    set -e
+
+}
+
+function generate_trending () {
+
+    # Generate trending content.
+    #
+    # Variable read:
+    # - ${TOOLS_DIR} - Path to existing resources subdirectory "tools".
+    # - ${GERRIT_BRANCH} - Path to existing resources subdirectory "tools".
+    # Variables set:
+    # - TRENDING_EXIT_STATUS - Exit status of trending generation.
+    # Functions called:
+    # - die - Print to stderr and exit.
+
+    set -exuo pipefail
+
+    pushd "${TOOLS_DIR}"/presentation || die "Pushd failed!"
+
+    # Set default values in config array.
+    typeset -A DIR
+
+    DIR[WORKING]="_tmp"
+
+    # Create working directories.
+    mkdir "${DIR[WORKING]}" || die "Mkdir failed!"
+
+    export PYTHONPATH=`pwd`:`pwd`/../../../ || die "Export failed!"
+
+    all_options=("pal.py")
+    all_options+=("--specification" "specifications/trending")
+    all_options+=("--logging" "INFO")
+    all_options+=("--force")
+
+    set +e
+    python "${all_options[@]}"
+    TRENDING_EXIT_STATUS="$?"
+    set -e
+
+}
