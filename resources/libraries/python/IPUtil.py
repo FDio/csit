bdad06ba886fe6cdf33a2e5d78a81b62d57219a5
Vratko Polak <vrpolak@cisco.com>
2021-07-14 18:00:37 +0200

    [reopen] Scale tests: Remove VAT
    VAT function ip_route_add_del was removed in
    https://gerrit.fd.io/r/c/vpp/+/32878
    and we cannot rely on other VAT functions staying either.
    
    This makes some failing tests pass, but take long to finish.
    
    Change-Id: Idf41d44235d47b2afe9ce35d936062f78cf8db8f
    Signed-off-by: Vratko Polak <vrpolak@cisco.com>

--- resources/libraries/python/IPUtil.py
+++ resources/libraries/python/IPUtil.py
@@ -26,7 +26,6 @@ from resources.libraries.python.IPAddress import IPAddress
 from resources.libraries.python.PapiExecutor import PapiSocketExecutor
 from resources.libraries.python.ssh import exec_cmd_no_error, exec_cmd
 from resources.libraries.python.topology import Topology
-from resources.libraries.python.VatExecutor import VatTerminal
 from resources.libraries.python.Namespaces import Namespaces
 
 
@@ -708,28 +707,6 @@ class IPUtil:
         :type kwargs: dict
         """
         count = kwargs.get(u"count", 1)
-
-        if count > 100:
-            gateway = kwargs.get(u"gateway", '')
-            interface = kwargs.get(u"interface", '')
-            vrf = kwargs.get(u"vrf", None)
-            multipath = kwargs.get(u"multipath", False)
-
-            with VatTerminal(node, json_param=False) as vat:
-
-                vat.vat_terminal_exec_cmd_from_template(
-                    u"vpp_route_add.vat",
-                    network=network,
-                    prefix_length=prefix_len,
-                    via=f"via {gateway}" if gateway else u"",
-                    sw_if_index=f"sw_if_index "
-                    f"{InterfaceUtil.get_interface_index(node, interface)}"
-                    if interface else u"",
-                    vrf=f"vrf {vrf}" if vrf else u"",
-                    count=f"count {count}" if count else u"",
-                    multipath=u"multipath" if multipath else u""
-                )
-            return
 
         net_addr = ip_address(network)
         cmd = u"ip_route_add_del"
