83a8bc7ef05e241f0f5ffeeba385ddd6076ced1a
pmikus <pmikus@cisco.com>
2021-07-21 12:01:34 +0000

    Core: T-rex 2.90
    Signed-off-by: pmikus <pmikus@cisco.com>
    Change-Id: Id4d84aa7268080843b099fd7ab9851234612968b

--- GPL/traffic_profiles/trex/profile_trex_astf_base_class.py
+++ GPL/traffic_profiles/trex/profile_trex_astf_base_class.py
@@ -133,7 +133,7 @@ class TrafficProfileBaseClass:
         """
         self.framesize = kwargs[u"framesize"]
         self._pcap_dir = kwargs.get(
-            u"pcap_dir", u"/opt/trex-core-2.88/scripts/avl"
+            u"pcap_dir", u"/opt/trex-core-2.90/scripts/avl"
         )
 
         return self.create_profile()
