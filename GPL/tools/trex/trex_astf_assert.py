7b770ca29a82fe608dfcea3af58cb214eb8c9db2
pmikus <pmikus@cisco.com>
2021-07-21 12:01:34 +0000

    Core: T-rex 2.90
    Signed-off-by: pmikus <pmikus@cisco.com>
    Change-Id: Id4d84aa7268080843b099fd7ab9851234612968b

--- GPL/tools/trex/trex_astf_assert.py
+++ GPL/tools/trex/trex_astf_assert.py
@@ -38,7 +38,7 @@ Functionality:
 import sys
 
 sys.path.insert(
-    0, u"/opt/trex-core-2.88/scripts/automation/trex_control_plane/interactive/"
+    0, u"/opt/trex-core-2.90/scripts/automation/trex_control_plane/interactive/"
 )
 from trex.astf.api import ASTFClient, TRexError
 
