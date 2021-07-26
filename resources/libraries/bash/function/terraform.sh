480e37b7c87175832327347ea5c75352d1bbe7ea
pmikus <pmikus@cisco.com>
2021-07-26 12:54:58 +0000

    Infra: Terraform add ansible plugin init
    + Manual override
    
    Signed-off-by: pmikus <pmikus@cisco.com>
    Change-Id: If296d28dc3eaa023c935a0887a14ac10e83076ba
    (cherry picked from commit b27f2c7cbfe08712a00959c8cc6a2f618a25b3e7)

---
+++ resources/libraries/bash/function/terraform.sh
@@ -0,0 +1,116 @@
+<<<<<<< HEAD   (779d17 Trending: Save latency data in csv files)
+=======
+#!/usr/bin/env bash
+
+# Copyright (c) 2021 Cisco and/or its affiliates.
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at:
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+set -exuo pipefail
+
+
+function terraform_apply () {
+
+    # Run terraform apply command to prepare topology.
+    #
+    # Variable read:
+    # - ${CSIT_DIR} - CSIT main directory, where terraform modules are located.
+    # - ${NODENESS} - Node multiplicity of desired testbed.
+    # - ${FLAVOR} - Node flavor string, see common.sh
+
+    set -exuo pipefail
+
+    if ! installed terraform; then
+        die "Please install terraform!"
+    fi
+
+    pushd "${CSIT_DIR}"/fdio.infra.terraform || die "Pushd failed!"
+    pushd "${NODENESS}_${FLAVOR}_c5n" || die "Pushd failed!"
+    export TF_LOG=INFO
+    terraform apply -auto-approve  || die "Failed to run terraform apply!"
+    popd || die "Popd failed!"
+    popd || die "Popd failed!"
+}
+
+function terraform_destroy () {
+
+    # Run terraform destroy command to prepare module.
+    #
+    # Variable read:
+    # - ${CSIT_DIR} - CSIT main directory, where terraform modules are located.
+    # - ${NODENESS} - Node multiplicity of desired testbed.
+    # - ${FLAVOR} - Node flavor string, see common.sh
+
+    set -exuo pipefail
+
+    if ! installed terraform; then
+        die "Please install terraform!"
+    fi
+
+    pushd "${CSIT_DIR}"/fdio.infra.terraform || die "Pushd failed!"
+    pushd "${NODENESS}_${FLAVOR}_c5n" || die "Pushd failed!"
+    export TF_LOG=INFO
+    terraform destroy -auto-approve || die "Failed to run terraform destroy!"
+    popd || die "Popd failed!"
+    popd || die "Popd failed!"
+}
+
+
+function terraform_init () {
+
+    # Run terraform init command to prepare module.
+    #
+    # Variable read:
+    # - ${CSIT_DIR} - CSIT main directory, where terraform modules are located.
+    # - ${NODENESS} - Node multiplicity of desired testbed.
+    # - ${FLAVOR} - Node flavor string, see common.sh
+
+    set -exuo pipefail
+
+    if ! installed terraform; then
+        die "Please install terraform!"
+    fi
+
+    pushd "${CSIT_DIR}"/fdio.infra.terraform || die "Pushd failed!"
+    pushd "${NODENESS}_${FLAVOR}_c5n" || die "Pushd failed!"
+    export TF_LOG=INFO
+    terraform init || die "Failed to run terraform init!"
+
+    plugin_url="https://github.com/radekg/terraform-provisioner-ansible/"
+    plugin_url+="releases/download/v2.5.0/"
+    plugin_url+="terraform-provisioner-ansible-linux-amd64_v2.5.0"
+    plugin_dir="${HOME}/.terraform.d/plugins/"
+    plugin_path+="${plugin_dir}terraform-provisioner-ansible-linux-amd64_v2.5.0"
+
+    mkdir -p "${plugin_dir}" || die "Failed to create dir!"
+    wget -O "${plugin_path}" "${plugin_url}" || die "Failed to download plugin!"
+
+    popd || die "Popd failed!"
+    popd || die "Popd failed!"
+}
+
+
+function installed () {
+
+    # Check if the given utility is installed. Fail if not installed.
+    #
+    # Arguments:
+    # - ${1} - Utility to check.
+    # Returns (implicitly):
+    # - 0 - If command is installed.
+    # - 1 - If command is not installed.
+
+    set -exuo pipefail
+
+    command -v "${1}"
+}
+>>>>>>> CHANGE (b27f2c Infra: Terraform add ansible plugin init)
