f337a7b3f04e910017181cfa6826fd607db83b94
Tibor Frank <tifrank@cisco.com>
2021-08-13 08:17:45 +0200

    Report: configure rls2106.33
    Change-Id: Ic7d1a776817dda52007bcebd663388cc4bd1fa66
    Signed-off-by: Tibor Frank <tifrank@cisco.com>

--- resources/tools/presentation/sphinx_conf/report/conf.py
+++ resources/tools/presentation/sphinx_conf/report/conf.py
@@ -46,7 +46,7 @@ source_suffix = [u'.rst', u'.md']
 master_doc = u'index'
 
 # General information about the project.
-report_week = u'32'
+report_week = u'33'
 project = u'FD.io CSIT-2106.{week}'.format(week=report_week)
 copyright = u'2021, FD.io'
 author = u'FD.io CSIT'
