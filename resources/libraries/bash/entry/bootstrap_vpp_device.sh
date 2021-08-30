f8bde99511b7211b79ddaeef9fa0b3f0fd08e560
Vratko Polak <vrpolak@cisco.com>
2021-08-30 12:11:20 +0200

    Unify device bootstraps: part 2
    Now there is just one bootstrap script for both
    csit-vpp and vpp-csit device jobs.
    
    The entry script has two blocks for different ways
    the jobs arrive at VPP build to test,
    but the rest is common sequence of functions.
    This is to ensure CSIT changes that break vpp-csit job
    will also break csit-vpp (so -1 and no CSIT merge),
    unless the edit is obviously in VPP build block.
    
    Change-Id: I4b62df014ce5cff2cdb8ec551e585f8974b00823
    Signed-off-by: Vratko Polak <vrpolak@cisco.com>

--- resources/libraries/bash/entry/bootstrap_vpp_device.sh
+++ resources/libraries/bash/entry/bootstrap_vpp_device.sh
@@ -15,13 +15,22 @@
 
 set -exuo pipefail
 
-# Assumptions:
-# + There is a directory holding CSIT code to use (this script is there).
+# Run selection of "device" tests on a VPP build, fail if a test fails.
+#
+# This is a common bootstrap for csit-vpp and vpp-csit jobs,
+# the difference is whether VPP build is downloaded or built from source.
+# The two jobs have different directory structure in workspace,
+# but the functions hide this fact, so most of the logic is identical.
+# That is important, otherwise a CSIT change verified on csit-vpp
+# could break vpp-csit, which votes on VPP changes.
+#
+# This entry script does not change CSIT branch,
+# use "with_oper_for_vpp.sh" wrapper for that.
+#
+# Common assumptions:
 # + At least one of the following is true:
 # ++ JOB_NAME environment variable is set,
-# ++ or this entry script has access to arguments.
-# Consequences (and specific assumptions) are multiple,
-# examine tree of functions for current description.
+# ++ or this entry script has access to arguments, first one is job name.
 
 # "set -eu" handles failures from the following two lines.
 BASH_ENTRY_DIR="$(dirname $(readlink -e "${BASH_SOURCE[0]}"))"
@@ -30,14 +39,40 @@ source "${BASH_FUNCTION_DIR}/common.sh" || {
     echo "Source failed." >&2
     exit 1
 }
-source "${BASH_FUNCTION_DIR}/gather.sh" || die "Source failed."
 common_dirs || die
-check_prerequisites || die
 get_test_code "${1-}" || die
+if [[ "${TEST_CODE}" == "csit-vpp"*"device"* ]]; then
+    # Download the VPP build to test.
+    # Assumptions:
+    # + There is a directory holding CSIT code to use (this script is there).
+    # Consequences (and specific assumptions) are multiple,
+    # examine tree of functions for current description.
+    source "${BASH_FUNCTION_DIR}/gather.sh" || die "Source failed."
+    select_arch_os || die
+    gather_build || die
+elif [[ "${TEST_CODE}" == "vpp-csit"*"device"* ]]; then
+    # Compile the VPP build to test.
+    # Assumptions:
+    # + There is a directory holding VPP repo with patch under test checked out.
+    # + It contains csit subdir with CSIT code to use (this script is there).
+    # + Everything needed to build VPP is already installed locally.
+    # Consequences:
+    # + The following directories (relative to VPP repo) are (re)created:
+    # + csit_current, build_current, archives, csit/archives, csit_download_dir.
+    source "${BASH_FUNCTION_DIR}/per_patch.sh" || die "Source failed."
+    set_perpatch_vpp_dir || die
+    initialize_csit_dirs || die
+    build_vpp_ubuntu_amd64 "CURRENT" || die
+    set_aside_commit_build_artifacts || die
+    select_build "build_current" || die
+    set_perpatch_dut || die
+else
+    die "Devicetest bootstrap supports only csit-vpp and vpp-csit device jobs."
+fi
+check_download_dir || die
+check_prerequisites || die
 get_test_tag_string || die
 select_arch_os || die
-gather_build || die
-check_download_dir || die
 activate_virtualenv || die
 generate_tests || die
 archive_tests || die
