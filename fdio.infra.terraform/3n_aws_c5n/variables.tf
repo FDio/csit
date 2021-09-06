801998ce03823fe778133e6b3b9ea07dec803818
pmikus <pmikus@cisco.com>
2021-09-06 12:19:11 +0000

    Terraform: Cleanup
    Signed-off-by: pmikus <pmikus@cisco.com>
    Change-Id: Ifec4087b4522754cb05f5b0408c02dad4e9ada67

--- fdio.infra.terraform/3n_aws_c5n/variables.tf
+++ fdio.infra.terraform/3n_aws_c5n/variables.tf
@@ -1,37 +1,37 @@
 variable "region" {
-  description = "AWS Region"
-  type = string
-  default = "eu-central-1"
+  description     = "AWS Region"
+  type            = string
+  default         = "eu-central-1"
 }
 
 variable "avail_zone" {
-  description = "AWS availability zone"
-  type = string
-  default = "eu-central-1a"
+  description     = "AWS availability zone"
+  type            = string
+  default         = "eu-central-1a"
 }
 
 variable "ami_image" {
   # eu-central-1/focal-20.04-amd64-hvm-ssd-20210119.1
   # kernel 5.4.0-1035-aws (~5.4.0-65)
-  description = "AWS AMI image ID"
-  type = string
-  default = "ami-0a875db8a031a9efb"
+  description     = "AWS AMI image ID"
+  type            = string
+  default         = "ami-0a875db8a031a9efb"
 }
 
 variable "instance_initiated_shutdown_behavior" {
-  description = "Shutdown behavior for the instance"
-  type        = string
-  default     = "terminate"
+  description     = "Shutdown behavior for the instance"
+  type            = string
+  default         = "terminate"
 }
 
 variable "instance_type" {
-  description = "AWS instance type"
-  type = string
-  default = "c5n.9xlarge"
+  description     = "AWS instance type"
+  type            = string
+  default         = "c5n.9xlarge"
 }
 
 variable "testbed_name" {
-  description = "Testbed name"
-  type = string
-  default = "testbed1"
+  description     = "Testbed name"
+  type            = string
+  default         = "testbed1"
 }
