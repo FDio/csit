74c38531f9fd1573e94da54423cdb3e3edf1aa4f
Vratko Polak <vrpolak@cisco.com>
2021-09-10 14:57:53 +0200

    UTI: Export debug messages from MLRsearch
    Change-Id: I89a72b0c1ea9117ee26b7963dbc7ef5ce5b10ab7
    Signed-off-by: Vratko Polak <vrpolak@cisco.com>

--- resources/libraries/python/model/ExportLog.py
+++ resources/libraries/python/model/ExportLog.py
@@ -305,3 +305,32 @@ def export_ssh_timeout(
         ),
     )
     debug_data[u"log"].append(papi_record)
+
+
+def export_mlrsearch_debug(message, timestamp=None):
+    """Add a log item with debug messages from MLRsearch.
+
+    Only for debug log.
+
+    Message is put as message, data is an empty string.
+
+    Timestamp marks time when MLRsearch thinks the message applies.
+    Current time is used if timestamp is missing.
+    Log level is always DEBUG.
+
+    :param message: Text to log.
+    :param timestamp: Local UTC time just before sending.
+    :type message: str
+    :type timestamp: Optional[str]
+    """
+    debug_data, _ = get_export_data()
+    mlrsearch_record = dict(
+        source_type=u"search_algorithm",
+        source_id=u"mlrsearch",
+        msg_type=u"mlrsearch_debug",
+        log_level=u"DEBUG",
+        timestamp=timestamp_or_now(timestamp),
+        msg=str(message),
+        data=u"",
+    )
+    debug_data[u"log"].append(mlrsearch_record)
