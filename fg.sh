09412d2de970930db8ae9bc4f7057090e0750384
Viliam Luc <vluc@cisco.com>
2021-09-17 08:32:25 +0200

    CRC: trailing comments updated
    Change-Id: I54fd3116bd2191ab6870e1bba6b3b24f8117b899
    Signed-off-by: Viliam Luc <vluc@cisco.com>

---
+++ fg.sh
@@ -0,0 +1,7 @@
+if [[ "$#" != "1" ]]; then
+    echo "Use precisely one argument!"
+fi
+#arg="$(cat "$1" | sed -iE 's#[ _]#\[ _\]?#g')"
+arg="${1//[ _]/\[ _\]\\?}"
+echo grep -rin --color "${arg}"
+grep -rin --color "${arg}"
