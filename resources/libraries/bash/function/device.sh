55629cb3e0406fd7b054ab620202167b2d954140
pmikus <pmikus@cisco.com>
2021-09-22 12:58:51 +0000

    fix Device: Cleanup previous interfaces
    Signed-off-by: pmikus <pmikus@cisco.com>
    Change-Id: I62deca750d4494427d2ea1916e0678c06dad7533

--- resources/libraries/bash/function/device.sh
+++ resources/libraries/bash/function/device.sh
@@ -269,8 +269,8 @@ function get_available_interfaces () {
             # Add Intel Corporation E810 Virtual Function to the
             # whitelist.
             pci_id="0x154c\|0x1889"
-            tg_netdev=(ens1 enp94 enp134)
-            dut1_netdev=(ens5 enp175 enp175)
+            tg_netdev=(ens1 enp134)
+            dut1_netdev=(ens5 enp175)
             ports_per_nic=2
             ;;
        "1n_tx2")
