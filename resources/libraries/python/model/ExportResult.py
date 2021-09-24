304652951cf6fe5b5988d19ddfb053d21ae22223
Vratko Polak <vrpolak@cisco.com>
2021-09-24 14:13:46 +0200

    UTI: Export results
    - Some of fields are missing.
    - Only some results are exported.
     + MRR, NDRPDR and SOAK.
     - Other test types are missing.
    - Suite names with dot in them do not work correctly.
     + Will be addressed in subsequent change.
    + Also export for suite setups and teardowns.
     + It is needed mainly for the "log" list.
    + Convert custom classes to JSON-serializable equivalents.
    + Sort dict keys before converting to JSON.
    + Add utility function for overridable timestamps.
    + Include documentation tree of what is in the implemented output.
    
    Change-Id: I175e72fd08745ea4da87855058dadd30288cc8c1
    Signed-off-by: Vratko Polak <vrpolak@cisco.com>

---
+++ resources/libraries/python/model/ExportResult.py
@@ -0,0 +1,192 @@
+# Copyright (c) 2021 Cisco and/or its affiliates.
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at:
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Module with keywords that publish parts of result structure."""
+
+from resources.libraries.python.jumpavg.AvgStdevStats import AvgStdevStats
+from resources.libraries.python.model.util import descend, get_export_data
+
+
+def export_vpp_version(version):
+    """Export the argument as sut_version.
+
+    Sets suit_type to "VPP".
+
+    :param version: VPP version as returned by PAPI.
+    :type version: str
+    """
+    version = str(version)
+    debug_data, info_data = get_export_data()
+    debug_data[u"sut_type"] = u"VPP"
+    info_data[u"sut_type"] = u"VPP"
+    debug_data[u"sut_version"] = version
+    info_data[u"sut_version"] = version
+
+
+def append_mrr_value(mrr_value):
+    """Store mrr value to proper place so it is dumped into json.
+
+    The inner node "value" may or may not exist before calling this.
+    Test type is not overwritten here, but when setting unit.
+
+    :param mrr_value: Forwarding rate from MRR trial, unit specified elsewhere.
+    :type mrr_value: float
+    """
+    debug_data, info_data = get_export_data()
+    debug_results_node = debug_data[u"results"]
+    samples = descend(debug_results_node, u"samples", list())
+    samples.append(float(mrr_value))
+    info_results_node = info_data[u"results"]
+    samples = descend(info_results_node, u"samples", list())
+    samples.append(float(mrr_value))
+    # Stats are derived quantity, so for info only.
+    # TODO: Implement incremental udates.
+    # That means storing stats somewhere json does not export.
+    stats = AvgStdevStats.for_runs(samples)
+    info_results_node[u"avg"] = stats.avg
+    info_results_node[u"stdev"] = stats.stdev
+
+
+def export_mrr_unit(unit):
+    """Store MRR unit so it is dumped into json.
+
+    If a previous value exists, it is overwritten silently.
+    test type is set (overwritten) to MRR (info only).
+
+    :param unit: Unit of MRR forwarding rate, either cps or pps.
+    :type unit: str
+    """
+    unit = str(unit)
+    debug_data, info_data = get_export_data()
+    debug_data[u"results"][u"unit"] = unit
+    info_data[u"results"][u"unit"] = unit
+    info_data[u"test_type"] = u"mrr"
+
+
+def export_search_bound(text, value, bandwidth=None):
+    """Store bound value and unit.
+
+    This function works for both NDRPDR and SOAK, decided by text.
+    If bandwidth is not given, unit is assumed to be cps, otherwise pps.
+
+    If a node does not exist, it is created.
+    If a previous value exists, it is overwritten silently.
+    Test type is set (overwritten) to ndrpdr (or soak), info only.
+
+    Text is used to determine whether it is ndr or pdr, upper or lower bound,
+    as the Robot caller has the information only there.
+
+    :param text: Info from Robot caller to determime bound type.
+    :param value: The bound value in packets (or connections) per second.
+    :param bandwidth: The same value recomputed into L1 gigabits per second.
+        None means value is cps.
+    :type text: str
+    :type value: float
+    :type bandwidth: Optional[float]
+    """
+    value = float(value)
+    text = str(text).lower()
+    test_type = u"soak" if u"plrsearch" in text else u"ndrpdr"
+    upper_or_lower = u"upper" if u"upper" in text else u"lower"
+    ndr_or_pdr = u"ndr" if u"ndr" in text else u"pdr"
+    unit = u"cps" if bandwidth is None else u"pps"
+
+    debug_data, info_data = get_export_data()
+    info_data[u"test_type"] = test_type
+    debug_results_node = debug_data[u"results"]
+    info_results_node = info_data[u"results"]
+    debug_type_node = descend(debug_results_node, test_type)
+    info_type_node = descend(info_results_node, test_type)
+    debug_type_node[u"unit"] = unit
+    info_type_node[u"unit"] = unit
+    if test_type == u"soak":
+        debug_type_node[upper_or_lower] = value
+        info_type_node[upper_or_lower] = value
+        # TODO: Support Gbps values for SOAK?
+        return
+    debug_ndrpdr_node = descend(debug_type_node, ndr_or_pdr)
+    info_ndrpdr_node = descend(info_type_node, ndr_or_pdr)
+    debug_ndrpdr_node[upper_or_lower] = value
+    info_ndrpdr_node[upper_or_lower] = value
+    if bandwidth is None:
+        return
+    bandwidth = float(bandwidth)
+    debug_ndrpdr_node[upper_or_lower + u"_gbps"] = bandwidth
+    info_ndrpdr_node[upper_or_lower + u"_gbps"] = bandwidth
+
+
+def export_ndrpdr_latency(text, latency):
+    """Store NDRPDR hdrh latency data.
+
+    If "latency" node does not exist, it is created.
+    If a previous value exists, it is overwritten silently.
+
+    Text is used to determine what percentage of PDR is the load,
+    as the Robot caller has the information only there.
+
+    Reverse data may be missing, we assume the test was unidirectional.
+
+    :param text: Info from Robot caller to determime load.
+    :param latency: Output from TRex utility, min/avg/max/hdrh.
+    :type text: str
+    :type latency: 1-tuple or 2-tuple of str
+    """
+    # TODO: R0914: Too many local variables (20/15) (too-many-locals)
+    # Probably by moving the l_* blocks to a single function.
+    debug_data, info_data = get_export_data()
+    debug_results_node = debug_data[u"results"]
+    info_results_node = info_data[u"results"]
+    debug_ndrpdr_node = descend(debug_results_node, u"ndrpdr")
+    info_ndrpdr_node = descend(info_results_node, u"ndrpdr")
+    percent = 0
+    if u"90" in text:
+        percent = 90
+    elif u"50" in text:
+        percent = 50
+    elif u"10" in text:
+        percent = 10
+    node_name = f"pdr_{percent}"
+    l_min, l_avg, l_max, l_hdrh = latency[0].split(u"/", 3)
+    l_min, l_avg, l_max = float(l_min), float(l_avg), float(l_max)
+    debug_forward_node = descend(debug_ndrpdr_node, u"latency_forward")
+    debug_percent_node = descend(debug_forward_node, node_name)
+    debug_percent_node[u"min"] = l_min
+    debug_percent_node[u"avg"] = l_avg
+    debug_percent_node[u"max"] = l_max
+    debug_percent_node[u"hdrd"] = l_hdrh
+    if l_min > -1:
+        info_forward_node = descend(info_ndrpdr_node, u"latency_forward")
+        info_percent_node = descend(info_forward_node, node_name)
+        info_percent_node[u"min"] = l_min
+        info_percent_node[u"avg"] = l_avg
+        info_percent_node[u"max"] = l_max
+        info_percent_node[u"hdrd"] = l_hdrh
+    # Else TRex does not support latency measurement for this traffic profile.
+    if len(latency) < 2:
+        return
+    debug_reverse_node = descend(debug_ndrpdr_node, u"latency_reverse")
+    debug_percent_node = descend(debug_reverse_node, node_name)
+    l_min, l_avg, l_max, l_hdrh = latency[1].split(u"/", 3)
+    l_min, l_avg, l_max = float(l_min), float(l_avg), float(l_max)
+    debug_percent_node[u"min"] = l_min
+    debug_percent_node[u"avg"] = l_avg
+    debug_percent_node[u"max"] = l_max
+    debug_percent_node[u"hdrd"] = l_hdrh
+    if l_min > -1:
+        info_reverse_node = descend(info_ndrpdr_node, u"latency_reverse")
+        info_percent_node = descend(info_reverse_node, node_name)
+        info_percent_node[u"min"] = l_min
+        info_percent_node[u"avg"] = l_avg
+        info_percent_node[u"max"] = l_max
+        info_percent_node[u"hdrd"] = l_hdrh
+    # Else TRex does not support latency measurement for this traffic profile.
---
+++ resources/libraries/python/model/export_json.py
@@ -0,0 +1,330 @@
+# Copyright (c) 2021 Cisco and/or its affiliates.
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at:
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Module tracking json in-memory data and saving it to files.
+
+The current implementation tracks data for two files.
+Debug file contains all log items but no derived quantities,
+info file contains only important log items but also derived quantities.
+The overlap between two files is big.
+
+Each test case, suite setup (hierarchical) and teardown has its own file pair.
+"""
+
+from collections.abc import Mapping, Iterable
+from copy import deepcopy
+from enum import IntFlag
+import json
+import os
+
+from robot.libraries.BuiltIn import BuiltIn
+
+from resources.libraries.python.Constants import Constants
+from resources.libraries.python.time_measurement import (
+    timestamp_or_now, posix_from_iso
+)
+
+
+def _pre_serialize_recursive(data):
+    """Recursively sort and convert to a more serializable form.
+
+    VPP PAPI code can give data with its own MACAddres type,
+    or various other enum and flag types.
+    The default json.JSONEncoder method raises TypeError on that.
+    First point of this function is to apply str() or repr()
+    to leaf values that need it.
+
+    Also, PAPI responses are namedtuples, which confuses
+    the json.JSONEncoder method (so it does not recurse).
+    Dictization (see PapiExecutor) helps somewhat, but it turns namedtuple
+    into a UserDict, which also confuses json.JSONEncoder.
+    Therefore, we recursively convert any Mapping into an ordinary dict.
+
+    We also convert iterables to list,
+    and prevent numbers from getting converted to strings.
+
+    As we are doing such low level operations,
+    we also convert mapping keys to strings
+    and sort the mapping items by keys alphabetically,
+    except "data" field moved to the end.
+
+    :param data: Object to make serializable, dictized when applicable.
+    :type data: object
+    :returns: Serializable equivalent of the argument.
+    :rtype: object
+    :raises ValueError: If the argument does not support string conversion.
+    """
+    # Recursion ends at scalar values, first handle irregular ones.
+    if isinstance(data, IntFlag):
+        return repr(data)
+    if isinstance(data, bytes):
+        return data.hex()
+    # The regular ones are good to go.
+    if isinstance(data, (str, int, float, bool)):
+        return data
+    # Recurse over, convert and sort mappings.
+    if isinstance(data, Mapping):
+        # Convert and sort alphabetically.
+        ret = {
+            str(key): _pre_serialize_recursive(data[key])
+            for key in sorted(data)
+        }
+        # If exists, move "data" field to the end.
+        data_value = ret.pop(u"data", None)
+        if data_value is not None:
+            ret[u"data"] = data_value
+        return ret
+    # Recurse over and convert iterables.
+    if isinstance(data, Iterable):
+        return [_pre_serialize_recursive(item) for item in data]
+    # Unknown structure, attempt str().
+    return str(data)
+
+
+def _pre_serialize_root(data):
+    """Recursively convert to a more serializable form, tweak order.
+
+    See _pre_serialize_recursive for most of changes this does.
+
+    The logic here (outside the recursive function) only affects
+    field ordering in the root mapping,
+    to make it more human friendly.
+    We are moving "version" to the top,
+    and various long fields (such as "log") to the bottom.
+
+    Some edits are done in-place, do not trust the argument value after calling.
+
+    :param data: Root data to make serializable, dictized when applicable.
+    :type data: dict
+    :returns: Order-tweaked version of the argument.
+    :rtype: dict
+    :raises KeyError: If the data does not contain required fields.
+    :raises TypeError: If the argument is not a dict.
+    :raises ValueError: If the argument does not support string conversion.
+    """
+    if not isinstance(data, dict):
+        raise RuntimeError(f"Root data object needs to be a dict: {data!r}")
+    log = data.pop(u"log")
+    new_data = {u"version": data.pop(u"version")}
+    new_data.update(_pre_serialize_recursive(data))
+    new_data[u"log"] = _pre_serialize_recursive(log)
+    return new_data
+
+
+class export_json():
+    """Class handling the json data setting and export."""
+
+    ROBOT_LIBRARY_SCOPE = u"GLOBAL"
+
+    def __init__(self):
+        """Declare required fields, cache output dir."""
+        self.output_dir = BuiltIn().get_variable_value(u"\\${OUTPUT_DIR}", ".")
+        self.info_file_path = None
+        self.debug_file_path = None
+        self.info_data = None
+        self.debug_data = None
+
+    def export_pending_data(self):
+        """Write the accumulated data to disk.
+
+        Create missing directories.
+        Reset both file path and data to avoid writing multiple times.
+
+        Functions which set file path are calling this.
+        Call explicitly at the end of the global suite teardown,
+        even after finalizing export forthat teardown.
+
+        If no file path is set, return silently,
+        as that is the expected behavior when starting global suite setup.
+        """
+        if not Constants.EXPORT_JSON or not self.debug_file_path:
+            return
+        os.makedirs(os.path.dirname(self.debug_file_path), exist_ok=True)
+        debug_data = _pre_serialize_root(self.debug_data)
+        with open(self.debug_file_path, u"w") as file_out:
+            json.dump(debug_data, file_out, indent=1)
+        self.debug_data = None
+        self.debug_file_path = None
+        info_data = _pre_serialize_root(self.info_data)
+        with open(self.info_file_path, u"w") as file_out:
+            json.dump(info_data, file_out, indent=1)
+        self.info_data = None
+        self.info_file_path = None
+
+    def start_suite_setup_export(self):
+        """Set new file path, initialize data for the suite setup.
+
+        Write data from previous test/suite.
+
+        This has to be called explicitly at start of suite setup,
+        otherwise Robot likes to postpone initialization
+        until first call by a data-adding keyword.
+
+        File path is set based on suite.
+        """
+        self.export_pending_data()
+        start_time = timestamp_or_now()
+        suite_name = BuiltIn().get_variable_value(u"\\${SUITE_NAME}")
+        suite_name = suite_name.replace(u" ", u"_")
+        suite_path_part = os.path.join(*suite_name.split(u"."))
+        self.debug_file_path = os.path.join(
+            self.output_dir, suite_path_part, u"setup.debug.json"
+        )
+        self.info_file_path = os.path.join(
+            self.output_dir, suite_path_part, u"setup.info.json"
+        )
+        data = dict()
+        data[u"version"] = Constants.MODEL_VERSION
+        # TODO: Add example to model document.
+        data[u"suite_name"] = BuiltIn().get_variable_value(u"\\${SUITE_NAME}")
+        data[u"suite_documentation"] = BuiltIn().get_variable_value(
+            u"\\${SUITE_DOCUMENTATION}"
+        )
+        data[u"start_time"] = start_time
+        # "end_time" and "duration" is added on flush.
+        data[u"log"] = list()
+        self.debug_data = data
+        self.info_data = deepcopy(data)
+
+    def start_test_export(self):
+        """Set new file path, initialize data to minimal tree for the test case.
+
+        Write data from previous test/suite.
+
+        This has to be called explicitly at the start of test setup,
+        otherwise Robot likes to postpone initialization
+        until first call by a data-adding keyword.
+
+        File path is set based on suite and test.
+        """
+        self.export_pending_data()
+        start_time = timestamp_or_now()
+        suite_name = BuiltIn().get_variable_value(u"\\${SUITE_NAME}")
+        suite_name = suite_name.replace(u" ", u"_")
+        suite_path_part = os.path.join(*suite_name.split(u"."))
+        test_name = BuiltIn().get_variable_value(u"\\${TEST_NAME}")
+        self.debug_file_path = os.path.join(
+            self.output_dir, suite_path_part, test_name + u".debug.json"
+        )
+        self.info_file_path = os.path.join(
+            self.output_dir, suite_path_part, test_name + u".info.json"
+        )
+        data = dict()
+        data[u"version"] = Constants.MODEL_VERSION
+        # TODO: "ci".
+        # TODO: "job".
+        # TODO: "build_number".
+        # TODO: "testbed".
+        # "sut_version" is set by a separate keyword.
+        data[u"test_name"] = test_name
+        # "test_type" is set by keyword setting results.
+        test_doc = BuiltIn().get_variable_value(u"\\${TEST_DOCUMENTATION}", u"")
+        data[u"test_documentation"] = test_doc
+        # "tags" is detected and added on flush.
+        data[u"start_time"] = start_time
+        # "end_time" and "duration" is added on flush.
+        # "status" is added on flush.
+        # "message" is added on flush.
+        data[u"results"] = dict()
+        # TODO: "resource"
+        # TODO: "network"
+        data[u"log"] = list()
+        self.debug_data = data
+        self.info_data = deepcopy(data)
+        # Convenience for PAL: Lowercase ID including suite.
+        self.info_data[u"test_id"] = u".".join(
+            [suite_name, test_name]
+        ).lower()
+
+    def start_suite_teardown_export(self):
+        """Set new file path, initialize data for the suite teardown.
+
+        Write data from previous test/suite.
+
+        This has to be called explicitly at start of suite teardown,
+        otherwise Robot likes to postpone initialization
+        until first call by a data-adding keyword.
+
+        File path is set based on suite.
+        """
+        self.export_pending_data()
+        start_time = timestamp_or_now()
+        suite_name = BuiltIn().get_variable_value(u"\\${SUITE_NAME}")
+        suite_name = suite_name.replace(u" ", u"_")
+        suite_path_part = os.path.join(*suite_name.split(u"."))
+        self.debug_file_path = os.path.join(
+            self.output_dir, suite_path_part, u"teardown.debug.json"
+        )
+        self.info_file_path = os.path.join(
+            self.output_dir, suite_path_part, u"teardown.info.json"
+        )
+        data = dict()
+        data[u"version"] = Constants.MODEL_VERSION
+        data[u"start_time"] = start_time
+        # "end_time" and "duration" is added on flush.
+        data[u"log"] = list()
+        self.debug_data = data
+        self.info_data = deepcopy(data)
+
+    def finalize_suite_setup_export(self):
+        """Add the missing fields to data. Do not write yet.
+
+        Should be run at the end of suite setup.
+        The write is done at next start (or at the end of global teardown).
+        """
+        end_time = timestamp_or_now()
+        self.debug_data[u"end_time"] = end_time
+        self.info_data[u"end_time"] = end_time
+        start_time_float = posix_from_iso(self.info_data[u"start_time"])
+        duration = posix_from_iso(end_time) - start_time_float
+        self.info_data[u"duration"] = duration
+
+    def finalize_test_export(self):
+        """Add the missing fields to data. Do not write yet.
+
+        Should be at the end of test teardown, as the implementation
+        reads various Robot variables, some of them only available at teardown.
+
+        The write is done at next start (or at the end of global teardown).
+        """
+        end_time = timestamp_or_now()
+        test_tags = BuiltIn().get_variable_value(u"\\${TEST_TAGS}")
+        self.debug_data[u"tags"] = [str(tag) for tag in test_tags]
+        self.debug_data[u"end_time"] = end_time
+        self.debug_data[u"status"] = BuiltIn().get_variable_value(
+            u"\\${TEST_STATUS}"
+        )
+        self.debug_data[u"message"] = BuiltIn().get_variable_value(
+            u"\\${TEST_MESSAGE}"
+        )
+        self.info_data[u"tags"] = self.debug_data[u"tags"]
+        self.info_data[u"end_time"] = self.debug_data[u"end_time"]
+        self.info_data[u"status"] = self.debug_data[u"status"]
+        self.info_data[u"message"] = self.debug_data[u"message"]
+        start_time_float = posix_from_iso(self.info_data[u"start_time"])
+        duration = posix_from_iso(end_time) - start_time_float
+        self.info_data[u"duration"] = duration
+
+    def finalize_suite_teardown_export(self):
+        """Add the missing fields to data. Do not write yet.
+
+        Should be run at the end of suite teardown
+        (but before the explicit write in the global suite teardown).
+        The write is done at next start (or explicitly for global teardown).
+        """
+        end_time = timestamp_or_now()
+        self.debug_data[u"end_time"] = end_time
+        self.info_data[u"end_time"] = end_time
+        start_time_float = posix_from_iso(self.info_data[u"start_time"])
+        duration = posix_from_iso(end_time) - start_time_float
+        self.info_data[u"duration"] = duration
