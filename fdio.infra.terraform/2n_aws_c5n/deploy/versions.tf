9a907a66a8f28e1c2f4dc1aa0e871eebd875cb77
pmikus <pmikus@cisco.com>
2021-10-05 10:05:17 +0000

    fix(Terraform): Bump AWS API
    Signed-off-by: pmikus <pmikus@cisco.com>
    Change-Id: I057bd9875a3981245b570e61ff3ba2f478def769
    (cherry picked from commit 5015522409d29bdeabb247568ebb0760833b939b)

--- fdio.infra.terraform/2n_aws_c5n/deploy/versions.tf
+++ fdio.infra.terraform/2n_aws_c5n/deploy/versions.tf
@@ -2,15 +2,15 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 3.32.0"
+      version = "~> 3.61.0"
     }
     null = {
       source  = "hashicorp/null"
-      version = "~> 3.0.0"
+      version = "~> 3.1.0"
     }
     tls = {
       source  = "hashicorp/tls"
-      version = "~> 3.0.0"
+      version = "~> 3.1.0"
     }
     vault = {
       version = ">=2.22.1"
