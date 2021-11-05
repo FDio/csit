acec8978bb5424e687238f578c1f35bcba8a1360
pmikus <pmikus@cisco.com>
2021-11-05 18:55:40 +0100

    feat(Terraform): Minio s3 gateway
    Signed-off-by: pmikus <pmikus@cisco.com>
    Change-Id: Icdf6531b083a8fcdfe2a5680b48e3c88f1679d45

--- fdio.infra.terraform/1n_nmd/variables.tf
+++ fdio.infra.terraform/1n_nmd/variables.tf
@@ -1,11 +1,35 @@
+variable "nomad_acl" {
+  description = "Nomad ACLs enabled/disabled"
+  type        = bool
+  default     = false
+}
+
 variable "nomad_provider_address" {
   description = "FD.io Nomad cluster address."
   type        = string
   default     = "http://nomad.service.consul:4646"
 }
 
-variable "nomad_acl" {
-  description = "Nomad ACLs enabled/disabled"
-  type        = bool
-  default     = false
+variable "nomad_provider_ca_file" {
+  description = "A local file path to a PEM-encoded certificate authority."
+  type        = string
+  default     = "/etc/nomad.d/ssl/nomad-ca.pem"
+}
+
+variable "nomad_provider_cert_file" {
+  description = "A local file path to a PEM-encoded certificate."
+  type        = string
+  default     = "/etc/nomad.d/ssl/nomad-cli.pem"
+}
+
+variable "nomad_provider_key_file" {
+  description = "A local file path to a PEM-encoded private key."
+  type        = string
+  default     = "/etc/nomad.d/ssl/nomad-cli-key.pem"
+}
+
+variable "token" {
+  description = "Vault root token"
+  type        = string
+  sensitive   = true
 }
