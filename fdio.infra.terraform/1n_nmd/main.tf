8fd9136fafcf560e5fb1c38fb3469f162702d812
pmikus <pmikus@cisco.com>
2021-11-08 12:50:51 +0100

    feat(Terraform): Minio S3 gateway proxy
    Signed-off-by: pmikus <pmikus@cisco.com>
    Change-Id: I9543a856a2e5791b61accd7a183bc5a2bf2e6187

--- fdio.infra.terraform/1n_nmd/main.tf
+++ fdio.infra.terraform/1n_nmd/main.tf
@@ -34,88 +34,116 @@ module "alertmanager" {
   alertmanager_slack_default_channel = "fdio-infra-monitoring"
 }
 
-module "grafana" {
-  source = "./grafana"
-  providers = {
-    nomad = nomad.yul1
-  }
+#module "grafana" {
+#  source = "./grafana"
+#  providers = {
+#    nomad = nomad.yul1
+#  }
+#
+#  # nomad
+#  nomad_datacenters = ["yul1"]
+#
+#  # grafana
+#  grafana_job_name    = "prod-grafana"
+#  grafana_use_canary  = true
+#  grafana_group_count = 1
+#  grafana_vault_secret = {
+#    use_vault_provider        = false,
+#    vault_kv_policy_name      = "kv-secret",
+#    vault_kv_path             = "secret/data/grafana",
+#    vault_kv_field_access_key = "access_key",
+#    vault_kv_field_secret_key = "secret_key"
+#  }
+#  grafana_container_image = "grafana/grafana:7.3.7"
+#  grafana_cpu             = 1000
+#  grafana_mem             = 2048
+#  grafana_port            = 3000
+#}
+#
+#module "minio" {
+#  source = "./minio"
+#  providers = {
+#    nomad = nomad.yul1
+#  }
+#
+#  # nomad
+#  nomad_datacenters = ["yul1"]
+#  nomad_host_volume = "prod-volume-data1-1"
+#
+#  # minio
+#  minio_job_name        = "prod-minio"
+#  minio_group_count     = 4
+#  minio_service_name    = "storage"
+#  minio_host            = "http://10.32.8.1{4...7}"
+#  minio_port            = 9000
+#  minio_container_image = "minio/minio:RELEASE.2021-07-27T02-40-15Z"
+#  minio_vault_secret = {
+#    use_vault_provider        = false,
+#    vault_kv_policy_name      = "kv-secret",
+#    vault_kv_path             = "secret/data/minio",
+#    vault_kv_field_access_key = "access_key",
+#    vault_kv_field_secret_key = "secret_key"
+#  }
+#  minio_data_dir        = "/data/"
+#  minio_use_host_volume = true
+#  minio_use_canary      = true
+#  minio_envs            = ["MINIO_BROWSER=\"off\""]
+#
+#  minio_buckets = ["logs.fd.io"]
+#}
 
-  # nomad
-  nomad_datacenters = ["yul1"]
-
-  # grafana
-  grafana_job_name    = "prod-grafana"
-  grafana_use_canary  = true
-  grafana_group_count = 1
-  grafana_vault_secret = {
-    use_vault_provider        = false,
-    vault_kv_policy_name      = "kv-secret",
-    vault_kv_path             = "secret/data/grafana",
-    vault_kv_field_access_key = "access_key",
-    vault_kv_field_secret_key = "secret_key"
-  }
-  grafana_container_image = "grafana/grafana:7.3.7"
-  grafana_cpu             = 1000
-  grafana_mem             = 2048
-  grafana_port            = 3000
+data "vault_generic_secret" "minio_creds" {
+  path = "kv/secret/data/minio"
 }
 
-module "minio" {
-  source = "./minio"
+module "minio_s3_gateway" {
+  source = "./minio_s3_gateway"
   providers = {
     nomad = nomad.yul1
   }
 
   # nomad
-  nomad_datacenters = ["yul1"]
-  nomad_host_volume = "prod-volume-data1-1"
+  datacenters   = ["yul1"]
+  volume_source = "prod-volume-data1-1"
 
   # minio
-  minio_job_name        = "prod-minio"
-  minio_group_count     = 4
-  minio_service_name    = "storage"
-  minio_host            = "http://10.32.8.1{4...7}"
-  minio_port            = 9000
-  minio_container_image = "minio/minio:RELEASE.2021-07-27T02-40-15Z"
-  minio_vault_secret = {
-    use_vault_provider        = false,
-    vault_kv_policy_name      = "kv-secret",
-    vault_kv_path             = "secret/data/minio",
-    vault_kv_field_access_key = "access_key",
-    vault_kv_field_secret_key = "secret_key"
-  }
-  minio_data_dir        = "/data/"
-  minio_use_host_volume = true
-  minio_use_canary      = true
-  minio_envs            = ["MINIO_BROWSER=\"off\""]
-
-  # minio client
-  mc_job_name        = "prod-mc"
-  mc_container_image = "minio/mc:RELEASE.2021-07-27T02-40-15Z"
-  mc_extra_commands = [
-    "mc policy set public LOCALMINIO/logs.fd.io",
-    "mc policy set public LOCALMINIO/docs.fd.io",
-    "mc ilm add --expiry-days '180' LOCALMINIO/logs.fd.io",
-    "mc admin user add LOCALMINIO storage Storage1234",
-    "mc admin policy set LOCALMINIO writeonly user=storage"
+  job_name           = "minio-s3-gateway"
+  group_count        = 4
+  service_name       = "minio"
+  mode               = "gateway"
+  port_base          = 9001
+  image              = "minio/minio:latest"
+  access_key         = data.vault_generic_secret.minio_creds.data["access_key"]
+  secret_key         = data.vault_generic_secret.minio_creds.data["secret_key"]
+  volume_destination = "/data/"
+  use_host_volume    = true
+  use_canary         = true
+  envs = [
+    "MINIO_BROWSER=\"on\"",
+    "MINIO_CACHE=\"on\"",
+    "MINIO_CACHE_DRIVES=\"/data/s3_cache1\"",
+    "MINIO_CACHE_EXCLUDE=\"\"",
+    "MINIO_CACHE_QUOTA=80",
+    "MINIO_CACHE_AFTER=3",
+    "MINIO_CACHE_WATERMARK_LOW=70",
+    "MINIO_CACHE_WATERMARK_HIGH=90"
   ]
-  minio_buckets = ["logs.fd.io", "docs.fd.io"]
 }
 
-module "nginx" {
-  source = "./nginx"
-  providers = {
-    nomad = nomad.yul1
-  }
-
-  # nomad
-  nomad_datacenters = ["yul1"]
-  nomad_host_volume = "prod-volume-data1-1"
-
-  # nginx
-  nginx_job_name        = "prod-nginx"
-  nginx_use_host_volume = true
-}
+#module "nginx" {
+#  source = "./nginx"
+#  providers = {
+#    nomad = nomad.yul1
+#  }
+#
+#  # nomad
+#  nomad_datacenters = ["yul1"]
+#  nomad_host_volume = "prod-volume-data1-1"
+#
+#  # nginx
+#  nginx_job_name        = "prod-nginx"
+#  nginx_use_host_volume = true
+#}
 
 module "prometheus" {
   source = "./prometheus"
@@ -146,20 +174,21 @@ module "prometheus" {
   prometheus_port            = 9090
 }
 
-module "vpp_device" {
-  source = "./vpp_device"
-  providers = {
-    nomad = nomad.yul1
-  }
-
-  # nomad
-  nomad_datacenters = ["yul1"]
-
-  # csit_shim
-  csit_shim_job_name      = "prod-device-csit-shim"
-  csit_shim_group_count   = "1"
-  csit_shim_cpu           = "1500"
-  csit_shim_mem           = "4096"
-  csit_shim_image_aarch64 = "fdiotools/csit_shim-ubuntu2004:2021_03_02_143938_UTC-aarch64"
-  csit_shim_image_x86_64  = "fdiotools/csit_shim-ubuntu2004:2021_03_04_142103_UTC-x86_64"
-}
+#module "vpp_device" {
+#  source = "./vpp_device"
+#  providers = {
+#    nomad = nomad.yul1
+#  }
+#
+#  # nomad
+#  nomad_datacenters = ["yul1"]
+#
+#  # csit_shim
+#  csit_shim_job_name      = "prod-device-csit-shim"
+#  csit_shim_group_count   = "1"
+#  csit_shim_cpu           = "1500"
+#  csit_shim_mem           = "4096"
+#  csit_shim_image_aarch64 = "fdiotools/csit_shim-ubuntu2004:2021_03_02_143938_UTC-aarch64"
+#  csit_shim_image_x86_64  = "fdiotools/csit_shim-ubuntu2004:2021_03_04_142103_UTC-x86_64"
+#}
+#
--- fdio.infra.terraform/1n_nmd/versions.tf
+++ fdio.infra.terraform/1n_nmd/versions.tf
@@ -1,4 +1,9 @@
 terraform {
+  backend "consul" {
+    address = "consul.service.consul:8500"
+    scheme  = "http"
+    path    = "terraform/nomad"
+  }
   required_providers {
     nomad = {
       source  = "hashicorp/nomad"
@@ -6,7 +11,10 @@ terraform {
     }
     template = {
       source  = "hashicorp/template"
-      version = "~> 2.1.2"
+      version = "~> 2.2.0"
+    }
+    vault = {
+      version = ">=2.14.0"
     }
   }
   required_version = ">= 1.0.3"
