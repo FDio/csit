032667c203dcd5e8e9e7722c88f7a845cea439e8
pmikus <pmikus@cisco.com>
2021-11-08 12:50:51 +0100

    feat(Terraform): Minio S3 gateway proxy
    Signed-off-by: pmikus <pmikus@cisco.com>
    Change-Id: I9543a856a2e5791b61accd7a183bc5a2bf2e6187

--- fdio.infra.terraform/1n_nmd/providers.tf
+++ fdio.infra.terraform/1n_nmd/providers.tf
@@ -1,21 +1,13 @@
-terraform {
-  required_providers {
-    nomad = {
-      source  = "hashicorp/nomad"
-      version = "~> 1.4.9"
-    }
-    template = {
-      source  = "hashicorp/template"
-      version = "~> 2.1.2"
-    }
-    vault = {
-      version = ">=2.14.0"
-    }
-  }
-  required_version = ">= 0.13"
-}
-
 provider "nomad" {
   address = var.nomad_provider_address
   alias   = "yul1"
+  #  ca_file   = var.nomad_provider_ca_file
+  #  cert_file = var.nomad_provider_cert_file
+  #  key_file  = var.nomad_provider_key_file
+}
+
+provider "vault" {
+  address         = "http://10.30.51.28:8200"
+  skip_tls_verify = true
+  token           = var.token
 }
