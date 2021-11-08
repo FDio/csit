1048c8eff1097fc66ecbd59ad399a59e1423fd2d
pmikus <pmikus@cisco.com>
2021-11-08 12:50:51 +0100

    feat(Terraform): Minio S3 gateway proxy
    Signed-off-by: pmikus <pmikus@cisco.com>
    Change-Id: I9543a856a2e5791b61accd7a183bc5a2bf2e6187

--- fdio.infra.terraform/1n_nmd/main.tf
+++ fdio.infra.terraform/1n_nmd/main.tf
@@ -4,162 +4,191 @@
 # and downstream modules can simply declare resources for that provider
 # and have them automatically associated with the root provider
 # configurations.
-module "alertmanager" {
-  source = "./alertmanager"
-  providers = {
-    nomad = nomad.yul1
-  }
+#module "alertmanager" {
+#  source = "./alertmanager"
+#  providers = {
+#    nomad = nomad.yul1
+#  }
+#
+#  # nomad
+#  nomad_datacenters = ["yul1"]
+#
+#  # alertmanager
+#  alertmanager_job_name    = "prod-alertmanager"
+#  alertmanager_use_canary  = true
+#  alertmanager_group_count = 1
+#  alertmanager_vault_secret = {
+#    use_vault_provider        = false,
+#    vault_kv_policy_name      = "kv-secret",
+#    vault_kv_path             = "secret/data/prometheus",
+#    vault_kv_field_access_key = "access_key",
+#    vault_kv_field_secret_key = "secret_key"
+#  }
+#  alertmanager_version               = "0.21.0"
+#  alertmanager_cpu                   = 1000
+#  alertmanager_mem                   = 1024
+#  alertmanager_port                  = 9093
+#  alertmanager_slack_jenkins_api_key = "TE07RD1V1/B01U1NV9HV3/hKZXJJ74g2JcISq4K3QC1eG9"
+#  alertmanager_slack_jenkins_channel = "fdio-jobs-monitoring"
+#  alertmanager_slack_default_api_key = "TE07RD1V1/B01UUK23B6C/hZTcCu42FUv8d6rtirHtcYIi"
+#  alertmanager_slack_default_channel = "fdio-infra-monitoring"
+#}
+#
+#module "grafana" {
+#  source = "./grafana"
+#  providers = {
+#    nomad = nomad.yul1
+#  }
+#
+#  # nomad
+#  nomad_datacenters = ["yul1"]
+#
+#  # grafana
+#  grafana_job_name    = "prod-grafana"
+#  grafana_use_canary  = true
+#  grafana_group_count = 1
+#  grafana_vault_secret = {
+#    use_vault_provider        = false,
+#    vault_kv_policy_name      = "kv-secret",
+#    vault_kv_path             = "secret/data/grafana",
+#    vault_kv_field_access_key = "access_key",
+#    vault_kv_field_secret_key = "secret_key"
+#  }
+#  grafana_container_image = "grafana/grafana:7.3.7"
+#  grafana_cpu             = 1000
+#  grafana_mem             = 2048
+#  grafana_port            = 3000
+#}
+#
+#module "minio" {
+#  source = "./minio"
+#  providers = {
+#    nomad = nomad.yul1
+#  }
+#
+#  # nomad
+#  nomad_datacenters = ["yul1"]
+#  nomad_host_volume = "prod-volume-data1-1"
+#
+#  # minio
+#  minio_job_name        = "prod-minio"
+#  minio_group_count     = 4
+#  minio_service_name    = "storage"
+#  minio_host            = "http://10.32.8.1{4...7}"
+#  minio_port            = 9000
+#  minio_container_image = "minio/minio:RELEASE.2021-07-27T02-40-15Z"
+#  minio_vault_secret = {
+#    use_vault_provider        = false,
+#    vault_kv_policy_name      = "kv-secret",
+#    vault_kv_path             = "secret/data/minio",
+#    vault_kv_field_access_key = "access_key",
+#    vault_kv_field_secret_key = "secret_key"
+#  }
+#  minio_data_dir        = "/data/"
+#  minio_use_host_volume = true
+#  minio_use_canary      = true
+#  minio_envs            = ["MINIO_BROWSER=\"off\""]
+#
+#  minio_buckets = ["logs.fd.io"]
+#}
 
-  # nomad
-  nomad_datacenters = ["yul1"]
-
-  # alertmanager
-  alertmanager_job_name    = "prod-alertmanager"
-  alertmanager_use_canary  = true
-  alertmanager_group_count = 1
-  alertmanager_vault_secret = {
-    use_vault_provider        = false,
-    vault_kv_policy_name      = "kv-secret",
-    vault_kv_path             = "secret/data/prometheus",
-    vault_kv_field_access_key = "access_key",
-    vault_kv_field_secret_key = "secret_key"
-  }
-  alertmanager_version               = "0.21.0"
-  alertmanager_cpu                   = 1000
-  alertmanager_mem                   = 1024
-  alertmanager_port                  = 9093
-  alertmanager_slack_jenkins_api_key = "TE07RD1V1/B01U1NV9HV3/hKZXJJ74g2JcISq4K3QC1eG9"
-  alertmanager_slack_jenkins_channel = "fdio-jobs-monitoring"
-  alertmanager_slack_default_api_key = "TE07RD1V1/B01UUK23B6C/hZTcCu42FUv8d6rtirHtcYIi"
-  alertmanager_slack_default_channel = "fdio-infra-monitoring"
+data "vault_generic_secret" "minio_creds" {
+  path = "kv/secret/data/minio"
 }
 
-module "grafana" {
-  source = "./grafana"
+module "minio_s3_gateway" {
+  source = "./minio_s3_gateway"
   providers = {
     nomad = nomad.yul1
   }
 
   # nomad
-  nomad_datacenters = ["yul1"]
-
-  # grafana
-  grafana_job_name    = "prod-grafana"
-  grafana_use_canary  = true
-  grafana_group_count = 1
-  grafana_vault_secret = {
-    use_vault_provider        = false,
-    vault_kv_policy_name      = "kv-secret",
-    vault_kv_path             = "secret/data/grafana",
-    vault_kv_field_access_key = "access_key",
-    vault_kv_field_secret_key = "secret_key"
-  }
-  grafana_container_image = "grafana/grafana:7.3.7"
-  grafana_cpu             = 1000
-  grafana_mem             = 2048
-  grafana_port            = 3000
-}
-
-module "minio" {
-  source = "./minio"
-  providers = {
-    nomad = nomad.yul1
-  }
-
-  # nomad
-  nomad_datacenters = ["yul1"]
-  nomad_host_volume = "prod-volume-data1-1"
+  datacenters   = ["yul1"]
+  volume_source = "prod-volume-data1-1"
 
   # minio
-  minio_job_name        = "prod-minio"
-  minio_group_count     = 4
-  minio_service_name    = "storage"
-  minio_host            = "http://10.32.8.1{4...7}"
-  minio_port            = 9000
-  minio_container_image = "minio/minio:RELEASE.2021-07-27T02-40-15Z"
-  minio_vault_secret = {
-    use_vault_provider        = false,
-    vault_kv_policy_name      = "kv-secret",
-    vault_kv_path             = "secret/data/minio",
-    vault_kv_field_access_key = "access_key",
-    vault_kv_field_secret_key = "secret_key"
-  }
-  minio_data_dir        = "/data/"
-  minio_use_host_volume = true
-  minio_use_canary      = true
-  minio_envs            = ["MINIO_BROWSER=\"off\""]
-
-  # minio client
-  mc_job_name        = "prod-mc"
-  mc_container_image = "minio/mc:RELEASE.2021-07-27T02-40-15Z"
-  mc_extra_commands = [
-    "mc policy set public LOCALMINIO/logs.fd.io",
-    "mc policy set public LOCALMINIO/docs.fd.io",
-    "mc ilm add --expiry-days '180' LOCALMINIO/logs.fd.io",
-    "mc admin user add LOCALMINIO storage Storage1234",
-    "mc admin policy set LOCALMINIO writeonly user=storage"
+  job_name           = "minio-s3-gateway"
+  group_count        = 4
+  service_name       = "minio"
+  mode               = "gateway"
+  port_static        = 9001
+  image              = "minio/minio:latest"
+  access_key         = data.vault_generic_secret.minio_creds.data["access_key"]
+  secret_key         = data.vault_generic_secret.minio_creds.data["secret_key"]
+  volume_destination = "/data/"
+  use_host_volume    = true
+  use_canary         = true
+  envs = [
+    "MINIO_BROWSER=\"on\"",
+    "MINIO_CACHE=\"on\"",
+    "MINIO_CACHE_DRIVES=\"/data/s3_cache1\"",
+    "MINIO_CACHE_EXCLUDE=\"\"",
+    "MINIO_CACHE_QUOTA=80",
+    "MINIO_CACHE_AFTER=3",
+    "MINIO_CACHE_WATERMARK_LOW=70",
+    "MINIO_CACHE_WATERMARK_HIGH=90"
   ]
-  minio_buckets = ["logs.fd.io", "docs.fd.io"]
 }
 
-module "nginx" {
-  source = "./nginx"
-  providers = {
-    nomad = nomad.yul1
-  }
-
-  # nomad
-  nomad_datacenters = ["yul1"]
-  nomad_host_volume = "prod-volume-data1-1"
-
-  # nginx
-  nginx_job_name        = "prod-nginx"
-  nginx_use_host_volume = true
-}
-
-module "prometheus" {
-  source = "./prometheus"
-  providers = {
-    nomad = nomad.yul1
-  }
-
-  # nomad
-  nomad_datacenters = ["yul1"]
-  nomad_host_volume = "prod-volume-data1-1"
-
-  # prometheus
-  prometheus_job_name    = "prod-prometheus"
-  prometheus_use_canary  = true
-  prometheus_group_count = 4
-  prometheus_vault_secret = {
-    use_vault_provider        = false,
-    vault_kv_policy_name      = "kv-secret",
-    vault_kv_path             = "secret/data/prometheus",
-    vault_kv_field_access_key = "access_key",
-    vault_kv_field_secret_key = "secret_key"
-  }
-  prometheus_data_dir        = "/data/"
-  prometheus_use_host_volume = true
-  prometheus_version         = "2.28.1"
-  prometheus_cpu             = 2000
-  prometheus_mem             = 8192
-  prometheus_port            = 9090
-}
-
-module "vpp_device" {
-  source = "./vpp_device"
-  providers = {
-    nomad = nomad.yul1
-  }
-
-  # nomad
-  nomad_datacenters = ["yul1"]
-
-  # csit_shim
-  csit_shim_job_name      = "prod-device-csit-shim"
-  csit_shim_group_count   = "1"
-  csit_shim_cpu           = "1500"
-  csit_shim_mem           = "4096"
-  csit_shim_image_aarch64 = "fdiotools/csit_shim-ubuntu2004:2021_03_02_143938_UTC-aarch64"
-  csit_shim_image_x86_64  = "fdiotools/csit_shim-ubuntu2004:2021_03_04_142103_UTC-x86_64"
-}
+#module "nginx" {
+#  source = "./nginx"
+#  providers = {
+#    nomad = nomad.yul1
+#  }
+#
+#  # nomad
+#  nomad_datacenters = ["yul1"]
+#  nomad_host_volume = "prod-volume-data1-1"
+#
+#  # nginx
+#  nginx_job_name        = "prod-nginx"
+#  nginx_use_host_volume = true
+#}
+#
+#module "prometheus" {
+#  source = "./prometheus"
+#  providers = {
+#    nomad = nomad.yul1
+#  }
+#
+#  # nomad
+#  nomad_datacenters = ["yul1"]
+#  nomad_host_volume = "prod-volume-data1-1"
+#
+#  # prometheus
+#  prometheus_job_name    = "prod-prometheus"
+#  prometheus_use_canary  = true
+#  prometheus_group_count = 4
+#  prometheus_vault_secret = {
+#    use_vault_provider        = false,
+#    vault_kv_policy_name      = "kv-secret",
+#    vault_kv_path             = "secret/data/prometheus",
+#    vault_kv_field_access_key = "access_key",
+#    vault_kv_field_secret_key = "secret_key"
+#  }
+#  prometheus_data_dir        = "/data/"
+#  prometheus_use_host_volume = true
+#  prometheus_version         = "2.28.1"
+#  prometheus_cpu             = 2000
+#  prometheus_mem             = 8192
+#  prometheus_port            = 9090
+#}
+#
+#module "vpp_device" {
+#  source = "./vpp_device"
+#  providers = {
+#    nomad = nomad.yul1
+#  }
+#
+#  # nomad
+#  nomad_datacenters = ["yul1"]
+#
+#  # csit_shim
+#  csit_shim_job_name      = "prod-device-csit-shim"
+#  csit_shim_group_count   = "1"
+#  csit_shim_cpu           = "1500"
+#  csit_shim_mem           = "4096"
+#  csit_shim_image_aarch64 = "fdiotools/csit_shim-ubuntu2004:2021_03_02_143938_UTC-aarch64"
+#  csit_shim_image_x86_64  = "fdiotools/csit_shim-ubuntu2004:2021_03_04_142103_UTC-x86_64"
+#}
+#
---
+++ fdio.infra.terraform/1n_nmd/minio_s3_gateway/conf/nomad/minio.hcl
@@ -0,0 +1,246 @@
+job "${job_name}" {
+  # The "region" parameter specifies the region in which to execute the job.
+  # If omitted, this inherits the default region name of "global".
+  # region    = "${region}"
+
+  # The "datacenters" parameter specifies the list of datacenters which should
+  # be considered when placing this task. This must be provided.
+  datacenters = "${datacenters}"
+
+  # The "type" parameter controls the type of job, which impacts the scheduler's
+  # decision on placement. This configuration is optional and defaults to
+  # "service". For a full list of job types and their differences, please see
+  # the online documentation.
+  #
+  #     https://www.nomadproject.io/docs/jobspec/schedulers
+  #
+  type        = "service"
+
+  update {
+    # The "max_parallel" parameter specifies the maximum number of updates to
+    # perform in parallel. In this case, this specifies to update a single task
+    # at a time.
+    max_parallel      = ${max_parallel}
+
+    health_check      = "checks"
+
+    # The "min_healthy_time" parameter specifies the minimum time the allocation
+    # must be in the healthy state before it is marked as healthy and unblocks
+    # further allocations from being updated.
+    min_healthy_time  = "10s"
+
+    # The "healthy_deadline" parameter specifies the deadline in which the
+    # allocation must be marked as healthy after which the allocation is
+    # automatically transitioned to unhealthy. Transitioning to unhealthy will
+    # fail the deployment and potentially roll back the job if "auto_revert" is
+    # set to true.
+    healthy_deadline  = "3m"
+
+    # The "progress_deadline" parameter specifies the deadline in which an
+    # allocation must be marked as healthy. The deadline begins when the first
+    # allocation for the deployment is created and is reset whenever an allocation
+    # as part of the deployment transitions to a healthy state. If no allocation
+    # transitions to the healthy state before the progress deadline, the
+    # deployment is marked as failed.
+    progress_deadline = "10m"
+
+%{ if use_canary }
+    # The "canary" parameter specifies that changes to the job that would result
+    # in destructive updates should create the specified number of canaries
+    # without stopping any previous allocations. Once the operator determines the
+    # canaries are healthy, they can be promoted which unblocks a rolling update
+    # of the remaining allocations at a rate of "max_parallel".
+    #
+    # Further, setting "canary" equal to the count of the task group allows
+    # blue/green deployments. When the job is updated, a full set of the new
+    # version is deployed and upon promotion the old version is stopped.
+    canary            = ${canary}
+
+    # Specifies if the job should auto-promote to the canary version when all
+    # canaries become healthy during a deployment. Defaults to false which means
+    # canaries must be manually updated with the nomad deployment promote
+    # command.
+    auto_promote      = ${auto_promote}
+
+    # The "auto_revert" parameter specifies if the job should auto-revert to the
+    # last stable job on deployment failure. A job is marked as stable if all the
+    # allocations as part of its deployment were marked healthy.
+    auto_revert       = ${auto_revert}
+%{ endif }
+  }
+
+  # All groups in this job should be scheduled on different hosts.
+  constraint {
+    operator = "distinct_hosts"
+    value    = "true"
+  }
+
+  # The "group" stanza defines a series of tasks that should be co-located on
+  # the same Nomad client. Any task within a group will be placed on the same
+  # client.
+  #
+  #     https://www.nomadproject.io/docs/job-specification/group
+  #
+  group "${job_name}-group-1" {
+    # The "count" parameter specifies the number of the task groups that should
+    # be running under this group. This value must be non-negative and defaults
+    # to 1.
+    count = ${group_count}
+
+    # The volume stanza allows the group to specify that it requires a given
+    # volume from the cluster. The key of the stanza is the name of the volume
+    # as it will be exposed to task configuration.
+    #
+    # https://www.nomadproject.io/docs/job-specification/volume
+    %{ if use_host_volume }
+    volume "${job_name}-volume-1" {
+      type      = "host"
+      read_only = false
+      source    = "${volume_source}"
+    }
+    %{ endif }
+
+    # The restart stanza configures a tasks's behavior on task failure. Restarts
+    # happen on the client that is running the task.
+    #
+    # https://www.nomadproject.io/docs/job-specification/restart
+    #
+    restart {
+      interval = "30m"
+      attempts = 40
+      delay    = "15s"
+      mode     = "delay"
+    }
+
+    # The network stanza specifies the networking requirements for the task
+    # group, including the network mode and port allocations. When scheduling
+    # jobs in Nomad they are provisioned across your fleet of machines along
+    # with other jobs and services. Because you don't know in advance what host
+    # your job will be provisioned on, Nomad will provide your tasks with
+    # network configuration when they start up.
+    #
+    #     https://www.nomadproject.io/docs/job-specification/network
+    #
+    network {
+      port "base" {
+        static = ${port_static}
+        to     = ${port_static}
+      }
+      port "console" {
+        static = 9002
+        to     = 9002
+      }
+    }
+
+    # The "task" stanza creates an individual unit of work, such as a Docker
+    # container, web application, or batch processing.
+    #
+    #     https://www.nomadproject.io/docs/job-specification/task.html
+    #
+    task "${job_name}-task-1" {
+      # The "driver" parameter specifies the task driver that should be used to
+      # run the task.
+      driver = "exec"
+
+    %{ if use_host_volume }
+      volume_mount {
+        volume      = "${job_name}-volume-1"
+        destination = "${volume_destination}"
+        read_only   = false
+      }
+    %{ endif }
+
+    %{ if use_vault_provider }
+      vault {
+        policies = "${vault_kv_policy_name}"
+      }
+    %{ endif }
+
+      # The "config" stanza specifies the driver configuration, which is passed
+      # directly to the driver to start the task. The details of configurations
+      # are specific to each driver, so please see specific driver
+      # documentation for more information.
+      config {
+        command      = "local/minio"
+        args         = [
+          "${mode}", "s3",
+          "-address", ":${port_static}",
+          "-console-address", ":9002"
+        ]
+      }
+
+      # The artifact stanza instructs Nomad to fetch and unpack a remote resource,
+      # such as a file, tarball, or binary. Nomad downloads artifacts using the
+      # popular go-getter library, which permits downloading artifacts from a
+      # variety of locations using a URL as the input source.
+      #
+      # For more information and examples on the "artifact" stanza, please see
+      # the online documentation at:
+      #
+      #     https://www.nomadproject.io/docs/job-specification/artifact
+      #
+      artifact {
+        source      = "https://dl.min.io/server/minio/release/linux-amd64/minio"
+      }
+
+      # The env stanza configures a list of environment variables to populate
+      # the task's environment before starting.
+      env {
+%{ if use_vault_provider }
+{{ with secret "${vault_kv_path}" }}
+        MINIO_ROOT_USER     = "{{ .Data.data.${vault_kv_field_access_key} }}"
+        MINIO_ROOT_PASSWORD = "{{ .Data.data.${vault_kv_field_secret_key} }}"
+{{ end }}
+%{ else }
+        MINIO_ROOT_USER       = "${access_key}"
+        MINIO_ROOT_PASSWORD   = "${secret_key}"
+        AWS_ACCESS_KEY_ID     = "${access_key}"
+        AWS_SECRET_ACCESS_KEY = "${secret_key}"
+%{ endif }
+        ${ envs }
+      }
+
+      # The service stanza instructs Nomad to register a service with Consul.
+      #
+      #     https://www.nomadproject.io/docs/job-specification/service
+      #
+      service {
+        name       = "${service_name}"
+        port       = "http"
+        tags       = [ "${service_name}$${NOMAD_ALLOC_INDEX}" ]
+        check {
+          name     = "Min.io Server HTTP Check Live"
+          type     = "http"
+          port     = "base"
+          protocol = "http"
+          method   = "GET"
+          path     = "/minio/health/live"
+          interval = "10s"
+          timeout  = "2s"
+        }
+        check {
+          name     = "Min.io Server HTTP Check Ready"
+          type     = "http"
+          port     = "base"
+          protocol = "http"
+          method   = "GET"
+          path     = "/minio/health/ready"
+          interval = "10s"
+          timeout  = "2s"
+        }
+      }
+
+      # The "resources" stanza describes the requirements a task needs to
+      # execute. Resource requirements include memory, network, cpu, and more.
+      # This ensures the task will execute on a machine that contains enough
+      # resource capacity.
+      #
+      #     https://www.nomadproject.io/docs/job-specification/resources
+      #
+      resources {
+        cpu        = ${cpu}
+        memory     = ${memory}
+      }
+    }
+  }
+}
