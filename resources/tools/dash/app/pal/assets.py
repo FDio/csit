dfa912b0e63a329dbb48a6312fcc2c6849de3954
pmikus <pmikus@cisco.com>
2021-11-19 15:23:35 +0100

    feat(aws): CSIT ETL
    Signed-off-by: pmikus <pmikus@cisco.com>
    Change-Id: Ic19043fce754441b66111faa5e73cdabb424d618

---
+++ resources/tools/dash/app/pal/assets.py
@@ -0,0 +1,38 @@
+# Copyright (c) 2021 Cisco and/or its affiliates.
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at:
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Compile static assets."""
+from flask import current_app as app
+from flask_assets import Bundle
+
+
+def compile_static_assets(assets):
+    """Compile stylesheets if in development mode.
+
+    :param assets: Flask-Assets Environment.
+    :type assets: Environment
+    :returns: Compiled stylesheets.
+    :rtype: Environment
+    """
+    assets.auto_build = True
+    assets.debug = False
+    less_bundle = Bundle(
+        "less/*.less",
+        filters="less,cssmin",
+        output="dist/css/styles.css",
+        extra={"rel": "stylesheet/less"},
+    )
+    assets.register("less_all", less_bundle)
+    if app.config["FLASK_ENV"] == "development":
+        less_bundle.build()
+    return assets
