e23d469a072d44fd0158c5dacae7768cf25d091f
pmikus <pmikus@cisco.com>
2021-11-19 15:23:35 +0100

    feat(aws): Glue
    Signed-off-by: pmikus <pmikus@cisco.com>
    Change-Id: Ic19043fce754441b66111faa5e73cdabb424d618

---
+++ resources/tools/glue/ci.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env bash
+
+# Copyright (c) 2021 Cisco and/or its affiliates.
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at:
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+set -exuo pipefail
+
+docker build \
+    --tag csit-glue:glue_libs_3.0.0 .
+
+docker run \
+    --rm \
+    --interactive \
+    --tty \
+    --detach \
+    --publish 4040:4040 \
+    --volume $HOME/.aws:/root/.aws \
+    --volume $PWD:/job_queue \
+    --name csit-glue \
+    csit-glue:glue_libs_3.0.0
+
+docker exec \
+    -it \
+    --interactive \
+    --tty \
+    csit-glue \
+    gluesparksubmit \
+        --master local \
+        --deploy-mode client \
+        --name etl_script_s3 \
+        /job_queue/etl_script_s3.py
