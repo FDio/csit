47c215df6c349e7ba5ae85d86c66cc50a30b95d7
pmikus <pmikus@cisco.com>
2021-11-19 15:23:35 +0100

    feat(aws): CSIT ETL
    Signed-off-by: pmikus <pmikus@cisco.com>
    Change-Id: Ic19043fce754441b66111faa5e73cdabb424d618

---
+++ resources/tools/dash/app/pal/trending/data.py
@@ -0,0 +1,27 @@
+# Copyright (c) 2021 Cisco and/or its affiliates.
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at:
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Prepare data for Plotly Dash."""
+import numpy as np
+import pandas as pd
+
+
+def create_dataframe():
+    """Create Pandas DataFrame from local CSV."""
+    df = pd.read_csv("data/311-calls.csv", parse_dates=["created"])
+    df["created"] = df["created"].dt.date
+    df.drop(columns=["incident_zip"], inplace=True)
+    num_complaints = df["complaint_type"].value_counts()
+    to_remove = num_complaints[num_complaints <= 30].index
+    df.replace(to_remove, np.nan, inplace=True)
+    return df
