23927330f4e5428bd56af8aa59224879e42c6200
pmikus <pmikus@cisco.com>
2021-11-19 15:23:35 +0100

    feat(aws): CSIT ETL
    Signed-off-by: pmikus <pmikus@cisco.com>
    Change-Id: Ic19043fce754441b66111faa5e73cdabb424d618

---
+++ resources/tools/dash/app/pal/routes.py
@@ -0,0 +1,28 @@
+# Copyright (c) 2021 Cisco and/or its affiliates.
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at:
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Routes for parent Flask app."""
+from flask import current_app as app
+from flask import render_template
+
+
+@app.route("/")
+def home():
+    """Landing page."""
+    return render_template(
+        "index.jinja2",
+        title="CSIT PLOTLY DASH for PAL DEMO",
+        description="CSIT DASH based PAL based on Flask application.",
+        template="home-template",
+        body="This is a homepage served with Flask.",
+    )
