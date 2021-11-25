91a74739a14e5daec9db306d07026c1e3b0fc180
Vratko Polak <vrpolak@cisco.com>
2021-11-25 16:15:44 +0100

    UTI: Export results
    + Model version 1.0.0.
    - Only some result types are exported.
     + MRR, NDRPDR and SOAK.
     - Other test types to be added later.
    + Also export outputs for suite setups and teardowns.
     + It will be needed later, mainly for the "log" list.
     + Additionally suite.output.info.json is created to hold total duration.
    + Convert custom classes to JSON-serializable equivalents.
    + Sort dict keys before converting to JSON.
     + Override the order for some known keys.
    + Convert to info content from serialized raw content.
    + Define model via JSON schema:
     - Just test case, suite setup/teardown/suite to be added later.
     - Just info, raw to be added later.
     + Proper descriptions.
     + TODOs in comment fields.
     + Json is generated from yaml.
    + Validate exported instance against the schema.
     + Include format checking.
     + Update CSIT requirements for validation dependencies.
    + Add utility module for converting to and from in RFC 3339 format.
     + Use dateutil, as it is already a dependency of jsonschema.
    + Compute bandwidth also for soak tests.
     + This unifies with NDRPDR to simplify schema definition.
     - PAL may need an update for parsing soak test message.
    + Generate all outputs in a single filesystem tree.
     + Move raw outputs to a compressed tar.
     + Rename existing tar with denerated suites to suites.tar.xz
    
    Change-Id: I82fe6c830d1f878e5e942c4170c0735220d7c269
    Signed-off-by: Vratko Polak <vrpolak@cisco.com>

---
+++ resources/libraries/python/model/ExportResult.py
@@ -0,0 +1,158 @@
+# Copyright (c) 2021 Cisco and/or its affiliates.
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at:
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Module with keywords that publish parts of result structure."""
+
+from resources.libraries.python.model.util import descend, get_export_data
+
+
+def export_dpdk_version(version):
+    """Export the argument as sut_version.
+
+    Sets suit_type to "DPDK".
+
+    :param version: DPDK version as seen on DUT1 disk.
+    :type version: str
+    """
+    data = get_export_data()
+    data[u"sut_type"] = u"DPDK"
+    data[u"sut_version"] = str(version)
+
+
+def export_vpp_version(version):
+    """Export the argument as sut_version.
+
+    Sets suit_type to "VPP".
+
+    :param version: VPP version as returned by PAPI.
+    :type version: str
+    """
+    data = get_export_data()
+    data[u"sut_type"] = u"VPP"
+    data[u"sut_version"] = str(version)
+
+
+def append_mrr_value(mrr_value, unit):
+    """Store mrr value to proper place so it is dumped into json.
+
+    The value is appended only when unit is not empty.
+
+    :param mrr_value: Forwarding rate from MRR trial.
+    :param unit: Unit of measurement for the rate.
+    :type mrr_value: float
+    :type unit: str
+    """
+    if not unit:
+        return
+    data = get_export_data()
+    mrr_node = descend(data[u"results"], u"mrr")
+    samples_node = descend(mrr_node, u"samples")
+    rate_node = descend(samples_node, u"rate")
+    rate_node[u"unit"] = str(unit)
+    values_list = descend(rate_node, u"values", list)
+    values_list.append(float(mrr_value))
+    # TODO: Fill in the bandwidth part for pps.
+
+
+def export_search_bound(text, value, unit, bandwidth=None):
+    """Store bound value and unit.
+
+    This function works for both NDRPDR and SOAK, decided by text.
+
+    If a node does not exist, it is created.
+    If a previous value exists, it is overwritten silently.
+    Test type is set (overwritten) to ndrpdr (or soak), info only.
+
+    Text is used to determine whether it is ndr or pdr, upper or lower bound,
+    as the Robot caller has the information only there.
+
+    :param text: Info from Robot caller to determime bound type.
+    :param value: The bound value in packets (or connections) per second.
+    :param unit: Rate unit the bound is measured (or estimated) in.
+    :param bandwidth: The same value recomputed into L1 gigabits per second.
+    :type text: str
+    :type value: float
+    :type unit: str
+    :type bandwidth: Optional[float]
+    """
+    value = float(value)
+    text = str(text).lower()
+    test_type = u"soak" if u"plrsearch" in text else u"ndrpdr"
+    upper_or_lower = u"upper" if u"upper" in text else u"lower"
+    ndr_or_pdr = u"ndr" if u"ndr" in text else u"pdr"
+
+    data = get_export_data()
+    type_node = descend(data[u"results"], test_type)
+    rate_item = dict(rate=dict(value=value, unit=unit))
+    if bandwidth:
+        rate_item[u"bandwidth"] = dict(value=float(bandwidth), unit=u"Gbps")
+    if test_type == u"soak":
+        type_node[upper_or_lower] = rate_item
+        return
+    descend(type_node, ndr_or_pdr)[upper_or_lower] = rate_item
+
+
+def _add_latency(ndrpdr_node, percent, whichward, latency_string):
+    """Descend to a corresponding node and add values from latency string.
+
+    This is an internal block, moved out from export_ndrpdr_latency,
+    as it can be called up to 4 times.
+
+    :param ndrpdr_node: UTI tree node to descend from.
+    :param percent: Percent value to use in node key (90, 50, 10, 0).
+    :param whichward: "forward" or "reverse".
+    :param latency_item: Unidir output from TRex utility, min/avg/max/hdrh.
+    :type ndrpdr_node: dict
+    :type percent: int
+    :type whichward: str
+    :latency_string: str
+    """
+    l_min, l_avg, l_max, l_hdrh = latency_string.split(u"/", 3)
+    whichward_node = descend(ndrpdr_node, f"latency_{whichward}")
+    percent_node = descend(whichward_node, f"pdr_{percent}")
+    percent_node[u"min"] = int(l_min)
+    percent_node[u"avg"] = int(l_avg)
+    percent_node[u"max"] = int(l_max)
+    percent_node[u"hdrh"] = l_hdrh
+
+
+def export_ndrpdr_latency(text, latency):
+    """Store NDRPDR hdrh latency data.
+
+    If "latency" node does not exist, it is created.
+    If a previous value exists, it is overwritten silently.
+
+    Text is used to determine what percentage of PDR is the load,
+    as the Robot caller has the information only there.
+
+    Reverse data may be missing, we assume the test was unidirectional.
+
+    :param text: Info from Robot caller to determime load.
+    :param latency: Output from TRex utility, min/avg/max/hdrh.
+    :type text: str
+    :type latency: 1-tuple or 2-tuple of str
+    """
+    data = get_export_data()
+    ndrpdr_node = descend(data[u"results"], u"ndrpdr")
+    percent = 0
+    if u"90" in text:
+        percent = 90
+    elif u"50" in text:
+        percent = 50
+    elif u"10" in text:
+        percent = 10
+    _add_latency(ndrpdr_node, percent, u"forward", latency[0])
+    # Else TRex does not support latency measurement for this traffic profile.
+    if len(latency) < 2:
+        return
+    _add_latency(ndrpdr_node, percent, u"reverse", latency[1])
