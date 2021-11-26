b833f042f32ee9585263ee39787a06ef238fd6f0
Juraj Linkeš <juraj.linkes@pantheon.tech>
2021-11-26 11:03:05 +0100

    fix(IPsec): policy inbound/outbound ranges
    Replace the hardcoded values with values derived from test inputs.
    Also add show ipsec spd to teardown of tests where SPD configuration
    is important.
    
    Change-Id: I7d89a99fcf457a701c87bf6ac07364b62802677d
    Signed-off-by: Juraj Linkeš <juraj.linkes@pantheon.tech>

--- resources/libraries/python/IPsecUtil.py
+++ resources/libraries/python/IPsecUtil.py
@@ -2136,6 +2136,8 @@ class IPsecUtil:
         sa_id_2 = 200000
         spi_1 = 300000
         spi_2 = 400000
+        dut1_local_inbound_range = u"0.0.0.0/0"
+        dut1_remote_inbound_range = u"0.0.0.0/0"
 
         crypto_key = gen_key(
             IPsecUtil.get_crypto_alg_key_len(crypto_alg)
@@ -2155,11 +2157,13 @@ class IPsecUtil:
         IPsecUtil.vpp_ipsec_spd_add_if(nodes[u"DUT1"], spd_id, interface1)
         IPsecUtil.vpp_ipsec_add_spd_entry(
             nodes[u"DUT1"], spd_id, p_hi, PolicyAction.BYPASS, inbound=False,
-            proto=50, laddr_range=u"100.0.0.0/8", raddr_range=u"100.0.0.0/8"
+            proto=50, laddr_range=dut1_remote_inbound_range,
+            raddr_range=dut1_local_inbound_range
         )
         IPsecUtil.vpp_ipsec_add_spd_entry(
             nodes[u"DUT1"], spd_id, p_hi, PolicyAction.BYPASS, inbound=True,
-            proto=50, laddr_range=u"100.0.0.0/8", raddr_range=u"100.0.0.0/8"
+            proto=50, laddr_range=dut1_remote_inbound_range,
+            raddr_range=dut1_local_inbound_range
         )
 
         IPsecUtil.vpp_ipsec_add_sad_entries(
@@ -2194,13 +2198,13 @@ class IPsecUtil:
             IPsecUtil.vpp_ipsec_spd_add_if(nodes[u"DUT2"], spd_id, interface2)
             IPsecUtil.vpp_ipsec_add_spd_entry(
                 nodes[u"DUT2"], spd_id, p_hi, PolicyAction.BYPASS,
-                inbound=False, proto=50, laddr_range=u"100.0.0.0/8",
-                raddr_range=u"100.0.0.0/8"
+                inbound=False, proto=50, laddr_range=dut1_local_inbound_range,
+                raddr_range=dut1_remote_inbound_range
             )
             IPsecUtil.vpp_ipsec_add_spd_entry(
                 nodes[u"DUT2"], spd_id, p_hi, PolicyAction.BYPASS,
-                inbound=True, proto=50, laddr_range=u"100.0.0.0/8",
-                raddr_range=u"100.0.0.0/8"
+                inbound=True, proto=50, laddr_range=dut1_remote_inbound_range,
+                raddr_range=dut1_local_inbound_range
             )
 
             IPsecUtil.vpp_ipsec_add_sad_entries(
@@ -2235,6 +2239,15 @@ class IPsecUtil:
         :type node: dict
         """
         PapiSocketExecutor.run_cli_cmd(node, u"show ipsec all")
+
+    @staticmethod
+    def vpp_ipsec_show_spd(node):
+        """Run "show ipsec spd" debug CLI command.
+
+        :param node: Node to run command on.
+        :type node: dict
+        """
+        PapiSocketExecutor.run_cli_cmd(node, u"show ipsec spd")
 
     @staticmethod
     def show_ipsec_security_association(node):
