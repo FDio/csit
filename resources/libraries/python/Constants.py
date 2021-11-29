cca05a55f3434d8a031b98f4a496adb8df20c122
Vratko Polak <vrpolak@cisco.com>
2021-11-29 16:17:15 +0100

    Tox: Add checker for CSIT model version
    + Implementation stub so checker can check already.
    + Also add documentation stub for the implemented model.
    + Checker checks also for bumps in documentation version.
    - Not comparing implementation and documentation version yet.
    
    Change-Id: I4d19c00315a1c171de325c4494c28f5210635f32
    Signed-off-by: Vratko Polak <vrpolak@cisco.com>

--- resources/libraries/python/Constants.py
+++ resources/libraries/python/Constants.py
@@ -119,6 +119,9 @@ def get_optimistic_bool_from_env(env_var_names):
 class Constants:
     """Constants used in CSIT."""
 
+    # Version for CSIT data model. See docs/model/.
+    MODEL_VERSION = u"0.1.0"
+
     # OpenVPP testing directory location at topology nodes
     REMOTE_FW_DIR = u"/tmp/openvpp-testing"
 
