673ee8317c8d4359094d650b70674c4a06f9e44b
pmikus <pmikus@cisco.com>
2021-12-22 13:44:51 +0100

    feat(uti): etl
    Signed-off-by: pmikus <pmikus@cisco.com>
    Change-Id: I0c259e72e658ca2ff1a6b19b5045c6a7e4058dd2

---
+++ fdio.infra.terraform/1n_nmd/etl/fdio/main.tf
@@ -0,0 +1,15 @@
+data "vault_generic_secret" "creds" {
+  path = "kv/secret/data/etl"
+}
+
+module "etl" {
+  providers = {
+    nomad = nomad.yul1
+  }
+  source = "../"
+
+  access_key  = data.vault_generic_secret.creds.data["access_key"]
+  cron        = "@daily"
+  datacenters = ["yul1"]
+  secret_key  = data.vault_generic_secret.creds.data["secret_key"]
+}
