03a4cde78b36bb5600d53cabcb9f379bfda7de24
pmikus <pmikus@cisco.com>
2021-12-22 13:44:51 +0100

    feat(uti): etl
    Signed-off-by: pmikus <pmikus@cisco.com>
    Change-Id: I0c259e72e658ca2ff1a6b19b5045c6a7e4058dd2

---
+++ fdio.infra.terraform/1n_nmd/etl/fdio/versions.tf
@@ -0,0 +1,17 @@
+terraform {
+  backend "consul" {
+    address = "10.32.8.14:8500"
+    scheme  = "http"
+    path    = "terraform/etl"
+  }
+  required_providers {
+    nomad = {
+      source  = "hashicorp/nomad"
+      version = ">= 1.4.16"
+    }
+    vault = {
+      version = ">= 3.2.1"
+    }
+  }
+  required_version = ">= 1.1.4"
+}
