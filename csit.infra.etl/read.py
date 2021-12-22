a26befff66b52517a2fd2be132a550c286a712b3
pmikus <pmikus@cisco.com>
2021-12-22 13:44:51 +0100

    feat(uti): etl
    Signed-off-by: pmikus <pmikus@cisco.com>
    Change-Id: I0c259e72e658ca2ff1a6b19b5045c6a7e4058dd2

---
+++ csit.infra.etl/read.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+
+# Copyright (c) 2022 Cisco and/or its affiliates.
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at:
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""ETL script running on top of the s3://"""
+
+from time import time
+
+import awswrangler as wr
+from awswrangler.exceptions import EmptyDataFrame
+from boto3 import session
+
+
+S3_DOCS_BUCKET="fdio-docs-s3-cloudfront-index"
+
+partition_filter = lambda part: True if part["test_type"] == "mrr" else False
+
+start = time()
+df = wr.s3.read_parquet(
+    path=f"s3://{S3_DOCS_BUCKET}/csit/sandbox/parquet/trending",
+    path_suffix="parquet",
+    ignore_empty=True,
+    use_threads=True,
+    dataset=True,
+    partition_filter=partition_filter
+)
+end = time()
+print(df.to_string())
+print(end - start)
+
+print(
+    wr.s3.size_objects(
+        path=f"s3://{S3_DOCS_BUCKET}/csit/sandbox/parquet/trending"
+    )
+)
