fe3b5b9d196a29ecd1e0bd870b18f30fe81d1627
Peter Mikus <pmikus@cisco.com>
2022-01-10 13:19:31 +0100

    feat(uti): dash
    Signed-off-by: Peter Mikus <pmikus@cisco.com>
    Change-Id: Ic0d48290105ccd846c7de9ee4d8acb78e0b72f00

---
+++ resources/tools/dash/app/pal/__init__.py
@@ -0,0 +1,39 @@
+# Copyright (c) 2021 Cisco and/or its affiliates.
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at:
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Initialize Flask app."""
+from flask import Flask
+from flask_assets import Environment
+
+
+def init_app():
+    """Construct core Flask application with embedded Dash app."""
+    app = Flask(__name__, instance_relative_config=False)
+    app.config.from_object("config.Config")
+    assets = Environment()
+    assets.init_app(app)
+
+    with app.app_context():
+        # Import parts of our core Flask app.
+        from . import routes
+        from .assets import compile_static_assets
+
+        # Import Trending Dash application.
+        from .trending.dashboard import init_dashboard
+
+        app = init_dashboard(app)
+
+        # Compile static assets.
+        compile_static_assets(assets)
+
+        return app
