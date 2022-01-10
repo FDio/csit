b129a32c7e431e6c42c9bb6eda64daf7fc5bddf4
Peter Mikus <pmikus@cisco.com>
2022-01-10 13:19:31 +0100

    feat(uti): Dash demo
    - displays the table with trending data downloaded from trending pages
    
    Signed-off-by: Peter Mikus <pmikus@cisco.com>
    Change-Id: Ic0d48290105ccd846c7de9ee4d8acb78e0b72f00

---
+++ resources/tools/dash/app/config.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+
+# Copyright (c) 2022 Cisco and/or its affiliates.
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at:
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from os import environ
+
+
+class Config:
+    """Flask configuration variables."""
+
+    # General Config
+    FLASK_APP = environ.get("FLASK_APP")
+    FLASK_ENV = environ.get("FLASK_ENV")
+    SECRET_KEY = environ.get("SECRET_KEY")
+
+    # Assets
+    LESS_BIN = environ.get("LESS_BIN")
+    ASSETS_DEBUG = environ.get("ASSETS_DEBUG")
+    LESS_RUN_IN_DEBUG = environ.get("LESS_RUN_IN_DEBUG")
+
+    # Static Assets
+    STATIC_FOLDER = "static"
+    TEMPLATES_FOLDER = "templates"
+    COMPRESSOR_DEBUG = environ.get("COMPRESSOR_DEBUG")
