02b36a81ae9bbfab1044b6f9b4dceef968fafe0c
Vratko Polak <vrpolak@cisco.com>
2022-01-18 15:03:18 +0100

    Experiment: Investigating 1n-tx2 MAC mismatch
    Hoping to fix by aviding unbind+bind when already iavf.
    
    Change-Id: Idb650cdf906c65e9631766326a4e391bf7006632
    Signed-off-by: Vratko Polak <vrpolak@cisco.com>

--- resources/libraries/bash/function/device.sh
+++ resources/libraries/bash/function/device.sh
@@ -129,6 +129,7 @@ function bind_interfaces_to_driver () {
     pci_path="/sys/bus/pci/devices/${ADDR}"
     drv_path="/sys/bus/pci/drivers/${DRIVER}"
     if [ -d "${pci_path}/driver" ]; then
+        ls -lAn "${pci_path}/driver"
         echo ${ADDR} | sudo tee ${pci_path}/driver/unbind > /dev/null || {
             die "Failed to unbind interface ${ADDR}!"
         }
