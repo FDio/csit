e2279a4ad5cf78851b7ff5913bb40e3a3ff5dfb4
Peter Mikus <pmikus@cisco.com>
2022-02-04 09:56:00 +0100

    feat(terraform): Refactor vault backend
    Signed-off-by: Peter Mikus <pmikus@cisco.com>
    Change-Id: I914ecf444b5f8870969f1d996ba03a42fd92a5d5

--- fdio.infra.terraform/1n_nmd/vault-aws-secret-backend/versions.tf
+++ fdio.infra.terraform/1n_nmd/vault-aws-secret-backend/versions.tf
@@ -1,13 +1,8 @@
 terraform {
-  backend "consul" {
-    address = "consul.service.consul:8500"
-    scheme  = "http"
-    path    = "fdio/terraform/1n/nomad"
-  }
   required_providers {
     vault = {
       version = ">=2.22.1"
     }
   }
-  required_version = ">= 1.0.3"
+  required_version = ">= 1.1.4"
 }
