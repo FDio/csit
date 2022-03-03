ebc93e0b847f2b500f4de591a0601077c65c4445
Tibor Frank <tifrank@cisco.com>
2022-03-03 14:38:47 +0100

    UTI: PoC - Dash application for Trending
    Change-Id: I0b7b67cdf928aec6a668b3e151e84ecfd600f959
    Signed-off-by: Tibor Frank <tifrank@cisco.com>

--- resources/tools/dash/app/pal/trending/layout.py
+++ resources/tools/dash/app/pal/trending/layout.py
@@ -14,336 +14,363 @@
 """Plotly Dash HTML layout override.
 """
 
+import time
+from copy import deepcopy
 import logging
 
 from dash import dcc
 from dash import html
-from dash import Input, Output, callback
+from dash import callback_context
+from dash import Input, Output, State, callback
 from dash.exceptions import PreventUpdate
 from yaml import load, FullLoader, YAMLError
 
+from pprint import pformat
 
-class Layout:
+
+spec_file="pal/trending/spec_test_selection.yaml"
+spec_tbs = None
+
+try:
+    with open(spec_file, "r") as file_read:
+        spec_tbs = load(file_read, Loader=FullLoader)
+except IOError as err:
+    logging.error(f"Not possible to open the file {spec_file}\n{err}")
+except YAMLError as err:
+    logging.error(
+        f"An error occurred while parsing the specification file "
+        f"{spec_file}\n"
+        f"{err}"
+    )
+
+
+# State of controls:
+def _reset_ctrls():
+    logging.info("### _reset_ctrls")
+    ctrls = dict(
+        # info_val=u"",
+        phy_val=None,
+        # area_opts=list(),
+        area_val=None,
+        # area_dis=True,
+        # test_opts=list(),
+        test_val=None,
+        # test_dis=True,
+        btn_val=None,
+        # btn_dis=True,
+    )
+    return ctrls
+
+_ctrls = _reset_ctrls()
+
+def _set_ctrls(key, val):
+    global _ctrls
+    try:
+        _ctrls[key] = val
+    except KeyError as err:
+        logging.error(err)
+        raise RuntimeError
+
+
+def create_layout(html_layout_file):
     """
     """
+    try:
+        with open(html_layout_file, "r") as layout_file:
+            return layout_file.read()
+    except IOError as err:
+        logging.error(f"Not possible to open the file {layout_file}\n{err}")
 
-    def __init__(self, app, html_layout_file, spec_file):
-        """
-        """
 
-        # Inputs
-        self._app = app
-        self._html_layout_file = html_layout_file
-        self._spec_file = spec_file
-
-        # Read from files:
-        self._html_layout = ""
-        self._spec_test = None
-
-        try:
-            with open(self._html_layout_file, "r") as layout_file:
-                self._html_layout = layout_file.read()
-        except IOError as err:
-            logging.error(f"Not possible to open the file {layout_file}\n{err}")
-
-        try:
-            with open(self._spec_file, "r") as file_read:
-                self._spec_test = load(file_read, Loader=FullLoader)
-        except IOError as err:
-            logging.error(f"Not possible to open the file {spec_file}\n{err}")
-        except YAMLError as err:
-            logging.error(
-                f"An error occurred while parsing the specification file "
-                f"{spec_file}\n"
-                f"{err}"
-            )
-
-        # Callbacks:
-        if self._app is not None and hasattr(self, 'callbacks'):
-            self.callbacks(self._app)
-
-        # User choice (one test):
-        self._test_selection = {
-            "phy": "",
-            "area": "",
-            "test": "",
-            "core": "",
-            "frame-size": "",
-            "test-type": ""
-        }
-
-    @property
-    def html_layout(self):
-        return self._html_layout
-
-    @property
-    def spec_test(self):
-        return self._spec_test
-
-    def _reset_test_selection(self):
-        self._test_selection = {
-            "phy": "",
-            "area": "",
-            "test": "",
-            "core": "",
-            "frame-size": "",
-            "test-type": ""
-        }
-
-    def add_content(self):
-        """
-        """
-        if self._html_layout and self._spec_test:
-            return html.Div(
-                id="div-main",
-                children=[
-                    self._add_ctrl_div(),
-                    self._add_plotting_div()
-                ]
-            )
-        else:
-            return html.Div(
-            id="div-main-error",
-            children="An Error Occured."
-        )
-
-    def _add_ctrl_div(self):
-        """Add div with controls. It is placed on the left side.
-        """
+def add_content():
+    """
+    """
+    if spec_tbs:
         return html.Div(
-            id="div-controls",
+            id="div-main",
             children=[
-                html.Div(
-                    id="div-controls-tabs",
-                    children=[
-                        self._add_ctrl_select(),
-                        self._add_ctrl_shown()
-                    ]
-                )
-            ],
-            style={
-                "display": "inline-block",
-                "width": "18%",
-                "padding": "5px"
-            }
-        )
-
-    def _add_plotting_div(self):
-        """Add div with plots and tables. It is placed on the right side.
-        """
-        return html.Div(
-            id="div-plotting-area",
-            children=[
-                # Only a visible note.
-                # TODO: Add content.
-                html.H3(
-                    "Graphs and Tables",
-                    style={
-                        "vertical-align": "middle",
-                        "text-align": "center"
-                    }
-                )
-            ],
-            style={
-                "vertical-align": "middle",
-                "display": "inline-block",
-                "width": "80%",
-                "padding": "5px"
-            }
-        )
-
-    def _add_ctrl_shown(self):
-        """
-        """
-        return html.Div(
-            id="div-ctrl-shown",
-            children="List of selected tests"
-        )
-
-    def _add_ctrl_select(self):
-        """
-        """
-        return html.Div(
-            id="div-ctrl-select",
-            children=[
-                html.Br(),
-                html.Div(
-                    children="Physical Test Bed Topology, NIC and Driver"
-                ),
-                dcc.Dropdown(
-                    id="dd-ctrl-phy",
-                    placeholder="Select a Physical Test Bed Topology...",
-                    multi=False,
-                    clearable=False,
-                    options=[
-                        {"label": k, "value": k} for k in self._spec_test.keys()
-                    ],
-                ),
-                html.Br(),
-                html.Div(
-                    children="Area"
-                ),
-                dcc.Dropdown(
-                    id="dd-ctrl-area",
-                    placeholder="Select an Area...",
-                    disabled=True,
-                    multi=False,
-                    clearable=False,
-                ),
-                html.Br(),
-                html.Div(
-                    children="Test"
-                ),
-                dcc.Dropdown(
-                    id="dd-ctrl-test",
-                    placeholder="Select a Test...",
-                    disabled=True,
-                    multi=False,
-                    clearable=False,
-                ),
-
-                # Change to radio buttons:
-                html.Br(),
-                html.Div(
-                    children="Number of Cores"
-                ),
-                dcc.Dropdown(
-                    id="dd-ctrl-core",
-                    placeholder="Select a Number of Cores...",
-                    disabled=True,
-                    multi=False,
-                    clearable=False,
-                ),
-                html.Br(),
-                html.Div(
-                    children="Frame Size"
-                ),
-                dcc.Dropdown(
-                    id="dd-ctrl-framesize",
-                    placeholder="Select a Frame Size...",
-                    disabled=True,
-                    multi=False,
-                    clearable=False,
-                ),
-                html.Br(),
-                html.Div(
-                    children="Test Type"
-                ),
-                dcc.Dropdown(
-                    id="dd-ctrl-testtype",
-                    placeholder="Select a Test Type...",
-                    disabled=True,
-                    multi=False,
-                    clearable=False,
-                ),
-                            html.Br(),
-                html.Button(
-                    id="btn-ctrl-add",
-                    children="Add",
-                    disabled=True
-                ),
-                html.Br(),
-                html.Div(
-                    id="div-ctrl-info"
-                )
+                _add_ctrl_div(),
+                _add_plotting_div()
             ]
         )
+    else:
+        return html.Div(
+        id="div-main-error",
+        children="An Error Occured."
+    )
 
-    def callbacks(self, app):
 
-        @app.callback(
-            Output("dd-ctrl-area", "options"),
-            Output("dd-ctrl-area", "disabled"),
-            Input("dd-ctrl-phy", "value"),
-        )
-        def _update_dd_area(phy):
-            """
-            """
-
-            if phy is None:
-                raise PreventUpdate
-
-            try:
-                options = [
-                    {"label": self._spec_test[phy][v]["label"], "value": v}
-                        for v in [v for v in self._spec_test[phy].keys()]
+def _add_ctrl_div():
+    """Add div with controls. It is placed on the left side.
+    """
+    return html.Div(
+        id="div-controls",
+        children=[
+            html.Div(
+                id="div-controls-tabs",
+                children=[
+                    _add_ctrl_select(),
+                    _add_ctrl_shown()
                 ]
-            except KeyError:
-                options = list()
-
-            return options, False
-
-        @app.callback(
-            Output("dd-ctrl-test", "options"),
-            Output("dd-ctrl-test", "disabled"),
-            Input("dd-ctrl-phy", "value"),
-            Input("dd-ctrl-area", "value"),
-        )
-        def _update_dd_test(phy, area):
-            """
-            """
-
-            if not all((phy, area, )):
-                raise PreventUpdate
-
-            try:
-                options = [
-                    {"label": v, "value": v}
-                        for v in self._spec_test[phy][area]["test"]
-                ]
-            except KeyError:
-                options = list()
-
-            return options, False
-
-        @app.callback(
-            Output("btn-ctrl-add", "disabled"),
-            Input("dd-ctrl-phy", "value"),
-            Input("dd-ctrl-area", "value"),
-            Input("dd-ctrl-test", "value"),
-        )
-        def _update_btn_add(phy, area, test):
-            """
-            """
-
-            if all((phy, area, test, )):
-                self._test_selection["phy"] = phy
-                self._test_selection["area"] = area
-                self._test_selection["test"] = test
-                return False
-            else:
-                return True
-
-        @app.callback(
-            Output("div-ctrl-info", "children"),
-            Output("dd-ctrl-phy", "value"),
-            Output("dd-ctrl-area", "value"),
-            Output("dd-ctrl-test", "value"),
-            Output("btn-ctrl-add", "n_clicks"),
-            Input("btn-ctrl-add", "n_clicks")
-        )
-        def _print_user_selection(n_clicks):
-            """
-            """
-
-            logging.info(f"\n\n{n_clicks}\n\n")
-
-            if not n_clicks:
-                raise PreventUpdate
-
-            selected = (
-                f"{self._test_selection['phy']} # "
-                f"{self._test_selection['area']} # "
-                f"{self._test_selection['test']} # "
-                f"{n_clicks}\n"
             )
+        ],
+        style={
+            "display": "inline-block",
+            "width": "18%",
+            "padding": "5px"
+        }
+    )
 
-            self._reset_test_selection()
 
-            return (
-                selected,
-                None,
-                None,
-                None,
-                0,
+def _add_plotting_div():
+    """Add div with plots and tables. It is placed on the right side.
+    """
+    return html.Div(
+        id="div-plotting-area",
+        children=[
+            # Only a visible note.
+            # TODO: Add content.
+            html.H3(
+                "Graphs and Tables",
+                style={
+                    "vertical-align": "middle",
+                    "text-align": "center"
+                }
             )
+        ],
+        style={
+            "vertical-align": "middle",
+            "display": "inline-block",
+            "width": "80%",
+            "padding": "5px"
+        }
+    )
+
+
+def _add_ctrl_shown():
+    """
+    """
+    return html.Div(
+        id="div-ctrl-shown",
+        children="List of selected tests"
+    )
+
+
+def _add_ctrl_select():
+    """
+    """
+    return html.Div(
+        id="div-ctrl-select",
+        children=[
+            html.Br(),
+            html.Div(
+                children="Physical Test Bed Topology, NIC and Driver"
+            ),
+            dcc.Dropdown(
+                id="dd-ctrl-phy",
+                placeholder="Select a Physical Test Bed Topology...",
+                multi=False,
+                clearable=False,
+                options=[
+                    {"label": k, "value": k} for k in spec_tbs.keys()
+                ],
+                # value=list(spec_tbs.keys())[0]
+            ),
+            html.Br(),
+            html.Div(
+                children="Area"
+            ),
+            dcc.Dropdown(
+                id="dd-ctrl-area",
+                placeholder="Select an Area...",
+                disabled=True,
+                multi=False,
+                clearable=False,
+            ),
+            html.Br(),
+            html.Div(
+                children="Test"
+            ),
+            dcc.Dropdown(
+                id="dd-ctrl-test",
+                placeholder="Select a Test...",
+                disabled=True,
+                multi=False,
+                clearable=False,
+            ),
+
+            # # Change to radio buttons:
+            # html.Br(),
+            # html.Div(
+            #     children="Number of Cores"
+            # ),
+            # dcc.Dropdown(
+            #     id="dd-ctrl-core",
+            #     placeholder="Select a Number of Cores...",
+            #     disabled=True,
+            #     multi=False,
+            #     clearable=False,
+            # ),
+            # html.Br(),
+            # html.Div(
+            #     children="Frame Size"
+            # ),
+            # dcc.Dropdown(
+            #     id="dd-ctrl-framesize",
+            #     placeholder="Select a Frame Size...",
+            #     disabled=True,
+            #     multi=False,
+            #     clearable=False,
+            # ),
+            # html.Br(),
+            # html.Div(
+            #     children="Test Type"
+            # ),
+            # dcc.Dropdown(
+            #     id="dd-ctrl-testtype",
+            #     placeholder="Select a Test Type...",
+            #     disabled=True,
+            #     multi=False,
+            #     clearable=False,
+            # ),
+            html.Br(),
+            html.Button(
+                id="btn-ctrl-add",
+                children="Add",
+                disabled=True
+            ),
+            html.Br(),
+            html.Div(
+                id="div-ctrl-info"
+            )
+        ]
+    )
+
+
+@callback(
+    Output("dd-ctrl-area", "options"),
+    Output("dd-ctrl-area", "disabled"),
+    Input("dd-ctrl-phy", "value"),
+)
+def _update_dd_area(phy):
+    """
+    """
+
+    logging.info("_update_dd_area")
+
+    if phy is None:
+        logging.info("_update_dd_area: PreventUpdate")
+        raise PreventUpdate
+
+    # _set_ctrls("phy_val", phy)
+
+    try:
+        options = [
+            {"label": spec_tbs[phy][v]["label"], "value": v}
+                for v in [v for v in spec_tbs[phy].keys()]
+        ]
+        disable = False
+        logging.info("_update_dd_area: options")
+        # logging.info(pformat(options))
+    except KeyError:
+        options = list()
+        disable = True
+
+    # _set_ctrls("area_opts", options)
+    # _set_ctrls("area_dis", disable)
+
+    logging.info("_update_dd_area: return")
+    # logging.info(pformat(_ctrls))
+
+    return options, disable
+
+
+@callback(
+    Output("dd-ctrl-test", "options"),
+    Output("dd-ctrl-test", "disabled"),
+    State("dd-ctrl-phy", "value"),
+    Input("dd-ctrl-area", "value"),
+)
+def _update_dd_test(phy, area):
+    """
+    """
+
+    if not area:
+        logging.info("_update_dd_test: PreventUpdate")
+        raise PreventUpdate
+
+    try:
+        options = [
+            {"label": v, "value": v}
+                for v in spec_tbs[phy][area]["test"]
+        ]
+        disable = False
+    except KeyError:
+        options = list()
+        disable = True
+
+    return options, disable
+
+
+@callback(
+    Output("btn-ctrl-add", "disabled"),
+    Input("dd-ctrl-test", "value"),
+)
+def _update_btn_add(test):
+    """
+    """
+
+    logging.info("_update_btn_add")
+
+    if test is None:
+        logging.info("_update_btn_add: PreventUpdate")
+        raise PreventUpdate
+
+    if test:
+
+        # TODO: Add validation
+
+        # _set_ctrls("btn_dis", False)
+        logging.info("_update_btn_add: return OK")
+        return False
+    else:
+        logging.info("_update_btn_add: return BAD")
+        return True
+
+
+@callback(
+    Output("div-ctrl-info", "children"),
+    Output("dd-ctrl-phy", "value"),
+    Output("dd-ctrl-area", "value"),
+    Output("dd-ctrl-test", "value"),
+    Output("btn-ctrl-add", "n_clicks"),
+    State("dd-ctrl-phy", "value"),
+    State("dd-ctrl-area", "value"),
+    State("dd-ctrl-test", "value"),
+    Input("btn-ctrl-add", "n_clicks"),
+)
+def _print_user_selection(phy, area, test, n_clicks):
+    """
+    """
+
+    logging.info("_print_user_selection")
+    logging.info(n_clicks)
+
+    if not n_clicks:
+        logging.info("_print_user_selection: PreventUpdate")
+        raise PreventUpdate
+
+    _set_ctrls("phy_val", phy)
+    _set_ctrls("area_val", area)
+    _set_ctrls("test_val", test)
+
+    selected = (
+        f"{_ctrls['phy_val']} - "
+        f"{_ctrls['area_val']} - "
+        f"{_ctrls['test_val']}\n\n"
+    )
+    logging.info(selected)
+
+    _reset_ctrls()
+
+    return selected, None, None, None, 0
--- resources/tools/dash/app/pal/trending/trending.py
+++ resources/tools/dash/app/pal/trending/trending.py
@@ -14,14 +14,8 @@
 """Instantiate the Trending Dash applocation.
 """
 import dash
-from dash import dcc
-from dash import html
-from dash import dash_table
-import numpy as np
-import pandas as pd
 
-from .data import create_dataframe
-from .layout import Layout
+from .layout import create_layout, add_content
 
 
 def init_trending(server):
@@ -42,33 +36,10 @@ def init_trending(server):
         ],
     )
 
-    # Load DataFrame
-    df = create_dataframe()
-
     # Custom HTML layout
-    layout = Layout(
-        app=dash_app,
-        html_layout_file="pal/trending/html_layout.txt",
-        spec_file="pal/trending/spec_test_selection.yaml"
+    dash_app.index_string = create_layout(
+        html_layout_file="pal/trending/html_layout.txt"
     )
-    dash_app.index_string = layout.html_layout
-    dash_app.layout = layout.add_content()
+    dash_app.layout = add_content()
 
     return dash_app.server
-
-
-def create_data_table(df):
-    """Create Dash datatable from Pandas DataFrame.
-
-    DEMO
-    """
-
-    table = dash_table.DataTable(
-        id=u"database-table",
-        columns=[{u"name": i, u"id": i} for i in df.columns],
-        data=df.to_dict(u"records"),
-        sort_action=u"native",
-        sort_mode=u"native",
-        page_size=10,
-    )
-    return table
