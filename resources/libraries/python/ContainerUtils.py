1f4addab899aad814c2a88b040dce8a050ecb19c
Filip Tehlar <ftehlar@cisco.com>
2022-03-04 11:56:16 +0000

    [WIP] Tune host stack performance tests
    * remove evt_qs_memfd_seg and evt_qs_seg_size as they are obsolete
    * use app socket api instead of vpp's api
    
    Signed-off-by: Filip Tehlar <ftehlar@cisco.com>
    Change-Id: Ie7ac12b8f587e0b1962884127e8eb68a4e588aee

--- resources/libraries/python/ContainerUtils.py
+++ resources/libraries/python/ContainerUtils.py
@@ -697,7 +697,7 @@ class ContainerEngine:
             # If more cores in the list, the rest will be used as workers.
             corelist_workers = u",".join(str(cpu) for cpu in cpuset_cpus)
             vpp_config.add_cpu_corelist_workers(corelist_workers)
-        vpp_config.add_buffers_per_numa(215040)
+        vpp_config.add_buffers_per_numa(16384)
         vpp_config.add_plugin(u"disable", u"default")
         vpp_config.add_plugin(u"enable", u"memif_plugin.so")
         vpp_config.add_plugin(u"enable", u"perfmon_plugin.so")
@@ -737,6 +737,8 @@ class ContainerEngine:
         vpp_config.add_dpdk_log_level(u"debug")
         vpp_config.add_dpdk_no_tx_checksum_offload()
         vpp_config.add_dpdk_dev_default_rxq(rxq)
+        vpp_config.add_dpdk_dev_default_rxd(256)
+        vpp_config.add_dpdk_dev_default_txd(256)
         vpp_config.add_plugin(u"enable", u"dpdk_plugin.so")
         vpp_config.add_plugin(u"enable", u"perfmon_plugin.so")
 
