FDio/csit

commit 14a71b74b414df7616ccb5ada3d50ecb90d96bae

Improve pf layer
+ Merge single/double link
+ Introduce _pf{n}[0] variables so we can access physical function
  same way as virtual function
+ Cleanup code by moving complex logic to python
+ Prepare code for multiple vf functions

Signed-off-by: Peter Mikus <pmikus@cisco.com>
Change-Id: Ic2e74a38bfa146441357de8f0916aeb638941c49

resources/libraries/robot/overlay/lisp.robot
@@ -1,4 +1,4 @@
-# Copyright (c) 2019 Cisco and/or its affiliates.
+# Copyright (c) 2020 Cisco and/or its affiliates.
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at:
@@ -29,15 +29,15 @@
 | |
 | | Set interfaces in path up
 | | VPP Interface Set IP Address
-| | ... | ${dut1} | ${dut1_if1} | ${dut_if1_ip4} | ${ip4_plen}
+| | ... | ${dut1} | ${DUT1_${int}1}[0] | ${dut_if1_ip4} | ${ip4_plen}
 | | VPP Interface Set IP Address
-| | ... | ${dut1} | ${dut1_if2} | ${dut_if2_ip4} | ${ip4_plen}
+| | ... | ${dut1} | ${DUT1_${int}2}[0] | ${dut_if2_ip4} | ${ip4_plen}
 | | VPP Add IP Neighbor
-| | ... | ${dut1} | ${dut1_if1} | ${src_ip4} | ${tg_if1_mac}
+| | ... | ${dut1} | ${DUT1_${int}1}[0] | ${src_ip4} | ${TG_pf1_mac}[0]
 | | VPP Add IP Neighbor
-| | ... | ${dut1} | ${dut1_if2} | ${dst_ip4} | ${tg_if2_mac}
+| | ... | ${dut1} | ${DUT1_${int}2}[0] | ${dst_ip4} | ${TG_pf2_mac}[0]
 | | VPP Add IP Neighbor
-| | ... | ${dut1} | ${dut1_if2} | ${tg_if2_ip4} | ${tg_if2_mac}
+| | ... | ${dut1} | ${DUT1_${int}2}[0] | ${tg_if2_ip4} | ${TG_pf2_mac}[0]
 
 | Configure topology for IPv6 LISP testing
 | | [Documentation] | Setup topology fo IPv6 LISP testing.
@@ -47,15 +47,15 @@
 | |
 | | Set interfaces in path up
 | | VPP Interface Set IP Address
-| | ... | ${dut1} | ${dut1_if1} | ${dut_if1_ip6} | ${ip6_plen}
+| | ... | ${dut1} | ${DUT1_${int}1}[0] | ${dut_if1_ip6} | ${ip6_plen}
 | | VPP Interface Set IP Address
-| | ... | ${dut1} | ${dut1_if2} | ${dut_if2_ip6} | ${ip6_plen}
+| | ... | ${dut1} | ${DUT1_${int}2}[0] | ${dut_if2_ip6} | ${ip6_plen}
 | | VPP Add IP Neighbor
-| | ... | ${dut1} | ${dut1_if1} | ${src_ip6} | ${tg_if1_mac}
+| | ... | ${dut1} | ${DUT1_${int}1}[0] | ${src_ip6} | ${TG_pf1_mac}[0]
 | | VPP Add IP Neighbor
-| | ... | ${dut1} | ${dut1_if2} | ${dst_ip6} | ${tg_if2_mac}
+| | ... | ${dut1} | ${DUT1_${int}2}[0] | ${dst_ip6} | ${TG_pf2_mac}[0]
 | | VPP Add IP Neighbor
-| | ... | ${dut1} | ${dut1_if2} | ${tg_if2_ip6} | ${tg_if2_mac}
+| | ... | ${dut1} | ${DUT1_${int}2}[0] | ${tg_if2_ip6} | ${TG_pf2_mac}[0]
 
 | Configure LISP in 2-node circular topology
 | | [Documentation] | Configure LISP topology in 2-node circular topology.
@@ -124,15 +124,15 @@
 | |
 | | Set interfaces in path up
 | | VPP Interface Set IP Address
-| | ... | ${dut1} | ${dut1_if1} | ${dut_if1_ip6} | ${ip6_plen}
+| | ... | ${dut1} | ${DUT1_${int}1}[0] | ${dut_if1_ip6} | ${ip6_plen}
 | | VPP Interface Set IP Address
-| | ... | ${dut1} | ${dut1_if2} | ${dut_if2_ip4} | ${ip4_plen}
+| | ... | ${dut1} | ${DUT1_${int}2}[0] | ${dut_if2_ip4} | ${ip4_plen}
 | | VPP Add IP Neighbor
-| | ... | ${dut1} | ${dut1_if1} | ${src_ip6} | ${tg_if1_mac}
+| | ... | ${dut1} | ${DUT1_${int}1}[0] | ${src_ip6} | ${TG_pf1_mac}[0]
 | | VPP Add IP Neighbor
-| | ... | ${dut1} | ${dut1_if2} | ${dst_ip6} | ${tg_if2_mac}
+| | ... | ${dut1} | ${DUT1_${int}2}[0] | ${dst_ip6} | ${TG_pf2_mac}[0]
 | | VPP Add IP Neighbor
-| | ... | ${dut1} | ${dut1_if2} | ${tg_if2_ip4} | ${tg_if2_mac}
+| | ... | ${dut1} | ${DUT1_${int}2}[0] | ${tg_if2_ip4} | ${TG_pf2_mac}[0]
 
 | Configure topology for IPv4 LISPoIP6 testing
 | | [Documentation] | Setup topology fo IPv4 LISPoIPV6 testing.
@@ -142,12 +142,227 @@
 | |
 | | Set interfaces in path up
 | | VPP Interface Set IP Address
-| | ... | ${dut1} | ${dut1_if1} | ${dut_if1_ip4} | ${ip4_plen}
+| | ... | ${dut1} | ${DUT1_${int}1}[0] | ${dut_if1_ip4} | ${ip4_plen}
 | | VPP Interface Set IP Address
-| | ... | ${dut1} | ${dut1_if2} | ${dut_if2_ip6} | ${ip6_plen}
+| | ... | ${dut1} | ${DUT1_${int}2}[0] | ${dut_if2_ip6} | ${ip6_plen}
 | | VPP Add IP Neighbor
-| | ... | ${dut1} | ${dut1_if1} | ${src_ip4} | ${tg_if1_mac}
+| | ... | ${dut1} | ${DUT1_${int}1}[0] | ${src_ip4} | ${TG_pf1_mac}[0]
 | | VPP Add IP Neighbor
-| | ... | ${dut1} | ${dut1_if2} | ${dst_ip4} | ${tg_if2_mac}
+| | ... | ${dut1} | ${DUT1_${int}2}[0] | ${dst_ip4} | ${TG_pf2_mac}[0]
 | | VPP Add IP Neighbor
-| | ... | ${dut1} | ${dut1_if2} | ${tg_if2_ip6} | ${tg_if2_mac}
+| | ... | ${dut1} | ${DUT1_${int}2}[0] | ${tg_if2_ip6} | ${TG_pf2_mac}[0]
+
+| Initialize LISP IPv4 forwarding in 3-node circular topology
+| | [Documentation] | Custom setup of IPv4 addresses on all DUT nodes and TG \
+| | ... | Don`t set route.
+| |
+| | ... | *Arguments:*
+| | ... | - dut1_dut2_address - Ip address from DUT1 to DUT2. Type: string
+| | ... | - dut1_tg_address - Ip address from DUT1 to tg. Type: string
+| | ... | - dut2_dut1_address - Ip address from DUT2 to DUT1. Type: string
+| | ... | - dut1_tg_address - Ip address from DUT1 to tg. Type: string
+| | ... | - duts_prefix - ip prefix. Type: int
+| |
+| | ... | *Return:*
+| | ... | - No value returned
+| |
+| | ... | *Example:*
+| | ... | \| Initialize LISP IPv4 forwarding in 3-node circular topology \
+| | ... | \| ${dut1_dut2_address} \| ${dut1_tg_address} \
+| | ... | \| ${dut2_dut1_address} \| ${dut2_tg_address} \| ${duts_prefix} \|
+| |
+| | [Arguments] | ${dut1_dut2_address} | ${dut1_tg_address}
+| | ... | ${dut2_dut1_address} | ${dut2_tg_address} | ${duts_prefix}
+| |
+| | Set interfaces in path up
+| | VPP Add IP Neighbor
+| | ... | ${dut1} | ${DUT1_${int}1}[0] | 10.10.10.2 | ${TG_pf1_mac}[0]
+| | VPP Add IP Neighbor
+| | ... | ${dut1} | ${DUT1_${int}2}[0] | ${dut2_dut1_address}
+| | ... | ${DUT2_${int}1_mac}[0]
+| | VPP Add IP Neighbor
+| | ... | ${dut2} | ${DUT2_${int}1}[0] | ${dut1_dut2_address}
+| | ... | ${DUT1_${int}2_mac}[0]
+| | VPP Add IP Neighbor
+| | ... | ${dut2} | ${DUT2_${int}2}[0] | 20.20.20.2 | ${TG_pf2_mac}[0]
+| | VPP Interface Set IP Address
+| | ... | ${dut1} | ${DUT1_${int}1}[0] | ${dut1_tg_address} | ${duts_prefix}
+| | VPP Interface Set IP Address
+| | ... | ${dut1} | ${DUT1_${int}2}[0] | ${dut1_dut2_address} | ${duts_prefix}
+| | VPP Interface Set IP Address
+| | ... | ${dut2} | ${DUT2_${int}1}[0] | ${dut2_dut1_address} | ${duts_prefix}
+| | VPP Interface Set IP Address
+| | ... | ${dut2} | ${DUT2_${int}2}[0] | ${dut2_tg_address} | ${duts_prefix}
+
+| Initialize LISP GPE IPv4 over IPsec in 3-node circular topology
+| | [Documentation] | Setup Lisp GPE IPv4 forwarding over IPsec.
+| |
+| | ... | *Arguments:*
+| | ... | - encr_alg - Encryption algorithm. Type: string
+| | ... | - auth_alg - Authentication algorithm. Type: string
+| |
+| | ... | *Return:*
+| | ... | - No value returned
+| |
+| | ... | *Example:*
+| | ... | \| Initialize LISP GPE IPv4 over IPsec in 3-node circular topology\
+| | ... | \| ${encr_alg} \| ${auth_alg}
+| |
+| | [Arguments] | ${encr_alg} | ${auth_alg}
+| |
+| | Generate keys for IPSec | ${encr_alg} | ${auth_alg}
+| | Initialize LISP IPv4 forwarding in 3-node circular topology
+| | ... | ${dut1_to_dut2_ip4} | ${dut1_to_tg_ip4} | ${dut2_to_dut1_ip4}
+| | ... | ${dut2_to_tg_ip4} | ${prefix4}
+| | Configure LISP GPE topology in 3-node circular topology
+| | ... | ${dut1} | ${DUT1_${int}2}[0] | ${NONE}
+| | ... | ${dut2} | ${DUT2_${int}1}[0] | ${NONE}
+| | ... | ${duts_locator_set} | ${dut1_ip4_eid} | ${dut2_ip4_eid}
+| | ... | ${dut1_ip4_static_adjacency} | ${dut2_ip4_static_adjacency}
+| | Configure manual keyed connection for IPSec
+| | ... | ${dut1} | ${DUT1_${int}2}[0] | ${encr_alg} | ${encr_key}
+| | ... | ${auth_alg} | ${auth_key} | ${dut1_spi} | ${dut2_spi}
+| | ... | ${dut1_to_dut2_ip4} | ${dut2_to_dut1_ip4}
+| | Configure manual keyed connection for IPSec
+| | ... | ${dut2} | ${DUT2_${int}1}[0] | ${encr_alg} | ${encr_key}
+| | ... | ${auth_alg} | ${auth_key} | ${dut2_spi} | ${dut1_spi}
+| | ... | ${dut2_to_dut1_ip4} | ${dut1_to_dut2_ip4}
+
+| Initialize LISP IPv6 forwarding in 3-node circular topology
+| | [Documentation] | Custom setup of IPv6 topology on all DUT nodes \
+| | ... | Don`t set route.
+| |
+| | ... | *Arguments:*
+| | ... | - dut1_dut2_address - Ip address from DUT1 to DUT2. Type: string
+| | ... | - dut1_tg_address - Ip address from DUT1 to tg. Type: string
+| | ... | - dut2_dut1_address - Ip address from DUT2 to DUT1. Type: string
+| | ... | - dut1_tg_address - Ip address from DUT1 to tg. Type: string
+| | ... | - duts_prefix - ip prefix. Type: int
+| |
+| | ... | *Return:*
+| | ... | - No value returned
+| |
+| | ... | *Example:*
+| | ... | \| Initialize LISP IPv6 forwarding in 3-node circular topology \
+| | ... | \| ${dut1_dut2_address} \| ${dut1_tg_address} \
+| | ... | \| ${dut2_dut1_address} \| ${dut2_tg_address} \| ${duts_prefix} \|
+| |
+| | [Arguments] | ${dut1_dut2_address} | ${dut1_tg_address}
+| | ... | ${dut2_dut1_address} | ${dut2_tg_address} | ${prefix}
+| |
+| | Set interfaces in path up
+| | VPP Interface Set IP Address
+| | ... | ${dut1} | ${DUT1_${int}1}[0] | ${dut1_tg_address} | ${prefix}
+| | VPP Interface Set IP Address
+| | ... | ${dut1} | ${DUT1_${int}2}[0] | ${dut1_dut2_address} | ${prefix}
+| | VPP Interface Set IP Address
+| | ... | ${dut2} | ${DUT2_${int}1}[0] | ${dut2_dut1_address} | ${prefix}
+| | VPP Interface Set IP Address
+| | ... | ${dut2} | ${DUT2_${int}2}[0] | ${dut2_tg_address} | ${prefix}
+| | Vpp All Ra Suppress Link Layer | ${nodes}
+| | VPP Add IP Neighbor
+| | ... | ${dut1} | ${DUT1_${int}1}[0] | 2001:1::2 | ${TG_pf1_mac}[0]
+| | VPP Add IP Neighbor
+| | ... | ${dut2} | ${DUT2_${int}1}[0] | 2001:2::2 | ${TG_pf2_mac}[0]
+| | VPP Add IP Neighbor
+| | ... | ${dut1} | ${DUT1_${int}2}[0] | ${dut2_dut1_address}
+| | ... | ${DUT2_${int}1_mac}[0]
+| | VPP Add IP Neighbor
+| | ... | ${dut2} | ${DUT2_${int}1}[0] | ${dut1_dut2_address}
+| | ... | ${DUT1_${int}2_mac}[0]
+
+| Initialize LISP IPv4 over IPv6 forwarding in 3-node circular topology
+| | [Documentation] | Custom setup of IPv4 over IPv6 topology on all DUT nodes \
+| | ... | Don`t set route.
+| |
+| | ... | *Arguments:*
+| | ... | - dut1_dut2_ip6_address - IPv6 address from DUT1 to DUT2.
+| | ... | Type: string
+| | ... | - dut1_tg_ip4_address - IPv4 address from DUT1 to tg. Type: string
+| | ... | - dut2_dut1_ip6_address - IPv6 address from DUT2 to DUT1.
+| | ... | Type: string
+| | ... | - dut1_tg_ip4_address - IPv4 address from DUT1 to tg. Type: string
+| | ... | - prefix4 - IPv4 prefix. Type: int
+| | ... | - prefix6 - IPv6 prefix. Type: int
+| |
+| | ... | *Return:*
+| | ... | - No value returned
+| |
+| | ... | *Example:*
+| | ... | \| Lisp IPv4 over IPv6 forwarding initialized in a 3-node circular \
+| | ... | topology \| ${dut1_dut2_ip6_address} \| ${dut1_tg_ip4_address} \
+| | ... | \| ${dut2_dut1_ip6_address} \| ${dut2_tg_ip4_address} \
+| | ... | \| ${prefix4} \| ${prefix6} \|
+| |
+| | [Arguments] | ${dut1_dut2_ip6_address} | ${dut1_tg_ip4_address}
+| | ... | ${dut2_dut1_ip6_address} | ${dut2_tg_ip4_address}
+| | ... | ${prefix4} | ${prefix6}
+| |
+| | Set interfaces in path up
+| | VPP Interface Set IP Address
+| | ... | ${dut1} | ${DUT1_${int}1}[0] | ${dut1_tg_ip4_address} | ${prefix4}
+| | VPP Interface Set IP Address
+| | ... | ${dut1} | ${DUT1_${int}2}[0] | ${dut1_dut2_ip6_address} | ${prefix6}
+| | VPP Interface Set IP Address
+| | ... | ${dut2} | ${DUT2_${int}1}[0] | ${dut2_dut1_ip6_address} | ${prefix6}
+| | VPP Interface Set IP Address
+| | ... | ${dut2} | ${DUT2_${int}2}[0] | ${dut2_tg_ip4_address} | ${prefix4}
+| | Vpp All Ra Suppress Link Layer | ${nodes}
+| | VPP Add IP Neighbor
+| | ... | ${dut1} | ${DUT1_${int}1}[0] | 10.10.10.2 | ${TG_pf1_mac}[0]
+| | VPP Add IP Neighbor
+| | ... | ${dut2} | ${DUT2_${int}2}[0] | 20.20.20.2 | ${TG_pf2_mac}[0]
+| | VPP Add IP Neighbor
+| | ... | ${dut1} | ${DUT1_${int}2}[0] | ${dut2_dut1_ip6_address}
+| | ... | ${DUT2_${int}1_mac}[0]
+| | VPP Add IP Neighbor
+| | ... | ${dut2} | ${DUT2_${int}1}[0] | ${dut1_dut2_ip6_address}
+| | ... | ${DUT1_${int}2_mac}[0]
+
+| Initialize LISP IPv6 over IPv4 forwarding in 3-node circular topology
+| | [Documentation] | Custom setup of IPv4 over IPv6 topology on all DUT nodes \
+| | ... | Don`t set route.
+| |
+| | ... | *Arguments:*
+| | ... | - dut1_dut2_ip4_address - IPv4 address from DUT1 to DUT2.
+| | ... | Type: string
+| | ... | - dut1_tg_ip6_address - IPv6 address from DUT1 to tg. Type: string
+| | ... | - dut2_dut1_ip4_address - IPv4 address from DUT2 to DUT1.
+| | ... | Type: string
+| | ... | - dut1_tg_ip6_address - IPv6 address from DUT1 to tg. Type: string
+| | ... | - prefix4 - IPv4 prefix. Type: int
+| | ... | - prefix6 - IPv6 prefix. Type: int
+| |
+| | ... | *Return:*
+| | ... | - No value returned
+| |
+| | ... | *Example:*
+| | ... | \| Lisp IPv6 over IPv4 forwarding initialized in a 3-node circular \
+| | ... | topology \| ${dut1_dut2_ip4_address} \| ${dut1_tg_ip6_address} \
+| | ... | \| ${dut2_dut1_ip4_address} \| ${dut2_tg_ip6_address} \
+| | ... | \| ${prefix6} \| ${prefix4} \|
+| |
+| | [Arguments] | ${dut1_dut2_ip4_address} | ${dut1_tg_ip6_address}
+| | ... | ${dut2_dut1_ip4_address} | ${dut2_tg_ip6_address}
+| | ... | ${prefix6} | ${prefix4}
+| |
+| | Set interfaces in path up
+| | VPP Interface Set IP Address
+| | ... | ${dut1} | ${DUT1_${int}1}[0] | ${dut1_tg_ip6_address} | ${prefix6}
+| | VPP Interface Set IP Address
+| | ... | ${dut1} | ${DUT1_${int}2}[0] | ${dut1_dut2_ip4_address} | ${prefix4}
+| | VPP Interface Set IP Address
+| | ... | ${dut2} | ${DUT2_${int}1}[0] | ${dut2_dut1_ip4_address} | ${prefix4}
+| | VPP Interface Set IP Address
+| | ... | ${dut2} | ${DUT2_${int}2}[0] | ${dut2_tg_ip6_address} | ${prefix6}
+| | Vpp All Ra Suppress Link Layer | ${nodes}
+| | VPP Add IP Neighbor
+| | ... | ${dut1} | ${DUT1_${int}1}[0] | 2001:1::2 | ${TG_pf1_mac}[0]
+| | VPP Add IP Neighbor
+| | ... | ${dut2} | ${DUT2_${int}2}[0] | 2001:2::2 | ${TG_pf2_mac}[0]
+| | VPP Add IP Neighbor
+| | ... | ${dut1} | ${DUT1_${int}2}[0] | ${dut2_dut1_ip4_address}
+| | ... | ${DUT2_${int}1_mac}[0]
+| | VPP Add IP Neighbor
+| | ... | ${dut2} | ${DUT2_${int}1}[0] | ${dut1_dut2_ip4_address}
+| | ... | ${DUT1_${int}2_mac}[0]

commit 27208b0e1c8ae9563b56188d9ccbd5f88588a5c4

fix(lisp): repair a typo
Ticket: CSIT-1806
Fixes: 14a71b74b414df7616ccb5ada3d50ecb90d96bae

Change-Id: Ia20d05b4e16daf60260fcbaea806406fa654a94e
Signed-off-by: Vratko Polak <vrpolak@cisco.com>

resources/libraries/robot/overlay/lisp.robot
@@ -344,7 +344,7 @@
 | | ... | ${dut1} | ${DUT1_${int}2}[0] | ${dut2_dut1_address}
 | | ... | ${DUT2_${int}1_mac}[0]
 | | VPP Add IP Neighbor
-| | ... | ${dut2} | ${DUT2_${int}1}[0] | ${dut1_dut2_address}
+| | ... | ${dut2} | ${DUT2_${int}2}[0] | ${dut1_dut2_address}
 | | ... | ${DUT1_${int}2_mac}[0]
 
 | Initialize LISP IPv4 over IPv6 forwarding in 3-node circular topology