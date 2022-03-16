1213c9c7bf210538cb6c23c1cf19e8a6ee144c66
Juraj Linkeš <juraj.linkes@pantheon.tech>
2022-03-16 09:46:15 +0100

    Framework: exclude env and .git when copying
    The tarball is unnecessarily big, remove superflous directories.
    
    Change-Id: I55f6fae64973cfac34a1dc18cbdc688cd416e8b9
    Signed-off-by: Juraj Linkeš <juraj.linkes@pantheon.tech>

--- resources/libraries/python/SetupFramework.py
+++ resources/libraries/python/SetupFramework.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2021 Cisco and/or its affiliates.
+# Copyright (c) 2022 Cisco and/or its affiliates.
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at:
@@ -58,7 +58,8 @@ def pack_framework_dir():
     run(
         [
             u"tar", u"--sparse", u"--exclude-vcs", u"--exclude=output*.xml",
-            u"--exclude=./tmp", u"-zcf", file_name, u"."
+            u"--exclude=./tmp", u"--exclude=./env", u"--exclude=./.git",
+            u"-zcf", file_name, u"."
         ], msg=u"Could not pack testing framework"
     )
 
@@ -136,7 +137,7 @@ def create_env_directory_at_node(node):
         f"&& source env/bin/activate && ANSIBLE_SKIP_CONFLICT_CHECK=1 " \
         f"pip3 install -r requirements.txt"
     stdout, stderr = exec_cmd_no_error(
-        node, cmd, timeout=100, include_reason=True, export=False,
+        node, cmd, timeout=300, include_reason=True, export=False,
         message=f"Failed install at node {node[u'type']} host {node[u'host']}, "
         f"port {node[u'port']}"
     )
