091c688cb3969c756428ac265c9f20015297d1f0
Juraj Linkeš <juraj.linkes@pantheon.tech>
2022-04-01 14:29:23 +0200

    WIP update 3n-alt ansible and scripts
    Change-Id: I834b991f281e0b29b4af9dc37fe8028460298d83
    Signed-off-by: Juraj Linkeš <juraj.linkes@pantheon.tech>

--- resources/libraries/bash/function/common.sh
+++ resources/libraries/bash/function/common.sh
@@ -495,6 +495,10 @@ function get_test_code () {
             NODENESS="3n"
             FLAVOR="tsh"
             ;;
+        *"3n-alt"*)
+            NODENESS="3n"
+            FLAVOR="alt"
+            ;;
     esac
 }
 
@@ -884,7 +888,7 @@ function select_tags () {
         *"3n-skx"* | *"2n-skx"* | *"2n-clx"* | *"2n-zn2"*)
             default_nic="nic_intel-xxv710"
             ;;
-        *"2n-tx2"* | *"mrr-daily-master")
+        *"2n-tx2"* | *"3n-alt"* | *"mrr-daily-master")
             default_nic="nic_intel-xl710"
             ;;
         *"1n-aws"* | *"2n-aws"* | *"3n-aws"*)
@@ -1020,7 +1024,7 @@ function select_tags () {
             test_tag_array+=("!vts")
             test_tag_array+=("!drv_avf")
             ;;
-        *"2n-tx2"*)
+        *"2n-tx2"* | *"3n-alt"*)
             test_tag_array+=("!ipsechw")
             ;;
         *"3n-dnv"*)
