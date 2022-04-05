00d585ac8955318e0950593c5abf96ded46c50bc
Peter Mikus <pmikus@cisco.com>
2022-04-05 11:16:48 +0200

    test
    Signed-off-by: Peter Mikus <pmikus@cisco.com>
    Change-Id: I5c0d4fb0f05ef7641715e1740cb17abae906c7f1

--- fdio.infra.terraform/terraform-aws-vpc/versions.tf
+++ fdio.infra.terraform/terraform-aws-vpc/versions.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 4.3.0"
+      version = "~> 3.61.0"
     }
   }
   required_version = ">= 1.0.4"
