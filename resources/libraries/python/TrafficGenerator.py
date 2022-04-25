5c51814c684e4a98e93d89fcc01cdcb9b26fc9bd
Vratko Polak <vrpolak@cisco.com>
2022-04-25 10:22:05 +0200

    feat(astf): Support framesizes for ASTF
    - No support for IMIX.
    + Fix a bad bug in padding (most ASTF profiles had wrong frame sizes).
    + Fix a big typo in TCP PPS profiles (s->c was not data, just RST).
    + Control transaction size via ASTF_N_DATA_FRAMES env variable.
     - Default value 5 leads to transactions smaller than before.
     + It ensures transaction is one burst (per direction) even for jumbo.
    + Edit autogen to set supported frame sizes based on suite id.
     + Both TCP and UDP use the same values:
      + 64B for CPS (exact for UDP, nominal for TCP).
      + 100B, 1518B and 9000B for TPUT and PPS.
       - TCP TPUT achievable minimum is 70B.
       + Used 100B to leave room for possible IPv6 ASTF tests.
     + Separate function for code reused by vpp and trex tests.
     - I do not really like the new "copy and edit" approach added here.
      + But it is a quick edit, better autogen refactor is low priority.
    + Consider both established and transitory sessions as valid.
     - Mostly for compatibility with 2202 behavior and to avoid ramp-ups.
     - Assuming both session states have similar enough VPP CPU overhead.
     + Added a TODO to investigate and maybe reconsider later.
    + Update the state timeout value to 240s.
     + That is the default for TCP (for transitory state).
     - UDP could keep using 300s.
      + But I prefer UDP and TCP to behave as similarly as possible.
    + Use TRex tunables to get the exact frame size (for data packets).
     - It is not clear why the recipe for MSS has to be this complicated.
     + Move code away from profile init, as frame size is not known there.
     + Change internal profile API, so values related to MSS are passed.
    + Lower ramp-up rate for TCP TPUT tests.
     + Because without lower rate, jumbo fails on packet loss in ramp-up.
     + UDP TPUT ramp-up rate also lowered (just to keep suites more similar).
    + Distinguish one-direction and aggregated average frame size.
     + Update keyword documentation where the distiction matters.
     + One-direction is needed for turning bandwidth limit to TPS limit.
     + Aggregated is needed for correct NDRPDR bandwidth result value.
     - TCP TPUT will always be few percent below bidirectional maximum.
      + That is unavoidable, as one direction sends more control packets.
    + Add runtime consistency checks so future refactors are safer.
     + Fail if padding requested would be negative.
     + Fail if suite claims unexpected values for packets per transaction.
    + Edit the 4 types of ASTF profiles to keep them similar to each other.
     + Move UDP TPUT limit value from a field back to direct argument.
     + Stop pretending first UDP packet is not data.
    + Apply small improvements where convenient.
     + Replace "aggregate" with "aggregated" where possible.
      + To lower probability of any future typos in variable names.
     + Avoid calling Set Numeric Frame Sizes twice.
     + Code formatting, keyword documentation, code comments, ...
     + Add TODOs for less important code quality improvements.
    - Postpone updating of methodology pages to a subsequent change.
    
    Change-Id: I4b381e5210e69669f972326202fdcc5a2c9c923b
    Signed-off-by: Vratko Polak <vrpolak@cisco.com>

--- resources/libraries/python/TrafficGenerator.py
+++ resources/libraries/python/TrafficGenerator.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2021 Cisco and/or its affiliates.
+# Copyright (c) 2022 Cisco and/or its affiliates.
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at:
@@ -643,6 +643,9 @@ class TrafficGenerator(AbstractMeasurer):
         )
         command_line.add_with_value(u"duration", f"{computed_duration!r}")
         command_line.add_with_value(u"frame_size", self.frame_size)
+        command_line.add_with_value(
+            u"n_data_frames", Constants.ASTF_N_DATA_FRAMES
+        )
         command_line.add_with_value(u"multiplier", multiplier)
         command_line.add_with_value(u"port_0", p_0)
         command_line.add_with_value(u"port_1", p_1)
@@ -811,7 +814,7 @@ class TrafficGenerator(AbstractMeasurer):
             use_latency=False,
             ramp_up_rate=None,
             ramp_up_duration=None,
-            state_timeout=300.0,
+            state_timeout=240.0,
             ramp_up_only=False,
         ):
         """Send traffic from all configured interfaces on TG.
@@ -1215,7 +1218,7 @@ class TrafficGenerator(AbstractMeasurer):
         The target_tr field of ReceiveRateMeasurement is in
         transactions per second. Transmit count and loss count units
         depend on the transaction type. Usually they are in transactions
-        per second, or aggregate packets per second.
+        per second, or aggregated packets per second.
 
         TODO: Fail on running or already reported measurement.
 
@@ -1382,7 +1385,7 @@ class TrafficGenerator(AbstractMeasurer):
             use_latency=False,
             ramp_up_rate=None,
             ramp_up_duration=None,
-            state_timeout=300.0,
+            state_timeout=240.0,
         ):
         """Store values accessed by measure().
 
@@ -1451,7 +1454,7 @@ class OptimizedSearch:
     """Class to be imported as Robot Library, containing search keywords.
 
     Aside of setting up measurer and forwarding arguments,
-    the main business is to translate min/max rate from unidir to aggregate.
+    the main business is to translate min/max rate from unidir to aggregated.
     """
 
     @staticmethod
@@ -1475,7 +1478,7 @@ class OptimizedSearch:
             use_latency=False,
             ramp_up_rate=None,
             ramp_up_duration=None,
-            state_timeout=300.0,
+            state_timeout=240.0,
             expansion_coefficient=4.0,
     ):
         """Setup initialized TG, perform optimized search, return intervals.
@@ -1616,7 +1619,7 @@ class OptimizedSearch:
             use_latency=False,
             ramp_up_rate=None,
             ramp_up_duration=None,
-            state_timeout=300.0,
+            state_timeout=240.0,
     ):
         """Setup initialized TG, perform soak search, return avg and stdev.
 
@@ -1674,7 +1677,7 @@ class OptimizedSearch:
         :type ramp_up_rate: float
         :type ramp_up_duration: float
         :type state_timeout: float
-        :returns: Average and stdev of estimated aggregate rate giving PLR.
+        :returns: Average and stdev of estimated aggregated rate giving PLR.
         :rtype: 2-tuple of float
         """
         tg_instance = BuiltIn().get_library_instance(
