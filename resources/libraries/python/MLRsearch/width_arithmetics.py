5d929c70368783deb61276310549a86a6214f388
Vratko Polak <vrpolak@cisco.com>
2022-05-03 13:14:10 +0200

    WiP: MLRsearch new logic
    - I still think there are too many remeasurements.
    + Syntax errors fixed.
    + No more errors with simulator (even with 10k runs).
    + Slight overall duration decrease (~2% on default two-phase measurer).
     + It mostly seems to come from limit avoidance.
     - Simulator would need another model to show value of ratio before duration.
    + Change expansion coefficient (Python) default to 4,
      so simulator is fair with comparing to real improvements.
    
    Change-Id: Iae68279ac6f16f898899beed33177e7b3615beba
    Signed-off-by: Vratko Polak <vrpolak@cisco.com>

--- resources/libraries/python/MLRsearch/width_arithmetics.py
+++ resources/libraries/python/MLRsearch/width_arithmetics.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2021 Cisco and/or its affiliates.
+# Copyright (c) 2022 Cisco and/or its affiliates.
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at:
@@ -24,43 +24,48 @@ def multiply_relative_width(relative_width, coefficient):
     The multiplication happens in logarithmic space,
     so the resulting relative width is always less than 1.
 
+    When expanding, it is recommended to multiply coefficient with
+    ROUNDING_CONSTANT before passing,
+    so the subsequent halving does not result in a wider than goal interval.
+
     :param relative_width: The base relative width to multiply.
     :param coefficient: Multiply by this in logarithmic space.
+    :param rounding_c: Use to manipulate rounding errors.
     :type relative_width: float
     :type coefficient: float
     :returns: The relative width of multiplied logarithmic size.
     :rtype: float
     """
-    old_log_width = math.log(1.0 - relative_width)
+    # Using log1p and expm1 to avoid rounding errors for small widths.
+    old_log_width = math.log1p(-relative_width)
     # Slight decrease to prevent rounding errors from prolonging the search.
     # TODO: Make the nines configurable.
-    new_log_width = old_log_width * coefficient * ROUNDING_CONSTANT
-    return 1.0 - math.exp(new_log_width)
+    new_log_width = old_log_width * coefficient
+    return -math.expm1(new_log_width)
 
-def halve_relative_width(relative_width, goal_width):
+def halve_relative_width(relative_width, width_goal):
     """Return relative width corresponding to half logarithmic width.
 
-    The logic attempts to save some halvings in future by performing
-    uneven split. If rounding error risk is detected,
-    even split is used.
+    The logic attempts to save some halvings in future
+    by performing uneven split here.
+    If rounding error risk is detected even split is used.
 
     :param relative_width: The base relative width to halve.
-    :param goal_width: Width goal for final phase.
+    :param width_goal: Width goal for final phase.
     :type relative_width: float
-    :type goal_width: float
+    :type width_goal: float
     :returns: The relative width of half logarithmic size.
     :rtype: float
     """
-    fallback_width = 1.0 - math.sqrt(1.0 - relative_width)
+    wig = math.log1p(-relative_width) / math.log1p(-width_goal)
     # Wig means Width In Goals.
-    wig = math.log(1.0 - relative_width) / math.log(1.0 - goal_width)
     cwig = 2.0 * math.ceil(wig / 2.0)
-    fwig = 2.0 * math.ceil(wig * ROUNDING_CONSTANT / 2.0)
+    fwig = 2.0 * math.ceil(wig * ROUNDING_CONSTANT * ROUNDING_CONSTANT / 2.0)
     if wig <= 2.0 or cwig != fwig:
         # Avoid too uneven splits.
-        return fallback_width
+        return multiply_relative_width(relative_width, 0.5)
     coefficient = cwig / 2
-    new_width = multiply_relative_width(goal_width, coefficient)
+    new_width = multiply_relative_width(width_goal, coefficient)
     return new_width
 
 def step_down(current_bound, relative_width):
@@ -92,6 +97,7 @@ def multiple_step_down(current_bound, relative_width, coefficient):
 
     The multiplication happens in logarithmic space,
     so the resulting applied relative width is always less than 1.
+    Rounding constant is automatically applied.
 
     :param relative_width: The base relative width to double.
     :param current_bound: The current target transmit rate to move [pps].
@@ -102,7 +108,8 @@ def multiple_step_down(current_bound, relative_width, coefficient):
     :returns: Transmit rate smaller by logarithmically multiplied width [pps].
     :rtype: float
     """
-    new_width = multiply_relative_width(relative_width, coefficient)
+    coeff = coefficient * ROUNDING_CONSTANT
+    new_width = multiply_relative_width(relative_width, coeff)
     return step_down(current_bound, new_width)
 
 def multiple_step_up(current_bound, relative_width, coefficient):
@@ -110,6 +117,7 @@ def multiple_step_up(current_bound, relative_width, coefficient):
 
     The multiplication happens in logarithmic space,
     so the resulting applied relative width is always less than 1.
+    Rounding constant is automatically applied.
 
     :param current_bound: The current target transmit rate to move [pps].
     :param relative_width: The base relative width to double.
@@ -120,11 +128,35 @@ def multiple_step_up(current_bound, relative_width, coefficient):
     :returns: Transmit rate larger by logarithmically multiplied width [pps].
     :rtype: float
     """
-    new_width = multiply_relative_width(relative_width, coefficient)
+    coeff = coefficient * ROUNDING_CONSTANT
+    new_width = multiply_relative_width(relative_width, coeff)
     return step_up(current_bound, new_width)
 
-def half_step_up(current_bound, relative_width, goal_width):
+def half_step_up(current_bound, relative_width, width_goal):
     """Return rate of half logarithmic width above.
+
+    This function is smart, using uneven splits to avoid some measurements,
+    that is why the width goal is needed.
+
+    Not supporting optional strict halving here, because pylint has trouble
+    determining where an Optional typed variable can no longer be None.
+
+    :param relative_width: The base relative width to halve.
+    :param current_bound: The current target transmit rate to move [pps].
+    :param width_goal: Apply uneven splits when this is provided.
+    :type relative_width: float
+    :type current_bound: float
+    :type width_goal: Optional[float]
+    :returns: Transmit rate larger by logarithmically half width [pps].
+    :rtype: float
+    """
+    new_width = halve_relative_width(relative_width, width_goal)
+    return step_up(current_bound, new_width)
+
+def strict_half_step_up(current_bound, relative_width):
+    """Return rate of half logarithmic width above.
+
+    This function avoids the smart logic.
 
     :param relative_width: The base relative width to halve.
     :param current_bound: The current target transmit rate to move [pps].
@@ -133,5 +165,5 @@ def half_step_up(current_bound, relative_width, goal_width):
     :returns: Transmit rate larger by logarithmically half width [pps].
     :rtype: float
     """
-    new_width = halve_relative_width(relative_width, goal_width)
+    new_width = multiply_relative_width(relative_width, 0.5)
     return step_up(current_bound, new_width)
