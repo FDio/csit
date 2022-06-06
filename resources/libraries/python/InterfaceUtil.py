8b30ecf31638c792044368709c37226266766cb0
xinfeng zhao <xinfengx.zhao@intel.com>
2022-06-06 11:19:22 +0800

    test
    Signed-off-by: xinfeng zhao <xinfengx.zhao@intel.com>
    Change-Id: Idb1a70ba0968f6ff406e227a5b9e97bed458367e

--- resources/libraries/python/InterfaceUtil.py
+++ resources/libraries/python/InterfaceUtil.py
@@ -1133,6 +1133,52 @@ class InterfaceUtil:
             papi_exec.add(cmd, **args).get_reply(err_msg)
 
     @staticmethod
+    def vpp_get_ice(node):
+        cmd = u"ls -l /lib/firmware/intel/ice/ddp/"
+
+        err_msg = u"Failed."
+        stdout, _ = exec_cmd_no_error(
+            node, cmd, sudo=False, message=err_msg, retries=10
+            )
+        logger.info(stdout)
+
+        cmd = u"ls -l /lib/firmware/updates/intel/ice/ddp/"
+
+        err_msg = u"Failed."
+        stdout, _ = exec_cmd_no_error(
+            node, cmd, sudo=False, message=err_msg, retries=10
+            )
+        logger.info(stdout)
+
+
+    @staticmethod
+    def vpp_get_ddp(node):
+        cmd = u"modinfo ice"
+
+        err_msg = u"Failed."
+        stdout, _ = exec_cmd_no_error(
+            node, cmd, sudo=False, message=err_msg, retries=10
+            )
+        logger.info(stdout)
+
+        cmd = u"dmesg | grep DDP"
+
+        err_msg = u"Failed."
+        ret, stdout, _ = exec_cmd(
+            node, cmd, sudo=True, message=err_msg, retries=10
+            )
+        logger.info(stdout)
+
+    @staticmethod
+    def vpp_set_flow(node, interface):
+        from resources.libraries.python.FlowUtil import FlowUtil
+        from resources.libraries.python.ssh import exec_cmd_no_error
+
+        flow_index = FlowUtil.vpp_create_ip4_ipsec_flow(
+                    node, "ESP", 100, "redirect-to-queue", value=0)
+        FlowUtil.vpp_flow_enable(node, interface, flow_index)
+
+    @staticmethod
     def vpp_create_loopback(node, mac=None):
         """Create loopback interface on VPP node.
 
