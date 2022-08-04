565eb027c7ba5626fa141a78fbac99d8386f4633
Jing Peng <jing@meter.com>
2022-08-04 16:24:41 -0400

    bash: fix catch-all pattern in csit model matching
    Change-Id: Ic3c78f8f447f59bcd10fd8b88f50f93f7279430b
    Signed-off-by: Jing Peng <jing@meter.com>
    Signed-off-by: Vratko Polak <vrpolak@cisco.com>

--- resources/libraries/bash/function/device.sh
+++ resources/libraries/bash/function/device.sh
@@ -1,4 +1,4 @@
-# Copyright (c) 2021 Cisco and/or its affiliates.
+# Copyright (c) 2022 Cisco and/or its affiliates.
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at:
@@ -495,7 +495,7 @@ function get_csit_model () {
             "0x1572"|"0x154c")
                 MODEL="Intel-X710"
                 ;;
-            "*")
+            *)
                 MODEL="virtual"
         esac
     fi
