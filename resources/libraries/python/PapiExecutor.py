74dadef65eb27646051f1465ca39026f3a0886d5
Vratko Polak <vrpolak@cisco.com>
2022-08-11 10:47:15 +0200

    Experiment: Log what is sent to fast PAPI wire
    Change-Id: Icdd88618e8a735bee79da4ae803292312a54d320
    Signed-off-by: Vratko Polak <vrpolak@cisco.com>

--- resources/libraries/python/PapiExecutor.py
+++ resources/libraries/python/PapiExecutor.py
@@ -34,6 +34,7 @@ from resources.libraries.python.Constants import Constants
 from resources.libraries.python.LocalExecution import run
 from resources.libraries.python.FilteredLogger import FilteredLogger
 from resources.libraries.python.PapiHistory import PapiHistory
+from resources.libraries.python.spying_socket import SpyingSocket
 from resources.libraries.python.ssh import (
     SSH, SSHTimeout, exec_cmd_no_error, scp_node)
 from resources.libraries.python.topology import Topology, SocketType
@@ -504,6 +505,7 @@ class PapiSocketExecutor:
                 break
         else:
             raise RuntimeError(u"Failed to connect to VPP over a socket.")
+        vpp_instance.transport.socket = SpyingSocket(vpp_instance.transport.socket)
         logger.trace(
             f"Establishing socket connection took {time.time()-time_enter}s"
         )
