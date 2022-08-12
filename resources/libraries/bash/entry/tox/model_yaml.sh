780bd0e0688d8cd326649c88440e1e5bc6824fd1
Vratko Polak <vrpolak@cisco.com>
2022-08-12 12:31:44 +0200

    feat(tox): add more checkers related to UTI model
    + Check for json being in synch with yaml.
    + Check for schema being valid according to its metaschema.
    + Fix detection for model version checker.
    + Use safe yaml load to supress warning.
    - Python scripts left close to schema for now, so no pylint.
    
    Change-Id: Ibc1de27d7af448f14a2dd3281e91511d56762c0e
    Signed-off-by: Vratko Polak <vrpolak@cisco.com>

---
+++ resources/libraries/bash/entry/tox/model_yaml.sh
@@ -0,0 +1,56 @@
+# Copyright (c) 2022 Cisco and/or its affiliates.
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at:
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+set -exuo pipefail
+
+# This file should be executed from tox, as the assumed working directory
+# is different from where this file is located.
+# This file does not have executable flag nor shebang,
+# to dissuade non-tox callers.
+
+# This script call a Python script to make sure
+# the yaml and json formats of UTI model schemas are in sync.
+# The check is skipped if no edits in schema directory are detected.
+
+# "set -eu" handles failures from the following two lines.
+BASH_CHECKS_DIR="$(dirname $(readlink -e "${BASH_SOURCE[0]}"))"
+BASH_FUNCTION_DIR="$(readlink -e "${BASH_CHECKS_DIR}/../../function")"
+source "${BASH_FUNCTION_DIR}/common.sh" || {
+    echo "Source failed." >&2
+    exit 1
+}
+common_dirs || die
+
+impl_log="edited_files.log"
+git diff --name-only HEAD~ > "${impl_log}"
+if ! grep -q '^docs/model/current/schema/' "${impl_log}"; then
+    # Failing grep means no model edits.
+    warn "No model schema edits detected."
+    warn
+    warn "CSIT model yaml checker: PASS"
+    exit 0
+fi
+
+pushd "${CSIT_DIR}/docs/model/current/schema"
+if ! python3 "check_yaml2json.py"; then
+    warn
+    warn "CSIT model yaml checker: FAIL"
+    popd
+    exit 1
+fi
+popd
+
+warn "Model schema edited, json in sync with yaml."
+warn
+warn "CSIT model yaml checker: PASS"
+exit 0
