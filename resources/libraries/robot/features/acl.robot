FDio/csit

commit 14a71b74b414df7616ccb5ada3d50ecb90d96bae

Improve pf layer
+ Merge single/double link
+ Introduce _pf{n}[0] variables so we can access physical function
  same way as virtual function
+ Cleanup code by moving complex logic to python
+ Prepare code for multiple vf functions

Signed-off-by: Peter Mikus <pmikus@cisco.com>
Change-Id: Ic2e74a38bfa146441357de8f0916aeb638941c49

resources/libraries/robot/features/acl.robot
@@ -1,4 +1,4 @@
-# Copyright (c) 2019 Cisco and/or its affiliates.
+# Copyright (c) 2020 Cisco and/or its affiliates.
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at:
@@ -259,40 +259,41 @@
 | | ... | Set Variable | ${dut2}
 | | ... | ELSE | Set Variable | ${dut1}
 | | ${dut_if2}= | Run Keyword If | '${dut2_status}' == 'PASS'
-| | ... | Set Variable | ${dut2_if2}
-| | ... | ELSE | Set Variable | ${dut1_if2}
+| | ... | Set Variable | ${DUT2_${int}2}[0]
+| | ... | ELSE | Set Variable | ${DUT1_${int}2}[0]
 | |
 | | Set interfaces in path up
 | |
 | | FOR | ${number} | IN RANGE | 2 | ${ip_nr}+2
 | | | VPP Add IP Neighbor
-| | | ... | ${dut1} | ${dut1_if1} | 10.10.10.${number} | ${tg_if1_mac}
+| | | ... | ${dut1} | ${DUT1_${int}1}[0] | 10.10.10.${number} | ${TG_pf1_mac}[0]
 | | | VPP Add IP Neighbor
-| | | ... | ${dut} | ${dut_if2} | 20.20.20.${number} | ${tg_if2_mac}
+| | | ... | ${dut} | ${dut_if2} | 20.20.20.${number} | ${TG_pf2_mac}[0]
 | | END
 | | Run Keyword If | '${dut2_status}' == 'PASS'
 | | ... | VPP Add IP Neighbor
-| | ... | ${dut1} | ${dut1_if2} | 1.1.1.2 | ${dut2_if1_mac}
+| | ... | ${dut1} | ${DUT1_${int}2}[0] | 1.1.1.2 | ${DUT2_${int}1_mac}[0]
 | | Run Keyword If | '${dut2_status}' == 'PASS'
 | | ... | VPP Add IP Neighbor
-| | ... | ${dut2} | ${dut2_if1} | 1.1.1.1 | ${dut1_if2_mac}
+| | ... | ${dut2} | ${DUT2_${int}1}[0] | 1.1.1.1 | ${DUT1_${int}2_mac}[0]
 | |
 | | VPP Interface Set IP Address
-| | ... | ${dut1} | ${dut1_if1} | 10.10.10.1 | 24
+| | ... | ${dut1} | ${DUT1_${int}1}[0] | 10.10.10.1 | 24
 | | VPP Interface Set IP Address
 | | ... | ${dut} | ${dut_if2} | 20.20.20.1 | 24
 | | Run Keyword If | '${dut2_status}' == 'PASS'
 | | ... | VPP Interface Set IP Address
-| | ... | ${dut1} | ${dut1_if2} | 1.1.1.1 | 30
+| | ... | ${dut1} | ${DUT1_${int}2}[0] | 1.1.1.1 | 30
 | | Run Keyword If | '${dut2_status}' == 'PASS'
 | | ... | VPP Interface Set IP Address
-| | ... | ${dut2} | ${dut2_if1} | 1.1.1.2 | 30
+| | ... | ${dut2} | ${DUT2_${int}1}[0] | 1.1.1.2 | 30
 | |
 | | Run Keyword If | '${dut2_status}' == 'PASS'
 | | ... | Vpp Route Add | ${dut1} | 20.20.20.0 | 24 | gateway=1.1.1.2
-| | ... | interface=${dut1_if2}
+| | ... | interface=${DUT1_${int}2}[0]
 | | Run Keyword If | '${dut2_status}' == 'PASS'
 | | ... | Vpp Route Add | ${dut2} | 10.10.10.0 | 24 | gateway=1.1.1.1
-| | ... | interface=${dut2_if1}
+| | ... | interface=${DUT2_${int}1}[0]
 | |
-| | Configure IPv4 ACLs | ${dut1} | ${dut1_if1} | ${dut1_if2}
+| | Configure IPv4 ACLs
+| | ... | ${dut1} | ${DUT1_${int}1}[0] | ${DUT1_${int}2}[0]

commit 233ad364b6939e60123dc51cca70be782b6eadc4

fix(robot): replace Run Keyword Unless
Use Run Keyword If, with negated condition.
This silences the deprecation warning we see since Robot bump.

+ Fix the "even" vs "_even" typo.

Fixes: 14a71b74b414df7616ccb5ada3d50ecb90d96bae

Change-Id: I9a88ff551994df3e129d71327f08207133a958a6
Signed-off-by: Vratko Polak <vrpolak@cisco.com>

resources/libraries/robot/features/acl.robot
@@ -100,10 +100,10 @@
 | | Add Macip Acl Multi Entries | ${dut} | rules=${acl0}
 | | Add Macip Acl Multi Entries | ${dut} | rules=${acl1}
 | | ${acl_idx}= | Set Variable | 0
-| | Run Keyword Unless | '${dut_if1}' == '${NONE}'
+| | Run Keyword If | '${dut_if1}' != '${NONE}'
 | | ... | Add Del Macip Acl Interface | ${dut} | ${dut_if1} | add | ${acl_idx}
 | | ${acl_idx}= | Set Variable | 1
-| | Run Keyword Unless | '${dut_if2}' == '${NONE}'
+| | Run Keyword If | '${dut_if2}' != '${NONE}'
 | | ... | Add Del Macip Acl Interface | ${dut} | ${dut_if2} | add | ${acl_idx}
 
 | Configure IPv4 ACLs

resources/libraries/robot/ip/ip4.robot
@@ -83,17 +83,17 @@
 | | ... | Vpp Route Add | ${dut2} | 10.10.10.0 | 24 | gateway=1.1.1.1
 | | ... | interface=${DUT2_${int}1}[0]
 | |
-| | Run Keyword Unless | '${remote_host1_ip}' == '${NONE}'
+| | Run Keyword If | '${remote_host1_ip}' != '${NONE}'
 | | ... | Vpp Route Add | ${dut1} | ${remote_host1_ip} | ${remote_host_mask}
 | | ... | gateway=10.10.10.2 | interface=${DUT1_${int}1}[0]
-| | Run Keyword Unless | '${remote_host2_ip}' == '${NONE}'
+| | Run Keyword If | '${remote_host2_ip}' != '${NONE}'
 | | ... | Vpp Route Add | ${dut} | ${remote_host2_ip} | ${remote_host_mask}
 | | ... | gateway=20.20.20.2 | interface=${dut_if2}
-| | Run Keyword Unless | '${remote_host1_ip}' == '${NONE}'
+| | Run Keyword If | '${remote_host1_ip}' != '${NONE}'
 | | ... | Run Keyword If | '${dut2_status}' == 'PASS'
 | | ... | Vpp Route Add | ${dut1} | ${remote_host1_ip} | ${remote_host_mask}
 | | ... | gateway=1.1.1.2 | interface=${DUT1_${int}2}[0]
-| | Run Keyword Unless | '${remote_host2_ip}' == '${NONE}'
+| | Run Keyword If | '${remote_host2_ip}' != '${NONE}'
 | | ... | Run Keyword If | '${dut2_status}' == 'PASS'
 | | ... | Vpp Route Add | ${dut2} | ${remote_host2_ip} | ${remote_host_mask}
 | | ... | gateway=1.1.1.1 | interface=${DUT2_${int}1}[0]
@@ -267,7 +267,7 @@
 | | | ${memif2}= | Set Variable | ${${dut}-memif-${number}-if2}
 | | | ${fib_table_1}= | Evaluate | ${fib_table_1} + ${1}
 | | | ${fib_table_2}= | Evaluate | ${fib_table_1} + ${1}
-| | | Run Keyword Unless | ${number} == ${count}
+| | | Run Keyword If | ${number} != ${count}
 | | | ... | Add Fib Table | ${nodes['${dut}']} | ${fib_table_2}
 | | | Assign Interface To Fib Table | ${nodes['${dut}']}
 | | | ... | ${memif1} | ${fib_table_1}
@@ -375,10 +375,10 @@
 | | | ... | ${dut1} | ${dut1-vhost-${number}-if1} | 1.1.1.2 | 30
 | | | VPP Interface Set IP Address
 | | | ... | ${dut1} | ${dut1-vhost-${number}-if2} | 1.1.2.2 | 30
-| | | Run Keyword Unless | ${testpmd_mac}
+| | | Run Keyword If | not ${testpmd_mac}
 | | | ... | Vpp Route Add | ${dut1} | 20.0.0.0 | 8 | gateway=1.1.1.1
 | | | ... | interface=${dut1-vhost-${number}-if1} | vrf=${fib_table_1}
-| | | Run Keyword Unless | ${testpmd_mac}
+| | | Run Keyword If | not ${testpmd_mac}
 | | | ... | Vpp Route Add | ${dut1} | 10.0.0.0 | 8 | gateway=1.1.2.1
 | | | ... | interface=${dut1-vhost-${number}-if2} | vrf=${fib_table_2}
 | | | Run Keyword If | ${testpmd_mac}
@@ -494,16 +494,16 @@
 | | | ... | ${dut2} | ${dut2-vhost-${number}-if1} | 1.1.1.2 | 30
 | | | VPP Interface Set IP Address
 | | | ... | ${dut2} | ${dut2-vhost-${number}-if2} | 1.1.2.2 | 30
-| | | Run Keyword Unless | ${testpmd_mac}
+| | | Run Keyword If | not ${testpmd_mac}
 | | | ... | Vpp Route Add | ${dut1} | 20.0.0.0 | 8 | gateway=1.1.1.1
 | | | ... | interface=${dut1-vhost-${number}-if1} | vrf=${fib_table_1}
-| | | Run Keyword Unless | ${testpmd_mac}
+| | | Run Keyword If | not ${testpmd_mac}
 | | | ... | Vpp Route Add | ${dut1} | 10.0.0.0 | 8 | gateway=1.1.2.1
 | | | ... | interface=${dut1-vhost-${number}-if2} | vrf=${fib_table_2}
-| | | Run Keyword Unless | ${testpmd_mac}
+| | | Run Keyword If | not ${testpmd_mac}
 | | | ... | Vpp Route Add | ${dut2} | 20.0.0.0 | 8 | gateway=1.1.1.1
 | | | ... | interface=${dut2-vhost-${number}-if1} | vrf=${fib_table_1}
-| | | Run Keyword Unless | ${testpmd_mac}
+| | | Run Keyword If | not ${testpmd_mac}
 | | | ... | Vpp Route Add | ${dut2} | 10.0.0.0 | 8 | gateway=1.1.2.1
 | | | ... | interface=${dut2-vhost-${number}-if2} | vrf=${fib_table_2}
 | | | Run Keyword If | ${testpmd_mac}

resources/libraries/robot/ip/ip6.robot
@@ -84,17 +84,17 @@
 | | ... | Vpp Route Add | ${dut2} | 2001:1::0 | 64 | gateway=2001:3::1
 | | ... | interface=${DUT2_${int}1}[0]
 | |
-| | Run Keyword Unless | '${remote_host1_ip}' == '${NONE}'
+| | Run Keyword If | '${remote_host1_ip}' != '${NONE}'
 | | ... | Vpp Route Add | ${dut1} | ${remote_host1_ip} | 128
 | | ... | gateway=2001:1::2 | interface=${DUT1_${int}1}[0]
-| | Run Keyword Unless | '${remote_host2_ip}' == '${NONE}'
+| | Run Keyword If | '${remote_host2_ip}' != '${NONE}'
 | | ... | Vpp Route Add | ${dut} | ${remote_host2_ip} | 128
 | | ... | gateway=2001:2::2 | interface=${dut_if2}
-| | Run Keyword Unless | '${remote_host1_ip}' == '${NONE}'
+| | Run Keyword If | '${remote_host1_ip}' != '${NONE}'
 | | ... | Run Keyword If | '${dut2_status}' == 'PASS'
 | | ... | Vpp Route Add | ${dut1} | ${remote_host1_ip} | 128
 | | ... | gateway=2001:3::2 | interface=${DUT1_${int}2}[0]
-| | Run Keyword Unless | '${remote_host2_ip}' == '${NONE}'
+| | Run Keyword If | '${remote_host2_ip}' != '${NONE}'
 | | ... | Run Keyword If | '${dut2_status}' == 'PASS'
 | | ... | Vpp Route Add | ${dut2} | ${remote_host2_ip} | 128
 | | ... | gateway=2001:3::1 | interface=${DUT2_${int}1}[0]

resources/libraries/robot/l2/l2_bridge_domain.robot
@@ -495,7 +495,7 @@
 | | VPP Set interface MTU | ${dut1} | ${dut1_eth_bond_if1}
 | | FOR | ${pf} | IN RANGE | 1 | ${nic_pfs} + 1
 | | | ${_even}= | Evaluate | ${pf} % 2
-| | | Run Keyword Unless | ${even}
+| | | Run Keyword If | not ${_even}
 | | | ... | VPP Add Bond Member
 | | | ... | ${dut1} | ${DUT1_${int}${pf}}[0] | ${dut1_eth_bond_if1}
 | | END
@@ -505,7 +505,7 @@
 | | VPP Set interface MTU | ${dut2} | ${dut2_eth_bond_if1}
 | | FOR | ${pf} | IN RANGE | 1 | ${nic_pfs} + 1
 | | | ${_even}= | Evaluate | ${pf} % 2
-| | | Run Keyword If | ${even}
+| | | Run Keyword If | ${_even}
 | | | ... | VPP Add Bond Member
 | | | ... | ${dut2} | ${DUT2_${int}${pf}}[0] | ${dut2_eth_bond_if1}
 | | END

resources/libraries/robot/l2/l2_xconnect.robot
@@ -247,7 +247,7 @@
 | | VPP Set interface MTU | ${dut1} | ${dut1_eth_bond_if1}
 | | FOR | ${pf} | IN RANGE | 1 | ${nic_pfs} + 1
 | | | ${_even}= | Evaluate | ${pf} % 2
-| | | Run Keyword Unless | ${even}
+| | | Run Keyword If | not ${_even}
 | | | ... | VPP Add Bond Member
 | | | ... | ${dut1} | ${DUT1_${int}${pf}}[0] | ${dut1_eth_bond_if1}
 | | END
@@ -257,7 +257,7 @@
 | | VPP Set interface MTU | ${dut2} | ${dut2_eth_bond_if1}
 | | FOR | ${pf} | IN RANGE | 1 | ${nic_pfs} + 1
 | | | ${_even}= | Evaluate | ${pf} % 2
-| | | Run Keyword If | ${even}
+| | | Run Keyword If | ${_even}
 | | | ... | VPP Add Bond Member
 | | | ... | ${dut2} | ${DUT2_${int}${pf}}[0] | ${dut2_eth_bond_if1}
 | | END

resources/libraries/robot/l2/tagging.robot
@@ -82,24 +82,24 @@
 | | ... | \| ${dut1_if2} \| 10 \|
 | |
 | | Set Interface State | ${DUT1} | ${INT1} | up
-| | Run Keyword Unless | ${DUT2} == ${None}
+| | Run Keyword If | ${DUT2} != ${None}
 | | ... | Set Interface State | ${DUT2} | ${INT2} | up
 | | ${INT1_NAME}= | Get interface name | ${DUT1} | ${INT1}
-| | ${INT2_NAME}= | Run Keyword Unless | ${DUT2} == ${None}
+| | ${INT2_NAME}= | Run Keyword If | ${DUT2} != ${None}
 | | ... | Get interface name | ${DUT2} | ${INT2}
 | | ${subif_name_1} | ${subif_index_1}= | Create Vlan Subinterface
 | | ... | ${DUT1} | ${INT1_NAME} | ${SUB_ID}
 | | ${subif_name_2} | ${subif_index_2}=
-| | ... | Run Keyword Unless | ${DUT2} == ${None}
+| | ... | Run Keyword If | ${DUT2} != ${None}
 | | ... | Create Vlan Subinterface | ${DUT2} | ${INT2_NAME} | ${SUB_ID}
 | | Set Interface State | ${DUT1} | ${subif_index_1} | up
-| | Run Keyword Unless | ${DUT2} == ${None}
+| | Run Keyword If | ${DUT2} != ${None}
 | | ... | Set Interface State | ${DUT2} | ${subif_index_2} | up
 | | Set Test Variable | ${subif_name_1}
 | | Set Test Variable | ${subif_index_1}
-| | Run Keyword Unless | ${DUT2} == ${None}
+| | Run Keyword If | ${DUT2} != ${None}
 | | ... | Set Test Variable | ${subif_name_2}
-| | Run Keyword Unless | ${DUT2} == ${None}
+| | Run Keyword If | ${DUT2} != ${None}
 | | ... | Set Test Variable | ${subif_index_2}
 
 | Configure L2 tag rewrite method on interfaces
@@ -115,7 +115,7 @@
 | | ... | - TAG_REWRITE_METHOD - Method of tag rewrite.
 | |
 | | L2 Vlan tag rewrite | ${DUT1} | ${SUB_INT1} | ${TAG_REWRITE_METHOD}
-| | Run Keyword Unless | ${DUT2} == ${None}
+| | Run Keyword If | ${DUT2} != ${None}
 | | ... | L2 Vlan tag rewrite | ${DUT2} | ${SUB_INT2} | ${TAG_REWRITE_METHOD}
 
 | Configure L2 tag rewrite method on interface

resources/libraries/robot/overlay/srv6.robot
@@ -263,7 +263,7 @@
 | | ... | VPP Add IP Neighbor
 | | ... | ${dut1} | ${DUT1_${int}2}[0] | ${dut2_if1_ip6}
 | | ... | ${DUT2_${int}1_mac}[0]
-| | Run Keyword Unless | ${dut2_status}
+| | Run Keyword If | not ${dut2_status}
 | | ... | VPP Add IP Neighbor
 | | ... | ${dut1} | ${DUT1_${int}2}[0] | ${dut2_if1_ip6} | ${TG_pf2_mac}[0]
 | | Run Keyword If | ${dut2_status}
@@ -405,7 +405,7 @@
 | | ... | VPP Add IP Neighbor
 | | ... | ${dut1} | ${DUT1_${int}2}[0] | ${dut2_if1_ip6}
 | | ... | ${DUT2_${int}1_mac}[0]
-| | Run Keyword Unless | ${dut2_status}
+| | Run Keyword If | not ${dut2_status}
 | | ... | VPP Add IP Neighbor
 | | ... | ${dut1} | ${DUT1_${int}2}[0] | ${dut2_if1_ip6} | ${TG_pf2_mac}[0]
 | | Run Keyword If | ${dut2_status}

resources/libraries/robot/performance/performance_utils.robot
@@ -330,7 +330,7 @@
 | | ... | \| Send ramp-up traffic \|
 | |
 | | ${ramp_up_rate} = | Get Ramp Up Rate
-| | Run Keyword Unless | ${ramp_up_rate} > 0.0 | Fail | Ramp up rate missing!
+| | Run Keyword If | ${ramp_up_rate} <= 0.0 | Fail | Ramp up rate missing!
 | | ${ramp_up_duration} = | Get Ramp Up Duration
 | | ${ppta} = | Get Packets Per Transaction Aggregated
 | | ${traffic_directions} = | Get Traffic Directions

resources/libraries/robot/shared/test_setup.robot
@@ -49,4 +49,4 @@
 | | ... | Additional Setup for tests which uses namespace.
 | |
 | | ${trex_running}= | Is Trex Running | ${tg}
-| | Run Keyword Unless | ${trex_running} | Startup Trex | ${tg} | ${osi_layer}
+| | Run Keyword If | not ${trex_running} | Startup Trex | ${tg} | ${osi_layer}