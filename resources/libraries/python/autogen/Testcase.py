7d5e0ed72a5ac1de7ee4e12331e82c8701c28c08
Vratko Polak <vrpolak@cisco.com>
2022-08-26 15:02:52 +0200

    feat(autogen): add skipping by topology and job
    The decision to skip a suite is based on compatibility between
    suite template content (e.g. robot tags as substrings), filename,
    job name (if any) and topology reserved (if any).
    
    Notably, information sources which still do not affect suite generation
    include trigger tag expressions and job specs.
    
    + Bootstraps generate suites only after reserving a testbed.
    + Rules previously hardcoded in select_tags bash function are gone.
     + TRex suites are now correctly skipped for NIC without TG-TG link.
    + Methodology updated.
    
    Change-Id: Ic96853dc29b7b26ebf00ca3f9f4d8cc5e0d3141a
    Signed-off-by: Vratko Polak <vrpolak@cisco.com>

--- resources/libraries/python/autogen/Testcase.py
+++ resources/libraries/python/autogen/Testcase.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2021 Cisco and/or its affiliates.
+# Copyright (c) 2022 Cisco and/or its affiliates.
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at:
@@ -103,7 +103,7 @@ class Testcase:
         # TODO: Choose a better frame size identifier for streamed protocols
         # (TCP, QUIC, SCTP, ...) where DUT (not TG) decides frame size.
         if u"tcphttp" in suite_id:
-            if u"rps" or u"cps" in suite_id:
+            if u"rps" in suite_id or u"cps" in suite_id:
                 template_string = f'''
 | ${{frame_str}}-${{cores_str}}c-{suite_id}
 | | [Tags] | ${{frame_str}} | ${{cores_str}}C
