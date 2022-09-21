2db267e9c38aa0027627cf19afd670e1a6daff53
Vratko Polak <vrpolak@cisco.com>
2022-09-21 19:33:11 +0200

    WiP: Investigating and fixing ipsec density
    Ticket: CSIT-1856
    
    Change-Id: Iaf6cf9f06242647e5b1d2da2f4802a114ddc8c36
    Signed-off-by: Vratko Polak <vrpolak@cisco.com>

--- resources/libraries/python/ContainerUtils.py
+++ resources/libraries/python/ContainerUtils.py
@@ -430,7 +430,7 @@ class ContainerManager:
 
         if dut == u"DUT1":
             tnl_local_ip = f"{local_ip_base}.{nf_instance + 100}"
-            tnl_remote_ip = f"{local_ip_base}.{nf_instance}"
+            tnl_remote_ip = f"{local_ip_base}.{nf_instance + 100}"
             remote_ip_base = kwargs[u"dut1_if1_ip4"].rsplit(u".", 1)[0]
             tg_pf_ip4 = kwargs[u"tg_pf1_ip4"]
             tg_pf_mac = kwargs[u"tg_pf1_mac"]
@@ -439,7 +439,7 @@ class ContainerManager:
             l_mac2 = 18
             r_mac = 1
         else:
-            tnl_local_ip = f"{local_ip_base}.{nf_instance}"
+            tnl_local_ip = f"{local_ip_base}.{nf_instance + 100}"
             tnl_remote_ip = f"{local_ip_base}.{nf_instance + 100}"
             remote_ip_base = kwargs[u"dut2_if2_ip4"].rsplit(u".", 1)[0]
             tg_pf_ip4 = kwargs[u"tg_pf2_ip4"]
@@ -465,12 +465,13 @@ class ContainerManager:
             tnl_local_ip=tnl_local_ip,
             tnl_remote_ip=tnl_remote_ip,
             tnl_remote_mac=f"02:02:00:00:{r_mac:02X}:{(nf_instance - 1):02X}",
-            remote_ip=f"{remote_ip_base}.{nf_instance}"
+            remote_ip=f"{remote_ip_base}.{nf_instance + 10}"
         )
         self.engine.execute(
             f"cat {kwargs['guest_dir']}/ipsec_create_tunnel_cnf_"
             f"{dut}_{nf_instance}.config >> /tmp/running.exec"
         )
+        self.engine.execute(f"cat /tmp/running.exec")
 
     def _configure_vpp_pipeline_ip4(self, **kwargs):
         """Configure VPP in pipeline topology with ip4.
@@ -525,6 +526,12 @@ class ContainerManager:
         for container in self.containers:
             self.engine.container = self.containers[container]
             self.engine.destroy()
+
+    def cat_log_in_all_containers(self):
+        """Cat log in all containers."""
+        for container in self.containers:
+            self.engine.container = self.containers[container]
+            self.engine.cat_log()
 
 
 class ContainerEngine:
@@ -618,6 +625,10 @@ class ContainerEngine:
         self.verify_vppctl()
         self.adjust_privileges()
         self.verify_vpp_papi()
+
+    def cat_log(self, filepath=u"/tmp/vppd.log"):
+        """Execute cat in container."""
+        self.execute(u"cat {filepath}")
 
     # TODO Rewrite to use the VPPUtil.py functionality and remove this.
     def verify_vppctl(self, retries=120, retry_wait=1):
