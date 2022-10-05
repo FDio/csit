fafab05b772d0f535d0fb822cbbe5fad43d6b868
pmikus <peter.mikus@protonmail.ch>
2022-10-05 14:57:18 +0200

    feat(trex): Bump T-Rex to v3.00
    Signed-off-by: pmikus <peter.mikus@protonmail.ch>
    Change-Id: I9aa88fb094b03888fc30d84edc1deaa406075db4

--- GPL/tools/trex/trex_stl_assert.py
+++ GPL/tools/trex/trex_stl_assert.py
@@ -38,7 +38,7 @@ Functionality:
 import sys
 
 sys.path.insert(
-    0, u"/opt/trex-core-2.97/scripts/automation/trex_control_plane/interactive/"
+    0, u"/opt/trex-core-3.00/scripts/automation/trex_control_plane/interactive/"
 )
 from trex.stl.api import STLClient, STLError
 
