9226d2cd1d88c4a6a4a80b40d53d6719accb02e5
Vratko Polak <vrpolak@cisco.com>
2022-10-07 10:46:58 +0200

    feat(papi): wiping remains of VAT
    - Some older documentation files are not updated yet.
    
    Change-Id: If1717e12308f0e2e76c10024e6eebe68ddeddc9f
    Signed-off-by: Vratko Polak <vrpolak@cisco.com>

--- resources/libraries/python/SetupFramework.py
+++ resources/libraries/python/SetupFramework.py
@@ -259,9 +259,9 @@ def cleanup_node(node, results=None, logs=None):
 class SetupFramework:
     """Setup suite run on topology nodes.
 
-    Many VAT/CLI based tests need the scripts at remote hosts before executing
-    them. This class packs the whole testing directory and copies it over
-    to all nodes in topology under /tmp/
+    Some tests need the scripts at remote hosts before executing them.
+    This class packs the whole testing directory and copies it over
+    to all nodes in topology under /tmp/.
     """
 
     @staticmethod
