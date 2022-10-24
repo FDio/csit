9f1e0ea4f3dd4eacb33f419e8b467d256fac2b29
Vratko Polak <vrpolak@cisco.com>
2022-10-24 19:48:24 +0200

    feat(MLRsearch): use multiple short trials
    From API point of view, this is a BREAKING_CHANGE.
    
    - Next round of testing after added RFC2544 compatibility.
    - Doscrings outdated or missing.
    
    Change-Id: I853afca479ebb6fb7590d710426b8f5b6fd629c6
    Signed-off-by: Vratko Polak <vrpolak@cisco.com>

--- resources/libraries/python/MLRsearch/discrete_interval.py
+++ resources/libraries/python/MLRsearch/discrete_interval.py
@@ -11,38 +11,69 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-"""Module defining MeasurementInterval class."""
+"""Module defining DiscreteInterval class."""
 
 from dataclasses import dataclass, field
 
 from .discrete_load import DiscreteLoad
 from .discrete_width import DiscreteWidth
-from .measurement_interval import MeasurementInterval
 
 
 @dataclass
-class DiscreteInterval(MeasurementInterval):
+class DiscreteInterval:
     """Interval class with more computations available.
-
-    Contrary to MeasurementInterval,
-    intervals of this class cannot be zero-width.
 
     Along discrete form of width,
     a MLR specific way for halving the interval is also included.
     """
 
+    low_end: DiscreteLoad
+    """Measurement for the lower (or higher) load."""
+    high_end: DiscreteLoad
+    """Measurement for the higher (or lower) load."""
     discrete_width: DiscreteWidth = field(init=False, repr=False)
     """Discrete width between intended loads (high_end minus low_end)."""
 
     def __post_init__(self):
-        """Compute secondary quantities.
+        """Sort bounds by intended load, compute secondary quantities.
 
         :raises RuntimeError: If a result used non-rounded load.
         """
-        super().__post_init__()
-        self.discrete_width = (
-            self.high_end.discrete_load - self.low_end.discrete_load
-        )
+        if not self.low_end.is_round:
+            raise RuntimeError(f"Non-round low end: {self.low_end!r}")
+        if not self.high_end.is_round:
+            raise RuntimeError(f"Non-round low end: {self.high_end!r}")
+        if self.low_end > self.high_end:
+            self.low_end, self.high_end = self.high_end, self.low_end
+        self.discrete_width = self.high_end - self.low_end
+
+    def width_in_goals(self, goal: DiscreteWidth) -> float:
+        """Return relative width as a logaritmic multiple of given goal.
+
+        Integer forms are used for computation, safe as end loads are rounded.
+        The result is in float, as load rounding is to int, not to width goal.
+
+        :param goal: high_end bound (float) times this (float) is the goal
+            difference between high_end bound and low_end bound.
+        :type relative_width_goal: DiscreteWidth
+        :returns: Current width as (logarithmic) multiple of goal width.
+        :rtype: float
+        """
+        return (int(self.high_end) - int(self.low_end)) / int(goal)
+
+    @property
+    def relative_width(self) -> float:
+        """Return relative width computed from float values.
+
+        This is useful for debugging final MLR results,
+        which may have zero-width intervals
+        (or even different rounding instances).
+
+        :returns: Difference of float load divided by high end load.
+        :rtype: float
+        """
+        difference = float(self.high_end) - float(self.low_end)
+        return difference / float(self.high_end)
 
     def middle(self, width_goal: DiscreteWidth) -> DiscreteLoad:
         """Return new intended load (discrete form) in the middle.
@@ -70,7 +101,7 @@ class DiscreteInterval(MeasurementInterval):
             raise RuntimeError(f"Do not halve small enough interval: {self!r}")
         if int_self == 2 * int_goal:
             # Even split, return here simplifies the while loop below.
-            return self.low_end.discrete_load + width_goal
+            return self.low_end + width_goal
         if int_self < 2 * int_goal:
             # This can only happen when int_goal >= 2.
             # In this case, we do not have good enough split at this width goal,
@@ -80,7 +111,7 @@ class DiscreteInterval(MeasurementInterval):
                 return self.middle(width_goal=width_goal.half_rounded_down())
             # Odd int_goal, so this must by the last phase. Do even split.
             lo_width = self.discrete_width.half_rounded_down()
-            return self.low_end.discrete_load + lo_width
+            return self.low_end + lo_width
         hi_width = width_goal
         lo_width = self.discrete_width - hi_width
         # We know lo_width > hi_width because we did not do the even split.
@@ -93,4 +124,4 @@ class DiscreteInterval(MeasurementInterval):
         # Which of the two options is more even? Product decides.
         if int(hi_width) * int(lo_width) < int(hi2_width) * int(lo2_width):
             hi_width, lo_width = hi2_width, lo2_width
-        return self.low_end.discrete_load + lo_width
+        return self.low_end + lo_width
