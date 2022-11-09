5572b37d9b3c6d4c57e6e5b726ce6cc50198d152
pmikus <peter.mikus@protonmail.ch>
2022-11-09 13:22:00 +0000

    feat(cdash): CI Scripts
    Signed-off-by: pmikus <peter.mikus@protonmail.ch>
    Change-Id: I857265b1f63fb795d5efbb312777edebe700a49a

--- csit.infra.dash/do_run.sh
+++ csit.infra.dash/do_run.sh
@@ -2,13 +2,9 @@
 
 set -exuo pipefail
 
-command -v docker-compose || exit 1
+command -v docker || exit 1
 
-#export UID=$(id -u)
-#export GID=$(id -g)
+export UID=$(id -u)
+export GID=$(id -g)
 
-docker-compose run \
-    --user $(id -u):$(id -g) \
-    --publish 5000 \
-    --publish 9001 \
-    dash
+docker compose up --remove-orphans
