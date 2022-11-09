c8fc35dd3af1b463b7a77428b6be6a866d1dd41a
pmikus <peter.mikus@protonmail.ch>
2022-11-09 13:22:00 +0000

    feat(cdash): CI Scripts
    Signed-off-by: pmikus <peter.mikus@protonmail.ch>
    Change-Id: I857265b1f63fb795d5efbb312777edebe700a49a

--- csit.infra.dash/do_release.sh
+++ csit.infra.dash/do_release.sh
@@ -8,5 +8,6 @@ rm -f app.zip
 
 pushd app
 find . -type d -name "__pycache__" -exec rm -rf "{}" \; || true
+find . -type d -name ".webassets-cache" -exec rm -rf "{}" \; || true
 zip -r ../app.zip .
 popd
