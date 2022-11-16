b3b8738af5cec9ee81dd3b91ef80a8d3c1197d84
pmikus <peter.mikus@protonmail.ch>
2022-11-16 14:36:35 +0000

    feat(cdash): Local data
    Signed-off-by: pmikus <peter.mikus@protonmail.ch>
    Change-Id: I25aa1d124c0a4fd039211054e599e542294a4b5d

---
+++ csit.infra.dash/do_certs.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+mkdir -p ./nginx/ssl
+cd ./nginx/ssl
+
+FILE_NAME="subdomains.amazonaws.com"
+
+openssl genrsa -des3 -out CA.key 2048
+
+openssl req -x509 -new -nodes -key CA.key -sha256 -days 8000 -out CA.pem
+
+openssl x509 -in CA.pem -inform PEM -out CA.crt
+
+openssl genrsa -out $FILE_NAME.key 2048
+openssl req -new -key $FILE_NAME.key -out $FILE_NAME.csr
+
+cat > $FILE_NAME.ext << EOF
+authorityKeyIdentifier=keyid,issuer
+basicConstraints=CA:FALSE
+keyUsage = digitalSignature, nonRepudiation, keyEncipherment, dataEncipherment
+subjectAltName = @alt_names
+[alt_names]
+DNS.1 = *.amazonaws.com
+DNS.2 = *.us-east-1.amazonaws.com
+DNS.3 = *.s3.amazonaws.com
+EOF
+
+openssl x509 -req -in $FILE_NAME.csr -CA CA.pem -CAkey CA.key -CAcreateserial -out $FILE_NAME.crt -days 8000 -sha256 -extfile $FILE_NAME.ext
