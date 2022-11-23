52385d7dac5bc46c9b47b64178f0c105ba35ab3f
Vratko Polak <vrpolak@cisco.com>
2022-11-23 17:12:30 +0100

    fix(testpmd): require both state changes
    - Not sure what happens if there are two events for a link.
    
    Ticket: CSIT-1848
    
    Change-Id: I8d1c608e1dbb21ce9751f1e6e032e7ec6206c229
    Signed-off-by: Vratko Polak <vrpolak@cisco.com>

--- resources/libraries/bash/function/dpdk.sh
+++ resources/libraries/bash/function/dpdk.sh
@@ -331,16 +331,23 @@ function dpdk_testpmd_check () {
 
     set -exuo pipefail
 
-    for attempt in {1..60}; do
+    for attempt in {1..120}; do
         echo "Checking if testpmd links state changed, attempt nr ${attempt}"
-        if fgrep "link state change event" screenlog.0; then
-            cat screenlog.0
-            exit 0
+        if fgrep "Port 0: link state change event" screenlog.0; then
+            echo "0: OK"
+        else
+            sleep 1
+            continue
         fi
-        sleep 1
+        if fgrep "Port 1: link state change event" screenlog.0; then
+            echo "1: OK"
+            return 0
+        else
+            sleep 1
+            continue
+        fi
     done
     cat screenlog.0
-
     exit 1
 }
 
