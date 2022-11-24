b83b179d5604fe40413cf9c757e8a5f9235b46b3
pmikus <peter.mikus@protonmail.ch>
2022-11-24 13:27:53 +0000

    feat(model): Remove raw, introduce telemetry
    - Due to divergence from original design path the RAW was never
      consumed. It adds too much code complexity and requires processing
      on both storage and compute. Removing entirely to make modeling
      efficient.
    - log (apparently SSH) section will never be consumed in the way it is
      coded in model. This section is also not part of model schema itself
      due to the point above.
    - Introducing telemetry section that is going to carry telemetry
      items required for CDash.
    
    Signed-off-by: pmikus <peter.mikus@protonmail.ch>
    Change-Id: I7e0256c6c9715de8ee559eed29dce96329aac97d

--- resources/libraries/python/model/export_json.py
+++ resources/libraries/python/model/export_json.py
@@ -13,12 +13,6 @@
 
 """Module tracking json in-memory data and saving it to files.
 
-The current implementation tracks data for raw output,
-and info output is created from raw output on disk (see raw2info module).
-Raw file contains all log items but no derived quantities,
-info file contains only important log items but also derived quantities.
-The overlap between two files is big.
-
 Each test case, suite setup (hierarchical) and teardown has its own file pair.
 
 Validation is performed for output files with available JSON schema.
@@ -29,16 +23,19 @@ as serialization might have introduced subtle errors.
 import datetime
 import os.path
 
+from dateutil.parser import parse
 from robot.api import logger
 from robot.libraries.BuiltIn import BuiltIn
 
 from resources.libraries.python.Constants import Constants
+from resources.libraries.python.jumpavg.AvgStdevStats import AvgStdevStats
 from resources.libraries.python.model.ExportResult import (
     export_dut_type_and_version, export_tg_type_and_version
 )
-from resources.libraries.python.model.mem2raw import write_raw_output
-from resources.libraries.python.model.raw2info import convert_content_to_info
-from resources.libraries.python.model.validate import (get_validators, validate)
+from resources.libraries.python.model.MemDump import write_output
+from resources.libraries.python.model.validate import (
+    get_validators, validate
+)
 
 
 class export_json():
@@ -52,9 +49,39 @@ class export_json():
         Also memorize schema validator instances.
         """
         self.output_dir = BuiltIn().get_variable_value(u"\\${OUTPUT_DIR}", ".")
-        self.raw_file_path = None
-        self.raw_data = None
+        self.file_path = None
+        self.data = None
         self.validators = get_validators()
+
+    def _detect_test_type(self):
+        """Return test_type, as inferred from robot test tags.
+
+        :returns: The inferred test type value.
+        :rtype: str
+        :raises RuntimeError: If the test tags does not contain expected values.
+        """
+        tags = self.data[u"tags"]
+        # First 5 options are specific for VPP tests.
+        if u"DEVICETEST" in tags:
+            test_type = u"device"
+        elif u"LDP_NGINX" in tags:
+            test_type = u"vsap"
+        elif u"HOSTSTACK" in tags:
+            test_type = u"hoststack"
+        elif u"GSO_TRUE" in tags or u"GSO_FALSE" in tags:
+            test_type = u"gso"
+        elif u"RECONF" in tags:
+            test_type = u"reconf"
+        # The remaining 3 options could also apply to DPDK and TRex tests.
+        elif u"SOAK" in tags:
+            test_type = u"soak"
+        elif u"NDRPDR" in tags:
+            test_type = u"ndrpdr"
+        elif u"MRR" in tags:
+            test_type = u"mrr"
+        else:
+            raise RuntimeError(f"Unable to infer test type from tags: {tags}")
+        return test_type
 
     def export_pending_data(self):
         """Write the accumulated data to disk.
@@ -72,30 +99,26 @@ class export_json():
 
         Regardless of whether data was written, it is cleared.
         """
-        if not Constants.EXPORT_JSON or not self.raw_file_path:
-            self.raw_data = None
-            self.raw_file_path = None
+        if not Constants.EXPORT_JSON or not self.file_path:
+            self.data = None
+            self.file_path = None
             return
-        write_raw_output(self.raw_file_path, self.raw_data)
-        # Raw data is going to be cleared (as a sign that raw export succeeded),
+        new_file_path = write_output(self.file_path, self.data)
+        # Data is going to be cleared (as a sign that export succeeded),
         # so this is the last chance to detect if it was for a test case.
-        is_testcase = u"result" in self.raw_data
-        self.raw_data = None
-        # Validation for raw output goes here when ready.
-        info_file_path = convert_content_to_info(self.raw_file_path)
-        self.raw_file_path = None
-        # If "result" is missing from info content,
-        # it could be a bug in conversion from raw test case content,
-        # so instead of that we use the flag detected earlier.
+        is_testcase = u"result" in self.data
+        self.data = None
+        # Validation for output goes here when ready.
+        self.file_path = None
         if is_testcase:
-            validate(info_file_path, self.validators[u"tc_info"])
+            validate(new_file_path, self.validators[u"tc_info"])
 
     def warn_on_bad_export(self):
         """If bad state is detected, log a warning and clean up state."""
-        if self.raw_file_path is not None or self.raw_data is not None:
-            logger.warn(f"Previous export not clean, path {self.raw_file_path}")
-            self.raw_data = None
-            self.raw_file_path = None
+        if self.file_path is not None or self.data is not None:
+            logger.warn(f"Previous export not clean, path {self.file_path}")
+            self.data = None
+            self.file_path = None
 
     def start_suite_setup_export(self):
         """Set new file path, initialize data for the suite setup.
@@ -114,19 +137,19 @@ class export_json():
         suite_id = suite_name.lower().replace(u" ", u"_")
         suite_path_part = os.path.join(*suite_id.split(u"."))
         output_dir = self.output_dir
-        self.raw_file_path = os.path.join(
-            output_dir, suite_path_part, u"setup.raw.json"
+        self.file_path = os.path.join(
+            output_dir, suite_path_part, u"setup.info.json"
         )
-        self.raw_data = dict()
-        self.raw_data[u"version"] = Constants.MODEL_VERSION
-        self.raw_data[u"start_time"] = start_time
-        self.raw_data[u"suite_name"] = suite_name
-        self.raw_data[u"suite_documentation"] = BuiltIn().get_variable_value(
+        self.data = dict()
+        self.data[u"version"] = Constants.MODEL_VERSION
+        self.data[u"start_time"] = start_time
+        self.data[u"suite_name"] = suite_name
+        self.data[u"suite_documentation"] = BuiltIn().get_variable_value(
             u"\\${SUITE_DOCUMENTATION}"
         )
-        # "end_time" and "duration" is added on flush.
-        self.raw_data[u"hosts"] = set()
-        self.raw_data[u"log"] = list()
+        # "end_time" and "duration" are added on flush.
+        self.data[u"hosts"] = set()
+        self.data[u"telemetry"] = list()
 
     def start_test_export(self):
         """Set new file path, initialize data to minimal tree for the test case.
@@ -148,24 +171,24 @@ class export_json():
         suite_id = suite_name.lower().replace(u" ", u"_")
         suite_path_part = os.path.join(*suite_id.split(u"."))
         test_name = BuiltIn().get_variable_value(u"\\${TEST_NAME}")
-        self.raw_file_path = os.path.join(
+        self.file_path = os.path.join(
             self.output_dir, suite_path_part,
-            test_name.lower().replace(u" ", u"_") + u".raw.json"
+            test_name.lower().replace(u" ", u"_") + u".info.json"
         )
-        self.raw_data = dict()
-        self.raw_data[u"version"] = Constants.MODEL_VERSION
-        self.raw_data[u"start_time"] = start_time
-        self.raw_data[u"suite_name"] = suite_name
-        self.raw_data[u"test_name"] = test_name
+        self.data = dict()
+        self.data[u"version"] = Constants.MODEL_VERSION
+        self.data[u"start_time"] = start_time
+        self.data[u"suite_name"] = suite_name
+        self.data[u"test_name"] = test_name
         test_doc = BuiltIn().get_variable_value(u"\\${TEST_DOCUMENTATION}", u"")
-        self.raw_data[u"test_documentation"] = test_doc
-        # "test_type" is added when converting to info.
+        self.data[u"test_documentation"] = test_doc
+        # "test_type" is added on flush.
         # "tags" is detected and added on flush.
         # "end_time" and "duration" is added on flush.
         # Robot status and message are added on flush.
-        self.raw_data[u"result"] = dict(type=u"unknown")
-        self.raw_data[u"hosts"] = set()
-        self.raw_data[u"log"] = list()
+        self.data[u"result"] = dict(type=u"unknown")
+        self.data[u"hosts"] = BuiltIn().get_variable_value(u"\\${hosts}")
+        self.data[u"telemetry"] = list()
         export_dut_type_and_version()
         export_tg_type_and_version()
 
@@ -185,16 +208,16 @@ class export_json():
         suite_name = BuiltIn().get_variable_value(u"\\${SUITE_NAME}")
         suite_id = suite_name.lower().replace(u" ", u"_")
         suite_path_part = os.path.join(*suite_id.split(u"."))
-        self.raw_file_path = os.path.join(
-            self.output_dir, suite_path_part, u"teardown.raw.json"
+        self.file_path = os.path.join(
+            self.output_dir, suite_path_part, u"teardown.info.json"
         )
-        self.raw_data = dict()
-        self.raw_data[u"version"] = Constants.MODEL_VERSION
-        self.raw_data[u"start_time"] = start_time
-        self.raw_data[u"suite_name"] = suite_name
+        self.data = dict()
+        self.data[u"version"] = Constants.MODEL_VERSION
+        self.data[u"start_time"] = start_time
+        self.data[u"suite_name"] = suite_name
         # "end_time" and "duration" is added on flush.
-        self.raw_data[u"hosts"] = set()
-        self.raw_data[u"log"] = list()
+        self.data[u"hosts"] = BuiltIn().get_variable_value(u"\\${hosts}")
+        self.data[u"telemetry"] = list()
 
     def finalize_suite_setup_export(self):
         """Add the missing fields to data. Do not write yet.
@@ -203,7 +226,8 @@ class export_json():
         The write is done at next start (or at the end of global teardown).
         """
         end_time = datetime.datetime.utcnow().strftime(u"%Y-%m-%dT%H:%M:%S.%fZ")
-        self.raw_data[u"end_time"] = end_time
+        self.data[u"hosts"] = BuiltIn().get_variable_value(u"\\${hosts}")
+        self.data[u"end_time"] = end_time
         self.export_pending_data()
 
     def finalize_test_export(self):
@@ -216,12 +240,16 @@ class export_json():
         """
         end_time = datetime.datetime.utcnow().strftime(u"%Y-%m-%dT%H:%M:%S.%fZ")
         message = BuiltIn().get_variable_value(u"\\${TEST_MESSAGE}")
-        status = BuiltIn().get_variable_value(u"\\${TEST_STATUS}")
         test_tags = BuiltIn().get_variable_value(u"\\${TEST_TAGS}")
-        self.raw_data[u"end_time"] = end_time
-        self.raw_data[u"tags"] = list(test_tags)
-        self.raw_data[u"status"] = status
-        self.raw_data[u"message"] = message
+        self.data[u"end_time"] = end_time
+        start_float = parse(self.data[u"start_time"]).timestamp()
+        end_float = parse(self.data[u"end_time"]).timestamp()
+        self.data[u"duration"] = end_float - start_float
+        self.data[u"tags"] = list(test_tags)
+        self.data[u"message"] = message
+        self.process_passed()
+        self.process_test_name()
+        self.process_results()
         self.export_pending_data()
 
     def finalize_suite_teardown_export(self):
@@ -232,5 +260,134 @@ class export_json():
         The write is done at next start (or explicitly for global teardown).
         """
         end_time = datetime.datetime.utcnow().strftime(u"%Y-%m-%dT%H:%M:%S.%fZ")
-        self.raw_data[u"end_time"] = end_time
+        self.data[u"end_time"] = end_time
         self.export_pending_data()
+
+    def process_test_name(self):
+        """Replace raw test name with short and long test name and set
+        test_type.
+
+        Perform in-place edits on the data dictionary.
+        Remove raw suite_name and test_name, they are not published.
+        Return early if the data is not for test case.
+        Insert test ID and long and short test name into the data.
+        Besides suite_name and test_name, also test tags are read.
+
+        Short test name is basically a suite tag, but with NIC driver prefix,
+        if the NIC driver used is not the default one (drv_vfio_pci for VPP
+        tests).
+
+        Long test name has the following form:
+        {nic_short_name}-{frame_size}-{threads_and_cores}-{suite_part}
+        Lookup in test tags is needed to get the threads value.
+        The threads_and_cores part may be empty, e.g. for TRex tests.
+
+        Test ID has form {suite_name}.{test_name} where the two names come from
+        Robot variables, converted to lower case and spaces replaces by
+        undescores.
+
+        Test type is set in an internal function.
+
+        :raises RuntimeError: If the data does not contain expected values.
+        """
+        suite_part = self.data.pop(u"suite_name").lower().replace(u" ", u"_")
+        if u"test_name" not in self.data:
+            # There will be no test_id, provide suite_id instead.
+            self.data[u"suite_id"] = suite_part
+            return
+        test_part = self.data.pop(u"test_name").lower().replace(u" ", u"_")
+        self.data[u"test_id"] = f"{suite_part}.{test_part}"
+        tags = self.data[u"tags"]
+        # Test name does not contain thread count.
+        subparts = test_part.split(u"c-", 1)
+        if len(subparts) < 2 or subparts[0][-2:-1] != u"-":
+            # Physical core count not detected, assume it is a TRex test.
+            if u"--" not in test_part:
+                raise RuntimeError(f"Cores not found for {subparts}")
+            short_name = test_part.split(u"--", 1)[1]
+        else:
+            short_name = subparts[1]
+            # Add threads to test_part.
+            core_part = subparts[0][-1] + u"c"
+            for tag in tags:
+                tag = tag.lower()
+                if len(tag) == 4 and core_part == tag[2:] and tag[1] == u"t":
+                    test_part = test_part.replace(f"-{core_part}-", f"-{tag}-")
+                    break
+            else:
+                raise RuntimeError(
+                    f"Threads not found for {test_part} tags {tags}"
+                )
+        # For long name we need NIC model, which is only in suite name.
+        last_suite_part = suite_part.split(u".")[-1]
+        # Short name happens to be the suffix we want to ignore.
+        prefix_part = last_suite_part.split(short_name)[0]
+        # Also remove the trailing dash.
+        prefix_part = prefix_part[:-1]
+        # Throw away possible link prefix such as "1n1l-".
+        nic_code = prefix_part.split(u"-", 1)[-1]
+        nic_short = Constants.NIC_CODE_TO_SHORT_NAME[nic_code]
+        long_name = f"{nic_short}-{test_part}"
+        # Set test type.
+        test_type = self._detect_test_type()
+        self.data[u"test_type"] = test_type
+        # Remove trailing test type from names (if present).
+        short_name = short_name.split(f"-{test_type}")[0]
+        long_name = long_name.split(f"-{test_type}")[0]
+        # Store names.
+        self.data[u"test_name_short"] = short_name
+        self.data[u"test_name_long"] = long_name
+
+    def process_passed(self):
+        """Process the test status information as boolean.
+
+        Boolean is used to make post processing more efficient.
+        In case the test status is PASS, we will truncate the test message.
+        """
+        status = BuiltIn().get_variable_value(u"\\${TEST_STATUS}")
+        if status is not None:
+            self.data[u"passed"] = (status == u"PASS")
+            if self.data[u"passed"]:
+                # Also truncate success test messages.
+                self.data[u"message"] = u""
+
+    def process_results(self):
+        """Process measured results.
+
+        Results are used to avoid future post processing, making it more
+        efficient to consume.
+        """
+        if u"result" not in self.data:
+            return
+        result_node = self.data[u"result"]
+        result_type = result_node[u"type"]
+        if result_type == u"unknown":
+            # Device or something else not supported.
+            return
+
+        # Compute avg and stdev for mrr.
+        if result_type == u"mrr":
+            rate_node = result_node[u"receive_rate"][u"rate"]
+            stats = AvgStdevStats.for_runs(rate_node[u"values"])
+            rate_node[u"avg"] = stats.avg
+            rate_node[u"stdev"] = stats.stdev
+            return
+
+        # Multiple processing steps for ndrpdr.
+        if result_type != u"ndrpdr":
+            return
+        # Filter out invalid latencies.
+        for which_key in (u"latency_forward", u"latency_reverse"):
+            if which_key not in result_node:
+                # Probably just an unidir test.
+                continue
+            for load in (u"pdr_0", u"pdr_10", u"pdr_50", u"pdr_90"):
+                if result_node[which_key][load][u"max"] <= 0:
+                    # One invalid number is enough to remove all loads.
+                    break
+            else:
+                # No break means all numbers are ok, nothing to do here.
+                continue
+            # Break happened, something is invalid, remove all loads.
+            result_node.pop(which_key)
+        return
