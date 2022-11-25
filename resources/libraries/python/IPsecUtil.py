a6d64e600475ab32823e315f90d5270a2fd14f05
Vratko Polak <vrpolak@cisco.com>
2022-11-25 16:45:16 +0100

    [reopen after 2302] fix(api): use ipsec_spd_entry_add_del_v2
    Testing VPP 22.06 is no longer needed.
    Only one request/reply has been upgraded in 22.10 cycle.
    
    Change-Id: Ibcc089ccbf933c019b5e7188c06ef229e68d39a8
    Signed-off-by: Vratko Polak <vrpolak@cisco.com>

--- resources/libraries/python/IPsecUtil.py
+++ resources/libraries/python/IPsecUtil.py
@@ -927,7 +927,7 @@ class IPsecUtil:
         local_net = ip_network(laddr_range, strict=False)
         remote_net = ip_network(raddr_range, strict=False)
 
-        cmd = u"ipsec_spd_entry_add_del"
+        cmd = u"ipsec_spd_entry_add_del_v2"
         err_msg = f"Failed to add entry to Security Policy Database " \
                   f"{spd_id} on host {node[u'host']}"
 
