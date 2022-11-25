4df54dc031cb61442a0bb855f8b043d42807b480
Vratko Polak <vrpolak@cisco.com>
2022-11-25 12:19:28 +0100

    [reopen] feat(tox): add more checkers related to UTI model
    + Check for json being in sync with yaml.
    + Check for schema being valid according to its metaschema.
    + Fix detection for model version checker.
    + Use safe yaml load to supress the warning.
    - Python scripts left close to schema for now, so no pylint yet.
    
    Change-Id: Ibc1de27d7af448f14a2dd3281e91511d56762c0e
    Signed-off-by: Vratko Polak <vrpolak@cisco.com>

--- docs/model/current/schema/check_yaml2json.py
+++ docs/model/current/schema/check_yaml2json.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2021 Cisco and/or its affiliates.
+# Copyright (c) 2022 Cisco and/or its affiliates.
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at:
@@ -11,17 +11,25 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-"""Utility to convert from .schema.yaml to .schema.json.
+"""Utility compare contents of .schema.yaml and .schema.json.
 
 TODO: Read the input file name from command line argument.
+TODO: Make callable from another working directory.
 """
 
 import glob
 import json
 import yaml
+import sys
 
 
 for filename in glob.glob(u"*.schema.yaml"):
     name = filename[:-5]
-    with open(f"{name}.yaml", u"r") as fin, open(f"{name}.json", u"w") as fout:
-        json.dump(yaml.load(fin.read()), fout, indent=2)
+    with open(f"{name}.yaml", u"rt", encoding="utf-8") as fin:
+        tyaml = json.dumps(yaml.safe_load(fin.read()), indent=2)
+    with open(f"{name}.json", u"rt", encoding="utf-8") as fin:
+        tjson = fin.read()
+    if tyaml != tjson:
+        print(u"Json schema does not match yaml, run fixer!")
+        sys.exit(1)
+print(u"Schemas are in sync.")
