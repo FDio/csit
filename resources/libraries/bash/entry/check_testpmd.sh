2b335db58e5d83aa493643c97d2cafb851251305
Vratko Polak <vrpolak@cisco.com>
2022-12-06 17:52:16 +0100

    fix(testpmd): require two link state changes
    One link state change event is not enough.
    If test passes, it is because the second event
    happened during the test.
    
    Usually, one event per link makes the test pass,
    but once I have seen (on 3n-icx) passing test
    for two events on the same link (zero on the other).
    I assume it is just a reporting error.
    
    Also, more than two events can happen,
    test is passing in that case.
    
    + Check for 120 seconds.
    + Do 10 starts before giving up.
    (Both of those are to improve passrate on 3n-alt.)
    
    Ticket: CSIT-1848
    
    Change-Id: I8d1c608e1dbb21ce9751f1e6e032e7ec6206c229
    Signed-off-by: Vratko Polak <vrpolak@cisco.com>

--- resources/libraries/bash/entry/check_testpmd.sh
+++ resources/libraries/bash/entry/check_testpmd.sh
@@ -13,7 +13,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# Helper functions for starting testpmd.
+# A single check for whether this testpms is ready for traffic.
 
 set -exuo pipefail
 
