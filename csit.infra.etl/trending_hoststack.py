0aecf7d3fb0a0bfc847878c3de6a50a78b9b9df3
pmikus <peter.mikus@protonmail.ch>
2023-01-12 13:17:50 +0000

    feat(etl): rls2302 + hoststack
    Signed-off-by: pmikus <peter.mikus@protonmail.ch>
    Change-Id: I511265b8d38793f74d23f73e434fa241d969b560

--- csit.infra.etl/trending_hoststack.py
+++ csit.infra.etl/trending_hoststack.py
@@ -143,7 +143,7 @@ paths = wr.s3.list_objects(
 
 filtered_paths = [path for path in paths if "daily" in path or "weekly" in path]
 
-out_sdf = process_json_to_dataframe("soak", filtered_paths)
+out_sdf = process_json_to_dataframe("hoststack", filtered_paths)
 out_sdf.show(truncate=False)
 out_sdf.printSchema()
 out_sdf = out_sdf \
