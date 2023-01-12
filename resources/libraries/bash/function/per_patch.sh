10d694f3b768698c62bce45444efdf8eaf694f3c
Vratko Polak <vrpolak@cisco.com>
2023-01-12 14:01:11 +0100

    Experiment: fixes for 37840/5 compilation
    The version of vpp-ext-deps has been bumped in the meantime.
    
    Change-Id: I031493123894dbdd6db54ef92ab610d596e44bfb
    Signed-off-by: Vratko Polak <vrpolak@cisco.com>

--- resources/libraries/bash/function/per_patch.sh
+++ resources/libraries/bash/function/per_patch.sh
@@ -98,6 +98,9 @@ function build_vpp_ubuntu_amd64 () {
              "using build default ($(grep -c ^processor /proc/cpuinfo))."
     fi
 
+    git branch "build" || die
+    git branch --set-upstream-to=master || die
+    git pull -r || die
     make UNATTENDED=y pkg-verify || die "VPP build with make pkg-verify failed."
     echo "* VPP ${1-} BUILD SUCCESSFULLY COMPLETED" || {
         die "Argument not found."
