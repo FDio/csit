a03c63b745e2d33a0a19813af808cc58731c423a
pmikus <peter.mikus@protonmail.ch>
2023-01-16 14:04:51 +0000

    fix(etl): Split memory usage
    Signed-off-by: pmikus <peter.mikus@protonmail.ch>
    Change-Id: Icd6b6bbff65ef70fd85bf3595c1d2a1a06655416

--- fdio.infra.terraform/terraform-nomad-pyspark-etl/fdio/versions.tf
+++ fdio.infra.terraform/terraform-nomad-pyspark-etl/fdio/versions.tf
@@ -7,11 +7,11 @@ terraform {
   required_providers {
     nomad = {
       source  = "hashicorp/nomad"
-      version = ">= 1.4.16"
+      version = ">= 1.4.19"
     }
     vault = {
-      version = ">= 3.2.1"
+      version = ">= 3.12.0"
     }
   }
-  required_version = ">= 1.1.4"
+  required_version = ">= 1.3.7"
 }
