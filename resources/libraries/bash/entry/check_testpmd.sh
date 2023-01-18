2130290de65eb5ecf55325e360f480a00af39a00
Vratko Polak <vrpolak@cisco.com>
2023-01-18 13:53:01 +0100

    fix(testpmd): require two link state changes
    One link state change event is not enough.
    If test passes, it is because the second event
    happened during the test.
    
    Usually, one event per link makes the test pass,
    but once I have seen (on 3n-icx) passing test
    for two events on the same link (zero on the other).
    I assume it is just a reporting error.
    
    Also, more than two events can happen,
    test is passing in that case.
    
    + Do multiple restars, with increasing check time.
     + Currently 5 starts, wait time increasing from 10 to ~20 minutes.
     + Both to improve passrate on 3n-alt.
     - Some testbed+NIC combinations still fail 100%.
      + But those are not used for trending / release report testing.
    + Move the (wrong) robot tag computation out of the loop.
    + A temporary list "duts" is used to save checks of node type.
    + Minor formatting and docstring improvements.
     + Mostly towards "black" style.
    
    Ticket: CSIT-1848
    
    Change-Id: I8d1c608e1dbb21ce9751f1e6e032e7ec6206c229
    Signed-off-by: Vratko Polak <vrpolak@cisco.com>

--- resources/libraries/bash/entry/check_testpmd.sh
+++ resources/libraries/bash/entry/check_testpmd.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-# Copyright (c) 2022 Cisco and/or its affiliates.
+# Copyright (c) 2023 Cisco and/or its affiliates.
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at:
@@ -13,7 +13,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# Helper functions for starting testpmd.
+# Check to see whether testpmd is fully ready for traffic.
 
 set -exuo pipefail
 
@@ -36,4 +36,4 @@ source "${BASH_FUNCTION_DIR}/common.sh" || {
 }
 source "${BASH_FUNCTION_DIR}/dpdk.sh" || die "Source failed."
 common_dirs || die
-dpdk_testpmd_check || die
+dpdk_testpmd_check "${1}" || die
