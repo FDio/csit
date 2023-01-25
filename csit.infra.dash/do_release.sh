5104703e39dbde99ff3b7a482e57514fc34206aa
pmikus <peter.mikus@protonmail.ch>
2023-01-25 12:54:28 +0000

    feat(terraform): Refactor application version deploy
    Signed-off-by: pmikus <peter.mikus@protonmail.ch>
    Change-Id: I065a018745a82762c816de96d8a69116416aa0ce

--- csit.infra.dash/do_release.sh
+++ csit.infra.dash/do_release.sh
@@ -11,3 +11,19 @@ find . -type d -name "__pycache__" -exec rm -rf "{}" \;
 find . -type d -name ".webassets-cache" -exec rm -rf "{}" \;
 zip -r ../app.zip .
 popd
+
+pushd "../fdio.infra.terraform/"
+pushd "terraform-aws-fdio-csit-dash-app-base"
+export BUILD_ID=41
+export TF_VAR_application_version="${BUILD_ID-}"
+export TF_LOG=INFO
+terraform validate
+terraform init
+terraform apply -no-color -auto-approve
+application_version="$(terraform output application_version)"
+popd
+popd
+
+#aws --region eu-central-1 elasticbeanstalk update-environment \
+#    --environment-name fdio-csit-dash-env \
+#    --version-label "${application_version}"
---
+++ fdio.infra.terraform/terraform-aws-fdio-csit-dash-app-base/output.tf
@@ -0,0 +1,3 @@
+output "application_version" {
+  value = module.elastic_beanstalk_application_version.application_version
+}
