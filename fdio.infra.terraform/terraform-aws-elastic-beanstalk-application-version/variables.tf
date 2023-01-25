61cf4d230c3bd88cd0a6f3782796cc3949485b72
pmikus <peter.mikus@protonmail.ch>
2023-01-25 12:54:28 +0000

    feat(terraform): Refactor application version deploy
    Signed-off-by: pmikus <peter.mikus@protonmail.ch>
    Change-Id: I065a018745a82762c816de96d8a69116416aa0ce

--- fdio.infra.terraform/terraform-aws-elastic-beanstalk-application-version/variables.tf
+++ fdio.infra.terraform/terraform-aws-elastic-beanstalk-application-version/variables.tf
@@ -1,3 +1,9 @@
+variable "application_bucket" {
+  description = "The name of the bucket."
+  type        = string
+  default     = "elasticbeanstalk-eu-central-1"
+}
+
 variable "application_description" {
   description = "Short description of the Application Version."
   type        = string
@@ -5,13 +11,19 @@ variable "application_description" {
 }
 
 variable "application_name" {
-  description = "Name of the Beanstalk Application the version is associated."
+  description = "Name of the Beanstalk Application."
   type        = string
-  default     = "Beanstalk"
+  default     = "beanstalk"
 }
 
-variable "application_version_name" {
-  description = "Unique name for the this Application Version."
+variable "application_name_version" {
+  description = "Version of the Beanstalk Application."
   type        = string
-  default     = "Beanstalk Version"
+  default     = "beanstalk-1"
+}
+
+variable "application_source" {
+  description = "The source file with application code."
+  type        = string
+  default     = "app.zip"
 }
