d39570ec2dcbc3dec1c1a14043e7a573aa52de03
pmikus <peter.mikus@protonmail.ch>
2023-01-25 12:54:28 +0000

    feat(terraform): Refactor application version deploy
    Signed-off-by: pmikus <peter.mikus@protonmail.ch>
    Change-Id: I065a018745a82762c816de96d8a69116416aa0ce

---
+++ fdio.infra.terraform/terraform-aws-elastic-beanstalk-application-version/output.tf
@@ -0,0 +1,3 @@
+output "application_version" {
+  value = aws_elastic_beanstalk_application_version.application_version.name
+}
--- fdio.infra.terraform/terraform-aws-elastic-beanstalk-application/versions.tf
+++ fdio.infra.terraform/terraform-aws-elastic-beanstalk-application/versions.tf
@@ -2,8 +2,8 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 4.3.0"
+      version = "~> 4.51.0"
     }
   }
-  required_version = ">= 1.1.4"
+  required_version = ">= 1.3.7"
 }
