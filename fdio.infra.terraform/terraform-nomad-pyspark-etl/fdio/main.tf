aee95eeda8e735b21c7fc188de213b4c4dc76031
pmikus <peter.mikus@protonmail.ch>
2023-02-03 11:15:18 +0000

    feat(terraform): Optimize ETL execution
    Signed-off-by: pmikus <peter.mikus@protonmail.ch>
    Change-Id: I11dfe8f642c6d565d39b1e0fbc41361460a42320

--- fdio.infra.terraform/terraform-nomad-pyspark-etl/fdio/main.tf
+++ fdio.infra.terraform/terraform-nomad-pyspark-etl/fdio/main.tf
@@ -6,7 +6,7 @@ data "vault_generic_secret" "fdio_docs" {
   path = "kv/secret/data/etl/fdio_docs"
 }
 
-module "etl" {
+module "etl-stats" {
   providers = {
     nomad = nomad.yul1
   }
@@ -20,4 +20,90 @@ module "etl" {
   out_aws_default_region    = data.vault_generic_secret.fdio_docs.data["region"]
   cron                      = "@daily"
   datacenters               = ["yul1"]
+  job_name                  = "etl-stats"
+}
+
+module "etl-trending-hoststack" {
+  providers = {
+    nomad = nomad.yul1
+  }
+  source = "../"
+
+  aws_access_key_id         = data.vault_generic_secret.fdio_logs.data["access_key"]
+  aws_secret_access_key     = data.vault_generic_secret.fdio_logs.data["secret_key"]
+  aws_default_region        = data.vault_generic_secret.fdio_logs.data["region"]
+  out_aws_access_key_id     = data.vault_generic_secret.fdio_docs.data["access_key"]
+  out_aws_secret_access_key = data.vault_generic_secret.fdio_docs.data["secret_key"]
+  out_aws_default_region    = data.vault_generic_secret.fdio_docs.data["region"]
+  cron                      = "@daily"
+  datacenters               = ["yul1"]
+  job_name                  = "etl-trending-hoststack"
+}
+
+module "etl-trending-mrr" {
+  providers = {
+    nomad = nomad.yul1
+  }
+  source = "../"
+
+  aws_access_key_id         = data.vault_generic_secret.fdio_logs.data["access_key"]
+  aws_secret_access_key     = data.vault_generic_secret.fdio_logs.data["secret_key"]
+  aws_default_region        = data.vault_generic_secret.fdio_logs.data["region"]
+  out_aws_access_key_id     = data.vault_generic_secret.fdio_docs.data["access_key"]
+  out_aws_secret_access_key = data.vault_generic_secret.fdio_docs.data["secret_key"]
+  out_aws_default_region    = data.vault_generic_secret.fdio_docs.data["region"]
+  cron                      = "@daily"
+  datacenters               = ["yul1"]
+  job_name                  = "etl-trending-mrr"
+}
+
+module "etl-trending-ndrpdr" {
+  providers = {
+    nomad = nomad.yul1
+  }
+  source = "../"
+
+  aws_access_key_id         = data.vault_generic_secret.fdio_logs.data["access_key"]
+  aws_secret_access_key     = data.vault_generic_secret.fdio_logs.data["secret_key"]
+  aws_default_region        = data.vault_generic_secret.fdio_logs.data["region"]
+  out_aws_access_key_id     = data.vault_generic_secret.fdio_docs.data["access_key"]
+  out_aws_secret_access_key = data.vault_generic_secret.fdio_docs.data["secret_key"]
+  out_aws_default_region    = data.vault_generic_secret.fdio_docs.data["region"]
+  cron                      = "@daily"
+  datacenters               = ["yul1"]
+  job_name                  = "etl-trending-ndrpdr"
+}
+
+module "etl-iterative-rls2302" {
+  providers = {
+    nomad = nomad.yul1
+  }
+  source = "../"
+
+  aws_access_key_id         = data.vault_generic_secret.fdio_logs.data["access_key"]
+  aws_secret_access_key     = data.vault_generic_secret.fdio_logs.data["secret_key"]
+  aws_default_region        = data.vault_generic_secret.fdio_logs.data["region"]
+  out_aws_access_key_id     = data.vault_generic_secret.fdio_docs.data["access_key"]
+  out_aws_secret_access_key = data.vault_generic_secret.fdio_docs.data["secret_key"]
+  out_aws_default_region    = data.vault_generic_secret.fdio_docs.data["region"]
+  cron                      = "@daily"
+  datacenters               = ["yul1"]
+  job_name                  = "etl-iterative-rls2302"
+}
+
+module "etl-coverage-rls2302" {
+  providers = {
+    nomad = nomad.yul1
+  }
+  source = "../"
+
+  aws_access_key_id         = data.vault_generic_secret.fdio_logs.data["access_key"]
+  aws_secret_access_key     = data.vault_generic_secret.fdio_logs.data["secret_key"]
+  aws_default_region        = data.vault_generic_secret.fdio_logs.data["region"]
+  out_aws_access_key_id     = data.vault_generic_secret.fdio_docs.data["access_key"]
+  out_aws_secret_access_key = data.vault_generic_secret.fdio_docs.data["secret_key"]
+  out_aws_default_region    = data.vault_generic_secret.fdio_docs.data["region"]
+  cron                      = "@daily"
+  datacenters               = ["yul1"]
+  job_name                  = "etl-coverage-rls2302"
 }
