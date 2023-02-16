6b1c3d4394ce671333362f9cbc7e7e591e7a8002
Vratko Polak <vrpolak@cisco.com>
2023-02-16 17:20:46 +0100

    fix(bootstrap): per-patch perf: apply zn2 trex fix
    + per_patch_device does not need that call (yet).
    
    Change-Id: I4c85a038962a2cfea3fa91bb4a3c37a8348132ca
    Signed-off-by: Vratko Polak <vrpolak@cisco.com>

--- resources/libraries/bash/entry/per_patch_perf.sh
+++ resources/libraries/bash/entry/per_patch_perf.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-# Copyright (c) 2021 Cisco and/or its affiliates.
+# Copyright (c) 2023 Cisco and/or its affiliates.
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at:
@@ -60,6 +60,7 @@ archive_tests || die
 reserve_and_cleanup_testbed || die
 select_tags || die
 compose_pybot_arguments || die
+set_environment_variables || die
 # Support for interleaved measurements is kept for future.
 iterations=1 # 8
 for ((iter=0; iter<iterations; iter++)); do
