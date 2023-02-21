a7bce2cad514beb27f9d8c31536f187f3b691b07
Vratko Polak <vrpolak@cisco.com>
2023-02-21 14:11:51 +0100

    Experiment: more debugs on qemu start
    Trying to find any differences between cases tsh is slow vs fast.
    
    + More retries when tsh is slow so it does not fail beore the debugs.
    
    Change-Id: If457bf34927a6ba868f85989993b35332414279a
    Signed-off-by: Vratko Polak <vrpolak@cisco.com>

--- resources/libraries/python/QemuUtils.py
+++ resources/libraries/python/QemuUtils.py
@@ -508,6 +508,8 @@ class QemuUtils:
                     sleep(1)
                     continue
                 for qemu_cpu, host_cpu in zip(qemu_cpus, host_cpus):
+                    command = f"cat /proc/{qemu_cpu}/numa_maps"
+                    exec_cmd(self._node, command, sudo=True)
                     command = f"taskset -pc {host_cpu} {qemu_cpu}"
                     message = f"QEMU: Set affinity failed " \
                         f"on {self._node[u'host']}!"
@@ -605,7 +607,7 @@ class QemuUtils:
         except AttributeError:
             self._wait_default()
 
-    def _wait_default(self, retries=60):
+    def _wait_default(self, retries=600):
         """Wait until QEMU with VPP is booted.
 
         :param retries: Number of retries.
