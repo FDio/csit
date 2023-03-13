8573d8db0f4698d7bc301c392da0d408833d808a
Tibor Frank <tifrank@cisco.com>
2023-03-13 10:13:57 +0100

    CDash: Add comparison tables
    Signed-off-by: Tibor Frank <tifrank@cisco.com>
    Change-Id: I8ce9e670721e1fdb1f297b3bfb8f0d8ffb916713

---
+++ csit.infra.dash/app/cdash/comparisons/comparisons.py
@@ -0,0 +1,50 @@
+# Copyright (c) 2023 Cisco and/or its affiliates.
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at:
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Instantiate the Report Dash application.
+"""
+import dash
+import pandas as pd
+
+from ..utils.constants import Constants as C
+from .layout import Layout
+
+
+def init_comparisons(
+        server,
+        data_iterative: pd.DataFrame
+    ) -> dash.Dash:
+    """Create a Plotly Dash dashboard.
+
+    :param server: Flask server.
+    :type server: Flask
+    :returns: Dash app server.
+    :rtype: Dash
+    """
+
+    dash_app = dash.Dash(
+        server=server,
+        routes_pathname_prefix=C.COMP_ROUTES_PATHNAME_PREFIX,
+        external_stylesheets=C.EXTERNAL_STYLESHEETS,
+        title=C.COMP_TITLE
+    )
+
+    layout = Layout(
+        app=dash_app,
+        data_iterative=data_iterative,
+        html_layout_file=C.HTML_LAYOUT_FILE
+    )
+    dash_app.index_string = layout.html_layout
+    dash_app.layout = layout.add_content()
+
+    return dash_app.server
