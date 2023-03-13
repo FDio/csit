0fc5aff9887fa7a3125c71d0662475a3f9a763ba
Tibor Frank <tifrank@cisco.com>
2023-03-13 10:13:57 +0100

    CDash: Add comparison tables
    Signed-off-by: Tibor Frank <tifrank@cisco.com>
    Change-Id: I8ce9e670721e1fdb1f297b3bfb8f0d8ffb916713

--- csit.infra.dash/app/cdash/utils/constants.py
+++ csit.infra.dash/app/cdash/utils/constants.py
@@ -142,6 +142,7 @@ class Constants:
 
     NORM_FREQUENCY = 2.0  # [GHz]
     FREQUENCY = {  # [GHz]
+        "1n-aws": 1.000,
         "2n-aws": 1.000,
         "2n-dnv": 2.000,
         "2n-clx": 2.300,
@@ -289,6 +290,18 @@ class Constants:
 
     # Default name of downloaded file with selected data.
     REPORT_DOWNLOAD_FILE_NAME = "iterative_data.csv"
+
+    ############################################################################
+    # Comparisons.
+
+    # The title.
+    COMP_TITLE = "Per Release Performance Comparisons"
+
+    # The pathname prefix for the application.
+    COMP_ROUTES_PATHNAME_PREFIX = "/comparisons/"
+
+    # Default name of downloaded file with selected data.
+    COMP_DOWNLOAD_FILE_NAME = "comparison_data.csv"
 
     ############################################################################
     # Statistics.
