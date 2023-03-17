dd0d2f1de10ca4c9c125df3f00c5f99cad8475ca
pmikus <peter.mikus@protonmail.ch>
2023-03-17 13:24:42 +0000

    feat(bootstrap): CDash CI
    Signed-off-by: pmikus <peter.mikus@protonmail.ch>
    Change-Id: Ia9f5b1c9036b9f09c4d9bb041a41e9b49d2ead7f

---
+++ resources/libraries/bash/function/eb_version.sh
@@ -0,0 +1,145 @@
+#!/usr/bin/env bash
+
+# Copyright (c) 2023 Cisco and/or its affiliates.
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at:
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+set -exuo pipefail
+
+
+function die_on_error () {
+
+    # Source this fragment if you want to abort on any failure.
+    #
+    # Functions called:
+    # - die - Print to stderr and exit.
+
+    set -exuo pipefail
+
+    if [[ "${CODE_EXIT_STATUS}" != "0" ]]; then
+        die "Failed to generate docs!" "${CODE_EXIT_STATUS}"
+    fi
+}
+
+
+function eb_version_deploy () {
+
+    # Deploy Elastic Beanstalk CDash content.
+    #
+    # Variable read:
+    # - ${CSIT_DIR} - CSIT main directory.
+    # Variables set:
+    # - CODE_EXIT_STATUS - Exit status of report generation.
+    # Functions called:
+    # - die - Print to stderr and exit.
+
+    set -exuo pipefail
+
+    eb_version_verify || die "Failed to call Elastic Beanstalk verify!"
+    terraform_apply || die "Failed to call terraform apply!"
+
+    TERRAFORM_OUTPUT_VAR="application_version"
+    export application_version="$(terraform_output)"
+
+    #aws --region eu-central-1 elasticbeanstalk update-environment \
+    #    --environment-name fdio-csit-dash-env \
+    #    --version-label "${application_version}"
+}
+
+
+function eb_version_verify () {
+
+    # Verify Elastic Beanstalk CDash integrity.
+    #
+    # Variable read:
+    # - ${CSIT_DIR} - CSIT main directory.
+    # Functions called:
+    # - die - Print to stderr and exit.
+
+    set -exuo pipefail
+
+    if ! installed zip; then
+        die "Please install zip!"
+    fi
+
+    pushd "${CSIT_DIR}"/csit.infra.dash || die "Pushd failed!"
+    pushd app || die "Pushd failed!"
+    find . -type d -name "__pycache__" -exec rm -rf "{}" \;
+    find . -type d -name ".webassets-cache" -exec rm -rf "{}" \;
+    zip -r ../app.zip . || die "Compress failed!"
+    popd || die "Popd failed!"
+    popd || die "Popd failed!"
+
+    TERRAFORM_MODULE_DIR="terraform-aws-fdio-csit-dash-app-base"
+
+    export TF_VAR_application_version="1.${BUILD_ID:-50}"
+    hugo_init_modules || die "Failed to call Hugo initialize!"
+    hugo_build_site || die "Failed to call Hugo build!"
+    terraform_init || die "Failed to call terraform init!"
+    terraform_validate || die "Failed to call terraform validate!"
+}
+
+
+function generate_report () {
+
+    # Generate report content.
+    #
+    # Variable read:
+    # - ${TOOLS_DIR} - Path to existing resources subdirectory "tools".
+    # - ${GERRIT_BRANCH} - Gerrit branch used for release tagging.
+    # Variables set:
+    # - CODE_EXIT_STATUS - Exit status of report generation.
+    # Functions called:
+    # - die - Print to stderr and exit.
+
+    set -exuo pipefail
+
+    pushd "${TOOLS_DIR}"/presentation || die "Pushd failed!"
+
+    # Set default values in config array.
+    typeset -A CFG
+    typeset -A DIR
+
+    DIR[WORKING]="_tmp"
+
+    # Create working directories.
+    mkdir "${DIR[WORKING]}" || die "Mkdir failed!"
+
+    export PYTHONPATH=`pwd`:`pwd`/../../../ || die "Export failed!"
+
+    all_options=("pal.py")
+    all_options+=("--specification" "specifications/report")
+    all_options+=("--release" "${GERRIT_BRANCH:-master}")
+    all_options+=("--week" $(date "+%V"))
+    all_options+=("--logging" "INFO")
+    all_options+=("--force")
+
+    set +e
+    python "${all_options[@]}"
+    CODE_EXIT_STATUS="$?"
+    set -e
+}
+
+function installed () {
+
+    # Check if the given utility is installed. Fail if not installed.
+    #
+    # Arguments:
+    # - ${1} - Utility to check.
+    # Returns (implicitly):
+    # - 0 - If command is installed.
+    # - 1 - If command is not installed.
+
+    set -exuo pipefail
+
+    command -v "${1}"
+}
---
+++ resources/libraries/bash/function/hugo.sh
@@ -0,0 +1,91 @@
+#!/usr/bin/env bash
+
+# Copyright (c) 2023 Cisco and/or its affiliates.
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at:
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+set -exuo pipefail
+
+
+function hugo_build_site () {
+
+    # Build site via Hugo.
+    #
+    # Variable read:
+    # - ${CSIT_DIR} - CSIT main directory.
+    # Functions called:
+    # - die - Print to stderr and exit.
+
+    if ! installed hugo; then
+        die "Please install Hugo!"
+    fi
+
+    pushd "${CSIT_DIR}"/docs || die "Pushd failed!"
+    hugo || die "Failed to run Hugo build!"
+    popd || die "Popd failed!"
+}
+
+
+function hugo_init_modules () {
+
+    # Initialize Hugo modules.
+    #
+    # Variable read:
+    # - ${CSIT_DIR} - CSIT main directory.
+    # Functions called:
+    # - die - Print to stderr and exit.
+
+    if ! installed hugo; then
+        #die "Please install Hugo!"
+        go_version="go1.20.2.linux-arm64.tar.gz"
+        go_url="https://go.dev/dl"
+        wget "${go_url}/${go_version}"
+        rm -rf "/usr/local/go"
+        tar -C "/usr/local" -xzf "go1.20.2.linux-arm64.tar.gz"
+        export PATH=$PATH:/usr/local/go/bin
+        hugo_install || die "Failed to install Hugo!"
+        terraform_install || die "Failed to install Terraform!"
+    fi
+
+    pushd "${CSIT_DIR}"/docs || die "Pushd failed!"
+    hugo mod get -u || die "Failed to run Hugo mod!"
+    popd || die "Popd failed!"
+}
+
+
+function hugo_install () {
+
+    # Install Hugo Extended.
+
+    hugo_version="v0.111.3/hugo_extended_0.111.3_linux-arm64.deb"
+    hugo_url="https://github.com/gohugoio/hugo/releases/download"
+    hugo_link="${hugo_url}/${hugo_version}"
+    wget -O "hugo.deb" "${hugo_link}" || die "Failed to install Hugo!"
+    dpkg -i "hugo.deb" || die "Failed to install Hugo!"
+    rm "hugo.deb" || die "Failed to install Hugo!"
+}
+
+
+function installed () {
+
+    # Check if the given utility is installed. Fail if not installed.
+    #
+    # Arguments:
+    # - ${1} - Utility to check.
+    # Returns (implicitly):
+    # - 0 - If command is installed.
+    # - 1 - If command is not installed.
+
+    set -exuo pipefail
+
+    command -v "${1}"
+}
