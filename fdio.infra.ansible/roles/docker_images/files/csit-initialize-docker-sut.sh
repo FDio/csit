d5ab20b646800445794d4348c4ad1519deec76f9
pmikus <peter.mikus@protonmail.ch>
2023-03-31 13:53:02 +0000

    feat(infra): 2vn-spr
    Signed-off-by: pmikus <peter.mikus@protonmail.ch>
    Change-Id: Icb7b6124dcba7bb57c18ceb91120284f6fe02c2e

---
+++ fdio.infra.ansible/roles/docker_images/files/csit-initialize-docker-sut.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+case "${1:-start}" in
+    "start" )
+        # Run SUT
+        for cnt in $(seq 1 ${2:-1}); do
+            docker network create --driver bridge csit-nw-sut${cnt} || true
+            # If the IMAGE is not already loaded then docker run will pull the
+            # IMAGE, and all image dependencies, before it starts the container.
+            dcr_image="base-ubuntu2204:local"
+            # Run the container in the background and print the new container
+            # ID.
+            dcr_stc_params="--detach=true "
+            # Give extended privileges to this container. A "privileged"
+            # container is given access to all devices and able to run nested
+            # containers.
+            dcr_stc_params+="--privileged "
+            # Publish all exposed ports to random ports on the host interfaces.
+            dcr_stc_params+="--publish 600${cnt}:2222 "
+            # Automatically remove the container when it exits.
+            dcr_stc_params+="--rm "
+            # Size of /dev/shm.
+            dcr_stc_params+="--shm-size 4G "
+            # Mount vfio to be able to bind to see binded interfaces. We cannot
+            # use --device=/dev/vfio as this does not see newly binded
+            # interfaces.
+            dcr_stc_params+="--volume /dev:/dev "
+            # Mount /opt/boot/ where VM kernel and initrd are located.
+            dcr_stc_params+="--volume /opt:/opt "
+            # Mount host hugepages for VMs.
+            dcr_stc_params+="--volume /dev/hugepages:/dev/hugepages "
+
+            params=(${dcr_stc_params} --name csit-sut-"${cnt}" "${dcr_image}")
+            docker run --network=csit-nw-sut${cnt} "${params[@]}"
+        done
+        ;;
+    "stop" )
+        docker rm --force $(docker ps --all --quiet --filter name=csit)
+        docker network rm $(docker network ls --filter name=csit --quiet)
+        ;;
+esac
