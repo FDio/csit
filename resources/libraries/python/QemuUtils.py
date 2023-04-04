7b318be719fcbe9a09b1f3210954e1b78e181c96
Juraj Linkeš <juraj.linkes@pantheon.tech>
2023-04-04 14:23:49 +0200

    improvement(qemu): vm resource allocation
    Also increase timeout.
    
    Change-Id: Id22aac07700067278b23ffffc81403f96a317375
    Signed-off-by: Juraj Linkeš <juraj.linkes@pantheon.tech>

--- resources/libraries/python/QemuUtils.py
+++ resources/libraries/python/QemuUtils.py
@@ -1,4 +1,5 @@
-# Copyright (c) 2022 Cisco and/or its affiliates.
+# Copyright (c) 2022-2023 Cisco and/or its affiliates.
+# Copyright (c) 2023 PANTHEON.tech s.r.o.
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at:
@@ -12,7 +13,7 @@
 # limitations under the License.
 
 """QEMU utilities library."""
-
+import datetime
 import json
 
 from re import match
@@ -22,6 +23,7 @@ from time import sleep
 from robot.api import logger
 
 from resources.libraries.python.Constants import Constants
+from resources.libraries.python.CpuUtils import CpuUtils
 from resources.libraries.python.DpdkUtil import DpdkUtil
 from resources.libraries.python.DUTSetup import DUTSetup
 from resources.libraries.python.OptionString import OptionString
@@ -41,11 +43,12 @@ class QemuUtils:
     ROBOT_LIBRARY_SCOPE = u"TEST CASE"
 
     def __init__(
-            self, node, qemu_id=1, smp=1, mem=512, vnf=None,
+            self, node, numa_node, qemu_id=1, smp=1, mem=512, vnf=None,
             img=Constants.QEMU_VM_IMAGE, page_size=u""):
         """Initialize QemuUtil class.
 
         :param node: Node to run QEMU on.
+        :param numa_node: The NUMA node id on which to start the VM.
         :param qemu_id: QEMU identifier.
         :param smp: Number of virtual SMP units (cores).
         :param mem: Amount of memory.
@@ -53,6 +56,7 @@ class QemuUtils:
         :param img: QEMU disk image or kernel image path.
         :param page_size: Hugepage Size.
         :type node: dict
+        :type numa_node: int
         :type qemu_id: int
         :type smp: int
         :type mem: int
@@ -60,6 +64,7 @@ class QemuUtils:
         :type img: str
         :type page_size: str
         """
+        self._numa_node = numa_node
         self._nic_id = 0
         self._node = node
         self._arch = Topology.get_node_arch(self._node)
@@ -604,8 +609,9 @@ class QemuUtils:
             getattr(self, f'_wait_{self._opt["vnf"]}')()
         except AttributeError:
             self._wait_default()
+        self._qemu_vm_boot_time = datetime.datetime.now()
 
-    def _wait_default(self, retries=60):
+    def _wait_default(self, retries=120):
         """Wait until QEMU with VPP is booted.
 
         :param retries: Number of retries.
@@ -726,7 +732,12 @@ class QemuUtils:
         :returns: VM node info.
         :rtype: dict
         """
+        smt_used = CpuUtils.is_smt_enabled(self._node[u"cpuinfo"])
+        numa_cpus = CpuUtils.cpu_list_per_node(self._node, self._numa_node, smt_used)
         cmd_opts = OptionString()
+        cmd_opts.add(u"taskset")
+        cmd_opts.add_with_value(u"-c",
+                                u",".join((str(numa_cpu) for numa_cpu in numa_cpus)))
         cmd_opts.add(f"{Constants.QEMU_BIN_PATH}/qemu-system-{self._arch}")
         cmd_opts.extend(self._params)
         message = f"QEMU: Start failed on {self._node[u'host']}!"
@@ -736,6 +747,8 @@ class QemuUtils:
                 int(self._opt.get(u"mem"))
             )
 
+            self._qemu_start_time = datetime.datetime.now()
+
             exec_cmd_no_error(
                 self._node, cmd_opts, timeout=300, sudo=True, message=message
             )
@@ -743,6 +756,10 @@ class QemuUtils:
         except RuntimeError:
             self.qemu_kill_all()
             raise
+
+        logger.console(f"Node {self._node[u'host']} VM boot time: "
+                       f"{self._qemu_vm_boot_time - self._qemu_start_time}")
+
         return self._vm_info
 
     def qemu_kill(self):
