1188b9ab062ea30c315cf9a265ae5ef70aaea7d4
Vratko Polak <vrpolak@cisco.com>
2023-04-20 15:58:17 +0200

    fix(cdash): do not report failures as regressions
    The previous implementation of classify_anomalies
    took care to treat NaN as zero (classified as outlier).
    
    But it turns out we want to always skip over NaN values.
    This makes no difference for graphs and fixes news.
    (No code was using the outlier possibility.)
    
    + Minor improvement to variable names and code comments.
    
    Change-Id: I224cfd0e20f55d1cdccf5d091afac3e21429b871
    Signed-off-by: Vratko Polak <vrpolak@cisco.com>

--- csit.infra.dash/app/cdash/utils/utils.py
+++ csit.infra.dash/app/cdash/utils/utils.py
@@ -34,50 +34,50 @@ from ..utils.url_processing import url_encode
 def classify_anomalies(data):
     """Process the data and return anomalies and trending values.
 
-    Gather data into groups with average as trend value.
+    Ignore (skip over) data values that are NaN
+    (failed tests should not introduce a performance anomaly).
+
+    Gather data values into groups with average as trend value.
     Decorate values within groups to be normal,
     the first value of changed average as a regression, or a progression.
 
     :param data: Full data set with unavailable samples replaced by nan.
-    :type data: OrderedDict
+    :type data: Mapping
     :returns: Classification and trend values
     :rtype: 3-tuple, list of strings, list of floats and list of floats
     """
-    # NaN means something went wrong.
-    # Use 0.0 to cause that being reported as a severe regression.
-    bare_data = [0.0 if isnan(sample) else sample for sample in data.values()]
+    values = [sample for sample in data.values() if not isnan(sample)]
+    if not values:
+        return list(), list(), list()
     # TODO: Make BitCountingGroupList a subclass of list again?
-    group_list = classify(bare_data).group_list
+    group_list = classify(values).group_list
     group_list.reverse()  # Just to use .pop() for FIFO.
     classification = list()
     avgs = list()
     stdevs = list()
     active_group = None
-    values_left = 0
+    group_samples_left = 0
     avg = 0.0
     stdv = 0.0
-    for sample in data.values():
-        if isnan(sample):
-            classification.append("outlier")
-            avgs.append(sample)
-            stdevs.append(sample)
-            continue
-        if values_left < 1 or active_group is None:
-            values_left = 0
-            while values_left < 1:  # Ignore empty groups (should not happen).
+    for sample in values:
+        if group_samples_left < 1 or active_group is None:
+            # Start emptying the next group.
+            group_samples_left = 0
+            while group_samples_left < 1:  # Ignore empty (should not happen).
                 active_group = group_list.pop()
-                values_left = len(active_group.run_list)
+                group_samples_left = len(active_group.run_list)
             avg = active_group.stats.avg
             stdv = active_group.stats.stdev
             classification.append(active_group.comment)
             avgs.append(avg)
             stdevs.append(stdv)
-            values_left -= 1
+            group_samples_left -= 1
             continue
+        # We have not emptied the active group yet.
         classification.append("normal")
         avgs.append(avg)
         stdevs.append(stdv)
-        values_left -= 1
+        group_samples_left -= 1
     return classification, avgs, stdevs
 
 
