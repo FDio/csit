c2c65a8aaa571bf89733a4cdc7ace52c26efc9f3
Vratko Polak <vrpolak@cisco.com>
2023-04-24 16:31:21 +0200

    Sim example for IETF 116
    Uncovers an error (no raise just suboptimal).
    I forgot what the exact issue is, so git-added log.txt file.
    
    Change-Id: Iacacc2e81ed843e363baf9890e44ce1311bb2889
    Signed-off-by: Vratko Polak <vrpolak@cisco.com>

--- resources/tools/search_simulators/manual_tests.py
+++ resources/tools/search_simulators/manual_tests.py
@@ -15,21 +15,20 @@ import dataclasses
 import logging
 import sys
 
-#from KeyboardMeasurer import KeyboardMeasurer as measurer
-#from RandomMeasurer import RandomMeasurer as measurer
-#from PromilesMeasurer import PromilesMeasurer as measurer
-#from SingleLossMeasurer import SingleLossMeasurer as measurer
-#from TwoPhaseMeasurer import TwoPhaseMeasurer as measurer
+##from RandomMeasurer import RandomMeasurer as measurer
+##from PromilesMeasurer import PromilesMeasurer as measurer
+##from SingleLossMeasurer import SingleLossMeasurer as measurer
+##from TwoPhaseMeasurer import TwoPhaseMeasurer as measurer
 from StretchMeasurer import StretchMeasurer as measurer
+#from KeyboardMeasurer import KeyboardMeasurer as measurer
 from LoggingMeasurer import LoggingMeasurer as infologging
 from TimeTrackingMeasurer import TimeTrackingMeasurer as tracking
 
 logging.basicConfig(level=getattr(logging, "DEBUG"))
 
 # 0.75 is not a good value for twophase, but it found a bug, and it is good for singleloss.
-p = tracking(infologging(measurer(offsets={30.0: 1e6}, mrr=1e8, spread=5e6)))
-
-print(u"optimized search, stretch measurer, printing")
+p = tracking(infologging(measurer(mrr=1e5, spread=8e3)))
+#p = tracking(infologging(measurer()))
 
 from resources.libraries.python.MLRsearch import (
     MultipleLossRatioSearch as my_search, Config, Criteria, Criterion
@@ -37,37 +36,27 @@ from resources.libraries.python.MLRsearch import (
 criteria = list()
 criteria.append(Criterion(
     loss_ratio=0.0,
-    exceed_ratio=0.0,
-    relative_width=1e-2,
-    single_duration_min=2.0,
-    single_duration_max=5.0,
-    sum_duration_min=11.0,
-    intermediate_phases=1,
-    expansion_coefficient=8,
-))
-criteria.append(Criterion(
-    loss_ratio=0.0,
-    exceed_ratio=0.8,
-    relative_width=3e-3,
+    exceed_ratio=0.75,
+    relative_width=1e-1,
     single_duration_min=1.0,
-    single_duration_max=3.5,
-    sum_duration_min=61.0,
+    single_duration_max=10.0,
+    sum_duration_min=100.0,
     intermediate_phases=2,
-    expansion_coefficient=4,
-))
-criteria.append(Criterion(
-    loss_ratio=0.005,
-    exceed_ratio=0.1,
-    relative_width=1e-3,
-    single_duration_min=1.6,
-    single_duration_max=8.0,
-    sum_duration_min=31.0,
-    intermediate_phases=3,
     expansion_coefficient=2,
 ))
+#criteria.append(Criterion(
+#    loss_ratio=0.005,
+#    exceed_ratio=0.0,
+#    relative_width=1e-3,
+#    single_duration_min=1.0,
+#    single_duration_max=3.0,
+#    sum_duration_min=27.0,
+#    intermediate_phases=3,
+#    expansion_coefficient=2,
+#))
 config = Config()
-config.min_load = 1e4
-config.max_load = 1e9
+config.min_load = 1e3
+config.max_load = 1e6
 config.criteria = Criteria(criteria)
 algo = my_search(config)
 
