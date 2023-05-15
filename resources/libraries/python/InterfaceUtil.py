FDio/csit

commit d74acc4db2987d9ac40aae10be8453c922a16731

fix(core): Unbind only when needed
Signed-off-by: pmikus <peter.mikus@protonmail.ch>
Change-Id: I7b7b8744fac6669a696253acb94e7a5d713fdb5b

resources/libraries/python/InterfaceUtil.py
@@ -1884,12 +1884,20 @@ class InterfaceUtil:
                 node, pf_dev, state=u"up"
             )
 
-            DUTSetup.pci_vf_driver_unbind(node, pf_pci_addr, vf_id)
-            DUTSetup.pci_vf_driver_bind(node, pf_pci_addr, vf_id, uio_driver)
+            vf_pci_addr = DUTSetup.get_virtfn_pci_addr(node, pf_pci_addr, vf_id)
+            current_driver = DUTSetup.get_pci_dev_driver(
+                node, vf_pci_addr.replace(":", r"\:")
+            )
+            if current_driver and current_driver != uio_driver:
+                DUTSetup.pci_vf_driver_unbind(
+                    node, pf_pci_addr, vf_id
+                )
+            DUTSetup.pci_vf_driver_bind(
+                node, pf_pci_addr, vf_id, uio_driver
+            )
 
             # Add newly created ports into topology file
             vf_ifc_name = f"{ifc_key}_vif"
-            vf_pci_addr = DUTSetup.get_virtfn_pci_addr(node, pf_pci_addr, vf_id)
             vf_ifc_key = Topology.add_new_port(node, vf_ifc_name)
             Topology.update_interface_name(
                 node, vf_ifc_key, vf_ifc_name+str(vf_id+1)

commit 6eeb36b8fc5841d9f9b9567b9b6c430bc8e35c67

fix(InterfaceUtil): fix VF driver binding
If the correct driver is already bound, do not try to bind it again.

Ticket: CSIT-1913
Fixes: d74acc4db2987d9ac40aae10be8453c922a16731

Change-Id: Ia957a221b9d02bd64f54f13042f17307eb8a40b7
Signed-off-by: Vratko Polak <vrpolak@cisco.com>

resources/libraries/python/InterfaceUtil.py
@@ -1888,13 +1888,14 @@ class InterfaceUtil:
             current_driver = DUTSetup.get_pci_dev_driver(
                 node, vf_pci_addr.replace(":", r"\:")
             )
-            if current_driver and current_driver != uio_driver:
-                DUTSetup.pci_vf_driver_unbind(
-                    node, pf_pci_addr, vf_id
+            if current_driver != uio_driver:
+                if current_driver:
+                    DUTSetup.pci_vf_driver_unbind(
+                        node, pf_pci_addr, vf_id
+                    )
+                DUTSetup.pci_vf_driver_bind(
+                    node, pf_pci_addr, vf_id, uio_driver
                 )
-            DUTSetup.pci_vf_driver_bind(
-                node, pf_pci_addr, vf_id, uio_driver
-            )
 
             # Add newly created ports into topology file
             vf_ifc_name = f"{ifc_key}_vif"