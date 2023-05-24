d164bef0373edfd2b6cc7d4aaa27b928065df3e5
pmikus <peter.mikus@protonmail.ch>
2023-05-24 13:12:36 +0000

    feat(etl): 2306
    Signed-off-by: pmikus <peter.mikus@protonmail.ch>
    Change-Id: I54e80bc78f6deb5d2774a2ddd7b3b4153ffb106b

--- csit.infra.etl/iterative_soak_rls2306.py
+++ csit.infra.etl/iterative_soak_rls2306.py
@@ -141,7 +141,7 @@ paths = wr.s3.list_objects(
     ignore_empty=True
 )
 
-filtered_paths = [path for path in paths if "report-iterative-2302" in path]
+filtered_paths = [path for path in paths if "report-iterative-2306" in path]
 
 out_sdf = process_json_to_dataframe("soak", filtered_paths)
 out_sdf.printSchema()
@@ -154,7 +154,7 @@ out_sdf = out_sdf \
 try:
     wr.s3.to_parquet(
         df=out_sdf.toPandas(),
-        path=f"s3://{S3_DOCS_BUCKET}/csit/parquet/iterative_rls2302",
+        path=f"s3://{S3_DOCS_BUCKET}/csit/parquet/iterative_rls2306",
         dataset=True,
         partition_cols=["test_type", "year", "month", "day"],
         compression="snappy",
