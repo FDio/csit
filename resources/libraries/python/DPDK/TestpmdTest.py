19ad18806775f20327e10e2c3e1b6ee2366c8f37
Vratko Polak <vrpolak@cisco.com>
2023-06-12 17:08:10 +0200

    fix(dpdk): check testpmd also with mellanox nics
    + Fix some pylint issues related to f" and u'.
    
    Change-Id: Ifeb7ec4317d8403364512343d4162ab4f5941656
    Signed-off-by: Vratko Polak <vrpolak@cisco.com>

--- resources/libraries/python/DPDK/TestpmdTest.py
+++ resources/libraries/python/DPDK/TestpmdTest.py
@@ -13,7 +13,6 @@
 
 """This module exists to start testpmd on topology nodes."""
 
-from robot.api import logger
 from robot.libraries.BuiltIn import BuiltIn
 
 from resources.libraries.python.Constants import Constants
@@ -102,7 +101,7 @@ class TestpmdTest:
             all_ready = True
             for dut in duts:
                 if1 = topology_info[f"{dut}_pf1"][0]
-                if not TestpmdTest.is_testpmd_ready(nodes[dut], if1):
+                if not TestpmdTest.is_testpmd_ready(nodes[dut]):
                     all_ready = False
                     break
             for dut in duts:
@@ -111,7 +110,7 @@ class TestpmdTest:
                 return
             # Even if one app is ready, we need to restart both
             # to confirm link state on both DUTs again.
-        raise RuntimeError(f"Testpmd failed to start properly.")
+        raise RuntimeError("Testpmd failed to start properly.")
 
     @staticmethod
     def start_testpmd(
@@ -184,7 +183,7 @@ class TestpmdTest:
             exec_cmd_no_error(node, command, timeout=5, message=message)
 
     @staticmethod
-    def is_testpmd_ready(node, if1):
+    def is_testpmd_ready(node):
         """Execute the testpmd check on the DUT node.
 
         See description in dpdk_testpmd_check bash function for more details.
@@ -194,12 +193,7 @@ class TestpmdTest:
         :returns: False if testpmd is not ready for traffic yet.
         :rtype: bool
         """
-        nic_model = node["interfaces"][if1]["model"]
-        logger.debug(f"NIC model: {nic_model}")
-        if nic_model in ("Mellanox-CX7VEAT", "Mellanox-CX6DX"):
-            logger.debug("Skipping check for mellanox NICs.")
-            return True
         command = f"{Constants.REMOTE_FW_DIR}/{Constants.RESOURCES_LIB_SH}"
-        command += f"/entry/check_testpmd.sh"
+        command += "/entry/check_testpmd.sh"
         return_code, _, _ = exec_cmd(node, command)
         return return_code == 0
