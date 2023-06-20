5f7b6a038789778d7cf6e28b81a7161ba86f432b
Yulong Pei <yulong.pei@intel.com>
2023-06-20 07:30:42 +0000

    Add IPSec and wireguard hwasync test cases to 3nb-spr job specs
    Also add 10000tnlhwasync int mode and 4tnlhwasync policy mode test suites
    in order to compare sw crypto and QAT crypto and IPsec int mode and
    policy mode performance.
    
    Signed-off-by: xinfeng zhao <xinfengx.zhao@intel.com>
    Signed-off-by: Yulong Pei <yulong.pei@intel.com>
    Change-Id: I0aed06a0c75c1a545720649d3561a0aa5a861db2
    Signed-off-by: Yulong Pei <yulong.pei@intel.com>

--- resources/libraries/bash/function/common.sh
+++ resources/libraries/bash/function/common.sh
@@ -1052,7 +1052,6 @@ function select_tags () {
             test_tag_array+=("!ipsechw")
             ;;
         *"3nb-spr"*)
-            test_tag_array+=("!ipsechw")
             ;;
         *"3n-tsh"*)
             test_tag_array+=("!drv_avf")
