b7c74300b6ec3473bad62668fc420a8557a73dca
Vratko Polak <vrpolak@cisco.com>
2023-06-28 15:41:25 +0200

    style(dpdk): apply black to dpdk libraries
    Used black 23.3.0 with all defaults (so line length 88, not 80).
    
    Separated from subsequent edits to make them more readable.
    
    Change-Id: I3f64bdf66b41a17992816f33c88a1effbf23f026
    Signed-off-by: Vratko Polak <vrpolak@cisco.com>

--- resources/libraries/python/DPDK/L3fwdTest.py
+++ resources/libraries/python/DPDK/L3fwdTest.py
@@ -26,14 +26,18 @@ NB_PORTS = 2
 
 
 class L3fwdTest:
-    """This class start l3fwd on topology nodes and check if properly started.
-    """
+    """This class start l3fwd on topology nodes and check if properly started."""
 
     @staticmethod
     def start_l3fwd_on_all_duts(
-            nodes: dict, topology_info: dict, phy_cores: int,
-            rx_queues: int = None, jumbo_frames: bool = False,
-            rxd: int = None, txd: int = None) -> None:
+        nodes: dict,
+        topology_info: dict,
+        phy_cores: int,
+        rx_queues: int = None,
+        jumbo_frames: bool = False,
+        rxd: int = None,
+        txd: int = None,
+    ) -> None:
         """
         Execute the l3fwd on all dut nodes.
 
@@ -55,43 +59,56 @@ class L3fwdTest:
         :raises RuntimeError: If bash return code is not 0.
         """
         cpu_count_int = dp_count_int = int(phy_cores)
-        dp_cores = cpu_count_int+1
+        dp_cores = cpu_count_int + 1
         tg_flip = topology_info[f"tg_if1_pci"] > topology_info[f"tg_if2_pci"]
         for node in nodes:
-            if u"DUT" in node:
+            if "DUT" in node:
                 compute_resource_info = CpuUtils.get_affinity_vswitch(
-                    nodes, node, phy_cores, rx_queues=rx_queues,
-                    rxd=rxd, txd=txd
+                    nodes,
+                    node,
+                    phy_cores,
+                    rx_queues=rx_queues,
+                    rxd=rxd,
+                    txd=txd,
                 )
                 if dp_count_int > 1:
-                    BuiltIn().set_tags('MTHREAD')
+                    BuiltIn().set_tags("MTHREAD")
                 else:
-                    BuiltIn().set_tags('STHREAD')
-                BuiltIn().set_tags(
-                    f"{dp_count_int}T{cpu_count_int}C"
-                )
+                    BuiltIn().set_tags("STHREAD")
+                BuiltIn().set_tags(f"{dp_count_int}T{cpu_count_int}C")
 
-                cpu_dp = compute_resource_info[u"cpu_dp"]
-                rxq_count_int = compute_resource_info[u"rxq_count_int"]
+                cpu_dp = compute_resource_info["cpu_dp"]
+                rxq_count_int = compute_resource_info["rxq_count_int"]
                 if1 = topology_info[f"{node}_pf1"][0]
                 if2 = topology_info[f"{node}_pf2"][0]
                 L3fwdTest.start_l3fwd(
-                    nodes, nodes[node], if1=if1, if2=if2, lcores_list=cpu_dp,
-                    nb_cores=dp_count_int, queue_nums=rxq_count_int,
-                    jumbo_frames=jumbo_frames, tg_flip=tg_flip
+                    nodes,
+                    nodes[node],
+                    if1=if1,
+                    if2=if2,
+                    lcores_list=cpu_dp,
+                    nb_cores=dp_count_int,
+                    queue_nums=rxq_count_int,
+                    jumbo_frames=jumbo_frames,
+                    tg_flip=tg_flip,
                 )
         for node in nodes:
-            if u"DUT" in node:
+            if "DUT" in node:
                 for i in range(3):
                     try:
                         L3fwdTest.check_l3fwd(nodes[node])
                         break
                     except RuntimeError:
                         L3fwdTest.start_l3fwd(
-                            nodes, nodes[node], if1=if1, if2=if2,
-                            lcores_list=cpu_dp, nb_cores=dp_count_int,
-                            queue_nums=rxq_count_int, jumbo_frames=jumbo_frames,
-                            tg_flip=tg_flip
+                            nodes,
+                            nodes[node],
+                            if1=if1,
+                            if2=if2,
+                            lcores_list=cpu_dp,
+                            nb_cores=dp_count_int,
+                            queue_nums=rxq_count_int,
+                            jumbo_frames=jumbo_frames,
+                            tg_flip=tg_flip,
                         )
                 else:
                     message = f"Failed to start l3fwd at node {node}"
@@ -99,9 +116,16 @@ class L3fwdTest:
 
     @staticmethod
     def start_l3fwd(
-            nodes: dict, node: dict, if1: str, if2: str, lcores_list: str,
-            nb_cores: str, queue_nums: str, jumbo_frames: bool,
-            tg_flip: bool) -> None:
+        nodes: dict,
+        node: dict,
+        if1: str,
+        if2: str,
+        lcores_list: str,
+        nb_cores: str,
+        queue_nums: str,
+        jumbo_frames: bool,
+        tg_flip: bool,
+    ) -> None:
         """
         Execute the l3fwd on the dut_node.
 
@@ -129,22 +153,21 @@ class L3fwdTest:
         :type jumbo_frames: bool
         :type tg_flip: bool
         """
-        if node[u"type"] == NodeType.DUT:
+        if node["type"] == NodeType.DUT:
             adj_mac0, adj_mac1, if_pci0, if_pci1 = L3fwdTest.get_adj_mac(
                 nodes, node, if1, if2, tg_flip
             )
 
-            lcores = [int(item) for item in lcores_list.split(u",")]
+            lcores = [int(item) for item in lcores_list.split(",")]
 
             # prepare the port config param
             nb_cores = int(nb_cores)
             index = 0
-            port_config = ''
+            port_config = ""
             for port in range(0, NB_PORTS):
                 for queue in range(0, int(queue_nums)):
                     index = 0 if nb_cores == 1 else index
-                    port_config += \
-                        f"({port}, {queue}, {lcores[index % NB_PORTS]}),"
+                    port_config += f"({port}, {queue}, {lcores[index % NB_PORTS]}),"
                     index += 1
 
             if jumbo_frames:
@@ -155,10 +178,10 @@ class L3fwdTest:
                     eal_pci_whitelist1=if_pci1,
                     eal_in_memory=True,
                     pmd_config=f"\\\"{port_config.rstrip(u',')}\\\"",
-                    pmd_eth_dest_0=f"\\\"0,{adj_mac0}\\\"",
-                    pmd_eth_dest_1=f"\\\"1,{adj_mac1}\\\"",
+                    pmd_eth_dest_0=f'\\"0,{adj_mac0}\\"',
+                    pmd_eth_dest_1=f'\\"1,{adj_mac1}\\"',
                     pmd_parse_ptype=True,
-                    pmd_max_pkt_len=jumbo_frames
+                    pmd_max_pkt_len=jumbo_frames,
                 )
             else:
                 l3fwd_args = DpdkUtil.get_l3fwd_args(
@@ -168,13 +191,15 @@ class L3fwdTest:
                     eal_pci_whitelist1=if_pci1,
                     eal_in_memory=True,
                     pmd_config=f"\\\"{port_config.rstrip(u',')}\\\"",
-                    pmd_eth_dest_0=f"\\\"0,{adj_mac0}\\\"",
-                    pmd_eth_dest_1=f"\\\"1,{adj_mac1}\\\"",
-                    pmd_parse_ptype=True
+                    pmd_eth_dest_0=f'\\"0,{adj_mac0}\\"',
+                    pmd_eth_dest_1=f'\\"1,{adj_mac1}\\"',
+                    pmd_parse_ptype=True,
                 )
 
-            command = f"{Constants.REMOTE_FW_DIR}/{Constants.RESOURCES_LIB_SH}"\
-                f"/entry/run_l3fwd.sh \"{l3fwd_args} -P -L -p 0x3\""
+            command = (
+                f"{Constants.REMOTE_FW_DIR}/{Constants.RESOURCES_LIB_SH}"
+                f'/entry/run_l3fwd.sh "{l3fwd_args} -P -L -p 0x3"'
+            )
             message = f"Failed to execute l3fwd test at node {node['host']}"
             exec_cmd_no_error(node, command, timeout=1800, message=message)
 
@@ -187,15 +212,16 @@ class L3fwdTest:
         :type node: dict
         :raises RuntimeError: If the script "check_l3fwd.sh" fails.
         """
-        if node[u"type"] == NodeType.DUT:
-            command = f"{Constants.REMOTE_FW_DIR}/{Constants.RESOURCES_LIB_SH}"\
+        if node["type"] == NodeType.DUT:
+            command = (
+                f"{Constants.REMOTE_FW_DIR}/{Constants.RESOURCES_LIB_SH}"
                 f"/entry/check_l3fwd.sh"
+            )
             message = "L3fwd not started properly"
             exec_cmd_no_error(node, command, timeout=1800, message=message)
 
     @staticmethod
-    def get_adj_mac(
-            nodes: dict, node: dict, if1: str, if2: str, tg_flip: bool) -> None:
+    def get_adj_mac(nodes: dict, node: dict, if1: str, if2: str, tg_flip: bool) -> None:
         """
         Get adjacency MAC addresses of the DUT node.
 
@@ -233,9 +259,9 @@ class L3fwdTest:
         if dut_flip:
             if_key0, if_key1 = if_key1, if_key0
             if tg_flip:
-                L3fwdTest.patch_l3fwd(node, u"patch_l3fwd_flip_routes")
+                L3fwdTest.patch_l3fwd(node, "patch_l3fwd_flip_routes")
         elif not tg_flip:
-            L3fwdTest.patch_l3fwd(node, u"patch_l3fwd_flip_routes")
+            L3fwdTest.patch_l3fwd(node, "patch_l3fwd_flip_routes")
 
         adj_node0, adj_if_key0 = Topology.get_adjacent_node_and_interface(
             nodes, node, if_key0
@@ -261,11 +287,13 @@ class L3fwdTest:
         :type patch: str
         :raises RuntimeError: Patching of l3fwd failed.
         """
-        command = f"{Constants.REMOTE_FW_DIR}/{Constants.RESOURCES_LIB_SH}"\
-            f"/entry/patch_l3fwd.sh " \
-            f"{Constants.REMOTE_FW_DIR}/{Constants.RESOURCES_LIB_SH}"\
+        command = (
+            f"{Constants.REMOTE_FW_DIR}/{Constants.RESOURCES_LIB_SH}"
+            f"/entry/patch_l3fwd.sh "
+            f"{Constants.REMOTE_FW_DIR}/{Constants.RESOURCES_LIB_SH}"
             f"/entry/{patch}"
+        )
         message = f"Failed to patch l3fwd at node {node['host']}"
         ret_code, stdout, _ = exec_cmd(node, command, timeout=1800)
-        if ret_code != 0 and u"Skipping patch." not in stdout:
+        if ret_code != 0 and "Skipping patch." not in stdout:
             raise RuntimeError(message)
