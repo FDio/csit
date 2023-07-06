737fd0e2bb07ee98be8ecd7695e362bc8d3b7f71
pmikus <peter.mikus@protonmail.ch>
2023-07-06 08:57:07 +0000

    test(aws): Debug
    Signed-off-by: pmikus <peter.mikus@protonmail.ch>
    Change-Id: Ief2aa455b9a66e67e26aeb73d158654816735c4c

--- fdio.infra.terraform/terraform-aws-2n-aws-c5n/variables.tf
+++ fdio.infra.terraform/terraform-aws-2n-aws-c5n/variables.tf
@@ -38,7 +38,7 @@ variable "placement_group_strategy" {
 variable "tg_ami" {
   description = "AMI to use for the instance."
   type        = string
-  default     = "ami-098a7a7d220d31850"
+  default     = "ami-07430bfa17fd4e597"
 }
 
 variable "tg_associate_public_ip_address" {
@@ -74,7 +74,7 @@ variable "tg_source_dest_check" {
 variable "sut1_ami" {
   description = "AMI to use for the instance."
   type        = string
-  default     = "ami-08f78d81d21cdaf14"
+  default     = "ami-0a890555652963ec2"
 }
 
 variable "sut1_associate_public_ip_address" {
