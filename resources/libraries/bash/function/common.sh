94a73953f531359d6cdd612d2b2a044835c7cfa5
Vratko Polak <vrpolak@cisco.com>
2023-11-24 10:32:33 +0100

    Add bisect script for locating regressions
    - WiP: Testing the new json parsing library.
    
    - TODOs:
     - Update commit message.
     - Add documentation where missing (search for FIXME).
     - Test if this still works after the rewrite.
      - Also ARMs and Denverton, to see pkg-verify does not crash.
    
    ---
    
    + Introduce bash entry script for calling "git bisect".
     + Extract start commit hash for new job type.
     + Configure "parent" in set_aside_current_build_artifacts.
    + Introduce Python integrated script for classifying results.
    + Improve failure handling:
     + Two more retries if test fails.
     + Only then use the passrate fake results.
    + Move compose_pybot_arguments out of entry scripts.
     + Needed to set correct topology with repeated reservations.
    + Improvements to PAPI stability:
     + Do not retry on PAPI connect.
     + Ignore exception on PAPI disconnect.
    + Improve bash naming:
     + Drop _amd64 from build_vpp_ubuntu_amd64 as it works on ARM.
     + Drop _bmrr as parsing now looks for PDR_LOWER as a fallback.
     + Rename commit -> current in function name.
    + Print more details about commits built in per-patch and device jobs.
    + Make functions more flexible with respect to directory naming.
     + Per-patch still uses "parent" and "current", device uses "current".
     + Bisect uses "early", "middle" and "late".
    
    Ticket: CSIT-1618
    
    Change-Id: I185bea084a29e6a37ef94e9da42b192a6a81fc17
    Signed-off-by: Vratko Polak <vrpolak@cisco.com>

--- resources/libraries/bash/function/common.sh
+++ resources/libraries/bash/function/common.sh
@@ -257,6 +257,10 @@ function common_dirs () {
 
 function compose_robot_arguments () {
 
+    # This function is called by run_tests function.
+    # The reason is that some jobs (bisect) perform reservation multiple times,
+    # so WORKING_TOPOLOGY can be different each time.
+    #
     # Variables read:
     # - WORKING_TOPOLOGY - Path to topology yaml file of the reserved testbed.
     # - DUT - CSIT test/ subdirectory, set while processing tags.
@@ -274,11 +278,13 @@ function compose_robot_arguments () {
     ROBOT_ARGS=("--loglevel" "TRACE")
     ROBOT_ARGS+=("--variable" "TOPOLOGY_PATH:${WORKING_TOPOLOGY}")
 
+    # TODO: The rest does not need to be recomputed on each reservation.
+    #       Refactor TEST_CODE so this part can be called only once.
     case "${TEST_CODE}" in
         *"device"*)
             ROBOT_ARGS+=("--suite" "tests.${DUT}.device")
             ;;
-        *"perf"*)
+        *"perf"* | *"bisect"*)
             ROBOT_ARGS+=("--suite" "tests.${DUT}.perf")
             ;;
         *)
@@ -545,6 +551,8 @@ function get_test_tag_string () {
     # Variables set:
     # - TEST_TAG_STRING - The string following trigger word in gerrit comment.
     #   May be empty, or even not set on event types not adding comment.
+    # - GIT_BISECT_FROM - If bisecttest, the commit hash to bisect from.
+    #   Else not set.
     # Variables exported optionally:
     # - GRAPH_NODE_VARIANT - Node variant to test with, set if found in trigger.
 
@@ -554,6 +562,10 @@ function get_test_tag_string () {
 
     if [[ "${GERRIT_EVENT_TYPE-}" == "comment-added" ]]; then
         case "${TEST_CODE}" in
+            # Order matters, bisect job contains "perf" in its name.
+            *"bisect"*)
+                trigger="bisecttest"
+                ;;
             *"device"*)
                 trigger="devicetest"
                 ;;
@@ -578,6 +590,18 @@ function get_test_tag_string () {
             comment=$(base64 --decode <<< "${comment}" || true)
             comment=$(fgrep "${trigger}" <<< "${comment}" || true)
             TEST_TAG_STRING=$("${cmd[@]}" <<< "${comment}" || true)
+        fi
+        if [[ "${trigger}" == "bisecttest" ]]; then
+            # Intentionally without quotes, so spaces delimit elements.
+            test_tag_array=(${TEST_TAG_STRING}) || die "How could this fail?"
+            # First "argument" of bisecttest is a commit hash.
+            GIT_BISECT_FROM="${test_tag_array[0]}" || {
+                die "Bisect job requires commit hash."
+            }
+            # Update the tag string (tag expressions only, no commit hash).
+            TEST_TAG_STRING="${test_tag_array[@]:1}" || {
+                die "Bisect job needs a single test, no default."
+            }
         fi
         if [[ -n "${TEST_TAG_STRING-}" ]]; then
             test_tag_array=(${TEST_TAG_STRING})
@@ -717,6 +741,9 @@ function reserve_and_cleanup_testbed () {
     # When cleanup fails, remove from topologies and keep retrying
     # until all topologies are removed.
     #
+    # Multiple other functions are called from here,
+    # as they set variables that depend on reserved topology data.
+    #
     # Variables read:
     # - TOPOLOGIES - Array of paths to topology yaml to attempt reservation on.
     # - PYTHON_SCRIPTS_DIR - Path to directory holding the reservation script.
@@ -785,6 +812,11 @@ function reserve_and_cleanup_testbed () {
         echo "Sleeping ${sleep_time}"
         sleep "${sleep_time}" || die "Sleep failed."
     done
+
+    # Subfunctions to update data that may depend on topology reserved.
+    set_environment_variables || die
+    select_tags || die
+    compose_robot_arguments || die
 }
 
 
@@ -792,12 +824,23 @@ function run_robot () {
 
     # Run robot with options based on input variables.
     #
+    # Testbed has to be reserved already,
+    # as some data may have changed between reservations,
+    # for example excluded NICs.
+    #
     # Variables read:
     # - CSIT_DIR - Path to existing root of local CSIT git repository.
     # - ARCHIVE_DIR - Path to store robot result files in.
     # - ROBOT_ARGS, EXPANDED_TAGS - See compose_robot_arguments.sh
     # - GENERATED_DIR - Tests are assumed to be generated under there.
+    # - WORKING_TOPOLOGY - Path to topology yaml file of the reserved testbed.
+    # - DUT - CSIT test/ subdirectory, set while processing tags.
+    # - TAGS - Array variable holding selected tag boolean expressions.
+    # - TOPOLOGIES_TAGS - Tag boolean expression filtering tests for topology.
+    # - TEST_CODE - The test selection string from environment or argument.
     # Variables set:
+    # - ROBOT_ARGS - String holding part of all arguments for robot.
+    # - EXPANDED_TAGS - Array of string robot arguments compiled from tags.
     # - ROBOT_EXIT_STATUS - Exit status of most recent robot invocation.
     # Functions called:
     # - die - Print to stderr and exit.
@@ -864,6 +907,9 @@ function select_arch_os () {
 
 function select_tags () {
 
+    # Only to be called from the reservation function,
+    # as resulting tags may change based on topology data.
+    #
     # Variables read:
     # - WORKING_TOPOLOGY - Path to topology yaml file of the reserved testbed.
     # - TEST_CODE - String affecting test selection, usually jenkins job name.
@@ -1274,6 +1320,9 @@ function set_environment_variables () {
 
     # Depending on testbed topology, overwrite defaults set in the
     # resources/libraries/python/Constants.py file
+    #
+    # Only to be called from the reservation function,
+    # as resulting values may change based on topology data.
     #
     # Variables read:
     # - TEST_CODE - String affecting test selection, usually jenkins job name.
