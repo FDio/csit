5277da96e14dddb903192115e6b376645dabb16e
Vratko Polak <vrpolak@cisco.com>
2023-12-13 17:02:22 +0100

    feat(bisect): introduce scripts for VPP bisecting
    + Parsing common with per-patch job is moved to a library.
    
    Ticket: CSIT-1618
    
    Change-Id: I185bea084a29e6a37ef94e9da42b192a6a81fc17
    Signed-off-by: Vratko Polak <vrpolak@cisco.com>

--- resources/tools/integrated/compare_perpatch.py
+++ resources/tools/integrated/compare_perpatch.py
@@ -16,7 +16,7 @@
 This script expects a particular tree created on a filesystem by
 per_patch_perf.sh bootstrap script, including test results
 exported as json files according to a current model schema.
-This script extracts the results (according to tresult type)
+This script extracts the results (according to result type)
 and joins them into one list of floats for parent and one for current.
 
 This script then uses jumpavg library to determine whether there was
@@ -26,64 +26,10 @@ If the set of test names does not match, or there was a regression,
 this script votes -1 (by exiting with code 1), otherwise it votes +1 (exit 0).
 """
 
-import json
-import os
 import sys
 
-from typing import Dict, List
-
 from resources.libraries.python import jumpavg
-
-
-def parse(dirpath: str, fake_value: float) -> Dict[str, List[float]]:
-    """Looks for test jsons, extract scalar results.
-
-    Files other than .json are skipped, jsons without test_id are skipped.
-    If the test failed, four fake values are used as a fake result.
-
-    Units are ignored, as both parent and current are tested
-    with the same CSIT code so the unit should be identical.
-
-    :param dirpath: Path to the directory tree to examine.
-    :param fail_value: Fake value to use for test cases that failed.
-    :type dirpath: str
-    :returns: Mapping from test IDs to list of measured values.
-    :rtype: Dict[str, List[float]]
-    :raises RuntimeError: On duplicate test ID or unknown test type.
-    """
-    results = {}
-    for root, _, files in os.walk(dirpath):
-        for filename in files:
-            if not filename.endswith(".json"):
-                continue
-            filepath = os.path.join(root, filename)
-            with open(filepath, "rt", encoding="utf8") as file_in:
-                data = json.load(file_in)
-            if "test_id" not in data:
-                continue
-            name = data["test_id"]
-            if name in results:
-                raise RuntimeError(f"Duplicate: {name}")
-            if not data["passed"]:
-                results[name] = [fake_value] * 4
-                continue
-            result_object = data["result"]
-            result_type = result_object["type"]
-            if result_type == "mrr":
-                results[name] = result_object["receive_rate"]["rate"]["values"]
-            elif result_type == "ndrpdr":
-                results[name] = [result_object["pdr"]["lower"]["rate"]["value"]]
-            elif result_type == "soak":
-                results[name] = [
-                    result_object["critical_rate"]["lower"]["rate"]["value"]
-                ]
-            elif result_type == "reconf":
-                results[name] = [result_object["loss"]["time"]["value"]]
-            elif result_type == "hoststack":
-                results[name] = [result_object["bandwidth"]["value"]]
-            else:
-                raise RuntimeError(f"Unknown result type: {result_type}")
-    return results
+from resources.libraries.python.model.parse import parse
 
 
 def main() -> int:
