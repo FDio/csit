fb428284721dd47865831faa2be5b819ac8c8c07
pmikus <peter.mikus@protonmail.ch>
2023-12-15 11:18:19 +0000

    feat(terraform): Migrate C-DASH to M7g.2xlarge
    Signed-off-by: Peter Mikus <peter.mikus@protonmail.ch>
    Change-Id: Idc39227487a89b7e637c8e2193be27fae088322d

--- fdio.infra.terraform/terraform-aws-fdio-csit-dash-app-base/versions.tf
+++ fdio.infra.terraform/terraform-aws-fdio-csit-dash-app-base/versions.tf
@@ -2,11 +2,11 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 5.7.0"
+      version = ">= 5.31.0"
     }
     vault = {
-      version = ">= 3.2.1"
+      version = ">= 3.23.0"
     }
   }
-  required_version = ">= 1.4.2"
+  required_version = ">= 1.5.4"
 }
