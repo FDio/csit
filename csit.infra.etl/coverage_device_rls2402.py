d5b27835fe0f15b60327a3779a847125c9db7558
pmikus <peter.mikus@protonmail.ch>
2024-01-12 07:25:36 +0000

    feat(etl): Add rls2402
    Signed-off-by: Peter Mikus <peter.mikus@protonmail.ch>
    Change-Id: Ie1b124c81f7b81dc431af2d97386dbb4499ea13b

--- csit.infra.etl/coverage_device_rls2402.py
+++ csit.infra.etl/coverage_device_rls2402.py
@@ -141,7 +141,7 @@ paths = wr.s3.list_objects(
     ignore_empty=True
 )
 
-filtered_paths = [path for path in paths if "report-coverage-2310" in path]
+filtered_paths = [path for path in paths if "report-coverage-2402" in path]
 
 out_sdf = process_json_to_dataframe("device", filtered_paths)
 out_sdf.printSchema()
@@ -154,7 +154,7 @@ out_sdf = out_sdf \
 try:
     wr.s3.to_parquet(
         df=out_sdf.toPandas(),
-        path=f"s3://{S3_DOCS_BUCKET}/csit/parquet/coverage_rls2310",
+        path=f"s3://{S3_DOCS_BUCKET}/csit/parquet/coverage_rls2402",
         dataset=True,
         partition_cols=["test_type", "year", "month", "day"],
         compression="snappy",
