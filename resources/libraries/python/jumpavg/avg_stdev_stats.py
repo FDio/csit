455bd62382842c10b5c39c343554f7f40e47ebeb
Vratko Polak <vrpolak@cisco.com>
2024-01-22 17:58:22 +0100

    fix(jumpavg): Tolerate zero sized runs
    Change-Id: I7c2426fe47ca9ee44338423597be90a71aaa3653
    Signed-off-by: Vratko Polak <vrpolak@cisco.com>

--- resources/libraries/python/jumpavg/avg_stdev_stats.py
+++ resources/libraries/python/jumpavg/avg_stdev_stats.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2023 Cisco and/or its affiliates.
+# Copyright (c) 2024 Cisco and/or its affiliates.
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at:
@@ -73,6 +73,8 @@ class AvgStdevStats:
                 run_size = run.size
                 run_avg = run.avg
                 run_stdev = run.stdev
+            if run_size < 1:
+                continue
             old_total_size = total_size
             delta = run_avg - total_avg
             total_size += run_size
