6067078db338b930972ad6f183a4c75d2cf9214a
pmikus <peter.mikus@protonmail.ch>
2024-01-25 07:37:31 +0000

    feat(terraform): Migrating old structures
    Signed-off-by: Peter Mikus <peter.mikus@protonmail.ch>
    Change-Id: Ib953739b4d34a7f803bff54cec92a2130fcbbc66

---
+++ fdio.infra.terraform/terraform-nomad-vpp-device/fdio/main.tf
@@ -0,0 +1,15 @@
+module "vpp-device" {
+  providers = {
+    nomad = nomad.yul1
+  }
+  source = "../"
+
+  # nomad
+  datacenters   = ["yul1"]
+  job_name      = "prod-device-csit-shim"
+  group_count   = "1"
+  cpu           = "1500"
+  memory        = "4096"
+  image_aarch64 = "fdiotools/csit_shim-ubuntu2004:2021_03_02_143938_UTC-aarch64"
+  image_x86_64  = "fdiotools/csit_shim-ubuntu2004:2021_03_04_142103_UTC-x86_64"
+}
