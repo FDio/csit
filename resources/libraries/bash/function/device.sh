ba60589590bd1e2d1904fd78e2680784d188dea2
Juraj Linkeš <juraj.linkes@pantheon.tech>
2024-02-12 11:25:41 +0100

    feat(device): cx6 update of 1n-alt device jobspec
    Update the jobspec according to the current Arm policy:
    1. Run most of the tests on cx6, crypto coverage being the most
       important,
    2. Match the cx6 ipv4 crypto coverage with x710 with the DPDK driver,
    3. Run basic ipv4 and l2 tests with x710 with borh DPDK and native (avf)
       drivers.
    
    The above is the policy used in performance testing where time is a
    major constraint. The same is true for functional testing to some
    extent, so it makes sense to apply the same logic for choosing the tests.
    
    Change-Id: Id48f450063e405b1faca57431676f91d62e829b9
    Signed-off-by: Juraj Linkeš <juraj.linkes@pantheon.tech>

--- resources/libraries/bash/function/device.sh
+++ resources/libraries/bash/function/device.sh
@@ -316,8 +316,8 @@ function get_available_interfaces () {
             # Add Intel Corporation XL710/X710 Virtual Function to the
             # whitelist.
             pci_id="0x154c"
-            tg_netdev=(enp1s0f0 enp1s0f1)
-            dut1_netdev=(enP3p2s0f0 enP3p2s0f1)
+            tg_netdev=(enp1s0f0 enp1s0f1 enP1p1s0f0)
+            dut1_netdev=(enP3p2s0f0 enP3p2s0f1 enP1p1s0f1)
             ports_per_nic=2
             ;;
         "1n_spr")
