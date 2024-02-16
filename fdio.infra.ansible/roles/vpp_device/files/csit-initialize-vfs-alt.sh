136fb9d2383547876bfb40f1dde8ba33991208a0
Juraj Linkeš <juraj.linkes@pantheon.tech>
2024-02-16 12:02:45 +0100

    feat(device): 1n-alt ansible improvement
    We're seeing extra IGMP traffic which interferes with tests which must
    be disabled. This is done by disabling the setting by default on all
    interfaces.
    
    The default setting is also adopted for other traffic-related
    configuration:
    Default sets the configuration when the interface is created. For
    hardware interfaces, that is at boot time. For VF interfaces, when we
    create them with the csit-initialize-vfs.sh script and for virtual
    interfaces when they're created.
    On the other hand, the all configuration option doesn't set the
    configuration. It uses the current configuration value along with the
    passed configuration value to produce a new value, which could be
    expressed as:
    
    f(old_value, value_from_all) -> new_value
    
    Where f is a function specific to each configuration option. Using all
    thus requires us to know the function and the old value which is less
    reliable than just using default everywhere.
    
    Also fix the 1n-alt VF init script.
    
    Change-Id: I3da15abfb5a1e76769076edd95d20e2168b6f71f
    Signed-off-by: Juraj Linkeš <juraj.linkes@pantheon.tech>

--- fdio.infra.ansible/roles/vpp_device/files/csit-initialize-vfs-alt.sh
+++ fdio.infra.ansible/roles/vpp_device/files/csit-initialize-vfs-alt.sh
@@ -24,7 +24,7 @@ PCI_BLACKLIST+=($(lspci -Dmmd ':1017:0200' | cut -f1 -d' '))
 # Add Intel Corporation Ethernet Controller XL710 for 40GbE QSFP+ to whitelist.
 PCI_WHITELIST=($(lspci -Dmmd ':1583:0200' | cut -f1 -d' '))
 # Add MT2892 Family [ConnectX-6 Dx] 101d to whitelist.
-PCI_WHITELIST=($(lspci -Dmmd ':101d:0200' | cut -f1 -d' '))
+PCI_WHITELIST+=($(lspci -Dmmd ':101d:0200' | cut -f1 -d' '))
 
 # See http://pci-ids.ucw.cz/v2.2/pci.ids for more info.
 
