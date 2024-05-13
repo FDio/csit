6ef967bb6fce5b08bee89d3b948710bf581626d1
Vratko Polak <vrpolak@cisco.com>
2024-05-13 20:36:19 +0200

    feat(CpuUtils): Allow suites to forbid siblings
    Some perf suites use a single unidirectional flow of traffic.
    On SMT-enabled machines, 1C tests by default create 2 workers.
    But if there is only one stream/flow,
    only one worker does useful work.
    The other worker on the sibling logical core
    does busy-wait, which provably lowers the peformance.
    
    Some suite already used smt_used (as a suite variable)
    set to False for these purposes.
    This change edits a lower-level L1 keyword to honor this value,
    and adds the suite variable to single-stream (and client) hoststack
    suites (where the detrimental effect of busy simbling was observed).
    
    Change-Id: I203f1a700d8e6cd84640b9c0b40487cb832908aa
    Signed-off-by: Vratko Polak <vrpolak@cisco.com>

--- resources/libraries/python/CpuUtils.py
+++ resources/libraries/python/CpuUtils.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2023 Cisco and/or its affiliates.
+# Copyright (c) 2024 Cisco and/or its affiliates.
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at:
@@ -278,6 +278,7 @@ class CpuUtils:
             raise RuntimeError(u"NodeID is out of range!")
 
         smt_used = CpuUtils.is_smt_enabled(node[u"cpuinfo"])
+        smt_used = BuiltIn().get_variable_value("\${smt_used}", smt_used)
         cpu_list = CpuUtils.cpu_list_per_node(node, cpu_node, smt_used)
         # CPU thread sibling offset.
         sib = len(cpu_list) // CpuUtils.NR_OF_THREADS
