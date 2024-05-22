2ebf9e56d0dd200fa09979505a2da070b39da63f
pmikus <peter.mikus@protonmail.ch>
2024-05-22 14:30:49 +0200

    feat(etl): Release pipelines
    Signed-off-by: pmikus <peter.mikus@protonmail.ch>
    Change-Id: I4ce20267b4747bf1901b6175e0ec5936b583a510

--- fdio.infra.terraform/terraform-nomad-pyspark-etl/fdio/main.tf
+++ fdio.infra.terraform/terraform-nomad-pyspark-etl/fdio/main.tf
@@ -94,190 +94,190 @@ module "etl-trending-soak" {
   memory                    = 60000
 }
 
-#module "etl-iterative-hoststack-rls2402" {
-#  providers = {
-#    nomad = nomad.yul1
-#  }
-#  source = "../"
-#
-#  aws_access_key_id         = data.vault_generic_secret.fdio_logs.data["access_key"]
-#  aws_secret_access_key     = data.vault_generic_secret.fdio_logs.data["secret_key"]
-#  aws_default_region        = data.vault_generic_secret.fdio_logs.data["region"]
-#  out_aws_access_key_id     = data.vault_generic_secret.fdio_docs.data["access_key"]
-#  out_aws_secret_access_key = data.vault_generic_secret.fdio_docs.data["secret_key"]
-#  out_aws_default_region    = data.vault_generic_secret.fdio_docs.data["region"]
-#  cron                      = "0 30 0 * * * *"
-#  datacenters               = ["yul1"]
-#  job_name                  = "etl-iterative-hoststack-rls2402"
-#}
-#
-#module "etl-iterative-mrr-rls2402" {
-#  providers = {
-#    nomad = nomad.yul1
-#  }
-#  source = "../"
-#
-#  aws_access_key_id         = data.vault_generic_secret.fdio_logs.data["access_key"]
-#  aws_secret_access_key     = data.vault_generic_secret.fdio_logs.data["secret_key"]
-#  aws_default_region        = data.vault_generic_secret.fdio_logs.data["region"]
-#  out_aws_access_key_id     = data.vault_generic_secret.fdio_docs.data["access_key"]
-#  out_aws_secret_access_key = data.vault_generic_secret.fdio_docs.data["secret_key"]
-#  out_aws_default_region    = data.vault_generic_secret.fdio_docs.data["region"]
-#  cron                      = "0 30 0 * * * *"
-#  datacenters               = ["yul1"]
-#  job_name                  = "etl-iterative-mrr-rls2402"
-#}
-#
-#module "etl-iterative-ndrpdr-rls2402" {
-#  providers = {
-#    nomad = nomad.yul1
-#  }
-#  source = "../"
-#
-#  aws_access_key_id         = data.vault_generic_secret.fdio_logs.data["access_key"]
-#  aws_secret_access_key     = data.vault_generic_secret.fdio_logs.data["secret_key"]
-#  aws_default_region        = data.vault_generic_secret.fdio_logs.data["region"]
-#  out_aws_access_key_id     = data.vault_generic_secret.fdio_docs.data["access_key"]
-#  out_aws_secret_access_key = data.vault_generic_secret.fdio_docs.data["secret_key"]
-#  out_aws_default_region    = data.vault_generic_secret.fdio_docs.data["region"]
-#  cron                      = "0 30 0 * * * *"
-#  datacenters               = ["yul1"]
-#  job_name                  = "etl-iterative-ndrpdr-rls2402"
-#}
-#
-#module "etl-iterative-reconf-rls2402" {
-#  providers = {
-#    nomad = nomad.yul1
-#  }
-#  source = "../"
-#
-#  aws_access_key_id         = data.vault_generic_secret.fdio_logs.data["access_key"]
-#  aws_secret_access_key     = data.vault_generic_secret.fdio_logs.data["secret_key"]
-#  aws_default_region        = data.vault_generic_secret.fdio_logs.data["region"]
-#  out_aws_access_key_id     = data.vault_generic_secret.fdio_docs.data["access_key"]
-#  out_aws_secret_access_key = data.vault_generic_secret.fdio_docs.data["secret_key"]
-#  out_aws_default_region    = data.vault_generic_secret.fdio_docs.data["region"]
-#  cron                      = "0 30 0 * * * *"
-#  datacenters               = ["yul1"]
-#  job_name                  = "etl-iterative-reconf-rls2402"
-#}
-#
-#module "etl-iterative-soak-rls2402" {
-#  providers = {
-#    nomad = nomad.yul1
-#  }
-#  source = "../"
-#
-#  aws_access_key_id         = data.vault_generic_secret.fdio_logs.data["access_key"]
-#  aws_secret_access_key     = data.vault_generic_secret.fdio_logs.data["secret_key"]
-#  aws_default_region        = data.vault_generic_secret.fdio_logs.data["region"]
-#  out_aws_access_key_id     = data.vault_generic_secret.fdio_docs.data["access_key"]
-#  out_aws_secret_access_key = data.vault_generic_secret.fdio_docs.data["secret_key"]
-#  out_aws_default_region    = data.vault_generic_secret.fdio_docs.data["region"]
-#  cron                      = "0 30 0 * * * *"
-#  datacenters               = ["yul1"]
-#  job_name                  = "etl-iterative-soak-rls2402"
-#}
-#
-#module "etl-coverage-device-rls2402" {
-#  providers = {
-#    nomad = nomad.yul1
-#  }
-#  source = "../"
-#
-#  aws_access_key_id         = data.vault_generic_secret.fdio_logs.data["access_key"]
-#  aws_secret_access_key     = data.vault_generic_secret.fdio_logs.data["secret_key"]
-#  aws_default_region        = data.vault_generic_secret.fdio_logs.data["region"]
-#  out_aws_access_key_id     = data.vault_generic_secret.fdio_docs.data["access_key"]
-#  out_aws_secret_access_key = data.vault_generic_secret.fdio_docs.data["secret_key"]
-#  out_aws_default_region    = data.vault_generic_secret.fdio_docs.data["region"]
-#  cron                      = "0 30 0 * * * *"
-#  datacenters               = ["yul1"]
-#  job_name                  = "etl-coverage-device-rls2402"
-#}
-#
-#module "etl-coverage-hoststack-rls2402" {
-#  providers = {
-#    nomad = nomad.yul1
-#  }
-#  source = "../"
-#
-#  aws_access_key_id         = data.vault_generic_secret.fdio_logs.data["access_key"]
-#  aws_secret_access_key     = data.vault_generic_secret.fdio_logs.data["secret_key"]
-#  aws_default_region        = data.vault_generic_secret.fdio_logs.data["region"]
-#  out_aws_access_key_id     = data.vault_generic_secret.fdio_docs.data["access_key"]
-#  out_aws_secret_access_key = data.vault_generic_secret.fdio_docs.data["secret_key"]
-#  out_aws_default_region    = data.vault_generic_secret.fdio_docs.data["region"]
-#  cron                      = "0 30 0 * * * *"
-#  datacenters               = ["yul1"]
-#  job_name                  = "etl-coverage-hoststack-rls2402"
-#}
-#
-#module "etl-coverage-mrr-rls2402" {
-#  providers = {
-#    nomad = nomad.yul1
-#  }
-#  source = "../"
-#
-#  aws_access_key_id         = data.vault_generic_secret.fdio_logs.data["access_key"]
-#  aws_secret_access_key     = data.vault_generic_secret.fdio_logs.data["secret_key"]
-#  aws_default_region        = data.vault_generic_secret.fdio_logs.data["region"]
-#  out_aws_access_key_id     = data.vault_generic_secret.fdio_docs.data["access_key"]
-#  out_aws_secret_access_key = data.vault_generic_secret.fdio_docs.data["secret_key"]
-#  out_aws_default_region    = data.vault_generic_secret.fdio_docs.data["region"]
-#  cron                      = "0 30 0 * * * *"
-#  datacenters               = ["yul1"]
-#  job_name                  = "etl-coverage-mrr-rls2402"
-#}
-#
-#module "etl-coverage-ndrpdr-rls2402" {
-#  providers = {
-#    nomad = nomad.yul1
-#  }
-#  source = "../"
-#
-#  aws_access_key_id         = data.vault_generic_secret.fdio_logs.data["access_key"]
-#  aws_secret_access_key     = data.vault_generic_secret.fdio_logs.data["secret_key"]
-#  aws_default_region        = data.vault_generic_secret.fdio_logs.data["region"]
-#  out_aws_access_key_id     = data.vault_generic_secret.fdio_docs.data["access_key"]
-#  out_aws_secret_access_key = data.vault_generic_secret.fdio_docs.data["secret_key"]
-#  out_aws_default_region    = data.vault_generic_secret.fdio_docs.data["region"]
-#  cron                      = "0 30 0 * * * *"
-#  datacenters               = ["yul1"]
-#  job_name                  = "etl-coverage-ndrpdr-rls2402"
-#}
-#
-#module "etl-coverage-reconf-rls2402" {
-#  providers = {
-#    nomad = nomad.yul1
-#  }
-#  source = "../"
-#
-#  aws_access_key_id         = data.vault_generic_secret.fdio_logs.data["access_key"]
-#  aws_secret_access_key     = data.vault_generic_secret.fdio_logs.data["secret_key"]
-#  aws_default_region        = data.vault_generic_secret.fdio_logs.data["region"]
-#  out_aws_access_key_id     = data.vault_generic_secret.fdio_docs.data["access_key"]
-#  out_aws_secret_access_key = data.vault_generic_secret.fdio_docs.data["secret_key"]
-#  out_aws_default_region    = data.vault_generic_secret.fdio_docs.data["region"]
-#  cron                      = "0 30 0 * * * *"
-#  datacenters               = ["yul1"]
-#  job_name                  = "etl-coverage-reconf-rls2402"
-#}
-#
-#module "etl-coverage-soak-rls2402" {
-#  providers = {
-#    nomad = nomad.yul1
-#  }
-#  source = "../"
-#
-#  aws_access_key_id         = data.vault_generic_secret.fdio_logs.data["access_key"]
-#  aws_secret_access_key     = data.vault_generic_secret.fdio_logs.data["secret_key"]
-#  aws_default_region        = data.vault_generic_secret.fdio_logs.data["region"]
-#  out_aws_access_key_id     = data.vault_generic_secret.fdio_docs.data["access_key"]
-#  out_aws_secret_access_key = data.vault_generic_secret.fdio_docs.data["secret_key"]
-#  out_aws_default_region    = data.vault_generic_secret.fdio_docs.data["region"]
-#  cron                      = "0 30 0 * * * *"
-#  datacenters               = ["yul1"]
-#  job_name                  = "etl-coverage-soak-rls2402"
-#}
-#
+module "etl-iterative-hoststack-rls2406" {
+  providers = {
+    nomad = nomad.yul1
+  }
+  source = "../"
+
+  aws_access_key_id         = data.vault_generic_secret.fdio_logs.data["access_key"]
+  aws_secret_access_key     = data.vault_generic_secret.fdio_logs.data["secret_key"]
+  aws_default_region        = data.vault_generic_secret.fdio_logs.data["region"]
+  out_aws_access_key_id     = data.vault_generic_secret.fdio_docs.data["access_key"]
+  out_aws_secret_access_key = data.vault_generic_secret.fdio_docs.data["secret_key"]
+  out_aws_default_region    = data.vault_generic_secret.fdio_docs.data["region"]
+  cron                      = "0 30 0 * * * *"
+  datacenters               = ["yul1"]
+  job_name                  = "etl-iterative-hoststack-rls2406"
+}
+
+module "etl-iterative-mrr-rls2406" {
+  providers = {
+    nomad = nomad.yul1
+  }
+  source = "../"
+
+  aws_access_key_id         = data.vault_generic_secret.fdio_logs.data["access_key"]
+  aws_secret_access_key     = data.vault_generic_secret.fdio_logs.data["secret_key"]
+  aws_default_region        = data.vault_generic_secret.fdio_logs.data["region"]
+  out_aws_access_key_id     = data.vault_generic_secret.fdio_docs.data["access_key"]
+  out_aws_secret_access_key = data.vault_generic_secret.fdio_docs.data["secret_key"]
+  out_aws_default_region    = data.vault_generic_secret.fdio_docs.data["region"]
+  cron                      = "0 30 0 * * * *"
+  datacenters               = ["yul1"]
+  job_name                  = "etl-iterative-mrr-rls2406"
+}
+
+module "etl-iterative-ndrpdr-rls2406" {
+  providers = {
+    nomad = nomad.yul1
+  }
+  source = "../"
+
+  aws_access_key_id         = data.vault_generic_secret.fdio_logs.data["access_key"]
+  aws_secret_access_key     = data.vault_generic_secret.fdio_logs.data["secret_key"]
+  aws_default_region        = data.vault_generic_secret.fdio_logs.data["region"]
+  out_aws_access_key_id     = data.vault_generic_secret.fdio_docs.data["access_key"]
+  out_aws_secret_access_key = data.vault_generic_secret.fdio_docs.data["secret_key"]
+  out_aws_default_region    = data.vault_generic_secret.fdio_docs.data["region"]
+  cron                      = "0 30 0 * * * *"
+  datacenters               = ["yul1"]
+  job_name                  = "etl-iterative-ndrpdr-rls2406"
+}
+
+module "etl-iterative-reconf-rls2406" {
+  providers = {
+    nomad = nomad.yul1
+  }
+  source = "../"
+
+  aws_access_key_id         = data.vault_generic_secret.fdio_logs.data["access_key"]
+  aws_secret_access_key     = data.vault_generic_secret.fdio_logs.data["secret_key"]
+  aws_default_region        = data.vault_generic_secret.fdio_logs.data["region"]
+  out_aws_access_key_id     = data.vault_generic_secret.fdio_docs.data["access_key"]
+  out_aws_secret_access_key = data.vault_generic_secret.fdio_docs.data["secret_key"]
+  out_aws_default_region    = data.vault_generic_secret.fdio_docs.data["region"]
+  cron                      = "0 30 0 * * * *"
+  datacenters               = ["yul1"]
+  job_name                  = "etl-iterative-reconf-rls2406"
+}
+
+module "etl-iterative-soak-rls2406" {
+  providers = {
+    nomad = nomad.yul1
+  }
+  source = "../"
+
+  aws_access_key_id         = data.vault_generic_secret.fdio_logs.data["access_key"]
+  aws_secret_access_key     = data.vault_generic_secret.fdio_logs.data["secret_key"]
+  aws_default_region        = data.vault_generic_secret.fdio_logs.data["region"]
+  out_aws_access_key_id     = data.vault_generic_secret.fdio_docs.data["access_key"]
+  out_aws_secret_access_key = data.vault_generic_secret.fdio_docs.data["secret_key"]
+  out_aws_default_region    = data.vault_generic_secret.fdio_docs.data["region"]
+  cron                      = "0 30 0 * * * *"
+  datacenters               = ["yul1"]
+  job_name                  = "etl-iterative-soak-rls2406"
+}
+
+module "etl-coverage-device-rls2406" {
+  providers = {
+    nomad = nomad.yul1
+  }
+  source = "../"
+
+  aws_access_key_id         = data.vault_generic_secret.fdio_logs.data["access_key"]
+  aws_secret_access_key     = data.vault_generic_secret.fdio_logs.data["secret_key"]
+  aws_default_region        = data.vault_generic_secret.fdio_logs.data["region"]
+  out_aws_access_key_id     = data.vault_generic_secret.fdio_docs.data["access_key"]
+  out_aws_secret_access_key = data.vault_generic_secret.fdio_docs.data["secret_key"]
+  out_aws_default_region    = data.vault_generic_secret.fdio_docs.data["region"]
+  cron                      = "0 30 0 * * * *"
+  datacenters               = ["yul1"]
+  job_name                  = "etl-coverage-device-rls2406"
+}
+
+module "etl-coverage-hoststack-rls2406" {
+  providers = {
+    nomad = nomad.yul1
+  }
+  source = "../"
+
+  aws_access_key_id         = data.vault_generic_secret.fdio_logs.data["access_key"]
+  aws_secret_access_key     = data.vault_generic_secret.fdio_logs.data["secret_key"]
+  aws_default_region        = data.vault_generic_secret.fdio_logs.data["region"]
+  out_aws_access_key_id     = data.vault_generic_secret.fdio_docs.data["access_key"]
+  out_aws_secret_access_key = data.vault_generic_secret.fdio_docs.data["secret_key"]
+  out_aws_default_region    = data.vault_generic_secret.fdio_docs.data["region"]
+  cron                      = "0 30 0 * * * *"
+  datacenters               = ["yul1"]
+  job_name                  = "etl-coverage-hoststack-rls2406"
+}
+
+module "etl-coverage-mrr-rls2406" {
+  providers = {
+    nomad = nomad.yul1
+  }
+  source = "../"
+
+  aws_access_key_id         = data.vault_generic_secret.fdio_logs.data["access_key"]
+  aws_secret_access_key     = data.vault_generic_secret.fdio_logs.data["secret_key"]
+  aws_default_region        = data.vault_generic_secret.fdio_logs.data["region"]
+  out_aws_access_key_id     = data.vault_generic_secret.fdio_docs.data["access_key"]
+  out_aws_secret_access_key = data.vault_generic_secret.fdio_docs.data["secret_key"]
+  out_aws_default_region    = data.vault_generic_secret.fdio_docs.data["region"]
+  cron                      = "0 30 0 * * * *"
+  datacenters               = ["yul1"]
+  job_name                  = "etl-coverage-mrr-rls2406"
+}
+
+module "etl-coverage-ndrpdr-rls2406" {
+  providers = {
+    nomad = nomad.yul1
+  }
+  source = "../"
+
+  aws_access_key_id         = data.vault_generic_secret.fdio_logs.data["access_key"]
+  aws_secret_access_key     = data.vault_generic_secret.fdio_logs.data["secret_key"]
+  aws_default_region        = data.vault_generic_secret.fdio_logs.data["region"]
+  out_aws_access_key_id     = data.vault_generic_secret.fdio_docs.data["access_key"]
+  out_aws_secret_access_key = data.vault_generic_secret.fdio_docs.data["secret_key"]
+  out_aws_default_region    = data.vault_generic_secret.fdio_docs.data["region"]
+  cron                      = "0 30 0 * * * *"
+  datacenters               = ["yul1"]
+  job_name                  = "etl-coverage-ndrpdr-rls2406"
+}
+
+module "etl-coverage-reconf-rls2406" {
+  providers = {
+    nomad = nomad.yul1
+  }
+  source = "../"
+
+  aws_access_key_id         = data.vault_generic_secret.fdio_logs.data["access_key"]
+  aws_secret_access_key     = data.vault_generic_secret.fdio_logs.data["secret_key"]
+  aws_default_region        = data.vault_generic_secret.fdio_logs.data["region"]
+  out_aws_access_key_id     = data.vault_generic_secret.fdio_docs.data["access_key"]
+  out_aws_secret_access_key = data.vault_generic_secret.fdio_docs.data["secret_key"]
+  out_aws_default_region    = data.vault_generic_secret.fdio_docs.data["region"]
+  cron                      = "0 30 0 * * * *"
+  datacenters               = ["yul1"]
+  job_name                  = "etl-coverage-reconf-rls2406"
+}
+
+module "etl-coverage-soak-rls2406" {
+  providers = {
+    nomad = nomad.yul1
+  }
+  source = "../"
+
+  aws_access_key_id         = data.vault_generic_secret.fdio_logs.data["access_key"]
+  aws_secret_access_key     = data.vault_generic_secret.fdio_logs.data["secret_key"]
+  aws_default_region        = data.vault_generic_secret.fdio_logs.data["region"]
+  out_aws_access_key_id     = data.vault_generic_secret.fdio_docs.data["access_key"]
+  out_aws_secret_access_key = data.vault_generic_secret.fdio_docs.data["secret_key"]
+  out_aws_default_region    = data.vault_generic_secret.fdio_docs.data["region"]
+  cron                      = "0 30 0 * * * *"
+  datacenters               = ["yul1"]
+  job_name                  = "etl-coverage-soak-rls2406"
+}
+
