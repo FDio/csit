3de6d8fb2442942ce2f938722bf0e175fcca60e1
Peter Mikus <peter.mikus@protonmail.ch>
2024-05-24 11:32:39 +0000

    Revert "fix(packer): Update AWS image"
    This reverts commit 980263450cab4b4d2f8cbf6493534cf8561c4721.
    
    Reason for revert: .
    
    Change-Id: Ifed4a7d5aace9665657a94bee8ad829c5e6d04ce
    Signed-off-by: Peter Mikus <peter.mikus@protonmail.ch>

--- fdio.infra.terraform/terraform-aws-2n-c6in/variables.tf
+++ fdio.infra.terraform/terraform-aws-2n-c6in/variables.tf
@@ -50,7 +50,7 @@ variable "placement_group_strategy" {
 variable "tg_ami" {
   description = "AMI to use for the instance."
   type        = string
-  default     = "ami-0b156cada4f639c46"
+  default     = "ami-07430bfa17fd4e597"
 }
 
 variable "tg_associate_public_ip_address" {
@@ -86,7 +86,7 @@ variable "tg_source_dest_check" {
 variable "sut1_ami" {
   description = "AMI to use for the instance."
   type        = string
-  default     = "ami-0aaed4424cfea9a24"
+  default     = "ami-0a890555652963ec2"
 }
 
 variable "sut1_associate_public_ip_address" {
