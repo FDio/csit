da7789571bf1de68615f86fb08ac283a29acd001
pmikus <peter.mikus@protonmail.ch>
2024-05-29 12:27:43 +0200

    feat(terraform): New AWS AMI image
    Signed-off-by: Peter Mikus <peter.mikus@protonmail.ch>
    Change-Id: I6951c4fc9f96a0acf44ba3201c91c9cb903889d7
    (cherry picked from commit 5bb211044c10e791277d7de8561dc50af30c6abc)

--- fdio.infra.terraform/terraform-aws-2n-c6in/variables.tf
+++ fdio.infra.terraform/terraform-aws-2n-c6in/variables.tf
@@ -50,7 +50,7 @@ variable "placement_group_strategy" {
 variable "tg_ami" {
   description = "AMI to use for the instance."
   type        = string
-  default     = "ami-07430bfa17fd4e597"
+  default     = "ami-0285a978d8bd77dcb"
 }
 
 variable "tg_associate_public_ip_address" {
@@ -86,7 +86,7 @@ variable "tg_source_dest_check" {
 variable "sut1_ami" {
   description = "AMI to use for the instance."
   type        = string
-  default     = "ami-0a890555652963ec2"
+  default     = "ami-0f3ecd7f56fc232bd"
 }
 
 variable "sut1_associate_public_ip_address" {
