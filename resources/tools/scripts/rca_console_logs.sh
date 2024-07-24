2071b6f17b2f3356201c51100adfd6e60767cc70
Vratko Polak <vrpolak@cisco.com>
2024-07-24 12:08:59 +0200

    feat(scripts): Introduce console log analyzer
    This semi-automatic script saves time for RCA after release.
    
    Change-Id: Idda3de00635b3c16b71df024616f1e2e94d7c1d6
    Signed-off-by: Vratko Polak <vrpolak@cisco.com>

---
+++ resources/tools/scripts/rca_console_logs.sh
@@ -0,0 +1,46 @@
+# Copyright (c) 2024 Cisco and/or its affiliates.
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at:
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+set -exuo pipefail
+set +x
+
+# This tool saves time for RCA after release.
+
+# First argument: Jenkins job to analyze.
+# Second argument: Pattern to looks for (e.g. identifying release instead of RC2).
+
+# For each run, this script prints hints on whether skip or look deeper.
+
+jobname="${1}"
+build_pattern="${2}"
+# TODO: Detect last run and gor backward?
+for i in {1..999}; do
+    if ! curl -sf "${jobname}/${i}/console.log.gz" | zcat > "console.log"; then
+        echo "${i}: failed to download. No more runs?"
+        exit 0
+    fi
+    if ! fgrep -q "${build_pattern}" "console.log"; then
+        echo "${i}: not matching the pattern, skip."
+        continue
+    fi
+    if fgrep " FAIL" "console.log" > "fails.txt"; then
+        echo "${i}: has $(wc -l < 'fails.txt') failures, look at it."
+        continue
+    fi
+    if ! fgrep " PASS" "console.log" > "passes.txt"; then
+        echo "${i}: no passes? suspicious."
+        continue
+    fi
+    echo "${i}: no failures, $(wc -l < 'passes.txt') passes, skip."
+    continue
+done
