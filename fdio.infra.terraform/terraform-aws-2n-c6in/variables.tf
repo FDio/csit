477a7263c8e4f85cc3b45c7ce1ef2e91dbb2493a
Peter Mikus <peter.mikus@protonmail.ch>
2025-01-10 12:29:36 +0100

    fix(infra): New AWS images
    Signed-off-by: Peter Mikus <peter.mikus@protonmail.ch>
    Change-Id: Ibd1892cfc57b9c172fa9a08e39d704c1a812ad3d

--- fdio.infra.terraform/terraform-aws-2n-c6in/variables.tf
+++ fdio.infra.terraform/terraform-aws-2n-c6in/variables.tf
@@ -50,7 +50,7 @@ variable "placement_group_strategy" {
 variable "tg_ami" {
   description = "AMI to use for the instance."
   type        = string
-  default     = "ami-0c18f927fc6ac6c52"
+  default     = "ami-06ea2ad776e2a8461"
 }
 
 variable "tg_associate_public_ip_address" {
