9fe3732a9c66f4068eabf3da3154ab5d6a8f70d8
Peter Mikus <peter.mikus@icloud.com>
2025-01-23 10:10:49 +0100

    feat(infra): DPDK 24.11.1
    Signed-off-by: Peter Mikus <peter.mikus@icloud.com>
    Change-Id: Ic935ddf79d06b9d8f1df329bf0e4485ee2d646cf
    (cherry picked from commit 7f8e6a48400aef8d0e1d0f5fa994bea8d24e5409)

--- resources/libraries/python/Constants.py
+++ resources/libraries/python/Constants.py
@@ -180,7 +180,7 @@ class Constants:
     QEMU_VM_IMAGE = "/var/lib/vm/image.iso"
 
     # QEMU VM DPDK path
-    QEMU_VM_DPDK = "/opt/dpdk-24.07"
+    QEMU_VM_DPDK = "/opt/dpdk-24.11.1"
 
     # Docker container SUT image
     DOCKER_SUT_IMAGE_UBUNTU = "csit_sut-ubuntu2404:local"
