ec1bd1ed5c663caaa026de9f303417905ba5a81e
Vratko Polak <vrpolak@cisco.com>
2025-02-10 10:14:42 +0100

    Experiments: udir
    Change-Id: I4067346eb6eee785802f6905b20e3a1b53754a15
    Signed-off-by: Vratko Polak <vrpolak@cisco.com>

--- resources/libraries/python/L2Util.py
+++ resources/libraries/python/L2Util.py
@@ -337,7 +337,7 @@ class L2Util:
         with PapiSocketExecutor(node) as papi_exec:
             # Cannot use get_replies due to VPP-2203.
             papi_exec.add(cmd, **args1).get_reply(err_msg)
-            papi_exec.add(cmd, **args2).get_reply(err_msg)
+            #papi_exec.add(cmd, **args2).get_reply(err_msg)
 
     @staticmethod
     def linux_add_bridge(node, br_name, if_1, if_2, set_up=True):
