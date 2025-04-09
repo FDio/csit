962a797353b62da9c6d121f72ec5fef6f0164a9c
Vratko Polak <vrpolak@cisco.com>
2025-04-09 13:08:42 +0200

    feat(mrr): Ignore negative receive count
    Sometimes, TRex reports large negative number of forwarded packets.
    As model (json schema) requires non-negative values,
    the best workaround for MRR tests is to simply skip those values
    when exporting results to json.
    This skipping could be added in Robot keywords,
    but export keywords are a better place,
    as keeping json data valid is their responsibility.
    
    Theoretically, MRR can fail validation if all 10 trials are skipped,
    but that would probably be due to a new bug, so test failure is fine.
    
    For ndrpdr and soak tests, ignoring such trials would just repeat them,
    so in this case the simpler fix is to add repeats to TrafficGenerator.
    If three consecutive trials give negative results, the test will fail.
    
    Ticket: csit-3983
    
    Change-Id: I1e8abb2ece5bd457f1e2c0c0634f9d08e2697f12
    Signed-off-by: Vratko Polak <vrpolak@cisco.com>

--- resources/libraries/python/model/ExportResult.py
+++ resources/libraries/python/model/ExportResult.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2023 Cisco and/or its affiliates.
+# Copyright (c) 2025 Cisco and/or its affiliates.
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at:
@@ -100,7 +100,8 @@ def append_mrr_value(mrr_value, mrr_unit, bandwidth_value=None,
         bandwidth_unit="bps"):
     """Store mrr value to proper place so it is dumped into json.
 
-    The value is appended only when unit is not empty.
+    The value is appended only when unit is not empty
+    and the value is not negative, as otherwise results would fail valiadtion.
 
     :param mrr_value: Forwarding rate from MRR trial.
     :param mrr_unit: Unit of measurement for the rate.
@@ -110,7 +111,7 @@ def append_mrr_value(mrr_value, mrr_unit, bandwidth_value=None,
     :type bandwidth_value: Optional[float]
     :type bandwidth_unit: Optional[str]
     """
-    if not mrr_unit:
+    if not mrr_unit or mrr_value < 0:
         return
     data = get_export_data()
     data["result"]["type"] = "mrr"
