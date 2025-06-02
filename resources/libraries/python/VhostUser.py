4ec6a98d6d7d1c8e045db9021dc267287b2e8b2a
Peter Mikus <peter.mikus@icloud.com>
2025-06-02 09:41:23 +0200

    test
    Signed-off-by: Peter Mikus <peter.mikus@icloud.com>
    Change-Id: I251f8432ceab1608d5b45114bc2d3523a402268a

--- resources/libraries/python/VhostUser.py
+++ resources/libraries/python/VhostUser.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2021 Cisco and/or its affiliates.
+# Copyright (c) 2025 Cisco and/or its affiliates.
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at:
@@ -164,13 +164,15 @@ class VhostUser:
         :type pf_key: str
         :type skip_cnt: int
         """
-        pids, _ = exec_cmd_no_error(
-            node, f"grep -h vhost /proc/*/comm | uniq | xargs pidof")
+        command = "grep -e vhost /proc/$(pidof vpp)/task/*/comm "
+        command +="| awk 'BEGIN { FS = \"/\" } ; { print $5 }'"
+        pids, _ = exec_cmd_no_error(node, command)
 
         affinity = CpuUtils.get_affinity_vhost(
-            node, pf_key, skip_cnt=skip_cnt, cpu_cnt=len(pids.split(" ")))
+            node, pf_key, skip_cnt=skip_cnt, cpu_cnt=len(pids.splitlines())
+        )
 
-        for cpu, pid in zip(affinity, pids.split(" ")):
+        for cpu, pid in zip(affinity, pids.splitlines()):
             exec_cmd_no_error(node, f"taskset -pc {cpu} {pid}", sudo=True)
 
 
