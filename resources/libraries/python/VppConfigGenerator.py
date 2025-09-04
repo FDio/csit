a73c7cfb23b47934851d3ad906441a7fee03f11e
Vratko Polak <vrpolak@cisco.com>
2025-09-04 12:58:24 +0200

    Experiment: Do not enable perfmon
    For investigating ASAN issues on Xeons.
    
    Change-Id: I3a1c3940d22ef564a6cca6e33f5bbcd3e55332de
    Signed-off-by: Vratko Polak <vrpolak@cisco.com>

--- resources/libraries/python/VppConfigGenerator.py
+++ resources/libraries/python/VppConfigGenerator.py
@@ -549,6 +549,8 @@ class VppConfigGenerator:
         :type plugins: list
         """
         for plugin in plugins:
+            if "perfmon" in plugin:
+                continue
             path = ["plugins", f"plugin {plugin}", state]
             self.add_config_item(self._nodeconfig, " ", path)
 
