e5061d2c8bc22835c4453c7030d78f2617dc8b92
Vratko Polak <vrpolak@cisco.com>
2025-09-23 14:32:56 +0200

    Experiment: more logs
    + Kill vpp in case it got stuck, to generate core.
    
    Change-Id: Icd64e0891b45338fb00a6c4bb329a9e06d758857
    Signed-off-by: Vratko Polak <vrpolak@cisco.com>

--- resources/libraries/python/CoreDumpUtil.py
+++ resources/libraries/python/CoreDumpUtil.py
@@ -139,8 +139,9 @@ class CoreDumpUtil:
         for node in nodes.values():
             if node[u"type"] == NodeType.DUT:
                 command = (
-                    f"for f in {Constants.CORE_DUMP_DIR}/*.core; do"
-                    f" sleep 10; sudo gdb /usr/bin/vpp ${{f}}"
+                    "pkill -e vpp; sleep 10;"
+                    f" for f in {Constants.CORE_DUMP_DIR}/*.core; do"
+                    f" sudo gdb /usr/bin/vpp ${{f}}"
                     f" -ex 'source -v {Constants.REMOTE_FW_DIR}"
                     f"/resources/tools/scripts/gdb-commands' -ex quit;"
                     f" sudo rm -f ${{f}}; done"
