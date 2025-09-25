981c8bb58aa738dff8380f64f90dc8ceb11ef25b
Peter Mikus <peter.mikus@icloud.com>
2025-09-25 08:48:14 +0200

    feat(etl): Pipeline enable
    Signed-off-by: Peter Mikus <peter.mikus@icloud.com>
    Change-Id: I1bc7f71a95514d742c58484947c11d219a713be0

--- fdio.infra.terraform/terraform-nomad-pyspark-etl/versions.tf
+++ fdio.infra.terraform/terraform-nomad-pyspark-etl/versions.tf
@@ -7,11 +7,11 @@ terraform {
   required_providers {
     nomad = {
       source  = "hashicorp/nomad"
-      version = ">= 2.3.0"
+      version = ">= 2.5.0"
     }
     vault = {
       version = ">= 4.6.0"
     }
   }
-  required_version = ">= 1.10.4"
+  required_version = ">= 1.12.1"
 }
