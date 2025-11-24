999008f6b14feced64438daafb8096d347e16b78
Peter Mikus <peter.mikus@icloud.com>
2025-11-24 11:08:42 +0100

    feat(etl): Rework of pipelines
    Signed-off-by: Peter Mikus <peter.mikus@icloud.com>
    Change-Id: I1c32794365a5cd3ae9f02f9725ad4a686ba131db

--- fdio.infra.terraform/terraform-nomad-pyspark-etl/providers.tf
+++ fdio.infra.terraform/terraform-nomad-pyspark-etl/providers.tf
@@ -1,13 +1,7 @@
 provider "nomad" {
-  address = var.nomad_provider_address
+  address = "http://10.30.51.23:4646"
   alias   = "yul1"
   #  ca_file   = var.nomad_provider_ca_file
   #  cert_file = var.nomad_provider_cert_file
   #  key_file  = var.nomad_provider_key_file
-}
-
-provider "vault" {
-  address         = var.vault_provider_address
-  skip_tls_verify = var.vault_provider_skip_tls_verify
-  token           = var.vault_provider_token
 }
