96928308212095aaabd52fc8778b11f4fa8bdcad
Vratko Polak <vrpolak@cisco.com>
2026-02-04 12:27:39 +0000

    WiP: SFDP ip4
    Change-Id: Ie9486244588ff85297b7ea8b1d65b505252ed423
    Signed-off-by: Vratko Polak <vrpolak@cisco.com>

---
+++ resources/libraries/python/sfdp.py
@@ -0,0 +1,148 @@
+# Copyright (c) 2026 Cisco and/or its affiliates.
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at:
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""SFDP library."""
+
+import re
+from time import monotonic, sleep
+
+from robot.api import logger
+
+from resources.libraries.python.PapiExecutor import PapiSocketExecutor
+
+
+class Sfdp:
+    """General utilities for managing SFDP functionality in VPP"""
+
+    @staticmethod
+    def add_sfdp_tenant(node, tenant_id=1):
+        """FIXME"""
+        cmd = "sfdp_tenant_add_del"
+        err_msg = "Failed to add SFDP tenant"
+        args = dict(tenant_id=tenant_id, context_id=tenant_id, is_del=0)
+        with PapiSocketExecutor(node) as papi_exec:
+            papi_exec.add(cmd, **args).get_reply(err_msg)
+        cmd = "sfdp_set_timeout"
+        args = dict(tenant_id=tenant_id, timeout_id=0, timeout_value=300)
+        with PapiSocketExecutor(node) as papi_exec:
+            papi_exec.add(cmd, **args).get_reply(err_msg)
+            args["timeout_id"] += 1
+            papi_exec.add(cmd, **args).get_reply(err_msg)
+            args["timeout_id"] += 1
+            papi_exec.add(cmd, **args).get_reply(err_msg)
+            args["timeout_id"] += 1
+            papi_exec.add(cmd, **args).get_reply(err_msg)
+
+    @staticmethod
+    def enable_sfdp_interface_input(node, sw_if_index, tenant_id=1):
+        """FIXME"""
+        cmd = "sfdp_interface_input_set"
+        err_msg = f"Failed to enable SFDP input on {sw_if_index=}"
+        args = dict(sw_if_index=sw_if_index, tenant_id=tenant_id, is_del=0)
+        with PapiSocketExecutor(node) as papi_exec:
+            papi_exec.add(cmd, **args).get_reply(err_msg)
+
+    @staticmethod
+    def set_sfdp_services(node, services, tenant_id=1):
+        """FIXME"""
+        cmd = "sfdp_set_services"
+        err_msg = "Failed to enable IP4 for SFDP"
+        args = dict(
+            tenant_id=tenant_id,
+            services=[dict(data=service) for service in services],
+            n_services=len(services),
+        )
+        with PapiSocketExecutor(node) as papi_exec:
+            papi_exec.add(cmd, **args).get_reply(err_msg)
+        args["dir"] = 1
+        with PapiSocketExecutor(node) as papi_exec:
+            papi_exec.add(cmd, **args).get_reply(err_msg)
+
+    @staticmethod
+    def get_remaining_sfdp_tenant_sessions(node, tenant_id=1, log=True):
+        """FIXME"""
+        detail = None
+        try:
+            cmd = f"show sfdp tenant {tenant_id} detail"
+            detail = PapiSocketExecutor.run_cli_cmd(node, cmd, log=False)
+            created_match = re.search(r"created sessions:\s+(\d+)", detail)
+            if not created_match:
+                raise ValueError(f"{detail=} lacks created sessions.")
+            created_sessions = int(created_match.group(1))
+            removed_match = re.search(r"removed sessions:\s+(\d+)", detail)
+            if not removed_match:
+                raise ValueError(f"{detail=} lacks removed sessions.")
+            removed_sessions = int(removed_match.group(1))
+        except (ValueError, OSError) as err:
+            raise RuntimeError(f"{detail=} {err=}") from err
+        remaining_sessions = created_sessions - removed_sessions
+        if log:
+            logger.info(
+                f"SFDP sessions: {created_sessions} - {removed_sessions}"
+                f" = {remaining_sessions}"
+            )
+        return remaining_sessions, detail
+
+    @staticmethod
+    def wait_for_no_remaining_sfdp_sessions(
+        node, tenant_id=1, delay=0.1, timeout=10
+    ):
+        """FIXME"""
+        time_stop = monotonic() + timeout
+        while monotonic() < time_stop:
+            remaining, _ = Sfdp.get_remaining_sfdp_tenant_sessions(
+                node, tenant_id
+            )
+            if not remaining:
+                break
+            sleep(delay)
+        else:
+            raise RuntimeError("Timeout waiting for session table clear.")
+
+    @staticmethod
+    def log_sfdp_tenants(node):
+        """FIXME"""
+        cmd = "sfdp_tenant_dump"
+        err_msg = "Failed to get SFDP tenants"
+        args = dict()
+        with PapiSocketExecutor(node) as papi_exec:
+            tenants = papi_exec.add(cmd, **args).get_details(err_msg)
+        logger.info(f"{tenants=}")
+        _, detail = Sfdp.get_remaining_sfdp_tenant_sessions(node, log=False)
+        logger.info(f"{detail=}")
+
+    @staticmethod
+    def log_sfdp_sessions(node):
+        """FIXME"""
+        cmd = "sfdp_session_dump"
+        err_msg = "Failed to get SFDP sessions"
+        args = dict()
+        with PapiSocketExecutor(node) as papi_exec:
+            sessions = papi_exec.add(cmd, **args).get_details(err_msg)
+        logger.info(f"{sessions=}")
+        PapiSocketExecutor.run_cli_cmd(node, "show features verbose")
+
+    @staticmethod
+    def create_sfdp_resetter(node):
+        """FIXME"""
+
+        def kill_all_sfdp_sessions():
+            """FIXME"""
+            cmd = "sfdp_kill_session"
+            err_msg = "Failed to kill all SFDP sessions"
+            args = dict(is_all=True)
+            with PapiSocketExecutor(node) as papi_exec:
+                papi_exec.add(cmd, **args).get_reply(err_msg)
+            Sfdp.wait_for_no_remaining_sfdp_sessions(node)
+
+        return kill_all_sfdp_sessions
