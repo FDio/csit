ded9526e3789e40322491d65f06fcc4644813d89
Tibor Frank <tifrank@cisco.com>
2026-02-16 14:23:07 +0000

    hfr: Just to prevent loss of it
    Change-Id: I3febae3b982931a3896bae985bce4ba7abdca92b
    Signed-off-by: Tibor Frank <tifrank@cisco.com>

---
+++ hfr/hfr/app_configuration.py
@@ -0,0 +1,290 @@
+# Copyright (c) 2026 Cisco and/or its affiliates.
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at:
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""
+Application configuration
+=========================
+
+The application configuration is read from two sources:
+1. environment
+2. configuration file.
+
+All listed parameters are mandatory if it is not marked "optional".
+
+1. Environment
+   - GITHUB_URL
+   - GITHUB_ACCOUNT
+   - GITHUB_REPO
+   - GITHUB_PAT
+   - PATH_CONFIG
+
+2. Configuration file
+   - logging_level
+   - sleep_interval
+   - run_config - path to the run configuration file
+   - oper_data - path to the operational data.
+   - keep_old_oper_data (optional)
+   - dir_testbeds - path to the testbeds specifications
+
+Structure of application configuration data after processing
+------------------------------------------------------------
+
+{
+    "github": {
+        "url": str,
+        "account": str,
+        "repo": str,
+        "pat": str
+    },
+    "app": {
+        "path_config": str,
+        "logging_level": str,
+        "sleep_interval": int,
+        "run_config": str,
+        "oper_data": str,
+        "keep_old_oper_data": bool,
+        "dir_testbeds": str
+    }
+}
+
+"""
+
+
+import logging
+import os
+
+from yaml import load, FullLoader, YAMLError
+
+from hfr.constants import Constants as C
+
+
+class AppConfiguration:
+    """Read, validate and provide the application configuration.
+    """
+
+    def __init__(self):
+        self._conf = {
+            "github": dict(),
+            "app": dict()
+        }
+
+    def _validate_conf(self, conf_source: str):
+        """Validate the read configuration.
+
+        :param conf_source: The source of configuration, either environment or
+            configuration file. The valid values are:
+            - env
+            - file
+        :type conf_source: str
+        :raises RuntimeError: if a parameter is missing or it is invalid.
+        """
+
+        valid = True
+
+        # Validate parameters read from the environment.
+        if conf_source == "env":
+            for key, val in self._conf["github"].items():
+                if not val:
+                    valid = False
+                    logging.error(
+                        f"The value of 'GITHUB_{key.upper()}' must be defined "
+                        f"in the environment."
+                    )
+            if not self._conf["app"]["path_config"]:
+                valid = False
+                logging.error(
+                    "The value of 'PATH_CONFIG' must be defined in the "
+                    "environment."
+                )
+        # Validate parameters read from the configuration file.
+        elif conf_source == "file":
+            # Logging level.
+            log_level = \
+                self._conf["app"].get("logging_level", C.DEFAULT_LOG_LEVEL)
+            if log_level not in C.LOGGING_LEVEL:
+                logging.warning(
+                    f"The logging level '{log_level}' is incorrect, will be set"
+                    f" to '{C.DEFAULT_LOG_LEVEL}'."
+                )
+                self._conf["app"]["logging_level"] = C.DEFAULT_LOG_LEVEL
+            # Sleep interval.
+            sleep_interval = \
+                self._conf["app"].get("sleep_interval",C.DEFAULT_SLEEP_INTERVAL)
+            try:
+                sleep_interval = int(sleep_interval)
+                if sleep_interval < C.MIN_SLEEP_INTERVAL or \
+                        sleep_interval > C.MAX_SLEEP_INTERVAL:
+                    logging.warning(
+                        f"The sleep interval '{sleep_interval}' is out of the "
+                        f"range from {C.MIN_SLEEP_INTERVAL} to "
+                        f"{C.MAX_SLEEP_INTERVAL}, will be set to "
+                        f"{C.DEFAULT_SLEEP_INTERVAL}"
+                    )
+                    sleep_interval = C.DEFAULT_SLEEP_INTERVAL
+                self._conf["app"]["sleep_interval"] = sleep_interval
+            except ValueError:
+                logging.warning(
+                    f"The 'sleep_interval = {sleep_interval}' is incorrect, "
+                    f"will be set to {C.DEFAULT_SLEEP_INTERVAL}"
+                )
+                self._conf["app"]["sleep_interval"] = C.DEFAULT_SLEEP_INTERVAL
+            # Testbeds topologies.
+            if not self._conf["app"].get("dir_testbeds", None):
+                valid = False
+                logging.error(
+                    "The path to testbeds configuration must be defined."
+                )
+            # Run configuration file.
+            if not self._conf["app"].get("run_config", None):
+                valid = False
+                logging.error("The path to run configuration must be defined.")
+            # Operational data file.
+            if not self._conf["app"].get("oper_data", None):
+                valid = False
+                logging.error(
+                    "The path to file with operational data must be defined."
+                )
+            # CSV data file.
+            if not self._conf["app"].get("csv_data", None):
+                valid = False
+                logging.error("The path to file with CSV data must be defined.")
+            # Re-runs.
+            if not self._conf["app"].get("re_run_canceled", None):
+                self._conf["app"]["re_run_canceled"] = C.DEFAULT_RE_RUN_CANCELED
+            if not self._conf["app"].get("nr_of_re_runs", None):
+                self._conf["app"]["nr_of_re_runs"] = C.DEFAULT_NR_OF_RE_RUNS
+        # There is no other way.
+        else:
+            raise RuntimeError(
+                f"The configuration method '{conf_source}' is not supported."
+            )
+
+        # Conclusion.
+        if not valid:
+            raise RuntimeError("The application configuration is not valid.")
+
+    def read_conf(self):
+        """Read the configuration from envoronment at first and then from
+        specified configuration file.
+        """
+
+        logging.info("Reading the application configuration...")
+
+        # Read the configuration from the environment.
+        self._conf["github"] = {
+            "url": os.getenv("GITHUB_URL", C.DEFAULT_GITHUB_URL),
+            "account": os.getenv("GITHUB_ACCOUNT", C.DEFAULT_GITHUB_ACCOUNT),
+            "repo": os.getenv("GITHUB_REPO", C.DEFAULT_GITHUB_REPO),
+            "pat": os.getenv("GITHUB_PAT", C.DEFAULT_GITHUB_PAT)
+        }
+        self._conf["app"]["path_config"] = \
+            os.getenv("PATH_CONFIG", C.DEFAULT_PATH_CONFIG)
+
+        self._validate_conf(conf_source="env")
+
+        # Read the configuration from the configuration file.
+        try:
+            with open(self._conf["app"]["path_config"], "r") as file_read:
+                self._conf["app"].update(load(file_read, Loader=FullLoader))
+        except IOError as err:
+            raise RuntimeError(
+                f"Not possible to open the file "
+                f"{self._conf['app']['path_config']}\n {err}"
+            )
+        except YAMLError as err:
+            raise RuntimeError(
+                f"An error occurred while parsing the application configuration"
+                f" file {self._conf['app']['path_config']}\n"
+                f"{err}"
+            )
+
+        self._validate_conf(conf_source="file")
+
+        # logging.debug(self._conf)
+
+        logging.info("Done.")
+
+    @property
+    def conf(self):
+        """Application configuration.
+        :rtype: dict
+        """
+        return self._conf
+
+    @property
+    def logging_level(self):
+        """Logging level
+        :rtype: str
+        """
+        return self._conf["app"]["logging_level"]
+
+    @property
+    def sleep_interval(self):
+        """Interval between runs of run management in seconds.
+        :rtype: int
+        """
+        return self._conf["app"]["sleep_interval"]
+
+    @property
+    def github(self):
+        """GitHub parameters.
+        :rtype: dict
+        """
+        return self._conf["github"]
+
+    @property
+    def run_config_file(self):
+        """Path to the run configuration file.
+        :rtype: str
+        """
+        return self._conf["app"]["run_config"]
+
+    @property
+    def path_to_tbs(self):
+        """Path to the testbeds configuration files.
+        :rtype: str
+        """
+        return self._conf["app"]["dir_testbeds"]
+
+    @property
+    def path_to_oper(self):
+        """Path to the operational data.
+        :rtype: str
+        """
+        return self._conf["app"]["oper_data"]
+
+    @property
+    def path_to_csv(self):
+        """Path to the csv data.
+        :rtype: str
+        """
+        return self._conf["app"]["csv_data"]
+
+    @property
+    def keep_old_oper_data(self):
+        """Keep the file with old operational data and use it.
+        :rtype: bool
+        """
+        return self._conf["app"].get("keep_old_oper_data", False)
+
+    @property
+    def re_run_canceled(self):
+        """If True re-run the canceled runs.
+        """
+        return self._conf["app"]["re_run_canceled"]
+
+    @property
+    def nr_of_re_runs(self):
+        """Number of re-runs of the canceled runs.
+        """
+        return self._conf["app"]["nr_of_re_runs"]
---
+++ hfr/hfr/run_management.py
@@ -0,0 +1,305 @@
+# Copyright (c) 2026 Cisco and/or its affiliates.
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at:
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Run management
+"""
+
+
+import logging
+
+from datetime import datetime, timezone
+
+from hfr.app_configuration import AppConfiguration
+from hfr.intf_gha import Gha
+from hfr.intf_tb import Testbed, Testbeds
+from hfr.oper_data import OperData, Run
+
+
+class RunManagement:
+    """Run Management
+    """
+
+    def __init__(self, app_conf: AppConfiguration, oper: OperData, gha: Gha,
+                 tbs: Testbeds):
+        """Initialization.
+        """
+
+        self._app_conf = app_conf
+        self._oper = oper
+        self._gha = gha
+        self._tbs = tbs
+
+        self._runs_count = len(self._oper.data)
+        self._runs_done = 0
+
+        self._states_map = {
+            "queued": self._queued,
+            "started": self._started,
+            "waiting": self._waiting,
+            "running": self._running,
+            "cancelled": self._cancelled,
+            "finished": self._finished
+        }
+
+        logging.info("Run management started successfully.")
+
+    @staticmethod
+    def _generate_id():
+        """Based on the timestamp.
+
+        :returns: Last ten digits of the timestamp as a string.
+        :rtype: str
+        """
+        id = str(int(datetime.now(timezone.utc).timestamp() * 1e6))
+        return id[-10:]
+
+    def _get_free_tb(self, node: str) -> Testbed:
+        """Get a free testbed of given node-arch. It searches in the previously
+            populated list of testbeds, it does not checks the testbeds. It is
+            done so to save time. The first available is returned. If there is
+            no available testbed, returns None.
+
+        :param node: The node-arch (e.g. 2n-emr) of the testbed.
+        :type node: str
+        :returns: An available testbed or None.
+        :rtype: Testbed
+        """
+        for tb in self._tbs.testbeds:
+            if node == tb.node and tb.status == "available":
+                return tb
+        else:
+            return None
+
+    def _get_my_tb(self, run: Run) -> Testbed:
+        """Get the testbed where the "run" is running.
+        """
+        for tb in self._tbs.testbeds:
+            if run.node == tb.node and tb.status == "reserved":
+                if str(run.data.gha_data.get("id", str())) == tb.run_id:
+                    return tb
+        else:
+            return None
+
+    def _to_start(self, run: Run) -> bool:
+        """Start the run.
+        """
+        ret_val = self._gha.start_run(run)
+        return ret_val[0]
+
+    def _run_status(self, run: Run) -> tuple:
+        """Return the run status, conclusion, and the run operational data.
+        """
+        status, data = self._gha.get_run_status(run)
+        if status:
+            return data.get("status", None), data.get("conclusion", None), data
+        else:
+            return None, None, None
+
+    def _to_cancel(self, run: Run) -> bool:
+        """Cancel the run.
+        """
+        ret_val = self._gha.cancel_run(run)
+        return ret_val[0]
+
+    @staticmethod
+    def _print_run(run: Run):
+        # Only for the development. To be removed
+        id = f"{run.node}-{run.data.hfr_id}" if run.data.hfr_id else run.node
+        logging.info(f"{id}: {run.data.hfr_status}")
+
+    # States 23289764485
+
+    def _queued(self, run: Run):
+        """HFR run state: queued.
+
+        Possible transitions:
+        1. queued -> queued
+           - if there is no accessible testbed
+        2. queued -> started
+           - if there is an accessible testbed
+
+        Procedure:
+        1. Find testbed
+        2. If testbed, start run, otherwise return
+        3. change status to "started"
+
+        :param run: The run to be manipulated.
+        :type run: Run
+        """
+
+        # Find an available testbed
+        free_tb = self._get_free_tb(run.node)
+        if not free_tb:
+            return
+
+        # Pre-reserve the testbed. Keep in mind that another testbed can be used
+        # by this run, this is just to mark that one testbed (of this topo-arch)
+        # will be used.
+        free_tb.status = "pre-reserved"
+        run.tb = free_tb  # Could be replaced later.
+        # Set the parameters of a run - ID.
+        run.data.hfr_id = self._generate_id()
+        run.conf.inputs["my_run_id"] = run.data.hfr_id
+        # Start the run
+        started = self._to_start(run)
+        if started:
+            run.data.hfr_status = "started"
+
+    def _started(self, run: Run):
+        """HFR run state: started in GHA but not running.
+
+        Possible transitions:
+        1. started -> started
+           - If the run is not running in GHA.
+        2. started -> waiting
+           - The run is running in GHA but it is waiting for a testbed.
+        3. started -> finished
+           - If the run finished.
+        4. started -> canceled
+           - If the run was canceled either by GHA, HFR or manually.
+
+        :param run: The run to be manipulated.
+        :type run: Run
+        """
+
+        status, conclusion, run.data.gha_data = self._run_status(run)
+        if status == "in_progress":
+            run.data.hfr_status = "waiting"  # for a testbed
+        elif status == "completed":
+            if conclusion == "cancelled":
+                run.data.hfr_status = "cancelled"
+            else:
+                run.data.hfr_status = "finished"
+        # else: The run remains in the state "started".
+
+    def _waiting(self, run: Run):
+        """HFR run state: waiting for a testbed.
+
+        Possible transitions:
+        1. waiting -> waiting
+           - Still waiting for a testbed.
+        2. waiting -> running
+           - Running on a testbed.
+        3. waiting -> finished
+           - If the run finished.
+        4. waiting -> canceled
+           - If the run was canceled either by GHA or HFR.
+
+        :param run: The run to be manipulated.
+        :type run: Run
+        """
+
+        status, conclusion, run.data.gha_data = self._run_status(run)
+        if status == "in_progress":
+            # Check the TBs. If the run is on a testbed, change state to
+            # "running", otherwise keep the state as it is.
+            my_tb = self._get_my_tb(run)
+            if my_tb:
+                run.tb.status = "available"
+                run.tb = my_tb
+                run.data.hfr_status = "running"
+        elif status == "completed":
+            if conclusion == "cancelled":
+                run.data.hfr_status = "cancelled"
+            else:
+                run.data.hfr_status = "finished"
+        # else: The run remains in the state "waiting".
+
+    def _running(self, run: Run):
+        """HFR run state: running on a testbed.
+
+        Possible transitions:
+        1. running -> running
+           - If the run still runs on the testbed (no change in TB nor
+             GHA status)
+        2. running -> canceled
+           - If the run was canceled either by GHA or HFR.
+        3. running -> finished
+           - If the run finished.
+
+        :param run: The run to be manipulated.
+        :type run: Run
+        """
+
+        status, conclusion, run.data.gha_data = self._run_status(run)
+        if status == "completed":
+            if conclusion == "cancelled":
+                run.data.hfr_status = "cancelled"
+            else:
+                run.data.hfr_status = "finished"
+        # else: The run remains in the state "running".
+
+    def _cancelled(self, run: Run):
+        """HFR run state: canceled (by GHA, HFR or manually).
+
+        If "re-run" is enabled and the max count is not reached, change the
+        status to"queued" and incerase the counter of runs. Otherwise change the
+        status to "finished".
+
+        :param run: The run to be manipulated.
+        :type run: Run
+        """
+
+        if not self._app_conf.re_run_canceled or \
+                run.data.nr_of_runs == self._app_conf.nr_of_re_runs:
+            run.data.hfr_status = "finished"
+        else:
+            run.data.hfr_status = "queued"
+            run.data.hfr_id = str()
+            run.data.gha_data = dict()
+            run.data.nr_of_runs += 1
+            if run.tb.status == "pre-reserved":
+                run.tb.status = "available"
+            run.tb = None
+
+    def _finished(self, run: Run):
+        """HFR run state: finished.
+
+        If the run is done, continue with the next one for the same node-arch.
+
+        :param run: The run to be manipulated.
+        :type run: Run
+        """
+
+        # Check the status of the testbed ???
+
+        if run.tb.status == "pre-reserved":
+            run.tb.status = "available"
+
+        self._runs_done += 1
+
+    def manage(self) -> bool:
+        """Manage the runs.
+        """
+
+        # Update the status of testbeds
+        self._tbs.check_testbeds()
+
+        self._runs_done = 0
+        for run in self._oper.data:
+            RunManagement._print_run(run)
+            try:
+                self._states_map[run.data.hfr_status](run)
+            except KeyError:
+                raise RuntimeError(
+                    f"The run status '{run.data.hfr_status}' is invalid."
+                )
+
+        # Write the operational data
+        self._oper.to_json_file()
+
+        # Write a subset of operational data to csv file.
+        self._oper.to_csv_file()
+
+        # If True, the HFR did all its work and can exit.
+        return self._runs_done != self._runs_count
