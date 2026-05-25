e7fe1a90bfb9831e73ca357632f2076bfee1bace
Peter Mikus <peter.mikus@icloud.com>
2026-05-25 09:28:17 +0200

    infra: aws c8gn
    Signed-off-by: Peter Mikus <peter.mikus@icloud.com>
    Change-Id: I254a86fc2c7bcb79771a64b6cbb9e57e73414c89
    (cherry picked from commit fa0c6a141f22b57ff3149ee4c44ea85a601ab9b6)

--- fdio.infra.terraform/terraform-aws-2n-c8gn/variables.tf
+++ fdio.infra.terraform/terraform-aws-2n-c8gn/variables.tf
@@ -56,7 +56,7 @@ variable "tg_instance_initiated_shutdown_behavior" {
 variable "tg_instance_type" {
   description = "The instance type to use for the instance."
   type        = string
-  default     = "c6in.4xlarge"
+  default     = "c5n.4xlarge"
 }
 
 variable "tg_private_ip" {
