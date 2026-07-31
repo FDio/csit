7e88cffba9c3a04375781eef46cd6c519f6fbd4e
Vratko Polak <vrpolak@cisco.com>
2026-07-31 13:32:36 +0000

    feat(crc): Support SFDP changes
    + Use the YAML anchor/merge system.
    + Make collection selection visible in console log.
    - Do not use sfdp_timeout_dump yet.
    - One more change will follow when 46362 adds sfdp_kill_session_batch.
    
    Change-Id: I7cc4da03e334ebc4cc777312dc15bf3d1624f338
    Signed-off-by: Vratko Polak <vrpolak@cisco.com>
    (cherry picked from commit ed3f0f135a8282bcc26e555608b28ea894221f87)

--- resources/libraries/python/VppApiCrc.py
+++ resources/libraries/python/VppApiCrc.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2023 Cisco and/or its affiliates.
+# Copyright (c) 2026 Cisco and/or its affiliates.
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at:
@@ -290,7 +290,7 @@ class VppApiCrcChecker:
                     msg_crc = self._get_crc(msg_obj)
                     msg_options = self._get_options(msg_obj, version)
                     self._process_crc(msg_name, msg_crc, msg_options)
-        logger.debug(f"Surviving collections: {self._expected.keys()!r}")
+        logger.console(f"Surviving CRC collections: {self._expected.keys()!r}")
 
     def report_initial_conflicts(self, report_missing=False):
         """Report issues discovered by _check_dir, if not done that already.
