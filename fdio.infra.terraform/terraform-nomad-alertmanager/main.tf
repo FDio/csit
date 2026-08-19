1e53b2919ba1f1550300aa0064352f41f4a5f7d9
Peter Mikus <peter.mikus@icloud.com>
2026-08-19 12:51:16 +0200

    infra: alerting rise up
    Signed-off-by: Peter Mikus <peter.mikus@icloud.com>
    Change-Id: Ic6d0d27c24325d4896efccb9af9f11446f80fc3c

--- fdio.infra.terraform/terraform-nomad-alertmanager/main.tf
+++ fdio.infra.terraform/terraform-nomad-alertmanager/main.tf
@@ -1,48 +1,9 @@
-locals {
-  datacenters = join(",", var.datacenters)
-  url         = join("",
-    [
-      "https://github.com",
-      "/prometheus/alertmanager/releases/download/",
-      "v${var.am_version}/",
-      "alertmanager-${var.am_version}.linux-amd64.tar.gz"
-    ]
-  )
-}
-
-resource "nomad_job" "nomad_job_alertmanager" {
-  jobspec = templatefile(
-    "${path.module}/conf/nomad/alertmanager.hcl.tftpl",
-    {
-      auto_promote              = var.auto_promote,
-      auto_revert               = var.auto_revert,
-      canary                    = var.canary,
-      cpu                       = var.cpu,
-      datacenters               = local.datacenters,
-      group_count               = var.group_count,
-      job_name                  = var.job_name,
-      max_parallel              = var.max_parallel,
-      memory                    = var.memory
-      port                      = var.port,
-      region                    = var.region,
-      service_name              = var.service_name,
-      slack_jenkins_api_key     = var.slack_jenkins_api_key,
-      slack_jenkins_channel     = var.slack_jenkins_channel,
-      slack_jenkins_receiver    = var.slack_jenkins_receiver,
-      slack_default_api_key     = var.slack_default_api_key,
-      slack_default_channel     = var.slack_default_channel,
-      slack_default_receiver    = var.slack_default_receiver,
-      url                       = local.url,
-      use_canary                = var.use_canary,
-      use_host_volume           = var.use_host_volume,
-      use_vault_provider        = var.vault_secret.use_vault_provider,
-      vault_kv_policy_name      = var.vault_secret.vault_kv_policy_name,
-      vault_kv_path             = var.vault_secret.vault_kv_path,
-      vault_kv_field_access_key = var.vault_secret.vault_kv_field_access_key,
-      vault_kv_field_secret_key = var.vault_secret.vault_kv_field_secret_key,
-      version                   = var.am_version,
-      volume_destination        = var.volume_destination,
-      volume_source             = var.volume_source
-  })
+resource "nomad_job" "nomad_job" {
+  jobspec = file("${path.cwd}/conf/nomad/alertmanager.hcl")
+  hcl2 {
+    vars = {
+        version = "0.34.0"
+    }
+  }
   detach = false
 }
--- fdio.infra.terraform/terraform-nomad-alertmanager/variables.tf
+++ fdio.infra.terraform/terraform-nomad-alertmanager/variables.tf
@@ -1,157 +1,22 @@
-# Nomad
-variable "datacenters" {
-  description = "Specifies the list of DCs to be considered placing this task"
-  type        = list(string)
-  default     = ["dc1"]
-}
-
-variable "region" {
-  description = "Specifies the list of DCs to be considered placing this task"
-  type        = string
-  default     = "global"
-}
-
-variable "volume_source" {
-  description = "The name of the volume to request"
-  type        = string
-  default     = "persistence"
-}
-
-# Alertmanager
-variable "am_version" {
-  description = "Alertmanager version"
-  type        = string
-  default     = "0.21.0"
-}
-
-variable "auto_promote" {
-  description = "Specifies if the job should auto-promote to the canary version"
-  type        = bool
-  default     = true
-}
-
-variable "auto_revert" {
-  description = "Specifies if the job should auto-revert to the last stable job"
-  type        = bool
-  default     = true
-}
-
-variable "canary" {
-  description = "Equal to the count of the task group allows blue/green depl."
-  type        = number
-  default     = 1
-}
-
-variable "cpu" {
-  description = "CPU allocation"
-  type        = number
-  default     = 1000
-}
-
-variable "group_count" {
-  description = "Specifies the number of the task groups running under this one"
-  type        = number
-  default     = 1
-}
-
-variable "job_name" {
-  description = "Specifies a name for the job"
-  type        = string
-  default     = "alertmanager"
-}
-
-variable "max_parallel" {
-  description = "Specifies the maximum number of updates to perform in parallel"
-  type        = number
-  default     = 1
-}
-
-variable "memory" {
-  description = "Specifies the memory required in MB"
-  type        = number
-  default     = 1024
-}
-
-variable "port" {
-  description = "Specifies the static TCP/UDP port to allocate"
-  type        = number
-  default     = 9093
-}
-
-variable "service_name" {
-  description = "Specifies the name this service will be advertised in Consul"
-  type        = string
-  default     = "alertmanager"
-}
-
-variable "use_canary" {
-  description = "Uses canary deployment"
-  type        = bool
-  default     = true
-}
-
-variable "use_host_volume" {
-  description = "Use Nomad host volume feature"
+variable "nomad_acl" {
+  description = "Nomad ACLs enabled/disabled."
   type        = bool
   default     = false
 }
-
-variable "vault_secret" {
-  type = object({
-    use_vault_provider        = bool,
-    vault_kv_policy_name      = string,
-    vault_kv_path             = string,
-    vault_kv_field_access_key = string,
-    vault_kv_field_secret_key = string
-  })
-  description = "Set of properties to be able to fetch secret from vault."
-  default = {
-    use_vault_provider        = false
-    vault_kv_policy_name      = "kv"
-    vault_kv_path             = "secret/data/alertmanager"
-    vault_kv_field_access_key = "access_key"
-    vault_kv_field_secret_key = "secret_key"
-  }
+variable "nomad_provider_ca_file" {
+  description = "A local file path to a PEM-encoded certificate authority."
+  type        = string
+  default     = "/etc/nomad.d/ssl/nomad-ca.pem"
 }
 
-variable "volume_destination" {
-  description = "Specifies where the volume should be mounted inside the task"
+variable "nomad_provider_cert_file" {
+  description = "A local file path to a PEM-encoded certificate."
   type        = string
-  default     = "/data/"
+  default     = "/etc/nomad.d/ssl/nomad.pem"
 }
 
-variable "slack_jenkins_api_key" {
-  description = "Alertmanager jenkins slack API key"
+variable "nomad_provider_key_file" {
+  description = "A local file path to a PEM-encoded private key."
   type        = string
-  default     = "XXXXXXXXX/XXXXXXXXXXX/XXXXXXXXXXXXXXXXXXXXXXXX"
-}
-
-variable "slack_jenkins_receiver" {
-  description = "Alertmanager jenkins slack receiver"
-  type        = string
-  default     = "jenkins-slack-receiver"
-}
-
-variable "slack_jenkins_channel" {
-  description = "Alertmanager jenkins slack channel"
-  type        = string
-  default     = "jenkins-channel"
-}
-
-variable "slack_default_api_key" {
-  description = "Alertmanager default slack API key"
-  type        = string
-  default     = "XXXXXXXXX/XXXXXXXXXXX/XXXXXXXXXXXXXXXXXXXXXXXX"
-}
-
-variable "slack_default_receiver" {
-  description = "Alertmanager default slack receiver"
-  type        = string
-  default     = "default-slack-receiver"
-}
-
-variable "slack_default_channel" {
-  description = "Alertmanager default slack channel"
-  type        = string
-  default     = "default-channel"
+  default     = "/etc/nomad.d/ssl/nomad-key.pem"
 }
