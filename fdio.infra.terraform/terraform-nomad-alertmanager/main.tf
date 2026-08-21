ea65dc9e329418c46f75643aa233e189648d8837
Peter Mikus <peter.mikus@icloud.com>
2026-08-21 21:07:51 +0200

    chore: remove nomad services
    Signed-off-by: Peter Mikus <peter.mikus@icloud.com>
    Change-Id: I6926195bd18dba6535d7b299963c2764f1f8bd94

--- fdio.infra.terraform/terraform-nomad-alertmanager/main.tf
+++
@@ -1,9 +0,0 @@
-resource "nomad_job" "nomad_job" {
-  jobspec = file("${path.cwd}/conf/nomad/alertmanager.hcl")
-  hcl2 {
-    vars = {
-        version = "0.34.0"
-    }
-  }
-  detach = false
-}
--- fdio.infra.terraform/terraform-nomad-prometheus/main.tf
+++
@@ -1,9 +0,0 @@
-resource "nomad_job" "nomad_job" {
-  jobspec = file("${path.cwd}/conf/nomad/prometheus.hcl")
-  hcl2 {
-    vars = {
-        version = "3.14.0"
-    }
-  }
-  detach = false
-}
