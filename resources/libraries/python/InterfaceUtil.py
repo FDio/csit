037331005de3616da0f96eb33976f4eca58d8ab6
Vratko Polak <vrpolak@cisco.com>
2026-09-03 14:42:21 +0000

    Experiments: rdma
    + More MRR trials.
    
    Change-Id: I21ab16bc4fd4d30c93fcf935c3267dd5832e8346
    Signed-off-by: Vratko Polak <vrpolak@cisco.com>

--- resources/libraries/python/InterfaceUtil.py
+++ resources/libraries/python/InterfaceUtil.py
@@ -1384,8 +1384,7 @@ class InterfaceUtil:
             rxq_size=rxq_size,
             txq_size=txq_size,
             mode=getattr(RdmaMode, f"RDMA_API_MODE_{mode.upper()}").value,
-            # Note: Set True for non-jumbo packets.
-            no_multi_seg=False,
+            no_multi_seg=not BuiltIn().get_variable_value("\\${jumbo}", False),
             max_pktlen=0,
             # TODO: Apply desired RSS flags.
             # rss4 kept 0 (auto) as API default.
