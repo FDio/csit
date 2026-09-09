5b61a86b5cae863799cfc5a6be3a47679d72cd70
Vratko Polak <vrpolak@cisco.com>
2026-09-09 12:55:30 +0000

    feat(rdma): enable packet trace and handle jumbo
    Change-Id: If6403b4b6b5da54f294e8c4ff4ca751da801a609
    Signed-off-by: Vratko Polak <vrpolak@cisco.com>

--- resources/libraries/python/InterfaceUtil.py
+++ resources/libraries/python/InterfaceUtil.py
@@ -1385,8 +1385,7 @@ class InterfaceUtil:
             rxq_size=rxq_size,
             txq_size=txq_size,
             mode=getattr(RdmaMode, f"RDMA_API_MODE_{mode.upper()}").value,
-            # Note: Set True for non-jumbo packets.
-            no_multi_seg=False,
+            no_multi_seg=not Jumbo.jumbo_enabled(),
             max_pktlen=0,
             # TODO: Apply desired RSS flags.
             # rss4 kept 0 (auto) as API default.
