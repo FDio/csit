95b9654aeacdf2d1833c84d79d75a5ecd89ce25e
Peter Mikus <peter.mikus@icloud.com>
2026-09-24 19:49:46 +0200

    terraform: 2n-c8in
    Signed-off-by: Peter Mikus <peter.mikus@icloud.com>
    Change-Id: I2313dbdb2cc35be1f1c2917270476751e13c55fa

--- fdio.infra.terraform/terraform-aws-fdio-csit-dash-env/versions.tf
+++ fdio.infra.terraform/terraform-aws-fdio-csit-dash-env/versions.tf
@@ -7,11 +7,11 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 5.83.1"
+      version = ">= 6.27.0, < 7.0.0"
     }
     vault = {
-      version = ">= 4.6.0"
+      version = "5.12.0"
     }
   }
-  required_version = ">= 1.10.4"
+  required_version = ">= 1.14.6"
 }
